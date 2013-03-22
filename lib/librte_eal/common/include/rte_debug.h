9b226335bb0357b0152c68fa57d1bc335ac1626c
Thomas Monjalon <thomas.monjalon@6wind.com>
2013-03-22 16:34:54 +0100

    doc: fix some doxygen comments
    - rte_panic must be before rte_panic_ to be associated to its doc
    - marker /**< must be used when commenting after the declaration only
    - fix rte_string_fns.h title
    - typos
    
    Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>
    Acked-by: Olivier Matz <olivier.matz@6wind.com>

--- lib/librte_eal/common/include/rte_debug.h
+++ lib/librte_eal/common/include/rte_debug.h
@@ -75,8 +75,8 @@ void rte_dump_registers(void);
  * @param args
  *   The variable list of arguments.
  */
-#define rte_panic_(func, format, ...) __rte_panic(func, format "%.0s", __VA_ARGS__)
 #define rte_panic(...) rte_panic_(__func__, __VA_ARGS__, "dummy")
+#define rte_panic_(func, format, ...) __rte_panic(func, format "%.0s", __VA_ARGS__)
 
 #define	RTE_VERIFY(exp)	do {                                                  \
 	if (!(exp))                                                           \
