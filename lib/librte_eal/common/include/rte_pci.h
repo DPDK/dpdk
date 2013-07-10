b33d322d781f544d3eb258e1f4b3433ad45cacb4
Damien Millescamps <damien.millescamps@6wind.com>
2013-07-10 15:14:52 +0200

    pci: add flag to force unbind device
    Some devices need to be unbound in order to be used via the PMD
    without kernel module.
    
    Signed-off-by: Damien Millescamps <damien.millescamps@6wind.com>
    Acked-by: Thomas Monjalon <thomas.monjalon@6wind.com>

--- lib/librte_eal/common/include/rte_pci.h
+++ lib/librte_eal/common/include/rte_pci.h
@@ -187,6 +187,8 @@ struct rte_pci_driver {
 #define RTE_PCI_DRV_NEED_IGB_UIO 0x0001
 /**< Device driver must be registered several times until failure */
 #define RTE_PCI_DRV_MULTIPLE 0x0002
+/** Device needs to be unbound even if no module is provided */
+#define RTE_PCI_DRV_FORCE_UNBIND 0x0004
 
 /**
  * Perform clean up of pci drivers on application exits.
