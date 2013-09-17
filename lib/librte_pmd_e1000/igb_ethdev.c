c318a7387e9066df8fa58de6fdec6207dab8cf6a
Thomas Monjalon <thomas.monjalon@6wind.com>
2013-09-17 12:00:57 +0200

    pci: use igb_uio mapping only when needed
    Since DPDK 1.4, if RTE_EAL_UNBIND_PORTS is disabled, igb_uio mapping is
    done for all devices (commit eee16c964cd), breaking some non-Intel drivers.
    But pci_uio_map_resource() should only be called for Intel devices
    (using igb_uio kernel module).
    The flag RTE_PCI_DRV_NEED_IGB_UIO is set for all those devices, even when
    RTE_EAL_UNBIND_PORTS is disabled (fixes commit a22f5ce8fcc).
    
    Signed-off-by: David Marchand <david.marchand@6wind.com>
    Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>
    Acked-by: Damien Millescamps <damien.millescamps@6wind.com>

--- lib/librte_pmd_e1000/igb_ethdev.c
+++ lib/librte_pmd_e1000/igb_ethdev.c
@@ -533,9 +533,7 @@ static struct eth_driver rte_igb_pmd = {
 	{
 		.name = "rte_igb_pmd",
 		.id_table = pci_id_igb_map,
-#ifdef RTE_EAL_UNBIND_PORTS
 		.drv_flags = RTE_PCI_DRV_NEED_IGB_UIO,
-#endif
 	},
 	.eth_dev_init = eth_igb_dev_init,
 	.dev_private_size = sizeof(struct e1000_adapter),
@@ -548,9 +546,7 @@ static struct eth_driver rte_igbvf_pmd = {
 	{
 		.name = "rte_igbvf_pmd",
 		.id_table = pci_id_igbvf_map,
-#ifdef RTE_EAL_UNBIND_PORTS
 		.drv_flags = RTE_PCI_DRV_NEED_IGB_UIO,
-#endif
 	},
 	.eth_dev_init = eth_igbvf_dev_init,
 	.dev_private_size = sizeof(struct e1000_adapter),
