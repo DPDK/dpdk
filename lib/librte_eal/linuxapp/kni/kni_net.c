904d29a135381d81d0758f16404d33e99623abd5
Intel <intel.com>
2013-09-18 12:00:00 +0200

    kni: move FIFO functions
    Move FIFO functions into kni_fifo.h in order to reuse it for vhost.
    
    Signed-off-by: Intel

--- lib/librte_eal/linuxapp/kni/kni_net.c
+++ lib/librte_eal/linuxapp/kni/kni_net.c
@@ -38,6 +38,7 @@
 
 #include <rte_config.h>
 #include <exec-env/rte_kni_common.h>
+#include <kni_fifo.h>
 #include "kni_dev.h"
 
 #define WD_TIMEOUT 5 /*jiffies */
@@ -58,68 +59,6 @@ static int kni_net_process_request(struct kni_dev *kni,
 
 /* kni rx function pointer, with default to normal rx */
 static kni_net_rx_t kni_net_rx_func = kni_net_rx_normal;
-
-
-/**
- * Adds num elements into the fifo. Return the number actually written
- */
-static inline unsigned
-kni_fifo_put(struct rte_kni_fifo *fifo, void **data, unsigned num)
-{
-	unsigned i = 0;
-	unsigned fifo_write = fifo->write;
-	unsigned fifo_read = fifo->read;
-	unsigned new_write = fifo_write;
-
-	for (i = 0; i < num; i++) {
-		new_write = (new_write + 1) & (fifo->len - 1);
-
-		if (new_write == fifo_read)
-			break;
-		fifo->buffer[fifo_write] = data[i];
-		fifo_write = new_write;
-	}
-	fifo->write = fifo_write;
-	return i;
-}
-
-/**
- * Get up to num elements from the fifo. Return the number actully read
- */
-static inline unsigned
-kni_fifo_get(struct rte_kni_fifo *fifo, void **data, unsigned num)
-{
-	unsigned i = 0;
-	unsigned new_read = fifo->read;
-	unsigned fifo_write = fifo->write;
-	for (i = 0; i < num; i++) {
-		if (new_read == fifo_write)
-			break;
-
-		data[i] = fifo->buffer[new_read];
-		new_read = (new_read + 1) & (fifo->len - 1);
-	}
-	fifo->read = new_read;
-	return i;
-}
-
-/**
- * Get the num of elements in the fifo
- */
-static inline unsigned
-kni_fifo_count(struct rte_kni_fifo *fifo)
-{
-	return (fifo->len + fifo->write - fifo->read) &( fifo->len - 1);
-}
-
-/**
- * Get the num of available lements in the fifo
- */
-static inline unsigned
-kni_fifo_free_count(struct rte_kni_fifo *fifo)
-{
-	return (fifo->read - fifo->write - 1) & (fifo->len - 1);
-}
 
 /*
  * Open and close
