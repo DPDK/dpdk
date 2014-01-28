d8e2337ac4e32132e5a26dc351604447b341366d
Olivier Matz <olivier.matz@6wind.com>
2014-01-28 17:06:41 +0100

    kvargs: add const attribute in handler parameters
    The "value" argument is read-only and should be const.
    
    Signed-off-by: Olivier Matz <olivier.matz@6wind.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- lib/librte_pmd_pcap/rte_eth_pcap.c
+++ lib/librte_pmd_pcap/rte_eth_pcap.c
@@ -403,10 +403,10 @@ static struct eth_dev_ops ops = {
  * reference of it for use it later on.
  */
 static int
-open_rx_pcap(char *value, void *extra_args)
+open_rx_pcap(const char *value, void *extra_args)
 {
 	unsigned i;
-	char *pcap_filename = value;
+	const char *pcap_filename = value;
 	struct rx_pcaps *pcaps = extra_args;
 	pcap_t *rx_pcap;
 
@@ -426,10 +426,10 @@ open_rx_pcap(char *value, void *extra_args)
  * for use it later on.
  */
 static int
-open_tx_pcap(char *value, void *extra_args)
+open_tx_pcap(const char *value, void *extra_args)
 {
 	unsigned i;
-	char *pcap_filename = value;
+	const char *pcap_filename = value;
 	struct tx_pcaps *dumpers = extra_args;
 	pcap_t *tx_pcap;
 	pcap_dumper_t *dumper;
@@ -476,7 +476,7 @@ open_iface_live(const char *iface, pcap_t **pcap) {
  * Opens an interface for reading and writing
  */
 static inline int
-open_rx_tx_iface(char *value, void *extra_args)
+open_rx_tx_iface(const char *value, void *extra_args)
 {
 	const char *iface = value;
 	pcap_t **pcap = extra_args;
@@ -490,7 +490,7 @@ open_rx_tx_iface(char *value, void *extra_args)
  * Opens a NIC for reading packets from it
  */
 static inline int
-open_rx_iface(char *value, void *extra_args)
+open_rx_iface(const char *value, void *extra_args)
 {
 	unsigned i;
 	const char *iface = value;
@@ -510,7 +510,7 @@ open_rx_iface(char *value, void *extra_args)
  * Opens a NIC for writing packets to it
  */
 static inline int
-open_tx_iface(char *value, void *extra_args)
+open_tx_iface(const char *value, void *extra_args)
 {
 	unsigned i;
 	const char *iface = value;
