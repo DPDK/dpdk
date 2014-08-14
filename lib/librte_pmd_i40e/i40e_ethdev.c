4f41de4fd549172acc5460b5c3df2b7a8f7f08bd
Helin Zhang <helin.zhang@intel.com>
2014-08-14 14:12:53 +0800

    i40e: support xen domain0
    i40e was failing to run in XEN domain0, as the physical
    memory for adminq DMA should be allocated and translated
    in a different way for XEN domain0. So
    rte_memzone_reserve_bounded() should be used for DMA
    memory allocation, and rte_mem_phy2mch() should be used
    for DMA memory address translation to support running
    i40e PMD in XEN domain0.
    
    Signed-off-by: Helin Zhang <helin.zhang@intel.com>
    Tested-by: Zhaochen Zhan <zhaochen.zhan@intel.com>
    Acked-by:  Jijiang Liu <jijiang.liu@intel.com>

--- lib/librte_pmd_i40e/i40e_ethdev.c
+++ lib/librte_pmd_i40e/i40e_ethdev.c
@@ -1515,14 +1515,23 @@ i40e_allocate_dma_mem_d(__attribute__((unused)) struct i40e_hw *hw,
 
 	id++;
 	snprintf(z_name, sizeof(z_name), "i40e_dma_%"PRIu64, id);
+#ifdef RTE_LIBRTE_XEN_DOM0
+	mz = rte_memzone_reserve_bounded(z_name, size, 0, 0, alignment,
+							RTE_PGSIZE_2M);
+#else
 	mz = rte_memzone_reserve_aligned(z_name, size, 0, 0, alignment);
+#endif
 	if (!mz)
 		return I40E_ERR_NO_MEMORY;
 
 	mem->id = id;
 	mem->size = size;
 	mem->va = mz->addr;
+#ifdef RTE_LIBRTE_XEN_DOM0
+	mem->pa = rte_mem_phy2mch(mz->memseg_id, mz->phys_addr);
+#else
 	mem->pa = mz->phys_addr;
+#endif
 
 	return I40E_SUCCESS;
 }
