2eefe6f11b8d8b80ee6322562a6ed9311fccc775
Chao Zhu <bjzhuc@cn.ibm.com>
2014-10-28 13:50:52 +0100

    eal: split prefetch operations to architecture specific
    This patch splits the prefetch operations from DPDK and push them to
    architecture specific arch directories, so that other processor
    architecture to support DPDK can implement their own functions.
    
    Signed-off-by: Chao Zhu <bjzhuc@cn.ibm.com>
    Signed-off-by: David Marchand <david.marchand@6wind.com>
    Acked-by: Thomas Monjalon <thomas.monjalon@6wind.com>

--- lib/librte_eal/common/include/generic/rte_prefetch.h
+++ lib/librte_eal/common/include/generic/rte_prefetch.h
@@ -46,29 +46,19 @@
  * program.
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /**
  * Prefetch a cache line into all cache levels.
  * @param p
  *   Address to prefetch
  */
-static inline void rte_prefetch0(volatile void *p)
-{
-	asm volatile ("prefetcht0 %[p]" : [p] "+m" (*(volatile char *)p));
-}
+static inline void rte_prefetch0(volatile void *p);
 
 /**
  * Prefetch a cache line into all cache levels except the 0th cache level.
  * @param p
  *   Address to prefetch
  */
-static inline void rte_prefetch1(volatile void *p)
-{
-	asm volatile ("prefetcht1 %[p]" : [p] "+m" (*(volatile char *)p));
-}
+static inline void rte_prefetch1(volatile void *p);
 
 /**
  * Prefetch a cache line into all cache levels except the 0th and 1th cache
@@ -76,13 +66,6 @@ static inline void rte_prefetch1(volatile void *p)
  * @param p
  *   Address to prefetch
  */
-static inline void rte_prefetch2(volatile void *p)
-{
-	asm volatile ("prefetcht2 %[p]" : [p] "+m" (*(volatile char *)p));
-}
-
-#ifdef __cplusplus
-}
-#endif
+static inline void rte_prefetch2(volatile void *p);
 
 #endif /* _RTE_PREFETCH_H_ */
