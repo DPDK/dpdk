be04c7072745e0471fc185c31c2a2df835ee4e06
Sergio Gonzalez Monroy <sergio.gonzalez.monroy@intel.com>
2014-11-20 14:06:59 +0000

    eal/bsd: remove unused HPET support
    The HPET support in the BSD EAL was copied directly from the Linux version,
    but did not actually work on FreeBSD. We replace this old code with a simple
    compiler message that informs the user that we don't support HPET on BSD if
    they enable such support in the build-time configuration file.
    
    Signed-off-by: Sergio Gonzalez Monroy <sergio.gonzalez.monroy@intel.com>

--- lib/librte_eal/bsdapp/eal/eal_timer.c
+++ lib/librte_eal/bsdapp/eal/eal_timer.c
@@ -30,17 +30,10 @@
  *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 #include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <inttypes.h>
-#include <sys/mman.h>
-#include <sys/queue.h>
-#include <pthread.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #include <errno.h>
@@ -57,111 +50,14 @@
 #include "eal_private.h"
 #include "eal_internal_cfg.h"
 
-enum timer_source eal_timer_source = EAL_TIMER_HPET;
+#ifdef RTE_LIBEAL_USE_HPET
+#warning HPET is not supported in FreeBSD
+#endif
+
+enum timer_source eal_timer_source = EAL_TIMER_TSC;
 
 /* The frequency of the RDTSC timer resolution */
 static uint64_t eal_tsc_resolution_hz = 0;
-
-#ifdef RTE_LIBEAL_USE_HPET
-
-#define DEV_HPET "/dev/hpet"
-
-/* Maximum number of counters. */
-#define HPET_TIMER_NUM 3
-
-/* General capabilities register */
-#define CLK_PERIOD_SHIFT     32 /* Clock period shift. */
-#define CLK_PERIOD_MASK      0xffffffff00000000ULL /* Clock period mask. */
-
-/**
- * HPET timer registers. From the Intel IA-PC HPET (High Precision Event
- * Timers) Specification.
- */
-struct eal_hpet_regs {
-	/* Memory-mapped, software visible registers */
-	uint64_t capabilities;      /**< RO General Capabilities Register. */
-	uint64_t reserved0;         /**< Reserved for future use. */
-	uint64_t config;            /**< RW General Configuration Register. */
-	uint64_t reserved1;         /**< Reserved for future use. */
-	uint64_t isr;               /**< RW Clear General Interrupt Status. */
-	uint64_t reserved2[25];     /**< Reserved for future use. */
-	union {
-		uint64_t counter;   /**< RW Main Counter Value Register. */
-		struct {
-			uint32_t counter_l; /**< RW Main Counter Low. */
-			uint32_t counter_h; /**< RW Main Counter High. */
-		};
-	};
-	uint64_t reserved3;         /**< Reserved for future use. */
-	struct {
-		uint64_t config;    /**< RW Timer Config and Capability Reg. */
-		uint64_t comp;      /**< RW Timer Comparator Value Register. */
-		uint64_t fsb;       /**< RW FSB Interrupt Route Register. */
-		uint64_t reserved4; /**< Reserved for future use. */
-	} timers[HPET_TIMER_NUM]; /**< Set of HPET timers. */
-};
-
-/* Mmap'd hpet registers */
-static volatile struct eal_hpet_regs *eal_hpet = NULL;
-
-/* Period at which the HPET counter increments in
- * femtoseconds (10^-15 seconds). */
-static uint32_t eal_hpet_resolution_fs = 0;
-
-/* Frequency of the HPET counter in Hz */
-static uint64_t eal_hpet_resolution_hz = 0;
-
-/* Incremented 4 times during one 32bits hpet full count */
-static uint32_t eal_hpet_msb;
-
-static pthread_t msb_inc_thread_id;
-
-/*
- * This function runs on a specific thread to update a global variable
- * containing used to process MSB of the HPET (unfortunatelly, we need
- * this because hpet is 32 bits by default under linux).
- */
-static void
-hpet_msb_inc(__attribute__((unused)) void *arg)
-{
-	uint32_t t;
-
-	while (1) {
-		t = (eal_hpet->counter_l >> 30);
-		if (t != (eal_hpet_msb & 3))
-			eal_hpet_msb ++;
-		sleep(10);
-	}
-}
-
-uint64_t
-rte_get_hpet_hz(void)
-{
-	if(internal_config.no_hpet)
-		rte_panic("Error, HPET called, but no HPET present\n");
-
-	return eal_hpet_resolution_hz;
-}
-
-uint64_t
-rte_get_hpet_cycles(void)
-{
-	uint32_t t, msb;
-	uint64_t ret;
-
-	if(internal_config.no_hpet)
-		rte_panic("Error, HPET called, but no HPET present\n");
-
-	t = eal_hpet->counter_l;
-	msb = eal_hpet_msb;
-	ret = (msb + 2 - (t >> 30)) / 4;
-	ret <<= 32;
-	ret += t;
-	return ret;
-}
-
-#endif
-
 
 void
 rte_delay_us(unsigned us)
@@ -177,70 +73,6 @@ rte_get_tsc_hz(void)
 {
 	return eal_tsc_resolution_hz;
 }
-
-
-#ifdef RTE_LIBEAL_USE_HPET
-/*
- * Open and mmap /dev/hpet (high precision event timer) that will
- * provide our time reference.
- */
-int
-rte_eal_hpet_init(int make_default)
-{
-	int fd, ret;
-
-	if (internal_config.no_hpet) {
-		RTE_LOG(INFO, EAL, "HPET is disabled\n");
-		return -1;
-	}
-
-	fd = open(DEV_HPET, O_RDONLY);
-	if (fd < 0) {
-		RTE_LOG(ERR, EAL, "ERROR: Cannot open "DEV_HPET": %s!\n",
-			strerror(errno));
-		internal_config.no_hpet = 1;
-		return -1;
-	}
-	eal_hpet = mmap(NULL, 1024, PROT_READ, MAP_SHARED, fd, 0);
-	if (eal_hpet == MAP_FAILED) {
-		RTE_LOG(ERR, EAL, "ERROR: Cannot mmap "DEV_HPET"!\n"
-				"Please enable CONFIG_HPET_MMAP in your kernel configuration "
-				"to allow HPET support.\n"
-				"To run without using HPET, set CONFIG_RTE_LIBEAL_USE_HPET=n "
-				"in your build configuration or use '--no-hpet' EAL flag.\n");
-		close(fd);
-		internal_config.no_hpet = 1;
-		return -1;
-	}
-	close(fd);
-
-	eal_hpet_resolution_fs = (uint32_t)((eal_hpet->capabilities &
-					CLK_PERIOD_MASK) >>
-					CLK_PERIOD_SHIFT);
-
-	eal_hpet_resolution_hz = (1000ULL*1000ULL*1000ULL*1000ULL*1000ULL) /
-		(uint64_t)eal_hpet_resolution_fs;
-
-	RTE_LOG(INFO, EAL, "HPET frequency is ~%"PRIu64" kHz\n",
-			eal_hpet_resolution_hz/1000);
-
-	eal_hpet_msb = (eal_hpet->counter_l >> 30);
-
-	/* create a thread that will increment a global variable for
-	 * msb (hpet is 32 bits by default under linux) */
-	ret = pthread_create(&msb_inc_thread_id, NULL,
-			(void *(*)(void *))hpet_msb_inc, NULL);
-	if (ret < 0) {
-		RTE_LOG(ERR, EAL, "ERROR: Cannot create HPET timer thread!\n");
-		internal_config.no_hpet = 1;
-		return -1;
-	}
-
-	if (make_default)
-		eal_timer_source = EAL_TIMER_HPET;
-	return 0;
-}
-#endif
 
 static int
 set_tsc_freq_from_sysctl(void)
@@ -264,7 +96,6 @@ set_tsc_freq_from_sysctl(void)
 		RTE_LOG(WARNING, EAL, "TSC is not invariant\n");
 
 	sz = sizeof(eal_tsc_resolution_hz);
-
 	if (sysctlbyname("machdep.tsc_freq", &eal_tsc_resolution_hz, &sz, NULL, 0)) {
 		RTE_LOG(WARNING, EAL, "%s\n", strerror(errno));
 		return -1;
@@ -289,9 +120,8 @@ set_tsc_freq_fallback(void)
  * This function measures the TSC frequency. It uses a variety of approaches.
  *
  * 1. Read the TSC frequency value provided by the kernel
- * 2. If kernel does not provide that, and we have HPET support, tune using HPET
- * 3. Lastly, if neither of the above can be used, just sleep for 1 second and
- * tune off that, printing a warning about inaccuracy of timing
+ * 2. If above does not work, just sleep for 1 second and tune off that,
+ *    printing a warning about inaccuracy of timing
  */
 static void
 set_tsc_freq(void)
@@ -306,9 +136,6 @@ set_tsc_freq(void)
 int
 rte_eal_timer_init(void)
 {
-
-	eal_timer_source = EAL_TIMER_TSC;
-
 	set_tsc_freq();
 	return 0;
 }
