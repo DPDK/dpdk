545051540e3f4489cfd6e6584ee555bf0dd1b93c
David Marchand <david.marchand@6wind.com>
2014-11-28 16:42:44 +0100

    scripts: fix newline in configuration with bsd sed
    Use of \n in sed expression is not portable and triggered an invalid
    configuration on BSD (at least).
    Replace with an explicit newline.
    
    Reported-by: Bruce Richardson <bruce.richardson@intel.com>
    Signed-off-by: David Marchand <david.marchand@6wind.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- scripts/gen-config-h.sh
+++ scripts/gen-config-h.sh
@@ -35,9 +35,11 @@ echo "#ifndef __RTE_CONFIG_H"
 echo "#define __RTE_CONFIG_H"
 grep CONFIG_ $1 |
 grep -v '^[ \t]*#' |
-sed 's,CONFIG_\(.*\)=y.*$,#undef \1\n#define \1 1,' |
+sed 's,CONFIG_\(.*\)=y.*$,#undef \1\
+#define \1 1,' |
 sed 's,CONFIG_\(.*\)=n.*$,#undef \1,' |
-sed 's,CONFIG_\(.*\)=\(.*\)$,#undef \1\n#define \1 \2,' |
+sed 's,CONFIG_\(.*\)=\(.*\)$,#undef \1\
+#define \1 \2,' |
 sed 's,\# CONFIG_\(.*\) is not set$,#undef \1,'
 echo "#endif /* __RTE_CONFIG_H */"
 
