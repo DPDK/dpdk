3a811d2efce3300f5bdc60ed6bfc7f9954443b0a
Helin Zhang <helin.zhang@intel.com>
2015-04-30 23:03:32 +0800

    i40e/base: workaround for PHY type with firmware < 4.4
    The FW for SW3E is incorrectly reporting a breakout cable as PHY
    type 0x3 when it should be 0x16 (I40E_PHY_TYPE_10GBASE_SFPP_CU).
    As this is supposed to be fixed in the next FW version
    (FW versions >= 4.4), if we get this value back from FW and the
    version is < 4.4, reassign it to I40E_PHY_TYPE_10GBASE_SFPP_CU.
    
    Test report: http://www.dpdk.org/ml/archives/dev/2015-May/017384.html
    
    Signed-off-by: Helin Zhang <helin.zhang@intel.com>
    Acked-by: Jijiang Liu <jijiang.liu@intel.com>
    Tested-by: Min Cao <min.cao@intel.com>

--- lib/librte_pmd_i40e/i40e/i40e_common.c
+++ lib/librte_pmd_i40e/i40e/i40e_common.c
@@ -1448,6 +1448,10 @@ enum i40e_status_code i40e_aq_get_link_info(struct i40e_hw *hw,
 	else
 		hw_link_info->lse_enable = false;
 
+	if ((hw->aq.fw_maj_ver < 4 || (hw->aq.fw_maj_ver == 4 &&
+	     hw->aq.fw_min_ver < 40)) && hw_link_info->phy_type == 0xE)
+		hw_link_info->phy_type = I40E_PHY_TYPE_10GBASE_SFPP_CU;
+
 	/* save link status information */
 	if (link)
 		i40e_memcpy(link, hw_link_info, sizeof(struct i40e_link_status),
