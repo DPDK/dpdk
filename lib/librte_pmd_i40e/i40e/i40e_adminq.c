d5d5ac2505a48627ae3292e4fcb3c236aa33fc5a
Helin Zhang <helin.zhang@intel.com>
2015-04-30 23:03:24 +0800

    i40e/base: grab NVM devstarter version instead of image version
    The NVM image version has useful data but it is a per image version,
    and every image can have a different one. While for the dev starter
    version, all the images for release will have the same version. So
    it would be better to use dev starter version number.
    
    Test report: http://www.dpdk.org/ml/archives/dev/2015-May/017384.html
    
    Signed-off-by: Helin Zhang <helin.zhang@intel.com>
    Acked-by: Jijiang Liu <jijiang.liu@intel.com>
    Tested-by: Min Cao <min.cao@intel.com>

--- lib/librte_pmd_i40e/i40e/i40e_adminq.c
+++ lib/librte_pmd_i40e/i40e/i40e_adminq.c
@@ -621,7 +621,8 @@ enum i40e_status_code i40e_init_adminq(struct i40e_hw *hw)
 		goto init_adminq_free_arq;
 
 	/* get the NVM version info */
-	i40e_read_nvm_word(hw, I40E_SR_NVM_IMAGE_VERSION, &hw->nvm.version);
+	i40e_read_nvm_word(hw, I40E_SR_NVM_DEV_STARTER_VERSION,
+			   &hw->nvm.version);
 	i40e_read_nvm_word(hw, I40E_SR_NVM_EETRACK_LO, &eetrack_lo);
 	i40e_read_nvm_word(hw, I40E_SR_NVM_EETRACK_HI, &eetrack_hi);
 	hw->nvm.eetrack = (eetrack_hi << 16) | eetrack_lo;
