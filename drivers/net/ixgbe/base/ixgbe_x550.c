d4c9ffd4fe1c84dcc40757d937de43244cd89833
Wenzhuo Lu <wenzhuo.lu@intel.com>
2015-06-05 13:21:44 +0800

    ixgbe/base: disable X550em FEC to save power
    The FEC (Forward Error Correction) feature can improve BER (Bit Error Rate)
    but uses more power to do so. It also cannot be used with
    EEE (Energy Efficient Ethernet).
    EEE is an important feature, and we have no known BER issues, so FEC
    is not needed.
    
    Signed-off-by: Wenzhuo Lu <wenzhuo.lu@intel.com>
    Acked-by: Helin Zhang <helin.zhang@intel.com>

--- drivers/net/ixgbe/base/ixgbe_x550.c
+++ drivers/net/ixgbe/base/ixgbe_x550.c
@@ -1383,8 +1383,8 @@ s32 ixgbe_setup_kr_x550em(struct ixgbe_hw *hw)
 		return status;
 
 	reg_val |= IXGBE_KRM_LINK_CTRL_1_TETH_AN_ENABLE;
-	reg_val |= IXGBE_KRM_LINK_CTRL_1_TETH_AN_FEC_REQ;
-	reg_val |= IXGBE_KRM_LINK_CTRL_1_TETH_AN_CAP_FEC;
+	reg_val &= ~(IXGBE_KRM_LINK_CTRL_1_TETH_AN_FEC_REQ |
+		     IXGBE_KRM_LINK_CTRL_1_TETH_AN_CAP_FEC);
 	reg_val &= ~(IXGBE_KRM_LINK_CTRL_1_TETH_AN_CAP_KR |
 		     IXGBE_KRM_LINK_CTRL_1_TETH_AN_CAP_KX);
 
