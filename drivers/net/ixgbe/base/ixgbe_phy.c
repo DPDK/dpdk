58ddc803e412b83499111a2983283821cf1af333
Wenzhuo Lu <wenzhuo.lu@intel.com>
2015-06-24 11:26:26 +0800

    ixgbe/base: add new X550 PHY ids
    This patch adds support for new x550 PHY IDs:
    0x0154 0x0223
    0x0154 0x0221
    
    Signed-off-by: Wenzhuo Lu <wenzhuo.lu@intel.com>
    Acked-by: Helin Zhang <helin.zhang@intel.com>

--- drivers/net/ixgbe/base/ixgbe_phy.c
+++ drivers/net/ixgbe/base/ixgbe_phy.c
@@ -507,7 +507,9 @@ enum ixgbe_phy_type ixgbe_get_phy_type_from_id(u32 phy_id)
 	case TN1010_PHY_ID:
 		phy_type = ixgbe_phy_tn;
 		break;
-	case X550_PHY_ID:
+	case X550_PHY_ID1:
+	case X550_PHY_ID2:
+	case X550_PHY_ID3:
 	case X540_PHY_ID:
 		phy_type = ixgbe_phy_aq;
 		break;
