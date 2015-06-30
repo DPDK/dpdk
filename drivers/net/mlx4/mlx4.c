8b3ffe95e75d6d305992505005cbb95969874a15
Olga Shern <olgas@mellanox.com>
2015-06-30 11:28:08 +0200

    mlx4: disable multicast echo on PF device
    Multicast loopback must be disabled on PF devices to prevent the adapter
    from sending frames back. Required with MOFED 3.0.
    
    Signed-off-by: Olga Shern <olgas@mellanox.com>
    Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>

--- drivers/net/mlx4/mlx4.c
+++ drivers/net/mlx4/mlx4.c
@@ -1534,6 +1534,13 @@ txq_setup(struct rte_eth_dev *dev, struct txq *txq, uint16_t desc,
 		.intf_scope = IBV_EXP_INTF_GLOBAL,
 		.intf = IBV_EXP_INTF_QP_BURST,
 		.obj = tmpl.qp,
+#ifdef HAVE_EXP_QP_BURST_CREATE_DISABLE_ETH_LOOPBACK
+		/* MC loopback must be disabled when not using a VF. */
+		.family_flags =
+			(!priv->vf ?
+			 IBV_EXP_QP_BURST_CREATE_DISABLE_ETH_LOOPBACK :
+			 0),
+#endif
 	};
 	tmpl.if_qp = ibv_exp_query_intf(priv->ctx, &attr.params, &status);
 	if (tmpl.if_qp == NULL) {
