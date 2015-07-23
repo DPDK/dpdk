bd734cf778228233f62620efab0949df0ac0ed13
David Marchand <david.marchand@6wind.com>
2015-07-23 09:15:31 +0200

    eal: remove dependency on drivers
    There is no reason why eal should depend on drivers.
    Remove remaining (unused) references.
    
    Signed-off-by: David Marchand <david.marchand@6wind.com>

--- lib/librte_eal/bsdapp/eal/eal.c
+++ lib/librte_eal/bsdapp/eal/eal.c
@@ -71,7 +71,6 @@
 #include <rte_version.h>
 #include <rte_atomic.h>
 #include <malloc_heap.h>
-#include <rte_eth_ring.h>
 
 #include "eal_private.h"
 #include "eal_thread.h"
--- lib/librte_eal/linuxapp/eal/eal.c
+++ lib/librte_eal/linuxapp/eal/eal.c
@@ -75,7 +75,6 @@
 #include <rte_version.h>
 #include <rte_atomic.h>
 #include <malloc_heap.h>
-#include <rte_eth_ring.h>
 
 #include "eal_private.h"
 #include "eal_thread.h"
