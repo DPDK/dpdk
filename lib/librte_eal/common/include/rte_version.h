1b7e102356eeb57f3f6cdff48729f5d44cb69393
Thomas Monjalon <thomas.monjalon@6wind.com>
2015-08-11 00:35:04 +0200

    version: 2.1.0-rc4
    Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>

--- lib/librte_eal/common/include/rte_version.h
+++ lib/librte_eal/common/include/rte_version.h
@@ -77,7 +77,7 @@ extern "C" {
  *   0-15 = release candidates
  *   16   = release
  */
-#define RTE_VER_PATCH_RELEASE 3
+#define RTE_VER_PATCH_RELEASE 4
 
 /**
  * Macro to compute a version number usable for comparisons
