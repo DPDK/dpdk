3f12b9f23b6499ff66ec8b0de941fb469297e5d0
Jingjing Wu <jingjing.wu@intel.com>
2015-09-06 15:11:27 +0800

    i40e/base: add workaround to drop all flow control frames
    This patch adds a workaround to drop any flow control frames from being
    transmitted from any VSI. FW can still send Flow control frames if Flow
    control is enabled.
    
    Signed-off-by: Jingjing Wu <jingjing.wu@intel.com>
    Acked-by: Helin Zhang <helin.zhang@intel.com>
    Tested-by: Huilong Xu <huilongx.xu@intel.com>

--- drivers/net/i40e/base/i40e_common.c
+++ drivers/net/i40e/base/i40e_common.c
@@ -4899,6 +4899,28 @@ enum i40e_status_code i40e_aq_add_rem_control_packet_filter(struct i40e_hw *hw,
 }
 
 /**
+ * i40e_add_filter_to_drop_tx_flow_control_frames- filter to drop flow control
+ * @hw: pointer to the hw struct
+ * @seid: VSI seid to add ethertype filter from
+ **/
+#define I40E_FLOW_CONTROL_ETHTYPE 0x8808
+void i40e_add_filter_to_drop_tx_flow_control_frames(struct i40e_hw *hw,
+						    u16 seid)
+{
+	u16 flag = I40E_AQC_ADD_CONTROL_PACKET_FLAGS_IGNORE_MAC |
+		   I40E_AQC_ADD_CONTROL_PACKET_FLAGS_DROP |
+		   I40E_AQC_ADD_CONTROL_PACKET_FLAGS_TX;
+	u16 ethtype = I40E_FLOW_CONTROL_ETHTYPE;
+	enum i40e_status_code status;
+
+	status = i40e_aq_add_rem_control_packet_filter(hw, 0, ethtype, flag,
+						       seid, 0, true, NULL,
+						       NULL);
+	if (status)
+		DEBUGOUT("Ethtype Filter Add failed: Error pruning Tx flow control frames\n");
+}
+
+/**
  * i40e_aq_add_cloud_filters
  * @hw: pointer to the hardware structure
  * @seid: VSI seid to add cloud filters from
