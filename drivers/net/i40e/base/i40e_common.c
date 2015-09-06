1e32378f0774c51461ad2dd4c73fc65817eadab2
Jingjing Wu <jingjing.wu@intel.com>
2015-09-06 15:12:01 +0800

    i40e/base: increase PF reset max loop limit
    With latest LANconf and NVMupdate tools, pf_reset failed due to Firmware not
    being ready, so this patch increases I40E_PF_RESET_WAIT_COUNT to 200, using
    original value defined for A0 silicon, in order to make sure that FW is ready.
    
    Test result indicates that firmware became ready at the loop count of 114.
    
    Signed-off-by: Jingjing Wu <jingjing.wu@intel.com>
    Acked-by: Helin Zhang <helin.zhang@intel.com>
    Tested-by: Huilong Xu <huilongx.xu@intel.com>

--- drivers/net/i40e/base/i40e_common.c
+++ drivers/net/i40e/base/i40e_common.c
@@ -1102,7 +1102,7 @@ STATIC enum i40e_media_type i40e_get_media_type(struct i40e_hw *hw)
 	return media;
 }
 
-#define I40E_PF_RESET_WAIT_COUNT	110
+#define I40E_PF_RESET_WAIT_COUNT	200
 /**
  * i40e_pf_reset - Reset the PF
  * @hw: pointer to the hardware structure
