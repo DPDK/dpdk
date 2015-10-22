DPDK/dpdk

commit db935d0171dda1e1ae1cc414da46d8a127e38197

examples/qos_sched: use librte_cfgfile
This is a modification of qos_sched example to use
librte_cfgfile for parsing configuration file.

Signed-off-by: Michal Jastrzebski <michalx.k.jastrzebski@intel.com>
Acked-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>

examples/qos_sched/cfg_file.c
@@ -233,92 +233,7 @@ int cfg_close(struct cfg_file *cfg)
 }
 
 int
-cfg_num_sections(struct cfg_file *cfg, const char *sectionname, size_t length)
-{
-	int i;
-	int num_sections = 0;
-	for (i = 0; i < cfg->num_sections; i++) {
-		if (strncmp(cfg->sections[i]->name, sectionname, length) == 0)
-			num_sections++;
-	}
-	return num_sections;
-}
-
-int
-cfg_sections(struct cfg_file *cfg, char *sections[], int max_sections)
-{
-	int i;
-	for (i = 0; i < cfg->num_sections && i < max_sections; i++) {
-		snprintf(sections[i], CFG_NAME_LEN, "%s",  cfg->sections[i]->name);
-	}
-	return i;
-}
-
-static const struct cfg_section *
-_get_section(struct cfg_file *cfg, const char *sectionname)
-{
-	int i;
-	for (i = 0; i < cfg->num_sections; i++) {
-		if (strncmp(cfg->sections[i]->name, sectionname,
-				sizeof(cfg->sections[0]->name)) == 0)
-			return cfg->sections[i];
-	}
-	return NULL;
-}
-
-int
-cfg_has_section(struct cfg_file *cfg, const char *sectionname)
-{
-	return (_get_section(cfg, sectionname) != NULL);
-}
-
-int
-cfg_section_num_entries(struct cfg_file *cfg, const char *sectionname)
-{
-	const struct cfg_section *s = _get_section(cfg, sectionname);
-	if (s == NULL)
-		return -1;
-	return s->num_entries;
-}
-
-
-int
-cfg_section_entries(struct cfg_file *cfg, const char *sectionname,
-		struct cfg_entry *entries, int max_entries)
-{
-	int i;
-	const struct cfg_section *sect = _get_section(cfg, sectionname);
-	if (sect == NULL)
-		return -1;
-	for (i = 0; i < max_entries && i < sect->num_entries; i++)
-		entries[i] = *sect->entries[i];
-	return i;
-}
-
-const char *
-cfg_get_entry(struct cfg_file *cfg, const char *sectionname,
-		const char *entryname)
-{
-	int i;
-	const struct cfg_section *sect = _get_section(cfg, sectionname);
-	if (sect == NULL)
-		return NULL;
-	for (i = 0; i < sect->num_entries; i++)
-		if (strncmp(sect->entries[i]->name, entryname, CFG_NAME_LEN) == 0)
-			return sect->entries[i]->value;
-	return NULL;
-}
-
-int
-cfg_has_entry(struct cfg_file *cfg, const char *sectionname,
-		const char *entryname)
-{
-	return (cfg_get_entry(cfg, sectionname, entryname) != NULL);
-}
-
-
-int
-cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port_params)
+cfg_load_port(struct rte_cfgfile *cfg, struct rte_sched_port_params *port_params)
 {
 	const char *entry;
 	int j;
@@ -326,19 +241,19 @@ cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port_params)
 	if (!cfg || !port_params)
 		return -1;
 
-	entry = cfg_get_entry(cfg, "port", "frame overhead");
+	entry = rte_cfgfile_get_entry(cfg, "port", "frame overhead");
 	if (entry)
 		port_params->frame_overhead = (uint32_t)atoi(entry);
 
-	entry = cfg_get_entry(cfg, "port", "number of subports per port");
+	entry = rte_cfgfile_get_entry(cfg, "port", "number of subports per port");
 	if (entry)
 		port_params->n_subports_per_port = (uint32_t)atoi(entry);
 
-	entry = cfg_get_entry(cfg, "port", "number of pipes per subport");
+	entry = rte_cfgfile_get_entry(cfg, "port", "number of pipes per subport");
 	if (entry)
 		port_params->n_pipes_per_subport = (uint32_t)atoi(entry);
 
-	entry = cfg_get_entry(cfg, "port", "queue sizes");
+	entry = rte_cfgfile_get_entry(cfg, "port", "queue sizes");
 	if (entry) {
 		char *next;
 
@@ -356,7 +271,7 @@ cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port_params)
 
 		/* Parse WRED min thresholds */
 		snprintf(str, sizeof(str), "tc %d wred min", j);
-		entry = cfg_get_entry(cfg, "red", str);
+		entry = rte_cfgfile_get_entry(cfg, "red", str);
 		if (entry) {
 			char *next;
 			int k;
@@ -372,7 +287,7 @@ cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port_params)
 
 		/* Parse WRED max thresholds */
 		snprintf(str, sizeof(str), "tc %d wred max", j);
-		entry = cfg_get_entry(cfg, "red", str);
+		entry = rte_cfgfile_get_entry(cfg, "red", str);
 		if (entry) {
 			char *next;
 			int k;
@@ -388,7 +303,7 @@ cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port_params)
 
 		/* Parse WRED inverse mark probabilities */
 		snprintf(str, sizeof(str), "tc %d wred inv prob", j);
-		entry = cfg_get_entry(cfg, "red", str);
+		entry = rte_cfgfile_get_entry(cfg, "red", str);
 		if (entry) {
 			char *next;
 			int k;
@@ -405,7 +320,7 @@ cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port_params)
 
 		/* Parse WRED EWMA filter weights */
 		snprintf(str, sizeof(str), "tc %d wred weight", j);
-		entry = cfg_get_entry(cfg, "red", str);
+		entry = rte_cfgfile_get_entry(cfg, "red", str);
 		if (entry) {
 			char *next;
 			int k;
@@ -425,7 +340,7 @@ cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port_params)
 }
 
 int
-cfg_load_pipe(struct cfg_file *cfg, struct rte_sched_pipe_params *pipe_params)
+cfg_load_pipe(struct rte_cfgfile *cfg, struct rte_sched_pipe_params *pipe_params)
 {
 	int i, j;
 	char *next;
@@ -435,48 +350,48 @@ cfg_load_pipe(struct cfg_file *cfg, struct rte_sched_pipe_params *pipe_params)
 	if (!cfg || !pipe_params)
 		return -1;
 
-	profiles = cfg_num_sections(cfg, "pipe profile", sizeof("pipe profile") - 1);
+	profiles = rte_cfgfile_num_sections(cfg, "pipe profile", sizeof("pipe profile") - 1);
 	port_params.n_pipe_profiles = profiles;
 
 	for (j = 0; j < profiles; j++) {
 		char pipe_name[32];
 		snprintf(pipe_name, sizeof(pipe_name), "pipe profile %d", j);
 
-		entry = cfg_get_entry(cfg, pipe_name, "tb rate");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tb rate");
 		if (entry)
 			pipe_params[j].tb_rate = (uint32_t)atoi(entry);
 
-		entry = cfg_get_entry(cfg, pipe_name, "tb size");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tb size");
 		if (entry)
 			pipe_params[j].tb_size = (uint32_t)atoi(entry);
 
-		entry = cfg_get_entry(cfg, pipe_name, "tc period");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc period");
 		if (entry)
 			pipe_params[j].tc_period = (uint32_t)atoi(entry);
 
-		entry = cfg_get_entry(cfg, pipe_name, "tc 0 rate");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 0 rate");
 		if (entry)
 			pipe_params[j].tc_rate[0] = (uint32_t)atoi(entry);
 
-		entry = cfg_get_entry(cfg, pipe_name, "tc 1 rate");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 1 rate");
 		if (entry)
 			pipe_params[j].tc_rate[1] = (uint32_t)atoi(entry);
 
-		entry = cfg_get_entry(cfg, pipe_name, "tc 2 rate");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 2 rate");
 		if (entry)
 			pipe_params[j].tc_rate[2] = (uint32_t)atoi(entry);
 
-		entry = cfg_get_entry(cfg, pipe_name, "tc 3 rate");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 3 rate");
 		if (entry)
 			pipe_params[j].tc_rate[3] = (uint32_t)atoi(entry);
 
 #ifdef RTE_SCHED_SUBPORT_TC_OV
-		entry = cfg_get_entry(cfg, pipe_name, "tc 3 oversubscription weight");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 3 oversubscription weight");
 		if (entry)
 			pipe_params[j].tc_ov_weight = (uint8_t)atoi(entry);
 #endif
 
-		entry = cfg_get_entry(cfg, pipe_name, "tc 0 wrr weights");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 0 wrr weights");
 		if (entry) {
 			for(i = 0; i < RTE_SCHED_QUEUES_PER_TRAFFIC_CLASS; i++) {
 				pipe_params[j].wrr_weights[RTE_SCHED_TRAFFIC_CLASSES_PER_PIPE*0 + i] =
@@ -486,7 +401,7 @@ cfg_load_pipe(struct cfg_file *cfg, struct rte_sched_pipe_params *pipe_params)
 				entry = next;
 			}
 		}
-		entry = cfg_get_entry(cfg, pipe_name, "tc 1 wrr weights");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 1 wrr weights");
 		if (entry) {
 			for(i = 0; i < RTE_SCHED_QUEUES_PER_TRAFFIC_CLASS; i++) {
 				pipe_params[j].wrr_weights[RTE_SCHED_TRAFFIC_CLASSES_PER_PIPE*1 + i] =
@@ -496,7 +411,7 @@ cfg_load_pipe(struct cfg_file *cfg, struct rte_sched_pipe_params *pipe_params)
 				entry = next;
 			}
 		}
-		entry = cfg_get_entry(cfg, pipe_name, "tc 2 wrr weights");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 2 wrr weights");
 		if (entry) {
 			for(i = 0; i < RTE_SCHED_QUEUES_PER_TRAFFIC_CLASS; i++) {
 				pipe_params[j].wrr_weights[RTE_SCHED_TRAFFIC_CLASSES_PER_PIPE*2 + i] =
@@ -506,7 +421,7 @@ cfg_load_pipe(struct cfg_file *cfg, struct rte_sched_pipe_params *pipe_params)
 				entry = next;
 			}
 		}
-		entry = cfg_get_entry(cfg, pipe_name, "tc 3 wrr weights");
+		entry = rte_cfgfile_get_entry(cfg, pipe_name, "tc 3 wrr weights");
 		if (entry) {
 			for(i = 0; i < RTE_SCHED_QUEUES_PER_TRAFFIC_CLASS; i++) {
 				pipe_params[j].wrr_weights[RTE_SCHED_TRAFFIC_CLASSES_PER_PIPE*3 + i] =
@@ -521,7 +436,7 @@ cfg_load_pipe(struct cfg_file *cfg, struct rte_sched_pipe_params *pipe_params)
 }
 
 int
-cfg_load_subport(struct cfg_file *cfg, struct rte_sched_subport_params *subport_params)
+cfg_load_subport(struct rte_cfgfile *cfg, struct rte_sched_subport_params *subport_params)
 {
 	const char *entry;
 	int i, j, k;
@@ -535,39 +450,39 @@ cfg_load_subport(struct cfg_file *cfg, struct rte_sched_subport_params *subport_
 		char sec_name[CFG_NAME_LEN];
 		snprintf(sec_name, sizeof(sec_name), "subport %d", i);
 
-		if (cfg_has_section(cfg, sec_name)) {
-			entry = cfg_get_entry(cfg, sec_name, "tb rate");
+		if (rte_cfgfile_has_section(cfg, sec_name)) {
+			entry = rte_cfgfile_get_entry(cfg, sec_name, "tb rate");
 			if (entry)
 				subport_params[i].tb_rate = (uint32_t)atoi(entry);
 
-			entry = cfg_get_entry(cfg, sec_name, "tb size");
+			entry = rte_cfgfile_get_entry(cfg, sec_name, "tb size");
 			if (entry)
 				subport_params[i].tb_size = (uint32_t)atoi(entry);
 
-			entry = cfg_get_entry(cfg, sec_name, "tc period");
+			entry = rte_cfgfile_get_entry(cfg, sec_name, "tc period");
 			if (entry)
 				subport_params[i].tc_period = (uint32_t)atoi(entry);
 
-			entry = cfg_get_entry(cfg, sec_name, "tc 0 rate");
+			entry = rte_cfgfile_get_entry(cfg, sec_name, "tc 0 rate");
 			if (entry)
 				subport_params[i].tc_rate[0] = (uint32_t)atoi(entry);
 
-			entry = cfg_get_entry(cfg, sec_name, "tc 1 rate");
+			entry = rte_cfgfile_get_entry(cfg, sec_name, "tc 1 rate");
 			if (entry)
 				subport_params[i].tc_rate[1] = (uint32_t)atoi(entry);
 
-			entry = cfg_get_entry(cfg, sec_name, "tc 2 rate");
+			entry = rte_cfgfile_get_entry(cfg, sec_name, "tc 2 rate");
 			if (entry)
 				subport_params[i].tc_rate[2] = (uint32_t)atoi(entry);
 
-			entry = cfg_get_entry(cfg, sec_name, "tc 3 rate");
+			entry = rte_cfgfile_get_entry(cfg, sec_name, "tc 3 rate");
 			if (entry)
 				subport_params[i].tc_rate[3] = (uint32_t)atoi(entry);
 
-			int n_entries = cfg_section_num_entries(cfg, sec_name);
-			struct cfg_entry entries[n_entries];
+			int n_entries = rte_cfgfile_section_num_entries(cfg, sec_name);
+			struct rte_cfgfile_entry entries[n_entries];
 
-			cfg_section_entries(cfg, sec_name, entries, n_entries);
+			rte_cfgfile_section_entries(cfg, sec_name, entries, n_entries);
 
 			for (j = 0; j < n_entries; j++) {
 				if (strncmp("pipe", entries[j].name, sizeof("pipe") - 1) == 0) {
@@ -594,7 +509,7 @@ cfg_load_subport(struct cfg_file *cfg, struct rte_sched_subport_params *subport_
 
 						snprintf(profile_name, sizeof(profile_name),
 								"pipe profile %d", profile);
-						if (cfg_has_section(cfg, profile_name))
+						if (rte_cfgfile_has_section(cfg, profile_name))
 							app_pipe_to_profile[i][k] = profile;
 						else
 							rte_exit(EXIT_FAILURE, "Wrong pipe profile %s\n",

examples/qos_sched/cfg_file.h
@@ -35,6 +35,7 @@
 #define __CFG_FILE_H__
 
 #include <rte_sched.h>
+#include <rte_cfgfile.h>
 
 #define CFG_NAME_LEN 32
 #define CFG_VALUE_LEN 64
@@ -57,44 +58,16 @@ struct cfg_file {
 };
 
 
-int cfg_load_port(struct cfg_file *cfg, struct rte_sched_port_params *port);
+int cfg_load_port(struct rte_cfgfile *cfg, struct rte_sched_port_params *port);
 
-int cfg_load_pipe(struct cfg_file *cfg, struct rte_sched_pipe_params *pipe);
+int cfg_load_pipe(struct rte_cfgfile *cfg, struct rte_sched_pipe_params *pipe);
 
-int cfg_load_subport(struct cfg_file *cfg, struct rte_sched_subport_params *subport);
-
+int cfg_load_subport(struct rte_cfgfile *cfg, struct rte_sched_subport_params *subport);
 
 /* reads a config file from disk and returns a handle to the config
  * 'flags' is reserved for future use and must be 0
  */
 struct cfg_file *cfg_load(const char *filename, int flags);
-
-/* returns the number of sections in the config */
-int cfg_num_sections(struct cfg_file *cfg, const char *sec_name, size_t length);
-
-/* fills the array "sections" with the names of all the sections in the file
- * (up to a max of max_sections).
- * NOTE: buffers in the sections array must be at least CFG_NAME_LEN big
- */
-int cfg_sections(struct cfg_file *cfg, char *sections[], int max_sections);
-
-/* true if the named section exists, false otherwise */
-int cfg_has_section(struct cfg_file *cfg, const char *sectionname);
-
-/* returns the number of entries in a section */
-int cfg_section_num_entries(struct cfg_file *cfg, const char *sectionname);
-
-/* returns the entries in a section as key-value pairs in the "entries" array */
-int cfg_section_entries(struct cfg_file *cfg, const char *sectionname,
-		struct cfg_entry *entries, int max_entries);
-
-/* returns a pointer to the value of the named entry in the named section */
-const char *cfg_get_entry(struct cfg_file *cfg, const char *sectionname,
-		const char *entryname);
-
-/* true if the given entry exists in the given section, false otherwise */
-int cfg_has_entry(struct cfg_file *cfg, const char *sectionname,
-		const char *entryname);
 
 /* cleans up memory allocated by cfg_load() */
 int cfg_close(struct cfg_file *cfg);

examples/qos_sched/init.c
@@ -42,6 +42,7 @@
 #include <rte_sched.h>
 #include <rte_cycles.h>
 #include <rte_string_fns.h>
+#include <rte_cfgfile.h>
 
 #include "main.h"
 #include "cfg_file.h"
@@ -284,16 +285,15 @@ app_load_cfg_profile(const char *profile)
 {
 	if (profile == NULL)
 		return 0;
-
-	struct cfg_file *cfg_file = cfg_load(profile, 0);
-	if (cfg_file == NULL)
+	struct rte_cfgfile *file = rte_cfgfile_load(profile, 0);
+	if (file == NULL)
 		rte_exit(EXIT_FAILURE, "Cannot load configuration profile %s\n", profile);
 
-	cfg_load_port(cfg_file, &port_params);
-	cfg_load_subport(cfg_file, subport_params);
-	cfg_load_pipe(cfg_file, pipe_profiles);
+	cfg_load_port(file, &port_params);
+	cfg_load_subport(file, subport_params);
+	cfg_load_pipe(file, pipe_profiles);
 
-	cfg_close(cfg_file);
+	rte_cfgfile_close(file);
 
 	return 0;
 }

commit a73c93ec143b9556cd3160ae8216630a0c50342f

examples/qos_sched: remove duplicated cfgfile library
This is a supplement for previous patch that was incomplete.
Previous commit message: This is a modification of qos_sched
example to use librte_cfgfile for parsing configuration file.

Fixes: db935d0171dd ("examples/qos_sched: use librte_cfgfile")

Signed-off-by: Michal Jastrzebski <michalx.k.jastrzebski@intel.com>

examples/qos_sched/cfg_file.c
@@ -49,189 +49,6 @@
  * for new entries do we add in */
 #define CFG_ALLOC_ENTRY_BATCH 16
 
-static unsigned
-_strip(char *str, unsigned len)
-{
-	int newlen = len;
-	if (len == 0)
-		return 0;
-
-	if (isspace(str[len-1])) {
-		/* strip trailing whitespace */
-		while (newlen > 0 && isspace(str[newlen - 1]))
-			str[--newlen] = '\0';
-	}
-
-	if (isspace(str[0])) {
-		/* strip leading whitespace */
-		int i,start = 1;
-		while (isspace(str[start]) && start < newlen)
-			start++
-			; /* do nothing */
-		newlen -= start;
-		for (i = 0; i < newlen; i++)
-			str[i] = str[i+start];
-		str[i] = '\0';
-	}
-	return newlen;
-}
-
-struct cfg_file *
-cfg_load(const char *filename, int flags)
-{
-	int allocated_sections = CFG_ALLOC_SECTION_BATCH;
-	int allocated_entries = 0;
-	int curr_section = -1;
-	int curr_entry = -1;
-	char buffer[256];
-	int lineno = 0;
-	struct cfg_file *cfg = NULL;
-
-	FILE *f = fopen(filename, "r");
-	if (f == NULL)
-		return NULL;
-
-	cfg = malloc(sizeof(*cfg) +	sizeof(cfg->sections[0]) * allocated_sections);
-	if (cfg == NULL)
-		goto error2;
-
-	memset(cfg->sections, 0, sizeof(cfg->sections[0]) * allocated_sections);
-
-	while (fgets(buffer, sizeof(buffer), f) != NULL) {
-		char *pos = NULL;
-		size_t len = strnlen(buffer, sizeof(buffer));
-		lineno++;
-		if (len >=sizeof(buffer) - 1 && buffer[len-1] != '\n'){
-			printf("Error line %d - no \\n found on string. "
-					"Check if line too long\n", lineno);
-			goto error1;
-		}
-		if ((pos = memchr(buffer, ';', sizeof(buffer))) != NULL) {
-			*pos = '\0';
-			len = pos -  buffer;
-		}
-
-		len = _strip(buffer, len);
-		if (buffer[0] != '[' && memchr(buffer, '=', len) == NULL)
-			continue;
-
-		if (buffer[0] == '[') {
-			/* section heading line */
-			char *end = memchr(buffer, ']', len);
-			if (end == NULL) {
-				printf("Error line %d - no terminating '[' found\n", lineno);
-				goto error1;
-			}
-			*end = '\0';
-			_strip(&buffer[1], end - &buffer[1]);
-
-			/* close off old section and add start new one */
-			if (curr_section >= 0)
-				cfg->sections[curr_section]->num_entries = curr_entry + 1;
-			curr_section++;
-
-			/* resize overall struct if we don't have room for more sections */
-			if (curr_section == allocated_sections) {
-				allocated_sections += CFG_ALLOC_SECTION_BATCH;
-				struct cfg_file *n_cfg = realloc(cfg, sizeof(*cfg) +
-						sizeof(cfg->sections[0]) * allocated_sections);
-				if (n_cfg == NULL) {
-					printf("Error - no more memory\n");
-					goto error1;
-				}
-				cfg = n_cfg;
-			}
-
-			/* allocate space for new section */
-			allocated_entries = CFG_ALLOC_ENTRY_BATCH;
-			curr_entry = -1;
-			cfg->sections[curr_section] = malloc(sizeof(*cfg->sections[0]) +
-					sizeof(cfg->sections[0]->entries[0]) * allocated_entries);
-			if (cfg->sections[curr_section] == NULL) {
-				printf("Error - no more memory\n");
-				goto error1;
-			}
-
-			snprintf(cfg->sections[curr_section]->name,
-					sizeof(cfg->sections[0]->name),
-					"%s", &buffer[1]);
-		}
-		else {
-			/* value line */
-			if (curr_section < 0) {
-				printf("Error line %d - value outside of section\n", lineno);
-				goto error1;
-			}
-
-			struct cfg_section *sect = cfg->sections[curr_section];
-			char *split[2];
-			if (rte_strsplit(buffer, sizeof(buffer), split, 2, '=') != 2) {
-				printf("Error at line %d - cannot split string\n", lineno);
-				goto error1;
-			}
-
-			curr_entry++;
-			if (curr_entry == allocated_entries) {
-				allocated_entries += CFG_ALLOC_ENTRY_BATCH;
-				struct cfg_section *n_sect = realloc(sect, sizeof(*sect) +
-						sizeof(sect->entries[0]) * allocated_entries);
-				if (n_sect == NULL) {
-					printf("Error - no more memory\n");
-					goto error1;
-				}
-				sect = cfg->sections[curr_section] = n_sect;
-			}
-
-			sect->entries[curr_entry] = malloc(sizeof(*sect->entries[0]));
-			if (sect->entries[curr_entry] == NULL) {
-				printf("Error - no more memory\n");
-				goto error1;
-			}
-
-			struct cfg_entry *entry = sect->entries[curr_entry];
-			snprintf(entry->name, sizeof(entry->name), "%s", split[0]);
-			snprintf(entry->value, sizeof(entry->value), "%s", split[1]);
-			_strip(entry->name, strnlen(entry->name, sizeof(entry->name)));
-			_strip(entry->value, strnlen(entry->value, sizeof(entry->value)));
-		}
-	}
-	fclose(f);
-	cfg->flags = flags;
-	cfg->sections[curr_section]->num_entries = curr_entry + 1;
-	cfg->num_sections = curr_section + 1;
-	return cfg;
-
-error1:
-	cfg_close(cfg);
-error2:
-	fclose(f);
-	return NULL;
-}
-
-
-int cfg_close(struct cfg_file *cfg)
-{
-	int i, j;
-
-	if (cfg == NULL)
-		return -1;
-
-	for(i = 0; i < cfg->num_sections; i++) {
-		if (cfg->sections[i] != NULL) {
-			if (cfg->sections[i]->num_entries) {
-				for(j = 0; j < cfg->sections[i]->num_entries; j++) {
-					if (cfg->sections[i]->entries[j] != NULL)
-						free(cfg->sections[i]->entries[j]);
-				}
-			}
-			free(cfg->sections[i]);
-		}
-	}
-	free(cfg);
-
-	return 0;
-}
-
 int
 cfg_load_port(struct rte_cfgfile *cfg, struct rte_sched_port_params *port_params)
 {

examples/qos_sched/cfg_file.h
@@ -37,39 +37,10 @@
 #include <rte_sched.h>
 #include <rte_cfgfile.h>
 
-#define CFG_NAME_LEN 32
-#define CFG_VALUE_LEN 64
-
-struct cfg_entry {
-	char name[CFG_NAME_LEN];
-	char value[CFG_VALUE_LEN];
-};
-
-struct cfg_section {
-	char name[CFG_NAME_LEN];
-	int num_entries;
-	struct cfg_entry *entries[0];
-};
-
-struct cfg_file {
-	int flags;
-	int num_sections;
-	struct cfg_section *sections[0];
-};
-
-
 int cfg_load_port(struct rte_cfgfile *cfg, struct rte_sched_port_params *port);
 
 int cfg_load_pipe(struct rte_cfgfile *cfg, struct rte_sched_pipe_params *pipe);
 
 int cfg_load_subport(struct rte_cfgfile *cfg, struct rte_sched_subport_params *subport);
-
-/* reads a config file from disk and returns a handle to the config
- * 'flags' is reserved for future use and must be 0
- */
-struct cfg_file *cfg_load(const char *filename, int flags);
-
-/* cleans up memory allocated by cfg_load() */
-int cfg_close(struct cfg_file *cfg);
 
 #endif