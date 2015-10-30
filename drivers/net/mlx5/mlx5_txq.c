2bbd8c9f8c8f246a17d8748b6d433007bcb1f790
Adrien Mazarguil <adrien.mazarguil@6wind.com>
2015-10-30 19:55:17 +0100

    mlx5: enable multi packet send WR in Tx CQ
    For adapters that support it, this flag improves performance outside of VF
    context.
    
    Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>

--- drivers/net/mlx5/mlx5_txq.c
+++ drivers/net/mlx5/mlx5_txq.c
@@ -395,6 +395,13 @@ txq_setup(struct rte_eth_dev *dev, struct txq *txq, uint16_t desc,
 		.intf_scope = IBV_EXP_INTF_GLOBAL,
 		.intf = IBV_EXP_INTF_QP_BURST,
 		.obj = tmpl.qp,
+#ifdef HAVE_EXP_QP_BURST_CREATE_ENABLE_MULTI_PACKET_SEND_WR
+		/* Multi packet send WR can only be used outside of VF. */
+		.family_flags =
+			(!priv->vf ?
+			 IBV_EXP_QP_BURST_CREATE_ENABLE_MULTI_PACKET_SEND_WR :
+			 0),
+#endif
 	};
 	tmpl.if_qp = ibv_exp_query_intf(priv->ctx, &attr.params, &status);
 	if (tmpl.if_qp == NULL) {
