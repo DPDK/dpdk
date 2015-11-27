538020aaed8b3807935ecd7d7fb4b5f78222cddf
Michael Qiu <michael.qiu@intel.com>
2015-11-27 11:36:04 +0800

    examples/distributor: fix build with icc 2015
    examples/distributor/main.c(338): error #167:
    	argument of type "struct rte_mbuf *"
    	is incompatible with parameter of type "const char *"
    
    The first param passed to _mm_prefetch is wrong,
    need convert "struct rte_mbuf *" to "void *".
    
    Fixes: 07db4a975094 ("examples/distributor: new sample app")
    
    Signed-off-by: Michael Qiu <michael.qiu@intel.com>

--- examples/distributor/main.c
+++ examples/distributor/main.c
@@ -335,13 +335,13 @@ lcore_tx(struct rte_ring *in_r)
 
 			/* for traffic we receive, queue it up for transmit */
 			uint16_t i;
-			_mm_prefetch(bufs[0], 0);
-			_mm_prefetch(bufs[1], 0);
-			_mm_prefetch(bufs[2], 0);
+			_mm_prefetch((void *)bufs[0], 0);
+			_mm_prefetch((void *)bufs[1], 0);
+			_mm_prefetch((void *)bufs[2], 0);
 			for (i = 0; i < nb_rx; i++) {
 				struct output_buffer *outbuf;
 				uint8_t outp;
-				_mm_prefetch(bufs[i + 3], 0);
+				_mm_prefetch((void *)bufs[i + 3], 0);
 				/*
 				 * workers should update in_port to hold the
 				 * output port value
