ca26d3b4e03e1c59397d6c54e67edeecfba5d493
Stephen Hemminger <stephen@networkplumber.org>
2015-12-02 14:16:18 -0800

    cxgbe: use explicit PCI driver structure
    The upcoming Hyper-V driver converts the pci_drv element
    in struct eth_driver to a union.  When vmbus is added the
    pci_drv needs to be explicit. Easier to fix the issue
    ahead of time.
    
    This is backwards compatible with previous code.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Rahul Lakkireddy <rahul.lakkireddy@chelsio.com>

--- drivers/net/cxgbe/cxgbe_ethdev.c
+++ drivers/net/cxgbe/cxgbe_ethdev.c
@@ -847,7 +847,7 @@ out_free_adapter:
 }
 
 static struct eth_driver rte_cxgbe_pmd = {
-	{
+	.pci_drv = {
 		.name = "rte_cxgbe_pmd",
 		.id_table = cxgb4_pci_tbl,
 		.drv_flags = RTE_PCI_DRV_NEED_MAPPING | RTE_PCI_DRV_INTR_LSC,
