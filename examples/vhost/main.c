691693c6e6653dc887bd7eb5cc868d41082ca17f
Jijiang Liu <jijiang.liu@intel.com>
2016-02-05 15:31:40 +0800

    examples/vhost: remove IPv4 header definition
    Remove the ipv4_hdr structure defination in vhost sample.
    
    The same structure has already defined in the rte_ip.h file, so we
      remove the defination from the sample, and include that header file.
    
    Signed-off-by: Jijiang Liu <jijiang.liu@intel.com>
    Acked-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

--- examples/vhost/main.c
+++ examples/vhost/main.c
@@ -50,6 +50,7 @@
 #include <rte_string_fns.h>
 #include <rte_malloc.h>
 #include <rte_virtio_net.h>
+#include <rte_ip.h>
 
 #include "main.h"
 
@@ -293,20 +294,6 @@ struct vlan_ethhdr {
 	__be16          h_vlan_TCI;
 	__be16          h_vlan_encapsulated_proto;
 };
-
-/* IPv4 Header */
-struct ipv4_hdr {
-	uint8_t  version_ihl;		/**< version and header length */
-	uint8_t  type_of_service;	/**< type of service */
-	uint16_t total_length;		/**< length of packet */
-	uint16_t packet_id;		/**< packet ID */
-	uint16_t fragment_offset;	/**< fragmentation offset */
-	uint8_t  time_to_live;		/**< time to live */
-	uint8_t  next_proto_id;		/**< protocol ID */
-	uint16_t hdr_checksum;		/**< header checksum */
-	uint32_t src_addr;		/**< source address */
-	uint32_t dst_addr;		/**< destination address */
-} __attribute__((__packed__));
 
 /* Header lengths. */
 #define VLAN_HLEN       4
