3b845bf6bcd9d515a9836904a7d087e1fb263c91
Shaopeng He <shaopeng.he@intel.com>
2016-02-05 10:46:11 +0800

    fm10k: fix switch manager high CPU usage
    fm10k switch core uses source MAC + VID + SGLORT to do
    look up in MAC table. If no match, an exception interrupt
    will be sent to the switch manager. Too much of this kind
    of exception interrupts cause switch manager side high CPU
    usage.
    To reproduce this issue, one DPDK testpmd runs on a server
    with one fm10k NIC, mac forwards test traffic from one of
    fm10k ports to another port. The CPU usage for the switch
    manager will go up to about 20% for test traffic rate at
    10G bps, comparing to near 0% for no test traffic.
    
    This patch fixes this issue. A default SGLORT is assigned
    to each TX queue. This default value works for non-VMDq mode
    and current VMDq example. For advanced VMDq usage, e.g.
    different source MAC address for different TX queue, FTAG
    forwarding function could be used to change this default
    SGLORT value.
    
    Fixes: 9ae6068c86da ("fm10k: add dev start/stop")
    
    Signed-off-by: Shaopeng He <shaopeng.he@intel.com>
    Acked-by: Jing Chen <jing.d.chen@intel.com>

--- drivers/net/fm10k/fm10k_ethdev.c
+++ drivers/net/fm10k/fm10k_ethdev.c
@@ -675,6 +675,9 @@ fm10k_dev_tx_init(struct rte_eth_dev *dev)
 		FM10K_WRITE_REG(hw, FM10K_TDBAH(i),
 				base_addr >> (CHAR_BIT * sizeof(uint32_t)));
 		FM10K_WRITE_REG(hw, FM10K_TDLEN(i), size);
+
+		/* assign default SGLORT for each TX queue */
+		FM10K_WRITE_REG(hw, FM10K_TX_SGLORT(i), hw->mac.dglort_map);
 	}
 
 	/* set up vector or scalar TX function as appropriate */
