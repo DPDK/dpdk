7d85af136264188d8fa422589d64d7b6f2e1148a
Thomas Monjalon <thomas.monjalon@6wind.com>
2016-03-10 23:16:18 +0100

    scripts: test build with libsso
    The SNOW3G PMD has a dependency on libsso which can be downloaded
    after having passed some Intel validation about export restrictions.
    
    Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>
    Acked-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- scripts/test-build.sh
+++ scripts/test-build.sh
@@ -44,6 +44,7 @@ default_path=$PATH
 # - DPDK_DEP_ZLIB (y/[n])
 # - DPDK_MAKE_JOBS (int)
 # - DPDK_NOTIFY (notify-send)
+# - LIBSSO_PATH
 . $(dirname $(readlink -e $0))/load-devel-config.sh
 
 print_usage () {
@@ -118,6 +119,7 @@ reset_env ()
 	unset DPDK_DEP_SZE
 	unset DPDK_DEP_ZLIB
 	unset AESNI_MULTI_BUFFER_LIB_PATH
+	unset LIBSSO_PATH
 	unset PQOS_INSTALL_PATH
 }
 
@@ -157,6 +159,8 @@ config () # <directory> <target> <options>
 		sed -ri       's,(PMD_AESNI_MB=)n,\1y,' $1/.config
 		test -z "$AESNI_MULTI_BUFFER_LIB_PATH" || \
 		sed -ri      's,(PMD_AESNI_GCM=)n,\1y,' $1/.config
+		test -z "$LIBSSO_PATH" || \
+		sed -ri         's,(PMD_SNOW3G=)n,\1y,' $1/.config
 		test "$DPDK_DEP_SSL" != y || \
 		sed -ri            's,(PMD_QAT=)n,\1y,' $1/.config
 		sed -ri        's,(KNI_VHOST.*=)n,\1y,' $1/.config
