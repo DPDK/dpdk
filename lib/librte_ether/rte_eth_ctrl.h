DPDK/dpdk

commit 7b1312891b6995d0a20211d00b7421b51b87f9d5

ethdev: add IP in GRE tunnel
Signed-off-by: Xutao Sun <xutao.sun@intel.com>
Signed-off-by: Jijiang Liu <jijiang.liu@intel.com>

lib/librte_ether/rte_eth_ctrl.h
@@ -245,6 +245,7 @@ enum rte_eth_tunnel_type {
 	RTE_TUNNEL_TYPE_GENEVE,
 	RTE_TUNNEL_TYPE_TEREDO,
 	RTE_TUNNEL_TYPE_NVGRE,
+	RTE_TUNNEL_TYPE_IP_IN_GRE,
 	RTE_L2_TUNNEL_TYPE_E_TAG,
 	RTE_TUNNEL_TYPE_MAX,
 };

app/test-pmd/cmdline.c
@@ -298,11 +298,13 @@ static void cmd_help_long_parsed(void *parsed_result,
 			"filtered for VF(s) from port_id.\n\n"
 
 			"tunnel_filter add (port_id) (outer_mac) (inner_mac) (ip_addr) "
-			"(inner_vlan) (vxlan|nvgre) (filter_type) (tenant_id) (queue_id)\n"
+			"(inner_vlan) (vxlan|nvgre|ipingre) (imac-ivlan|imac-ivlan-tenid|"
+			"imac-tenid|imac|omac-imac-tenid|oip|iip) (tenant_id) (queue_id)\n"
 			"   add a tunnel filter of a port.\n\n"
 
 			"tunnel_filter rm (port_id) (outer_mac) (inner_mac) (ip_addr) "
-			"(inner_vlan) (vxlan|nvgre) (filter_type) (tenant_id) (queue_id)\n"
+			"(inner_vlan) (vxlan|nvgre|ipingre) (imac-ivlan|imac-ivlan-tenid|"
+			"imac-tenid|imac|omac-imac-tenid|oip|iip) (tenant_id) (queue_id)\n"
 			"   remove a tunnel filter of a port.\n\n"
 
 			"rx_vxlan_port add (udp_port) (port_id)\n"
@@ -6669,18 +6671,22 @@ cmd_tunnel_filter_parsed(void *parsed_result,
 	struct rte_eth_tunnel_filter_conf tunnel_filter_conf;
 	int ret = 0;
 
+	memset(&tunnel_filter_conf, 0, sizeof(tunnel_filter_conf));
+
 	ether_addr_copy(&res->outer_mac, &tunnel_filter_conf.outer_mac);
 	ether_addr_copy(&res->inner_mac, &tunnel_filter_conf.inner_mac);
 	tunnel_filter_conf.inner_vlan = res->inner_vlan;
 
 	if (res->ip_value.family == AF_INET) {
 		tunnel_filter_conf.ip_addr.ipv4_addr =
-			res->ip_value.addr.ipv4.s_addr;
+			rte_be_to_cpu_32(res->ip_value.addr.ipv4.s_addr);
 		tunnel_filter_conf.ip_type = RTE_TUNNEL_IPTYPE_IPV4;
 	} else {
-		memcpy(&(tunnel_filter_conf.ip_addr.ipv6_addr),
-			&(res->ip_value.addr.ipv6),
-			sizeof(struct in6_addr));
+		int i;
+		for (i = 0; i < 4; i++) {
+			tunnel_filter_conf.ip_addr.ipv6_addr[i] =
+			rte_be_to_cpu_32(res->ip_value.addr.ipv6.s6_addr32[i]);
+		}
 		tunnel_filter_conf.ip_type = RTE_TUNNEL_IPTYPE_IPV6;
 	}
 
@@ -6696,6 +6702,10 @@ cmd_tunnel_filter_parsed(void *parsed_result,
 	else if (!strcmp(res->filter_type, "omac-imac-tenid"))
 		tunnel_filter_conf.filter_type =
 			RTE_TUNNEL_FILTER_OMAC_TENID_IMAC;
+	else if (!strcmp(res->filter_type, "oip"))
+		tunnel_filter_conf.filter_type = ETH_TUNNEL_FILTER_OIP;
+	else if (!strcmp(res->filter_type, "iip"))
+		tunnel_filter_conf.filter_type = ETH_TUNNEL_FILTER_IIP;
 	else {
 		printf("The filter type is not supported");
 		return;
@@ -6705,6 +6715,8 @@ cmd_tunnel_filter_parsed(void *parsed_result,
 		tunnel_filter_conf.tunnel_type = RTE_TUNNEL_TYPE_VXLAN;
 	else if (!strcmp(res->tunnel_type, "nvgre"))
 		tunnel_filter_conf.tunnel_type = RTE_TUNNEL_TYPE_NVGRE;
+	else if (!strcmp(res->tunnel_type, "ipingre"))
+		tunnel_filter_conf.tunnel_type = RTE_TUNNEL_TYPE_IP_IN_GRE;
 	else {
 		printf("The tunnel type %s not supported.\n", res->tunnel_type);
 		return;
@@ -6750,11 +6762,11 @@ cmdline_parse_token_ipaddr_t cmd_tunnel_filter_ip_value =
 	ip_value);
 cmdline_parse_token_string_t cmd_tunnel_filter_tunnel_type =
 	TOKEN_STRING_INITIALIZER(struct cmd_tunnel_filter_result,
-	tunnel_type, "vxlan#nvgre");
+	tunnel_type, "vxlan#nvgre#ipingre");
 
 cmdline_parse_token_string_t cmd_tunnel_filter_filter_type =
 	TOKEN_STRING_INITIALIZER(struct cmd_tunnel_filter_result,
-	filter_type, "imac-ivlan#imac-ivlan-tenid#imac-tenid#"
+	filter_type, "oip#iip#imac-ivlan#imac-ivlan-tenid#imac-tenid#"
 		"imac#omac-imac-tenid");
 cmdline_parse_token_num_t cmd_tunnel_filter_tenant_id =
 	TOKEN_NUM_INITIALIZER(struct cmd_tunnel_filter_result,
@@ -6768,8 +6780,8 @@ cmdline_parse_inst_t cmd_tunnel_filter = {
 	.data = (void *)0,
 	.help_str = "add/rm tunnel filter of a port: "
 			"tunnel_filter add port_id outer_mac inner_mac ip "
-			"inner_vlan tunnel_type(vxlan|nvgre) filter_type "
-			"(imac-ivlan|imac-ivlan-tenid|imac-tenid|"
+			"inner_vlan tunnel_type(vxlan|nvgre|ipingre) filter_type "
+			"(oip|iip|imac-ivlan|imac-ivlan-tenid|imac-tenid|"
 			"imac|omac-imac-tenid) "
 			"tenant_id queue_num",
 	.tokens = {

commit 6354f576d803316f68eeb658dcefc6949d95c24f

ethdev: fix comments for filters
This patch fixes comments for tunnel filters and flow director flows.
e.g. states fields which are in big endian.

Fixes: 7b1312891b69 ("ethdev: add IP in GRE tunnel")
Fixes: d69be32d4d78 ("ethdev: structures to add or delete flow director")

Signed-off-by: Jingjing Wu <jingjing.wu@intel.com>

lib/librte_ether/rte_eth_ctrl.h
@@ -283,19 +283,22 @@ enum rte_tunnel_iptype {
  * Tunneling Packet filter configuration.
  */
 struct rte_eth_tunnel_filter_conf {
-	struct ether_addr outer_mac;  /**< Outer MAC address filter. */
-	struct ether_addr inner_mac;  /**< Inner MAC address filter. */
-	uint16_t inner_vlan;           /**< Inner VLAN filter. */
+	struct ether_addr outer_mac;    /**< Outer MAC address to match. */
+	struct ether_addr inner_mac;    /**< Inner MAC address to match. */
+	uint16_t inner_vlan;            /**< Inner VLAN to match. */
 	enum rte_tunnel_iptype ip_type; /**< IP address type. */
+	/** Outer destination IP address to match if ETH_TUNNEL_FILTER_OIP
+	    is set in filter_type, or inner destination IP address to match
+	    if ETH_TUNNEL_FILTER_IIP is set in filter_type . */
 	union {
-		uint32_t ipv4_addr;    /**< IPv4 source address to match. */
-		uint32_t ipv6_addr[4]; /**< IPv6 source address to match. */
-	} ip_addr; /**< IPv4/IPv6 source address to match (union of above). */
-
-	uint16_t filter_type;   /**< Filter type. */
+		uint32_t ipv4_addr;     /**< IPv4 address in big endian. */
+		uint32_t ipv6_addr[4];  /**< IPv6 address in big endian. */
+	} ip_addr;
+	/** Flags from ETH_TUNNEL_FILTER_XX - see above. */
+	uint16_t filter_type;
 	enum rte_eth_tunnel_type tunnel_type; /**< Tunnel Type. */
-	uint32_t tenant_id;     /**< Tenant number. */
-	uint16_t queue_id;      /**< Queue number. */
+	uint32_t tenant_id;     /**< Tenant ID to match. VNI, GRE key... */
+	uint16_t queue_id;      /**< Queue assigned to if match. */
 };
 
 /**
@@ -403,18 +406,18 @@ struct rte_eth_input_set_conf {
  * A structure used to define the input for L2 flow
  */
 struct rte_eth_l2_flow {
-	uint16_t ether_type;          /**< Ether type to match */
+	uint16_t ether_type;          /**< Ether type in big endian */
 };
 
 /**
  * A structure used to define the input for IPV4 flow
  */
 struct rte_eth_ipv4_flow {
-	uint32_t src_ip;      /**< IPv4 source address to match. */
-	uint32_t dst_ip;      /**< IPv4 destination address to match. */
+	uint32_t src_ip;      /**< IPv4 source address in big endian. */
+	uint32_t dst_ip;      /**< IPv4 destination address in big endian. */
 	uint8_t  tos;         /**< Type of service to match. */
 	uint8_t  ttl;         /**< Time to live to match. */
-	uint8_t  proto;       /**< Protocol, next header to match. */
+	uint8_t  proto;       /**< Protocol, next header in big endian. */
 };
 
 /**
@@ -422,8 +425,8 @@ struct rte_eth_ipv4_flow {
  */
 struct rte_eth_udpv4_flow {
 	struct rte_eth_ipv4_flow ip; /**< IPv4 fields to match. */
-	uint16_t src_port;           /**< UDP source port to match. */
-	uint16_t dst_port;           /**< UDP destination port to match. */
+	uint16_t src_port;           /**< UDP source port in big endian. */
+	uint16_t dst_port;           /**< UDP destination port in big endian. */
 };
 
 /**
@@ -431,8 +434,8 @@ struct rte_eth_udpv4_flow {
  */
 struct rte_eth_tcpv4_flow {
 	struct rte_eth_ipv4_flow ip; /**< IPv4 fields to match. */
-	uint16_t src_port;           /**< TCP source port to match. */
-	uint16_t dst_port;           /**< TCP destination port to match. */
+	uint16_t src_port;           /**< TCP source port in big endian. */
+	uint16_t dst_port;           /**< TCP destination port in big endian. */
 };
 
 /**
@@ -440,17 +443,17 @@ struct rte_eth_tcpv4_flow {
  */
 struct rte_eth_sctpv4_flow {
 	struct rte_eth_ipv4_flow ip; /**< IPv4 fields to match. */
-	uint16_t src_port;           /**< SCTP source port to match. */
-	uint16_t dst_port;           /**< SCTP destination port to match. */
-	uint32_t verify_tag;         /**< Verify tag to match */
+	uint16_t src_port;           /**< SCTP source port in big endian. */
+	uint16_t dst_port;           /**< SCTP destination port in big endian. */
+	uint32_t verify_tag;         /**< Verify tag in big endian */
 };
 
 /**
  * A structure used to define the input for IPV6 flow
  */
 struct rte_eth_ipv6_flow {
-	uint32_t src_ip[4];      /**< IPv6 source address to match. */
-	uint32_t dst_ip[4];      /**< IPv6 destination address to match. */
+	uint32_t src_ip[4];      /**< IPv6 source address in big endian. */
+	uint32_t dst_ip[4];      /**< IPv6 destination address in big endian. */
 	uint8_t  tc;             /**< Traffic class to match. */
 	uint8_t  proto;          /**< Protocol, next header to match. */
 	uint8_t  hop_limits;     /**< Hop limits to match. */
@@ -461,8 +464,8 @@ struct rte_eth_ipv6_flow {
  */
 struct rte_eth_udpv6_flow {
 	struct rte_eth_ipv6_flow ip; /**< IPv6 fields to match. */
-	uint16_t src_port;           /**< UDP source port to match. */
-	uint16_t dst_port;           /**< UDP destination port to match. */
+	uint16_t src_port;           /**< UDP source port in big endian. */
+	uint16_t dst_port;           /**< UDP destination port in big endian. */
 };
 
 /**
@@ -470,8 +473,8 @@ struct rte_eth_udpv6_flow {
  */
 struct rte_eth_tcpv6_flow {
 	struct rte_eth_ipv6_flow ip; /**< IPv6 fields to match. */
-	uint16_t src_port;           /**< TCP source port to match. */
-	uint16_t dst_port;           /**< TCP destination port to match. */
+	uint16_t src_port;           /**< TCP source port to in big endian. */
+	uint16_t dst_port;           /**< TCP destination port in big endian. */
 };
 
 /**
@@ -479,9 +482,9 @@ struct rte_eth_tcpv6_flow {
  */
 struct rte_eth_sctpv6_flow {
 	struct rte_eth_ipv6_flow ip; /**< IPv6 fields to match. */
-	uint16_t src_port;           /**< SCTP source port to match. */
-	uint16_t dst_port;           /**< SCTP destination port to match. */
-	uint32_t verify_tag;         /**< Verify tag to match */
+	uint16_t src_port;           /**< SCTP source port in big endian. */
+	uint16_t dst_port;           /**< SCTP destination port in big endian. */
+	uint32_t verify_tag;         /**< Verify tag in big endian. */
 };
 
 /**
@@ -506,7 +509,8 @@ enum rte_eth_fdir_tunnel_type {
  */
 struct rte_eth_tunnel_flow {
 	enum rte_eth_fdir_tunnel_type tunnel_type; /**< Tunnel type to match. */
-	uint32_t tunnel_id;                        /**< Tunnel ID to match. TNI, VNI... */
+	/** Tunnel ID to match. TNI, VNI... in big endian. */
+	uint32_t tunnel_id;
 	struct ether_addr mac_addr;                /**< Mac address to match. */
 };
 