e2dc61c806e3681d1f12300d62df9dcd4402190f
Ferruh Yigit <ferruh.yigit@intel.com>
2016-05-30 12:22:32 +0100

    kni: fix build with kernel 4.7
    Fix compile error because of Linux API change, 'trans_start' field
    removed from 'struct net_device'.
    
    Linux: 9b36627acecd ("net: remove dev->trans_start")
    
    Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- lib/librte_eal/linuxapp/kni/kni_net.c
+++ lib/librte_eal/linuxapp/kni/kni_net.c
@@ -38,6 +38,8 @@
 
 #include <exec-env/rte_kni_common.h>
 #include <kni_fifo.h>
+
+#include "compat.h"
 #include "kni_dev.h"
 
 #define WD_TIMEOUT 5 /*jiffies */
@@ -426,7 +428,12 @@ kni_net_tx(struct sk_buff *skb, struct net_device *dev)
 	struct rte_kni_mbuf *pkt_kva = NULL;
 	struct rte_kni_mbuf *pkt_va = NULL;
 
-	dev->trans_start = jiffies; /* save the timestamp */
+	/* save the timestamp */
+#ifdef HAVE_TRANS_START_HELPER
+	netif_trans_update(dev);
+#else
+	dev->trans_start = jiffies;
+#endif
 
 	/* Check if the length of skb is less than mbuf size */
 	if (skb->len > kni->mbuf_size)
