43b194b43344b262b00f6c8a24443b46ed82009b
Sergio Gonzalez Monroy <sergio.gonzalez.monroy@intel.com>
2016-06-09 09:19:47 +0100

    mempool: fix local cache initialization
    The mempool local cache was not initialized properly leading to
    undefined behavior in cases where the allocated memory was used
    previously and left with data.
    
    Fixes: 213af31e0960 ("mempool: reduce structure size if no cache needed")
    
    Signed-off-by: Sergio Gonzalez Monroy <sergio.gonzalez.monroy@intel.com>
    Acked-by: Olivier Matz <olivier.matz@6wind.com>

--- lib/librte_mempool/rte_mempool.c
+++ lib/librte_mempool/rte_mempool.c
@@ -787,7 +787,7 @@ rte_mempool_create_empty(const char *name, unsigned n, unsigned elt_size,
 
 	/* init the mempool structure */
 	mp = mz->addr;
-	memset(mp, 0, sizeof(*mp));
+	memset(mp, 0, MEMPOOL_HEADER_SIZE(mp, cache_size));
 	ret = snprintf(mp->name, sizeof(mp->name), "%s", name);
 	if (ret < 0 || ret >= (int)sizeof(mp->name)) {
 		rte_errno = ENAMETOOLONG;
