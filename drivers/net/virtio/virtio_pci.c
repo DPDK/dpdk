7e40200c567ff798d7cd54085c3744cf3393ce66
Huawei Xie <huawei.xie@intel.com>
2016-06-13 22:53:08 +0800

    net/virtio: fix crash when no devargs
    We skip kernel managed virtio devices, if it isn't whitelisted.
    Before checking if the virtio device is whitelisted, check if devargs
    is specified.
    
    Fixes: ac5e1d838dc1 ("virtio: skip error when probing kernel managed device")
    
    Reported-by: Vincent Li <vincent.mc.li@gmail.com>
    Signed-off-by: Huawei Xie <huawei.xie@intel.com>
    Acked-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

--- drivers/net/virtio/virtio_pci.c
+++ drivers/net/virtio/virtio_pci.c
@@ -694,6 +694,7 @@ next:
  * Return -1:
  *   if there is error mapping with VFIO/UIO.
  *   if port map error when driver type is KDRV_NONE.
+ *   if whitelisted but driver type is KDRV_UNKNOWN.
  * Return 1 if kernel driver is managing the device.
  * Return 0 on success.
  */
@@ -719,7 +720,8 @@ vtpci_init(struct rte_pci_device *dev, struct virtio_hw *hw,
 	PMD_INIT_LOG(INFO, "trying with legacy virtio pci.");
 	if (legacy_virtio_resource_init(dev, hw, dev_flags) < 0) {
 		if (dev->kdrv == RTE_KDRV_UNKNOWN &&
-		    dev->devargs->type != RTE_DEVTYPE_WHITELISTED_PCI) {
+		    (!dev->devargs ||
+		     dev->devargs->type != RTE_DEVTYPE_WHITELISTED_PCI)) {
 			PMD_INIT_LOG(INFO,
 				"skip kernel managed virtio device.");
 			return 1;
