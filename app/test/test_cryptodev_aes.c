f468db6fa8e68296c84c17b93c3df99ff9f54a1d
Fan Zhang <roy.fan.zhang@intel.com>
2016-06-15 12:02:50 +0100

    app/test: add more AES algorithms
    This patch adds the HMAC-SHA224 and HMAC-SHA384 digest generation and
    verification tests to crypto
    
    Signed-off-by: Fan Zhang <roy.fan.zhang@intel.com>
    Acked-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- app/test/test_cryptodev_aes.c
+++ app/test/test_cryptodev_aes.c
@@ -207,6 +207,32 @@ static const struct aes_test_case aes_test_cases[] = {
 		.feature_mask = AES_TEST_FEATURE_OOP,
 		.pmd_mask = AES_TEST_TARGET_PMD_QAT
 	},
+	{
+		.test_descr = "AES-128-CBC HMAC-SHA224 Encryption Digest",
+		.test_data = &aes_test_data_8,
+		.op_mask = AES_TEST_OP_ENC_AUTH_GEN,
+		.pmd_mask = AES_TEST_TARGET_PMD_MB
+	},
+	{
+		.test_descr = "AES-128-CBC HMAC-SHA224 Decryption Digest "
+			"Verify",
+		.test_data = &aes_test_data_8,
+		.op_mask = AES_TEST_OP_AUTH_VERIFY_DEC,
+		.pmd_mask = AES_TEST_TARGET_PMD_MB
+	},
+	{
+		.test_descr = "AES-128-CBC HMAC-SHA384 Encryption Digest",
+		.test_data = &aes_test_data_9,
+		.op_mask = AES_TEST_OP_ENC_AUTH_GEN,
+		.pmd_mask = AES_TEST_TARGET_PMD_MB
+	},
+	{
+		.test_descr = "AES-128-CBC HMAC-SHA384 Decryption Digest "
+			"Verify",
+		.test_data = &aes_test_data_9,
+		.op_mask = AES_TEST_OP_AUTH_VERIFY_DEC,
+		.pmd_mask = AES_TEST_TARGET_PMD_MB
+	},
 };
 
 static int
