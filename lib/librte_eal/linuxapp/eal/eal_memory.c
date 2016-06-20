DPDK/dpdk

commit 148f963fb5323c1c6b6d5cea95084deb25cc73f8

xen: core library changes
Core support for using the Intel DPDK with Xen Dom0 - including EAL
changes and mempool changes. These changes encompass how memory mapping
is done, including support for initializing a memory pool inside an
already-allocated block of memory.
KNI sample app updated to use KNI close function when used with Xen.

Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>

lib/librte_eal/linuxapp/eal/eal_memory.c
@@ -984,6 +984,17 @@ rte_eal_hugepage_init(void)
 		return 0;
 	}
 
+/* check if app runs on Xen Dom0 */
+	if (internal_config.xen_dom0_support) {
+#ifdef RTE_LIBRTE_XEN_DOM0
+		/* use dom0_mm kernel driver to init memory */
+		if (rte_xen_dom0_memory_init() < 0)
+			return -1;
+		else
+			return 0;
+#endif
+	}
+
 
 	/* calculate total number of hugepages available. at this point we haven't
 	 * yet started sorting them so they all are on socket 0 */
@@ -1269,6 +1280,17 @@ rte_eal_hugepage_attach(void)
 				"(ASLR) is enabled in the kernel.\n");
 		RTE_LOG(WARNING, EAL, "   This may cause issues with mapping memory "
 				"into secondary processes\n");
+	}
+
+	if (internal_config.xen_dom0_support) {
+#ifdef RTE_LIBRTE_XEN_DOM0
+		if (rte_xen_dom0_memory_attach() < 0) {
+			RTE_LOG(ERR, EAL,"Failed to attach memory setments of primay "
+					"process\n");
+			return -1;
+		}
+		return 0;
+#endif
 	}
 
 	fd_zero = open("/dev/zero", O_RDONLY);

app/test-pmd/config.c
@@ -974,10 +974,12 @@ pkt_fwd_config_display(struct fwd_config *cfg)
 	streamid_t sm_id;
 
 	printf("%s packet forwarding - ports=%d - cores=%d - streams=%d - "
-	       "NUMA support %s\n",
-	       cfg->fwd_eng->fwd_mode_name,
-	       cfg->nb_fwd_ports, cfg->nb_fwd_lcores, cfg->nb_fwd_streams,
-	       numa_support == 1 ? "enabled" : "disabled");
+		"NUMA support %s, MP over anonymous pages %s\n",
+		cfg->fwd_eng->fwd_mode_name,
+		cfg->nb_fwd_ports, cfg->nb_fwd_lcores, cfg->nb_fwd_streams,
+		numa_support == 1 ? "enabled" : "disabled",
+		mp_anon != 0 ? "enabled" : "disabled");
+
 	for (lc_id = 0; lc_id < cfg->nb_fwd_lcores; lc_id++) {
 		printf("Logical Core %u (socket %u) forwards packets on "
 		       "%d streams:",

app/test-pmd/mempool_anon.c
@@ -1,0 +1,201 @@
+/*-
+ *   BSD LICENSE
+ * 
+ *   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+ *   All rights reserved.
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ * 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "mempool_osdep.h"
+#include <rte_errno.h>
+
+#ifdef RTE_EXEC_ENV_LINUXAPP
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+
+#define	PAGEMAP_FNAME		"/proc/self/pagemap"
+
+/*
+ * the pfn (page frame number) are bits 0-54 (see pagemap.txt in linux
+ * Documentation).
+ */
+#define	PAGEMAP_PFN_BITS	54
+#define	PAGEMAP_PFN_MASK	RTE_LEN2MASK(PAGEMAP_PFN_BITS, phys_addr_t)
+
+
+static int
+get_phys_map(void *va, phys_addr_t pa[], uint32_t pg_num, uint32_t pg_sz)
+{
+	int32_t fd, rc;
+	uint32_t i, nb;
+	off_t ofs;
+
+	ofs = (uintptr_t)va / pg_sz * sizeof(*pa);
+	nb = pg_num * sizeof(*pa);
+
+	if ((fd = open(PAGEMAP_FNAME, O_RDONLY)) < 0)
+		return (ENOENT);
+
+	if ((rc = pread(fd, pa, nb, ofs)) < 0 || (rc -= nb) != 0) {
+
+		RTE_LOG(ERR, USER1, "failed read of %u bytes from \'%s\' "
+			"at offset %zu, error code: %d\n",
+			nb, PAGEMAP_FNAME, (size_t)ofs, errno);
+		rc = ENOENT;
+	} 
+
+	close(fd);
+
+	for (i = 0; i != pg_num; i++)
+		pa[i] = (pa[i] & PAGEMAP_PFN_MASK) * pg_sz;
+
+	return (rc);
+}
+
+struct rte_mempool *
+mempool_anon_create(const char *name, unsigned elt_num, unsigned elt_size,
+		   unsigned cache_size, unsigned private_data_size,
+		   rte_mempool_ctor_t *mp_init, void *mp_init_arg,
+		   rte_mempool_obj_ctor_t *obj_init, void *obj_init_arg,
+		   int socket_id, unsigned flags)
+{
+	struct rte_mempool *mp;
+	phys_addr_t *pa;
+	char *va, *uv;
+	uint32_t n, pg_num, pg_shift, pg_sz, total_size;
+	size_t sz;
+	ssize_t usz;
+	int32_t rc;
+
+	rc = ENOMEM;
+	mp = NULL;
+
+	pg_sz = getpagesize();
+	if (rte_is_power_of_2(pg_sz) == 0) {
+		rte_errno = EINVAL;
+		return (mp);
+	}
+
+	pg_shift = rte_bsf32(pg_sz);
+
+	total_size = rte_mempool_calc_obj_size(elt_size, flags, NULL);
+
+	/* calc max memory size and max number of pages needed. */
+	sz = rte_mempool_xmem_size(elt_num, total_size, pg_shift);
+	pg_num = sz >> pg_shift;
+
+	/* get chunk of virtually continuos memory.*/
+	if ((va = mmap(NULL, sz, PROT_READ | PROT_WRITE,
+			MAP_SHARED | MAP_ANONYMOUS | MAP_LOCKED,
+			-1, 0)) == MAP_FAILED) {
+		RTE_LOG(ERR, USER1, "%s(%s) failed mmap of %zu bytes, "
+			"error code: %d\n",
+			__func__, name, sz, errno);
+		rte_errno = rc;
+		return (mp);
+	}
+
+	/* extract physical mappings of the allocated memory. */
+	if ((pa = calloc(pg_num, sizeof (*pa))) != NULL &&
+			(rc = get_phys_map(va, pa, pg_num, pg_sz)) == 0) {
+
+		/*
+ 		 * Check that allocated size is big enough to hold elt_num
+ 		 * objects and a calcualte how many bytes are actually required.
+		 */
+
+		if ((usz = rte_mempool_xmem_usage(va, elt_num, total_size, pa,
+				pg_num, pg_shift)) < 0) {
+			
+			n = -usz;
+			rc = ENOENT;
+			RTE_LOG(ERR, USER1, "%s(%s) only %u objects from %u "
+				"requested can  be created over " 
+				"mmaped region %p of %zu bytes\n",
+				__func__, name, n, elt_num, va, sz);
+		} else {
+
+			/* unmap unused pages if any */
+			if ((size_t)usz < sz) {
+
+				uv = va + usz;
+				usz = sz - usz;
+				
+				RTE_LOG(INFO, USER1,
+					"%s(%s): unmap unused %zu of %zu "
+					"mmaped bytes @%p\n",
+					__func__, name, (size_t)usz, sz, uv);
+				munmap(uv, usz);
+				sz -= usz;
+				pg_num = sz >> pg_shift;
+			}
+			
+			if ((mp = rte_mempool_xmem_create(name, elt_num,
+					elt_size, cache_size, private_data_size,
+					mp_init, mp_init_arg,
+					obj_init, obj_init_arg,
+					socket_id, flags, va, pa, pg_num,
+					pg_shift)) != NULL)
+
+				RTE_VERIFY(elt_num == mp->size);
+		}
+	} 
+
+	if (mp == NULL) {
+		munmap(va, sz);
+		rte_errno = rc;
+	}
+
+	free(pa);
+	return (mp);
+}
+
+#else /* RTE_EXEC_ENV_LINUXAPP */
+
+
+struct rte_mempool *
+mempool_anon_create(__rte_unused const char *name,
+	__rte_unused unsigned elt_num, __rte_unused unsigned elt_size,
+	__rte_unused unsigned cache_size,
+	__rte_unused unsigned private_data_size,
+	__rte_unused rte_mempool_ctor_t *mp_init,
+	__rte_unused void *mp_init_arg,
+	__rte_unused rte_mempool_obj_ctor_t *obj_init,
+	__rte_unused void *obj_init_arg,
+	__rte_unused int socket_id, __rte_unused unsigned flags)
+{
+	rte_errno = ENOTSUP;
+	return (NULL);
+}
+
+#endif /* RTE_EXEC_ENV_LINUXAPP */

app/test-pmd/mempool_osdep.h
@@ -1,0 +1,54 @@
+/*-
+ *   BSD LICENSE
+ * 
+ *   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+ *   All rights reserved.
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ * 
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _MEMPOOL_OSDEP_H_
+#define _MEMPOOL_OSDEP_H_
+
+#include <rte_mempool.h>
+
+/**
+ * @file
+ * mempool OS specific header.
+ */
+
+/*
+ * Create mempool over objects from mmap(..., MAP_ANONYMOUS, ...).
+ */
+struct rte_mempool *
+mempool_anon_create(const char *name, unsigned n, unsigned elt_size,
+	unsigned cache_size, unsigned private_data_size,
+	rte_mempool_ctor_t *mp_init, void *mp_init_arg,
+	rte_mempool_obj_ctor_t *obj_init, void *obj_init_arg,
+	int socket_id, unsigned flags);
+
+#endif /*_RTE_MEMPOOL_OSDEP_H_ */

app/test-pmd/parameters.c
@@ -478,6 +478,7 @@ launch_args_parse(int argc, char** argv)
 		{ "coremask",			1, 0, 0 },
 		{ "portmask",			1, 0, 0 },
 		{ "numa",			0, 0, 0 },
+		{ "mp-anon",			0, 0, 0 },
 		{ "port-numa-config",           1, 0, 0 },
 		{ "ring-numa-config",           1, 0, 0 },
 		{ "socket-num",			1, 0, 0 },	
@@ -593,6 +594,9 @@ launch_args_parse(int argc, char** argv)
 				memset(port_numa,NUMA_NO_CONFIG,RTE_MAX_ETHPORTS);
 				memset(rxring_numa,NUMA_NO_CONFIG,RTE_MAX_ETHPORTS);
 				memset(txring_numa,NUMA_NO_CONFIG,RTE_MAX_ETHPORTS);
+			}
+			if (!strcmp(lgopts[opt_idx].name, "mp-anon")) {
+				mp_anon = 1;
 			}
 			if (!strcmp(lgopts[opt_idx].name, "port-numa-config")) {
 				if (parse_portnuma_config(optarg))

app/test-pmd/testpmd.c
@@ -72,8 +72,12 @@
 #include <rte_ether.h>
 #include <rte_ethdev.h>
 #include <rte_string_fns.h>
+#ifdef RTE_LIBRTE_PMD_XENVIRT
+#include <rte_eth_xenvirt.h>
+#endif
 
 #include "testpmd.h"
+#include "mempool_osdep.h"
 
 uint16_t verbose_level = 0; /**< Silent by default. */
 
@@ -94,6 +98,11 @@ uint8_t numa_support = 0; /**< No numa support by default */
  * not configured.
  */
 uint8_t socket_num = UMA_NO_CONFIG; 
+
+/*
+ * Use ANONYMOUS mapped memory (might be not physically continuous) for mbufs.
+ */
+uint8_t mp_anon = 0;
 
 /*
  * Record the Ethernet address of peer target ports to which packets are
@@ -407,8 +416,7 @@ testpmd_mbuf_pool_ctor(struct rte_mempool *mp,
 		return;
 	}
 	mbp_ctor_arg = (struct mbuf_pool_ctor_arg *) opaque_arg;
-	mbp_priv = (struct rte_pktmbuf_pool_private *)
-		((char *)mp + sizeof(struct rte_mempool));
+	mbp_priv = rte_mempool_get_priv(mp);
 	mbp_priv->mbuf_data_room_size = mbp_ctor_arg->seg_buf_size;
 }
 
@@ -429,15 +437,40 @@ mbuf_pool_create(uint16_t mbuf_seg_size, unsigned nb_mbuf,
 	mb_ctor_arg.seg_buf_size = mbp_ctor_arg.seg_buf_size;
 	mb_size = mb_ctor_arg.seg_buf_offset + mb_ctor_arg.seg_buf_size;
 	mbuf_poolname_build(socket_id, pool_name, sizeof(pool_name));
-	rte_mp = rte_mempool_create(pool_name, nb_mbuf, (unsigned) mb_size,
+
+#ifdef RTE_LIBRTE_PMD_XENVIRT
+	rte_mp = rte_mempool_gntalloc_create(pool_name, nb_mbuf, mb_size,
+                                   (unsigned) mb_mempool_cache,
+                                   sizeof(struct rte_pktmbuf_pool_private),
+                                   testpmd_mbuf_pool_ctor, &mbp_ctor_arg,
+                                   testpmd_mbuf_ctor, &mb_ctor_arg,
+                                   socket_id, 0);
+
+
+
+#else
+	if (mp_anon != 0)
+		rte_mp = mempool_anon_create(pool_name, nb_mbuf, mb_size,
 				    (unsigned) mb_mempool_cache,
 				    sizeof(struct rte_pktmbuf_pool_private),
 				    testpmd_mbuf_pool_ctor, &mbp_ctor_arg,
 				    testpmd_mbuf_ctor, &mb_ctor_arg,
 				    socket_id, 0);
+	else 
+		rte_mp = rte_mempool_create(pool_name, nb_mbuf, mb_size,
+				    (unsigned) mb_mempool_cache,
+				    sizeof(struct rte_pktmbuf_pool_private),
+				    testpmd_mbuf_pool_ctor, &mbp_ctor_arg,
+				    testpmd_mbuf_ctor, &mb_ctor_arg,
+				    socket_id, 0);
+
+#endif
+
 	if (rte_mp == NULL) {
 		rte_exit(EXIT_FAILURE, "Creation of mbuf pool for socket %u "
 						"failed\n", socket_id);
+	} else if (verbose_level > 0) {
+		rte_mempool_dump(rte_mp);
 	}
 }
 
@@ -1136,7 +1169,7 @@ all_ports_started(void)
 	return 1;
 }
 
-void
+int
 start_port(portid_t pid)
 {
 	int diag, need_check_link_status = 0;
@@ -1146,12 +1179,12 @@ start_port(portid_t pid)
 
 	if (test_done == 0) {
 		printf("Please stop forwarding first\n");
-		return;
+		return -1;
 	}
 
 	if (init_fwd_streams() < 0) {
 		printf("Fail from init_fwd_streams()\n");
-		return;
+		return -1;
 	}
 	
 	if(dcb_config)
@@ -1183,7 +1216,7 @@ start_port(portid_t pid)
 				printf("Fail to configure port %d\n", pi);
 				/* try to reconfigure port next time */
 				port->need_reconfig = 1;
-				return;
+				return -1;
 			}
 		}
 		if (port->need_reconfig_queues > 0) {
@@ -1212,7 +1245,7 @@ start_port(portid_t pid)
 				printf("Fail to configure port %d tx queues\n", pi);
 				/* try to reconfigure queues next time */
 				port->need_reconfig_queues = 1;
-				return;
+				return -1;
 			}
 			/* setup rx queues */
 			for (qi = 0; qi < nb_rxq; qi++) {
@@ -1225,7 +1258,7 @@ start_port(portid_t pid)
 							"No mempool allocation"
 							"on the socket %d\n",
 							rxring_numa[pi]);
-						return;
+						return -1;
 					}
 					
 					diag = rte_eth_rx_queue_setup(pi, qi,
@@ -1251,7 +1284,7 @@ start_port(portid_t pid)
 				printf("Fail to configure port %d rx queues\n", pi);
 				/* try to reconfigure queues next time */
 				port->need_reconfig_queues = 1;
-				return;
+				return -1;
 			}
 		}
 		/* start port */
@@ -1280,6 +1313,7 @@ start_port(portid_t pid)
 		printf("Please stop the ports first\n");
 
 	printf("Done\n");
+	return 0;
 }
 
 void
@@ -1732,7 +1766,8 @@ main(int argc, char** argv)
 		       nb_rxq, nb_txq);
 
 	init_config();
-	start_port(RTE_PORT_ALL);
+	if (start_port(RTE_PORT_ALL) != 0)
+		rte_exit(EXIT_FAILURE, "Start ports failed\n");
 
 	/* set all ports to promiscuous mode by default */
 	for (port_id = 0; port_id < nb_ports; port_id++)

app/test-pmd/testpmd.h
@@ -271,6 +271,7 @@ extern uint8_t  interactive;
 extern uint8_t  numa_support; /**< set by "--numa" parameter */
 extern uint16_t port_topology; /**< set by "--port-topology" parameter */
 extern uint8_t no_flush_rx; /**<set by "--no-flush-rx" parameter */
+extern uint8_t  mp_anon; /**< set by "--mp-anon" parameter */
 
 #ifdef RTE_NIC_BYPASS
 extern uint32_t bypass_timeout; /**< Store the NIC bypass watchdog timeout */
@@ -489,7 +490,7 @@ void start_packet_forwarding(int with_tx_first);
 void stop_packet_forwarding(void);
 void init_port_config(void);
 int init_port_dcb_config(portid_t pid,struct dcb_config *dcb_conf);
-void start_port(portid_t pid);
+int start_port(portid_t pid);
 void stop_port(portid_t pid);
 void close_port(portid_t pid);
 int all_ports_stopped(void);

app/test/process.h
@@ -53,7 +53,12 @@
 static inline int
 process_dup(const char *const argv[], int numargs, const char *env_value)
 {
+	int num;
+#ifdef RTE_LIBRTE_XEN_DOM0
+	char *argv_cpy[numargs + 2];
+#else
 	char *argv_cpy[numargs + 1];
+#endif
 	int i, fd, status;
 	char path[32];
 
@@ -64,7 +69,14 @@ process_dup(const char *const argv[], int numargs, const char *env_value)
 		/* make a copy of the arguments to be passed to exec */
 		for (i = 0; i < numargs; i++)
 			argv_cpy[i] = strdup(argv[i]);
+	#ifdef RTE_LIBRTE_XEN_DOM0
+        	argv_cpy[i] = strdup("--xen-dom0");
+        	argv_cpy[i + 1] = NULL;
+		num = numargs + 1;
+	#else
 		argv_cpy[i] = NULL;
+		num = numargs;
+	#endif
 
 		/* close all open file descriptors, check /proc/self/fd to only
 		 * call close on open fds. Exclude fds 0, 1 and 2*/
@@ -74,7 +86,7 @@ process_dup(const char *const argv[], int numargs, const char *env_value)
 				close(fd);
 		}
 		printf("Running binary with argv[]:");
-		for (i = 0; i < numargs; i++)
+		for (i = 0; i < num; i++)
 			printf("'%s' ", argv_cpy[i]);
 		printf("\n");
 

commit 2489f1193c1617d55af57d8184a0e9e9ace1c848

mem: fix error message with Xen
Minor typo fix to error message

Fixes: 148f963fb532 ("xen: core library changes")

Signed-off-by: Marcin Kerlin <marcinx.kerlin@intel.com>

lib/librte_eal/linuxapp/eal/eal_memory.c
@@ -1417,7 +1417,7 @@ rte_eal_hugepage_attach(void)
 	if (internal_config.xen_dom0_support) {
 #ifdef RTE_LIBRTE_XEN_DOM0
 		if (rte_xen_dom0_memory_attach() < 0) {
-			RTE_LOG(ERR, EAL,"Failed to attach memory setments of primay "
+			RTE_LOG(ERR, EAL, "Failed to attach memory segments of primary "
 					"process\n");
 			return -1;
 		}