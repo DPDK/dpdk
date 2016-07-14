a088b5ea35e78d3866d492ed5e8c8f47118696a6
Damjan Marion <damarion@cisco.com>
2016-07-14 15:27:29 +0200

    spinlock: move constructor out of x86 header file
    Having constructor function in the header file is generally
    a bad idea, as it will eventually be implanted to 3rd party
    library.
    
    In this case it causes linking issues with 3rd party libraries
    when an application is not linked to dpdk, due to missing
    symbol called by constructor.
    
    Fixes: ba7468997ea6 ("spinlock: add HTM lock elision for x86")
    
    Signed-off-by: Damjan Marion <damarion@cisco.com>
    Reviewed-by: Jan Viktorin <viktorin@rehivetech.com>
    Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>

--- lib/librte_eal/common/include/arch/x86/rte_spinlock.h
+++ lib/librte_eal/common/include/arch/x86/rte_spinlock.h
@@ -94,24 +94,17 @@ rte_spinlock_trylock (rte_spinlock_t *sl)
 }
 #endif
 
-static uint8_t rtm_supported; /* cache the flag to avoid the overhead
-				 of the rte_cpu_get_flag_enabled function */
-
-static inline void __attribute__((constructor))
-rte_rtm_init(void)
-{
-	rtm_supported = rte_cpu_get_flag_enabled(RTE_CPUFLAG_RTM);
-}
+extern uint8_t rte_rtm_supported;
 
 static inline int rte_tm_supported(void)
 {
-	return rtm_supported;
+	return rte_rtm_supported;
 }
 
 static inline int
 rte_try_tm(volatile int *lock)
 {
-	if (!rtm_supported)
+	if (!rte_rtm_supported)
 		return 0;
 
 	int retries = RTE_RTM_MAX_RETRIES;
