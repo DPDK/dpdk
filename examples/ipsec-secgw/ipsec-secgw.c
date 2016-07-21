b86eeb2aba2bd7c0fc22a317bcf7cfb5e3541e06
Hemant Agrawal <hemant.agrawal@nxp.com>
2016-07-21 16:24:04 +0530

    examples/ipsec-secgw: call start function
    The usual device sequence is configure, queue setup and start.
    Crypto device should be started before use.
    
    Signed-off-by: Akhil Goyal <akhil.goyal@nxp.com>
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>
    Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>
    Acked-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- examples/ipsec-secgw/ipsec-secgw.c
+++ examples/ipsec-secgw/ipsec-secgw.c
@@ -1273,6 +1273,10 @@ cryptodevs_init(void)
 						&qp_conf, dev_conf.socket_id))
 				rte_panic("Failed to setup queue %u for "
 						"cdev_id %u\n",	0, cdev_id);
+
+		if (rte_cryptodev_start(cdev_id))
+			rte_panic("Failed to start cryptodev %u\n",
+					cdev_id);
 	}
 
 	printf("\n");
