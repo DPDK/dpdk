e9261d5731450a97b462551d705bed8ae1a6c3ab
Ferruh Yigit <ferruh.yigit@intel.com>
2016-08-31 17:53:51 +0100

    net/ring: fix parameter string info
    Fixes: 65eca099f405 ("drivers: split parameters infos in multiple lines")
    
    Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- drivers/net/ring/rte_eth_ring.c
+++ drivers/net/ring/rte_eth_ring.c
@@ -630,4 +630,4 @@ static struct rte_vdev_driver pmd_ring_drv = {
 
 DRIVER_REGISTER_VDEV(net_ring, pmd_ring_drv);
 DRIVER_REGISTER_PARAM_STRING(net_ring,
-	"nodeaction=[attach|detach]");
+	ETH_RING_NUMA_NODE_ACTION_ARG "=name:node:action(ATTACH|CREATE)");
