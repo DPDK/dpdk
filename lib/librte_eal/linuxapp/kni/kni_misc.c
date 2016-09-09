DPDK/dpdk

commit e6734d21b4e1ebd0429eb7965e064afd1d2f6f7b

kni: fix build with kernel 2.6.32
It fixes the compile issue on kernel version 2.6.32 or old ones.

Error logs:
lib/librte_eal/linuxapp/kni/kni_misc.c:121: error: unknown field id specified in initializer
lib/librte_eal/linuxapp/kni/kni_misc.c:121: error: excess elements in struct initializer
lib/librte_eal/linuxapp/kni/kni_misc.c:121: error: (near initialization for kni_net_ops)
lib/librte_eal/linuxapp/kni/kni_misc.c:122: error: unknown field size specified in initializer
lib/librte_eal/linuxapp/kni/kni_misc.c:122: error: excess elements in struct initializer
lib/librte_eal/linuxapp/kni/kni_misc.c:122: error: (near initialization for kni_net_ops)

Fixes: 72a7a2b2469e ("kni: allow per-net instances")

Signed-off-by: Helin Zhang <helin.zhang@intel.com>
Acked-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

lib/librte_eal/linuxapp/kni/kni_misc.c
@@ -22,6 +22,7 @@
  *   Intel Corporation
  */
 
+#include <linux/version.h>
 #include <linux/module.h>
 #include <linux/miscdevice.h>
 #include <linux/netdevice.h>
@@ -102,9 +103,20 @@ struct kni_net {
 	struct list_head kni_list_head;
 };
 
-static __net_init int kni_init_net(struct net *net)
+static int __net_init kni_init_net(struct net *net)
 {
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32)
 	struct kni_net *knet = net_generic(net, kni_net_id);
+#else
+	struct kni_net *knet;
+	int ret;
+
+	knet = kmalloc(sizeof(struct kni_net), GFP_KERNEL);
+	if (!knet) {
+		ret = -ENOMEM;
+		return ret;
+	}
+#endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32) */
 
 	/* Clear the bit of device in use */
 	clear_bit(KNI_DEV_IN_USE_BIT_NUM, &knet->device_in_use);
@@ -112,14 +124,33 @@ static __net_init int kni_init_net(struct net *net)
 	init_rwsem(&knet->kni_list_lock);
 	INIT_LIST_HEAD(&knet->kni_list_head);
 
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32)
 	return 0;
+#else
+	ret = net_assign_generic(net, kni_net_id, knet);
+	if (ret < 0)
+		kfree(knet);
+
+	return ret;
+#endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32) */
+}
+
+static void __net_exit kni_exit_net(struct net *net)
+{
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 32)
+	struct kni_net *knet = net_generic(net, kni_net_id);
+
+	kfree(knet);
+#endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32) */
 }
 
 static struct pernet_operations kni_net_ops = {
 	.init = kni_init_net,
-	.exit = NULL,
+	.exit = kni_exit_net,
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32)
 	.id   = &kni_net_id,
 	.size = sizeof(struct kni_net),
+#endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32) */
 };
 
 static int __init
@@ -134,7 +165,11 @@ kni_init(void)
 		return -EINVAL;
 	}
 
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32)
 	rc = register_pernet_subsys(&kni_net_ops);
+#else
+	rc = register_pernet_gen_subsys(&kni_net_id, &kni_net_ops);
+#endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32) */
 	if (rc)
 		return -EPERM;
 
@@ -152,7 +187,11 @@ kni_init(void)
 	return 0;
 
 out:
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32)
 	unregister_pernet_subsys(&kni_net_ops);
+#else
+	register_pernet_gen_subsys(&kni_net_id, &kni_net_ops);
+#endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32) */
 	return rc;
 }
 
@@ -160,7 +199,11 @@ static void __exit
 kni_exit(void)
 {
 	misc_deregister(&kni_misc);
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32)
 	unregister_pernet_subsys(&kni_net_ops);
+#else
+	register_pernet_gen_subsys(&kni_net_id, &kni_net_ops);
+#endif /* LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 32) */
 	KNI_PRINT("####### DPDK kni module unloaded  #######\n");
 }
 

commit 6914c3b844195875c351d2c36685a84715f1e659

kni: fix module init/exit
Fix pernet calls when HAVE_SIMPLIFIED_PERNET_OPERATIONS is not set.

Fixes: e6734d21b4e1 ("kni: fix build with kernel 2.6.32")

Signed-off-by: Vincent Guo <guopengfei160@163.com>
Acked-by Ferruh Yigit <ferruh.yigit@intel.com>

lib/librte_eal/linuxapp/kni/kni_misc.c
@@ -194,7 +194,7 @@ out:
 #ifdef HAVE_SIMPLIFIED_PERNET_OPERATIONS
 	unregister_pernet_subsys(&kni_net_ops);
 #else
-	register_pernet_gen_subsys(&kni_net_id, &kni_net_ops);
+	unregister_pernet_gen_subsys(kni_net_id, &kni_net_ops);
 #endif
 	return rc;
 }
@@ -206,7 +206,7 @@ kni_exit(void)
 #ifdef HAVE_SIMPLIFIED_PERNET_OPERATIONS
 	unregister_pernet_subsys(&kni_net_ops);
 #else
-	register_pernet_gen_subsys(&kni_net_id, &kni_net_ops);
+	unregister_pernet_gen_subsys(kni_net_id, &kni_net_ops);
 #endif
 	KNI_PRINT("####### DPDK kni module unloaded  #######\n");
 }