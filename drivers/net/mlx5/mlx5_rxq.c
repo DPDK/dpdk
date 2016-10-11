cf0564e3cdbfc66417a8e2b1f9db66e129415c53
Nélio Laranjeiro <nelio.laranjeiro@6wind.com>
2016-10-11 16:44:50 +0200

    net/mlx5: fix Rx function selection
    mlx5_rx_queue_setup() was setting the Rx function by itself instead of
    using priv_select_rx_function() written for that purpose.
    
    Fixes: cdab90cb5c8d ("net/mlx5: add Tx/Rx burst function selection wrapper")
    
    Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>
    Acked-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>

--- drivers/net/mlx5/mlx5_rxq.c
+++ drivers/net/mlx5/mlx5_rxq.c
@@ -1262,7 +1262,7 @@ mlx5_rx_queue_setup(struct rte_eth_dev *dev, uint16_t idx, uint16_t desc,
 		      (void *)dev, (void *)rxq_ctrl);
 		(*priv->rxqs)[idx] = &rxq_ctrl->rxq;
 		/* Update receive callback. */
-		dev->rx_pkt_burst = mlx5_rx_burst;
+		priv_select_rx_function(priv);
 	}
 	priv_unlock(priv);
 	return -ret;
