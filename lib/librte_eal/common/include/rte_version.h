3549dd67152d26e46b136286ee0296a9aaa1923d
Thomas Monjalon <thomas.monjalon@6wind.com>
2016-11-14 15:16:10 +0100

    version: 17.02-rc0
    Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>

--- lib/librte_eal/common/include/rte_version.h
+++ lib/librte_eal/common/include/rte_version.h
@@ -56,12 +56,12 @@ extern "C" {
 /**
  * Major version/year number i.e. the yy in yy.mm.z
  */
-#define RTE_VER_YEAR 16
+#define RTE_VER_YEAR 17
 
 /**
  * Minor version/month number i.e. the mm in yy.mm.z
  */
-#define RTE_VER_MONTH 11
+#define RTE_VER_MONTH 2
 
 /**
  * Patch level number i.e. the z in yy.mm.z
@@ -71,14 +71,14 @@ extern "C" {
 /**
  * Extra string to be appended to version number
  */
-#define RTE_VER_SUFFIX ""
+#define RTE_VER_SUFFIX "-rc"
 
 /**
  * Patch release number
  *   0-15 = release candidates
  *   16   = release
  */
-#define RTE_VER_RELEASE 16
+#define RTE_VER_RELEASE 0
 
 /**
  * Macro to compute a version number usable for comparisons
