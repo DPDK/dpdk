02fc2241b1599c00bc561e64c05f7a7d53672a8e
Wenzhuo Lu <wenzhuo.lu@intel.com>
2016-11-23 12:22:45 -0500

    net/e1000/base: increase ULP timer
    With new hardware (I219), Ultra Low Power (ULP) exit takes significantly
    longer time. Therefore, driver must wait longer.
    
    Signed-off-by: Wenzhuo Lu <wenzhuo.lu@intel.com>

--- drivers/net/e1000/base/e1000_ich8lan.c
+++ drivers/net/e1000/base/e1000_ich8lan.c
@@ -1240,10 +1240,10 @@ s32 e1000_disable_ulp_lpt_lp(struct e1000_hw *hw, bool force)
 			E1000_WRITE_REG(hw, E1000_H2ME, mac_reg);
 		}
 
-		/* Poll up to 100msec for ME to clear ULP_CFG_DONE */
+		/* Poll up to 300msec for ME to clear ULP_CFG_DONE. */
 		while (E1000_READ_REG(hw, E1000_FWSM) &
 		       E1000_FWSM_ULP_CFG_DONE) {
-			if (i++ == 10) {
+			if (i++ == 30) {
 				ret_val = -E1000_ERR_PHY;
 				goto out;
 			}
