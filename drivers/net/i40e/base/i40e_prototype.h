842836682548f15ebceb37dd5b6176fcc45bd66a
Jingjing Wu <jingjing.wu@intel.com>
2016-12-10 19:24:49 +0800

    net/i40e/base: add broadcast promiscuous control per VLAN
    Add a new adminq function that allows driver to configure per-VLAN
    broadcast promiscuous mode, similar to how we handle unicast and
    multicast promiscuous modes.
    
    Signed-off-by: Jingjing Wu <jingjing.wu@intel.com>

--- drivers/net/i40e/base/i40e_prototype.h
+++ drivers/net/i40e/base/i40e_prototype.h
@@ -181,6 +181,9 @@ enum i40e_status_code i40e_aq_set_vsi_mc_promisc_on_vlan(struct i40e_hw *hw,
 enum i40e_status_code i40e_aq_set_vsi_uc_promisc_on_vlan(struct i40e_hw *hw,
 				u16 seid, bool enable, u16 vid,
 				struct i40e_asq_cmd_details *cmd_details);
+enum i40e_status_code i40e_aq_set_vsi_bc_promisc_on_vlan(struct i40e_hw *hw,
+				u16 seid, bool enable, u16 vid,
+				struct i40e_asq_cmd_details *cmd_details);
 enum i40e_status_code i40e_aq_set_vsi_vlan_promisc(struct i40e_hw *hw,
 				u16 seid, bool enable,
 				struct i40e_asq_cmd_details *cmd_details);
