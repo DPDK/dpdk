c0ec14757c5f762efdbc29f1f897bcb945450206
Wenzhuo Lu <wenzhuo.lu@intel.com>
2017-01-17 16:45:08 +0800

    net/i40e: set VF unicast promiscuous mode from PF
    Support enabling/disabling VF unicast promiscuous mode from
    PF.
    User can call the API on PF to enable/disable a specific
    VF's unicast promiscuous mode.
    
    Signed-off-by: Wenzhuo Lu <wenzhuo.lu@intel.com>
    Acked-by: Helin Zhang <helin.zhang@intel.com>
    Acked-by: Vincent Jardin <vincent.jardin@6wind.com>

--- drivers/net/i40e/i40e_ethdev.c
+++ drivers/net/i40e/i40e_ethdev.c
@@ -10693,3 +10693,44 @@ rte_pmd_i40e_set_tx_loopback(uint8_t port, uint8_t on)
 
 	return ret;
 }
+
+int
+rte_pmd_i40e_set_vf_unicast_promisc(uint8_t port, uint16_t vf_id, uint8_t on)
+{
+	struct rte_eth_dev *dev;
+	struct i40e_pf *pf;
+	struct i40e_vsi *vsi;
+	struct i40e_hw *hw;
+	int ret;
+
+	RTE_ETH_VALID_PORTID_OR_ERR_RET(port, -ENODEV);
+
+	dev = &rte_eth_devices[port];
+
+	if (is_i40e_pmd(dev->data->drv_name))
+		return -ENOTSUP;
+
+	pf = I40E_DEV_PRIVATE_TO_PF(dev->data->dev_private);
+
+	if (vf_id >= pf->vf_num || !pf->vfs) {
+		PMD_DRV_LOG(ERR, "Invalid argument.");
+		return -EINVAL;
+	}
+
+	vsi = pf->vfs[vf_id].vsi;
+	if (!vsi) {
+		PMD_DRV_LOG(ERR, "Invalid VSI.");
+		return -EINVAL;
+	}
+
+	hw = I40E_VSI_TO_HW(vsi);
+
+	ret = i40e_aq_set_vsi_unicast_promiscuous(hw, vsi->seid,
+						  on, NULL, true);
+	if (ret != I40E_SUCCESS) {
+		ret = -ENOTSUP;
+		PMD_DRV_LOG(ERR, "Failed to set unicast promiscuous mode");
+	}
+
+	return ret;
+}
