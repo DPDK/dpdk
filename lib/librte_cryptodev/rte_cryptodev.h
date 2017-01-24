547017d80af4f84a4dcf4e7d96ef60647b073aa6
Fan Zhang <roy.fan.zhang@intel.com>
2017-01-24 16:23:51 +0000

    cryptodev: add scheduler PMD name and type
    This patch adds the cryptodev scheduler PMD name and type identifier to
    librte_cryptodev.
    
    Signed-off-by: Fan Zhang <roy.fan.zhang@intel.com>
    Acked-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- lib/librte_cryptodev/rte_cryptodev.h
+++ lib/librte_cryptodev/rte_cryptodev.h
@@ -68,6 +68,8 @@ extern "C" {
 /**< KASUMI PMD device name */
 #define CRYPTODEV_NAME_ARMV8_PMD	crypto_armv8
 /**< ARMv8 Crypto PMD device name */
+#define CRYPTODEV_NAME_SCHEDULER_PMD	crypto_scheduler
+/**< Scheduler Crypto PMD device name */
 
 /** Crypto device type */
 enum rte_cryptodev_type {
@@ -80,6 +82,7 @@ enum rte_cryptodev_type {
 	RTE_CRYPTODEV_ZUC_PMD,		/**< ZUC PMD */
 	RTE_CRYPTODEV_OPENSSL_PMD,    /**<  OpenSSL PMD */
 	RTE_CRYPTODEV_ARMV8_PMD,	/**< ARMv8 crypto PMD */
+	RTE_CRYPTODEV_SCHEDULER_PMD,	/**< Crypto Scheduler PMD */
 };
 
 extern const char **rte_cyptodev_names;
