9dad5b9e892e5f7770a7b5992a523f8c6053ae79
Wenzhuo Lu <wenzhuo.lu@intel.com>
2017-03-01 14:04:51 +0800

    net/ixgbe/base: fix build error
    Fix ICC build error by removing the EWARN third parameter.
    
    Build error:
    .../drivers/net/ixgbe/base/ixgbe_phy.c(1543):
    	error #268: the format string ends before this argument
    			EWARN(hw, "WARNING: Intel (R) Network "
    			^
    
    .../drivers/net/ixgbe/base/ixgbe_phy.c(1805):
    	error #268: the format string ends before this argument
    			EWARN(hw, "WARNING: Intel (R) Network "
    			^
    
    Fixes: aa4fc14d2cee ("ixgbe: update base driver")
    Fixes: b94a06c1b451 ("ixgbe/base: support qsfp and lco")
    Cc: stable@dpdk.org
    
    Signed-off-by: Wenzhuo Lu <wenzhuo.lu@intel.com>

--- drivers/net/ixgbe/base/ixgbe_phy.c
+++ drivers/net/ixgbe/base/ixgbe_phy.c
@@ -1540,16 +1540,10 @@ s32 ixgbe_identify_sfp_module_generic(struct ixgbe_hw *hw)
 				status = IXGBE_SUCCESS;
 			} else {
 				if (hw->allow_unsupported_sfp == true) {
-					EWARN(hw, "WARNING: Intel (R) Network "
-					      "Connections are quality tested "
-					      "using Intel (R) Ethernet Optics."
-					      " Using untested modules is not "
-					      "supported and may cause unstable"
-					      " operation or damage to the "
-					      "module or the adapter. Intel "
-					      "Corporation is not responsible "
-					      "for any harm caused by using "
-					      "untested modules.\n", status);
+					EWARN(hw,
+						"WARNING: Intel (R) Network Connections are quality tested using Intel (R) Ethernet Optics. "
+						"Using untested modules is not supported and may cause unstable operation or damage to the module or the adapter. "
+						"Intel Corporation is not responsible for any harm caused by using untested modules.\n");
 					status = IXGBE_SUCCESS;
 				} else {
 					DEBUGOUT("SFP+ module not supported\n");
@@ -1802,16 +1796,10 @@ s32 ixgbe_identify_qsfp_module_generic(struct ixgbe_hw *hw)
 				status = IXGBE_SUCCESS;
 			} else {
 				if (hw->allow_unsupported_sfp == true) {
-					EWARN(hw, "WARNING: Intel (R) Network "
-					      "Connections are quality tested "
-					      "using Intel (R) Ethernet Optics."
-					      " Using untested modules is not "
-					      "supported and may cause unstable"
-					      " operation or damage to the "
-					      "module or the adapter. Intel "
-					      "Corporation is not responsible "
-					      "for any harm caused by using "
-					      "untested modules.\n", status);
+					EWARN(hw,
+						"WARNING: Intel (R) Network Connections are quality tested using Intel (R) Ethernet Optics. "
+						"Using untested modules is not supported and may cause unstable operation or damage to the module or the adapter. "
+						"Intel Corporation is not responsible for any harm caused by using untested modules.\n");
 					status = IXGBE_SUCCESS;
 				} else {
 					DEBUGOUT("QSFP module not supported\n");
@@ -1835,7 +1823,6 @@ err_read_i2c_eeprom:
 
 	return IXGBE_ERR_SFP_NOT_PRESENT;
 }
-
 
 /**
  *  ixgbe_get_sfp_init_sequence_offsets - Provides offset of PHY init sequence
