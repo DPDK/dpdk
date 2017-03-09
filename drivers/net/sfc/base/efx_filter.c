daa007afd04b40f903b02035b73c945b4a25efff
Mark Spender <mspender@solarflare.com>
2017-03-09 15:26:23 +0000

    net/sfc/base: split local MAC I/G back into separate flags
    The flag EFX_FILTER_MATCH_LOC_MAC_IG to represent filtering on the
    individual/group bit of the MAC address (with the two cases being
    distinguished by the MAC address in the filter specification) was
    introduced to mirror the Linux driver filtering code, but the
    implementations are different enough anyway that it isn't of much
    value.
    
    Having separate flags for unknown unicast and multicast simplifies
    the code and allows the set of flags to match those used by MCDI.
    
    It will also makes it easier to report whether these filters are
    supported.
    
    In the MCDI definitions, the unknown multicast and unicast flags
    have the values 0x40000000 and 0x80000000 respectively, and so using
    the same values for simplicity requires 32 bits in the filter
    specification to store the flags. This means the structure is now
    a little bigger than 64 bytes, but filters are not often used on
    critical paths so this shouldn't have much impact - on Linux they
    are also bigger than they used to be.
    
    Signed-off-by: Mark Spender <mspender@solarflare.com>
    Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>

--- drivers/net/sfc/base/efx_filter.c
+++ drivers/net/sfc/base/efx_filter.c
@@ -149,9 +149,6 @@ efx_filter_init(
 	const efx_filter_ops_t *efop;
 	efx_rc_t rc;
 
-	/* Check that efx_filter_spec_t is 64 bytes. */
-	EFX_STATIC_ASSERT(sizeof (efx_filter_spec_t) == 64);
-
 	EFSYS_ASSERT3U(enp->en_magic, ==, EFX_NIC_MAGIC);
 	EFSYS_ASSERT3U(enp->en_mod_flags, &, EFX_MOD_PROBE);
 	EFSYS_ASSERT(!(enp->en_mod_flags & EFX_MOD_FILTER));
@@ -387,7 +384,7 @@ efx_filter_spec_set_uc_def(
 {
 	EFSYS_ASSERT3P(spec, !=, NULL);
 
-	spec->efs_match_flags |= EFX_FILTER_MATCH_LOC_MAC_IG;
+	spec->efs_match_flags |= EFX_FILTER_MATCH_UNKNOWN_UCAST_DST;
 	return (0);
 }
 
@@ -400,8 +397,7 @@ efx_filter_spec_set_mc_def(
 {
 	EFSYS_ASSERT3P(spec, !=, NULL);
 
-	spec->efs_match_flags |= EFX_FILTER_MATCH_LOC_MAC_IG;
-	spec->efs_loc_mac[0] = 1;
+	spec->efs_match_flags |= EFX_FILTER_MATCH_UNKNOWN_MCAST_DST;
 	return (0);
 }
 
