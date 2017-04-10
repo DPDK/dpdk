7b46f62b8159dc5444cdcd48a46c6656b221510c
Fan Zhang <roy.fan.zhang@intel.com>
2017-04-10 15:59:34 +0100

    crypto/scheduler: fix capability update
    Fixes: 31439ee72b2c ("crypto/scheduler: add API implementations")
    Cc: stable@dpdk.org
    
    Signed-off-by: Fan Zhang <roy.fan.zhang@intel.com>

--- drivers/crypto/scheduler/rte_cryptodev_scheduler.c
+++ drivers/crypto/scheduler/rte_cryptodev_scheduler.c
@@ -64,7 +64,7 @@ sync_caps(struct rte_cryptodev_capabilities *caps,
 
 		for (j = 0; j < nb_slave_caps; j++) {
 			const struct rte_cryptodev_capabilities *s_cap =
-					&slave_caps[i];
+					&slave_caps[j];
 
 			if (s_cap->op != cap->op || s_cap->sym.xform_type !=
 					cap->sym.xform_type)
