d100f83dbfce6d1842066bdb9a6b734220d1a525
Hemant Agrawal <hemant.agrawal@nxp.com>
2017-04-11 19:07:07 +0530

    mk: add CRC support to DPAA2 machine type
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>
    Acked-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>

--- mk/machine/dpaa2/rte.vars.mk
+++ mk/machine/dpaa2/rte.vars.mk
@@ -1,6 +1,7 @@
 #   BSD LICENSE
 #
-#   Copyright(c) 2016 Freescale Semiconductor, Inc. All rights reserved.
+#   Copyright (c) 2016 Freescale Semiconductor, Inc. All rights reserved.
+#   Copyright (c) 2016 NXP. All rights reserved.
 #
 #   Redistribution and use in source and binary forms, with or without
 #   modification, are permitted provided that the following conditions
@@ -53,7 +54,7 @@
 # CPU_CFLAGS =
 # CPU_LDFLAGS =
 # CPU_ASFLAGS =
-MACHINE_CFLAGS += -march=armv8-a
+MACHINE_CFLAGS += -march=armv8-a+crc
 
 ifdef CONFIG_RTE_ARCH_ARM_TUNE
 MACHINE_CFLAGS += -mcpu=$(CONFIG_RTE_ARCH_ARM_TUNE:"%"=%)
