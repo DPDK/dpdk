da51d4d956516c4968261dbb4ce49c1434f18b71
Ferruh Yigit <ferruh.yigit@intel.com>
2017-04-13 14:34:37 +0100

    kni: fix ethtool support
    Use correct config option, without CONFIG prefix.
    
    Fixes: b2b0f85182ef ("kni: add build option for ethtool support")
    Cc: stable@dpdk.org
    
    Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- lib/librte_eal/linuxapp/kni/kni_misc.c
+++ lib/librte_eal/linuxapp/kni/kni_misc.c
@@ -197,7 +197,7 @@ kni_dev_remove(struct kni_dev *dev)
 	if (!dev)
 		return -ENODEV;
 
-#ifdef CONFIG_RTE_KNI_KMOD_ETHTOOL
+#ifdef RTE_KNI_KMOD_ETHTOOL
 	if (dev->pci_dev) {
 		if (pci_match_id(ixgbe_pci_tbl, dev->pci_dev))
 			ixgbe_kni_remove(dev->pci_dev);
@@ -319,6 +319,11 @@ kni_ioctl_create(struct net *net, uint32_t ioctl_num,
 	struct rte_kni_device_info dev_info;
 	struct net_device *net_dev = NULL;
 	struct kni_dev *kni, *dev, *n;
+#ifdef RTE_KNI_KMOD_ETHTOOL
+	struct pci_dev *found_pci = NULL;
+	struct net_device *lad_dev = NULL;
+	struct pci_dev *pci = NULL;
+#endif
 
 	pr_info("Creating kni...\n");
 	/* Check the buffer size, to avoid warning */
@@ -408,12 +413,7 @@ kni_ioctl_create(struct net *net, uint32_t ioctl_num,
 					dev_info.function,
 					dev_info.vendor_id,
 					dev_info.device_id);
-
-#ifdef CONFIG_RTE_KNI_KMOD_ETHTOOL
-	struct pci_dev *found_pci = NULL;
-	struct net_device *lad_dev = NULL;
-	struct pci_dev *pci = NULL;
-
+#ifdef RTE_KNI_KMOD_ETHTOOL
 	pci = pci_get_device(dev_info.vendor_id, dev_info.device_id, NULL);
 
 	/* Support Ethtool */
