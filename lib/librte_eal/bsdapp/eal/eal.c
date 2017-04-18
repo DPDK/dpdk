ea98a7afb47c25959735d31518f4944dde6552e5
Olivier Matz <olivier.matz@6wind.com>
2017-04-18 16:22:20 +0200

    log: fix default level
    The initialization of the default log level (from configuration) was
    removed by mistake in a previous commit. The global log level was
    wrongly set to debug when no --log-level argument was passed. Restore
    this initialization.
    
    Before:
      $ ./build/app/test
      RTE>>dump_log_types
      global log level is debug
      ...
    
    After:
      $ ./build/app/test
      RTE>>dump_log_types
      global log level is info
      ...
    
    Fixes: 845afe51e428 ("eal: change specific log levels at startup")
    
    Reported-by: Ferruh Yigit <ferruh.yigit@intel.com>
    Signed-off-by: Olivier Matz <olivier.matz@6wind.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- lib/librte_eal/bsdapp/eal/eal.c
+++ lib/librte_eal/bsdapp/eal/eal.c
@@ -324,6 +324,7 @@ eal_log_level_parse(int argc, char **argv)
 	optreset = 1;
 
 	eal_reset_internal_config(&internal_config);
+	rte_log_set_global_level(internal_config.log_level);
 
 	while ((opt = getopt_long(argc, argvopt, eal_short_options,
 				  eal_long_options, &option_index)) != EOF) {
