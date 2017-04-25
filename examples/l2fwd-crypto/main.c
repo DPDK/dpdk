d7acf6ba43b43dde40d13681738c6940049555cb
Pablo de Lara <pablo.de.lara.guarch@intel.com>
2017-04-25 16:32:36 +0100

    examples/l2fwd-crypto: fix packets array index
    Fixes: c0f87eb5252b ("cryptodev: change burst API to be crypto op oriented")
    Cc: stable@dpdk.org
    
    Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- examples/l2fwd-crypto/main.c
+++ examples/l2fwd-crypto/main.c
@@ -788,7 +788,7 @@ l2fwd_main_loop(struct l2fwd_crypto_options *options)
 						ops_burst, nb_rx) !=
 								nb_rx) {
 					for (j = 0; j < nb_rx; j++)
-						rte_pktmbuf_free(pkts_burst[i]);
+						rte_pktmbuf_free(pkts_burst[j]);
 
 					nb_rx = 0;
 				}
