b79afd0b7e77cbc09b45ee9bb0cd4f6f5542bd20
Ashwin Sekhar T K <ashwin.sekhar@caviumnetworks.com>
2017-04-27 23:15:48 -0700

    sched: add NEON optimizations
    * Removed setting CONFIG_RTE_SCHED_VECTOR=n from armv8a config
      so that the setting from common_base is taken as the default
      setting for armv8a
    * Verified the changes with sched_autotest unit test case
    
    Signed-off-by: Ashwin Sekhar T K <ashwin.sekhar@caviumnetworks.com>
    Acked-by: Jianbo Liu <jianbo.liu@linaro.org>

--- lib/librte_sched/rte_sched.c
+++ lib/librte_sched/rte_sched.c
@@ -58,6 +58,8 @@
 
 #if defined(__SSE4__)
 #define SCHED_VECTOR_SSE4
+#elif defined(RTE_MACHINE_CPUFLAG_NEON)
+#define SCHED_VECTOR_NEON
 #endif
 
 #endif
@@ -1730,6 +1732,26 @@ grinder_pipe_exists(struct rte_sched_port *port, uint32_t base_pipe)
 		return 0;
 
 	return 1;
+}
+
+#elif defined(SCHED_VECTOR_NEON)
+
+static inline int
+grinder_pipe_exists(struct rte_sched_port *port, uint32_t base_pipe)
+{
+	uint32x4_t index, pipes;
+	uint32_t *pos = (uint32_t *)port->grinder_base_bmp_pos;
+
+	index = vmovq_n_u32(base_pipe);
+	pipes = vld1q_u32(pos);
+	if (!vminvq_u32(veorq_u32(pipes, index)))
+		return 1;
+
+	pipes = vld1q_u32(pos + 4);
+	if (!vminvq_u32(veorq_u32(pipes, index)))
+		return 1;
+
+	return 0;
 }
 
 #else
