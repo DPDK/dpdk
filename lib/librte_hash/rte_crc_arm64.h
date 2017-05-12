fa50d3b27a090eec975c966c3fc9d1f6df2adfa2
Ashwin Sekhar T K <ashwin.sekhar@caviumnetworks.com>
2017-05-11 22:45:43 -0700

    hash: compile ARMv8 CRC32 support conditionally
    Compile the armv8a CRC32 support only if the machine
    has the CRC extensions i.e if RTE_MACHINE_CPUFLAG_CRC32
    is defined.
    
    Removed the .arch assembly directives as these are no
    more necessary.
    
    Signed-off-by: Ashwin Sekhar T K <ashwin.sekhar@caviumnetworks.com>
    Reviewed-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>
    Acked-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- lib/librte_hash/rte_crc_arm64.h
+++ lib/librte_hash/rte_crc_arm64.h
@@ -52,7 +52,6 @@ extern "C" {
 static inline uint32_t
 crc32c_arm64_u8(uint8_t data, uint32_t init_val)
 {
-	asm(".arch armv8-a+crc");
 	__asm__ volatile(
 			"crc32cb %w[crc], %w[crc], %w[value]"
 			: [crc] "+r" (init_val)
@@ -63,7 +62,6 @@ crc32c_arm64_u8(uint8_t data, uint32_t init_val)
 static inline uint32_t
 crc32c_arm64_u16(uint16_t data, uint32_t init_val)
 {
-	asm(".arch armv8-a+crc");
 	__asm__ volatile(
 			"crc32ch %w[crc], %w[crc], %w[value]"
 			: [crc] "+r" (init_val)
@@ -74,7 +72,6 @@ crc32c_arm64_u16(uint16_t data, uint32_t init_val)
 static inline uint32_t
 crc32c_arm64_u32(uint32_t data, uint32_t init_val)
 {
-	asm(".arch armv8-a+crc");
 	__asm__ volatile(
 			"crc32cw %w[crc], %w[crc], %w[value]"
 			: [crc] "+r" (init_val)
@@ -85,7 +82,6 @@ crc32c_arm64_u32(uint32_t data, uint32_t init_val)
 static inline uint32_t
 crc32c_arm64_u64(uint64_t data, uint32_t init_val)
 {
-	asm(".arch armv8-a+crc");
 	__asm__ volatile(
 			"crc32cx %w[crc], %w[crc], %x[value]"
 			: [crc] "+r" (init_val)
