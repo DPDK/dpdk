13916d5e7c5230adc069e4c70a2554fc3491d5c3
Pablo de Lara <pablo.de.lara.guarch@intel.com>
2017-06-21 06:07:33 +0100

    test/bonding: remove socket id check
    When creating a virtual pmd to test link bonding,
    the socket id was checked, if it was in the range
    of available sockets.
    This check is unnecessary, as the socket specified
    might not have memory anyway, so it will fail
    at memory allocation.
    
    Therefore, the best solution is to remove this check.
    
    Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>
    Acked-by: Declan Doherty <declan.doherty@intel.com>

--- test/test/virtual_pmd.c
+++ test/test/virtual_pmd.c
@@ -511,20 +511,6 @@ virtual_ethdev_get_mbufs_from_tx_queue(uint8_t port_id,
 		burst_length, NULL);
 }
 
-static uint8_t
-get_number_of_sockets(void)
-{
-	int sockets = 0;
-	int i;
-	const struct rte_memseg *ms = rte_eal_get_physmem_layout();
-
-	for (i = 0; i < RTE_MAX_MEMSEG && ms[i].addr != NULL; i++) {
-		if (sockets < ms[i].socket_id)
-			sockets = ms[i].socket_id;
-	}
-	/* Number of sockets = maximum socket_id + 1 */
-	return ++sockets;
-}
 
 int
 virtual_ethdev_create(const char *name, struct ether_addr *mac_addr,
@@ -541,9 +527,6 @@ virtual_ethdev_create(const char *name, struct ether_addr *mac_addr,
 	/* now do all data allocation - for eth_dev structure, dummy pci driver
 	 * and internal (dev_private) data
 	 */
-
-	if (socket_id >= get_number_of_sockets())
-		goto err;
 
 	pci_dev = rte_zmalloc_socket(name, sizeof(*pci_dev), 0, socket_id);
 	if (pci_dev == NULL)
