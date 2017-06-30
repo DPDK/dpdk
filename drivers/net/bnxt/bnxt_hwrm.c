28eef5e0a8fa592f8bdf3a3ea0a6a00fecee6acf
Ajit Khaparde <ajit.khaparde@broadcom.com>
2017-06-30 09:20:16 -0500

    net/bnxt: enable default VNIC allocation
    Previously, no VNICs were allocated as the default VNIC,
    even if they were configured as one.  This patch fixes that error.
    
    Fixes: db678d5c2b54 ("net/bnxt: add HWRM VNIC configure")
    Cc: stable@dpdk.org
    
    Signed-off-by: Stephen Hurd <stephen.hurd@broadcom.com>
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.c
+++ drivers/net/bnxt/bnxt_hwrm.c
@@ -970,6 +970,8 @@ int bnxt_hwrm_vnic_alloc(struct bnxt *bp, struct bnxt_vnic_info *vnic)
 				ETHER_CRC_LEN + VLAN_TAG_SIZE;
 	HWRM_PREP(req, VNIC_ALLOC, -1, resp);
 
+	if (vnic->func_default)
+		req.flags = HWRM_VNIC_ALLOC_INPUT_FLAGS_DEFAULT;
 	rc = bnxt_hwrm_send_message(bp, &req, sizeof(req));
 
 	HWRM_CHECK_RESULT;
