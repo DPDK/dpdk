DPDK/dpdk

commit 15b55dd751207bb3df4196baff49bc6c33b6d6dc

app/crypto-perf: return crypto ops to mempool in bulk
Use rte_mempool_put_bulk for both latency and throughput tests instead
of rte_crypto_op_free to improve application performance.

Signed-off-by: Sergio Gonzalez Monroy <sergio.gonzalez.monroy@intel.com>
Acked-by: Fiona Trahe <fiona.trahe@intel.com>

app/test-crypto-perf/cperf_test_latency.c
@@ -393,8 +393,10 @@ cperf_latency_test_runner(void *arg)
 			tsc_end = rte_rdtsc_precise();
 
 			/* Free memory for not enqueued operations */
-			for (i = ops_enqd; i < burst_size; i++)
-				rte_crypto_op_free(ops[i]);
+			if (ops_enqd != burst_size)
+				rte_mempool_put_bulk(ctx->crypto_op_pool,
+						(void **)&ops_processed[ops_enqd],
+						burst_size - ops_enqd);
 
 			for (i = 0; i < ops_enqd; i++) {
 				ctx->res[tsc_idx].tsc_start = tsc_start;
@@ -414,9 +416,9 @@ cperf_latency_test_runner(void *arg)
 							(ops_processed[i]->opaque_data);
 					pres->status = ops_processed[i]->status;
 					pres->tsc_end = tsc_end;
-
-					rte_crypto_op_free(ops_processed[i]);
 				}
+				rte_mempool_put_bulk(ctx->crypto_op_pool,
+						(void **)ops_processed, ops_deqd);
 
 				deqd_tot += ops_deqd;
 				deqd_max = max(ops_deqd, deqd_max);
@@ -450,9 +452,9 @@ cperf_latency_test_runner(void *arg)
 							(ops_processed[i]->opaque_data);
 					pres->status = ops_processed[i]->status;
 					pres->tsc_end = tsc_end;
-
-					rte_crypto_op_free(ops_processed[i]);
 				}
+				rte_mempool_put_bulk(ctx->crypto_op_pool,
+						(void **)ops_processed, ops_deqd);
 
 				deqd_tot += ops_deqd;
 				deqd_max = max(ops_deqd, deqd_max);

app/test-crypto-perf/cperf_test_throughput.c
@@ -396,8 +396,8 @@ cperf_throughput_test_runner(void *test_ctx)
 				 * the crypto operation will change the data and cause
 				 * failures.
 				 */
-				for (i = 0; i < ops_deqd; i++)
-					rte_crypto_op_free(ops_processed[i]);
+				rte_mempool_put_bulk(ctx->crypto_op_pool,
+						(void **)ops_processed, ops_deqd);
 
 				ops_deqd_total += ops_deqd;
 			} else {
@@ -426,8 +426,8 @@ cperf_throughput_test_runner(void *test_ctx)
 			if (ops_deqd == 0)
 				ops_deqd_failed++;
 			else {
-				for (i = 0; i < ops_deqd; i++)
-					rte_crypto_op_free(ops_processed[i]);
+				rte_mempool_put_bulk(ctx->crypto_op_pool,
+						(void **)ops_processed, ops_deqd);
 
 				ops_deqd_total += ops_deqd;
 			}

commit 61bec3623ceba3fda30a1dd74adc67cdb6dc9466

app/crypto-perf: fix operation free
In crypto latency test, when some crypto operations cannot be
enqueued, they are returned to its mempool.
However, instead of freeing the operations in the array passed
to the enqueue function, the app was freeing the operations
in the array passed to the dequeue function, leading to incorrect
results.

Fixes: 15b55dd75120 ("app/crypto-perf: return crypto ops to mempool in bulk")

Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

app/test-crypto-perf/cperf_test_latency.c
@@ -433,7 +433,7 @@ cperf_latency_test_runner(void *arg)
 			/* Free memory for not enqueued operations */
 			if (ops_enqd != burst_size)
 				rte_mempool_put_bulk(ctx->crypto_op_pool,
-						(void **)&ops_processed[ops_enqd],
+						(void **)&ops[ops_enqd],
 						burst_size - ops_enqd);
 
 			for (i = 0; i < ops_enqd; i++) {