bc48589e47be0311fe1ba56e262ae68fe599bb7b
Pavan Bhagavatula <pbhagavatula@caviumnetworks.com>
2017-09-21 17:20:20 +0530

    eal: move bitmap from sched library
    The librte_sched uses rte_bitmap to manage large arrays of bits in an
    optimized method so, moving it to eal/common would allow other libraries
    and applications to use it.
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@caviumnetworks.com>
    Acked-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>

--- lib/librte_sched/rte_sched.c
+++ lib/librte_sched/rte_sched.c
@@ -42,9 +42,9 @@
 #include <rte_prefetch.h>
 #include <rte_branch_prediction.h>
 #include <rte_mbuf.h>
+#include <rte_bitmap.h>
 
 #include "rte_sched.h"
-#include "rte_bitmap.h"
 #include "rte_sched_common.h"
 #include "rte_approx.h"
 #include "rte_reciprocal.h"
