044bf6e93c26df53c40711eaaf2482409efdbb76
Ajit Khaparde <ajit.khaparde@broadcom.com>
2017-09-28 16:43:23 -0500

    net/bnxt: use 64-bits of address for VLAN table
    We are wrongly using just 16 bits of address from rte_mem_virt2phy
    while filling the vlan table address instead of 64-bytes.
    Most likely a copy-paste error.
    
    Fixes: 36735a932ca7 ("net/bnxt: support set VF QOS and MAC anti spoof")
    Cc: stable@dpdk.org
    
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.c
+++ drivers/net/bnxt/bnxt_hwrm.c
@@ -276,7 +276,7 @@ int bnxt_hwrm_cfa_l2_set_rx_mask(struct bnxt *bp,
 	if (vlan_table) {
 		if (!(mask & HWRM_CFA_L2_SET_RX_MASK_INPUT_MASK_VLAN_NONVLAN))
 			mask |= HWRM_CFA_L2_SET_RX_MASK_INPUT_MASK_VLANONLY;
-		req.vlan_tag_tbl_addr = rte_cpu_to_le_16(
+		req.vlan_tag_tbl_addr = rte_cpu_to_le_64(
 			 rte_mem_virt2phy(vlan_table));
 		req.num_vlan_tags = rte_cpu_to_le_32((uint32_t)vlan_count);
 	}
