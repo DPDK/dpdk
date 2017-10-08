8700239f7767c48cf5db3379c58855e6372c244c
Santosh Shukla <santosh.shukla@caviumnetworks.com>
2017-10-08 18:10:03 +0530

    mempool/octeontx: add build and log infrastructure
    Signed-off-by: Santosh Shukla <santosh.shukla@caviumnetworks.com>
    Signed-off-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>

--- drivers/mempool/octeontx/octeontx_fpavf.h
+++ drivers/mempool/octeontx/octeontx_fpavf.h
@@ -33,6 +33,25 @@
 #ifndef	__OCTEONTX_FPAVF_H__
 #define	__OCTEONTX_FPAVF_H__
 
+#include <rte_debug.h>
+
+#ifdef RTE_LIBRTE_OCTEONTX_MEMPOOL_DEBUG
+#define fpavf_log_info(fmt, args...) \
+	RTE_LOG(INFO, PMD, "%s() line %u: " fmt "\n", \
+		__func__, __LINE__, ## args)
+#define fpavf_log_dbg(fmt, args...) \
+	RTE_LOG(DEBUG, PMD, "%s() line %u: " fmt "\n", \
+		__func__, __LINE__, ## args)
+#else
+#define fpavf_log_info(fmt, args...)
+#define fpavf_log_dbg(fmt, args...)
+#endif
+
+#define fpavf_func_trace fpavf_log_dbg
+#define fpavf_log_err(fmt, args...) \
+	RTE_LOG(ERR, PMD, "%s() line %u: " fmt "\n", \
+		__func__, __LINE__, ## args)
+
 /* fpa pool Vendor ID and Device ID */
 #define PCI_VENDOR_ID_CAVIUM		0x177D
 #define PCI_DEVICE_ID_OCTEONTX_FPA_VF	0xA053
