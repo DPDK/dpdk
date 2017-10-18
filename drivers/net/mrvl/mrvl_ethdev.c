00fabc423db0ee5ed47da6e4c7d3c0c1e1a738b4
Tomasz Duszynski <tdu@semihalf.com>
2017-10-18 10:59:45 +0200

    net/mrvl: update copyright holders
    Add Marvell International Ltd. to the copyright holders.
    
    Fixes: 0ddc9b815b11 ("net/mrvl: add net PMD skeleton")
    Fixes: 1a286a1139f2 ("doc: add mrvl NIC guide")
    
    Signed-off-by: Tomasz Duszynski <tdu@semihalf.com>

--- drivers/net/mrvl/mrvl_ethdev.c
+++ drivers/net/mrvl/mrvl_ethdev.c
@@ -1,7 +1,9 @@
 /*-
  *   BSD LICENSE
  *
- *   Copyright(c) 2017 Semihalf. All rights reserved.
+ *   Copyright(c) 2017 Marvell International Ltd.
+ *   Copyright(c) 2017 Semihalf.
+ *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
