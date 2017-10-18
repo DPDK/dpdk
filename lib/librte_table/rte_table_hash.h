4b08cd8927d7997792fd70911567b3393a5e0e9f
Cristian Dumitrescu <cristian.dumitrescu@intel.com>
2017-10-18 16:03:52 +0100

    table: update copyright
    Removed incorrect white spaces and updated year in copyrigh headers.
    
    Signed-off-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>

--- lib/librte_table/rte_table_hash.h
+++ lib/librte_table/rte_table_hash.h
@@ -1,7 +1,7 @@
 /*-
  *   BSD LICENSE
  *
- *   Copyright(c) 2010-2016 Intel Corporation. All rights reserved.
+ *   Copyright(c) 2010-2017 Intel Corporation. All rights reserved.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
