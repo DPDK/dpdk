DPDK/dpdk

commit 279b581c897d4bf01f2b35c7327e767400c3ee80

vfio: expose functions
The following symbols are used by vfio implementations within the PCI bus.
They need to be publicly available for the PCI bus to be outside the
EAL.

  + vfio_enable;
  + vfio_is_enabled;
  + vfio_noiommu_is_enabled;
  + vfio_release_device;
  + vfio_setup_device;

Signed-off-by: Gaetan Rivet <gaetan.rivet@6wind.com>

lib/librte_eal/bsdapp/eal/eal.c
@@ -739,3 +739,44 @@ rte_eal_vfio_intr_mode(void)
 {
 	return RTE_INTR_MODE_NONE;
 }
+
+/* dummy forward declaration. */
+struct vfio_device_info;
+
+/* dummy prototypes. */
+int vfio_setup_device(const char *sysfs_base, const char *dev_addr,
+		int *vfio_dev_fd, struct vfio_device_info *device_info);
+int vfio_release_device(const char *sysfs_base, const char *dev_addr, int fd);
+int vfio_enable(const char *modname);
+int vfio_is_enabled(const char *modname);
+int vfio_noiommu_is_enabled(void);
+
+int vfio_setup_device(__rte_unused const char *sysfs_base,
+		      __rte_unused const char *dev_addr,
+		      __rte_unused int *vfio_dev_fd,
+		      __rte_unused struct vfio_device_info *device_info)
+{
+	return -1;
+}
+
+int vfio_release_device(__rte_unused const char *sysfs_base,
+			__rte_unused const char *dev_addr,
+			__rte_unused int fd)
+{
+	return -1;
+}
+
+int vfio_enable(__rte_unused const char *modname)
+{
+	return -1;
+}
+
+int vfio_is_enabled(__rte_unused const char *modname)
+{
+	return 0;
+}
+
+int vfio_noiommu_is_enabled(void)
+{
+	return 0;
+}

lib/librte_eal/common/include/rte_vfio.h
@@ -1,0 +1,139 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright(c) 2017 6WIND S.A. All rights reserved.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of 6WIND nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _RTE_VFIO_H_
+#define _RTE_VFIO_H_
+
+#include <linux/vfio.h>
+
+#define VFIO_DIR "/dev/vfio"
+#define VFIO_CONTAINER_PATH "/dev/vfio/vfio"
+#define VFIO_GROUP_FMT "/dev/vfio/%u"
+#define VFIO_NOIOMMU_GROUP_FMT "/dev/vfio/noiommu-%u"
+#define VFIO_GET_REGION_ADDR(x) ((uint64_t) x << 40ULL)
+#define VFIO_GET_REGION_IDX(x) (x >> 40)
+#define VFIO_NOIOMMU_MODE      \
+	"/sys/module/vfio/parameters/enable_unsafe_noiommu_mode"
+
+/**
+ * Setup vfio_cfg for the device identified by its address.
+ * It discovers the configured I/O MMU groups or sets a new one for the device.
+ * If a new groups is assigned, the DMA mapping is performed.
+ *
+ * This function is only relevant to linux and will return
+ * an error on BSD.
+ *
+ * @param sysfs_base
+ *   sysfs path prefix.
+ *
+ * @param dev_addr
+ *   device location.
+ *
+ * @param vfio_dev_fd
+ *   VFIO fd.
+ *
+ * @param device_info
+ *   Device information.
+ *
+ * @return
+ *   0 on success.
+ *   <0 on failure.
+ *   >1 if the device cannot be managed this way.
+ */
+int vfio_setup_device(const char *sysfs_base, const char *dev_addr,
+		int *vfio_dev_fd, struct vfio_device_info *device_info);
+
+/**
+ * Release a device mapped to a VFIO-managed I/O MMU group.
+ *
+ * This function is only relevant to linux and will return
+ * an error on BSD.
+ *
+ * @param sysfs_base
+ *   sysfs path prefix.
+ *
+ * @param dev_addr
+ *   device location.
+ *
+ * @param fd
+ *   VFIO fd.
+ *
+ * @return
+ *   0 on success.
+ *   <0 on failure.
+ */
+int vfio_release_device(const char *sysfs_base, const char *dev_addr, int fd);
+
+/**
+ * Enable a VFIO-related kmod.
+ *
+ * This function is only relevant to linux and will return
+ * an error on BSD.
+ *
+ * @param modname
+ *   kernel module name.
+ *
+ * @return
+ *   0 on success.
+ *   <0 on failure.
+ */
+int vfio_enable(const char *modname);
+
+/**
+ * Check whether a VFIO-related kmod is enabled.
+ *
+ * This function is only relevant to linux and will return
+ * an error on BSD.
+ *
+ * @param modname
+ *   kernel module name.
+ *
+ * @return
+ *   !0 if true.
+ *   0 otherwise.
+ */
+int vfio_is_enabled(const char *modname);
+
+/**
+ * Whether VFIO NOIOMMU mode is enabled.
+ *
+ * This function is only relevant to linux and will return
+ * an error on BSD.
+ *
+ * @return
+ *   !0 if true.
+ *   0 otherwise.
+ */
+int vfio_noiommu_is_enabled(void);
+
+#endif /* _RTE_VFIO_H_ */

lib/librte_eal/linuxapp/eal/eal.c
@@ -77,6 +77,7 @@
 #include <rte_version.h>
 #include <rte_atomic.h>
 #include <malloc_heap.h>
+#include <rte_vfio.h>
 
 #include "eal_private.h"
 #include "eal_thread.h"

lib/librte_eal/linuxapp/eal/eal_vfio.c
@@ -39,6 +39,7 @@
 #include <rte_log.h>
 #include <rte_memory.h>
 #include <rte_eal_memconfig.h>
+#include <rte_vfio.h>
 
 #include "eal_filesystem.h"
 #include "eal_vfio.h"

lib/librte_eal/rte_eal_version.map
@@ -252,5 +252,10 @@ DPDK_17.11 {
 	rte_memcpy_ptr;
 	rte_pci_get_iommu_class;
 	rte_pci_match;
+	vfio_enable;
+	vfio_is_enabled;
+	vfio_noiommu_is_enabled;
+	vfio_release_device;
+	vfio_setup_device;
 
 } DPDK_17.08;

drivers/bus/fslmc/fslmc_vfio.h
@@ -34,6 +34,8 @@
 #ifndef _FSLMC_VFIO_H_
 #define _FSLMC_VFIO_H_
 
+#include <rte_vfio.h>
+
 #include "eal_vfio.h"
 
 #define DPAA2_MC_DPNI_DEVID	7

lib/librte_eal/common/Makefile
@@ -43,7 +43,7 @@ INC += rte_hexdump.h rte_devargs.h rte_bus.h rte_dev.h rte_vdev.h
 INC += rte_pci_dev_feature_defs.h rte_pci_dev_features.h
 INC += rte_malloc.h rte_keepalive.h rte_time.h
 INC += rte_service.h rte_service_component.h
-INC += rte_bitmap.h
+INC += rte_bitmap.h rte_vfio.h
 
 GENERIC_INC := rte_atomic.h rte_byteorder.h rte_cycles.h rte_prefetch.h
 GENERIC_INC += rte_spinlock.h rte_memcpy.h rte_cpuflags.h rte_rwlock.h

lib/librte_eal/linuxapp/eal/eal_interrupts.c
@@ -65,6 +65,7 @@
 #include <rte_errno.h>
 #include <rte_spinlock.h>
 #include <rte_pause.h>
+#include <rte_vfio.h>
 
 #include "eal_private.h"
 #include "eal_vfio.h"

commit 77dad68c20f8f77e20dd9618c00d211b650c387e

vfio: fix namespace prefix of newly exposed functions
Exposed VFIO functions simply uses a "vfio" prefix.
Use the proper "rte_vfio" prefix for those symbols.

Fixes: 279b581c897d ("vfio: expose functions")

Signed-off-by: Gaetan Rivet <gaetan.rivet@6wind.com>

drivers/bus/pci/linux/pci.c
@@ -594,7 +594,7 @@ rte_pci_get_iommu_class(void)
 	has_iova_va = pci_one_device_has_iova_va();
 	is_bound_uio = pci_one_device_bound_uio();
 #ifdef VFIO_PRESENT
-	is_vfio_noiommu_enabled = vfio_noiommu_is_enabled() == true ?
+	is_vfio_noiommu_enabled = rte_vfio_noiommu_is_enabled() == true ?
 					true : false;
 #endif
 

drivers/bus/pci/linux/pci_vfio.c
@@ -325,7 +325,7 @@ pci_vfio_is_ioport_bar(int vfio_dev_fd, int bar_index)
 }
 
 static int
-pci_vfio_setup_device(struct rte_pci_device *dev, int vfio_dev_fd)
+pci_rte_vfio_setup_device(struct rte_pci_device *dev, int vfio_dev_fd)
 {
 	if (pci_vfio_setup_interrupts(dev, vfio_dev_fd) != 0) {
 		RTE_LOG(ERR, EAL, "Error setting up interrupts!\n");
@@ -465,7 +465,7 @@ pci_vfio_map_resource_primary(struct rte_pci_device *dev)
 	snprintf(pci_addr, sizeof(pci_addr), PCI_PRI_FMT,
 			loc->domain, loc->bus, loc->devid, loc->function);
 
-	ret = vfio_setup_device(pci_get_sysfs_path(), pci_addr,
+	ret = rte_vfio_setup_device(pci_get_sysfs_path(), pci_addr,
 					&vfio_dev_fd, &device_info);
 	if (ret)
 		return ret;
@@ -546,7 +546,7 @@ pci_vfio_map_resource_primary(struct rte_pci_device *dev)
 		dev->mem_resource[i].addr = maps[i].addr;
 	}
 
-	if (pci_vfio_setup_device(dev, vfio_dev_fd) < 0) {
+	if (pci_rte_vfio_setup_device(dev, vfio_dev_fd) < 0) {
 		RTE_LOG(ERR, EAL, "  %s setup device failed\n", pci_addr);
 		goto err_vfio_res;
 	}
@@ -582,7 +582,7 @@ pci_vfio_map_resource_secondary(struct rte_pci_device *dev)
 	snprintf(pci_addr, sizeof(pci_addr), PCI_PRI_FMT,
 			loc->domain, loc->bus, loc->devid, loc->function);
 
-	ret = vfio_setup_device(pci_get_sysfs_path(), pci_addr,
+	ret = rte_vfio_setup_device(pci_get_sysfs_path(), pci_addr,
 					&vfio_dev_fd, &device_info);
 	if (ret)
 		return ret;
@@ -662,7 +662,7 @@ pci_vfio_unmap_resource(struct rte_pci_device *dev)
 		return -1;
 	}
 
-	ret = vfio_release_device(pci_get_sysfs_path(), pci_addr,
+	ret = rte_vfio_release_device(pci_get_sysfs_path(), pci_addr,
 				  dev->intr_handle.vfio_dev_fd);
 	if (ret < 0) {
 		RTE_LOG(ERR, EAL,
@@ -758,6 +758,6 @@ pci_vfio_ioport_unmap(struct rte_pci_ioport *p)
 int
 pci_vfio_is_enabled(void)
 {
-	return vfio_is_enabled("vfio_pci");
+	return rte_vfio_is_enabled("vfio_pci");
 }
 #endif

lib/librte_eal/bsdapp/eal/eal.c
@@ -743,14 +743,14 @@ rte_eal_vfio_intr_mode(void)
 struct vfio_device_info;
 
 /* dummy prototypes. */
-int vfio_setup_device(const char *sysfs_base, const char *dev_addr,
+int rte_vfio_setup_device(const char *sysfs_base, const char *dev_addr,
 		int *vfio_dev_fd, struct vfio_device_info *device_info);
-int vfio_release_device(const char *sysfs_base, const char *dev_addr, int fd);
-int vfio_enable(const char *modname);
-int vfio_is_enabled(const char *modname);
-int vfio_noiommu_is_enabled(void);
+int rte_vfio_release_device(const char *sysfs_base, const char *dev_addr, int fd);
+int rte_vfio_enable(const char *modname);
+int rte_vfio_is_enabled(const char *modname);
+int rte_vfio_noiommu_is_enabled(void);
 
-int vfio_setup_device(__rte_unused const char *sysfs_base,
+int rte_vfio_setup_device(__rte_unused const char *sysfs_base,
 		      __rte_unused const char *dev_addr,
 		      __rte_unused int *vfio_dev_fd,
 		      __rte_unused struct vfio_device_info *device_info)
@@ -758,24 +758,24 @@ int vfio_setup_device(__rte_unused const char *sysfs_base,
 	return -1;
 }
 
-int vfio_release_device(__rte_unused const char *sysfs_base,
+int rte_vfio_release_device(__rte_unused const char *sysfs_base,
 			__rte_unused const char *dev_addr,
 			__rte_unused int fd)
 {
 	return -1;
 }
 
-int vfio_enable(__rte_unused const char *modname)
+int rte_vfio_enable(__rte_unused const char *modname)
 {
 	return -1;
 }
 
-int vfio_is_enabled(__rte_unused const char *modname)
+int rte_vfio_is_enabled(__rte_unused const char *modname)
 {
 	return 0;
 }
 
-int vfio_noiommu_is_enabled(void)
+int rte_vfio_noiommu_is_enabled(void)
 {
 	return 0;
 }

lib/librte_eal/common/include/rte_vfio.h
@@ -82,7 +82,7 @@
  *   <0 on failure.
  *   >1 if the device cannot be managed this way.
  */
-int vfio_setup_device(const char *sysfs_base, const char *dev_addr,
+int rte_vfio_setup_device(const char *sysfs_base, const char *dev_addr,
 		int *vfio_dev_fd, struct vfio_device_info *device_info);
 
 /**
@@ -104,7 +104,7 @@ int vfio_setup_device(const char *sysfs_base, const char *dev_addr,
  *   0 on success.
  *   <0 on failure.
  */
-int vfio_release_device(const char *sysfs_base, const char *dev_addr, int fd);
+int rte_vfio_release_device(const char *sysfs_base, const char *dev_addr, int fd);
 
 /**
  * Enable a VFIO-related kmod.
@@ -119,7 +119,7 @@ int vfio_release_device(const char *sysfs_base, const char *dev_addr, int fd);
  *   0 on success.
  *   <0 on failure.
  */
-int vfio_enable(const char *modname);
+int rte_vfio_enable(const char *modname);
 
 /**
  * Check whether a VFIO-related kmod is enabled.
@@ -134,7 +134,7 @@ int vfio_enable(const char *modname);
  *   !0 if true.
  *   0 otherwise.
  */
-int vfio_is_enabled(const char *modname);
+int rte_vfio_is_enabled(const char *modname);
 
 /**
  * Whether VFIO NOIOMMU mode is enabled.
@@ -146,7 +146,7 @@ int vfio_is_enabled(const char *modname);
  *   !0 if true.
  *   0 otherwise.
  */
-int vfio_noiommu_is_enabled(void);
+int rte_vfio_noiommu_is_enabled(void);
 
 #endif /* VFIO_PRESENT */
 

lib/librte_eal/linuxapp/eal/eal.c
@@ -710,9 +710,9 @@ static int rte_eal_vfio_setup(void)
 {
 	int vfio_enabled = 0;
 
-	if (vfio_enable("vfio"))
+	if (rte_vfio_enable("vfio"))
 		return -1;
-	vfio_enabled = vfio_is_enabled("vfio");
+	vfio_enabled = rte_vfio_is_enabled("vfio");
 
 	if (vfio_enabled) {
 

lib/librte_eal/linuxapp/eal/eal_vfio.c
@@ -284,7 +284,7 @@ clear_group(int vfio_group_fd)
 }
 
 int
-vfio_setup_device(const char *sysfs_base, const char *dev_addr,
+rte_vfio_setup_device(const char *sysfs_base, const char *dev_addr,
 		int *vfio_dev_fd, struct vfio_device_info *device_info)
 {
 	struct vfio_group_status group_status = {
@@ -416,7 +416,7 @@ vfio_setup_device(const char *sysfs_base, const char *dev_addr,
 }
 
 int
-vfio_release_device(const char *sysfs_base, const char *dev_addr,
+rte_vfio_release_device(const char *sysfs_base, const char *dev_addr,
 		    int vfio_dev_fd)
 {
 	struct vfio_group_status group_status = {
@@ -478,7 +478,7 @@ vfio_release_device(const char *sysfs_base, const char *dev_addr,
 }
 
 int
-vfio_enable(const char *modname)
+rte_vfio_enable(const char *modname)
 {
 	/* initialize group list */
 	int i;
@@ -523,7 +523,7 @@ vfio_enable(const char *modname)
 }
 
 int
-vfio_is_enabled(const char *modname)
+rte_vfio_is_enabled(const char *modname)
 {
 	const int mod_available = rte_eal_check_module(modname);
 	return vfio_cfg.vfio_enabled && mod_available;
@@ -841,7 +841,7 @@ vfio_noiommu_dma_map(int __rte_unused vfio_container_fd)
 }
 
 int
-vfio_noiommu_is_enabled(void)
+rte_vfio_noiommu_is_enabled(void)
 {
 	int fd, ret, cnt __rte_unused;
 	char c;

lib/librte_eal/rte_eal_version.map
@@ -235,10 +235,10 @@ DPDK_17.11 {
 	rte_eal_vfio_intr_mode;
 	rte_lcore_has_role;
 	rte_memcpy_ptr;
-	vfio_enable;
-	vfio_is_enabled;
-	vfio_noiommu_is_enabled;
-	vfio_release_device;
-	vfio_setup_device;
+	rte_vfio_enable;
+	rte_vfio_is_enabled;
+	rte_vfio_noiommu_is_enabled;
+	rte_vfio_release_device;
+	rte_vfio_setup_device;
 
 } DPDK_17.08;