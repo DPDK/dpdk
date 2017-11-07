DPDK/dpdk

commit b48e0e2d9cb471941703eb26dc0dbd4fb9840d40

bus/pci: fix IOMMU class for sPAPR
PPC64 sPAPR iommu does not support iova as va.
Use pa mode instead.

Fixes: 815c7deaed2d ("pci: get IOMMU class on Linux")

Signed-off-by: Jonas Pfefferle <jpf@zurich.ibm.com>

drivers/bus/pci/linux/pci.c
@@ -586,6 +586,12 @@ rte_pci_get_iommu_class(void)
 	bool is_vfio_noiommu_enabled = true;
 	bool has_iova_va;
 	bool is_bound_uio;
+	bool spapr_iommu =
+#if defined(RTE_ARCH_PPC64)
+		true;
+#else
+		false;
+#endif
 
 	is_bound = pci_one_device_is_bound();
 	if (!is_bound)
@@ -598,7 +604,8 @@ rte_pci_get_iommu_class(void)
 					true : false;
 #endif
 
-	if (has_iova_va && !is_bound_uio && !is_vfio_noiommu_enabled)
+	if (has_iova_va && !is_bound_uio && !is_vfio_noiommu_enabled &&
+			!spapr_iommu)
 		return RTE_IOVA_VA;
 
 	if (has_iova_va) {
@@ -607,6 +614,8 @@ rte_pci_get_iommu_class(void)
 			RTE_LOG(WARNING, EAL, "vfio-noiommu mode configured\n");
 		if (is_bound_uio)
 			RTE_LOG(WARNING, EAL, "few device bound to UIO\n");
+		if (spapr_iommu)
+			RTE_LOG(WARNING, EAL, "sPAPR IOMMU does not support IOVA as VA\n");
 	}
 
 	return RTE_IOVA_PA;

commit f1b7c6b7f5eb51809aaec66a3fa31e600f2dcd12

bus/pci: fix PPC condition for IOMMU class
This fixes the use of an never defined PPC64 define in
ret_pci_get_iommu_class.

Fixes: b48e0e2d9cb4 ("bus/pci: fix IOMMU class for sPAPR")

Signed-off-by: Jonas Pfefferle <jpf@zurich.ibm.com>

drivers/bus/pci/linux/pci.c
@@ -587,7 +587,7 @@ rte_pci_get_iommu_class(void)
 	bool has_iova_va;
 	bool is_bound_uio;
 	bool spapr_iommu =
-#if defined(RTE_ARCH_PPC64)
+#if defined(RTE_ARCH_PPC_64)
 		true;
 #else
 		false;