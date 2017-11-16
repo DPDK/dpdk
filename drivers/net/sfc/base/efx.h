79e2a9d018585c13b92a91616c022ebf241b8709
Andy Moreton <amoreton@solarflare.com>
2017-11-16 08:03:54 +0000

    net/sfc/base: extend NVRAM RW finish to return verify result
    Extend efx_nvram_rw_finish() to return firmware verify result code.
    
    Signed-off-by: Andy Moreton <amoreton@solarflare.com>
    Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>

--- drivers/net/sfc/base/efx.h
+++ drivers/net/sfc/base/efx.h
@@ -1411,7 +1411,8 @@ efx_nvram_rw_start(
 extern	__checkReturn		efx_rc_t
 efx_nvram_rw_finish(
 	__in			efx_nic_t *enp,
-	__in			efx_nvram_type_t type);
+	__in			efx_nvram_type_t type,
+	__out_opt		uint32_t *verify_resultp);
 
 extern	__checkReturn		efx_rc_t
 efx_nvram_get_version(
