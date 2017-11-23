c7869af57e88bf0b9bd1a79ef1ae36b56d9525a8
Adrien Mazarguil <adrien.mazarguil@6wind.com>
2017-11-23 18:37:58 +0100

    net/mlx4: fix documentation in private structure
    A couple of structure fields are not Doxygen-friendly.
    
    Fixes: 5db1d364086e ("net/mlx4: restore Tx checksum offloads")
    Cc: stable@dpdk.org
    
    Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>

--- drivers/net/mlx4/mlx4.h
+++ drivers/net/mlx4/mlx4.h
@@ -126,8 +126,8 @@ struct priv {
 	uint32_t vf:1; /**< This is a VF device. */
 	uint32_t intr_alarm:1; /**< An interrupt alarm is scheduled. */
 	uint32_t isolated:1; /**< Toggle isolated mode. */
-	uint32_t hw_csum:1; /* Checksum offload is supported. */
-	uint32_t hw_csum_l2tun:1; /* Checksum support for L2 tunnels. */
+	uint32_t hw_csum:1; /**< Checksum offload is supported. */
+	uint32_t hw_csum_l2tun:1; /**< Checksum support for L2 tunnels. */
 	struct rte_intr_handle intr_handle; /**< Port interrupt handle. */
 	struct mlx4_drop *drop; /**< Shared resources for drop flow rules. */
 	LIST_HEAD(, mlx4_rss) rss; /**< Shared targets for Rx flow rules. */
