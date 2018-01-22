7d540a3e735dd4d3e5b57d281f079ecbf0a8f9fc
Neil Horman <nhorman@tuxdriver.com>
2018-01-21 20:48:04 -0500

    compat: add experimental tag macro
    The __rte_experimental macro tags a given exported function as being part of
    the EXPERIMENTAL api.  Use of this tag will cause any caller of the
    function (that isn't removed by dead code elimination) to emit a warning
    that the user is making use of an API whos stabilty isn't guaranteed.
    It also places the function in the .text.experimental section, which is
    used to validate the tag against the corresponding library version map
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- lib/librte_compat/rte_compat.h
+++ lib/librte_compat/rte_compat.h
@@ -101,5 +101,17 @@
  */
 #endif
 
+#ifndef ALLOW_EXPERIMENTAL_API
+
+#define __rte_experimental \
+__attribute__((deprecated("Symbol is not yet part of stable ABI"), \
+section(".text.experimental")))
+
+#else
+
+#define __rte_experimental \
+__attribute__((section(".text.experimental")))
+
+#endif
 
 #endif /* _RTE_COMPAT_H_ */
