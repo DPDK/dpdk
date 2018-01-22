59a3cae5305816b3739b4197d277f9455af53b9f
Jia He <jia.he@hxt-semitech.com>
2018-01-21 20:41:26 -0800

    eal/arm64: remove the braces in memory barrier macros
    for the code as follows:
    if (condition)
    	rte_smp_rmb();
    else
    	rte_smp_wmb();
    Without this patch, compiler will report this error:
    error: 'else' without a previous 'if'
    
    Fixes: 84733fd0d75e ("eal/arm64: fix memory barrier definition")
    Cc: stable@dpdk.org
    
    Signed-off-by: Jia He <jia.he@hxt-semitech.com>
    Acked-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>
    Acked-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- lib/librte_eal/common/include/arch/arm/rte_atomic_64.h
+++ lib/librte_eal/common/include/arch/arm/rte_atomic_64.h
@@ -15,8 +15,8 @@ extern "C" {
 
 #include "generic/rte_atomic.h"
 
-#define dsb(opt)  { asm volatile("dsb " #opt : : : "memory"); }
-#define dmb(opt)  { asm volatile("dmb " #opt : : : "memory"); }
+#define dsb(opt) asm volatile("dsb " #opt : : : "memory")
+#define dmb(opt) asm volatile("dmb " #opt : : : "memory")
 
 #define rte_mb() dsb(sy)
 
