e07a3ed786c50e31cbb6e01f9f26cada0c2a4d70
Bruce Richardson <bruce.richardson@intel.com>
2018-01-23 14:11:33 +0000

    event/opdl: fix license header and SPDX tags
    This patch ensures that the OPDL files all contain correct SPDX tags.
    The following changes were made to achieve this:
    * replace license text with SPDX tag
    * correct occurences where SPDX tag was not on first line of file
    * ensure license years were correct
    
    Fixes: 4236ce9bf5bf ("event/opdl: add OPDL ring infrastructure library")
    Fixes: 3c7f3dcfb099 ("event/opdl: add PMD main body and helper function")
    Fixes: d548ef513cd7 ("event/opdl: add unit tests")
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
    Acked-by: Liang Ma <liang.j.ma@intel.com>

--- drivers/event/opdl/opdl_ring.h
+++ drivers/event/opdl/opdl_ring.h
@@ -1,6 +1,5 @@
-/*-
- * SPDX-License-Identifier: BSD-3-Clause
- * Copyright(c) 2010-2014 Intel Corporation
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2017 Intel Corporation
  */
 
 #ifndef _OPDL_H_
