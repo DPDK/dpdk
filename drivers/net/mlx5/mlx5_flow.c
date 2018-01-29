DPDK/dpdk

commit 0a40a1363a4df8043f9a7e93289eda2ea52815a7

net/mlx5: fix flow type for allmulti rules
Chnaged ibv_flow_attr type for allmulti rule to IBV_FLOW_ATTR_MC_DEFAULT
instead of IBV_FLOW_ATTR_NORMAL, in case allmulti was enabled.

Fixes: 272733b5 ("net/mlx5: use flow to enable unicast traffic")
Cc: stable@dpdk.org

Signed-off-by: Raslan Darawsheh <rasland@mellanox.com>
Acked-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5_flow.c
@@ -429,6 +429,7 @@ static const struct mlx5_flow_items mlx5_flow_items[] = {
 /** Structure to pass to the conversion function. */
 struct mlx5_flow_parse {
 	uint32_t inner; /**< Set once VXLAN is encountered. */
+	uint32_t allmulti:1; /**< Set once allmulti dst MAC is encountered. */
 	uint32_t create:1;
 	/**< Whether resources should remain after a validate. */
 	uint32_t drop:1; /**< Target is a drop queue. */
@@ -1165,6 +1166,17 @@ exit_free:
 			}
 		}
 	}
+	if (parser->allmulti &&
+	    parser->layer == HASH_RXQ_ETH) {
+		for (i = 0; i != hash_rxq_init_n; ++i) {
+			if (!parser->queue[i].ibv_attr)
+				continue;
+			if (parser->queue[i].ibv_attr->num_of_specs != 1)
+				break;
+			parser->queue[i].ibv_attr->type =
+						IBV_FLOW_ATTR_MC_DEFAULT;
+		}
+	}
 	return ret;
 exit_enomem:
 	for (i = 0; i != hash_rxq_init_n; ++i) {
@@ -1262,6 +1274,7 @@ mlx5_flow_create_eth(const struct rte_flow_item *item,
 		eth.val.ether_type &= eth.mask.ether_type;
 	}
 	mlx5_flow_create_copy(parser, &eth, eth_size);
+	parser->allmulti = eth.val.dst_mac[0] & 1;
 	return 0;
 }
 

commit da646bd938882b85454faf56d6cd9c64c46262b6

net/mlx5: fix all multi verification code position
All multi code should not be handled in exit part of the code but in the
mainline of the function.

Fixes: 0a40a1363a4d ("net/mlx5: fix flow type for allmulti rules")
Cc: stable@dpdk.org

Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>
Acked-by: Yongseok Koh <yskoh@mellanox.com>

drivers/net/mlx5/mlx5_flow.c
@@ -1168,16 +1168,6 @@ priv_flow_convert(struct priv *priv,
 			attr->priority +
 			hash_rxq_init[parser->layer].flow_priority;
 	}
-exit_free:
-	/* Only verification is expected, all resources should be released. */
-	if (!parser->create) {
-		for (i = 0; i != hash_rxq_init_n; ++i) {
-			if (parser->queue[i].ibv_attr) {
-				rte_free(parser->queue[i].ibv_attr);
-				parser->queue[i].ibv_attr = NULL;
-			}
-		}
-	}
 	if (parser->allmulti &&
 	    parser->layer == HASH_RXQ_ETH) {
 		for (i = 0; i != hash_rxq_init_n; ++i) {
@@ -1187,6 +1177,16 @@ exit_free:
 				break;
 			parser->queue[i].ibv_attr->type =
 						IBV_FLOW_ATTR_MC_DEFAULT;
+		}
+	}
+exit_free:
+	/* Only verification is expected, all resources should be released. */
+	if (!parser->create) {
+		for (i = 0; i != hash_rxq_init_n; ++i) {
+			if (parser->queue[i].ibv_attr) {
+				rte_free(parser->queue[i].ibv_attr);
+				parser->queue[i].ibv_attr = NULL;
+			}
 		}
 	}
 	return ret;