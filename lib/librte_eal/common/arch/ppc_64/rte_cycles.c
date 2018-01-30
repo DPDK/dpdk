4e4b97b71ca0f25b2b7ceb03d21a100f74fa4512
Gowrishankar Muthukrishnan <gowrishankar.m@linux.vnet.ibm.com>
2018-01-30 14:29:13 +0530

    eal/ppc64: revert arch-specific TSC freq query
    This reverts commit 15692396fd68932b6a81f00f12d4b0da12baa7d3
    (eal/ppc64: implement arch-specific TSC freq query).
    We intended to derive pkt/sec estimation with cpu clock frequency.
    As timebase register serves the timer purpose, we need to stick with it
    for calculating pkt/sec, hence reverting the change.
    
    Fixes: 15692396fd ("eal/ppc64: implement arch-specific TSC freq query")
    Cc: stable@dpdk.org
    
    Signed-off-by: Gowrishankar Muthukrishnan <gowrishankar.m@linux.vnet.ibm.com>
    Acked-by: Chao Zhu <chaozhu@linux.vnet.ibm.com>

--- lib/librte_eal/common/arch/ppc_64/rte_cycles.c
+++ lib/librte_eal/common/arch/ppc_64/rte_cycles.c
@@ -1,52 +1,7 @@
-/*
- *   BSD LICENSE
- *
- *   Copyright (C) IBM Corporation 2014.
- *
- *   Redistribution and use in source and binary forms, with or without
- *   modification, are permitted provided that the following conditions
- *   are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in
- *       the documentation and/or other materials provided with the
- *       distribution.
- *     * Neither the name of IBM Corporation nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <rte_lcore.h>
-#include <rte_log.h>
-#include "eal_filesystem.h"
 #include "eal_private.h"
-
-static const char sys_cpu_dir[] = "/sys/devices/system/cpu";
 
 uint64_t
 get_tsc_freq_arch(void)
 {
-	unsigned long cpu_hz;
-	char path[PATH_MAX];
-
-	snprintf(path, sizeof(path), "%s/cpu%d/cpufreq/cpuinfo_cur_freq",
-			sys_cpu_dir, rte_get_master_lcore());
-	if (eal_parse_sysfs_value(path, &cpu_hz) < 0)
-		RTE_LOG(WARNING, EAL, "Unable to parse %s\n", path);
-
-	return cpu_hz*1000;
+	return 0;
 }
