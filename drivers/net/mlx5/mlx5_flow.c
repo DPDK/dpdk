DPDK/dpdk

commit 0fb2c9842b206ffbf255690b2674ddca8f24646a

net/mlx5: support IPv4 time-to-live filter
Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>
Acked-by: Yongseok Koh <yskoh@mellanox.com>

drivers/net/mlx5/mlx5_flow.c
@@ -353,6 +353,7 @@ static const struct mlx5_flow_items mlx5_flow_items[] = {
 				.dst_addr = -1,
 				.type_of_service = -1,
 				.next_proto_id = -1,
+				.time_to_live = -1,
 			},
 		},
 		.default_mask = &rte_flow_item_ipv4_mask,

commit 282da936f9ecc7ea60a7f48729af97e25c447fe6

net/mlx5: revert support of IPv4 time-to-live filter
Neither upstream kernel nor MLNX_OFED support such filter.
There is no point announcing this feature.

Reverts commit 0fb2c9842b20 ("net/mlx5: support IPv4 time-to-live filter")

Fixes: 0fb2c9842b20 ("net/mlx5: support IPv4 time-to-live filter")

Signed-off-by: Shahaf Shuler <shahafs@mellanox.com>
Acked-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5_flow.c
@@ -313,7 +313,6 @@ static const struct mlx5_flow_items mlx5_flow_items[] = {
 				.dst_addr = -1,
 				.type_of_service = -1,
 				.next_proto_id = -1,
-				.time_to_live = -1,
 			},
 		},
 		.default_mask = &rte_flow_item_ipv4_mask,