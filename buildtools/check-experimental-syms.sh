DPDK/dpdk

commit a4bcd61de82d821b7e81c186c9254f816e9653bd

buildtools: add script to check experimental API exports
This tools reads the given version map for a directory, and checks to
ensure that, for each symbol listed in the export list, the corresponding
definition is tagged as __rte_experimental, erroring out if its not.  In this
way, we can ensure that the EXPERIMENTAL api is kept in sync with the tags

Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

buildtools/check-experimental-syms.sh
@@ -1,0 +1,32 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: BSD-3-Clause
+
+MAPFILE=$1
+OBJFILE=$2
+
+if [ -d $MAPFILE ]
+then
+	exit 0
+fi
+
+for i in `awk 'BEGIN {found=0}
+		/.*EXPERIMENTAL.*/ {found=1}
+		/.*}.*;/ {found=0}
+		/.*;/ {if (found == 1) print $1}' $MAPFILE`
+do
+	SYM=`echo $i | sed -e"s/;//"`
+	objdump -t $OBJFILE | grep -q "\.text.*$SYM"
+	IN_TEXT=$?
+	objdump -t $OBJFILE | grep -q "\.text\.experimental.*$SYM"
+	IN_EXP=$?
+	if [ $IN_TEXT -eq 0 -a $IN_EXP -ne 0 ]
+	then
+		echo "$SYM is not flagged as experimental"
+		echo "but is listed in version map"
+		echo "Please add __rte_experimental to the definition of $SYM"
+		exit 1
+	fi
+done
+exit 0
+

MAINTAINERS
@@ -77,6 +77,7 @@ M: Neil Horman <nhorman@tuxdriver.com>
 F: lib/librte_compat/
 F: doc/guides/rel_notes/deprecation.rst
 F: devtools/validate-abi.sh
+F: buildtools/check-experimental-syms.sh
 
 Driver information
 F: buildtools/pmdinfogen/

commit 5c753861c399628e522d47533fe26b0aef876209

buildtools: output build failure reason to stderr
If build fails because of failed experimental check and stdout is
redirected to /dev/null, it is absolutely unclear why build fails.

Fixes: a4bcd61de82d ("buildtools: add script to check experimental API exports")

Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>
Acked-by: Neil Horman <nhorman@tuxdriver.com>

buildtools/check-experimental-syms.sh
@@ -22,9 +22,11 @@ do
 	IN_EXP=$?
 	if [ $IN_TEXT -eq 0 -a $IN_EXP -ne 0 ]
 	then
-		echo "$SYM is not flagged as experimental"
-		echo "but is listed in version map"
-		echo "Please add __rte_experimental to the definition of $SYM"
+		cat >&2 <<- END_OF_MESSAGE
+		$SYM is not flagged as experimental
+		but is listed in version map
+		Please add __rte_experimental to the definition of $SYM
+		END_OF_MESSAGE
 		exit 1
 	fi
 done