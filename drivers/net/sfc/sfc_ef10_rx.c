7e48cfe9ef36f9a11363a568332be5595119aea3
Andy Moreton <amoreton@solarflare.com>
2018-02-20 07:33:44 +0000

    net/sfc/base: update hardware headers for Medford2
    The changes to efx_regs_ef10.h are auto-generated and include:
    
     - Updated event RX_L4_CLASS which is now 2 bits (was 3).
       The encoding of TCP, UDP and UNKNOWN are unchanged so
       the narrower Medford2 field definition is compatible with
       all controllers.
    
     - Fix definition of FATSOv2 option descriptors. These were
       added manually and differ from the auto-generated values
       in some fields (not yet used in common code). The field
       definitions have been corrected to agree with the Linux net
       driver headers and SF-108452-SW.
    
    The remaining changes adapt the common code to use the updated
    headers.
    
    Signed-off-by: Andy Moreton <amoreton@solarflare.com>
    Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>

--- drivers/net/sfc/sfc_ef10_rx.c
+++ drivers/net/sfc/sfc_ef10_rx.c
@@ -325,22 +325,32 @@ sfc_ef10_rx_ev_to_offloads(struct sfc_ef10_rxq *rxq, const efx_qword_t rx_ev,
 		SFC_ASSERT(false);
 	}
 
-	switch (EFX_QWORD_FIELD(rx_ev, ESF_DZ_RX_L4_CLASS)) {
-	case ESE_DZ_L4_CLASS_TCP:
+	/*
+	 * RX_L4_CLASS is 3 bits wide on Huntington and Medford, but is only
+	 * 2 bits wide on Medford2. Check it is safe to use the Medford2 field
+	 * and values for all EF10 controllers.
+	 */
+	RTE_BUILD_BUG_ON(ESF_FZ_RX_L4_CLASS_LBN != ESF_DE_RX_L4_CLASS_LBN);
+	switch (EFX_QWORD_FIELD(rx_ev, ESF_FZ_RX_L4_CLASS)) {
+	case ESE_FZ_L4_CLASS_TCP:
+		 RTE_BUILD_BUG_ON(ESE_FZ_L4_CLASS_TCP != ESE_DE_L4_CLASS_TCP);
 		l4_ptype = (tun_ptype == 0) ? RTE_PTYPE_L4_TCP :
 			RTE_PTYPE_INNER_L4_TCP;
 		ol_flags |=
 			(EFX_TEST_QWORD_BIT(rx_ev, l4_csum_err_bit)) ?
 			PKT_RX_L4_CKSUM_BAD : PKT_RX_L4_CKSUM_GOOD;
 		break;
-	case ESE_DZ_L4_CLASS_UDP:
+	case ESE_FZ_L4_CLASS_UDP:
+		 RTE_BUILD_BUG_ON(ESE_FZ_L4_CLASS_UDP != ESE_DE_L4_CLASS_UDP);
 		l4_ptype = (tun_ptype == 0) ? RTE_PTYPE_L4_UDP :
 			RTE_PTYPE_INNER_L4_UDP;
 		ol_flags |=
 			(EFX_TEST_QWORD_BIT(rx_ev, l4_csum_err_bit)) ?
 			PKT_RX_L4_CKSUM_BAD : PKT_RX_L4_CKSUM_GOOD;
 		break;
-	case ESE_DZ_L4_CLASS_UNKNOWN:
+	case ESE_FZ_L4_CLASS_UNKNOWN:
+		 RTE_BUILD_BUG_ON(ESE_FZ_L4_CLASS_UNKNOWN !=
+				  ESE_DE_L4_CLASS_UNKNOWN);
 		break;
 	default:
 		/* Unexpected Layer 4 class */
