2a5002362d2af81590c958773021cd297830f270
Keith Wiles <keith.wiles@intel.com>
2018-03-10 10:24:28 -0600

    kvargs: fix syntax in comments
    Use commas as separator, not semicolons.
    
    Fixes: a8b97e3a1db0 ("devargs: use a comma instead of semicolon to separate key/values")
    Cc: stable@dpdk.org
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- lib/librte_kvargs/rte_kvargs.c
+++ lib/librte_kvargs/rte_kvargs.c
@@ -13,7 +13,7 @@
 
 /*
  * Receive a string with a list of arguments following the pattern
- * key=value;key=value;... and insert them into the list.
+ * key=value,key=value,... and insert them into the list.
  * strtok() is used so the params string will be copied to be modified.
  */
 static int
@@ -154,7 +154,7 @@ rte_kvargs_free(struct rte_kvargs *kvlist)
 }
 
 /*
- * Parse the arguments "key=value;key=value;..." string and return
+ * Parse the arguments "key=value,key=value,..." string and return
  * an allocated structure that contains a key/value list. Also
  * check if only valid keys were used.
  */
