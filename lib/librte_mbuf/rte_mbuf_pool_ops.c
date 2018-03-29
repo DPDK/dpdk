8ea081f381615308bb872a608ebbdb0aa6c9f90f
Stephen Hemminger <stephen@networkplumber.org>
2018-03-29 10:05:30 -0700

    mbuf: fix truncated strncpy
    Gcc-8 discovers issue with platform_mempool_ops.
    rte_mbuf_pool_ops.c:26:3: error: ‘strncpy’ output truncated before
      terminating nul copying as many bytes from a string as its length
      [-Werror=stringop-truncation]
      strncpy(mz->addr, ops_name,  strlen(ops_name));
    
    Since the ops_name is already checked for size, using strncpy
    here is unnecessary; just use strcpy.
    
    Fixes: a3acc3144a76 ("mbuf: add pool ops selection functions")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- lib/librte_mbuf/rte_mbuf_pool_ops.c
+++ lib/librte_mbuf/rte_mbuf_pool_ops.c
@@ -23,7 +23,7 @@ rte_mbuf_set_platform_mempool_ops(const char *ops_name)
 			RTE_MEMPOOL_OPS_NAMESIZE, SOCKET_ID_ANY, 0);
 		if (mz == NULL)
 			return -rte_errno;
-		strncpy(mz->addr, ops_name, strlen(ops_name));
+		strcpy(mz->addr, ops_name);
 		return 0;
 	} else if (strcmp(mz->addr, ops_name) == 0) {
 		return 0;
@@ -62,7 +62,7 @@ rte_mbuf_set_user_mempool_ops(const char *ops_name)
 			return -rte_errno;
 	}
 
-	strncpy(mz->addr, ops_name, strlen(ops_name));
+	strcpy(mz->addr, ops_name);
 	return 0;
 
 }
