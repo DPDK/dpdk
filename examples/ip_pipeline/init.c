411a5df91da6a9fbdcda1267697d84e44736c98d
Jasvinder Singh <jasvinder.singh@intel.com>
2018-03-29 19:31:32 +0100

    examples/ip_pipeline: remove passthrough pipeline
    remove passthrough pipeline.
    
    Signed-off-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>
    Signed-off-by: Jasvinder Singh <jasvinder.singh@intel.com>

--- examples/ip_pipeline/init.c
+++ examples/ip_pipeline/init.c
@@ -26,7 +26,6 @@
 #include "pipeline.h"
 #include "pipeline_common_fe.h"
 #include "pipeline_master.h"
-#include "pipeline_passthrough.h"
 #include "pipeline_firewall.h"
 #include "pipeline_flow_classification.h"
 #include "pipeline_flow_actions.h"
@@ -1822,7 +1821,6 @@ int app_init(struct app_params *app)
 	app_pipeline_common_cmd_push(app);
 	app_pipeline_thread_cmd_push(app);
 	app_pipeline_type_register(app, &pipeline_master);
-	app_pipeline_type_register(app, &pipeline_passthrough);
 	app_pipeline_type_register(app, &pipeline_flow_classification);
 	app_pipeline_type_register(app, &pipeline_flow_actions);
 	app_pipeline_type_register(app, &pipeline_firewall);
