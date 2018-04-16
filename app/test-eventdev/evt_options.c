DPDK/dpdk

commit 98c6292105d4461875d900949c8fd62353d27d22

app/eventdev: add options for event timer adapter
Add options to configure expiry timeout, max number of timers and number
of event timer adapters through command line parameters.

Signed-off-by: Pavan Nikhilesh <pbhagavatula@caviumnetworks.com>
Acked-by: Erik Gabriel Carrillo <erik.g.carrillo@intel.com>
Acked-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>

app/test-eventdev/evt_options.c
@@ -141,6 +141,56 @@ evt_parse_nb_pkts(struct evt_options *opt, const char *arg)
 }
 
 static int
+evt_parse_nb_timers(struct evt_options *opt, const char *arg)
+{
+	int ret;
+
+	ret = parser_read_uint64(&(opt->nb_timers), arg);
+
+	return ret;
+}
+
+static int
+evt_parse_timer_tick_nsec(struct evt_options *opt, const char *arg)
+{
+	int ret;
+
+	ret = parser_read_uint64(&(opt->timer_tick_nsec), arg);
+
+	return ret;
+}
+
+static int
+evt_parse_max_tmo_nsec(struct evt_options *opt, const char *arg)
+{
+	int ret;
+
+	ret = parser_read_uint64(&(opt->max_tmo_nsec), arg);
+
+	return ret;
+}
+
+static int
+evt_parse_expiry_nsec(struct evt_options *opt, const char *arg)
+{
+	int ret;
+
+	ret = parser_read_uint64(&(opt->expiry_nsec), arg);
+
+	return ret;
+}
+
+static int
+evt_parse_nb_timer_adptrs(struct evt_options *opt, const char *arg)
+{
+	int ret;
+
+	ret = parser_read_uint8(&(opt->nb_timer_adptrs), arg);
+
+	return ret;
+}
+
+static int
 evt_parse_pool_sz(struct evt_options *opt, const char *arg)
 {
 	opt->pool_sz = atoi(arg);
@@ -196,6 +246,11 @@ usage(char *program)
 		"\t                     in ns.\n"
 		"\t--prod_type_timerdev_burst : use timer device as producer\n"
 		"\t                             burst mode.\n"
+		"\t--nb_timers        : number of timers to arm.\n"
+		"\t--nb_timer_adptrs  : number of timer adapters to use.\n"
+		"\t--timer_tick_nsec  : timer tick interval in ns.\n"
+		"\t--max_tmo_nsec     : max timeout interval in ns.\n"
+		"\t--expiry_ns        : event timer expiry ns.\n"
 		);
 	printf("available tests:\n");
 	evt_test_dump_names();
@@ -259,6 +314,11 @@ static struct option lgopts[] = {
 	{ EVT_PROD_ETHDEV,         0, 0, 0 },
 	{ EVT_PROD_TIMERDEV,       0, 0, 0 },
 	{ EVT_PROD_TIMERDEV_BURST, 0, 0, 0 },
+	{ EVT_NB_TIMERS,           1, 0, 0 },
+	{ EVT_NB_TIMER_ADPTRS,     1, 0, 0 },
+	{ EVT_TIMER_TICK_NSEC,     1, 0, 0 },
+	{ EVT_MAX_TMO_NSEC,        1, 0, 0 },
+	{ EVT_EXPIRY_NSEC,         1, 0, 0 },
 	{ EVT_HELP,                0, 0, 0 },
 	{ NULL,                    0, 0, 0 }
 };
@@ -285,6 +345,11 @@ evt_opts_parse_long(int opt_idx, struct evt_options *opt)
 		{ EVT_PROD_ETHDEV, evt_parse_eth_prod_type},
 		{ EVT_PROD_TIMERDEV, evt_parse_timer_prod_type},
 		{ EVT_PROD_TIMERDEV_BURST, evt_parse_timer_prod_type_burst},
+		{ EVT_NB_TIMERS, evt_parse_nb_timers},
+		{ EVT_NB_TIMER_ADPTRS, evt_parse_nb_timer_adptrs},
+		{ EVT_TIMER_TICK_NSEC, evt_parse_timer_tick_nsec},
+		{ EVT_MAX_TMO_NSEC, evt_parse_max_tmo_nsec},
+		{ EVT_EXPIRY_NSEC, evt_parse_expiry_nsec},
 	};
 
 	for (i = 0; i < RTE_DIM(parsermap); i++) {

app/test-eventdev/evt_options.h
@@ -34,6 +34,11 @@
 #define EVT_PROD_ETHDEV          ("prod_type_ethdev")
 #define EVT_PROD_TIMERDEV        ("prod_type_timerdev")
 #define EVT_PROD_TIMERDEV_BURST  ("prod_type_timerdev_burst")
+#define EVT_NB_TIMERS            ("nb_timers")
+#define EVT_NB_TIMER_ADPTRS      ("nb_timer_adptrs")
+#define EVT_TIMER_TICK_NSEC      ("timer_tick_ns")
+#define EVT_MAX_TMO_NSEC         ("max_tmo_nsec")
+#define EVT_EXPIRY_NSEC          ("expiry_ns")
 #define EVT_HELP                 ("help")
 
 enum evt_prod_type {

commit 9e596d880fd5b71ca0b9e9e5539f9dc5b99ff77a

app/eventdev: fix typos in timer adapter options
The options names in code and doc are not the same.

Fixes: 98c6292105d4 ("app/eventdev: add options for event timer adapter")

Suggested-by: Thomas Monjalon <thomas@monjalon.net>
Signed-off-by: Pavan Nikhilesh <pbhagavatula@caviumnetworks.com>

app/test-eventdev/evt_options.c
@@ -250,7 +250,7 @@ usage(char *program)
 		"\t--nb_timer_adptrs  : number of timer adapters to use.\n"
 		"\t--timer_tick_nsec  : timer tick interval in ns.\n"
 		"\t--max_tmo_nsec     : max timeout interval in ns.\n"
-		"\t--expiry_ns        : event timer expiry ns.\n"
+		"\t--expiry_nsec        : event timer expiry ns.\n"
 		);
 	printf("available tests:\n");
 	evt_test_dump_names();

app/test-eventdev/evt_options.h
@@ -36,9 +36,9 @@
 #define EVT_PROD_TIMERDEV_BURST  ("prod_type_timerdev_burst")
 #define EVT_NB_TIMERS            ("nb_timers")
 #define EVT_NB_TIMER_ADPTRS      ("nb_timer_adptrs")
-#define EVT_TIMER_TICK_NSEC      ("timer_tick_ns")
+#define EVT_TIMER_TICK_NSEC      ("timer_tick_nsec")
 #define EVT_MAX_TMO_NSEC         ("max_tmo_nsec")
-#define EVT_EXPIRY_NSEC          ("expiry_ns")
+#define EVT_EXPIRY_NSEC          ("expiry_nsec")
 #define EVT_HELP                 ("help")
 
 enum evt_prod_type {
@@ -292,10 +292,10 @@ evt_dump_producer_type(struct evt_options *opt)
 		evt_dump("max_tmo_nsec", "%"PRIu64"", opt->max_tmo_nsec);
 		evt_dump("expiry_nsec", "%"PRIu64"", opt->expiry_nsec);
 		if (opt->optm_timer_tick_nsec)
-			evt_dump("optm_timer_tick_ns", "%"PRIu64"",
+			evt_dump("optm_timer_tick_nsec", "%"PRIu64"",
 					opt->optm_timer_tick_nsec);
 		else
-			evt_dump("timer_tick_ns", "%"PRIu64"",
+			evt_dump("timer_tick_nsec", "%"PRIu64"",
 					opt->timer_tick_nsec);
 		break;
 	}