088aed4b892e5e401af7fc5607c1eeaff563ebd1
Wei Zhao <wei.zhao1@intel.com>
2018-05-07 13:34:00 +0800

    net/i40e: fix flow RSS key array error
    This is a bug introduced into RSS key array span.
    
    Fixes: ac8d22de2394 ("ethdev: flatten RSS configuration in flow API")
    
    Signed-off-by: Wei Zhao <wei.zhao1@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/i40e/i40e_ethdev.h
+++ drivers/net/i40e/i40e_ethdev.h
@@ -886,7 +886,7 @@ struct i40e_rte_flow_rss_conf {
 	struct rte_flow_action_rss conf; /**< RSS parameters. */
 	uint16_t queue_region_conf; /**< Queue region config flag */
 	uint8_t key[(I40E_VFQF_HKEY_MAX_INDEX > I40E_PFQF_HKEY_MAX_INDEX ?
-		     I40E_VFQF_HKEY_MAX_INDEX : I40E_PFQF_HKEY_MAX_INDEX) + 1 *
+		     I40E_VFQF_HKEY_MAX_INDEX : I40E_PFQF_HKEY_MAX_INDEX + 1) *
 		    sizeof(uint32_t)]; /* Hash key. */
 	uint16_t queue[I40E_MAX_Q_PER_TC]; /**< Queues indices to use. */
 };
