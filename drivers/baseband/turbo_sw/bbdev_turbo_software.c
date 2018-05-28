DPDK/dpdk

commit 7ce00bf31bbe8c5566d53d6149354f5f12613048

bbdev: change names of baseband devices
Change baseband device name:
 - from turbo_sw to baseband_turbo_sw
 - from bbdev_null to baseband_null
To keep backwards compatibility the old names are still valid

Signed-off-by: Kamil Chalupnik <kamilx.chalupnik@intel.com>
Acked-by: Amr Mokhtar <amr.mokhtar@intel.com>

drivers/baseband/turbo_sw/bbdev_turbo_software.c
@@ -19,7 +19,7 @@
 #include <phy_rate_match.h>
 #include <divide.h>
 
-#define DRIVER_NAME turbo_sw
+#define DRIVER_NAME baseband_turbo_sw
 
 /* Turbo SW PMD logging ID */
 static int bbdev_turbo_sw_logtype;
@@ -1297,6 +1297,7 @@ RTE_PMD_REGISTER_VDEV(DRIVER_NAME, bbdev_turbo_sw_pmd_drv);
 RTE_PMD_REGISTER_PARAM_STRING(DRIVER_NAME,
 	TURBO_SW_MAX_NB_QUEUES_ARG"=<int> "
 	TURBO_SW_SOCKET_ID_ARG"=<int>");
+RTE_PMD_REGISTER_ALIAS(DRIVER_NAME, turbo_sw);
 
 RTE_INIT(null_bbdev_init_log);
 static void

app/test-bbdev/test-bbdev.py
@@ -33,7 +33,7 @@ parser.add_argument("-p", "--testapp-path",
                     default=dpdk_path + "/" + dpdk_target + "/app/testbbdev")
 parser.add_argument("-e", "--eal-params",
                     help="EAL arguments which are passed to the test app",
-                    default="--vdev=bbdev_null0")
+                    default="--vdev=baseband_null0")
 parser.add_argument("-t", "--timeout",
                     type=int,
                     help="Timeout in seconds",

drivers/baseband/null/bbdev_null.c
@@ -13,7 +13,7 @@
 #include <rte_bbdev.h>
 #include <rte_bbdev_pmd.h>
 
-#define DRIVER_NAME bbdev_null
+#define DRIVER_NAME baseband_null
 
 /* NULL BBDev logging ID */
 static int bbdev_null_logtype;
@@ -346,6 +346,7 @@ RTE_PMD_REGISTER_VDEV(DRIVER_NAME, bbdev_null_pmd_drv);
 RTE_PMD_REGISTER_PARAM_STRING(DRIVER_NAME,
 	BBDEV_NULL_MAX_NB_QUEUES_ARG"=<int> "
 	BBDEV_NULL_SOCKET_ID_ARG"=<int>");
+RTE_PMD_REGISTER_ALIAS(DRIVER_NAME, bbdev_null);
 
 RTE_INIT(null_bbdev_init_log);
 static void

commit ccaa250c02c96708360b50bbb65b91730a4928da

baseband/turbo_sw: shorten internal buffers names
Internal buffers names have been shortened to meet ring names size
requirements after device name changed.

Fixes: 7ce00bf31bbe ("bbdev: change names of baseband devices")

Signed-off-by: Kamil Chalupnik <kamilx.chalupnik@intel.com>
Acked-by: Amr Mokhtar <amr.mokhtar@intel.com>

drivers/baseband/turbo_sw/bbdev_turbo_software.c
@@ -227,7 +227,7 @@ q_setup(struct rte_bbdev *dev, uint16_t q_id,
 	}
 
 	/* Allocate memory for encoder output. */
-	ret = snprintf(name, RTE_RING_NAMESIZE, RTE_STR(DRIVER_NAME)"_enc_out%u:%u",
+	ret = snprintf(name, RTE_RING_NAMESIZE, RTE_STR(DRIVER_NAME)"_enc_o%u:%u",
 			dev->data->dev_id, q_id);
 	if ((ret < 0) || (ret >= (int)RTE_RING_NAMESIZE)) {
 		rte_bbdev_log(ERR,
@@ -247,7 +247,7 @@ q_setup(struct rte_bbdev *dev, uint16_t q_id,
 
 	/* Allocate memory for rate matching output. */
 	ret = snprintf(name, RTE_RING_NAMESIZE,
-			RTE_STR(DRIVER_NAME)"_enc_in%u:%u", dev->data->dev_id,
+			RTE_STR(DRIVER_NAME)"_enc_i%u:%u", dev->data->dev_id,
 			q_id);
 	if ((ret < 0) || (ret >= (int)RTE_RING_NAMESIZE)) {
 		rte_bbdev_log(ERR,
@@ -302,7 +302,7 @@ q_setup(struct rte_bbdev *dev, uint16_t q_id,
 
 	/* Allocate memory for Deinterleaver input. */
 	ret = snprintf(name, RTE_RING_NAMESIZE,
-			RTE_STR(DRIVER_NAME)"_deint_input%u:%u",
+			RTE_STR(DRIVER_NAME)"_de_i%u:%u",
 			dev->data->dev_id, q_id);
 	if ((ret < 0) || (ret >= (int)RTE_RING_NAMESIZE)) {
 		rte_bbdev_log(ERR,
@@ -321,7 +321,7 @@ q_setup(struct rte_bbdev *dev, uint16_t q_id,
 
 	/* Allocate memory for Deinterleaver output. */
 	ret = snprintf(name, RTE_RING_NAMESIZE,
-			RTE_STR(DRIVER_NAME)"_deint_output%u:%u",
+			RTE_STR(DRIVER_NAME)"_de_o%u:%u",
 			dev->data->dev_id, q_id);
 	if ((ret < 0) || (ret >= (int)RTE_RING_NAMESIZE)) {
 		rte_bbdev_log(ERR,
@@ -340,7 +340,7 @@ q_setup(struct rte_bbdev *dev, uint16_t q_id,
 
 	/* Allocate memory for Adapter output. */
 	ret = snprintf(name, RTE_RING_NAMESIZE,
-			RTE_STR(DRIVER_NAME)"_adapter_output%u:%u",
+			RTE_STR(DRIVER_NAME)"_ada_o%u:%u",
 			dev->data->dev_id, q_id);
 	if ((ret < 0) || (ret >= (int)RTE_RING_NAMESIZE)) {
 		rte_bbdev_log(ERR,