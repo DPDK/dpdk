97f5f8cbadbeb47cc283251564e56a6c20ceec00
Jerin Jacob <jerin.jacob@caviumnetworks.com>
2018-06-29 15:14:43 +0530

    ethdev: fix queue statistics mapping documentation
    The RTE_MAX_ETHPORT_QUEUE_STATS_MAPS does not exists, change
    to the correct definition(RTE_ETHDEV_QUEUE_STAT_CNTRS)
    
    Fixes: 5de201df8927 ("ethdev: add stats per queue")
    Cc: stable@dpdk.org
    
    Signed-off-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>
    Reviewed-by: Andrew Rybchenko <arybchenko@solarflare.com>

--- lib/librte_ethdev/rte_ethdev.h
+++ lib/librte_ethdev/rte_ethdev.h
@@ -2117,7 +2117,7 @@ void rte_eth_xstats_reset(uint16_t port_id);
  * @param stat_idx
  *   The per-queue packet statistics functionality number that the transmit
  *   queue is to be assigned.
- *   The value must be in the range [0, RTE_MAX_ETHPORT_QUEUE_STATS_MAPS - 1].
+ *   The value must be in the range [0, RTE_ETHDEV_QUEUE_STAT_CNTRS - 1].
  * @return
  *   Zero if successful. Non-zero otherwise.
  */
@@ -2137,7 +2137,7 @@ int rte_eth_dev_set_tx_queue_stats_mapping(uint16_t port_id,
  * @param stat_idx
  *   The per-queue packet statistics functionality number that the receive
  *   queue is to be assigned.
- *   The value must be in the range [0, RTE_MAX_ETHPORT_QUEUE_STATS_MAPS - 1].
+ *   The value must be in the range [0, RTE_ETHDEV_QUEUE_STAT_CNTRS - 1].
  * @return
  *   Zero if successful. Non-zero otherwise.
  */
