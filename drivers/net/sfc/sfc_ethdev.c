b7294d8808fd0591a29edc33c70794acd378db84
Andrew Rybchenko <arybchenko@solarflare.com>
2018-07-15 10:56:14 +0100

    net/sfc: fix Rx queue offloads reporting in queue info
    CRC_STRIP offload is always enabled, but lost here.
    
    It is more robust to use device level offloads directly to
    compose returned value.
    
    Fixes: ff6a1197c3b1 ("net/sfc: convert to new Rx offload API")
    Cc: stable@dpdk.org
    
    Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>
    Reviewed-by: Ivan Malov <ivan.malov@oktetlabs.ru>

--- drivers/net/sfc/sfc_ethdev.c
+++ drivers/net/sfc/sfc_ethdev.c
@@ -1055,9 +1055,7 @@ sfc_rx_queue_info_get(struct rte_eth_dev *dev, uint16_t rx_queue_id,
 	qinfo->conf.rx_free_thresh = rxq->refill_threshold;
 	qinfo->conf.rx_drop_en = 1;
 	qinfo->conf.rx_deferred_start = rxq_info->deferred_start;
-	qinfo->conf.offloads = DEV_RX_OFFLOAD_IPV4_CKSUM |
-			       DEV_RX_OFFLOAD_UDP_CKSUM |
-			       DEV_RX_OFFLOAD_TCP_CKSUM;
+	qinfo->conf.offloads = dev->data->dev_conf.rxmode.offloads;
 	if (rxq_info->type_flags & EFX_RXQ_FLAG_SCATTER) {
 		qinfo->conf.offloads |= DEV_RX_OFFLOAD_SCATTER;
 		qinfo->scattered_rx = 1;
