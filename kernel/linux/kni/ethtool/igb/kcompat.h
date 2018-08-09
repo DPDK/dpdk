3639c91df43d7cfa29cf4d419a2c56c45f6fcf26
Drocula Lambda <quzeyao@gmail.com>
2018-08-09 12:09:06 +0000

    kni: fix build on RHEL 7.5
    This patch fixes compilation errors on Centos 7.5 when
    CONFIG_RTE_KNI_KMOD_ETHTOOL is set to 'y'.
    On RHEL75 ndo_change_mtu has changed to ndo_change_mtu_rh74.
    
    See commit 37d477b6863e5c06 ("kni: fix build on RHEL 7.5")
    
    Signed-off-by: Drocula Lambda <quzeyao@gmail.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- kernel/linux/kni/ethtool/igb/kcompat.h
+++ kernel/linux/kni/ethtool/igb/kcompat.h
@@ -3929,6 +3929,11 @@ skb_set_hash(struct sk_buff *skb, __u32 hash, __always_unused int type)
 #endif
 #endif
 
+#if (defined(RHEL_RELEASE_CODE) && \
+	(RHEL_RELEASE_VERSION(7, 5) <= RHEL_RELEASE_CODE))
+#define ndo_change_mtu ndo_change_mtu_rh74
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 8, 0)
 #define HAVE_PCI_ENABLE_MSIX
 #endif
