6a3e44717d258c15a2312b989c327b544fe33fa4
Fiona Trahe <fiona.trahe@intel.com>
2018-08-10 16:18:39 +0100

    crypto/qat: fix typo
    Fixes: f9a3d7f68f05 ("crypto/qat: move code into appropriate files")
    
    Signed-off-by: Fiona Trahe <fiona.trahe@intel.com>

--- drivers/crypto/qat/qat_sym_pmd.h
+++ drivers/crypto/qat/qat_sym_pmd.h
@@ -12,7 +12,7 @@
 #include "qat_sym_capabilities.h"
 #include "qat_device.h"
 
-/**< Intel(R) QAT Symmetric Crypto PMD device name */
+/** Intel(R) QAT Symmetric Crypto PMD driver name */
 #define CRYPTODEV_NAME_QAT_SYM_PMD	crypto_qat
 
 extern uint8_t cryptodev_qat_driver_id;
