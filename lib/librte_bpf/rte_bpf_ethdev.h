2b4aa851ec18ccb0c16ed379528ddff36bcb5cff
Rami Rosen <ramirose@gmail.com>
2018-08-22 17:45:45 +0300

    bpf: fix a typo
    This trivial patch fixes a typo in rte_bpf_ethdev.h,
    
    Fixes: a93ff62a8938 ("bpf: introduce basic Rx/Tx filters")
    Cc: stable@dpdk.org
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>

--- lib/librte_bpf/rte_bpf_ethdev.h
+++ lib/librte_bpf/rte_bpf_ethdev.h
@@ -75,7 +75,7 @@ rte_bpf_eth_tx_unload(uint16_t port, uint16_t queue);
  * @param prm
  *  Parameters used to create and initialise the BPF exeution context.
  * @param flags
- *  Flags that define expected expected behavior of the loaded filter
+ *  Flags that define expected behavior of the loaded filter
  *  (i.e. jited/non-jited version to use).
  * @return
  *   Zero on successful completion or negative error code otherwise.
