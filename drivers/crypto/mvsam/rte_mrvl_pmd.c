7af56296d749ed9d5dd44489f6ade16e80ae0691
Tomasz Duszynski <tdu@semihalf.com>
2018-09-04 15:59:49 +0200

    crypto/mvsam: update features list
    Add following features to the device's features list
    and update documentation accordingly:
    * OOP SGL in LB out
    * OOP LB in LB out
    
    Signed-off-by: Tomasz Duszynski <tdu@semihalf.com>

--- drivers/crypto/mvsam/rte_mrvl_pmd.c
+++ drivers/crypto/mvsam/rte_mrvl_pmd.c
@@ -729,7 +729,9 @@ cryptodev_mrvl_crypto_create(const char *name,
 
 	dev->feature_flags = RTE_CRYPTODEV_FF_SYMMETRIC_CRYPTO |
 			RTE_CRYPTODEV_FF_SYM_OPERATION_CHAINING |
-			RTE_CRYPTODEV_FF_HW_ACCELERATED;
+			RTE_CRYPTODEV_FF_HW_ACCELERATED |
+			RTE_CRYPTODEV_FF_OOP_SGL_IN_LB_OUT |
+			RTE_CRYPTODEV_FF_OOP_LB_IN_LB_OUT;
 
 	/* Set vector instructions mode supported */
 	internals = dev->data->dev_private;
