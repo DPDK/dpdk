d0e22de5bd4da4c28dafc4b09e9631b37d302b21
Martin Harvey <mharvey@solarflare.com>
2018-09-10 10:33:04 +0100

    net/sfc/base: fix SAL annotation for input buffers
    Fixes: d96a34d165b1 ("net/sfc/base: import NVRAM support")
    Cc: stable@dpdk.org
    
    Signed-off-by: Martin Harvey <mharvey@solarflare.com>
    Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>

--- drivers/net/sfc/base/ef10_impl.h
+++ drivers/net/sfc/base/ef10_impl.h
@@ -453,7 +453,7 @@ ef10_nvram_partn_write(
 	__in			efx_nic_t *enp,
 	__in			uint32_t partn,
 	__in			unsigned int offset,
-	__out_bcount(size)	caddr_t data,
+	__in_bcount(size)	caddr_t data,
 	__in			size_t size);
 
 extern	__checkReturn		efx_rc_t
