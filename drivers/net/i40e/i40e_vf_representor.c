c8dc2d66c547daf5054557b485eff3fdc0f87864
Didier Pallard <didier.pallard@6wind.com>
2018-09-19 17:04:08 +0200

    net/i40e: fix missing Tx multi-segs capability
    In former API, ETH_TXQ_FLAGS_NOMULTSEGS was merely a hint indicating
    that application will never send multisegmented packets, allowing
    pmd to choose different tx methods accordingly.
    In new API, DEV_TX_OFFLOAD_MULTI_SEGS became an offload capability
    that is advertised by pmds, some of them do not advertise it and
    expect to never receive fragmented packets (octeontx, axgbe)
    So an ethdev that supports multisegmented packets should properly
    advertise it.
    
    Problem was spotted and tested on e1000, should be also present in
    i40e_vf representor.
    
    Fixes: e0cb96204b71 ("net/i40e: add support for representor ports")
    Cc: stable@dpdk.org
    
    Signed-off-by: Didier Pallard <didier.pallard@6wind.com>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>

--- drivers/net/i40e/i40e_vf_representor.c
+++ drivers/net/i40e/i40e_vf_representor.c
@@ -48,6 +48,7 @@ i40e_vf_representor_dev_infos_get(struct rte_eth_dev *ethdev,
 		DEV_RX_OFFLOAD_UDP_CKSUM |
 		DEV_RX_OFFLOAD_TCP_CKSUM;
 	dev_info->tx_offload_capa =
+		DEV_TX_OFFLOAD_MULTI_SEGS  |
 		DEV_TX_OFFLOAD_VLAN_INSERT |
 		DEV_TX_OFFLOAD_QINQ_INSERT |
 		DEV_TX_OFFLOAD_IPV4_CKSUM |
