d3f37c2222e59edbef87921290694299249bd24e
Tomasz Duszynski <tdu@semihalf.com>
2018-09-21 16:53:59 +0200

    crypto/mvsam: add 3DES ECB to the capabilities list
    3DES in ECB mode is supported by the PMD thus specific
    entry should exist in the crypto PMD capabilities list.
    
    Signed-off-by: Tomasz Duszynski <tdu@semihalf.com>
    Reviewed-by: Natalie Samsonov <nsamsono@marvell.com>

--- drivers/crypto/mvsam/rte_mrvl_pmd_ops.c
+++ drivers/crypto/mvsam/rte_mrvl_pmd_ops.c
@@ -413,6 +413,26 @@ static const struct rte_cryptodev_capabilities
 			}, }
 		}, }
 	},
+	{	/* 3DES ECB */
+		.op = RTE_CRYPTO_OP_TYPE_SYMMETRIC,
+		{.sym = {
+			.xform_type = RTE_CRYPTO_SYM_XFORM_CIPHER,
+			{.cipher = {
+				.algo = RTE_CRYPTO_CIPHER_3DES_ECB,
+				.block_size = 8,
+				.key_size = {
+					.min = 24,
+					.max = 24,
+					.increment = 0
+				},
+				.iv_size = {
+					.min = 0,
+					.max = 0,
+					.increment = 0
+				}
+			}, }
+		}, }
+	},
 	{	/* NULL (AUTH) */
 		.op = RTE_CRYPTO_OP_TYPE_SYMMETRIC,
 		{.sym = {
