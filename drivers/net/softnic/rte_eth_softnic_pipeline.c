e976ffaa5714792dc868be80fa2d4b0a2e45df48
Kevin Laatz <kevin.laatz@intel.com>
2018-09-25 16:32:30 +0100

    net/softnic: use table library headers
    This commit modifies SoftNIC to make use of the new header files in
    librte_table.
    
    As we are now using the new header files in librte_table in SoftNIC, we no
    longer need the old header files so they can be removed.
    
    Signed-off-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>
    Signed-off-by: Kevin Laatz <kevin.laatz@intel.com>
    Acked-by: Gavin Hu <gavin.hu@arm.com>
    Acked-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>

--- drivers/net/softnic/rte_eth_softnic_pipeline.c
+++ drivers/net/softnic/rte_eth_softnic_pipeline.c
@@ -19,13 +19,12 @@
 #include <rte_table_acl.h>
 #include <rte_table_array.h>
 #include <rte_table_hash.h>
+#include <rte_table_hash_func.h>
 #include <rte_table_lpm.h>
 #include <rte_table_lpm_ipv6.h>
 #include <rte_table_stub.h>
 
 #include "rte_eth_softnic_internals.h"
-
-#include "hash_func.h"
 
 #ifndef PIPELINE_MSGQ_SIZE
 #define PIPELINE_MSGQ_SIZE                                 64
@@ -797,28 +796,28 @@ softnic_pipeline_table_create(struct pmd_internals *softnic,
 
 		switch (params->match.hash.key_size) {
 		case  8:
-			f_hash = hash_default_key8;
+			f_hash = rte_table_hash_crc_key8;
 			break;
 		case 16:
-			f_hash = hash_default_key16;
+			f_hash = rte_table_hash_crc_key16;
 			break;
 		case 24:
-			f_hash = hash_default_key24;
+			f_hash = rte_table_hash_crc_key24;
 			break;
 		case 32:
-			f_hash = hash_default_key32;
+			f_hash = rte_table_hash_crc_key32;
 			break;
 		case 40:
-			f_hash = hash_default_key40;
+			f_hash = rte_table_hash_crc_key40;
 			break;
 		case 48:
-			f_hash = hash_default_key48;
+			f_hash = rte_table_hash_crc_key48;
 			break;
 		case 56:
-			f_hash = hash_default_key56;
+			f_hash = rte_table_hash_crc_key56;
 			break;
 		case 64:
-			f_hash = hash_default_key64;
+			f_hash = rte_table_hash_crc_key64;
 			break;
 		default:
 			return -1;
