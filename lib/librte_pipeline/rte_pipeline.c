5675c2ea15bb8cacb3b37ac17eceacbd0b80bdce
Anatoly Burakov <anatoly.burakov@intel.com>
2018-10-02 14:34:44 +0100

    pipeline: do not check for invalid socket ID
    We will be assigning "invalid" socket ID's to external heap, and
    malloc will now be able to verify if a supplied socket ID is in
    fact a valid one, rendering parameter checks for sockets
    obsolete.
    
    Signed-off-by: Anatoly Burakov <anatoly.burakov@intel.com>
    Acked-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>

--- lib/librte_pipeline/rte_pipeline.c
+++ lib/librte_pipeline/rte_pipeline.c
@@ -178,8 +178,7 @@ rte_pipeline_check_params(struct rte_pipeline_params *params)
 	}
 
 	/* socket */
-	if ((params->socket_id < 0) ||
-	    (params->socket_id >= RTE_MAX_NUMA_NODES)) {
+	if (params->socket_id < 0) {
 		RTE_LOG(ERR, PIPELINE,
 			"%s: Incorrect value for parameter socket_id\n",
 			__func__);
