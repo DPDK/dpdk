DPDK/dpdk

commit 63ab5e0c8fdad1ce773f31a71bba22f5b8126479

net/sfc: use zero RSS channels as disabled RSS indicator
Enabled RSS enables RSS hash computation and provision in pseudo header.
It still makes sense for applications even if only one Rx queue is used.

Fixes: 4ec1fc3ba881 ("net/sfc: add basic stubs for RSS support on driver attach")
Fixes: 088e17210a7a ("net/sfc: query RSS key and hash types config")
Fixes: 82faef507608 ("net/sfc: set RSS key and hash types config")
Fixes: af0d9317970c ("net/sfc: query RSS redirection table")
Fixes: 32bcfb0a50b1 ("net/sfc: update RSS redirection table")
Fixes: f5258439ee5d ("net/sfc: avoid failure on port start if Rx mode is rejected")

Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>
Reviewed-by: Andy Moreton <amoreton@solarflare.com>

drivers/net/sfc/sfc_ethdev.c
@@ -1084,9 +1084,11 @@ sfc_dev_rss_hash_conf_get(struct rte_eth_dev *dev,
 {
 	struct sfc_adapter *sa = dev->data->dev_private;
 
-	if ((sa->rss_channels == 1) ||
-	    (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE))
+	if (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE)
 		return -ENOTSUP;
+
+	if (sa->rss_channels == 0)
+		return -EINVAL;
 
 	sfc_adapter_lock(sa);
 
@@ -1114,10 +1116,14 @@ sfc_dev_rss_hash_update(struct rte_eth_dev *dev,
 	unsigned int efx_hash_types;
 	int rc = 0;
 
-	if ((sa->rss_channels == 1) ||
-	    (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE)) {
+	if (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE) {
 		sfc_err(sa, "RSS is not available");
 		return -ENOTSUP;
+	}
+
+	if (sa->rss_channels == 0) {
+		sfc_err(sa, "RSS is not configured");
+		return -EINVAL;
 	}
 
 	if ((rss_conf->rss_key != NULL) &&
@@ -1176,9 +1182,11 @@ sfc_dev_rss_reta_query(struct rte_eth_dev *dev,
 	struct sfc_adapter *sa = dev->data->dev_private;
 	int entry;
 
-	if ((sa->rss_channels == 1) ||
-	    (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE))
+	if (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE)
 		return -ENOTSUP;
+
+	if (sa->rss_channels == 0)
+		return -EINVAL;
 
 	if (reta_size != EFX_RSS_TBL_SIZE)
 		return -EINVAL;
@@ -1209,10 +1217,14 @@ sfc_dev_rss_reta_update(struct rte_eth_dev *dev,
 	int rc;
 
 
-	if ((sa->rss_channels == 1) ||
-	    (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE)) {
+	if (sa->rss_support != EFX_RX_SCALE_EXCLUSIVE) {
 		sfc_err(sa, "RSS is not available");
 		return -ENOTSUP;
+	}
+
+	if (sa->rss_channels == 0) {
+		sfc_err(sa, "RSS is not configured");
+		return -EINVAL;
 	}
 
 	if (reta_size != EFX_RSS_TBL_SIZE) {

drivers/net/sfc/sfc_rx.c
@@ -577,7 +577,7 @@ sfc_rx_qflush(struct sfc_adapter *sa, unsigned int sw_index)
 static int
 sfc_rx_default_rxq_set_filter(struct sfc_adapter *sa, struct sfc_rxq *rxq)
 {
-	boolean_t rss = (sa->rss_channels > 1) ? B_TRUE : B_FALSE;
+	boolean_t rss = (sa->rss_channels > 0) ? B_TRUE : B_FALSE;
 	struct sfc_port *port = &sa->port;
 	int rc;
 
@@ -1052,7 +1052,7 @@ sfc_rx_rss_config(struct sfc_adapter *sa)
 	int rc = 0;
 
 #if EFSYS_OPT_RX_SCALE
-	if (sa->rss_channels > 1) {
+	if (sa->rss_channels > 0) {
 		rc = efx_rx_scale_mode_set(sa->nic, EFX_RX_HASHALG_TOEPLITZ,
 					   sa->rss_hash_types, B_TRUE);
 		if (rc != 0)
@@ -1289,9 +1289,9 @@ sfc_rx_configure(struct sfc_adapter *sa)
 
 #if EFSYS_OPT_RX_SCALE
 	sa->rss_channels = (dev_conf->rxmode.mq_mode == ETH_MQ_RX_RSS) ?
-			   MIN(sa->rxq_count, EFX_MAXRSS) : 1;
+			   MIN(sa->rxq_count, EFX_MAXRSS) : 0;
 
-	if (sa->rss_channels > 1) {
+	if (sa->rss_channels > 0) {
 		for (sw_index = 0; sw_index < EFX_RSS_TBL_SIZE; ++sw_index)
 			sa->rss_tbl[sw_index] = sw_index % sa->rss_channels;
 	}

commit 662e382244fb29d8bf03113524a139541e10123f

net/sfc: allow to query RSS key and HF when RSS is disabled
If global RSS is not enabled in the multiqueue mode setting,
it will not be possible to change RSS configuration. However,
querying default RSS settings should be possible in any case since
it may be needed by RTE flow API users to find out what RSS settings
will be used by default for a flow rule with RSS action if custom
RSS key and hash function choice are not specified.

Fixes: 63ab5e0c8fda ("net/sfc: use zero RSS channels as disabled RSS indicator")
Cc: stable@dpdk.org

Signed-off-by: Ivan Malov <ivan.malov@oktetlabs.ru>
Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>

drivers/net/sfc/sfc_ethdev.c
@@ -1358,9 +1358,6 @@ sfc_dev_rss_hash_conf_get(struct rte_eth_dev *dev,
 	if (rss->context_type != EFX_RX_SCALE_EXCLUSIVE)
 		return -ENOTSUP;
 
-	if (rss->channels == 0)
-		return -EINVAL;
-
 	sfc_adapter_lock(sa);
 
 	/*