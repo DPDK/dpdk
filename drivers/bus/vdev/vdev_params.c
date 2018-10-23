2c29f879f2e06a792b5ee280cb0fe522d924f5b0
Thomas Monjalon <thomas@monjalon.net>
2018-10-23 10:28:36 +0200

    bus/vdev: add iteration filter on name
    A virtual device can be matched with following syntax:
    	bus=vdev,name=X
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Reviewed-by: Andrew Rybchenko <arybchenko@solarflare.com>

--- drivers/bus/vdev/vdev_params.c
+++ drivers/bus/vdev/vdev_params.c
@@ -2,6 +2,8 @@
  * Copyright 2018 Gaëtan Rivet
  */
 
+#include <string.h>
+
 #include <rte_dev.h>
 #include <rte_bus.h>
 #include <rte_kvargs.h>
@@ -11,10 +13,12 @@
 #include "vdev_private.h"
 
 enum vdev_params {
+	RTE_VDEV_PARAM_NAME,
 	RTE_VDEV_PARAM_MAX,
 };
 
 static const char * const vdev_params_keys[] = {
+	[RTE_VDEV_PARAM_NAME] = "name",
 	[RTE_VDEV_PARAM_MAX] = NULL,
 };
 
@@ -22,10 +26,21 @@ static int
 vdev_dev_match(const struct rte_device *dev,
 	       const void *_kvlist)
 {
+	int ret;
 	const struct rte_kvargs *kvlist = _kvlist;
+	char *name;
 
-	(void) kvlist;
-	(void) dev;
+	/* cannot pass const dev->name to rte_kvargs_process() */
+	name = strdup(dev->name);
+	if (name == NULL)
+		return -1;
+	ret = rte_kvargs_process(kvlist,
+		vdev_params_keys[RTE_VDEV_PARAM_NAME],
+		rte_kvargs_strcmp, name);
+	free(name);
+	if (ret != 0)
+		return -1;
+
 	return 0;
 }
 
