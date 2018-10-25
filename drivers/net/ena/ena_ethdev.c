e5df9f33db00eb9d322abaefff30da74fd0e625d
Stewart Allen <allenste@amazon.com>
2018-10-25 19:59:22 +0200

    net/ena: fix passing RSS hash to mbuf
    The driver was passing to the mbuf Rx queue ID instead of hash received
    from the device. Now, the RSS hash from the Rx descriptor is being set.
    
    Fixes: 1173fca25af9 ("ena: add polling-mode driver")
    Cc: stable@dpdk.org
    
    Signed-off-by: Stewart Allen <allenste@amazon.com>
    Acked-by: Michal Krawczyk <mk@semihalf.com>

--- drivers/net/ena/ena_ethdev.c
+++ drivers/net/ena/ena_ethdev.c
@@ -1910,7 +1910,7 @@ static uint16_t eth_ena_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts,
 
 		/* fill mbuf attributes if any */
 		ena_rx_mbuf_prepare(mbuf_head, &ena_rx_ctx);
-		mbuf_head->hash.rss = (uint32_t)rx_ring->id;
+		mbuf_head->hash.rss = ena_rx_ctx.hash;
 
 		/* pass to DPDK application head mbuf */
 		rx_pkts[recv_idx] = mbuf_head;
