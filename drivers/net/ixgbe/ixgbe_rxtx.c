aa1e69aebb12a57038f7ed5664605b7bcccbbe20
Zhirun Yan <zhirun.yan@intel.com>
2018-10-25 14:58:40 +0000

    net/ixgbe: update Tx offload mask
    Tx offload mask is updated in following commit: commit 1037ed842c37
    ("mbuf: fix Tx offload mask"). Currently, the new added offload
    flags are not supported in PMD and application will fail to call
    PMD transmit prepare function.
    
    This patch updates IXGBE_TX_OFFFLOAD_MASK.
    
    Fixes: 1037ed842c37 ("mbuf: fix Tx offload mask")
    Cc: stable@dpdk.org
    
    Signed-off-by: Zhirun Yan <zhirun.yan@intel.com>
    Reviewed-by: Xiaolong Ye <xiaolong.ye@intel.com>

--- drivers/net/ixgbe/ixgbe_rxtx.c
+++ drivers/net/ixgbe/ixgbe_rxtx.c
@@ -58,6 +58,10 @@
 #endif
 /* Bit Mask to indicate what bits required for building TX context */
 #define IXGBE_TX_OFFLOAD_MASK (			 \
+		PKT_TX_OUTER_IPV6 |		 \
+		PKT_TX_OUTER_IPV4 |		 \
+		PKT_TX_IPV6 |			 \
+		PKT_TX_IPV4 |			 \
 		PKT_TX_VLAN_PKT |		 \
 		PKT_TX_IP_CKSUM |		 \
 		PKT_TX_L4_MASK |		 \
