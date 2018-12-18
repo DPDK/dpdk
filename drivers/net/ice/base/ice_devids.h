6aa406714a6552bb70993e4ee02d1782b3768df5
Paul M Stillwell Jr <paul.m.stillwell.jr@intel.com>
2018-12-18 16:46:14 +0800

    net/ice/base: add device IDs for Intel E800 Series NICs
    Add all the device IDs that represent the NIC.
    
    Signed-off-by: Paul M Stillwell Jr <paul.m.stillwell.jr@intel.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

---
+++ drivers/net/ice/base/ice_devids.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2001-2018
+ */
+
+#ifndef _ICE_DEVIDS_H_
+#define _ICE_DEVIDS_H_
+
+
+/* Device IDs */
+/* Intel(R) Ethernet Controller E810-C for backplane */
+#define ICE_DEV_ID_E810C_BACKPLANE	0x1591
+/* Intel(R) Ethernet Controller E810-C for QSFP */
+#define ICE_DEV_ID_E810C_QSFP		0x1592
+/* Intel(R) Ethernet Controller E810-C for SFP */
+#define ICE_DEV_ID_E810C_SFP		0x1593
+
+#endif /* _ICE_DEVIDS_H_ */
