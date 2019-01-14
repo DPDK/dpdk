12d468a62bc19ca08ee9964dcb923f67f87fba7d
Yongseok Koh <yskoh@mellanox.com>
2019-01-14 13:16:22 -0800

    net/mlx5: fix instruction hotspot on replenishing Rx buffer
    On replenishing Rx buffers for vectorized Rx, mbuf->buf_addr isn't needed
    to be accessed as it is static and easily calculated from the mbuf address.
    Accessing the mbuf content causes unnecessary load stall and it is worsened
    on ARM.
    
    Fixes: 545b884b1da3 ("net/mlx5: fix buffer address posting in SSE Rx")
    Cc: stable@dpdk.org
    
    Signed-off-by: Yongseok Koh <yskoh@mellanox.com>
    Acked-by: Shahaf Shuler <shahafs@mellanox.com>

--- drivers/net/mlx5/mlx5_rxtx_vec.h
+++ drivers/net/mlx5/mlx5_rxtx_vec.h
@@ -102,7 +102,10 @@ mlx5_rx_replenish_bulk_mbuf(struct mlx5_rxq_data *rxq, uint16_t n)
 		return;
 	}
 	for (i = 0; i < n; ++i) {
-		wq[i].addr = rte_cpu_to_be_64((uintptr_t)elts[i]->buf_addr +
+		void *buf_addr = rte_mbuf_buf_addr(elts[i], rxq->mp);
+
+		assert(buf_addr == elts[i]->buf_addr);
+		wq[i].addr = rte_cpu_to_be_64((uintptr_t)buf_addr +
 					      RTE_PKTMBUF_HEADROOM);
 		/* If there's only one MR, no need to replace LKey in WQE. */
 		if (unlikely(mlx5_mr_btree_len(&rxq->mr_ctrl.cache_bh) > 1))
