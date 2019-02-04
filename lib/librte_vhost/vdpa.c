7c7b7562252742a6c298de64df486873336fd058
Pallantla Poornima <pallantlax.poornima@intel.com>
2019-02-04 07:28:06 +0000

    vhost: fix sprintf with snprintf
    sprintf function is not secure as it doesn't check the length of string.
    More secure function snprintf is used.
    
    Fixes: d7280c9fffcb ("vhost: support selective datapath")
    Cc: stable@dpdk.org
    
    Signed-off-by: Pallantla Poornima <pallantlax.poornima@intel.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Acked-by: Tiwei Bie <tiwei.bie@intel.com>

--- lib/librte_vhost/vdpa.c
+++ lib/librte_vhost/vdpa.c
@@ -66,7 +66,7 @@ rte_vdpa_register_device(struct rte_vdpa_dev_addr *addr,
 	if (i == MAX_VHOST_DEVICE)
 		return -1;
 
-	sprintf(device_name, "vdpa-dev-%d", i);
+	snprintf(device_name, sizeof(device_name), "vdpa-dev-%d", i);
 	dev = rte_zmalloc(device_name, sizeof(struct rte_vdpa_device),
 			RTE_CACHE_LINE_SIZE);
 	if (!dev)
