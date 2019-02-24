09c9c4d23dc764bf833e432c8f3d5fae612e18b7
Thomas Monjalon <thomas@monjalon.net>
2019-02-24 23:42:40 +0100

    net/mlx5: call generic strlcpy
    The call to strlcpy uses either libc, libbsd or internal rte_strlcpy.
    No need to call the DPDK flavor explicitly.
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Reviewed-by: Rami Rosen <ramirose@gmail.com>
    Acked-by: Shahaf Shuler <shahafs@mellanox.com>

--- drivers/net/mlx5/mlx5.c
+++ drivers/net/mlx5/mlx5.c
@@ -785,7 +785,7 @@ mlx5_dev_spawn(struct rte_device *dpdk_dev,
 	}
 	/* Build device name. */
 	if (!switch_info->representor)
-		rte_strlcpy(name, dpdk_dev->name, sizeof(name));
+		strlcpy(name, dpdk_dev->name, sizeof(name));
 	else
 		snprintf(name, sizeof(name), "%s_representor_%u",
 			 dpdk_dev->name, switch_info->port_name);
