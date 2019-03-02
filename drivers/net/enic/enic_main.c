af3a16282aaae67224f0220a29f663b8b1b0fb0a
Hyong Youb Kim <hyonkim@cisco.com>
2019-03-02 02:42:48 -0800

    net/enic: reset VXLAN port regardless of overlay offload
    Currently, the driver resets the vxlan port register only if overlay
    offload is enabled. But, the register is actually tied to hardware
    vxlan parsing, which is an independent feature and is always enabled
    even if overlay offload is disabled. If left uninitialized, it can
    affect flow rules that match vxlan. So always reset the port number
    when HW vxlan parsing is available.
    
    Fixes: 8a4efd17410c ("net/enic: add handlers to add/delete vxlan port number")
    Cc: stable@dpdk.org
    
    Signed-off-by: Hyong Youb Kim <hyonkim@cisco.com>

--- drivers/net/enic/enic_main.c
+++ drivers/net/enic/enic_main.c
@@ -1714,8 +1714,15 @@ static int enic_dev_init(struct enic *enic)
 			PKT_TX_OUTER_IP_CKSUM |
 			PKT_TX_TUNNEL_MASK;
 		enic->overlay_offload = true;
-		enic->vxlan_port = ENIC_DEFAULT_VXLAN_PORT;
 		dev_info(enic, "Overlay offload is enabled\n");
+	}
+	/*
+	 * Reset the vxlan port if HW vxlan parsing is available. It
+	 * is always enabled regardless of overlay offload
+	 * enable/disable.
+	 */
+	if (enic->vxlan) {
+		enic->vxlan_port = ENIC_DEFAULT_VXLAN_PORT;
 		/*
 		 * Reset the vxlan port to the default, as the NIC firmware
 		 * does not reset it automatically and keeps the old setting.
