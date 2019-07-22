DPDK/dpdk

commit 6b7780bfebe4de743c5edbe7a3c7f9fa817789ad

examples/ip_frag: fix use of ethdev internal device array
Modify ip_fragmentation example app to use rte_eth_info_get instead
of global rte_eth_devices structure.
Apps should not be using internal DPDK data structures directly.

Fixes: 9758b956dcf4 ("examples/ip_fragmentation: fix Tx queues init")
Cc: stable@dpdk.org

Signed-off-by: Marcin Zapolski <marcinx.a.zapolski@intel.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

examples/ip_fragmentation/main.c
@@ -991,7 +991,7 @@ main(int argc, char **argv)
 			if (rte_lcore_is_enabled(lcore_id) == 0)
 				continue;
 
-			if (queueid >= rte_eth_devices[portid].data->nb_tx_queues)
+			if (queueid >= dev_info.nb_tx_queues)
 				break;
 
 			socket = (int) rte_lcore_to_socket_id(lcore_id);

commit f0f96b472ca72ede5cda905b38c0dd95d1948363

examples/ip_frag: fix stale content of ethdev info
The eth_dev_info was used with content that was obsolete. Added update
of struct content prior to use.

Fixes: 6b7780bfebe4 ("examples/ip_frag: fix use of ethdev internal device array")
Cc: stable@dpdk.org

Signed-off-by: Marcin Zapolski <marcinx.a.zapolski@intel.com>
Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>

examples/ip_fragmentation/main.c
@@ -987,6 +987,7 @@ main(int argc, char **argv)
 		printf("\n");
 
 		/* init one TX queue per couple (lcore,port) */
+		rte_eth_dev_info_get(portid, &dev_info);
 		queueid = 0;
 		for (lcore_id = 0; lcore_id < RTE_MAX_LCORE; lcore_id++) {
 			if (rte_lcore_is_enabled(lcore_id) == 0)