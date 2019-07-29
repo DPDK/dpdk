DPDK/dpdk

commit debacba0297fbe214b4185a9791e6a9fdf6642ba

eal: fix parsing option --telemetry
Added telemetry to EAL long options so that when
--telemetry is passed as an EAL arg that there is
no unrecognized argument error message printed.

Fixes: 8877ac688b52 ("telemetry: introduce infrastructure")
Cc: stable@dpdk.org

Signed-off-by: Sean Morrissey <sean.morrissey@intel.com>
Tested-by: John OLoughlin <john.oloughlin@intel.com>
Acked-by: Kevin Laatz <kevin.laatz@intel.com>

lib/librte_eal/common/eal_common_options.c
@@ -81,6 +81,9 @@ eal_long_options[] = {
 	{OPT_LEGACY_MEM,        0, NULL, OPT_LEGACY_MEM_NUM       },
 	{OPT_SINGLE_FILE_SEGMENTS, 0, NULL, OPT_SINGLE_FILE_SEGMENTS_NUM},
 	{OPT_MATCH_ALLOCATIONS, 0, NULL, OPT_MATCH_ALLOCATIONS_NUM},
+#ifdef RTE_LIBRTE_TELEMETRY
+	{OPT_TELEMETRY,         0, NULL, OPT_TELEMETRY_NUM        },
+#endif
 	{0,                     0, NULL, 0                        }
 };
 

lib/librte_eal/common/eal_options.h
@@ -69,6 +69,10 @@ enum {
 	OPT_IOVA_MODE_NUM,
 #define OPT_MATCH_ALLOCATIONS  "match-allocations"
 	OPT_MATCH_ALLOCATIONS_NUM,
+#ifdef RTE_LIBRTE_TELEMETRY
+	#define OPT_TELEMETRY          "telemetry"
+		OPT_TELEMETRY_NUM,
+#endif
 	OPT_LONG_MAX_NUM
 };
 

commit 9f83b62f555998d7bc85400c3d5ea828f50919cf

eal: revert parsing option --telemetry
This reverts commit debacba0297fbe214b4185a9791e6a9fdf6642ba.

Reverting this patch as it currently breaks the initialization of
telemetry, more investigation is ongoing to fix the issue for the
printed error message for unrecognized argument.

Fixes: debacba0297f ("eal: fix parsing option --telemetry")
Cc: stable@dpdk.org

Signed-off-by: Sean Morrissey <sean.morrissey@intel.com>

lib/librte_eal/common/eal_common_options.c
@@ -81,9 +81,6 @@ eal_long_options[] = {
 	{OPT_LEGACY_MEM,        0, NULL, OPT_LEGACY_MEM_NUM       },
 	{OPT_SINGLE_FILE_SEGMENTS, 0, NULL, OPT_SINGLE_FILE_SEGMENTS_NUM},
 	{OPT_MATCH_ALLOCATIONS, 0, NULL, OPT_MATCH_ALLOCATIONS_NUM},
-#ifdef RTE_LIBRTE_TELEMETRY
-	{OPT_TELEMETRY,         0, NULL, OPT_TELEMETRY_NUM        },
-#endif
 	{0,                     0, NULL, 0                        }
 };
 

lib/librte_eal/common/eal_options.h
@@ -69,10 +69,6 @@ enum {
 	OPT_IOVA_MODE_NUM,
 #define OPT_MATCH_ALLOCATIONS  "match-allocations"
 	OPT_MATCH_ALLOCATIONS_NUM,
-#ifdef RTE_LIBRTE_TELEMETRY
-	#define OPT_TELEMETRY          "telemetry"
-		OPT_TELEMETRY_NUM,
-#endif
 	OPT_LONG_MAX_NUM
 };
 