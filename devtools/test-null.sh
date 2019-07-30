d879f5750f63b35bd40144bc0f2c832a59713810
Thomas Monjalon <thomas@monjalon.net>
2019-07-30 19:34:14 +0200

    devtools: allow misc options in null test
    In order to ease basic testing with customized options,
    EAL and testpmd options can be added as third and fourth arguments
    of the "null PMD" script.
    
    Also, the first argument becomes more flexible by accepting
    the testpmd path as an alternative to the build directory.
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>

--- devtools/test-null.sh
+++ devtools/test-null.sh
@@ -5,10 +5,13 @@
 
 # Run a quick testpmd forwarding with null PMD without hugepage
 
-build=${1:-build}
+build=${1:-build} # first argument can be the build directory
+testpmd=$1 # or first argument can be the testpmd path
 coremask=${2:-3} # default using cores 0 and 1
+eal_options=$3
+testpmd_options=$4
 
-testpmd=$build/app/dpdk-testpmd
+[ -f "$testpmd" ] || testpmd=$build/app/dpdk-testpmd
 [ -f "$testpmd" ] || testpmd=$build/app/testpmd
 if [ ! -f "$testpmd" ] ; then
 	echo 'ERROR: testpmd cannot be found' >&2
@@ -23,5 +26,5 @@ fi
 
 (sleep 1 && echo stop) |
 $testpmd -c $coremask --no-huge -m 150 \
-	$libs --vdev net_null1 --vdev net_null2 -- \
-	--no-mlockall --total-num-mbufs=2048 -ia
+	$libs --vdev net_null1 --vdev net_null2 $eal_options -- \
+	--no-mlockall --total-num-mbufs=2048 $testpmd_options -ia
