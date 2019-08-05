614de6c898aa938b3ec8c9991bec0bfdebc31545
Viacheslav Ovsiienko <viacheslavo@mellanox.com>
2019-08-05 13:03:49 +0000

    net/mlx5: fix default minimal data inline
    The patch [Fixes] sets the default value of required minimal
    inline data to 0 bytes. On some configurations (depends
    on switchdev/legacy settings and FW version/settings)
    the ConnectX-4LX NIC requires minimal 18 bytes of
    Tx descriptor inline data to operate correctly.
    
    Wrongly set to 0 default value may prevent NIC from operating
    with out-of-the-box settings, this patch reverts default
    value for ConnectX-4LX back to 18 bytes (inline L2).
    
    Fixes: 9f350504bb32 ("net/mlx5: fix ConnectX-4LX minimal inline data limit")
    
    Signed-off-by: Viacheslav Ovsiienko <viacheslavo@mellanox.com>
    Acked-by: Matan Azrad <matan@mellanox.com>

--- drivers/net/mlx5/mlx5.c
+++ drivers/net/mlx5/mlx5.c
@@ -1325,12 +1325,9 @@ mlx5_set_min_inline(struct mlx5_dev_spawn_data *spawn,
 	switch (spawn->pci_dev->id.device_id) {
 	case PCI_DEVICE_ID_MELLANOX_CONNECTX4:
 	case PCI_DEVICE_ID_MELLANOX_CONNECTX4VF:
-		config->txq_inline_min = MLX5_INLINE_HSIZE_L2;
-		config->hw_vlan_insert = 0;
-		break;
 	case PCI_DEVICE_ID_MELLANOX_CONNECTX4LX:
 	case PCI_DEVICE_ID_MELLANOX_CONNECTX4LXVF:
-		config->txq_inline_min = MLX5_INLINE_HSIZE_NONE;
+		config->txq_inline_min = MLX5_INLINE_HSIZE_L2;
 		config->hw_vlan_insert = 0;
 		break;
 	case PCI_DEVICE_ID_MELLANOX_CONNECTX5:
