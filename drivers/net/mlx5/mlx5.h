DPDK/dpdk

commit 34fa7c0268e7cd831e5a3443fd2c8b8f827c193a

net/mlx5: add drop action to Direct Verbs E-Switch
This commit adds support for drop action when creating E-Switch flow
using DV.

Signed-off-by: Ori Kam <orika@mellanox.com>
Acked-by: Yongseok Koh <yskoh@mellanox.com>

drivers/net/mlx5/mlx5.h
@@ -286,6 +286,7 @@ struct mlx5_ibv_shared {
 	/* RX Direct Rules tables. */
 	void *tx_ns; /* TX Direct Rules name space handle. */
 	struct mlx5_flow_tbl_resource tx_tbl[MLX5_MAX_TABLES];
+	void *esw_drop_action; /* Pointer to DR E-Switch drop action. */
 	/* TX Direct Rules tables/ */
 	LIST_HEAD(matchers, mlx5_flow_dv_matcher) matchers;
 	LIST_HEAD(encap_decap, mlx5_flow_dv_encap_decap_resource) encaps_decaps;

drivers/net/mlx5/mlx5.c
@@ -357,6 +357,7 @@ mlx5_alloc_shared_dr(struct mlx5_priv *priv)
 			goto error;
 		}
 		sh->fdb_ns = ns;
+		sh->esw_drop_action = mlx5_glue->dr_create_flow_action_drop();
 	}
 #endif
 	sh->dv_refcnt++;
@@ -376,6 +377,10 @@ error:
 	if (sh->fdb_ns) {
 		mlx5_glue->dr_destroy_ns(sh->fdb_ns);
 		sh->fdb_ns = NULL;
+	}
+	if (sh->esw_drop_action) {
+		mlx5_glue->destroy_flow_action(sh->esw_drop_action);
+		sh->esw_drop_action = NULL;
 	}
 	return err;
 #else
@@ -416,6 +421,10 @@ mlx5_free_shared_dr(struct mlx5_priv *priv)
 	if (sh->fdb_ns) {
 		mlx5_glue->dr_destroy_ns(sh->fdb_ns);
 		sh->fdb_ns = NULL;
+	}
+	if (sh->esw_drop_action) {
+		mlx5_glue->destroy_flow_action(sh->esw_drop_action);
+		sh->esw_drop_action = NULL;
 	}
 #endif
 	pthread_mutex_destroy(&sh->dv_mutex);

drivers/net/mlx5/mlx5_flow_dv.c
@@ -4052,6 +4052,7 @@ flow_dv_apply(struct rte_eth_dev *dev, struct rte_flow *flow,
 {
 	struct mlx5_flow_dv *dv;
 	struct mlx5_flow *dev_flow;
+	struct mlx5_priv *priv = dev->data->dev_private;
 	int n;
 	int err;
 
@@ -4059,15 +4060,20 @@ flow_dv_apply(struct rte_eth_dev *dev, struct rte_flow *flow,
 		dv = &dev_flow->dv;
 		n = dv->actions_n;
 		if (flow->actions & MLX5_FLOW_ACTION_DROP) {
-			dv->hrxq = mlx5_hrxq_drop_new(dev);
-			if (!dv->hrxq) {
-				rte_flow_error_set
-					(error, errno,
-					 RTE_FLOW_ERROR_TYPE_UNSPECIFIED, NULL,
-					 "cannot get drop hash queue");
-				goto error;
+			if (flow->transfer) {
+				dv->actions[n++] = priv->sh->esw_drop_action;
+			} else {
+				dv->hrxq = mlx5_hrxq_drop_new(dev);
+				if (!dv->hrxq) {
+					rte_flow_error_set
+						(error, errno,
+						 RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+						 NULL,
+						 "cannot get drop hash queue");
+					goto error;
+				}
+				dv->actions[n++] = dv->hrxq->action;
 			}
-			dv->actions[n++] = dv->hrxq->action;
 		} else if (flow->actions &
 			   (MLX5_FLOW_ACTION_QUEUE | MLX5_FLOW_ACTION_RSS)) {
 			struct mlx5_hrxq *hrxq;

drivers/net/mlx5/mlx5_glue.c
@@ -394,6 +394,16 @@ mlx5_glue_dr_create_flow_action_dest_vport(void *ns, uint32_t vport)
 }
 
 static void *
+mlx5_glue_dr_create_flow_action_drop(void)
+{
+#ifdef HAVE_MLX5DV_DR_ESWITCH
+	return mlx5dv_dr_create_action_drop();
+#else
+	return NULL;
+#endif
+}
+
+static void *
 mlx5_glue_dr_create_flow_tbl(void *ns, uint32_t level)
 {
 #ifdef HAVE_MLX5DV_DR
@@ -861,6 +871,8 @@ const struct mlx5_glue *mlx5_glue = &(const struct mlx5_glue){
 		mlx5_glue_dr_create_flow_action_dest_flow_tbl,
 	.dr_create_flow_action_dest_vport =
 		mlx5_glue_dr_create_flow_action_dest_vport,
+	.dr_create_flow_action_drop =
+		mlx5_glue_dr_create_flow_action_drop,
 	.dr_create_flow_tbl = mlx5_glue_dr_create_flow_tbl,
 	.dr_destroy_flow_tbl = mlx5_glue_dr_destroy_flow_tbl,
 	.dr_create_ns = mlx5_glue_dr_create_ns,

drivers/net/mlx5/mlx5_glue.h
@@ -147,6 +147,7 @@ struct mlx5_glue {
 	struct ibv_cq *(*cq_ex_to_cq)(struct ibv_cq_ex *cq);
 	void *(*dr_create_flow_action_dest_flow_tbl)(void *tbl);
 	void *(*dr_create_flow_action_dest_vport)(void *ns, uint32_t vport);
+	void *(*dr_create_flow_action_drop)();
 	void *(*dr_create_flow_tbl)(void *ns, uint32_t level);
 	int (*dr_destroy_flow_tbl)(void *tbl);
 	void *(*dr_create_ns)(struct ibv_context *ctx,

commit 48c91c8f3fc7edb3e90eef682d535af294ed7eec

net/mlx5: fix comment
In struct mlx5_ibv_shared, member esw_drop_action was added between
existing member tx_tbl and the comment line describing it.
This patch moves the comment line to its original location, and fixes
a typo in the comment.

Fixes: 34fa7c0268e7 ("net/mlx5: add drop action to Direct Verbs E-Switch")
Cc: stable@dpdk.org

Signed-off-by: Dekel Peled <dekelp@mellanox.com>
Acked-by: Viacheslav Ovsiienko <viacheslavo@mellanox.com>

drivers/net/mlx5/mlx5.h
@@ -569,8 +569,8 @@ struct mlx5_ibv_shared {
 	/* RX Direct Rules tables. */
 	void *tx_domain; /* TX Direct Rules name space handle. */
 	struct mlx5_flow_tbl_resource tx_tbl[MLX5_MAX_TABLES];
+	/* TX Direct Rules tables. */
 	void *esw_drop_action; /* Pointer to DR E-Switch drop action. */
-	/* TX Direct Rules tables/ */
 	LIST_HEAD(matchers, mlx5_flow_dv_matcher) matchers;
 	LIST_HEAD(encap_decap, mlx5_flow_dv_encap_decap_resource) encaps_decaps;
 	LIST_HEAD(modify_cmd, mlx5_flow_dv_modify_hdr_resource) modify_cmds;