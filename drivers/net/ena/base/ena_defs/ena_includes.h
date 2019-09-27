702928afeb1fecce27d59799a76a77a077d02f27
Maciej Bielski <mba@semihalf.com>
2019-09-27 13:41:45 +0200

    net/ena: change license clause to SPDX tags
    Signed-off-by: Maciej Bielski <mba@semihalf.com>
    Acked-by: Michal Krawczyk <mk@semihalf.com>

--- drivers/net/ena/base/ena_defs/ena_includes.h
+++ drivers/net/ena/base/ena_defs/ena_includes.h
@@ -1,3 +1,8 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2015-2019 Amazon.com, Inc. or its affiliates.
+ * All rights reserved.
+ */
+
 #include "ena_common_defs.h"
 #include "ena_regs_defs.h"
 #include "ena_admin_defs.h"
