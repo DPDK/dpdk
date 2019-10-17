fe35622659edc678992aea389beb152105051e7f
Robin Jarry <robin.jarry@6wind.com>
2019-10-17 19:32:12 +0200

    usertools: fix telemetry client with python 3
    When running the dpdk-telemetry-client.py with python 3, we get the
    following syntax errors:
    
      File "usertools/dpdk-telemetry-client.py", line 70
          print "\nResponse: \n", str(data)
                               ^
      SyntaxError: invalid syntax
    
      File "usertools/dpdk-telemetry-client.py", line 93
          print "\nResponse: \n", str(data)
                               ^
      SyntaxError: invalid syntax
    
      File "usertools/dpdk-telemetry-client.py", line 111
          file_path = sys.argv[1]
                                ^
      TabError: inconsistent use of tabs and spaces in indentation
    
    Import print_function from __future__ and add parentheses where missing.
    Also, use spaces for indentation everywhere.
    
    Fixes: d1b94da4a4e0 ("usertools: add client script for telemetry")
    Fixes: 53f293c9a783 ("usertools: replace unsafe input function")
    Fixes: 4080e46c8078 ("telemetry: support global metrics")
    Cc: stable@dpdk.org
    
    Signed-off-by: Robin Jarry <robin.jarry@6wind.com>
    Acked-by: Stephen Hemminger <stephen@networkplumber.org>

--- usertools/dpdk-telemetry-client.py
+++ usertools/dpdk-telemetry-client.py
@@ -2,6 +2,8 @@
 # SPDK-License-Identifier: BSD-3-Clause
 # Copyright(c) 2018 Intel Corporation
 
+from __future__ import print_function
+
 import socket
 import os
 import sys
@@ -16,9 +18,9 @@ GLOBAL_METRICS_REQ = "{\"action\":0,\"command\":\"global_stat_values\",\"data\":
 DEFAULT_FP = "/var/run/dpdk/default_client"
 
 try:
-	raw_input  # Python 2
+    raw_input  # Python 2
 except NameError:
-	raw_input = input  # Python 3
+    raw_input = input  # Python 3
 
 class Socket:
 
@@ -74,7 +76,7 @@ class Client:
     def requestMetrics(self): # Requests metrics for given client
         self.socket.client_fd.send(METRICS_REQ)
         data = self.socket.client_fd.recv(BUFFER_SIZE)
-        print "\nResponse: \n", str(data)
+        print("\nResponse: \n", str(data))
 
     def repeatedlyRequestMetrics(self, sleep_time): # Recursively requests metrics for given client
         print("\nPlease enter the number of times you'd like to continuously request Metrics:")
@@ -88,7 +90,7 @@ class Client:
     def requestGlobalMetrics(self): #Requests global metrics for given client
         self.socket.client_fd.send(GLOBAL_METRICS_REQ)
         data = self.socket.client_fd.recv(BUFFER_SIZE)
-        print "\nResponse: \n", str(data)
+        print("\nResponse: \n", str(data))
 
     def interactiveMenu(self, sleep_time): # Creates Interactive menu within the script
         while self.choice != 4:
@@ -121,10 +123,10 @@ if __name__ == "__main__":
     sleep_time = 1
     file_path = ""
     if (len(sys.argv) == 2):
-	file_path = sys.argv[1]
+        file_path = sys.argv[1]
     else:
         print("Warning - No filepath passed, using default (" + DEFAULT_FP + ").")
-	file_path = DEFAULT_FP
+        file_path = DEFAULT_FP
     client = Client()
     client.getFilepath(file_path)
     client.register()
