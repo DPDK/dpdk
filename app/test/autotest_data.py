48083b4d67c08f97c613b0df09d8945375aa7661
Vladimir Medvedkin <vladimir.medvedkin@intel.com>
2019-10-21 15:35:45 +0100

    test/ipsec: add SAD autotests
    add unittests for ipsec SAD library
    
    Signed-off-by: Vladimir Medvedkin <vladimir.medvedkin@intel.com>
    Acked-by: Akhil Goyal <akhil.goyal@nxp.com>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
    Tested-by: Konstantin Ananyev <konstantin.ananyev@intel.com>

--- app/test/autotest_data.py
+++ app/test/autotest_data.py
@@ -518,6 +518,12 @@ parallel_test_list = [
         "Func":    default_autotest,
         "Report":  None,
     },
+    {
+        "Name":    "IPsec_SAD",
+        "Command": "ipsec_sad_autotest",
+        "Func":    default_autotest,
+        "Report":  None,
+    },
     #
     #Please always keep all dump tests at the end and together!
     #
