DPDK/dpdk

commit 38dff79ba7360f7171303300fadc4f00592d8630

net/bnx2x: update HSI
Update hardware software common base driver code in preparation to
update the firmware to version 7.13.11.

Signed-off-by: Rasesh Mody <rmody@marvell.com>

drivers/net/bnx2x/bnx2x.c
@@ -2182,8 +2182,10 @@ int bnx2x_tx_encap(struct bnx2x_tx_queue *txq, struct rte_mbuf *m0)
 
 	tx_start_bd = &txq->tx_ring[TX_BD(bd_prod, txq)].start_bd;
 
-	tx_start_bd->addr =
-	    rte_cpu_to_le_64(rte_mbuf_data_iova(m0));
+	tx_start_bd->addr_lo =
+	    rte_cpu_to_le_32(U64_LO(rte_mbuf_data_iova(m0)));
+	tx_start_bd->addr_hi =
+	    rte_cpu_to_le_32(U64_HI(rte_mbuf_data_iova(m0)));
 	tx_start_bd->nbytes = rte_cpu_to_le_16(m0->data_len);
 	tx_start_bd->bd_flags.as_bitfield = ETH_TX_BD_FLAGS_START_BD;
 	tx_start_bd->general_data =
@@ -5015,13 +5017,14 @@ static void
 bnx2x_update_rx_prod(struct bnx2x_softc *sc, struct bnx2x_fastpath *fp,
 		   uint16_t rx_bd_prod, uint16_t rx_cq_prod)
 {
-	union ustorm_eth_rx_producers rx_prods;
+	struct ustorm_eth_rx_producers rx_prods;
 	uint32_t i;
 
+	memset(&rx_prods, 0, sizeof(rx_prods));
+
 	/* update producers */
-	rx_prods.prod.bd_prod = rx_bd_prod;
-	rx_prods.prod.cqe_prod = rx_cq_prod;
-	rx_prods.prod.reserved = 0;
+	rx_prods.bd_prod = rx_bd_prod;
+	rx_prods.cqe_prod = rx_cq_prod;
 
 	/*
 	 * Make sure that the BD and SGE data is updated before updating the
@@ -5034,9 +5037,8 @@ bnx2x_update_rx_prod(struct bnx2x_softc *sc, struct bnx2x_fastpath *fp,
 	wmb();
 
 	for (i = 0; i < (sizeof(rx_prods) / 4); i++) {
-		REG_WR(sc,
-		       (fp->ustorm_rx_prods_offset + (i * 4)),
-		       rx_prods.raw_data[i]);
+		REG_WR(sc, (fp->ustorm_rx_prods_offset + (i * 4)),
+		       ((uint32_t *)&rx_prods)[i]);
 	}
 
 	wmb();			/* keep prod updates ordered */

drivers/net/bnx2x/bnx2x.h
@@ -19,18 +19,7 @@
 #include <rte_bus_pci.h>
 #include <rte_io.h>
 
-#if RTE_BYTE_ORDER == RTE_LITTLE_ENDIAN
-#ifndef __LITTLE_ENDIAN
-#define __LITTLE_ENDIAN RTE_LITTLE_ENDIAN
-#endif
-#undef __BIG_ENDIAN
-#elif RTE_BYTE_ORDER == RTE_BIG_ENDIAN
-#ifndef __BIG_ENDIAN
-#define __BIG_ENDIAN    RTE_BIG_ENDIAN
-#endif
-#undef __LITTLE_ENDIAN
-#endif
-
+#include "bnx2x_osal.h"
 #include "bnx2x_ethdev.h"
 #include "ecore_mfw_req.h"
 #include "ecore_fw_defs.h"
@@ -1911,16 +1900,18 @@ bnx2x_hc_ack_sb(struct bnx2x_softc *sc, uint8_t sb_id, uint8_t storm,
 {
 	uint32_t hc_addr = (HC_REG_COMMAND_REG + SC_PORT(sc) * 32 +
 			COMMAND_REG_INT_ACK);
-	union igu_ack_register igu_ack;
+	struct igu_ack_register igu_ack;
+	uint32_t *val = NULL;
 
-	igu_ack.sb.status_block_index = index;
-	igu_ack.sb.sb_id_and_flags =
+	igu_ack.status_block_index = index;
+	igu_ack.sb_id_and_flags =
 		((sb_id << IGU_ACK_REGISTER_STATUS_BLOCK_ID_SHIFT) |
 		 (storm << IGU_ACK_REGISTER_STORM_ID_SHIFT) |
 		 (update << IGU_ACK_REGISTER_UPDATE_INDEX_SHIFT) |
 		 (op << IGU_ACK_REGISTER_INTERRUPT_MODE_SHIFT));
 
-	REG_WR(sc, hc_addr, igu_ack.raw_data);
+	val = (uint32_t *)&igu_ack;
+	REG_WR(sc, hc_addr, *val);
 
 	/* Make sure that ACK is written */
 	mb();

drivers/net/bnx2x/bnx2x_osal.h
@@ -1,0 +1,29 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2019 Cavium Inc.
+ *
+ * All rights reserved.
+ * www.cavium.com
+ */
+
+#ifndef BNX2X_OSAL_H
+#define BNX2X_OSAL_H
+
+#include <sys/stat.h>
+
+#if RTE_BYTE_ORDER == RTE_LITTLE_ENDIAN
+#ifndef __LITTLE_ENDIAN
+#define __LITTLE_ENDIAN RTE_LITTLE_ENDIAN
+#endif
+#undef __BIG_ENDIAN
+#elif RTE_BYTE_ORDER == RTE_BIG_ENDIAN
+#ifndef __BIG_ENDIAN
+#define __BIG_ENDIAN    RTE_BIG_ENDIAN
+#endif
+#undef __LITTLE_ENDIAN
+#endif
+
+#define __le16		uint16_t
+#define __le32		uint32_t
+#define __le64		uint64_t
+
+#endif /* BNX2X_OSAL_H */

drivers/net/bnx2x/bnx2x_rxtx.c
@@ -321,12 +321,14 @@ static inline void
 bnx2x_upd_rx_prod_fast(struct bnx2x_softc *sc, struct bnx2x_fastpath *fp,
 		uint16_t rx_bd_prod, uint16_t rx_cq_prod)
 {
-	union ustorm_eth_rx_producers rx_prods;
+	struct ustorm_eth_rx_producers rx_prods = { 0 };
+	uint32_t *val = NULL;
 
-	rx_prods.prod.bd_prod  = rx_bd_prod;
-	rx_prods.prod.cqe_prod = rx_cq_prod;
+	rx_prods.bd_prod  = rx_bd_prod;
+	rx_prods.cqe_prod = rx_cq_prod;
 
-	REG_WR(sc, fp->ustorm_rx_prods_offset, rx_prods.raw_data[0]);
+	val = (uint32_t *)&rx_prods;
+	REG_WR(sc, fp->ustorm_rx_prods_offset, val[0]);
 }
 
 static uint16_t

drivers/net/bnx2x/ecore_sp.c
@@ -3524,13 +3524,6 @@ static int ecore_setup_rss(struct bnx2x_softc *sc,
 		data->capabilities |=
 		    ETH_RSS_UPDATE_RAMROD_DATA_IPV6_UDP_CAPABILITY;
 
-	if (ECORE_TEST_BIT(ECORE_RSS_TUNNELING, &p->rss_flags)) {
-		data->udp_4tuple_dst_port_mask =
-		    ECORE_CPU_TO_LE16(p->tunnel_mask);
-		data->udp_4tuple_dst_port_value =
-		    ECORE_CPU_TO_LE16(p->tunnel_value);
-	}
-
 	/* Hashing mask */
 	data->rss_result_mask = p->rss_result_mask;
 
@@ -5088,8 +5081,6 @@ static int ecore_func_send_start(struct bnx2x_softc *sc,
 	rdata->sd_vlan_tag = ECORE_CPU_TO_LE16(start_params->sd_vlan_tag);
 	rdata->path_id = ECORE_PATH_ID(sc);
 	rdata->network_cos_mode = start_params->network_cos_mode;
-	rdata->gre_tunnel_mode = start_params->gre_tunnel_mode;
-	rdata->gre_tunnel_rss = start_params->gre_tunnel_rss;
 
 	/*
 	 *  No need for an explicit memory barrier here as long we would
@@ -5229,7 +5220,7 @@ static int ecore_func_send_tx_start(struct bnx2x_softc *sc, struct ecore_func_st
 
 	rdata->dcb_enabled = tx_start_params->dcb_enabled;
 	rdata->dcb_version = tx_start_params->dcb_version;
-	rdata->dont_add_pri_0 = tx_start_params->dont_add_pri_0;
+	rdata->dont_add_pri_0_en = tx_start_params->dont_add_pri_0;
 
 	for (i = 0; i < ARRAY_SIZE(rdata->traffic_type_to_priority_cos); i++)
 		rdata->traffic_type_to_priority_cos[i] =

commit a3d1111a778e9b3093093db253c43b0e520e58d5

net/bnx2x: fix meson build
Use kernel headers for __le* types to avoid potential conflicts
resulting in redefinition errors for some Linux build environments.
Add check for FreeBSD execution environments.

Without this fix, aarch64 builds can fail with error below:

In file included from ../drivers/net/bnx2x/bnx2x.h:22,
                 from ../drivers/net/bnx2x/bnx2x_ethdev.c:8:
../drivers/net/bnx2x/bnx2x_osal.h:27:17: error:
conflicting types for ‘uint64_t’
 #define __le64  uint64_t
               ^~~~~~~~
In file included from /usr/include/stdint.h:37,
                 from /usr/lib/gcc/aarch64-linux-gnu/8/include/stdint.h:9,
                 from ../lib/librte_eal/common/include/arch/arm/rte_byteorder.h:16,
                 from ../drivers/net/bnx2x/bnx2x.h:17,
                 from ../drivers/net/bnx2x/bnx2x_ethdev.c:8:
/usr/include/aarch64-linux-gnu/bits/stdint-uintn.h:27:20: note:
previous declaration of ‘uint64_t’ was here
 typedef __uint64_t uint64_t;
                    ^~~~~~~~

There is also one minor change mixed in this commit:
Some use of __FreeBSD__ are replaced by RTE_EXEC_ENV_FREEBSD.

Fixes: 38dff79ba736 ("net/bnx2x: update HSI")

Signed-off-by: Rasesh Mody <rmody@marvell.com>
Tested-by: Dharmik Thakkar <dharmik.thakkar@arm.com>
Tested-by: Gavin Hu <gavin.hu@arm.com>

drivers/net/bnx2x/bnx2x.c
@@ -9581,7 +9581,7 @@ static int bnx2x_pci_get_caps(struct bnx2x_softc *sc)
 		return -ENOMEM;
 	}
 
-#ifndef __FreeBSD__
+#ifndef RTE_EXEC_ENV_FREEBSD
 	pci_read(sc, PCI_STATUS, &status, 2);
 	if (!(status & PCI_STATUS_CAP_LIST)) {
 #else
@@ -9592,7 +9592,7 @@ static int bnx2x_pci_get_caps(struct bnx2x_softc *sc)
 		return -1;
 	}
 
-#ifndef __FreeBSD__
+#ifndef RTE_EXEC_ENV_FREEBSD
 	pci_read(sc, PCI_CAPABILITY_LIST, &pci_cap.next, 1);
 #else
 	pci_read(sc, PCIR_CAP_PTR, &pci_cap.next, 1);

drivers/net/bnx2x/bnx2x.h
@@ -30,7 +30,7 @@
 
 #include "elink.h"
 
-#ifndef __FreeBSD__
+#ifndef RTE_EXEC_ENV_FREEBSD
 #include <linux/pci_regs.h>
 
 #define PCIY_PMG                       PCI_CAP_ID_PM
@@ -60,7 +60,7 @@
 #define IFM_10G_TWINAX                 22 /* 10GBase Twinax copper */
 #define IFM_10G_T                      26 /* 10GBase-T - RJ45 */
 
-#ifndef __FreeBSD__
+#ifndef RTE_EXEC_ENV_FREEBSD
 #define PCIR_EXPRESS_DEVICE_STA        PCI_EXP_TYPE_RC_EC
 #define PCIM_EXP_STA_TRANSACTION_PND   PCI_EXP_DEVSTA_TRPND
 #define PCIR_EXPRESS_LINK_STA          PCI_EXP_LNKSTA

drivers/net/bnx2x/bnx2x_osal.h
@@ -8,7 +8,11 @@
 #ifndef BNX2X_OSAL_H
 #define BNX2X_OSAL_H
 
+#ifdef RTE_EXEC_ENV_FREEBSD
 #include <sys/stat.h>
+#else
+#include <linux/types.h>
+#endif
 
 #if RTE_BYTE_ORDER == RTE_LITTLE_ENDIAN
 #ifndef __LITTLE_ENDIAN
@@ -22,8 +26,10 @@
 #undef __LITTLE_ENDIAN
 #endif
 
+#ifdef RTE_EXEC_ENV_FREEBSD
 #define __le16		uint16_t
 #define __le32		uint32_t
 #define __le64		uint64_t
+#endif
 
 #endif /* BNX2X_OSAL_H */