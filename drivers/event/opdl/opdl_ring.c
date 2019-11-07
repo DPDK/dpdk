46090d658ebfc8b17bd2ccc7d1513cd6d81b5544
Gavin Hu <gavin.hu@arm.com>
2019-11-07 22:35:28 +0100

    event/opdl: use new API to save cycles on aarch64
    Use the new API to wait in low power state instead of continuous
    polling to save CPU cycles and power.
    
    Signed-off-by: Gavin Hu <gavin.hu@arm.com>
    Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- drivers/event/opdl/opdl_ring.c
+++ drivers/event/opdl/opdl_ring.c
@@ -16,6 +16,7 @@
 #include <rte_memcpy.h>
 #include <rte_memory.h>
 #include <rte_memzone.h>
+#include <rte_atomic.h>
 
 #include "opdl_ring.h"
 #include "opdl_log.h"
@@ -474,9 +475,7 @@ opdl_ring_input_multithread(struct opdl_ring *t, const void *entries,
 	/* If another thread started inputting before this one, but hasn't
 	 * finished, we need to wait for it to complete to update the tail.
 	 */
-	while (unlikely(__atomic_load_n(&s->shared.tail, __ATOMIC_ACQUIRE) !=
-			old_head))
-		rte_pause();
+	rte_wait_until_equal_32(&s->shared.tail, old_head, __ATOMIC_ACQUIRE);
 
 	__atomic_store_n(&s->shared.tail, old_head + num_entries,
 			__ATOMIC_RELEASE);
