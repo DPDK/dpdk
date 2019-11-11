0d20cda8a935d0df3c6e483422151705200907d0
Sachin Saxena <sachin.saxena@nxp.com>
2019-11-11 21:38:57 +0530

    net/dpaa2: fix Rx offload flags on jumbo MTU set
    The JUMBO frame handling in dpaa2_dev_mtu_set api was not correct.
    When frame_size is greater than RTE_ETHER_MAX_LEN, the
    intention is to add JUMBO flag in rx offload while it was resetting
    all other flags other than JUMBO as AND operator was used instead of OR.
    
    Fixes: 0ebce6129bc6 ("net/dpaa2: support new ethdev offload APIs")
    Cc: stable@dpdk.org
    
    Signed-off-by: Sachin Saxena <sachin.saxena@nxp.com>
    Reviewed-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/net/dpaa2/dpaa2_ethdev.c
+++ drivers/net/dpaa2/dpaa2_ethdev.c
@@ -1289,7 +1289,7 @@ dpaa2_dev_mtu_set(struct rte_eth_dev *dev, uint16_t mtu)
 		return -EINVAL;
 
 	if (frame_size > RTE_ETHER_MAX_LEN)
-		dev->data->dev_conf.rxmode.offloads &=
+		dev->data->dev_conf.rxmode.offloads |=
 						DEV_RX_OFFLOAD_JUMBO_FRAME;
 	else
 		dev->data->dev_conf.rxmode.offloads &=
