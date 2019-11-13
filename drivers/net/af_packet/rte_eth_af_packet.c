60b05125c42248fb86af4876828599431a5629f3
Paul Atkins <paul.atkins@intl.att.com>
2019-11-13 12:08:00 +0000

    net/af_packet: advertise Tx offload capabilities
    The af_packet pmd already supports MULTI_SEG tx packets, and tx
    VLAN_INSERT so advertise these capabilities.
    
    Signed-off-by: Paul Atkins <paul.atkins@intl.att.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- drivers/net/af_packet/rte_eth_af_packet.c
+++ drivers/net/af_packet/rte_eth_af_packet.c
@@ -316,6 +316,8 @@ eth_dev_info(struct rte_eth_dev *dev, struct rte_eth_dev_info *dev_info)
 	dev_info->max_rx_queues = (uint16_t)internals->nb_queues;
 	dev_info->max_tx_queues = (uint16_t)internals->nb_queues;
 	dev_info->min_rx_bufsize = 0;
+	dev_info->tx_offload_capa = DEV_TX_OFFLOAD_MULTI_SEGS |
+		DEV_TX_OFFLOAD_VLAN_INSERT;
 
 	return 0;
 }
