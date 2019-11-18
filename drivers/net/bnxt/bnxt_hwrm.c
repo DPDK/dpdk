89a0deb866dc42ead92b79e6e7159622e1ab8490
Ajit Khaparde <ajit.khaparde@broadcom.com>
2019-11-18 14:23:45 -0800

    net/bnxt: fix resource qcaps with older FW
    On some old versions of FW, bnxt_hwrm_func_resc_qcaps can return an
    error. This is because the command was not implemented completely
    in FW till the subsequent version. Ignore the error and continue with
    the driver initialization.
    
    Fixes: edafb57ba4a1 ("net/bnxt: fix VF resource allocation")
    Cc: stable@dpdk.org
    
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.c
+++ drivers/net/bnxt/bnxt_hwrm.c
@@ -692,7 +692,12 @@ int bnxt_hwrm_func_qcaps(struct bnxt *bp)
 			bp->flags |= BNXT_FLAG_NEW_RM;
 	}
 
-	return rc;
+	/* On older FW,
+	 * bnxt_hwrm_func_resc_qcaps can fail and cause init failure.
+	 * But the error can be ignored. Return success.
+	 */
+
+	return 0;
 }
 
 /* VNIC cap covers capability of all VNICs. So no need to pass vnic_id */
@@ -901,7 +906,7 @@ int bnxt_hwrm_func_resc_qcaps(struct bnxt *bp)
 
 	rc = bnxt_hwrm_send_message(bp, &req, sizeof(req), BNXT_USE_CHIMP_MB);
 
-	HWRM_CHECK_RESULT();
+	HWRM_CHECK_RESULT_SILENT();
 
 	if (BNXT_VF(bp)) {
 		bp->max_rsscos_ctx = rte_le_to_cpu_16(resp->max_rsscos_ctx);
