4f8c7b4fcf44a2f220e464762a4042d4643d604f
Amaranath Somalapuram <asomalap@amd.com>
2019-11-22 12:17:47 +0530

    crypto/ccp: fix digest size capabilities
    CCP can support varied digest sizes ranging from 1 to some max value
    But the current code support only fixed max values.
    This patch updates the minimum digest sizes to 1
    
    Fixes: e0d88a394e ("crypto/ccp: support run-time CPU based auth")
    Cc: stable@dpdk.org
    
    Signed-off-by: Amaranath Somalapuram <asomalap@amd.com>

--- drivers/crypto/ccp/ccp_pmd_ops.c
+++ drivers/crypto/ccp/ccp_pmd_ops.c
@@ -47,9 +47,9 @@
 					 .increment = 1                 \
 				 },                                     \
 				 .digest_size = {                       \
-					 .min = 20,                     \
+					 .min = 1,                      \
 					 .max = 20,                     \
-					 .increment = 0                 \
+					 .increment = 1                 \
 				 },                                     \
 				 .aad_size = { 0 }                      \
 			 }, }                                           \
@@ -89,9 +89,9 @@
 					 .increment = 1                 \
 				 },                                     \
 				 .digest_size = {                       \
-					 .min = 28,                     \
+					 .min = 1,                     \
 					 .max = 28,                     \
-					 .increment = 0                 \
+					 .increment = 1                 \
 				 },                                     \
 				 .aad_size = { 0 }                      \
 			 }, }                                           \
@@ -173,9 +173,9 @@
 					 .increment = 1                 \
 				 },                                     \
 				 .digest_size = {                       \
-					 .min = 32,                     \
+					 .min = 1,                     \
 					 .max = 32,                     \
-					 .increment = 0                 \
+					 .increment = 1                 \
 				 },                                     \
 				 .aad_size = { 0 }                      \
 			 }, }                                           \
@@ -257,9 +257,9 @@
 					 .increment = 1                 \
 				 },                                     \
 				 .digest_size = {                       \
-					 .min = 48,                     \
+					 .min = 1,                     \
 					 .max = 48,                     \
-					 .increment = 0                 \
+					 .increment = 1                 \
 				 },                                     \
 				 .aad_size = { 0 }                      \
 			 }, }                                           \
@@ -341,9 +341,9 @@
 					 .increment = 1                 \
 				 },                                     \
 				 .digest_size = {                       \
-					 .min = 64,                     \
+					 .min = 1,                     \
 					 .max = 64,                     \
-					 .increment = 0                 \
+					 .increment = 1                 \
 				 },                                     \
 				 .aad_size = { 0 }                      \
 			 }, }                                           \
@@ -383,9 +383,9 @@
 					 .increment = 1                 \
 				 },                                     \
 				 .digest_size = {                       \
-					 .min = 64,                     \
+					 .min = 1,                     \
 					 .max = 64,                     \
-					 .increment = 0                 \
+					 .increment = 1                 \
 				 },                                     \
 				 .aad_size = { 0 }                      \
 			}, }                                            \
@@ -536,9 +536,9 @@
 					 .increment = 1			\
 				 },					\
 				 .digest_size = {			\
-					 .min = 16,			\
+					 .min = 1,			\
 					 .max = 16,			\
-					 .increment = 0			\
+					 .increment = 1			\
 				 },					\
 				 .aad_size = { 0 }			\
 			}, }						\
