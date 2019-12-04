aef1d0733179afb56916e95058a4f1398b81af04
David Marchand <david.marchand@redhat.com>
2019-12-04 14:16:22 +0100

    eal/linux: fix build error on RHEL 7.6
    Previous fix gives hiccups to gcc on RHEL 7.6:
    
    == Build lib/librte_eal/linux/eal
      CC eal_interrupts.o
    ...lib/librte_eal/linux/eal/eal_interrupts.c: In function
      ‘eal_intr_thread_main’:
    ...lib/librte_eal/linux/eal/eal_interrupts.c:1048:9: error: missing
      initializer for field ‘events’ of ‘struct epoll_event’
      [-Werror=missing-field-initializers]
      struct epoll_event ev = { };
             ^
    In file included from ...lib/librte_eal/linux/eal/eal_interrupts.c:15:0:
    /usr/include/sys/epoll.h:89:12: note: ‘events’ declared here
       uint32_t events; /* Epoll events */
                ^
    ...lib/librte_eal/linux/eal/eal_interrupts.c: At top level:
    cc1: error: unrecognized command line option
      "-Wno-address-of-packed-member" [-Werror]
    cc1: all warnings being treated as errors
    
    Fixes: e0ab8020ac2a ("eal/linux: fix uninitialized data valgrind warning")
    Cc: stable@dpdk.org
    
    Reported-by: Andrew Rybchenko <arybchenko@solarflare.com>
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- lib/librte_eal/linux/eal/eal_interrupts.c
+++ lib/librte_eal/linux/eal/eal_interrupts.c
@@ -1045,8 +1045,6 @@ eal_intr_handle_interrupts(int pfd, unsigned totalfds)
 static __attribute__((noreturn)) void *
 eal_intr_thread_main(__rte_unused void *arg)
 {
-	struct epoll_event ev = { };
-
 	/* host thread, never break out */
 	for (;;) {
 		/* build up the epoll fd with all descriptors we are to
@@ -1078,8 +1076,11 @@ eal_intr_thread_main(__rte_unused void *arg)
 		rte_spinlock_lock(&intr_lock);
 
 		TAILQ_FOREACH(src, &intr_sources, next) {
+			struct epoll_event ev;
+
 			if (src->callbacks.tqh_first == NULL)
 				continue; /* skip those with no callbacks */
+			memset(&ev, 0, sizeof(ev));
 			ev.events = EPOLLIN | EPOLLPRI | EPOLLRDHUP | EPOLLHUP;
 			ev.data.fd = src->intr_handle.fd;
 
