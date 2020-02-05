966f89d998a20eddb45666f06dd42a3c3fc94574
Maxime Coquelin <maxime.coquelin@redhat.com>
2020-02-05 16:05:29 +0100

    vhost: check message header size read
    This patch adds a check to ensure the read size of
    the Vhost-user message header is not smaller than
    the expected size.
    
    In case of unexpected read size, report an error
    and close file descriptors passed with the message,
    if any.
    
    Fixes: 8f972312b8f4 ("vhost: support vhost-user")
    Cc: stable@dpdk.org
    
    Reported-by: Ilja Van Sprundel <ivansprundel@ioactive.com>
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Reviewed-by: Tiwei Bie <tiwei.bie@intel.com>

--- lib/librte_vhost/vhost_user.c
+++ lib/librte_vhost/vhost_user.c
@@ -2456,8 +2456,13 @@ read_vhost_message(int sockfd, struct VhostUserMsg *msg)
 
 	ret = read_fd_message(sockfd, (char *)msg, VHOST_USER_HDR_SIZE,
 		msg->fds, VHOST_MEMORY_MAX_NREGIONS, &msg->fd_num);
-	if (ret <= 0)
+	if (ret <= 0) {
 		return ret;
+	} else if (ret != VHOST_USER_HDR_SIZE) {
+		VHOST_LOG_CONFIG(ERR, "Unexpected header size read\n");
+		close_msg_fds(msg);
+		return -1;
+	}
 
 	if (msg->size) {
 		if (msg->size > sizeof(msg->payload)) {
