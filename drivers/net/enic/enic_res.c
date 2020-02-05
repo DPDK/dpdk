DPDK/dpdk

commit aae7dd40cda4626c6e476890c26990324f9dc31d

net/enic: move min/max macros
Move a macro from a widely included header file to a header file
used only by the one caller of the macro.

Signed-off-by: John Daley <johndale@cisco.com>
Reviewed-by: Hyong Youb Kim <hyonkim@cisco.com>

drivers/net/enic/enic_res.h
@@ -11,6 +11,16 @@
 #include "vnic_wq.h"
 #include "vnic_rq.h"
 
+#define min_t(type, x, y) ({                    \
+	type __min1 = (x);                      \
+	type __min2 = (y);                      \
+	__min1 < __min2 ? __min1 : __min2; })
+
+#define max_t(type, x, y) ({                    \
+	type __max1 = (x);                      \
+	type __max2 = (y);                      \
+	__max1 > __max2 ? __max1 : __max2; })
+
 #define ENIC_MIN_WQ_DESCS		64
 #define ENIC_MAX_WQ_DESCS		4096
 #define ENIC_MIN_RQ_DESCS		64
@@ -54,7 +64,6 @@
 #define ENIC_RSS_HASH_KEY_SIZE  40
 
 #define ENIC_SETTING(enic, f) ((enic->config.flags & VENETF_##f) ? 1 : 0)
-
 
 struct enic;
 

drivers/net/enic/enic_compat.h
@@ -73,14 +73,4 @@ static inline void writel(unsigned int val, volatile void __iomem *addr)
 	rte_write32(val, addr);
 }
 
-#define min_t(type, x, y) ({                    \
-	type __min1 = (x);                      \
-	type __min2 = (y);                      \
-	__min1 < __min2 ? __min1 : __min2; })
-
-#define max_t(type, x, y) ({                    \
-	type __max1 = (x);                      \
-	type __max2 = (y);                      \
-	__max1 > __max2 ? __max1 : __max2; })
-
 #endif /* _ENIC_COMPAT_H_ */

commit 25d22cd4f8d7f6d366c25bb565d5a11e7856f81b

net/enic: use common min and max macros
Use the RTE_MIN and RTE_MAX macros instead of private macros.

Fixes: aae7dd40cda4 ("net/enic: move min/max macros")

Signed-off-by: John Daley <johndale@cisco.com>
Reviewed-by: Hyong Youb Kim <hyonkim@cisco.com>

drivers/net/enic/enic_res.c
@@ -68,8 +68,8 @@ int enic_get_vnic_config(struct enic *enic)
 	if (c->mtu == 0)
 		c->mtu = 1500;
 
-	enic->rte_dev->data->mtu = min_t(uint16_t, enic->max_mtu,
-					 max_t(uint16_t, ENIC_MIN_MTU, c->mtu));
+	enic->rte_dev->data->mtu = RTE_MIN(enic->max_mtu,
+				RTE_MAX((uint16_t)ENIC_MIN_MTU, c->mtu));
 
 	enic->adv_filters = vnic_dev_capable_adv_filters(enic->vdev);
 	dev_info(enic, "Advanced Filters %savailable\n", ((enic->adv_filters)
@@ -100,20 +100,16 @@ int enic_get_vnic_config(struct enic *enic)
 		((enic->filter_actions & FILTER_ACTION_COUNTER_FLAG) ?
 		 "count " : ""));
 
-	c->wq_desc_count =
-		min_t(uint32_t, ENIC_MAX_WQ_DESCS,
-		max_t(uint32_t, ENIC_MIN_WQ_DESCS,
-		c->wq_desc_count));
+	c->wq_desc_count = RTE_MIN((uint32_t)ENIC_MAX_WQ_DESCS,
+			RTE_MAX((uint32_t)ENIC_MIN_WQ_DESCS, c->wq_desc_count));
 	c->wq_desc_count &= 0xffffffe0; /* must be aligned to groups of 32 */
 
-	c->rq_desc_count =
-		min_t(uint32_t, ENIC_MAX_RQ_DESCS,
-		max_t(uint32_t, ENIC_MIN_RQ_DESCS,
-		c->rq_desc_count));
+	c->rq_desc_count = RTE_MIN((uint32_t)ENIC_MAX_RQ_DESCS,
+			RTE_MAX((uint32_t)ENIC_MIN_RQ_DESCS, c->rq_desc_count));
 	c->rq_desc_count &= 0xffffffe0; /* must be aligned to groups of 32 */
 
-	c->intr_timer_usec = min_t(uint32_t, c->intr_timer_usec,
-		vnic_dev_get_intr_coal_timer_max(enic->vdev));
+	c->intr_timer_usec = RTE_MIN(c->intr_timer_usec,
+				  vnic_dev_get_intr_coal_timer_max(enic->vdev));
 
 	dev_info(enic_get_dev(enic),
 		"vNIC MAC addr %02x:%02x:%02x:%02x:%02x:%02x "

drivers/net/enic/enic_res.h
@@ -11,16 +11,6 @@
 #include "vnic_wq.h"
 #include "vnic_rq.h"
 
-#define min_t(type, x, y) ({                    \
-	type __min1 = (x);                      \
-	type __min2 = (y);                      \
-	__min1 < __min2 ? __min1 : __min2; })
-
-#define max_t(type, x, y) ({                    \
-	type __max1 = (x);                      \
-	type __max2 = (y);                      \
-	__max1 > __max2 ? __max1 : __max2; })
-
 #define ENIC_MIN_WQ_DESCS		64
 #define ENIC_MAX_WQ_DESCS		4096
 #define ENIC_MIN_RQ_DESCS		64