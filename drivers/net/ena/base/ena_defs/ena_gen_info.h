bde3b46f79b2356607fd1ce74ea7b48a9d8f886c
Michal Krawczyk <mk@semihalf.com>
2020-04-08 10:29:07 +0200

    net/ena/base: update generation date and commit
    The current ena_com version was generated on 25.09.2019.
    
    Signed-off-by: Michal Krawczyk <mk@semihalf.com>
    Reviewed-by: Igor Chauskin <igorch@amazon.com>
    Reviewed-by: Guy Tzalik <gtzalik@amazon.com>

--- drivers/net/ena/base/ena_defs/ena_gen_info.h
+++ drivers/net/ena/base/ena_defs/ena_gen_info.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright (c) 2015-2019 Amazon.com, Inc. or its affiliates.
+ * Copyright (c) 2015-2020 Amazon.com, Inc. or its affiliates.
  * All rights reserved.
  */
 
-#define	ENA_GEN_DATE	"Wed Mar 20 10:40:42 STD 2019"
-#define	ENA_GEN_COMMIT	"1476830"
+#define	ENA_GEN_DATE	"Wed Sep 25 11:32:57 UTC 2019"
+#define	ENA_GEN_COMMIT	"952697a9e0d3"
