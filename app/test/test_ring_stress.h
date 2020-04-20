bf28df24e915dbe62a94e38a7c2280a8929b846b
Konstantin Ananyev <konstantin.ananyev@intel.com>
2020-04-20 13:28:22 +0100

    test/ring: add contention stress test
    Introduce stress test for ring enqueue/dequeue operations.
    Performs the following pattern on each slave worker:
    dequeue/read-write data from the dequeued objects/enqueue.
    Serves as both functional and performance test of ring
    enqueue/dequeue operations under high contention
    (for both over committed and non-over committed scenarios).
    
    Signed-off-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
    Acked-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>

---
+++ app/test/test_ring_stress.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2020 Intel Corporation
+ */
+
+
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdalign.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <rte_ring.h>
+#include <rte_cycles.h>
+#include <rte_launch.h>
+#include <rte_pause.h>
+#include <rte_random.h>
+#include <rte_malloc.h>
+#include <rte_spinlock.h>
+
+#include "test.h"
+
+struct test_case {
+	const char *name;
+	int (*func)(int (*)(void *));
+	int (*wfunc)(void *arg);
+};
+
+struct test {
+	const char *name;
+	uint32_t nb_case;
+	const struct test_case *cases;
+};
+
+extern const struct test test_ring_mpmc_stress;
