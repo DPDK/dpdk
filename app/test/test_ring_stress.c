582581d578971df78c075bcaf68f3fb0df6bc8a3
Konstantin Ananyev <konstantin.ananyev@intel.com>
2020-04-20 13:28:29 +0100

    test/ring: add stress test for MT peek API
    Introduce new test case to test MT peek API.
    
    Signed-off-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
    Acked-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>

--- app/test/test_ring_stress.c
+++ app/test/test_ring_stress.c
@@ -46,6 +46,9 @@ test_ring_stress(void)
 	n += test_ring_hts_stress.nb_case;
 	k += run_test(&test_ring_hts_stress);
 
+	n += test_ring_peek_stress.nb_case;
+	k += run_test(&test_ring_peek_stress);
+
 	printf("Number of tests:\t%u\nSuccess:\t%u\nFailed:\t%u\n",
 		n, k, n - k);
 	return (k != n);
--- app/test/test_ring_stress.h
+++ app/test/test_ring_stress.h
@@ -35,3 +35,4 @@ struct test {
 extern const struct test test_ring_mpmc_stress;
 extern const struct test test_ring_rts_stress;
 extern const struct test test_ring_hts_stress;
+extern const struct test test_ring_peek_stress;
