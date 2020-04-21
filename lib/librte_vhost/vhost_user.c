41f32b052c78898d20d04913a7e205970b96309a
Roland Qi <roland.qi@ucloud.cn>
2020-04-21 16:59:39 +0800

    vhost: fix peer close check
    In process_slave_message_reply(), there is a
    possibility that receiving a peer close
    message instead of a real message response.
    
    This patch targeting to handle the peer close
    scenario and report the correct error message.
    
    Fixes: a277c7159876 ("vhost: refactor code structure")
    Cc: stable@dpdk.org
    
    Signed-off-by: Roland Qi <roland.qi@ucloud.cn>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

--- lib/librte_vhost/vhost_user.c
+++ lib/librte_vhost/vhost_user.c
@@ -2812,11 +2812,19 @@ static int process_slave_message_reply(struct virtio_net *dev,
 	if ((msg->flags & VHOST_USER_NEED_REPLY) == 0)
 		return 0;
 
-	if (read_vhost_message(dev->slave_req_fd, &msg_reply) < 0) {
+	ret = read_vhost_message(dev->slave_req_fd, &msg_reply);
+	if (ret <= 0) {
+		if (ret < 0)
+			VHOST_LOG_CONFIG(ERR,
+				"vhost read slave message reply failed\n");
+		else
+			VHOST_LOG_CONFIG(INFO,
+				"vhost peer closed\n");
 		ret = -1;
 		goto out;
 	}
 
+	ret = 0;
 	if (msg_reply.request.slave != msg->request.slave) {
 		VHOST_LOG_CONFIG(ERR,
 			"Received unexpected msg type (%u), expected %u\n",
