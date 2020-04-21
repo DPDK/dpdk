DPDK/dpdk

commit 5ad3db8d4bdd7dfca619f7acd969f09cf6adb1df

net/ice: enable advanced RSS
This patch supports:

- symmetric hash by rte_flow RSS action.
- input set change by rte_flow RSS action.

Signed-off-by: Simei Su <simei.su@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/ice/ice_ethdev.c
@@ -2370,6 +2370,7 @@ static int ice_init_rss(struct ice_pf *pf)
 	uint16_t i, nb_q;
 	int ret = 0;
 	bool is_safe_mode = pf->adapter->is_safe_mode;
+	uint32_t reg;
 
 	rss_conf = &dev->data->dev_conf.rx_adv_conf.rss_conf;
 	nb_q = dev->data->nb_rx_queues;
@@ -2412,6 +2413,12 @@ static int ice_init_rss(struct ice_pf *pf)
 				 vsi->rss_lut, vsi->rss_lut_size);
 	if (ret)
 		return -EINVAL;
+
+	/* Enable registers for symmetric_toeplitz function. */
+	reg = ICE_READ_REG(hw, VSIQF_HASH_CTL(vsi->vsi_id));
+	reg = (reg & (~VSIQF_HASH_CTL_HASH_SCHEME_M)) |
+		(1 << VSIQF_HASH_CTL_HASH_SCHEME_S);
+	ICE_WRITE_REG(hw, VSIQF_HASH_CTL(vsi->vsi_id), reg);
 
 	/* configure RSS for IPv4 with input set IPv4 src/dst */
 	ret = ice_add_rss_cfg(hw, vsi->idx, ICE_FLOW_HASH_IPV4,

drivers/net/ice/Makefile
@@ -63,6 +63,7 @@ endif
 
 SRCS-$(CONFIG_RTE_LIBRTE_ICE_PMD) += ice_switch_filter.c
 SRCS-$(CONFIG_RTE_LIBRTE_ICE_PMD) += ice_fdir_filter.c
+SRCS-$(CONFIG_RTE_LIBRTE_ICE_PMD) += ice_hash.c
 ifeq ($(findstring RTE_MACHINE_CPUFLAG_AVX2,$(CFLAGS)),RTE_MACHINE_CPUFLAG_AVX2)
 	CC_AVX2_SUPPORT=1
 else

drivers/net/ice/meson.build
@@ -11,7 +11,8 @@ sources = files(
 	'ice_rxtx.c',
 	'ice_switch_filter.c',
 	'ice_generic_flow.c',
-	'ice_fdir_filter.c'
+	'ice_fdir_filter.c',
+	'ice_hash.c'
 	)
 
 deps += ['hash']

commit 2aa55b41359c64aecadf670d8c0fc15c7cc23df5

net/ice: fix RSS advanced rule
This patch moved the RSS initialization from dev start to dev configure
to fix RSS advanced rule invalid issue after running port stop and port
start.

Fixes: 5ad3db8d4bdd ("net/ice: enable advanced RSS")
Cc: stable@dpdk.org

Signed-off-by: Junyu Jiang <junyux.jiang@intel.com>
Tested-by: Zhiwei He <zhiwei.he@intel.com>
Acked-by: Qiming Yang <qiming.yang@intel.com>

drivers/net/ice/ice_ethdev.c
@@ -2436,24 +2436,6 @@ ice_dev_uninit(struct rte_eth_dev *dev)
 	return 0;
 }
 
-static int
-ice_dev_configure(struct rte_eth_dev *dev)
-{
-	struct ice_adapter *ad =
-		ICE_DEV_PRIVATE_TO_ADAPTER(dev->data->dev_private);
-
-	/* Initialize to TRUE. If any of Rx queues doesn't meet the
-	 * bulk allocation or vector Rx preconditions we will reset it.
-	 */
-	ad->rx_bulk_alloc_allowed = true;
-	ad->tx_simple_allowed = true;
-
-	if (dev->data->dev_conf.rxmode.mq_mode & ETH_MQ_RX_RSS_FLAG)
-		dev->data->dev_conf.rxmode.offloads |= DEV_RX_OFFLOAD_RSS_HASH;
-
-	return 0;
-}
-
 static int ice_init_rss(struct ice_pf *pf)
 {
 	struct ice_hw *hw = ICE_PF_TO_HW(pf);
@@ -2580,6 +2562,32 @@ static int ice_init_rss(struct ice_pf *pf)
 	if (ret)
 		PMD_DRV_LOG(ERR, "%s PPPoE/PPPoD_SessionID rss flow fail %d",
 				__func__, ret);
+
+	return 0;
+}
+
+static int
+ice_dev_configure(struct rte_eth_dev *dev)
+{
+	struct ice_adapter *ad =
+		ICE_DEV_PRIVATE_TO_ADAPTER(dev->data->dev_private);
+	struct ice_pf *pf = ICE_DEV_PRIVATE_TO_PF(dev->data->dev_private);
+	int ret;
+
+	/* Initialize to TRUE. If any of Rx queues doesn't meet the
+	 * bulk allocation or vector Rx preconditions we will reset it.
+	 */
+	ad->rx_bulk_alloc_allowed = true;
+	ad->tx_simple_allowed = true;
+
+	if (dev->data->dev_conf.rxmode.mq_mode & ETH_MQ_RX_RSS_FLAG)
+		dev->data->dev_conf.rxmode.offloads |= DEV_RX_OFFLOAD_RSS_HASH;
+
+	ret = ice_init_rss(pf);
+	if (ret) {
+		PMD_DRV_LOG(ERR, "Failed to enable rss for PF");
+		return ret;
+	}
 
 	return 0;
 }
@@ -2785,12 +2793,6 @@ ice_dev_start(struct rte_eth_dev *dev)
 			PMD_DRV_LOG(ERR, "fail to start Rx queue %u", nb_rxq);
 			goto rx_err;
 		}
-	}
-
-	ret = ice_init_rss(pf);
-	if (ret) {
-		PMD_DRV_LOG(ERR, "Failed to enable rss for PF");
-		goto rx_err;
 	}
 
 	ice_set_rx_function(dev);