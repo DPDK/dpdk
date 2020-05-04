52b66b2f208aea74e5fc3b004c1e14056bb22970
Mattias Rönnblom <mattias.ronnblom@ericsson.com>
2020-05-04 11:30:59 +0200

    event/dsw: avoid reusing previously recorded events
    Avoid reusing recorded events when performing a migration, since this
    may make the migration selection logic pick an already-moved flow.
    
    Fixes: f6257b22e767 ("event/dsw: add load balancing")
    Cc: stable@dpdk.org
    
    Reported-by: Venky Venkatesh <vvenkatesh@paloaltonetworks.com>
    Signed-off-by: Mattias Rönnblom <mattias.ronnblom@ericsson.com>

--- drivers/event/dsw/dsw_event.c
+++ drivers/event/dsw/dsw_event.c
@@ -821,6 +821,9 @@ dsw_port_consider_emigration(struct dsw_evdev *dsw,
 	if (dsw->num_ports == 1)
 		return;
 
+	if (seen_events_len < DSW_MAX_EVENTS_RECORDED)
+		return;
+
 	DSW_LOG_DP_PORT(DEBUG, source_port->id, "Considering emigration.\n");
 
 	/* Randomize interval to avoid having all threads considering
