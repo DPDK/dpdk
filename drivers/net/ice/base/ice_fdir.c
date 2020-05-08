f002ee9c8e26d2d7f0cda418d2565adc696584cf
Simei Su <simei.su@intel.com>
2020-05-08 09:36:50 +0800

    net/ice/base: fix flow director rule completion report
    IAVF FDIR needs to check program status from FD completion descriptors
    for both successful and failed case, rather than only ask for completion
    on error. This patch corrects completion report value so that the called
    can determine whether to report completion on error or other cases.
    
    Fixes: 55daca4e45fc ("net/ice/base: change function to static")
    
    Signed-off-by: Simei Su <simei.su@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/ice/base/ice_fdir.c
+++ drivers/net/ice/base/ice_fdir.c
@@ -481,7 +481,7 @@ ice_fdir_get_prgm_desc(struct ice_hw *hw, struct ice_fdir_fltr *input,
 		ICE_FXD_FLTR_QW1_PCMD_REMOVE;
 	fdir_fltr_ctx.swap = ICE_FXD_FLTR_QW1_SWAP_NOT_SET;
 	fdir_fltr_ctx.comp_q = ICE_FXD_FLTR_QW0_COMP_Q_ZERO;
-	fdir_fltr_ctx.comp_report = ICE_FXD_FLTR_QW0_COMP_REPORT_SW_FAIL;
+	fdir_fltr_ctx.comp_report = ICE_FXD_FLTR_QW0_COMP_REPORT_SW;
 	fdir_fltr_ctx.fdid_prio = input->fdid_prio;
 	fdir_fltr_ctx.desc_prof = 1;
 	fdir_fltr_ctx.desc_prof_prio = 3;
