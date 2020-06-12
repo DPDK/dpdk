e0e1496be5fcabd31807ef9981c421963a9f8578
Rahul Lakkireddy <rahul.lakkireddy@chelsio.com>
2020-06-13 03:37:24 +0530

    net/cxgbe: fix L2T leak in filter error and free path
    Free up Layer 2 Table (L2T) entry properly during filter create
    failure and filter delete.
    
    Fixes: 1decc62b1cbe ("net/cxgbe: add flow operations to offload VLAN actions")
    Cc: stable@dpdk.org
    
    Signed-off-by: Rahul Lakkireddy <rahul.lakkireddy@chelsio.com>

--- drivers/net/cxgbe/cxgbe_filter.c
+++ drivers/net/cxgbe/cxgbe_filter.c
@@ -293,6 +293,9 @@ static void clear_filter(struct filter_entry *f)
 	if (f->clipt)
 		cxgbe_clip_release(f->dev, f->clipt);
 
+	if (f->l2t)
+		cxgbe_l2t_release(f->l2t);
+
 	/* The zeroing of the filter rule below clears the filter valid,
 	 * pending, locked flags etc. so it's all we need for
 	 * this operation.
@@ -755,20 +758,6 @@ static int set_filter_wr(struct rte_eth_dev *dev, unsigned int fidx)
 	unsigned int port_id = ethdev2pinfo(dev)->port_id;
 	int ret;
 
-	/*
-	 * If the new filter requires loopback Destination MAC and/or VLAN
-	 * rewriting then we need to allocate a Layer 2 Table (L2T) entry for
-	 * the filter.
-	 */
-	if (f->fs.newvlan || f->fs.newdmac) {
-		/* allocate L2T entry for new filter */
-		f->l2t = cxgbe_l2t_alloc_switching(f->dev, f->fs.vlan,
-						   f->fs.eport, f->fs.dmac);
-
-		if (!f->l2t)
-			return -ENOMEM;
-	}
-
 	/* If the new filter requires Source MAC rewriting then we need to
 	 * allocate a SMT entry for the filter
 	 */
@@ -1083,6 +1072,19 @@ int cxgbe_set_filter(struct rte_eth_dev *dev, unsigned int filter_id,
 	    memcmp(f->fs.val.lip, bitoff, sizeof(bitoff))) {
 		f->clipt = cxgbe_clip_alloc(dev, (u32 *)&f->fs.val.lip);
 		if (!f->clipt) {
+			ret = -ENOMEM;
+			goto free_tid;
+		}
+	}
+
+	/* If the new filter requires loopback Destination MAC and/or VLAN
+	 * rewriting then we need to allocate a Layer 2 Table (L2T) entry for
+	 * the filter.
+	 */
+	if (f->fs.newvlan || f->fs.newdmac) {
+		f->l2t = cxgbe_l2t_alloc_switching(f->dev, f->fs.vlan,
+						   f->fs.eport, f->fs.dmac);
+		if (!f->l2t) {
 			ret = -ENOMEM;
 			goto free_tid;
 		}
