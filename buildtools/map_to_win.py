309bf90bf99e1e3b41e2abfdfd0f15f8299034b8
Tal Shnaiderman <talshn@mellanox.com>
2020-06-29 15:37:41 +0300

    build: generate version map file for MinGW
    The MinGW build for Windows has special cases where exported
    function contain additional prefix:
    
    __emutls_v.per_lcore__*
    
    To avoid adding those prefixed functions to the version.map file
    the map_to_def.py script was modified to create a map file for MinGW
    with the needed changed.
    
    The file name was changed to map_to_win.py and lib/meson.build map output
    was unified with drivers/meson.build output
    
    Signed-off-by: Tal Shnaiderman <talshn@mellanox.com>

--- buildtools/map_to_win.py
+++ buildtools/map_to_win.py
@@ -10,11 +10,20 @@ from os.path import dirname, basename, join, exists
 def is_function_line(ln):
     return ln.startswith('\t') and ln.endswith(';\n') and ":" not in ln
 
+# MinGW keeps the original .map file but replaces per_lcore* to __emutls_v.per_lcore*
+def create_mingw_map_file(input_map, output_map):
+    with open(input_map) as f_in, open(output_map, 'w') as f_out:
+        f_out.writelines([lines.replace('per_lcore', '__emutls_v.per_lcore') for lines in f_in.readlines()])
 
 def main(args):
     if not args[1].endswith('version.map') or \
-            not args[2].endswith('exports.def'):
+            not args[2].endswith('exports.def') and \
+            not args[2].endswith('mingw.map'):
         return 1
+
+    if args[2].endswith('mingw.map'):
+        create_mingw_map_file(args[1], args[2])
+        return 0
 
 # special case, allow override if an def file already exists alongside map file
     override_file = join(dirname(args[1]), basename(args[2]))
