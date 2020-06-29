8517072c8715556968b59e5872f580c7cad2e53c
Tal Shnaiderman <talshn@mellanox.com>
2020-06-29 15:37:36 +0300

    pci: fix address domain format size
    the struct rte_pci_addr defines domain as uint32_t variable however
    the PCI_PRI_FMT macro used for logging the struct sets the format
    of domain to uint16_t.
    
    The mismatch causes the following warning messages
    in Windows clang build:
    
    format specifies type 'unsigned short' but the argument
    has type 'uint32_t' (aka 'unsigned int') [-Wformat]
    
    Fixes: af75078fece3 ("first public release")
    Cc: stable@dpdk.org
    
    Signed-off-by: Tal Shnaiderman <talshn@mellanox.com>

--- lib/librte_pci/rte_pci.h
+++ lib/librte_pci/rte_pci.h
@@ -23,7 +23,7 @@ extern "C" {
 #include <sys/types.h>
 
 /** Formatting string for PCI device identifier: Ex: 0000:00:01.0 */
-#define PCI_PRI_FMT "%.4" PRIx16 ":%.2" PRIx8 ":%.2" PRIx8 ".%" PRIx8
+#define PCI_PRI_FMT "%.4" PRIx32 ":%.2" PRIx8 ":%.2" PRIx8 ".%" PRIx8
 #define PCI_PRI_STR_SIZE sizeof("XXXXXXXX:XX:XX.X")
 
 /** Short formatting string, without domain, for PCI device: Ex: 00:01.0 */
