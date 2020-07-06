4e408146c8a1036ad8d6c2c1f72f9c97a7d6537d
Guinan Sun <guinanx.sun@intel.com>
2020-07-06 08:12:11 +0000

    net/e1000/base: expose more future extended NVM
    Future extended NVM 5 (five) required for a Dynamic Power Gating
    control in the MAC.
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Signed-off-by: Guinan Sun <guinanx.sun@intel.com>
    Reviewed-by: Wei Zhao <wei.zhao1@intel.com>

--- drivers/net/e1000/base/e1000_regs.h
+++ drivers/net/e1000/base/e1000_regs.h
@@ -35,6 +35,7 @@
 #define E1000_FEXTNVM	0x00028  /* Future Extended NVM - RW */
 #define E1000_FEXTNVM3	0x0003C  /* Future Extended NVM 3 - RW */
 #define E1000_FEXTNVM4	0x00024  /* Future Extended NVM 4 - RW */
+#define E1000_FEXTNVM5	0x00014  /* Future Extended NVM 5 - RW */
 #define E1000_FEXTNVM6	0x00010  /* Future Extended NVM 6 - RW */
 #define E1000_FEXTNVM7	0x000E4  /* Future Extended NVM 7 - RW */
 #define E1000_FEXTNVM9	0x5BB4  /* Future Extended NVM 9 - RW */
