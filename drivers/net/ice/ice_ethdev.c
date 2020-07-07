DPDK/dpdk

commit 68a1ab82ad74403de83eadbecd8c2ffdd5f63ee8

net/ice: speed up to retrieve EEPROM
Replace ice_read_sr_word with ice_read_sr_buf in ice_get_eeprom.

Fixes: d0dd1c8e1997 ("net/ice: support EEPROM information getting")
Cc: stable@dpdk.org

Signed-off-by: Leyi Rong <leyi.rong@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/ice/ice_ethdev.c
@@ -2814,26 +2814,26 @@ ice_get_eeprom(struct rte_eth_dev *dev,
 {
 	struct ice_hw *hw = ICE_DEV_PRIVATE_TO_HW(dev->data->dev_private);
 	uint16_t *data = eeprom->data;
-	uint16_t offset, length, i;
-	enum ice_status ret_code = ICE_SUCCESS;
+	uint16_t first_word, last_word, nwords;
+	enum ice_status status = ICE_SUCCESS;
 
-	offset = eeprom->offset >> 1;
-	length = eeprom->length >> 1;
+	first_word = eeprom->offset >> 1;
+	last_word = (eeprom->offset + eeprom->length - 1) >> 1;
+	nwords = last_word - first_word + 1;
 
-	if (offset > hw->nvm.sr_words ||
-	    offset + length > hw->nvm.sr_words) {
+	if (first_word > hw->nvm.sr_words ||
+	    last_word > hw->nvm.sr_words) {
 		PMD_DRV_LOG(ERR, "Requested EEPROM bytes out of range.");
 		return -EINVAL;
 	}
 
 	eeprom->magic = hw->vendor_id | (hw->device_id << 16);
 
-	for (i = 0; i < length; i++) {
-		ret_code = ice_read_sr_word(hw, offset + i, &data[i]);
-		if (ret_code != ICE_SUCCESS) {
-			PMD_DRV_LOG(ERR, "EEPROM read failed.");
-			return -EIO;
-		}
+	status = ice_read_sr_buf(hw, first_word, &nwords, data);
+	if (status) {
+		PMD_DRV_LOG(ERR, "EEPROM read failed.");
+		eeprom->length = sizeof(uint16_t) * nwords;
+		return -EIO;
 	}
 
 	return 0;

commit 01186263c2c79b9a22d3f3b1eeec844c6552fdc7

net/ice: fix EEPROM data
Kernel driver reads EEPROM data from flash but DPDK reads from
shadow ram. This patch fixes the issue by changing method to get
EEPROM data from flash.

Fixes: 68a1ab82ad74 ("net/ice: speed up to retrieve EEPROM")
Cc: stable@dpdk.org

Signed-off-by: Shougang Wang <shougangx.wang@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/ice/ice_ethdev.c
@@ -3979,8 +3979,7 @@ ice_get_eeprom_length(struct rte_eth_dev *dev)
 {
 	struct ice_hw *hw = ICE_DEV_PRIVATE_TO_HW(dev->data->dev_private);
 
-	/* Convert word count to byte count */
-	return hw->nvm.sr_words << 1;
+	return hw->nvm.flash_size;
 }
 
 static int
@@ -3988,26 +3987,24 @@ ice_get_eeprom(struct rte_eth_dev *dev,
 	       struct rte_dev_eeprom_info *eeprom)
 {
 	struct ice_hw *hw = ICE_DEV_PRIVATE_TO_HW(dev->data->dev_private);
-	uint16_t *data = eeprom->data;
-	uint16_t first_word, last_word, nwords;
 	enum ice_status status = ICE_SUCCESS;
-
-	first_word = eeprom->offset >> 1;
-	last_word = (eeprom->offset + eeprom->length - 1) >> 1;
-	nwords = last_word - first_word + 1;
-
-	if (first_word >= hw->nvm.sr_words ||
-	    last_word >= hw->nvm.sr_words) {
-		PMD_DRV_LOG(ERR, "Requested EEPROM bytes out of range.");
-		return -EINVAL;
-	}
+	uint8_t *data = eeprom->data;
 
 	eeprom->magic = hw->vendor_id | (hw->device_id << 16);
 
-	status = ice_read_sr_buf(hw, first_word, &nwords, data);
+	status = ice_acquire_nvm(hw, ICE_RES_READ);
+	if (status) {
+		PMD_DRV_LOG(ERR, "acquire nvm failed.");
+		return -EIO;
+	}
+
+	status = ice_read_flat_nvm(hw, eeprom->offset, &eeprom->length,
+				   data, false);
+
+	ice_release_nvm(hw);
+
 	if (status) {
 		PMD_DRV_LOG(ERR, "EEPROM read failed.");
-		eeprom->length = sizeof(uint16_t) * nwords;
 		return -EIO;
 	}
 