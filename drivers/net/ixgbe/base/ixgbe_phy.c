390445ec30b4c52a3d2887c3d2a202d9cf37ea8e
Guinan Sun <guinanx.sun@intel.com>
2020-07-09 08:00:35 +0000

    net/ixgbe/base: move increments after evaluations
    The retry variable was being incremented before it was evaluated by the
    subsequent conditional against the maximum retries to figure out which
    message to print.  So we'll move the increment op to the end.
    
    Signed-off-by: Jeb Cramer <jeb.j.cramer@intel.com>
    Signed-off-by: Guinan Sun <guinanx.sun@intel.com>
    Reviewed-by: Wei Zhao <wei.zhao1@intel.com>

--- drivers/net/ixgbe/base/ixgbe_phy.c
+++ drivers/net/ixgbe/base/ixgbe_phy.c
@@ -138,12 +138,12 @@ fail:
 		ixgbe_i2c_bus_clear(hw);
 		if (lock)
 			hw->mac.ops.release_swfw_sync(hw, swfw_mask);
-		retry++;
 		if (retry < max_retry)
 			DEBUGOUT("I2C byte read combined error - Retrying.\n");
 		else
 			DEBUGOUT("I2C byte read combined error.\n");
-	} while (retry < max_retry);
+		retry++;
+	} while (retry <= max_retry);
 
 	return IXGBE_ERR_I2C;
 }
@@ -203,12 +203,12 @@ fail:
 		ixgbe_i2c_bus_clear(hw);
 		if (lock)
 			hw->mac.ops.release_swfw_sync(hw, swfw_mask);
-		retry++;
 		if (retry < max_retry)
 			DEBUGOUT("I2C byte write combined error - Retrying.\n");
 		else
 			DEBUGOUT("I2C byte write combined error.\n");
-	} while (retry < max_retry);
+		retry++;
+	} while (retry <= max_retry);
 
 	return IXGBE_ERR_I2C;
 }
@@ -2057,13 +2057,12 @@ fail:
 			hw->mac.ops.release_swfw_sync(hw, swfw_mask);
 			msec_delay(100);
 		}
-		retry++;
 		if (retry < max_retry)
 			DEBUGOUT("I2C byte read error - Retrying.\n");
 		else
 			DEBUGOUT("I2C byte read error.\n");
-
-	} while (retry < max_retry);
+		retry++;
+	} while (retry <= max_retry);
 
 	return status;
 }
@@ -2161,12 +2160,12 @@ STATIC s32 ixgbe_write_i2c_byte_generic_int(struct ixgbe_hw *hw, u8 byte_offset,
 
 fail:
 		ixgbe_i2c_bus_clear(hw);
-		retry++;
 		if (retry < max_retry)
 			DEBUGOUT("I2C byte write error - Retrying.\n");
 		else
 			DEBUGOUT("I2C byte write error.\n");
-	} while (retry < max_retry);
+		retry++;
+	} while (retry <= max_retry);
 
 	if (lock)
 		hw->mac.ops.release_swfw_sync(hw, swfw_mask);
