b071f1fa8825f51a9e81921f05f31c36e57c43cf
David Marchand <david.marchand@redhat.com>
2020-07-16 09:26:48 +0200

    devtools: avoid explicit experimental build flag
    -DALLOW_EXPERIMENTAL_API is always set for in-tree compilation since
    https://git.dpdk.org/dpdk/commit/?id=acec04c4b2f5
    
    Warn people not to copy/paste this flag that was needed before.
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- devtools/checkpatches.sh
+++ devtools/checkpatches.sh
@@ -110,6 +110,14 @@ check_forbidden_additions() { # <patch>
 		-f $(dirname $(readlink -f $0))/check-forbidden-tokens.awk \
 		"$1" || res=1
 
+	# forbid use of experimental build flag except in examples
+	awk -v FOLDERS='lib drivers app' \
+		-v EXPRESSIONS='-DALLOW_EXPERIMENTAL_API allow_experimental_apis' \
+		-v RET_ON_FAIL=1 \
+		-v MESSAGE='Using experimental build flag for in-tree compilation' \
+		-f $(dirname $(readlink -f $0))/check-forbidden-tokens.awk \
+		"$1" || res=1
+
 	# svg figures must be included with wildcard extension
 	# because of png conversion for pdf docs
 	awk -v FOLDERS='doc' \
