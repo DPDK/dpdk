16f0d03098cb1ae71a375581e692c929557686cd
Fady Bader <fady@mellanox.com>
2020-07-23 10:08:25 +0300

    net: build on Windows
    librte_net was not compiling under Windows.
    To solve this, needed header files are added.
    
    Signed-off-by: Fady Bader <fady@mellanox.com>
    Acked-by: Ranjit Menon <ranjit.menon@intel.com>
    Tested-by: Pallavi Kadam <pallavi.kadam@intel.com>
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>

---
+++ lib/librte_eal/windows/include/netinet/in.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020 Mellanox Technologies, Ltd
+ */
+
+#ifndef _IN_H_
+#define _IN_H_
+
+#define IPPROTO_IP         0
+#define IPPROTO_HOPOPTS    0
+#define IPPROTO_ICMP       1
+#define IPPROTO_IPIP       4
+#define IPPROTO_TCP        6
+#define IPPROTO_UDP       17
+#define IPPROTO_IPV6      41
+#define IPPROTO_ROUTING   43
+#define IPPROTO_FRAGMENT  44
+#define IPPROTO_GRE       47
+#define IPPROTO_ESP       50
+#define IPPROTO_AH        51
+#define IPPROTO_ICMPV6    58
+#define IPPROTO_NONE      59
+#define IPPROTO_DSTOPTS   60
+#define IPPROTO_SCTP     132
+
+#endif
