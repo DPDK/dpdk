c6048730ea7cc16b087ec428fe33e014278013ab
Qi Zhang <qi.z.zhang@intel.com>
2020-08-26 20:05:49 +0800

    net/ice/base: refactor DCB related variables
    In this patch, the DCB related variables will be refactored out of the
    ice_port_info_struct. The goal is to make the ice_port_info struct
    cleaner.
    
    Signed-off-by: Chinh T Cao <chinh.t.cao@intel.com>
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>
    Acked-by: Qiming Yang <qiming.yang@intel.com>

--- drivers/net/ice/base/ice_type.h
+++ drivers/net/ice/base/ice_type.h
@@ -753,6 +753,14 @@ struct ice_dcbx_cfg {
 #define ICE_DCBX_APPS_NON_WILLING	0x1
 };
 
+struct ice_qos_cfg {
+	struct ice_dcbx_cfg local_dcbx_cfg;	/* Oper/Local Cfg */
+	struct ice_dcbx_cfg desired_dcbx_cfg;	/* CEE Desired Cfg */
+	struct ice_dcbx_cfg remote_dcbx_cfg;	/* Peer Cfg */
+	u8 dcbx_status : 3;			/* see ICE_DCBX_STATUS_DIS */
+	u8 is_sw_lldp : 1;
+};
+
 struct ice_port_info {
 	struct ice_sched_node *root;	/* Root Node per Port */
 	struct ice_hw *hw;		/* back pointer to HW instance */
@@ -778,13 +786,7 @@ struct ice_port_info {
 	struct LIST_HEAD_TYPE rl_prof_list[ICE_AQC_TOPO_MAX_LEVEL_NUM];
 	struct ice_bw_type_info root_node_bw_t_info;
 	struct ice_bw_type_info tc_node_bw_t_info[ICE_MAX_TRAFFIC_CLASS];
-	struct ice_dcbx_cfg local_dcbx_cfg;	/* Oper/Local Cfg */
-	/* DCBX info */
-	struct ice_dcbx_cfg remote_dcbx_cfg;	/* Peer Cfg */
-	struct ice_dcbx_cfg desired_dcbx_cfg;	/* CEE Desired Cfg */
-	/* LLDP/DCBX Status */
-	u8 dcbx_status:3;		/* see ICE_DCBX_STATUS_DIS */
-	u8 is_sw_lldp:1;
+	struct ice_qos_cfg qos_cfg;
 	u8 is_vf:1;
 };
 
