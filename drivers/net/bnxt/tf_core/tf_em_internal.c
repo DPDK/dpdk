f01b6fceac77d9649aed46daf231b334f6a9dc94
Mike Baucom <michael.baucom@broadcom.com>
2020-09-10 18:55:44 -0700

    net/bnxt: free EM index on failure
    When a Exact Match entry fails insertion, the allocated index needs to
    be pushed back to the allocation stack. This patch takes care of that.
    
    Signed-off-by: Mike Baucom <michael.baucom@broadcom.com>
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/tf_core/tf_em_internal.c
+++ drivers/net/bnxt/tf_core/tf_em_internal.c
@@ -175,8 +175,11 @@ tf_em_insert_int_entry(struct tf *tfp,
 					     &rptr_index,
 					     &rptr_entry,
 					     &num_of_entries);
-	if (rc)
+	if (rc) {
+		/* Free the allocated index before returning */
+		stack_push(pool, index);
 		return -1;
+	}
 
 	PMD_DRV_LOG
 		  (DEBUG,
