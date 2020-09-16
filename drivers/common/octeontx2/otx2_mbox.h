e590825d0bde2bb9c5c15b2f6973c622faf91dc2
Harman Kalra <hkalra@marvell.com>
2020-09-16 23:18:19 +0530

    common/octeontx2: upgrade mbox definition to version 9
    Update mail box data structures to sync with af driver mbox
    changes done to retrieve VF's base steering rule.
    
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>
    Signed-off-by: Harman Kalra <hkalra@marvell.com>

--- drivers/common/octeontx2/otx2_mbox.h
+++ drivers/common/octeontx2/otx2_mbox.h
@@ -90,7 +90,7 @@ struct mbox_msghdr {
 #define OTX2_MBOX_RSP_SIG (0xbeef)
 	/* Signature, for validating corrupted msgs */
 	uint16_t __otx2_io sig;
-#define OTX2_MBOX_VERSION (0x0007)
+#define OTX2_MBOX_VERSION (0x0009)
 	/* Version of msg's structure for this ID */
 	uint16_t __otx2_io ver;
 	/* Offset of next msg within mailbox region */
@@ -245,6 +245,8 @@ M(NPC_MCAM_READ_ENTRY,	  0x600f, npc_mcam_read_entry,			\
 M(NPC_SET_PKIND,          0x6010, npc_set_pkind,                        \
 				  npc_set_pkind,                        \
 				  msg_rsp)                              \
+M(NPC_MCAM_READ_BASE_RULE, 0x6011, npc_read_base_steer_rule, msg_req,   \
+				   npc_mcam_read_base_rule_rsp)         \
 /* NIX mbox IDs (range 0x8000 - 0xFFFF) */				\
 M(NIX_LF_ALLOC,		0x8000, nix_lf_alloc, nix_lf_alloc_req,		\
 				nix_lf_alloc_rsp)			\
@@ -767,6 +769,7 @@ struct nix_lf_alloc_rsp {
 	uint8_t __otx2_io cgx_links;  /* No. of CGX links present in HW */
 	uint8_t __otx2_io lbk_links;  /* No. of LBK links present in HW */
 	uint8_t __otx2_io sdp_links;  /* No. of SDP links present in HW */
+	uint8_t __otx2_io tx_link;    /* Transmit channel link number */
 };
 
 struct nix_lf_free_req {
@@ -1542,6 +1545,11 @@ struct npc_mcam_read_entry_rsp {
 	struct mcam_entry entry_data;
 	uint8_t __otx2_io intf;
 	uint8_t __otx2_io enable;
+};
+
+struct npc_mcam_read_base_rule_rsp {
+	struct mbox_msghdr hdr;
+	struct mcam_entry entry_data;
 };
 
 /* TIM mailbox error codes
