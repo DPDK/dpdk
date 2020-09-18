b29890822981ccb1baa64737a218dc416a079404
Qi Zhang <qi.z.zhang@intel.com>
2020-09-18 13:13:38 +0800

    net/ice/base: add more capability to admin queue
    Add below 3 new capability to "Get Capabilities" AQ commands
    0x000A and 0x000B.
    
    ICE_AQC_CAPS_IWARP
    ICE_AQC_CAPS_PCIE_RESET_AVOIDANCE
    ICE_AQC_CAPS_NVM_MGMT
    
    Signed-off-by: Shay Amir <shay.amir@intel.com>
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>
    Acked-by: Qiming Yang <qiming.yang@intel.com>

--- drivers/net/ice/base/ice_adminq_cmd.h
+++ drivers/net/ice/base/ice_adminq_cmd.h
@@ -109,6 +109,9 @@ struct ice_aqc_list_caps_elem {
 #define ICE_AQC_CAPS_MSIX				0x0043
 #define ICE_AQC_CAPS_FD					0x0045
 #define ICE_AQC_CAPS_MAX_MTU				0x0047
+#define ICE_AQC_CAPS_IWARP				0x0051
+#define ICE_AQC_CAPS_PCIE_RESET_AVOIDANCE		0x0076
+#define ICE_AQC_CAPS_NVM_MGMT				0x0080
 
 	u8 major_ver;
 	u8 minor_ver;
