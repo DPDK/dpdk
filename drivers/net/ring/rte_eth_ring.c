3cce95f35ec64a76f44ff708058fb25e7b96ada2
Dumitru Ceara <dceara@redhat.com>
2020-09-28 20:47:29 +0200

    net/ring: advertise multi segment Tx and scatter Rx
    Even though ring interfaces don't support any other TX/RX offloads they
    do support sending multi segment packets and this should be advertised
    in order to not break applications that use ring interfaces.
    
    Also advertise scatter RX support.
    
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- drivers/net/ring/rte_eth_ring.c
+++ drivers/net/ring/rte_eth_ring.c
@@ -160,6 +160,8 @@ eth_dev_info(struct rte_eth_dev *dev,
 	dev_info->max_mac_addrs = 1;
 	dev_info->max_rx_pktlen = (uint32_t)-1;
 	dev_info->max_rx_queues = (uint16_t)internals->max_rx_queues;
+	dev_info->rx_offload_capa = DEV_RX_OFFLOAD_SCATTER;
+	dev_info->tx_offload_capa = DEV_TX_OFFLOAD_MULTI_SEGS;
 	dev_info->max_tx_queues = (uint16_t)internals->max_tx_queues;
 	dev_info->min_rx_bufsize = 0;
 
