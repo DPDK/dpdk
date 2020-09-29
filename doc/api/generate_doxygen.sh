606fb48f3d648e2e819eb1026f433aa549190008
Bruce Richardson <bruce.richardson@intel.com>
2020-09-29 17:54:57 +0100

    doc: hide verbose doxygen standard output
    The standard output of doxygen is very verbose, and since ninja mixes
    stdout and stderr together it makes it difficult to see any warnings from
    the doxygen run. Therefore, we can just log the standard output to file,
    and only output the stderr to make warnings clear.
    
    Suggested-by: Thomas Monjalon <thomas@monjalon.net>
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>

--- doc/api/generate_doxygen.sh
+++ doc/api/generate_doxygen.sh
@@ -7,7 +7,7 @@ OUTDIR=$2
 SCRIPTCSS=$3
 
 # run doxygen, capturing all the header files it processed
-doxygen "${DOXYCONF}" | tee doxygen.out
+doxygen "${DOXYCONF}" > doxygen.out
 echo "$OUTDIR: $(awk '/Preprocessing/ {printf("%s ", substr($2, 1, length($2) - 3))}' doxygen.out)" > $OUTDIR.d
 
 "${SCRIPTCSS}" "${OUTDIR}"/doxygen.css
