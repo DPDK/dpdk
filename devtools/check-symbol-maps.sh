2941600108cdf47a618368e63339d845f0df6cd8
David Marchand <david.marchand@redhat.com>
2020-10-16 12:27:11 +0200

    devtools: check Windows export files
    Updating export files (supposed to disappear at some point, but still
    there) might be missed when removing symbols in the API / map files.
    Add a check for this case.
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Thomas Monjalon <thomas@monjalon.net>

--- devtools/check-symbol-maps.sh
+++ devtools/check-symbol-maps.sh
@@ -7,6 +7,8 @@ cd $(dirname $0)/..
 # speed up by ignoring Unicode details
 export LC_ALL=C
 
+ret=0
+
 find_orphan_symbols ()
 {
     for map in $(find lib drivers -name '*.map') ; do
@@ -30,5 +32,24 @@ orphan_symbols=$(find_orphan_symbols)
 if [ -n "$orphan_symbols" ] ; then
     echo "Found only in symbol map file:"
     echo "$orphan_symbols" | sed 's,^,\t,'
-    exit 1
+    ret=1
 fi
+
+find_orphan_windows_symbols ()
+{
+    for def in $(find lib drivers -name '*_exports.def') ; do
+        map=${def/_exports.def}_version.map
+        for sym in $(grep -v ^EXPORTS $def); do
+            grep -q $sym $map || echo $sym
+        done
+    done
+}
+
+orphan_windows_symbols=$(find_orphan_windows_symbols)
+if [ -n "$orphan_windows_symbols" ] ; then
+    echo "Found only in Windows export file:"
+    echo "$orphan_windows_symbols" | sed 's,^,\t,'
+    ret=1
+fi
+
+exit $ret
