ef3da1e7671fb4ccd66601619f478e84c8100be0
Timothy McDaniel <timothy.mcdaniel@intel.com>
2020-11-01 17:37:42 -0600

    event/dlb2: add dynamic logging
    This commit adds base support for dynamic logging.
    The default log level is NOTICE. Dynamic logging
    is used exclusively throughout this patchset.
    
    Signed-off-by: Timothy McDaniel <timothy.mcdaniel@intel.com>
    Reviewed-by: Gage Eads <gage.eads@intel.com>

---
+++ drivers/event/dlb2/dlb2.c
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2016-2020 Intel Corporation
+ */
+
+#include <rte_log.h>
+
+RTE_LOG_REGISTER(eventdev_dlb2_log_level, pmd.event.dlb2, NOTICE);
