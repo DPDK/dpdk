4a38856458861810635020a6f76c3c006f8b7715
Junfeng Guo <junfeng.guo@intel.com>
2020-11-10 16:45:03 +0800

    net/ice: fix some RSS hash fields
    Previous code in ice_rss_hash_set has mismatched hash fields and
    headers for UDP and TCP.
    
    Fixes: 16187528a923 ("net/ice/base: refactor RSS configure API")
    
    Signed-off-by: Junfeng Guo <junfeng.guo@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/ice/ice_ethdev.c
+++ drivers/net/ice/ice_ethdev.c
@@ -3068,7 +3068,7 @@ ice_rss_hash_set(struct ice_pf *pf, uint64_t rss_hf)
 	if (rss_hf & ETH_RSS_NONFRAG_IPV4_UDP) {
 		cfg.addl_hdrs = ICE_FLOW_SEG_HDR_GTPU_IP | ICE_FLOW_SEG_HDR_UDP |
 				ICE_FLOW_SEG_HDR_IPV4 | ICE_FLOW_SEG_HDR_IPV_OTHER;
-		cfg.hash_flds = ICE_FLOW_HASH_IPV4;
+		cfg.hash_flds = ICE_HASH_UDP_IPV4;
 		ret = ice_add_rss_cfg_wrap(pf, vsi->idx, &cfg);
 		if (ret)
 			PMD_DRV_LOG(ERR, "%s GTPU_IPV4_UDP rss flow fail %d",
@@ -3092,7 +3092,7 @@ ice_rss_hash_set(struct ice_pf *pf, uint64_t rss_hf)
 	if (rss_hf & ETH_RSS_NONFRAG_IPV6_UDP) {
 		cfg.addl_hdrs = ICE_FLOW_SEG_HDR_GTPU_IP | ICE_FLOW_SEG_HDR_UDP |
 				ICE_FLOW_SEG_HDR_IPV6 | ICE_FLOW_SEG_HDR_IPV_OTHER;
-		cfg.hash_flds = ICE_FLOW_HASH_IPV6;
+		cfg.hash_flds = ICE_HASH_UDP_IPV6;
 		ret = ice_add_rss_cfg_wrap(pf, vsi->idx, &cfg);
 		if (ret)
 			PMD_DRV_LOG(ERR, "%s GTPU_IPV6_UDP rss flow fail %d",
@@ -3116,7 +3116,7 @@ ice_rss_hash_set(struct ice_pf *pf, uint64_t rss_hf)
 	if (rss_hf & ETH_RSS_NONFRAG_IPV4_TCP) {
 		cfg.addl_hdrs = ICE_FLOW_SEG_HDR_GTPU_IP | ICE_FLOW_SEG_HDR_TCP |
 				ICE_FLOW_SEG_HDR_IPV4 | ICE_FLOW_SEG_HDR_IPV_OTHER;
-		cfg.hash_flds = ICE_FLOW_HASH_IPV4;
+		cfg.hash_flds = ICE_HASH_TCP_IPV4;
 		ret = ice_add_rss_cfg_wrap(pf, vsi->idx, &cfg);
 		if (ret)
 			PMD_DRV_LOG(ERR, "%s GTPU_IPV4_TCP rss flow fail %d",
@@ -3140,7 +3140,7 @@ ice_rss_hash_set(struct ice_pf *pf, uint64_t rss_hf)
 	if (rss_hf & ETH_RSS_NONFRAG_IPV6_TCP) {
 		cfg.addl_hdrs = ICE_FLOW_SEG_HDR_GTPU_IP | ICE_FLOW_SEG_HDR_TCP |
 				ICE_FLOW_SEG_HDR_IPV6 | ICE_FLOW_SEG_HDR_IPV_OTHER;
-		cfg.hash_flds = ICE_FLOW_HASH_IPV6;
+		cfg.hash_flds = ICE_HASH_TCP_IPV6;
 		ret = ice_add_rss_cfg_wrap(pf, vsi->idx, &cfg);
 		if (ret)
 			PMD_DRV_LOG(ERR, "%s GTPU_IPV6_TCP rss flow fail %d",
