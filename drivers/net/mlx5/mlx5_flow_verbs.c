DPDK/dpdk

commit d51475d1bfa520f10e676b1f3273d138c421ca51

net/mlx5: support item type error message in flow Verbs
Update the flow verbs error message to "item type X not supported",
when it is not supported,
instead of a generic error message "item not supported".

Signed-off-by: Li Zhang <lizh@nvidia.com>
Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

drivers/net/mlx5/mlx5_flow_verbs.c
@@ -1248,6 +1248,7 @@ flow_verbs_validate(struct rte_eth_dev *dev,
 	uint64_t last_item = 0;
 	uint8_t next_protocol = 0xff;
 	uint16_t ether_type = 0;
+	char errstr[32];
 
 	if (items == NULL)
 		return -1;
@@ -1399,9 +1400,11 @@ flow_verbs_validate(struct rte_eth_dev *dev,
 			last_item = MLX5_FLOW_LAYER_MPLS;
 			break;
 		default:
+			snprintf(errstr, sizeof(errstr), "item type %d not supported",
+				 items->type);
 			return rte_flow_error_set(error, ENOTSUP,
 						  RTE_FLOW_ERROR_TYPE_ITEM,
-						  NULL, "item not supported");
+						  NULL, errstr);
 		}
 		item_flags |= last_item;
 	}
@@ -1695,6 +1698,7 @@ flow_verbs_translate(struct rte_eth_dev *dev,
 	struct mlx5_flow_rss_desc *rss_desc = &((struct mlx5_flow_rss_desc *)
 					      priv->rss_desc)
 					      [!!priv->flow_nested_idx];
+	char errstr[32];
 
 	if (priority == MLX5_FLOW_PRIO_RSVD)
 		priority = priv->config.flow_prio - 1;
@@ -1840,10 +1844,11 @@ flow_verbs_translate(struct rte_eth_dev *dev,
 			item_flags |= MLX5_FLOW_LAYER_MPLS;
 			break;
 		default:
+			snprintf(errstr, sizeof(errstr), "item type %d not supported",
+				 items->type);
 			return rte_flow_error_set(error, ENOTSUP,
 						  RTE_FLOW_ERROR_TYPE_ITEM,
-						  NULL,
-						  "item not supported");
+						  NULL, errstr);
 		}
 	}
 	dev_flow->handle->layers = item_flags;

commit 58df16e08cd55cf7aa3de416117f720a4f166035

net/mlx5: fix use of local array for global error
Recent patch uses a local string array as input for function
rte_flow_error_set().
This stack memory may be later used by other code sections,
overwriting the desired error string.

This patch implements an error string for the specific case
requested, of ICMP item not supported in Verbs flow engine.

Fixes: d51475d1bfa5 ("net/mlx5: support item type error message in flow Verbs")

Signed-off-by: Dekel Peled <dekelp@nvidia.com>
Acked-by: Matan Azrad <matan@nvidia.com>

drivers/net/mlx5/mlx5_flow_verbs.c
@@ -1247,7 +1247,6 @@ flow_verbs_validate(struct rte_eth_dev *dev,
 	uint64_t last_item = 0;
 	uint8_t next_protocol = 0xff;
 	uint16_t ether_type = 0;
-	char errstr[32];
 
 	if (items == NULL)
 		return -1;
@@ -1398,12 +1397,16 @@ flow_verbs_validate(struct rte_eth_dev *dev,
 				return ret;
 			last_item = MLX5_FLOW_LAYER_MPLS;
 			break;
-		default:
-			snprintf(errstr, sizeof(errstr), "item type %d not supported",
-				 items->type);
+		case RTE_FLOW_ITEM_TYPE_ICMP:
+		case RTE_FLOW_ITEM_TYPE_ICMP6:
 			return rte_flow_error_set(error, ENOTSUP,
 						  RTE_FLOW_ERROR_TYPE_ITEM,
-						  NULL, errstr);
+						  NULL, "ICMP/ICMP6 "
+						  "item not supported");
+		default:
+			return rte_flow_error_set(error, ENOTSUP,
+						  RTE_FLOW_ERROR_TYPE_ITEM,
+						  NULL, "item not supported");
 		}
 		item_flags |= last_item;
 	}
@@ -1698,7 +1701,6 @@ flow_verbs_translate(struct rte_eth_dev *dev,
 	struct mlx5_priv *priv = dev->data->dev_private;
 	struct mlx5_flow_workspace *wks = mlx5_flow_get_thread_workspace();
 	struct mlx5_flow_rss_desc *rss_desc;
-	char errstr[32];
 
 	MLX5_ASSERT(wks);
 	rss_desc = &wks->rss_desc[!!wks->flow_nested_idx];
@@ -1846,11 +1848,9 @@ flow_verbs_translate(struct rte_eth_dev *dev,
 			item_flags |= MLX5_FLOW_LAYER_MPLS;
 			break;
 		default:
-			snprintf(errstr, sizeof(errstr), "item type %d not supported",
-				 items->type);
 			return rte_flow_error_set(error, ENOTSUP,
 						  RTE_FLOW_ERROR_TYPE_ITEM,
-						  NULL, errstr);
+						  NULL, "item not supported");
 		}
 	}
 	dev_flow->handle->layers = item_flags;