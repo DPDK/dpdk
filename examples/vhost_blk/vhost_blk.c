9c3586a6436d4df4bc12ad01d1c5130d24b09bd6
David Marchand <david.marchand@redhat.com>
2020-11-14 10:05:31 +0100

    examples/vhost_blk: fix build with pkg-config
    vhost_blk.c: In function ‘ctrlr_worker’:
    vhost_blk.c:543:2: warning: implicit declaration of function ‘CPU_ZERO’
    [-Wimplicit-function-declaration]
      543 |  CPU_ZERO(&cpuset);
          |  ^~~~~~~~
    vhost_blk.c:544:2: warning: implicit declaration of function ‘CPU_SET’
    [-Wimplicit-function-declaration]
      544 |  CPU_SET(0, &cpuset);
          |  ^~~~~~~
    vhost_blk.c:545:2: warning: implicit declaration of function
    ‘pthread_setaffinity_np’ [-Wimplicit-function-declaration]
      545 |  pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
          |  ^~~~~~~~~~~~~~~~~~~~~~
    /usr/bin/ld: /tmp/cczpiMWH.o: in function `ctrlr_worker':
    vhost_blk.c:(.text+0x1076): undefined reference to `CPU_ZERO'
    /usr/bin/ld: vhost_blk.c:(.text+0x1082): undefined reference to
    `CPU_SET'
    collect2: error: ld returned 1 exit status
    gmake: *** [Makefile:34: build/vhost-blk-shared] Error 1
    
    Explicitly pass _GNU_SOURCE and include missing headers (rather than
    rely on automagic inclusion from other system headers).
    
    Fixes: c19beb3f38cd ("examples/vhost_blk: introduce vhost storage sample")
    Cc: stable@dpdk.org
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- examples/vhost_blk/vhost_blk.c
+++ examples/vhost_blk/vhost_blk.c
@@ -2,6 +2,12 @@
  * Copyright(c) 2010-2019 Intel Corporation
  */
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <pthread.h>
+#include <sched.h>
+
 #include <stdint.h>
 #include <unistd.h>
 #include <stdbool.h>
