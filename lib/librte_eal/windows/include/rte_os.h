DPDK/dpdk

commit c917b54b0c743c7a570a4d8df8954c61d8b818c4

eal/windows: add definition of ETOOMANYREFS
The ETOOMANYREFS errno was missing from the Windows build.
It is used in initialization of flow error structures.

It is defined with the same error code used by WSAETOOMANYREFS.

Signed-off-by: Tal Shnaiderman <talshn@nvidia.com>
Acked-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>

lib/librte_eal/windows/include/rte_os.h
@@ -51,6 +51,8 @@ extern "C" {
 /* as in <windows.h> */
 typedef long long ssize_t;
 
+#define ETOOMANYREFS 10059 /* WSAETOOMANYREFS */
+
 #ifndef RTE_TOOLCHAIN_GCC
 
 static inline int

commit ba5b133e3348dd7c266791cbb456519a27b7c08c

eal/windows: remove definition of ETOOMANYREFS
The definition of ETOOMANYREFS is reverted as it breaks build of
external applications already defining it.

Fixes: c917b54b0c74 ("eal/windows: add definition of ETOOMANYREFS")

Signed-off-by: Tal Shnaiderman <talshn@nvidia.com>
Reviewed-by: Nick Connolly <nick.connolly@mayadata.io>
Acked-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>

lib/librte_eal/windows/include/rte_os.h
@@ -51,8 +51,6 @@ extern "C" {
 /* as in <windows.h> */
 typedef long long ssize_t;
 
-#define ETOOMANYREFS 10059 /* WSAETOOMANYREFS */
-
 #ifndef RTE_TOOLCHAIN_GCC
 
 static inline int