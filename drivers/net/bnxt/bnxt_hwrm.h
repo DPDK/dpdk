265d1a2bb869ce5f2b3d0d8d6f96c76d3f3b65e2
Ajit Khaparde <ajit.khaparde@broadcom.com>
2020-12-04 15:22:50 -0800

    net/bnxt: remove function declaration
    Remove an orphan function declaration.
    It has not been defined yet and hence not used anywhere.
    
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Acked-by: Lance Richardson <lance.richardson@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.h
+++ drivers/net/bnxt/bnxt_hwrm.h
@@ -288,8 +288,6 @@ int bnxt_hwrm_cfa_counter_qstats(struct bnxt *bp,
 				 enum bnxt_flow_dir dir,
 				 uint16_t cntr,
 				 uint16_t num_entries);
-int bnxt_hwrm_get_dflt_vnic_id(struct bnxt *bp, uint16_t fid,
-			       uint16_t *vnic_id);
 int bnxt_hwrm_get_dflt_vnic_svif(struct bnxt *bp, uint16_t fid,
 				 uint16_t *vnic_id, uint16_t *svif);
 int bnxt_hwrm_parent_pf_qcfg(struct bnxt *bp);
