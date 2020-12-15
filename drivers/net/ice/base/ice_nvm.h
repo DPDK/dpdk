b1d08cb258cd3e7757f2727381f59fa66d9d78b5
Qi Zhang <qi.z.zhang@intel.com>
2020-12-15 10:39:28 +0800

    net/ice/base: read option ROM combo version from CIVD
    The driver currently reads the combo image version data from within the
    Boot Configuration TLV block of the PFA area of the NVM. This allows
    access to the active Option ROM version data, assuming that it has been
    properly copied into this section.
    
    There is no equivalent method for reading the Option ROM version data
    from a pending Option ROM update, as it will not yet have been copied
    into the PFA boot configuration block. Instead, replace this
    implementation with one which scans for the CIVD data section of the
    Option ROM image data.
    
    This CIVD data is stored in a packed structured format within the Option
    ROM. It is always aligned to a 512 byte boundary, and starts with
    a special '$CIV' 4-byte signature. Data integrity is checked using
    a simple modulo 256 sum of the structure bytes.
    
    Implement a new ice_get_orom_civd_data function which allows reading
    from the selected flash bank (active or inactive), and scans for valid
    CIVD data. Use this instead of the boot configuration TLV in order to
    report the combo version data of precisely what is in the Option ROM
    data.
    
    To allow access to reading the inactive Option ROM bank, introduce a new
    ice_get_inactive_orom_ver function. Use of a new function is done in
    order to avoid leaking the bank selection abstraction outside of
    ice_nvm.c
    
    With this new function, the driver can now read and display the version
    of the to-be-activated Option ROM when an update has been initiated but
    not yet finalized.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>
    Acked-by: Qiming Yang <qiming.yang@intel.com>

--- drivers/net/ice/base/ice_nvm.h
+++ drivers/net/ice/base/ice_nvm.h
@@ -26,6 +26,17 @@
 #define ICE_NVM_REG_RW_MODULE	0x0
 #define ICE_NVM_REG_RW_FLAGS	0x1
 
+#pragma pack(1)
+struct ice_orom_civd_info {
+	u8 signature[4];	/* Must match ASCII '$CIV' characters */
+	u8 checksum;		/* Simple modulo 256 sum of all structure bytes must equal 0 */
+	__le32 combo_ver;	/* Combo Image Version number */
+	u8 combo_name_len;	/* Length of the unicode combo image version string, max of 32 */
+	__le16 combo_name[32];	/* Unicode string representing the Combo Image version */
+};
+
+#pragma pack()
+
 #define ICE_NVM_ACCESS_MAJOR_VER	0
 #define ICE_NVM_ACCESS_MINOR_VER	5
 
@@ -97,6 +108,8 @@ ice_read_flat_nvm(struct ice_hw *hw, u32 offset, u32 *length, u8 *data,
 enum ice_status
 ice_get_pfa_module_tlv(struct ice_hw *hw, u16 *module_tlv, u16 *module_tlv_len,
 		       u16 module_type);
+enum ice_status
+ice_get_inactive_orom_ver(struct ice_hw *hw, struct ice_orom_info *orom);
 enum ice_status
 ice_read_pba_string(struct ice_hw *hw, u8 *pba_num, u32 pba_num_size);
 enum ice_status ice_init_nvm(struct ice_hw *hw);
