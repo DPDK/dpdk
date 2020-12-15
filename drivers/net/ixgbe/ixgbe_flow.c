06cad275fecf4e1892b076796065942cdc1ef44e
Dapeng Yu <dapengx.yu@intel.com>
2020-12-15 18:10:31 +0800

    net/ixgbe: fix flex bytes flow director rule
    When a flexbytes flow director rule is created, the FDIRCTRL.FLEX_OFFSET
    register is set, and it keeps its affect even after the flow director
    flexbytes rule is destroyed, causing packets to be transferred to the
    wrong place.
    
    It is because setting FDIRCTRL shall only be permitted on Flow Director
    initialization flow or clearing the Flow Director table according to the
    datasheet, otherwise device may behave unexpectedly.
    
    In order to evade this limitation, simulate the Flow Director
    initialization flow or clearing the Flow Director table by setting
    FDIRCMD.CLEARHT to 0x1B and then clear it back to 0x0B.
    
    Fixes: f35fec63dde1 ("net/ixgbe: enable flex bytes for generic flow API")
    Cc: stable@dpdk.org
    
    Signed-off-by: Dapeng Yu <dapengx.yu@intel.com>
    Tested-by: Jun W Zhou <junx.w.zhou@intel.com>
    Acked-by: Jeff Guo <jia.guo@intel.com>

--- drivers/net/ixgbe/ixgbe_flow.c
+++ drivers/net/ixgbe/ixgbe_flow.c
@@ -3137,13 +3137,13 @@ ixgbe_flow_create(struct rte_eth_dev *dev,
 				rte_memcpy(&fdir_info->mask,
 					&fdir_rule.mask,
 					sizeof(struct ixgbe_hw_fdir_mask));
-				fdir_info->flex_bytes_offset =
-					fdir_rule.flex_bytes_offset;
 
-				if (fdir_rule.mask.flex_bytes_mask)
-					ixgbe_fdir_set_flexbytes_offset(dev,
+				if (fdir_rule.mask.flex_bytes_mask) {
+					ret = ixgbe_fdir_set_flexbytes_offset(dev,
 						fdir_rule.flex_bytes_offset);
-
+					if (ret)
+						goto out;
+				}
 				ret = ixgbe_fdir_set_input_mask(dev);
 				if (ret)
 					goto out;
@@ -3161,8 +3161,9 @@ ixgbe_flow_create(struct rte_eth_dev *dev,
 				if (ret)
 					goto out;
 
-				if (fdir_info->flex_bytes_offset !=
-						fdir_rule.flex_bytes_offset)
+				if (fdir_rule.mask.flex_bytes_mask &&
+				    fdir_info->flex_bytes_offset !=
+				    fdir_rule.flex_bytes_offset)
 					goto out;
 			}
 		}
