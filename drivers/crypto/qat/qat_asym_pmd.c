87086b3398edf20d46628481a2959a40ad95b477
Vladimir Medvedkin <vladimir.medvedkin@intel.com>
2020-12-15 18:23:51 +0000

    crypto/qat: fix access to uninitialized variable
    QAT_LOG() has access to the uninitialized variable "name"
    
    Fixes: 21792c443205 ("crypto/qat: add multi-process handling of driver ID")
    Cc: stable@dpdk.org
    
    Signed-off-by: Vladimir Medvedkin <vladimir.medvedkin@intel.com>
    Acked-by: Arek Kusztal <arkadiuszx.kusztal@intel.com>
    Acked-by: Declan Doherty <declan.doherty@intel.com>

--- drivers/crypto/qat/qat_asym_pmd.c
+++ drivers/crypto/qat/qat_asym_pmd.c
@@ -251,6 +251,10 @@ qat_asym_dev_create(struct qat_pci_device *qat_pci_dev,
 	struct rte_cryptodev *cryptodev;
 	struct qat_asym_dev_private *internals;
 
+	snprintf(name, RTE_CRYPTODEV_NAME_MAX_LEN, "%s_%s",
+			qat_pci_dev->name, "asym");
+	QAT_LOG(DEBUG, "Creating QAT ASYM device %s\n", name);
+
 	if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
 		qat_pci_dev->qat_asym_driver_id =
 				qat_asym_driver_id;
@@ -263,10 +267,6 @@ qat_asym_dev_create(struct qat_pci_device *qat_pci_dev,
 			return -(EFAULT);
 		}
 	}
-
-	snprintf(name, RTE_CRYPTODEV_NAME_MAX_LEN, "%s_%s",
-			qat_pci_dev->name, "asym");
-	QAT_LOG(DEBUG, "Creating QAT ASYM device %s\n", name);
 
 	/* Populate subset device to use in cryptodev device creation */
 	qat_dev_instance->asym_rte_dev.driver = &cryptodev_qat_asym_driver;
