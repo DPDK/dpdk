7a7a9907bf919101ead845c7b9a4b29aa67a2fb6
Ori Kam <orika@nvidia.com>
2021-01-07 18:57:11 +0200

    regex/mlx5: fix number of supported queues
    The RegEx engine has no limitation on number of queues.
    This commits modifies the max supported queues reported to the application.
    
    Fixes: fbc8c7003b93 ("regex/mlx5: add completion queue creation")
    Cc: stable@dpdk.org
    
    Signed-off-by: Ori Kam <orika@nvidia.com>

--- drivers/regex/mlx5/mlx5_rxp.c
+++ drivers/regex/mlx5/mlx5_rxp.c
@@ -103,11 +103,10 @@ mlx5_regex_info_get(struct rte_regexdev *dev __rte_unused,
 	info->max_payload_size = MLX5_REGEX_MAX_PAYLOAD_SIZE;
 	info->max_rules_per_group = MLX5_REGEX_MAX_RULES_PER_GROUP;
 	info->max_groups = MLX5_REGEX_MAX_GROUPS;
-	info->max_queue_pairs = 1;
 	info->regexdev_capa = RTE_REGEXDEV_SUPP_PCRE_GREEDY_F |
 			      RTE_REGEXDEV_CAPA_QUEUE_PAIR_OOS_F;
 	info->rule_flags = 0;
-	info->max_queue_pairs = 10;
+	info->max_queue_pairs = UINT16_MAX;
 	return 0;
 }
 
