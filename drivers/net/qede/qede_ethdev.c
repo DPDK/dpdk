e51e0295b6ed6357d541238b4d55f64dfd6eac66
Steve Yang <stevex.yang@intel.com>
2021-01-18 07:04:19 +0000

    net/qede: fix jumbo frame flag condition for MTU set
    [ upstream commit f9a69f8b5e94cae87d96ca5c160261687e0b9ec2 ]
    
    The jumbo frame uses the 'RTE_ETHER_MAX_LEN' as boundary condition,
    but the Ether overhead is larger than 18 when it supports dual VLAN tags.
    That will cause the jumbo flag rx offload is wrong when MTU size is
    'RTE_ETHER_MTU'.
    
    This fix will change the boundary condition with 'RTE_ETHER_MTU' and
    overhead, that perhaps impacts the cases of the jumbo frame related.
    
    Fixes: 200645ac7909 ("net/qede: set MTU")
    
    Signed-off-by: Steve Yang <stevex.yang@intel.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- drivers/net/qede/qede_ethdev.c
+++ drivers/net/qede/qede_ethdev.c
@@ -2369,7 +2369,7 @@ static int qede_set_mtu(struct rte_eth_dev *dev, uint16_t mtu)
 			fp->rxq->rx_buf_size = rc;
 		}
 	}
-	if (max_rx_pkt_len > RTE_ETHER_MAX_LEN)
+	if (frame_size > QEDE_ETH_MAX_LEN)
 		dev->data->dev_conf.rxmode.offloads |= DEV_RX_OFFLOAD_JUMBO_FRAME;
 	else
 		dev->data->dev_conf.rxmode.offloads &= ~DEV_RX_OFFLOAD_JUMBO_FRAME;
