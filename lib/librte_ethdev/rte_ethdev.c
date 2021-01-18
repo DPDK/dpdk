be78a4db33176cc20cea883d2702972ac60aa8b4
Steve Yang <stevex.yang@intel.com>
2021-01-18 07:04:07 +0000

    ethdev: fix max Rx packet length check
    [ upstream commit bf0f90d92d3010431c9187ed9a2b412030cded4e ]
    
    Ethdev is using default Ethernet overhead to decide if provided
    'max_rx_pkt_len' value is bigger than max (non jumbo) MTU value,
    and limits it to MAX if it is.
    
    Since the application/driver used Ethernet overhead is different than
    the ethdev one, check result is wrong.
    
    If the driver is using Ethernet overhead bigger than the default one,
    the provided 'max_rx_pkt_len' is trimmed down, and in the driver when
    correct Ethernet overhead is used to convert back, the resulting MTU is
    less than the intended one, causing some packets to be dropped.
    
    Like,
    app     -> max_rx_pkt_len = 1500/*mtu*/ + 22/*overhead*/ = 1522
    ethdev  -> 1522 > 1518/*MAX*/; max_rx_pkt_len = 1518
    driver  -> MTU = 1518 - 22 = 1496
    Packets with size 1497-1500 are dropped although intention is to be able
    to send/receive them.
    
    The fix is to make ethdev use the correct Ethernet overhead for port,
    instead of default one.
    
    Fixes: 59d0ecdbf0e1 ("ethdev: MTU accessors")
    
    Signed-off-by: Steve Yang <stevex.yang@intel.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- lib/librte_ethdev/rte_ethdev.c
+++ lib/librte_ethdev/rte_ethdev.c
@@ -1292,8 +1292,10 @@ rte_eth_dev_configure(uint16_t port_id, uint16_t nb_rx_q, uint16_t nb_tx_q,
 	struct rte_eth_dev *dev;
 	struct rte_eth_dev_info dev_info;
 	struct rte_eth_conf orig_conf;
+	uint16_t overhead_len;
 	int diag;
 	int ret;
+	uint16_t old_mtu;
 
 	RTE_ETH_VALID_PORTID_OR_ERR_RET(port_id, -ENODEV);
 
@@ -1319,9 +1321,19 @@ rte_eth_dev_configure(uint16_t port_id, uint16_t nb_rx_q, uint16_t nb_tx_q,
 		memcpy(&dev->data->dev_conf, dev_conf,
 		       sizeof(dev->data->dev_conf));
 
+	/* Backup mtu for rollback */
+	old_mtu = dev->data->mtu;
+
 	ret = rte_eth_dev_info_get(port_id, &dev_info);
 	if (ret != 0)
 		goto rollback;
+
+	/* Get the real Ethernet overhead length */
+	if (dev_info.max_mtu != UINT16_MAX &&
+	    dev_info.max_rx_pktlen > dev_info.max_mtu)
+		overhead_len = dev_info.max_rx_pktlen - dev_info.max_mtu;
+	else
+		overhead_len = RTE_ETHER_HDR_LEN + RTE_ETHER_CRC_LEN;
 
 	/* If number of queues specified by application for both Rx and Tx is
 	 * zero, use driver preferred values. This cannot be done individually
@@ -1409,12 +1421,17 @@ rte_eth_dev_configure(uint16_t port_id, uint16_t nb_rx_q, uint16_t nb_tx_q,
 			ret = -EINVAL;
 			goto rollback;
 		}
+
+		/* Scale the MTU size to adapt max_rx_pkt_len */
+		dev->data->mtu = dev->data->dev_conf.rxmode.max_rx_pkt_len -
+				overhead_len;
 	} else {
-		if (dev_conf->rxmode.max_rx_pkt_len < RTE_ETHER_MIN_LEN ||
-			dev_conf->rxmode.max_rx_pkt_len > RTE_ETHER_MAX_LEN)
+		uint16_t pktlen = dev_conf->rxmode.max_rx_pkt_len;
+		if (pktlen < RTE_ETHER_MIN_MTU + overhead_len ||
+		    pktlen > RTE_ETHER_MTU + overhead_len)
 			/* Use default value */
 			dev->data->dev_conf.rxmode.max_rx_pkt_len =
-							RTE_ETHER_MAX_LEN;
+						RTE_ETHER_MTU + overhead_len;
 	}
 
 	/*
@@ -1549,6 +1566,8 @@ reset_queues:
 	eth_dev_tx_queue_config(dev, 0);
 rollback:
 	memcpy(&dev->data->dev_conf, &orig_conf, sizeof(dev->data->dev_conf));
+	if (old_mtu != dev->data->mtu)
+		dev->data->mtu = old_mtu;
 
 	rte_ethdev_trace_configure(port_id, nb_rx_q, nb_tx_q, dev_conf, ret);
 	return ret;
