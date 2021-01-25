3feefc80ac3a24e0aba9d3730dd1fa4ea1bf121a
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-01-26 01:12:50 +0300

    buildtools: use build directory for temporary files
    Use current build directory as base for temporary directories,
    so that all build files are isolated there.
    
    Fixes: e6e9730c7066 ("buildtools: support object file extraction for Windows")
    
    Signed-off-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>

--- buildtools/gen-pmdinfo-cfile.py
+++ buildtools/gen-pmdinfo-cfile.py
@@ -7,8 +7,8 @@ import subprocess
 import sys
 import tempfile
 
-_, ar, archive, output, *pmdinfogen = sys.argv
-with tempfile.TemporaryDirectory() as temp:
+_, tmp_root, ar, archive, output, *pmdinfogen = sys.argv
+with tempfile.TemporaryDirectory(dir=tmp_root) as temp:
     proc = subprocess.run(
         # Don't use "ar p", because its output is corrupted on Windows.
         [ar, "xv", os.path.abspath(archive)], stdout=subprocess.PIPE, check=True, cwd=temp
