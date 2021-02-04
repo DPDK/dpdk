DPDK/dpdk

commit c8d4b02f72aefda2dbe2895cd661c29161a6770d

net/virtio: move legacy IO to virtio PCI
This patch moves Virtio PCI legacy IO handling to
virtio_pci.c. Two functions are created so that
virtio_pci_ethdev does not have to care about it.

Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
Reviewed-by: Chenbo Xia <chenbo.xia@intel.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

drivers/net/virtio/virtio_pci.c
@@ -31,6 +31,15 @@
 #define VIRTIO_PCI_CONFIG(hw) \
 		(((hw)->use_msix == VIRTIO_MSIX_ENABLED) ? 24 : 20)
 
+
+struct virtio_pci_internal {
+	struct rte_pci_ioport io;
+};
+
+#define VTPCI_IO(hw) (&virtio_pci_internal[(hw)->port_id].io)
+
+struct virtio_pci_internal virtio_pci_internal[RTE_MAX_ETHPORTS];
+
 static inline int
 check_vq_phys_addr_ok(struct virtqueue *vq)
 {
@@ -300,7 +309,9 @@ legacy_notify_queue(struct virtio_hw *hw, struct virtqueue *vq)
 static void
 legacy_intr_detect(struct virtio_hw *hw)
 {
-	hw->use_msix = vtpci_msix_detect(VTPCI_DEV(hw));
+	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
+
+	hw->use_msix = vtpci_msix_detect(dev->pci_dev);
 }
 
 static int
@@ -558,7 +569,9 @@ modern_notify_queue(struct virtio_hw *hw, struct virtqueue *vq)
 static void
 modern_intr_detect(struct virtio_hw *hw)
 {
-	hw->use_msix = vtpci_msix_detect(VTPCI_DEV(hw));
+	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
+
+	hw->use_msix = vtpci_msix_detect(dev->pci_dev);
 }
 
 static int
@@ -857,3 +870,14 @@ msix_detect:
 	return 0;
 }
 
+void vtpci_legacy_ioport_unmap(struct virtio_hw *hw)
+{
+	rte_pci_ioport_unmap(VTPCI_IO(hw));
+}
+
+int vtpci_legacy_ioport_map(struct virtio_hw *hw)
+{
+	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
+
+	return rte_pci_ioport_map(dev->pci_dev, 0, VTPCI_IO(hw));
+}

drivers/net/virtio/virtio_pci.h
@@ -298,17 +298,11 @@ struct virtio_pci_dev {
  */
 struct virtio_hw_internal {
 	const struct virtio_pci_ops *vtpci_ops;
-	struct rte_pci_ioport io;
-	struct rte_pci_device *dev;
 };
 
 #define VTPCI_OPS(hw)	(virtio_hw_internal[(hw)->port_id].vtpci_ops)
-#define VTPCI_IO(hw)	(&virtio_hw_internal[(hw)->port_id].io)
-#define VTPCI_DEV(hw)	(virtio_hw_internal[(hw)->port_id].dev)
-
 
 extern struct virtio_hw_internal virtio_hw_internal[RTE_MAX_ETHPORTS];
-
 
 /*
  * This structure is just a reference to read
@@ -381,6 +375,9 @@ void vtpci_write_dev_config(struct virtio_hw *, size_t, const void *, int);
 void vtpci_read_dev_config(struct virtio_hw *, size_t, void *, int);
 
 uint8_t vtpci_isr(struct virtio_hw *);
+
+void vtpci_legacy_ioport_unmap(struct virtio_hw *hw);
+int vtpci_legacy_ioport_map(struct virtio_hw *hw);
 
 extern const struct virtio_pci_ops legacy_ops;
 extern const struct virtio_pci_ops modern_ops;

drivers/net/virtio/virtio_pci_ethdev.c
@@ -60,7 +60,7 @@ virtio_remap_pci(struct rte_pci_device *pci_dev, struct virtio_pci_dev *dev)
 			return -1;
 		}
 	} else {
-		if (rte_pci_ioport_map(pci_dev, 0, VTPCI_IO(hw)) < 0)
+		if (vtpci_legacy_ioport_map(hw) < 0)
 			return -1;
 	}
 
@@ -74,8 +74,6 @@ eth_virtio_pci_init(struct rte_eth_dev *eth_dev)
 	struct virtio_hw *hw = &dev->hw;
 	struct rte_pci_device *pci_dev = RTE_ETH_DEV_TO_PCI(eth_dev);
 	int ret;
-
-	VTPCI_DEV(hw) = pci_dev;
 
 	if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
 		ret = vtpci_init(RTE_ETH_DEV_TO_PCI(eth_dev), dev);
@@ -111,7 +109,7 @@ eth_virtio_pci_init(struct rte_eth_dev *eth_dev)
 err_unmap:
 	rte_pci_unmap_device(RTE_ETH_DEV_TO_PCI(eth_dev));
 	if (!dev->modern)
-		rte_pci_ioport_unmap(VTPCI_IO(hw));
+		vtpci_legacy_ioport_unmap(hw);
 
 	return ret;
 }

commit aa0d4b8a0186060d452d37f715236b4b31fd3752

net/virtio: fix secondary process crash with PCI devices
The Virtio rework series mistakenly moved the rte_pci_device
pointer to struct virtio_hw, which is shared between the two
processes. But this structure is per-process, so this change
made secondary process to try accessing primary process-only
memory, leading to a crash.

This patch reverts to proper behavior, by storing the
rte_pci_device pointer into the per-process
virtio_pci_internal struct. It also provides helper to get
the pointer from the virtio_hw struct pointer.

Bugzilla ID: 633
Fixes: c8d4b02f72ae ("net/virtio: move legacy IO to virtio PCI")

Reported-by: Anatoly Burakov <anatoly.burakov@intel.com>
Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

drivers/net/virtio/virtio_pci.c
@@ -31,13 +31,6 @@
 #define VIRTIO_PCI_CONFIG(dev) \
 		(((dev)->msix_status == VIRTIO_MSIX_ENABLED) ? 24 : 20)
 
-
-struct virtio_pci_internal {
-	struct rte_pci_ioport io;
-};
-
-#define VTPCI_IO(hw) (&virtio_pci_internal[(hw)->port_id].io)
-
 struct virtio_pci_internal virtio_pci_internal[RTE_MAX_ETHPORTS];
 
 static inline int
@@ -313,16 +306,14 @@ legacy_intr_detect(struct virtio_hw *hw)
 {
 	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
 
-	dev->msix_status = vtpci_msix_detect(dev->pci_dev);
+	dev->msix_status = vtpci_msix_detect(VTPCI_DEV(hw));
 	hw->intr_lsc = !!dev->msix_status;
 }
 
 static int
 legacy_dev_close(struct virtio_hw *hw)
 {
-	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
-
-	rte_pci_unmap_device(dev->pci_dev);
+	rte_pci_unmap_device(VTPCI_DEV(hw));
 	rte_pci_ioport_unmap(VTPCI_IO(hw));
 
 	return 0;
@@ -574,16 +565,14 @@ modern_intr_detect(struct virtio_hw *hw)
 {
 	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
 
-	dev->msix_status = vtpci_msix_detect(dev->pci_dev);
+	dev->msix_status = vtpci_msix_detect(VTPCI_DEV(hw));
 	hw->intr_lsc = !!dev->msix_status;
 }
 
 static int
 modern_dev_close(struct virtio_hw *hw)
 {
-	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
-
-	rte_pci_unmap_device(dev->pci_dev);
+	rte_pci_unmap_device(VTPCI_DEV(hw));
 
 	return 0;
 }
@@ -772,8 +761,6 @@ vtpci_init(struct rte_pci_device *pci_dev, struct virtio_pci_dev *dev)
 
 	RTE_BUILD_BUG_ON(offsetof(struct virtio_pci_dev, hw) != 0);
 
-	dev->pci_dev = pci_dev;
-
 	/*
 	 * Try if we can succeed reading virtio pci caps, which exists
 	 * only on modern pci device. If failed, we fallback to legacy
@@ -816,7 +803,5 @@ void vtpci_legacy_ioport_unmap(struct virtio_hw *hw)
 
 int vtpci_legacy_ioport_map(struct virtio_hw *hw)
 {
-	struct virtio_pci_dev *dev = virtio_pci_get_dev(hw);
-
-	return rte_pci_ioport_map(dev->pci_dev, 0, VTPCI_IO(hw));
+	return rte_pci_ioport_map(VTPCI_DEV(hw), 0, VTPCI_IO(hw));
 }

drivers/net/virtio/virtio_pci.h
@@ -104,7 +104,6 @@ enum virtio_msix_status {
 
 struct virtio_pci_dev {
 	struct virtio_hw hw;
-	struct rte_pci_device *pci_dev;
 	struct virtio_pci_common_cfg *common_cfg;
 	struct virtio_net_config *dev_cfg;
 	enum virtio_msix_status msix_status;
@@ -115,6 +114,17 @@ struct virtio_pci_dev {
 };
 
 #define virtio_pci_get_dev(hwp) container_of(hwp, struct virtio_pci_dev, hw)
+
+struct virtio_pci_internal {
+	struct rte_pci_ioport io;
+	struct rte_pci_device *dev;
+};
+
+extern struct virtio_pci_internal virtio_pci_internal[RTE_MAX_ETHPORTS];
+
+#define VTPCI_IO(hw) (&virtio_pci_internal[(hw)->port_id].io)
+#define VTPCI_DEV(hw) (virtio_pci_internal[(hw)->port_id].dev)
+
 
 /*
  * How many bits to shift physical queue address written to QUEUE_PFN.

drivers/net/virtio/virtio_pci_ethdev.c
@@ -78,12 +78,14 @@ eth_virtio_pci_init(struct rte_eth_dev *eth_dev)
 
 	if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
 		hw->port_id = eth_dev->data->port_id;
+		VTPCI_DEV(hw) = pci_dev;
 		ret = vtpci_init(RTE_ETH_DEV_TO_PCI(eth_dev), dev);
 		if (ret) {
 			PMD_INIT_LOG(ERR, "Failed to init PCI device\n");
 			return -1;
 		}
 	} else {
+		VTPCI_DEV(hw) = pci_dev;
 		if (dev->modern)
 			VIRTIO_OPS(hw) = &modern_ops;
 		else