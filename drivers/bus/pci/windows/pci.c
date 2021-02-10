75c0d71c9f47c541375815b9b02d9185e32b10e1
Pallavi Kadam <pallavi.kadam@intel.com>
2021-02-10 12:36:54 -0800

    bus/pci: skip probing some Windows NDIS devices
    [ upstream commit de04405b336ed01e234db39fc211b4592b79c2b3 ]
    
    Implement rte_pci_map_device() to distinguish between the devices bound
    to netuio and NDIS devices.
    Only return success for the netuio devices.
    
    Fixes: c76ec01b4591 ("bus/pci: support netuio on Windows")
    
    Suggested-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
    Signed-off-by: Pallavi Kadam <pallavi.kadam@intel.com>
    Reviewed-by: Ranjit Menon <ranjit.menon@intel.com>
    Acked-by: Tal Shnaiderman <talshn@nvidia.com>
    Acked-by: Narcisa Vasile <navasile@linux.microsoft.com>
    Tested-by: Narcisa Vasile <navasile@linux.microsoft.com>

--- drivers/bus/pci/windows/pci.c
+++ drivers/bus/pci/windows/pci.c
@@ -23,20 +23,22 @@ DEFINE_DEVPROPKEY(DEVPKEY_Device_Numa_Node, 0x540b947e, 0x8b40, 0x45bc,
  * the registry hive for PCI devices.
  */
 
-/* The functions below are not implemented on Windows,
+/* Some of the functions below are not implemented on Windows,
  * but need to be defined for compilation purposes
  */
 
 /* Map pci device */
 int
-rte_pci_map_device(struct rte_pci_device *dev __rte_unused)
+rte_pci_map_device(struct rte_pci_device *dev)
 {
-	/* This function is not implemented on Windows.
-	 * We really should short-circuit the call to these functions by
-	 * clearing the RTE_PCI_DRV_NEED_MAPPING flag
-	 * in the rte_pci_driver flags.
+	/* Only return success for devices bound to netuio.
+	 * Devices that are bound to netuio are mapped at
+	 * the bus probing stage.
 	 */
-	return 0;
+	if (dev->kdrv == RTE_PCI_KDRV_NIC_UIO)
+		return 0;
+	else
+		return -1;
 }
 
 /* Unmap pci device */
