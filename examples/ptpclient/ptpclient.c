512393d56e52da7d5916b129a272cac337ee06da
Min Hu (Connor) <humin29@huawei.com>
2021-03-27 15:36:26 +0800

    examples/ptpclient: remove wrong comment
    [ upstream commit e1d10a42f6e0e44b07ea7936f6704b93a8d2a1ef ]
    
    This patch deletes the comments which are wrong and unnecessary.
    
    Fixes: ab129e9065a5 ("examples/ptpclient: add minimal PTP client")
    
    Signed-off-by: Min Hu (Connor) <humin29@huawei.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- examples/ptpclient/ptpclient.c
+++ examples/ptpclient/ptpclient.c
@@ -603,10 +603,6 @@ lcore_main(void)
 	unsigned nb_rx;
 	struct rte_mbuf *m;
 
-	/*
-	 * Check that the port is on the same NUMA node as the polling thread
-	 * for best performance.
-	 */
 	printf("\nCore %u Waiting for SYNC packets. [Ctrl+C to quit]\n",
 			rte_lcore_id());
 
