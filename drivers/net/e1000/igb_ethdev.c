b1fab4dc0d5c372e21b2e497d50414ded0bac30e
Alvin Zhang <alvinx.zhang@intel.com>
2021-04-01 13:21:32 +0800

    net/e1000: fix Rx error counter for bad length
    [ upstream commit 7b0a60dd394969fb17409519fd673e61b05f284e ]
    
    When the size of a packet in Rx channel is less than the minimum
    or greater than the maximum, the packet will be simultaneously
    counted by RLEC(Receive Length Error Count) and
    RUC(Receive Under Size Count)/ROC(Receive Oversize Count) registers.
    
    This patch fixes the issue of counting a length error packet twice
    when counting the total number of received error packets.
    
    Fixes: 70bdb18657da ("ethdev: add Rx error counters for missed, badcrc and badlen packets")
    
    Signed-off-by: Alvin Zhang <alvinx.zhang@intel.com>
    Acked-by: Haiyue Wang <haiyue.wang@intel.com>

--- drivers/net/e1000/igb_ethdev.c
+++ drivers/net/e1000/igb_ethdev.c
@@ -1827,8 +1827,7 @@ eth_igb_stats_get(struct rte_eth_dev *dev, struct rte_eth_stats *rte_stats)
 
 	/* Rx Errors */
 	rte_stats->imissed = stats->mpc;
-	rte_stats->ierrors = stats->crcerrs +
-	                     stats->rlec + stats->ruc + stats->roc +
+	rte_stats->ierrors = stats->crcerrs + stats->rlec +
 	                     stats->rxerrc + stats->algnerrc + stats->cexterr;
 
 	/* Tx Errors */
