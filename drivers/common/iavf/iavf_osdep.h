38d786564542f8ad37ab7f4e461dfe39cfa93afd
Pallavi Kadam <pallavi.kadam@intel.com>
2021-04-01 18:26:19 -0700

    common/iavf: build on Windows
    Enable IAVF driver to build on Windows as it is required
    to build ice PMD.
    Disable all other drivers from common directory.
    
    This patch also includes fix for a macro redefinition warning
    in the IAVF driver.
    
    Signed-off-by: Pallavi Kadam <pallavi.kadam@intel.com>
    Reviewed-by: Ranjit Menon <ranjit.menon@intel.com>
    Acked-by: Jie Zhou <jizh@microsoft.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- drivers/common/iavf/iavf_osdep.h
+++ drivers/common/iavf/iavf_osdep.h
@@ -55,6 +55,15 @@ typedef uint64_t        s64;
 #define __be64          uint64_t
 #endif
 
+/* Avoid macro redefinition warning on Windows */
+#ifdef RTE_EXEC_ENV_WINDOWS
+#ifdef min
+#undef min
+#endif
+#ifdef max
+#undef max
+#endif
+#endif
 #define min(a, b) RTE_MIN(a, b)
 #define max(a, b) RTE_MAX(a, b)
 
