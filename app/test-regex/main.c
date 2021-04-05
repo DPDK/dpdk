7b581788eec45529377f6f9a645d93bf664cd8a0
Thomas Monjalon <thomas@monjalon.net>
2021-04-05 21:33:24 +0200

    app/regex: fix usage text
    [ upstream commit e0512833d4acc6d037fa0ce8d453584618b98019 ]
    
    The usage syntax help includes the program name which was fake.
    It is replaced with the real name from argv.
    
    Fixes: de06137cb295 ("app/regex: add RegEx test application")
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Acked-by: Ori Kam <orika@nvidia.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Acked-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Acked-by: David Marchand <david.marchand@redhat.com>

--- app/test-regex/main.c
+++ app/test-regex/main.c
@@ -105,11 +105,11 @@ args_parse(int argc, char **argv, char *rules_file, char *data_file,
 			*nb_iterations = atoi(optarg);
 			break;
 		case ARG_HELP:
-			usage("RegEx test app");
+			usage(argv[0]);
 			break;
 		default:
 			fprintf(stderr, "Invalid option: %s\n", argv[optind]);
-			usage("RegEx test app");
+			usage(argv[0]);
 			rte_exit(EXIT_FAILURE, "Invalid option\n");
 			break;
 		}
