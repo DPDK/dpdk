3be42081bae707c4ad53e62a01a4af82151b8019
Thomas Monjalon <thomas@monjalon.net>
2021-04-06 15:22:03 +0200

    drivers: fix log level after loading
    When compiled as a shared object, and loaded at runtime as a plugin,
    the drivers should get the log level set earlier at EAL init
    by the user through --log-level option.
    
    The function for applying the log level setting is
    rte_log_register_type_and_pick_level().
    It is called by most drivers via RTE_LOG_REGISTER().
    
    The drivers common/mlx5, bcmfs and e1000 were missing,
    so the user-specified log level was not applied when
    those drivers were loaded as plugins.
    The macro RTE_LOG_REGISTER() is used for those drivers.
    
    The unnecessary protection for double registration
    is removed from e1000.
    
    Fixes: 9c99878aa1b1 ("log: introduce logtype register macro")
    Fixes: c8e79da7c676 ("crypto/bcmfs: introduce BCMFS driver")
    Cc: stable@dpdk.org
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Reviewed-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/e1000/em_ethdev.c
+++ drivers/net/e1000/em_ethdev.c
@@ -1850,9 +1850,3 @@ eth_em_set_mc_addr_list(struct rte_eth_dev *dev,
 RTE_PMD_REGISTER_PCI(net_e1000_em, rte_em_pmd);
 RTE_PMD_REGISTER_PCI_TABLE(net_e1000_em, pci_id_em_map);
 RTE_PMD_REGISTER_KMOD_DEP(net_e1000_em, "* igb_uio | uio_pci_generic | vfio-pci");
-
-/* see e1000_logs.c */
-RTE_INIT(igb_init_log)
-{
-	e1000_igb_init_log();
-}
