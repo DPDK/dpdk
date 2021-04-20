0090535f7ad427a0b12f302e84f230db508d4b59
Jiayu Hu <jiayu.hu@intel.com>
2021-04-20 04:57:44 -0400

    vhost: remove unnecessary free
    This patch removes unnecessary rte_free() for async_pkts_info
    and async_descs_split.
    
    Signed-off-by: Jiayu Hu <jiayu.hu@intel.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Tested-by: Yinan Wang <yinan.wang@intel.com>

--- lib/vhost/vhost_user.c
+++ lib/vhost/vhost_user.c
@@ -2013,13 +2013,6 @@ vhost_user_get_vring_base(struct virtio_net **pdev,
 	} else {
 		rte_free(vq->shadow_used_split);
 		vq->shadow_used_split = NULL;
-
-		if (vq->async_pkts_info)
-			rte_free(vq->async_pkts_info);
-		if (vq->async_descs_split)
-			rte_free(vq->async_descs_split);
-		vq->async_pkts_info = NULL;
-		vq->async_descs_split = NULL;
 	}
 
 	rte_free(vq->batch_copy_elems);
