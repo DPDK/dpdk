1ee899977d701768f6437e2b178a06e46283f821
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-04-21 22:33:49 +0300

    eal: add timespec_get shim
    C11 timespec_get() is not provided on some platforms:
    
    * MinGW-w64 does not currently implement it [1].
    * FreeBSD 11 with Clang 10.0.0 does not provide it.
    
    Add internal shims to Windows and FreeBSD EALs.
    For Windows, it can be removed after [1] is fixed.
    
    [1]: https://sourceforge.net/p/mingw-w64/mailman/message/37224689/
    
    Signed-off-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
    Acked-by: Jie Zhou <jizh@linux.microsoft.com>
    Acked-by: Nick Connolly <nick.connolly@mayadata.io>

--- lib/eal/windows/include/rte_os_shim.h
+++ lib/eal/windows/include/rte_os_shim.h
@@ -3,7 +3,10 @@
 #ifndef _RTE_OS_SHIM_
 #define _RTE_OS_SHIM_
 
+#include <time.h>
+
 #include <rte_os.h>
+#include <rte_windows.h>
 
 /**
  * @file
@@ -32,5 +35,35 @@
 #ifdef RTE_TOOLCHAIN_GCC
 #define IPPROTO_SCTP	132
 #endif
+
+#ifdef RTE_TOOLCHAIN_GCC
+
+#define TIME_UTC 1
+
+static inline int
+rte_timespec_get(struct timespec *now, int base)
+{
+	/* 100ns ticks from 1601-01-01 to 1970-01-01 */
+	static const uint64_t EPOCH = 116444736000000000ULL;
+	static const uint64_t TICKS_PER_SEC = 10000000;
+	static const uint64_t NS_PER_TICK = 100;
+
+	FILETIME ft;
+	uint64_t ticks;
+
+	if (base != TIME_UTC)
+		return 0;
+
+	GetSystemTimePreciseAsFileTime(&ft);
+	ticks = ((uint64_t)ft.dwHighDateTime << 32) | ft.dwLowDateTime;
+	ticks -= EPOCH;
+	now->tv_sec = ticks / TICKS_PER_SEC;
+	now->tv_nsec = (ticks - now->tv_sec * TICKS_PER_SEC) * NS_PER_TICK;
+	return base;
+}
+
+#define timespec_get(ts, base) rte_timespec_get(ts, base)
+
+#endif /* RTE_TOOLCHAIN_GCC */
 
 #endif /* _RTE_OS_SHIM_ */
