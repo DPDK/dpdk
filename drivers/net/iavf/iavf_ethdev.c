DPDK/dpdk

commit c678299594a817c62c5d50ec61f444d2e30f5619

net/iavf: fix default RSS configuration
Add advanced RSS offloads check due to some legacy driver (kernel/DPDK
PF) does not support virtual channel command VIRTCHNL_OP_RSS_HENA with
hena = 0 and VIRTCHNL_OP_ADD_RSS_CFG.

Fixes: 95f2f0e9fc2a ("net/iavf: improve default RSS")

Signed-off-by: Xuan Ding <xuan.ding@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/iavf/iavf_ethdev.c
@@ -291,11 +291,13 @@ iavf_init_rss(struct iavf_adapter *adapter)
 	if (ret)
 		return ret;
 
-	/* Set RSS hash configuration based on rss_conf->rss_hf. */
-	ret = iavf_rss_hash_set(adapter, rss_conf->rss_hf, true);
-	if (ret) {
-		PMD_DRV_LOG(ERR, "fail to set default RSS");
-		return ret;
+	if (vf->vf_res->vf_cap_flags & VIRTCHNL_VF_OFFLOAD_ADV_RSS_PF) {
+		/* Set RSS hash configuration based on rss_conf->rss_hf. */
+		ret = iavf_rss_hash_set(adapter, rss_conf->rss_hf, true);
+		if (ret) {
+			PMD_DRV_LOG(ERR, "fail to set default RSS");
+			return ret;
+		}
 	}
 
 	return 0;
@@ -1251,21 +1253,23 @@ iavf_dev_rss_hash_update(struct rte_eth_dev *dev,
 	if (rss_conf->rss_hf == 0)
 		return 0;
 
-	/* Clear existing RSS. */
-	ret = iavf_set_hena(adapter, 0);
+	if (vf->vf_res->vf_cap_flags & VIRTCHNL_VF_OFFLOAD_ADV_RSS_PF) {
+		/* Clear existing RSS. */
+		ret = iavf_set_hena(adapter, 0);
 
-	/* It is a workaround, temporarily allow error to be returned
-	 * due to possible lack of PF handling for hena = 0.
-	 */
-	if (ret)
-		PMD_DRV_LOG(WARNING, "fail to clean existing RSS,"
-			    "lack PF support");
+		/* It is a workaround, temporarily allow error to be returned
+		 * due to possible lack of PF handling for hena = 0.
+		 */
+		if (ret)
+			PMD_DRV_LOG(WARNING, "fail to clean existing RSS,"
+				    "lack PF support");
 
-	/* Set new RSS configuration. */
-	ret = iavf_rss_hash_set(adapter, rss_conf->rss_hf, true);
-	if (ret) {
-		PMD_DRV_LOG(ERR, "fail to set new RSS");
-		return ret;
+		/* Set new RSS configuration. */
+		ret = iavf_rss_hash_set(adapter, rss_conf->rss_hf, true);
+		if (ret) {
+			PMD_DRV_LOG(ERR, "fail to set new RSS");
+			return ret;
+		}
 	}
 
 	return 0;
@@ -2092,6 +2096,24 @@ iavf_dev_filter_ctrl(struct rte_eth_dev *dev,
 	return ret;
 }
 
+static void
+iavf_default_rss_disable(struct iavf_adapter *adapter)
+{
+	struct iavf_info *vf = IAVF_DEV_PRIVATE_TO_VF(adapter);
+	int ret = 0;
+
+	if (vf->vf_res->vf_cap_flags & VIRTCHNL_VF_OFFLOAD_ADV_RSS_PF) {
+		/* Set hena = 0 to ask PF to cleanup all existing RSS. */
+		ret = iavf_set_hena(adapter, 0);
+		if (ret)
+			/* It is a workaround, temporarily allow error to be
+			 * returned due to possible lack of PF handling for
+			 * hena = 0.
+			 */
+			PMD_INIT_LOG(WARNING, "fail to disable default RSS,"
+				    "lack PF support");
+	}
+}
 
 static int
 iavf_dev_init(struct rte_eth_dev *eth_dev)
@@ -2180,14 +2202,7 @@ iavf_dev_init(struct rte_eth_dev *eth_dev)
 		return ret;
 	}
 
-	/* Set hena = 0 to ask PF to cleanup all existing RSS. */
-	ret = iavf_set_hena(adapter, 0);
-	if (ret)
-		/* It is a workaround, temporarily allow error to be returned
-		 * due to possible lack of PF handling for hena = 0.
-		 */
-		PMD_DRV_LOG(WARNING, "fail to disable default RSS,"
-			    "lack PF support");
+	iavf_default_rss_disable(adapter);
 
 	return 0;
 }

commit 91fba40ea9b9c3f0ff7f583fbe5627188a49b24e

net/iavf: fix RSS initialization
When the default RSS is disabled during initialization, the RSS should
be turned on when creating RSS rule.

This patch adds support to configure RSS key and lookup table even
when the default RSS is disabled.

Fixes: c678299594a8 ("net/iavf: fix default RSS configuration")
Cc: stable@dpdk.org

Signed-off-by: Xuan Ding <xuan.ding@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/iavf/iavf_ethdev.c
@@ -362,14 +362,6 @@ iavf_init_rss(struct iavf_adapter *adapter)
 		PMD_DRV_LOG(DEBUG, "RSS is not supported");
 		return -ENOTSUP;
 	}
-	if (adapter->eth_dev->data->dev_conf.rxmode.mq_mode != ETH_MQ_RX_RSS) {
-		PMD_DRV_LOG(WARNING, "RSS is enabled by PF by default");
-		/* set all lut items to default queue */
-		for (i = 0; i < vf->vf_res->rss_lut_size; i++)
-			vf->rss_lut[i] = 0;
-		ret = iavf_configure_rss_lut(adapter);
-		return ret;
-	}
 
 	/* configure RSS key */
 	if (!rss_conf->rss_key) {