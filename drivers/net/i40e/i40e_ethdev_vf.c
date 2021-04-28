40c046441f3716ad0ded74e7b3b7afb1ae65ed64
Robin Zhang <robinx.zhang@intel.com>
2021-04-28 08:04:52 +0000

    net/i40e: fix primary MAC type when starting port
    [ upstream commit 2e468c03b0a4095988ec788a62669abf8e6f4908 ]
    
    When start port, all MAC addresses will be set. We should set the MAC
    type of default MAC address as VIRTCHNL_ETHER_ADDR_PRIMARY.
    
    Fixes: 3f604ddf33cf ("net/i40e: fix lack of MAC type when set MAC address")
    
    Signed-off-by: Robin Zhang <robinx.zhang@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/i40e/i40e_ethdev_vf.c
+++ drivers/net/i40e/i40e_ethdev_vf.c
@@ -2081,7 +2081,9 @@ i40evf_add_del_all_mac_addr(struct rte_eth_dev *dev, bool add)
 				continue;
 			rte_memcpy(list->list[j].addr, addr->addr_bytes,
 					 sizeof(addr->addr_bytes));
-			list->list[j].type = VIRTCHNL_ETHER_ADDR_EXTRA;
+			list->list[j].type = (j == 0 ?
+					      VIRTCHNL_ETHER_ADDR_PRIMARY :
+					      VIRTCHNL_ETHER_ADDR_EXTRA);
 			PMD_DRV_LOG(DEBUG, "add/rm mac:%x:%x:%x:%x:%x:%x",
 				    addr->addr_bytes[0], addr->addr_bytes[1],
 				    addr->addr_bytes[2], addr->addr_bytes[3],
