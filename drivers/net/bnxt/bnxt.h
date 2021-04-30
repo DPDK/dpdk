a49844ffa39df02bbcb8353436ae10b830bfed73
Ajit Khaparde <ajit.khaparde@broadcom.com>
2021-04-30 13:14:13 -0700

    net/bnxt: fix ring count calculation
    Fix ring count calculation for Thor. VNIC count does not have a
    direct bearing on the number of rings that can be used.
    
    Fixes: fe8dd26f86c7 ("net/bnxt: cap max Rx rings for Thor")
    
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt.h
+++ drivers/net/bnxt/bnxt.h
@@ -837,7 +837,6 @@ struct bnxt {
 static
 inline uint16_t bnxt_max_rings(struct bnxt *bp)
 {
-	struct rte_eth_conf *dev_conf = &bp->eth_dev->data->dev_conf;
 	uint16_t max_tx_rings = bp->max_tx_rings;
 	uint16_t max_rx_rings = bp->max_rx_rings;
 	uint16_t max_cp_rings = bp->max_cp_rings;
@@ -855,17 +854,12 @@ inline uint16_t bnxt_max_rings(struct bnxt *bp)
 				       bp->max_stat_ctx / 2U);
 	}
 
-	if (BNXT_CHIP_P5(bp)) {
-		/* RSS table size in Thor is 512.
-		 * Cap max Rx rings to the same value for RSS.
-		 * For non-RSS case cap it to the max VNIC count.
-		 */
-		if (dev_conf->rxmode.mq_mode & ETH_MQ_RX_RSS_FLAG)
-			max_rx_rings = RTE_MIN(max_rx_rings,
-					       BNXT_RSS_TBL_SIZE_P5);
-		else
-			max_rx_rings = RTE_MIN(max_rx_rings, bp->max_vnics);
-	}
+	/*
+	 * RSS table size in Thor is 512.
+	 * Cap max Rx rings to the same value for RSS.
+	 */
+	if (BNXT_CHIP_P5(bp))
+		max_rx_rings = RTE_MIN(max_rx_rings, BNXT_RSS_TBL_SIZE_P5);
 
 	max_tx_rings = RTE_MIN(max_tx_rings, max_rx_rings);
 	if (max_cp_rings > BNXT_NUM_ASYNC_CPR(bp))
