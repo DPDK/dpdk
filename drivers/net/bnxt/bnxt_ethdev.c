a0a21cb0c7f032799d9a9eee1ccedcddb78121f5
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2021-05-03 10:51:50 +0530

    net/bnxt: prevent device access in error state
    [ upstream commit c967098a4c0aafa0cb244ab9423fdea3d943d1ab ]
    
    Driver should prevent any DMA with the device when it
    detects an error. When firmware is in fatal state,
    stop tx/rx by assigning them to dummy functions.
    
    Fixes: be14720def9c ("net/bnxt: support FW reset")
    Fixes: 9d0cbaecc91a ("net/bnxt: support periodic FW health monitoring")
    
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Reviewed-by: Andy Gospodarek <gospo@broadcom.com>
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_ethdev.c
+++ drivers/net/bnxt/bnxt_ethdev.c
@@ -4254,6 +4254,8 @@ reset:
 	bp->flags |= BNXT_FLAG_FATAL_ERROR;
 	bp->flags |= BNXT_FLAG_FW_RESET;
 
+	bnxt_stop_rxtx(bp);
+
 	PMD_DRV_LOG(ERR, "Detected FW dead condition\n");
 
 	if (bnxt_is_master_func(bp))
