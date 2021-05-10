d97428bfb94c77921bda8e688ff745b0dcab0abc
Feifei Wang <feifei.wang2@arm.com>
2021-05-10 14:11:48 +0800

    app/eventdev: remove unnecessary barrier from order test
    For "order_launch_lcores" function, wmb after that the main lcore
    updates the variable "t->err", which represents the end of the test
    signal, is unnecessary. Because after the main lcore updates this
    signal variable, it will jump out of the launch function loop, and wait
    other lcores stop or return error in the main function(evt_main.c).
    During this time, there is no storing operation and thus no need for
    wmb.
    
    Signed-off-by: Feifei Wang <feifei.wang2@arm.com>
    Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
    Acked-by: Jerin Jacob <jerinj@marvell.com>

--- app/test-eventdev/test_order_common.c
+++ app/test-eventdev/test_order_common.c
@@ -308,7 +308,6 @@ order_launch_lcores(struct evt_test *test, struct evt_options *opt,
 				rte_event_dev_dump(opt->dev_id, stdout);
 				evt_err("No schedules for seconds, deadlock");
 				t->err = true;
-				rte_smp_wmb();
 				break;
 			}
 			old_remaining = remaining;
