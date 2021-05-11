428eaeb822c229d92e287322c80eeb15e0b53351
Thomas Monjalon <thomas@monjalon.net>
2021-05-11 22:57:36 +0200

    doc: remove PDF requirements
    The documentation is generated in HTML only.
    The PDF format is abandoned since DPDK 20.11
    while dropping support of the make-based build.
    
    This decision has been mentioned by the Technical Board:
    https://mails.dpdk.org/archives/dev/2021-January/195549.html
    
    Fixes: 3cc6ecfdfe85 ("build: remove makefiles")
    Cc: stable@dpdk.org
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -5,8 +5,6 @@
 from docutils import nodes
 from distutils.version import LooseVersion
 from sphinx import __version__ as sphinx_version
-from sphinx.highlighting import PygmentsBridge
-from pygments.formatters.latex import LatexFormatter
 from os import listdir
 from os import environ
 from os.path import basename
@@ -30,7 +28,6 @@ stop_on_error = ('-W' in argv)
 
 project = 'Data Plane Development Kit'
 html_logo = '../logo/DPDK_logo_vertical_rev_small.png'
-latex_logo = '../logo/DPDK_logo_horizontal_tag.png'
 if LooseVersion(sphinx_version) >= LooseVersion('3.5'):
     html_permalinks = False
 else:
@@ -48,46 +45,6 @@ feature_str_len = 30
 
 # Figures, tables and code-blocks automatically numbered if they have caption
 numfig = True
-
-latex_documents = [
-    ('index',
-     'doc.tex',
-     '',
-     '',
-     'manual')
-]
-
-# Latex directives to be included directly in the latex/pdf docs.
-custom_latex_preamble = r"""
-\usepackage{textalpha}
-\RecustomVerbatimEnvironment{Verbatim}{Verbatim}{xleftmargin=5mm}
-\usepackage{etoolbox}
-\robustify\(
-\robustify\)
-"""
-
-# Configuration for the latex/pdf docs.
-latex_elements = {
-    'papersize': 'a4paper',
-    'pointsize': '11pt',
-    # remove blank pages
-    'classoptions': ',openany,oneside',
-    'babel': '\\usepackage[english]{babel}',
-    # customize Latex formatting
-    'preamble': custom_latex_preamble
-}
-
-
-# Override the default Latex formatter in order to modify the
-# code/verbatim blocks.
-class CustomLatexFormatter(LatexFormatter):
-    def __init__(self, **options):
-        super(CustomLatexFormatter, self).__init__(**options)
-        # Use the second smallest font size for code/verbatim blocks.
-        self.verboptions = r'formatcom=\footnotesize'
-
-# Replace the default latex formatter.
-PygmentsBridge.latex_formatter = CustomLatexFormatter
 
 # Configuration for man pages
 man_pages = [("testpmd_app_ug/run_app", "testpmd",
