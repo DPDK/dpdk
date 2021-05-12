b20cb0fe6ea4bcc92de025e4b1565c9a9520d066
Jiawei Wang <jiaweiw@nvidia.com>
2021-05-12 09:53:24 +0300

    net/mlx5: fix mirroring with metering action
    While there's mirror action prior to the meter action in the E-Switch
    flow, means that the packets should be duplicated into port firstly,
    and then do meter and send to the original destination.
    MLX5 PMD will split the above E-Switch flow into two sub flows,
    similar as mirror with modify action before.
    
    Fixes: 07627fbf1506 ("net/mlx5: support E-Switch mirroring with modify action")
    Cc: stable@dpdk.org
    
    Signed-off-by: Jiawei Wang <jiaweiw@nvidia.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

--- drivers/net/mlx5/mlx5_flow.c
+++ drivers/net/mlx5/mlx5_flow.c
@@ -5136,6 +5136,7 @@ flow_check_match_action(const struct rte_flow_action actions[],
 		case RTE_FLOW_ACTION_TYPE_NVGRE_DECAP:
 		case RTE_FLOW_ACTION_TYPE_RAW_DECAP:
 		case RTE_FLOW_ACTION_TYPE_MODIFY_FIELD:
+		case RTE_FLOW_ACTION_TYPE_METER:
 			if (fdb_mirror)
 				*modify_after_mirror = 1;
 			break;
