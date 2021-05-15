0b92fa1eb0648c489447853073d47e56b7717e74
Huisong Li <lihuisong@huawei.com>
2021-05-15 08:52:36 +0800

    net/hns3: fix DCB configuration
    Currently, the DCB configuration takes effect in the dev_start stage, and
    the mapping between TCs and queues are also updated in this stage.
    However, the DCB configuration is delivered in the dev_configure stage.
    
    If the configuration fails, it should be intercepted in this stage. If
    the configuration succeeds, the user should be able to obtain the
    corresponding updated information, such as the mapping between TCs and
    queues. So this patch moves DCB configuration to dev_configure.
    
    Fixes: 62e3ccc2b94c ("net/hns3: support flow control")
    Cc: stable@dpdk.org
    
    Signed-off-by: Huisong Li <lihuisong@huawei.com>
    Signed-off-by: Min Hu (Connor) <humin29@huawei.com>

--- drivers/net/hns3/hns3_dcb.h
+++ drivers/net/hns3/hns3_dcb.h
@@ -207,7 +207,7 @@ int hns3_dcb_pfc_enable(struct rte_eth_dev *dev,
 int hns3_queue_to_tc_mapping(struct hns3_hw *hw, uint16_t nb_rx_q,
 			     uint16_t nb_tx_q);
 
-int hns3_dcb_cfg_update(struct hns3_adapter *hns);
+int hns3_update_queue_map_configure(struct hns3_adapter *hns);
 int hns3_port_shaper_update(struct hns3_hw *hw, uint32_t speed);
 int hns3_pg_shaper_rate_cfg(struct hns3_hw *hw, uint8_t pg_id, uint32_t rate);
 int hns3_pri_shaper_rate_cfg(struct hns3_hw *hw, uint8_t tc_no, uint32_t rate);
