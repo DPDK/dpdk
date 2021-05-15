91f0c38c0d6561a1abca4ed00115e13829d327a4
Huisong Li <lihuisong@huawei.com>
2021-05-15 08:52:38 +0800

    net/hns3: fix link speed when VF device is down
    [ upstream commit d1ced85a7e7a193051e96c2d34a8961a2e3f96d9 ]
    
    When the port is link down state, it is meaningless to display the
    port link speed. It should be an undefined state.
    
    Fixes: 59fad0f32135 ("net/hns3: support link update operation")
    
    Signed-off-by: Huisong Li <lihuisong@huawei.com>
    Signed-off-by: Min Hu (Connor) <humin29@huawei.com>

--- drivers/net/hns3/hns3_ethdev_vf.c
+++ drivers/net/hns3/hns3_ethdev_vf.c
@@ -2065,15 +2065,17 @@ hns3vf_dev_link_update(struct rte_eth_dev *eth_dev,
 	case ETH_SPEED_NUM_50G:
 	case ETH_SPEED_NUM_100G:
 	case ETH_SPEED_NUM_200G:
-		new_link.link_speed = mac->link_speed;
+		if (mac->link_status)
+			new_link.link_speed = mac->link_speed;
 		break;
 	default:
 		if (mac->link_status)
 			new_link.link_speed = ETH_SPEED_NUM_UNKNOWN;
-		else
-			new_link.link_speed = ETH_SPEED_NUM_NONE;
 		break;
 	}
+
+	if (!mac->link_status)
+		new_link.link_speed = ETH_SPEED_NUM_NONE;
 
 	new_link.link_duplex = mac->link_duplex;
 	new_link.link_status = mac->link_status ? ETH_LINK_UP : ETH_LINK_DOWN;
