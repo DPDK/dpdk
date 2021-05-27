03c8f2fe111c2b4c4fddc960dc82253ac7e6c5c5
Somnath Kotur <somnath.kotur@broadcom.com>
2021-05-27 11:48:46 +0530

    net/bnxt: detect bad opaque in Rx completion
    There is a rare hardware bug that can cause a bad opaque value in the RX
    or TPA start completion. When this happens, the hardware may have used the
    same buffer twice for 2 Rx packets.  In addition, the driver might also
    crash later using the bad opaque as an index into the ring.
    
    The Rx opaque value is predictable and is always monotonically increasing.
    The workaround is to keep track of the expected next opaque value and
    compare it with the one returned by hardware during RX and TPA start
    completions. If they miscompare, log it, discard the completion,
    schedule a ring reset and move on to the next one.
    
    Fixes: 0958d8b6435d ("net/bnxt: support LRO")
    Cc: stable@dpdk.org
    
    Signed-off-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.h
+++ drivers/net/bnxt/bnxt_hwrm.h
@@ -301,4 +301,5 @@ int bnxt_hwrm_cfa_adv_flow_mgmt_qcaps(struct bnxt *bp);
 int bnxt_hwrm_fw_echo_reply(struct bnxt *bp, uint32_t echo_req_data1,
 			    uint32_t echo_req_data2);
 int bnxt_hwrm_poll_ver_get(struct bnxt *bp);
+int bnxt_hwrm_rx_ring_reset(struct bnxt *bp, int queue_index);
 #endif
