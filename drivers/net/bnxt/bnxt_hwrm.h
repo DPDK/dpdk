219842b9990c2a3a426c14c7911a44d2cb9b6fdf
Somnath Kotur <somnath.kotur@broadcom.com>
2021-05-31 11:23:01 +0530

    net/bnxt: workaround spurious zero stats in Thor
    There is a HW bug that can result in certain stats being reported as
    zero.
    Workaround this by ignoring stats with a value of zero based on the
    previously stored snapshot of the same stat.
    This bug mainly manifests in the output of func_qstats as FW aggregrates
    each ring's stat value to give the per function stat and if one of
    them is zero, the per function stat value ends up being lower than the
    previous snapshot which shows up as a zero PPS value in testpmd.
    Eliminate invocation of func_qstats and aggregate the per-ring stat
    values in the driver itself to derive the func_qstats output post
    accounting for the spurious zero stat value.
    
    Bugzilla ID: 641
    Fixes: f8168ca0e690 ("net/bnxt: support thor controller")
    Cc: stable@dpdk.org
    
    Signed-off-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Reviewed-by: Lance Richardson <lance.richardson@broadcom.com>
    Reviewed-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Acked-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.h
+++ drivers/net/bnxt/bnxt_hwrm.h
@@ -168,9 +168,6 @@ int bnxt_hwrm_ring_grp_alloc(struct bnxt *bp, unsigned int idx);
 int bnxt_hwrm_ring_grp_free(struct bnxt *bp, unsigned int idx);
 
 int bnxt_hwrm_stat_clear(struct bnxt *bp, struct bnxt_cp_ring_info *cpr);
-int bnxt_hwrm_ctx_qstats(struct bnxt *bp, uint32_t cid, int idx,
-			 struct rte_eth_stats *stats, uint8_t rx);
-
 int bnxt_hwrm_ver_get(struct bnxt *bp, uint32_t timeout);
 
 int bnxt_hwrm_vnic_alloc(struct bnxt *bp, struct bnxt_vnic_info *vnic);
@@ -302,4 +299,6 @@ int bnxt_hwrm_fw_echo_reply(struct bnxt *bp, uint32_t echo_req_data1,
 			    uint32_t echo_req_data2);
 int bnxt_hwrm_poll_ver_get(struct bnxt *bp);
 int bnxt_hwrm_rx_ring_reset(struct bnxt *bp, int queue_index);
+int bnxt_hwrm_ring_stats(struct bnxt *bp, uint32_t cid, int idx,
+			 struct bnxt_ring_stats *stats, bool rx);
 #endif
