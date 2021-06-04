5f70ea4e97394bac64f00630ca66125025f39eb8
Dapeng Yu <dapengx.yu@intel.com>
2021-06-04 10:02:01 +0800

    net/i40e: fix use after free in FDIR release
    [ upstream commit 4b5b8ce8ffe57c611ccee438dea67b612021cfba ]
    
    The original code use a heap pointer after it is freed.
    
    Fixes: 460d1679586e ("drivers/net: delete HW rings while freeing queues")
    
    Signed-off-by: Dapeng Yu <dapengx.yu@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/i40e/i40e_fdir.c
+++ drivers/net/i40e/i40e_fdir.c
@@ -300,11 +300,11 @@ i40e_fdir_teardown(struct i40e_pf *pf)
 	if (err)
 		PMD_DRV_LOG(DEBUG, "Failed to do FDIR RX switch off");
 
-	i40e_dev_rx_queue_release(pf->fdir.rxq);
 	rte_eth_dma_zone_free(dev, "fdir_rx_ring", pf->fdir.rxq->queue_id);
+	i40e_dev_rx_queue_release(pf->fdir.rxq);
 	pf->fdir.rxq = NULL;
-	i40e_dev_tx_queue_release(pf->fdir.txq);
 	rte_eth_dma_zone_free(dev, "fdir_tx_ring", pf->fdir.txq->queue_id);
+	i40e_dev_tx_queue_release(pf->fdir.txq);
 	pf->fdir.txq = NULL;
 	i40e_vsi_release(vsi);
 	pf->fdir.fdir_vsi = NULL;
