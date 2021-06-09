43bcce71fe2c8879881d7112927c1c88097364b3
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2021-06-09 08:43:32 +0530

    net/bnxt: invoke device removal event on recovery failure
    [ upstream commit 8ac3cbba621ad1887b069b271115faf76bf04b75 ]
    
    When the driver receives RESET_NOTIFY async event from FW or detects
    a FW fatal error condition, it tries to recover from the error.
    When the driver fails to recover from the error condition, fixed to
    send device removal event to the application.
    
    Fixes: df6cd7c1f73a ("net/bnxt: handle reset notify async event from FW")
    
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_ethdev.c
+++ drivers/net/bnxt/bnxt_ethdev.c
@@ -4130,6 +4130,10 @@ err_start:
 err:
 	bp->flags |= BNXT_FLAG_FATAL_ERROR;
 	bnxt_uninit_resources(bp, false);
+	if (bp->eth_dev->data->dev_conf.intr_conf.rmv)
+		rte_eth_dev_callback_process(bp->eth_dev,
+					     RTE_ETH_EVENT_INTR_RMV,
+					     NULL);
 	PMD_DRV_LOG(ERR, "Failed to recover from FW reset\n");
 }
 
@@ -6141,6 +6145,7 @@ static int bnxt_pci_remove(struct rte_pci_device *pci_dev)
 static struct rte_pci_driver bnxt_rte_pmd = {
 	.id_table = bnxt_pci_id_map,
 	.drv_flags = RTE_PCI_DRV_NEED_MAPPING | RTE_PCI_DRV_INTR_LSC |
+			RTE_PCI_DRV_INTR_RMV |
 			RTE_PCI_DRV_PROBE_AGAIN, /* Needed in case of VF-REPs
 						  * and OVS-DPDK
 						  */
