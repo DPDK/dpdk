471ed659c1747d04d992310c8c08fca34e9c26c5
Chengchang Tang <tangchengchang@huawei.com>
2021-07-17 10:02:49 +0800

    net/hns3: fix residual MAC address entry
    [ upstream commit 976ddf2f5a7aa1ade4300de394cdd481a5e79f1c ]
    
    Currently, even if we fail to remove the origin MAC address from the HW,
    the set_default_mac will go on, and add the new MAC address to the HW.
    Eventually cause the original MAC address entry to remain in the HW, and
    users may receive unexpected packets.
    
    This patch make set_default_mac return directly to failure if deleting
    the original MAC address fails, simplifying the behavior of the driver
    and solving the problem of residual MAC address entry.
    
    Fixes: 7d7f9f80bbfb ("net/hns3: support MAC address related operations")
    
    Signed-off-by: Chengchang Tang <tangchengchang@huawei.com>
    Signed-off-by: Min Hu (Connor) <humin29@huawei.com>

--- drivers/net/hns3/hns3_ethdev.c
+++ drivers/net/hns3/hns3_ethdev.c
@@ -1666,7 +1666,6 @@ hns3_set_default_mac_addr(struct rte_eth_dev *dev,
 	struct rte_ether_addr *oaddr;
 	char mac_str[RTE_ETHER_ADDR_FMT_SIZE];
 	bool default_addr_setted;
-	bool rm_succes = false;
 	int ret, ret_val;
 
 	/*
@@ -1686,9 +1685,10 @@ hns3_set_default_mac_addr(struct rte_eth_dev *dev,
 					      oaddr);
 			hns3_warn(hw, "Remove old uc mac address(%s) fail: %d",
 				  mac_str, ret);
-			rm_succes = false;
-		} else
-			rm_succes = true;
+
+			rte_spinlock_unlock(&hw->lock);
+			return ret;
+		}
 	}
 
 	ret = hns3_add_uc_addr_common(hw, mac_addr);
@@ -1723,16 +1723,12 @@ err_pause_addr_cfg:
 	}
 
 err_add_uc_addr:
-	if (rm_succes) {
-		ret_val = hns3_add_uc_addr_common(hw, oaddr);
-		if (ret_val) {
-			rte_ether_format_addr(mac_str, RTE_ETHER_ADDR_FMT_SIZE,
-					      oaddr);
-			hns3_warn(hw,
-				  "Failed to restore old uc mac addr(%s): %d",
+	ret_val = hns3_add_uc_addr_common(hw, oaddr);
+	if (ret_val) {
+		rte_ether_format_addr(mac_str, RTE_ETHER_ADDR_FMT_SIZE, oaddr);
+		hns3_warn(hw, "Failed to restore old uc mac addr(%s): %d",
 				  mac_str, ret_val);
-			hw->mac.default_addr_setted = false;
-		}
+		hw->mac.default_addr_setted = false;
 	}
 	rte_spinlock_unlock(&hw->lock);
 
