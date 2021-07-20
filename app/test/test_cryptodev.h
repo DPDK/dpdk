0f678fdd8c909e126406bd1ba386e9da8937027f
Suanming Mou <suanmingm@nvidia.com>
2021-07-20 16:28:11 +0300

    test/crypto: increase mbuf data payload size to 4K
    Currently, there is vendor which can support bigger crypto data size.
    Increase the default mbuf data payload size to 4K as needed.
    
    Signed-off-by: Suanming Mou <suanmingm@nvidia.com>
    Acked-by: Akhil Goyal <gakhil@marvell.com>

--- app/test/test_cryptodev.h
+++ app/test/test_cryptodev.h
@@ -19,7 +19,7 @@
 #define DEFAULT_NUM_XFORMS              (2)
 #define NUM_MBUFS                       (8191)
 #define MBUF_CACHE_SIZE                 (256)
-#define MBUF_DATAPAYLOAD_SIZE		(2048 + DIGEST_BYTE_LENGTH_SHA512)
+#define MBUF_DATAPAYLOAD_SIZE		(4096 + DIGEST_BYTE_LENGTH_SHA512)
 #define MBUF_SIZE			(sizeof(struct rte_mbuf) + \
 		RTE_PKTMBUF_HEADROOM + MBUF_DATAPAYLOAD_SIZE)
 
