586add6ef49adb18710b7a6562677e6eb6f7bc29
Shiri Kuzin <shirik@nvidia.com>
2021-07-20 16:09:31 +0300

    crypto/mlx5: add DEK object management
    A DEK (Data encryption Key) is an mlx5 HW object which represents
    the cipher algorithm key.
    The DEKs are used during data encryption/decryption operations.
    
    In symmetric algorithms like AES-XTS, we use the same DEK for both
    encryption and decryption.
    
    Use the mlx5 hash-list tool to manage the DEK objects in the PMD.
    
    Provide the compare, create and destroy functions to manage DEKs in
    hash-list and introduce an internal API to setup and unset the DEK
    management and to prepare and destroy specific DEK object.
    
    The DEK hash-list will be created in dev_configure routine and
    destroyed in dev_close routine.
    
    Signed-off-by: Shiri Kuzin <shirik@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>
    Acked-by: Akhil Goyal <gakhil@marvell.com>

--- drivers/crypto/mlx5/mlx5_crypto.h
+++ drivers/crypto/mlx5/mlx5_crypto.h
@@ -12,6 +12,9 @@
 
 #include <mlx5_common_utils.h>
 
+#define MLX5_CRYPTO_DEK_HTABLE_SZ (1 << 11)
+#define MLX5_CRYPTO_KEY_LENGTH 80
+
 struct mlx5_crypto_priv {
 	TAILQ_ENTRY(mlx5_crypto_priv) next;
 	struct ibv_context *ctx; /* Device context. */
@@ -20,6 +23,28 @@ struct mlx5_crypto_priv {
 	void *uar; /* User Access Region. */
 	uint32_t pdn; /* Protection Domain number. */
 	struct ibv_pd *pd;
+	struct mlx5_hlist *dek_hlist; /* Dek hash list. */
 };
+
+struct mlx5_crypto_dek {
+	struct mlx5_list_entry entry; /* Pointer to DEK hash list entry. */
+	struct mlx5_devx_obj *obj; /* Pointer to DEK DevX object. */
+	uint8_t data[MLX5_CRYPTO_KEY_LENGTH]; /* DEK key data. */
+	bool size_is_48; /* Whether the key\data size is 48 bytes or not. */
+} __rte_cache_aligned;
+
+int
+mlx5_crypto_dek_destroy(struct mlx5_crypto_priv *priv,
+			struct mlx5_crypto_dek *dek);
+
+struct mlx5_crypto_dek *
+mlx5_crypto_dek_prepare(struct mlx5_crypto_priv *priv,
+			struct rte_crypto_cipher_xform *cipher);
+
+int
+mlx5_crypto_dek_setup(struct mlx5_crypto_priv *priv);
+
+void
+mlx5_crypto_dek_unset(struct mlx5_crypto_priv *priv);
 
 #endif /* MLX5_CRYPTO_H_ */
