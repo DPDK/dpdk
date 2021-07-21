9f430dd75107d47fb9b67ab756503397b59b8955
Viacheslav Ovsiienko <viacheslavo@nvidia.com>
2021-07-21 11:31:40 +0300

    net/mlx5: fix RoCE LAG bond device probing
    The RoCE LAG bond device requires neither E-Switch nor SR-IOV
    configurations. It means the RoCE LAG bond device might be
    presented as a single port Infiniband device.
    
    The mlx5 PMD wrongly recognized standalone RoCE LAG bond device
    as E-Switch configuration, this triggered the calls of E-Switch
    ports related API and the latter failed (over the new OFED kernel
    driver, starting since 5.4.1), causing the overall device probe
    failure.
    
    If there is a single port Infiniband bond device found the
    E-Switch related flags must be cleared indicating standalone
    configuration.
    
    Also, it is not true anymore the bond device can exist
    over E-Switch configurations only (as it was claimed for VF LAG
    bond devices). The related checks are not relevant anymore
    and removed.
    
    Fixes: 790164ce1d2d ("net/mlx5: check kernel support for VF LAG bonding")
    Cc: stable@dpdk.org
    
    Signed-off-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>

--- drivers/net/mlx5/linux/mlx5_os.c
+++ drivers/net/mlx5/linux/mlx5_os.c
@@ -2238,19 +2238,6 @@ mlx5_os_pci_probe_pf(struct rte_pci_device *pci_dev,
 			goto exit;
 		}
 	}
-#ifndef HAVE_MLX5DV_DR_DEVX_PORT
-	if (bd >= 0) {
-		/*
-		 * This may happen if there is VF LAG kernel support and
-		 * application is compiled with older rdma_core library.
-		 */
-		DRV_LOG(ERR,
-			"No kernel/verbs support for VF LAG bonding found.");
-		rte_errno = ENOTSUP;
-		ret = -rte_errno;
-		goto exit;
-	}
-#endif
 	/*
 	 * Now we can determine the maximal
 	 * amount of devices to be spawned.
@@ -2314,10 +2301,18 @@ mlx5_os_pci_probe_pf(struct rte_pci_device *pci_dev,
 						(list[ns].ifindex,
 						 &list[ns].info);
 			}
-#ifdef HAVE_MLX5DV_DR_DEVX_PORT
 			if (!ret && bd >= 0) {
 				switch (list[ns].info.name_type) {
 				case MLX5_PHYS_PORT_NAME_TYPE_UPLINK:
+					if (np == 1) {
+						/*
+						 * Force standalone bonding
+						 * device for ROCE LAG
+						 * confgiurations.
+						 */
+						list[ns].info.master = 0;
+						list[ns].info.representor = 0;
+					}
 					if (list[ns].info.port_name == bd)
 						ns++;
 					break;
@@ -2334,7 +2329,6 @@ mlx5_os_pci_probe_pf(struct rte_pci_device *pci_dev,
 				}
 				continue;
 			}
-#endif
 			if (!ret && (list[ns].info.representor ^
 				     list[ns].info.master))
 				ns++;
