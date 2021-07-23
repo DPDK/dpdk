4f76ac98b787dbc893d9aaae487f79e69cb962c4
Feifei Wang <feifei.wang2@arm.com>
2021-07-23 11:10:47 +0800

    drivers/net: fix vector Rx comments
    For the loop to process packets in Rx vector path, some notes for the
    code are wrong, fix these errors.
    
    Fixes: 7092be8437bd ("fm10k: add vector Rx")
    Fixes: c3def6a8724c ("net/i40e: implement vector PMD for altivec")
    Fixes: ae0eb310f253 ("net/i40e: implement vector PMD for ARM")
    Fixes: 9ed94e5bb04e ("i40e: add vector Rx")
    Fixes: 319c421f3890 ("net/avf: enable SSE Rx Tx")
    Fixes: 1162f5a0ef31 ("net/iavf: support flexible Rx descriptor in SSE path")
    Fixes: c68a52b8b38c ("net/ice: support vector SSE in Rx")
    Fixes: cf4b4708a88a ("ixgbe: improve slow-path perf with vector scattered Rx")
    Cc: stable@dpdk.org
    
    Suggested-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Signed-off-by: Feifei Wang <feifei.wang2@arm.com>
    Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>

--- drivers/net/ixgbe/ixgbe_rxtx_vec_sse.c
+++ drivers/net/ixgbe/ixgbe_rxtx_vec_sse.c
@@ -454,7 +454,7 @@ _recv_raw_pkts_vec(struct ixgbe_rx_queue *rxq, struct rte_mbuf **rx_pkts,
 		mbp1 = _mm_loadu_si128((__m128i *)&sw_ring[pos]);
 
 		/* Read desc statuses backwards to avoid race condition */
-		/* A.1 load 4 pkts desc */
+		/* A.1 load desc[3] */
 		descs[3] = _mm_loadu_si128((__m128i *)(rxdp + 3));
 		rte_compiler_barrier();
 
@@ -466,9 +466,9 @@ _recv_raw_pkts_vec(struct ixgbe_rx_queue *rxq, struct rte_mbuf **rx_pkts,
 		mbp2 = _mm_loadu_si128((__m128i *)&sw_ring[pos+2]);
 #endif
 
+		/* A.1 load desc[2-0] */
 		descs[2] = _mm_loadu_si128((__m128i *)(rxdp + 2));
 		rte_compiler_barrier();
-		/* B.1 load 2 mbuf point */
 		descs[1] = _mm_loadu_si128((__m128i *)(rxdp + 1));
 		rte_compiler_barrier();
 		descs[0] = _mm_loadu_si128((__m128i *)(rxdp));
