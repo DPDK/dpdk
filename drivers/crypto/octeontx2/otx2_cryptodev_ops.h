7bfca3894ac022403a5fca9854cbe2f20319e523
Akhil Goyal <gakhil@marvell.com>
2021-08-17 23:15:40 +0530

    crypto/octeontx2: update minimum headroom and tailroom
    The driver consume 208B of tailroom but has min requirement
    as 8B and headroom needed is 48B, but minimum requirement
    is set as 24B. This patch correct minimum requirements
    which application should honour.
    
    Signed-off-by: Akhil Goyal <gakhil@marvell.com>
    Acked-by: Anoob Joseph <anoobj@marvell.com>

--- drivers/crypto/octeontx2/otx2_cryptodev_ops.h
+++ drivers/crypto/octeontx2/otx2_cryptodev_ops.h
@@ -7,8 +7,8 @@
 
 #include <cryptodev_pmd.h>
 
-#define OTX2_CPT_MIN_HEADROOM_REQ	24
-#define OTX2_CPT_MIN_TAILROOM_REQ	8
+#define OTX2_CPT_MIN_HEADROOM_REQ	48
+#define OTX2_CPT_MIN_TAILROOM_REQ	208
 
 extern struct rte_cryptodev_ops otx2_cpt_ops;
 
