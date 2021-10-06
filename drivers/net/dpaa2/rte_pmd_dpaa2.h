0b83e8b1b72674f886f0054c6ebcc1ea8bc75a47
Hemant Agrawal <hemant.agrawal@nxp.com>
2021-10-06 22:31:26 +0530

    net/dpaa2: generate HW hash key
    This patch add support to generate the hash key in software
    equivalent to WRIOP key generation.
    
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- drivers/net/dpaa2/rte_pmd_dpaa2.h
+++ drivers/net/dpaa2/rte_pmd_dpaa2.h
@@ -82,4 +82,23 @@ __rte_experimental
 void
 rte_pmd_dpaa2_thread_init(void);
 
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change, or be removed, without prior notice
+ *
+ * Generate the DPAA2 WRIOP based hash value
+ *
+ * @param key
+ *    Array of key data
+ * @param size
+ *    Size of the hash input key in bytes
+ *
+ * @return
+ *   - 0 if successful.
+ *   - Negative in case of failure.
+ */
+
+__rte_experimental
+uint32_t
+rte_pmd_dpaa2_get_tlu_hash(uint8_t *key, int size);
 #endif /* _RTE_PMD_DPAA2_H */
