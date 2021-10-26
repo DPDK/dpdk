b66412f24f17fcba5a248888bf4b3c1f5d6880de
Konstantin Ananyev <konstantin.ananyev@intel.com>
2021-10-26 12:19:43 +0100

    test: fix ring PMD initialisation
    (bitratestats_autotest|latencystats_autotest|pdump_autotest) tests
    generate a log of error messages like that:
    
    test_packet_forward() line 104: Error sending packet to port 0
    Send pkts Failed
    
    These tests use of app/test/sample_packet_forward.* code.
    This code creates a portid from a ring, but doesn't properly
    configure/start it.
    The fix adds code to configure/start given port before usage.
    
    Fixes: 7a0935239b9e ("ethdev: make fast-path functions to use new flat array")
    Fixes: a52966cd48fd ("test: add helpers using ring PMD Rx/Tx")
    Cc: stable@dpdk.org
    
    Reported-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
    Tested-by: David Marchand <david.marchand@redhat.com>

--- app/test/test_latencystats.c
+++ app/test/test_latencystats.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <string.h>
 
+#include <rte_ethdev.h>
 #include <rte_latencystats.h>
 #include "rte_lcore.h"
 #include "rte_metrics.h"
@@ -158,12 +159,21 @@ static int test_latency_packet_forward(void)
 		printf("allocate mbuf pool Failed\n");
 		return TEST_FAILED;
 	}
+	ret = test_dev_start(portid, mp);
+	if (ret < 0) {
+		printf("test_dev_start(%hu, %p) failed, error code: %d\n",
+			portid, mp, ret);
+		return TEST_FAILED;
+	}
+
 	ret = test_packet_forward(pbuf, portid, QUEUE_ID);
 	if (ret < 0)
 		printf("send pkts Failed\n");
+
+	rte_eth_dev_stop(portid);
 	test_put_mbuf_to_pool(mp, pbuf);
 
-	return TEST_SUCCESS;
+	return (ret >= 0) ? TEST_SUCCESS : TEST_FAILED;
 }
 
 static struct
