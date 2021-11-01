c78471cd061a0f64db006c538be34536bc66c12a
Bruce Richardson <bruce.richardson@intel.com>
2021-11-01 17:03:26 +0000

    buildtools: fix build with Meson 0.60
    Meson 0.60 switched the format of uninstalled static libraries
    to thin archives, that is, they contain only paths to object files,
    not the files themselves. Files cannot be extracted in this case,
    resulting in build errors:
    
        ar: `x' cannot be used on thin archives.
    
    Handle thin archives when invoking pmdinfogen by directly using the
    files referenced in the archive, when they already exist, and extracting
    them if not.
    
    Bugzilla ID: 836
    Fixes: e6e9730c7066 ("buildtools: support object file extraction for Windows")
    Cc: stable@dpdk.org
    
    Reported-by: Michal Berger <michallinuxstuff@gmail.com>
    Signed-off-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>

--- buildtools/gen-pmdinfo-cfile.py
+++ buildtools/gen-pmdinfo-cfile.py
@@ -9,12 +9,13 @@ import tempfile
 
 _, tmp_root, ar, archive, output, *pmdinfogen = sys.argv
 with tempfile.TemporaryDirectory(dir=tmp_root) as temp:
-    run_ar = lambda command: subprocess.run(
-        [ar, command, os.path.abspath(archive)],
-        stdout=subprocess.PIPE, check=True, cwd=temp
-    )
-    # Don't use "ar p", because its output is corrupted on Windows.
-    run_ar("x")
-    names = run_ar("t").stdout.decode().splitlines()
-    paths = [os.path.join(temp, name) for name in names]
+    paths = []
+    for name in subprocess.run([ar, "t", archive], stdout=subprocess.PIPE,
+                               check=True).stdout.decode().splitlines():
+        if os.path.exists(name):
+            paths.append(name)
+        else:
+            subprocess.run([ar, "x", os.path.abspath(archive), name],
+                           check=True, cwd=temp)
+            paths.append(os.path.join(temp, name))
     subprocess.run(pmdinfogen + paths + [output], check=True)
