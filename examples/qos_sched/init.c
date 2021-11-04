06135957c4b4427b0ceb69dbf32b244977c876d8
Wojciech Liguzinski <wojciechx.liguzinski@intel.com>
2021-11-04 11:03:34 +0000

    examples/qos_sched: support PIE congestion management
    patch add support enable PIE or RED by
    parsing config file.
    
    Signed-off-by: Wojciech Liguzinski <wojciechx.liguzinski@intel.com>
    Acked-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>
    Acked-by: Jasvinder Singh <jasvinder.singh@intel.com>

--- examples/qos_sched/init.c
+++ examples/qos_sched/init.c
@@ -203,15 +203,9 @@ static struct rte_sched_subport_profile_params
 	},
 };
 
-struct rte_sched_subport_params subport_params[MAX_SCHED_SUBPORTS] = {
-	{
-		.n_pipes_per_subport_enabled = 4096,
-		.qsize = {64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64},
-		.pipe_profiles = pipe_profiles,
-		.n_pipe_profiles = sizeof(pipe_profiles) /
-			sizeof(struct rte_sched_pipe_params),
-		.n_max_pipe_profiles = MAX_SCHED_PIPE_PROFILES,
 #ifdef RTE_SCHED_CMAN
+struct rte_sched_cman_params cman_params = {
+	.cman_mode = RTE_SCHED_CMAN_RED,
 	.red_params = {
 		/* Traffic Class 0 Colors Green / Yellow / Red */
 		[0][0] = {.min_th = 48, .max_th = 64, .maxp_inv = 10, .wq_log2 = 9},
@@ -278,6 +272,19 @@ struct rte_sched_subport_params subport_params[MAX_SCHED_SUBPORTS] = {
 		[12][1] = {.min_th = 40, .max_th = 64, .maxp_inv = 10, .wq_log2 = 9},
 		[12][2] = {.min_th = 32, .max_th = 64, .maxp_inv = 10, .wq_log2 = 9},
 	},
+};
+#endif /* RTE_SCHED_CMAN */
+
+struct rte_sched_subport_params subport_params[MAX_SCHED_SUBPORTS] = {
+	{
+		.n_pipes_per_subport_enabled = 4096,
+		.qsize = {64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64},
+		.pipe_profiles = pipe_profiles,
+		.n_pipe_profiles = sizeof(pipe_profiles) /
+			sizeof(struct rte_sched_pipe_params),
+		.n_max_pipe_profiles = MAX_SCHED_PIPE_PROFILES,
+#ifdef RTE_SCHED_CMAN
+		.cman_params = &cman_params,
 #endif /* RTE_SCHED_CMAN */
 	},
 };
