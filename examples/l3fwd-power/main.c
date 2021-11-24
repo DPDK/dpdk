DPDK/dpdk

commit 931e3a994597acb9ffa827376206e8aa324476b7

examples/l3fwd-power: add wakeup log
This patch adds a log in main telemetry loop to show the thread has
woken up and begun to send and receive packets.

Signed-off-by: Miao Li <miao.li@intel.com>
Acked-by: David Hunt <david.hunt@intel.com>

examples/l3fwd-power/main.c
@@ -1156,6 +1156,9 @@ main_telemetry_loop(__rte_unused void *dummy)
 			ep_nep[nb_rx == 0]++;
 			fp_nfp[nb_rx == MAX_PKT_BURST]++;
 			poll_count++;
+			RTE_LOG(INFO, L3FWD_POWER,
+				"lcore %u has woken up on port %d queue %d\n",
+				rte_lcore_id(), portid, queueid);
 			if (unlikely(nb_rx == 0))
 				continue;
 

commit 0f4611cc2642aa19a1fb845c443d10dd83f18ec0

examples/l3fwd-power: revert wakeup log
Wakeup happens quite often (several hundred times a second) in
l3fwd-power example app in PMD power management mode, so this
message is appearing too often to be useful.
This patch reverts that info message addition.

Fixes: 931e3a994597 ("examples/l3fwd-power: add wakeup log")

Signed-off-by: David Hunt <david.hunt@intel.com>
Tested-by: Reshma Pattan <reshma.pattan@intel.com>
Acked-by: Reshma Pattan <reshma.pattan@intel.com>

examples/l3fwd-power/main.c
@@ -1156,9 +1156,6 @@ main_telemetry_loop(__rte_unused void *dummy)
 			ep_nep[nb_rx == 0]++;
 			fp_nfp[nb_rx == MAX_PKT_BURST]++;
 			poll_count++;
-			RTE_LOG(INFO, L3FWD_POWER,
-				"lcore %u has woken up on port %d queue %d\n",
-				rte_lcore_id(), portid, queueid);
 			if (unlikely(nb_rx == 0))
 				continue;
 