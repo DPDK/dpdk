6c8ff52958f1aa307e35c319d907017075d0c9af
Dariusz Sosnowski <dsosnowski@nvidia.com>
2022-02-07 17:48:56 +0200

    net/mlx5: fix inline length for multi-segment TSO
    [ upstream commit 864678e420f00225572be476559a087bfecaafbf ]
    
    This patch removes a redundant assert in mlx5_tx_packet_multi_tso().
    That assert assured that the amount of bytes requested to be inlined
    is greater than or equal to the minimum amount of bytes required
    to be inlined. This requirement is either derived from the NIC
    inlining mode or configured through devargs. When using TSO this
    requirement can be disregarded, because on all NICs it is satisfied by
    TSO inlining requirements, since TSO requires L2, L3, and L4 headers to
    be inlined.
    
    Fixes: 18a1c20044c0 ("net/mlx5: implement Tx burst template")
    
    Signed-off-by: Dariusz Sosnowski <dsosnowski@nvidia.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

--- drivers/net/mlx5/mlx5_tx.h
+++ drivers/net/mlx5/mlx5_tx.h
@@ -1710,7 +1710,6 @@ mlx5_tx_packet_multi_tso(struct mlx5_txq_data *__rte_restrict txq,
 		     inlen <= MLX5_ESEG_MIN_INLINE_SIZE ||
 		     inlen > (dlen + vlan)))
 		return MLX5_TXCMP_CODE_ERROR;
-	MLX5_ASSERT(inlen >= txq->inlen_mode);
 	/*
 	 * Check whether there are enough free WQEBBs:
 	 * - Control Segment
