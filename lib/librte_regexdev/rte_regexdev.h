64452c878f225c147dfb7156e605f5b55da9f7c0
Thomas Monjalon <thomas@monjalon.net>
2022-03-06 10:20:22 +0100

    regexdev: fix section attribute of symbols
    [ upstream commit 89e290eb8ca99af9f7cfc3292d93860f8e672708 ]
    
    The functions used by the drivers must be internal,
    while the function and variables used in inline functions
    must be experimental.
    
    These are the changes done in the shared library:
    - DF .text  Base          rte_regexdev_get_device_by_name
    + DF .text  INTERNAL      rte_regexdev_get_device_by_name
    - DF .text  Base          rte_regexdev_register
    + DF .text  INTERNAL      rte_regexdev_register
    - DF .text  Base          rte_regexdev_unregister
    + DF .text  INTERNAL      rte_regexdev_unregister
    - DF .text  Base          rte_regexdev_is_valid_dev
    + DF .text  EXPERIMENTAL  rte_regexdev_is_valid_dev
    - DO .bss   Base          rte_regex_devices
    + DO .bss   EXPERIMENTAL  rte_regex_devices
    - DO .bss   Base          rte_regexdev_logtype
    + DO .bss   EXPERIMENTAL  rte_regexdev_logtype
    
    Because these symbols were exported in the default section in DPDK 21.11,
    any change in these functions would be seen as incompatible
    by the ABI compatibility check.
    An exception rule is added for this experimental library,
    so the ABI check will skip it until the next ABI version.
    
    Fixes: bab9497ef78b ("regexdev: introduce API")
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Acked-by: Ori Kam <orika@nvidia.com>

--- lib/librte_regexdev/rte_regexdev.h
+++ lib/librte_regexdev/rte_regexdev.h
@@ -228,6 +228,9 @@ extern int rte_regexdev_logtype;
 } while (0)
 
 /**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice.
+ *
  * Check if dev_id is ready.
  *
  * @param dev_id
@@ -237,6 +240,7 @@ extern int rte_regexdev_logtype;
  *   - 0 if device state is not in ready state.
  *   - 1 if device state is ready state.
  */
+__rte_experimental
 int rte_regexdev_is_valid_dev(uint16_t dev_id);
 
 /**
