a25197930d58fc754bd9c91d55db31d521ddcb5f
Tyler Retzlaff <roretzla@linux.microsoft.com>
2022-03-22 00:12:35 -0700

    test/bpf: skip test if libpcap is unavailable
    [ upstream commit 55ae8965bf8eecd5ebec36663bb0f36018abf64b ]
    
    test_bpf_convert is being conditionally registered depending on the
    presence of RTE_HAS_LIBPCAP except the UT unconditionally lists it as a
    test to run.
    
    When the UT runs test_bpf_convert test-dpdk can't find the registration
    and assumes the DPDK_TEST environment variable hasn't been defined
    resulting in test-dpdk dropping to interactive mode and subsequently
    waiting for the remainder of the UT fast-test timeout period before
    reporting the test as having timed out.
    
    * unconditionally register test_bpf_convert,
    * if ! RTE_HAS_LIBPCAP provide a stub test_bpf_convert that reports the
      test is skipped similar to that done with the test_bpf test.
    
    Fixes: 2eccf6afbea9 ("bpf: add function to convert classic BPF to DPDK BPF")
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Acked-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>

--- app/test/test_bpf.c
+++ app/test/test_bpf.c
@@ -3250,7 +3250,16 @@ test_bpf(void)
 
 REGISTER_TEST_COMMAND(bpf_autotest, test_bpf);
 
-#ifdef RTE_HAS_LIBPCAP
+#ifndef RTE_HAS_LIBPCAP
+
+static int
+test_bpf_convert(void)
+{
+	printf("BPF convert RTE_HAS_LIBPCAP is undefined, skipping test\n");
+	return TEST_SKIPPED;
+}
+
+#else
 #include <pcap/pcap.h>
 
 static void
@@ -3448,5 +3457,6 @@ test_bpf_convert(void)
 	return rc;
 }
 
-REGISTER_TEST_COMMAND(bpf_convert_autotest, test_bpf_convert);
 #endif /* RTE_HAS_LIBPCAP */
+
+REGISTER_TEST_COMMAND(bpf_convert_autotest, test_bpf_convert);
