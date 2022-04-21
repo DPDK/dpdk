DPDK/dpdk

commit 6856efa54eea23ce2079a23cb661e009421fb833

net/mlx5: fix PF leak on PCI probing failure
During PCI probe, the internal probe function is called per PF.

If one of them fails, it was missing a proper destroy for the previously
probed PFs.

This fixes the behavior by destroying all previously probed PFs.

Fixes: 08c2772fc747 ("net/mlx5: support list of representor PF")
Cc: stable@dpdk.org

Signed-off-by: Michael Baum <michaelba@nvidia.com>
Acked-by: Matan Azrad <matan@nvidia.com>

drivers/net/mlx5/linux/mlx5_os.c
@@ -2697,9 +2697,20 @@ mlx5_os_pci_probe(struct rte_pci_device *pci_dev)
 
 	if (eth_da.nb_ports > 0) {
 		/* Iterate all port if devargs pf is range: "pf[0-1]vf[...]". */
-		for (p = 0; p < eth_da.nb_ports; p++)
+		for (p = 0; p < eth_da.nb_ports; p++) {
 			ret = mlx5_os_pci_probe_pf(pci_dev, &eth_da,
 						   eth_da.ports[p]);
+			if (ret)
+				break;
+		}
+		if (ret) {
+			DRV_LOG(ERR, "Probe of PCI device " PCI_PRI_FMT " "
+				"aborted due to proding failure of PF %u",
+				pci_dev->addr.domain, pci_dev->addr.bus,
+				pci_dev->addr.devid, pci_dev->addr.function,
+				eth_da.ports[p]);
+			mlx5_net_remove(&pci_dev->device);
+		}
 	} else {
 		ret = mlx5_os_pci_probe_pf(pci_dev, &eth_da, 0);
 	}

drivers/net/mlx5/mlx5.c
@@ -2386,7 +2386,7 @@ mlx5_eth_find_next(uint16_t port_id, struct rte_device *odev)
  * @return
  *   0 on success, the function cannot fail.
  */
-static int
+int
 mlx5_net_remove(struct rte_device *dev)
 {
 	uint16_t port_id;

drivers/net/mlx5/mlx5.h
@@ -1483,6 +1483,7 @@ int mlx5_udp_tunnel_port_add(struct rte_eth_dev *dev,
 			      struct rte_eth_udp_tunnel *udp_tunnel);
 uint16_t mlx5_eth_find_next(uint16_t port_id, struct rte_device *odev);
 int mlx5_dev_close(struct rte_eth_dev *dev);
+int mlx5_net_remove(struct rte_device *dev);
 bool mlx5_is_hpf(struct rte_eth_dev *dev);
 bool mlx5_is_sf_repr(struct rte_eth_dev *dev);
 void mlx5_age_event_prepare(struct mlx5_dev_ctx_shared *sh);

commit f956d3d4c33cdfac5e352f457050029bd5c9b8a8

net/mlx5: fix probing with secondary bonding member
Users can probe primary or secondary PCIe id when bonding is
configured.
1. -a 0a:00.0,representor=pf[0-1]vf[0-1], PMD probes 5 ports
totally: bonding device plus 4 representor ports.
2. -a 0a:00.1,representor=pf[0-1]vf[0-1], PMD only probes 2
representor ports.

Under the 2nd condition, bonding IB device doesn't have the same
PCIe id and PMD needs to check bonding relationship otherwise
probe failure.

Fixes: 6856efa54eea ("net/mlx5: fix PF leak on PCI probing failure")
Cc: stable@dpdk.org

Signed-off-by: Rongwei Liu <rongweil@nvidia.com>
Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

drivers/common/mlx5/linux/mlx5_common_os.c
@@ -559,21 +559,33 @@ mlx5_os_get_ibv_device(const struct rte_pci_addr *addr)
 	int n;
 	struct ibv_device **ibv_list = mlx5_glue->get_device_list(&n);
 	struct ibv_device *ibv_match = NULL;
+	uint8_t guid1[32] = {0};
+	uint8_t guid2[32] = {0};
+	int ret1, ret2 = -1;
+	struct rte_pci_addr paddr;
 
-	if (ibv_list == NULL) {
+	if (ibv_list == NULL || !n) {
 		rte_errno = ENOSYS;
+		if (ibv_list)
+			mlx5_glue->free_device_list(ibv_list);
 		return NULL;
 	}
+	ret1 = mlx5_get_device_guid(addr, guid1, sizeof(guid1));
 	while (n-- > 0) {
-		struct rte_pci_addr paddr;
-
 		DRV_LOG(DEBUG, "Checking device \"%s\"..", ibv_list[n]->name);
 		if (mlx5_get_pci_addr(ibv_list[n]->ibdev_path, &paddr) != 0)
 			continue;
-		if (rte_pci_addr_cmp(addr, &paddr) != 0)
-			continue;
-		ibv_match = ibv_list[n];
-		break;
+		if (ret1 > 0)
+			ret2 = mlx5_get_device_guid(&paddr, guid2, sizeof(guid2));
+		/* Bond device can bond secondary PCIe */
+		if ((strstr(ibv_list[n]->name, "bond") &&
+		    ((ret1 > 0 && ret2 > 0 && !memcmp(guid1, guid2, sizeof(guid1))) ||
+		    (addr->domain == paddr.domain && addr->bus == paddr.bus &&
+		     addr->devid == paddr.devid))) ||
+		     !rte_pci_addr_cmp(addr, &paddr)) {
+			ibv_match = ibv_list[n];
+			break;
+		}
 	}
 	if (ibv_match == NULL) {
 		DRV_LOG(WARNING,

drivers/net/mlx5/linux/mlx5_os.c
@@ -1979,9 +1979,9 @@ mlx5_os_pci_probe_pf(struct mlx5_common_device *cdev,
 	if (!nd) {
 		/* No device matches, just complain and bail out. */
 		DRV_LOG(WARNING,
-			"No Verbs device matches PCI device " PCI_PRI_FMT ","
+			"PF %u doesn't have Verbs device matches PCI device " PCI_PRI_FMT ","
 			" are kernel drivers loaded?",
-			owner_pci.domain, owner_pci.bus,
+			owner_id, owner_pci.domain, owner_pci.bus,
 			owner_pci.devid, owner_pci.function);
 		rte_errno = ENOENT;
 		ret = -rte_errno;
@@ -2387,16 +2387,16 @@ mlx5_os_pci_probe(struct mlx5_common_device *cdev,
 		for (p = 0; p < eth_da.nb_ports; p++) {
 			ret = mlx5_os_pci_probe_pf(cdev, &eth_da,
 						   eth_da.ports[p], mkvlist);
-			if (ret)
-				break;
-		}
-		if (ret) {
-			DRV_LOG(ERR, "Probe of PCI device " PCI_PRI_FMT " "
-				"aborted due to prodding failure of PF %u",
-				pci_dev->addr.domain, pci_dev->addr.bus,
-				pci_dev->addr.devid, pci_dev->addr.function,
-				eth_da.ports[p]);
-			mlx5_net_remove(cdev);
+			if (ret) {
+				DRV_LOG(INFO, "Probe of PCI device " PCI_PRI_FMT " "
+					"aborted due to proding failure of PF %u",
+					pci_dev->addr.domain, pci_dev->addr.bus,
+					pci_dev->addr.devid, pci_dev->addr.function,
+					eth_da.ports[p]);
+				mlx5_net_remove(cdev);
+				if (p != 0)
+					break;
+			}
 		}
 	} else {
 		ret = mlx5_os_pci_probe_pf(cdev, &eth_da, 0, mkvlist);