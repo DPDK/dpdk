a7c72e3e6cfb8d2f163052edcc4812b0ebba03b8
Wenwu Ma <wenwux.ma@intel.com>
2022-05-07 13:27:53 +0000

    vhost: fix deadlock when message handling failed
    [ upstream commit 9e89b06d027c6087624f36f288bd82c643c5a269 ]
    
    In vhost_user_msg_handler(), if vhost message handling
    failed, we should check whether the queue is locked and
    release the lock before returning. Or, it will cause a
    deadlock later.
    
    Fixes: 7f31d4ea05ca ("vhost: fix lock on device readiness notification")
    
    Signed-off-by: Wenwu Ma <wenwux.ma@intel.com>
    Reviewed-by: Chenbo Xia <chenbo.xia@intel.com>
    Tested-by: Wei Ling <weix.ling@intel.com>
    Acked-by: David Marchand <david.marchand@redhat.com>

--- lib/vhost/vhost_user.c
+++ lib/vhost/vhost_user.c
@@ -2973,7 +2973,6 @@ vhost_user_msg_handler(int vid, int fd)
 		return -1;
 	}
 
-	ret = 0;
 	request = msg.request.master;
 	if (request > VHOST_USER_NONE && request < VHOST_USER_MAX &&
 			vhost_message_str[request]) {
@@ -3115,9 +3114,11 @@ skip_to_post_handle:
 	} else if (ret == RTE_VHOST_MSG_RESULT_ERR) {
 		VHOST_LOG_CONFIG(ERR,
 			"vhost message handling failed.\n");
-		return -1;
+		ret = -1;
+		goto unlock;
 	}
 
+	ret = 0;
 	for (i = 0; i < dev->nr_vring; i++) {
 		struct vhost_virtqueue *vq = dev->virtqueue[i];
 		bool cur_ready = vq_is_ready(dev, vq);
@@ -3128,10 +3129,11 @@ skip_to_post_handle:
 		}
 	}
 
+unlock:
 	if (unlock_required)
 		vhost_user_unlock_all_queue_pairs(dev);
 
-	if (!virtio_is_ready(dev))
+	if (ret != 0 || !virtio_is_ready(dev))
 		goto out;
 
 	/*
@@ -3158,7 +3160,7 @@ skip_to_post_handle:
 	}
 
 out:
-	return 0;
+	return ret;
 }
 
 static int process_slave_message_reply(struct virtio_net *dev,
