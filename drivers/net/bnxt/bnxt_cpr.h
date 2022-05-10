b54ab78654a449b2540d779ea3b756cad99fe59d
Ruifeng Wang <ruifeng.wang@arm.com>
2022-05-10 14:31:18 +0800

    net/bnxt: remove some dead code
    Removed some macros that were defined but not used in this driver.
    As a result, rte_smp_xx occurrence is removed from this driver.
    
    Signed-off-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Reviewed-by: Kathleen Capella <kathleen.capella@arm.com>
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_cpr.h
+++ drivers/net/bnxt/bnxt_cpr.h
@@ -39,23 +39,9 @@ struct bnxt_db_info;
 #define B_CP_DB_DISARM(cpr)	(*(uint32_t *)((cpr)->cp_db.doorbell) = \
 				 DB_KEY_CP | DB_IRQ_DIS)
 
-#define B_CP_DB_IDX_ARM(cpr, cons)					\
-		(*(uint32_t *)((cpr)->cp_db.doorbell) = (DB_CP_REARM_FLAGS | \
-				(cons)))
-
-#define B_CP_DB_IDX_DISARM(cpr, cons)	do {				\
-		rte_smp_wmb();						\
-		(*(uint32_t *)((cpr)->cp_db.doorbell) = (DB_CP_FLAGS |	\
-				(cons));				\
-} while (0)
 #define B_CP_DIS_DB(cpr, raw_cons)					\
 	rte_write32_relaxed((DB_CP_FLAGS |				\
 		    DB_RING_IDX(&((cpr)->cp_db), raw_cons)),		\
-		    ((cpr)->cp_db.doorbell))
-
-#define B_CP_DB(cpr, raw_cons, ring_mask)				\
-	rte_write32((DB_CP_FLAGS |					\
-		    RING_CMPL((ring_mask), raw_cons)),	\
 		    ((cpr)->cp_db.doorbell))
 
 struct bnxt_db_info {
