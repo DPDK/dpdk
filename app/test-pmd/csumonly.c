8378498b96ccb094f4f1a49fd6f9b92167674413
Wenwu Ma <wenwux.ma@intel.com>
2022-05-12 01:07:56 +0000

    app/testpmd: perform SW IP checksum for GRO/GSO packets
    [ upstream commit 1945c64674b2b9ad55af0ef31f8a02ae0b747400 ]
    
    The GRO/GSO library doesn't re-calculate checksums for
    merged/fragmented packets. If users want the packets to
    have correct IP checksums, they should select HW IP
    checksum calculation for the port which the packets are
    transmitted to. But if the port doesn't support HW IP
    checksum, users may perform a SW IP checksum.
    
    Fixes: b7091f1dcfbc ("app/testpmd: enable the heavyweight mode TCP/IPv4 GRO")
    Fixes: 52f38a2055ed ("app/testpmd: enable TCP/IPv4 VxLAN and GRE GSO")
    
    Signed-off-by: Wenwu Ma <wenwux.ma@intel.com>
    Reviewed-by: Jiayu Hu <jiayu.hu@intel.com>
    Tested-by: Wei Ling <weix.ling@intel.com>
    Acked-by: Yuying Zhang <yuying.zhang@intel.com>

--- app/test-pmd/csumonly.c
+++ app/test-pmd/csumonly.c
@@ -768,6 +768,28 @@ pkt_copy_split(const struct rte_mbuf *pkt)
 	return md[0];
 }
 
+#if defined(RTE_LIB_GRO) || defined(RTE_LIB_GSO)
+/*
+ * Re-calculate IP checksum for merged/fragmented packets.
+ */
+static void
+pkts_ip_csum_recalc(struct rte_mbuf **pkts_burst, const uint16_t nb_pkts, uint64_t tx_offloads)
+{
+	int i;
+	struct rte_ipv4_hdr *ipv4_hdr;
+	for (i = 0; i < nb_pkts; i++) {
+		if ((pkts_burst[i]->ol_flags & RTE_MBUF_F_TX_IPV4) &&
+			(tx_offloads & RTE_ETH_TX_OFFLOAD_IPV4_CKSUM) == 0) {
+			ipv4_hdr = rte_pktmbuf_mtod_offset(pkts_burst[i],
+						struct rte_ipv4_hdr *,
+						pkts_burst[i]->l2_len);
+			ipv4_hdr->hdr_checksum = 0;
+			ipv4_hdr->hdr_checksum = rte_ipv4_cksum(ipv4_hdr);
+		}
+	}
+}
+#endif
+
 /*
  * Receive a burst of packets, and for each packet:
  *  - parse packet, and try to recognize a supported packet type (1)
@@ -1091,6 +1113,8 @@ tunnel_update:
 				fs->gro_times = 0;
 			}
 		}
+
+		pkts_ip_csum_recalc(pkts_burst, nb_rx, tx_offloads);
 	}
 #endif
 
@@ -1124,6 +1148,8 @@ tunnel_update:
 
 		tx_pkts_burst = gso_segments;
 		nb_rx = nb_segments;
+
+		pkts_ip_csum_recalc(tx_pkts_burst, nb_rx, tx_offloads);
 	} else
 #endif
 		tx_pkts_burst = pkts_burst;
