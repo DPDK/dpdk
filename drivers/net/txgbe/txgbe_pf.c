09b010da8ec6388edae44ff55e1feab5c08191ab
Jiawen Wu <jiawenwu@trustnetic.com>
2022-05-30 17:30:16 +0800

    net/txgbe: fix max number of queues for SR-IOV
    Hardware restrictions require a maximum of 4 queues for every pool.
    
    Fixes: a6712cd029a4 ("net/txgbe: add PF module init and uninit for SRIOV")
    Cc: stable@dpdk.org
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>

--- drivers/net/txgbe/txgbe_pf.c
+++ drivers/net/txgbe/txgbe_pf.c
@@ -108,7 +108,7 @@ int txgbe_pf_host_init(struct rte_eth_dev *eth_dev)
 		nb_queue = 4;
 		RTE_ETH_DEV_SRIOV(eth_dev).active = RTE_ETH_32_POOLS;
 	} else {
-		nb_queue = 8;
+		nb_queue = 4;
 		RTE_ETH_DEV_SRIOV(eth_dev).active = RTE_ETH_16_POOLS;
 	}
 
