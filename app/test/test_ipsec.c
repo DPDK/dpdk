df7022dae82dc77ba99c5bb3a3e5d29b219aa51b
David Marchand <david.marchand@redhat.com>
2022-06-16 11:33:20 +0200

    test/ipsec: fix build with GCC 12
    [ upstream commit 6e108b6a7c0c0699e6304f7b5706736b34d32607 ]
    
    GCC 12 raises the following warning:
    
    In function ‘_mm256_loadu_si256’,
        inlined from ‘rte_mov32’ at
            ../lib/eal/x86/include/rte_memcpy.h:319:9,
        inlined from ‘rte_mov128’ at
            ../lib/eal/x86/include/rte_memcpy.h:344:2,
        inlined from ‘rte_memcpy_generic’ at
            ../lib/eal/x86/include/rte_memcpy.h:438:4,
        inlined from ‘rte_memcpy’ at
            ../lib/eal/x86/include/rte_memcpy.h:882:10,
        inlined from ‘setup_test_string.constprop’ at
            ../app/test/test_ipsec.c:572:4:
    /usr/lib/gcc/x86_64-redhat-linux/12/include/avxintrin.h:929:10: error:
        array subscript ‘__m256i_u[3]’ is partly outside array bounds of
        ‘const char[108]’ [-Werror=array-bounds]
      929 |   return *__P;
          |          ^~~~
    ../app/test/test_ipsec.c: In function ‘setup_test_string.constprop’:
    ../app/test/test_ipsec.c:539:12: note: at offset 96 into object
        ‘null_plain_data’ of size 108
      539 | const char null_plain_data[] =
          |            ^~~~~~~~~~~~~~~
    
    Add a hint so that the compiler understands the copied data is within
    the passed string boundaries.
    
    Bugzilla ID: 848
    Fixes: 05fe65eb66b2 ("test/ipsec: introduce functional test")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- app/test/test_ipsec.c
+++ app/test/test_ipsec.c
@@ -544,11 +544,13 @@ struct rte_ipv4_hdr ipv4_outer  = {
 };
 
 static struct rte_mbuf *
-setup_test_string(struct rte_mempool *mpool,
-		const char *string, size_t len, uint8_t blocksize)
+setup_test_string(struct rte_mempool *mpool, const char *string,
+	size_t string_len, size_t len, uint8_t blocksize)
 {
 	struct rte_mbuf *m = rte_pktmbuf_alloc(mpool);
 	size_t t_len = len - (blocksize ? (len % blocksize) : 0);
+
+	RTE_VERIFY(len <= string_len);
 
 	if (m) {
 		memset(m->buf_addr, 0, m->buf_len);
@@ -1355,7 +1357,8 @@ test_ipsec_crypto_outb_burst_null_null(int i)
 	/* Generate input mbuf data */
 	for (j = 0; j < num_pkts && rc == 0; j++) {
 		ut_params->ibuf[j] = setup_test_string(ts_params->mbuf_pool,
-			null_plain_data, test_cfg[i].pkt_sz, 0);
+			null_plain_data, sizeof(null_plain_data),
+			test_cfg[i].pkt_sz, 0);
 		if (ut_params->ibuf[j] == NULL)
 			rc = TEST_FAILED;
 		else {
@@ -1473,7 +1476,8 @@ test_ipsec_inline_crypto_inb_burst_null_null(int i)
 			/* Generate test mbuf data */
 			ut_params->obuf[j] = setup_test_string(
 				ts_params->mbuf_pool,
-				null_plain_data, test_cfg[i].pkt_sz, 0);
+				null_plain_data, sizeof(null_plain_data),
+				test_cfg[i].pkt_sz, 0);
 			if (ut_params->obuf[j] == NULL)
 				rc = TEST_FAILED;
 		}
@@ -1541,16 +1545,17 @@ test_ipsec_inline_proto_inb_burst_null_null(int i)
 
 	/* Generate inbound mbuf data */
 	for (j = 0; j < num_pkts && rc == 0; j++) {
-		ut_params->ibuf[j] = setup_test_string(
-			ts_params->mbuf_pool,
-			null_plain_data, test_cfg[i].pkt_sz, 0);
+		ut_params->ibuf[j] = setup_test_string(ts_params->mbuf_pool,
+			null_plain_data, sizeof(null_plain_data),
+			test_cfg[i].pkt_sz, 0);
 		if (ut_params->ibuf[j] == NULL)
 			rc = TEST_FAILED;
 		else {
 			/* Generate test mbuf data */
 			ut_params->obuf[j] = setup_test_string(
 				ts_params->mbuf_pool,
-				null_plain_data, test_cfg[i].pkt_sz, 0);
+				null_plain_data, sizeof(null_plain_data),
+				test_cfg[i].pkt_sz, 0);
 			if (ut_params->obuf[j] == NULL)
 				rc = TEST_FAILED;
 		}
@@ -1650,7 +1655,8 @@ test_ipsec_inline_crypto_outb_burst_null_null(int i)
 	/* Generate test mbuf data */
 	for (j = 0; j < num_pkts && rc == 0; j++) {
 		ut_params->ibuf[j] = setup_test_string(ts_params->mbuf_pool,
-			null_plain_data, test_cfg[i].pkt_sz, 0);
+			null_plain_data, sizeof(null_plain_data),
+			test_cfg[i].pkt_sz, 0);
 		if (ut_params->ibuf[0] == NULL)
 			rc = TEST_FAILED;
 
@@ -1728,15 +1734,17 @@ test_ipsec_inline_proto_outb_burst_null_null(int i)
 	/* Generate test mbuf data */
 	for (j = 0; j < num_pkts && rc == 0; j++) {
 		ut_params->ibuf[j] = setup_test_string(ts_params->mbuf_pool,
-			null_plain_data, test_cfg[i].pkt_sz, 0);
+			null_plain_data, sizeof(null_plain_data),
+			test_cfg[i].pkt_sz, 0);
 		if (ut_params->ibuf[0] == NULL)
 			rc = TEST_FAILED;
 
 		if (rc == 0) {
 			/* Generate test tunneled mbuf data for comparison */
 			ut_params->obuf[j] = setup_test_string(
-					ts_params->mbuf_pool,
-					null_plain_data, test_cfg[i].pkt_sz, 0);
+				ts_params->mbuf_pool, null_plain_data,
+				sizeof(null_plain_data), test_cfg[i].pkt_sz,
+				0);
 			if (ut_params->obuf[j] == NULL)
 				rc = TEST_FAILED;
 		}
@@ -1805,7 +1813,8 @@ test_ipsec_lksd_proto_inb_burst_null_null(int i)
 	for (j = 0; j < num_pkts && rc == 0; j++) {
 		/* packet with sequence number 0 is invalid */
 		ut_params->ibuf[j] = setup_test_string(ts_params->mbuf_pool,
-			null_encrypted_data, test_cfg[i].pkt_sz, 0);
+			null_encrypted_data, sizeof(null_encrypted_data),
+			test_cfg[i].pkt_sz, 0);
 		if (ut_params->ibuf[j] == NULL)
 			rc = TEST_FAILED;
 	}
