0581aae1d59fb28ed7ddf8b8dd3700df1b95e051
Kathleen Capella <kathleen.capella@arm.com>
2022-06-17 18:21:34 +0000

    net/iavf: add basic NEON Rx
    This patch adds the basic NEON Rx path to the iavf driver. It does not
    include scatter or flex varieties.
    
    Tested on N1SDP platform with Intel XL710 NIC and 40G connection.
    Tested with a single core and testpmd rxonly mode. Saw no significant
    performance difference between scalar and Arm vPMD paths using this test
    in iavf and saw the same results when comparing scalar and Arm vPMD
    path in i40e.
    
    Signed-off-by: Kathleen Capella <kathleen.capella@arm.com>
    Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Reviewed-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/iavf/iavf_rxtx.c
+++ drivers/net/iavf/iavf_rxtx.c
@@ -3059,7 +3059,21 @@ iavf_set_rx_function(struct rte_eth_dev *dev)
 
 		return;
 	}
+#elif defined RTE_ARCH_ARM
+	int check_ret;
 
+	check_ret = iavf_rx_vec_dev_check(dev);
+	if (check_ret >= 0 &&
+	    rte_vect_get_max_simd_bitwidth() >= RTE_VECT_SIMD_128) {
+		PMD_DRV_LOG(DEBUG, "Using a Vector Rx callback (port=%d).",
+			    dev->data->port_id);
+		for (i = 0; i < dev->data->nb_rx_queues; i++) {
+			rxq = dev->data->rx_queues[i];
+			(void)iavf_rxq_vec_setup(rxq);
+		}
+		dev->rx_pkt_burst = iavf_recv_pkts_vec;
+		return;
+	}
 #endif
 	if (dev->data->scattered_rx) {
 		PMD_DRV_LOG(DEBUG, "Using a Scattered Rx callback (port=%d).",
