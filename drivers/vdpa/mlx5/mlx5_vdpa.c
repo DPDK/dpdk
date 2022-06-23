2cf6f9aac9322e64cc4db838cf18b157c1dcf9e7
Wisam Jaddo <wisamm@nvidia.com>
2022-06-23 12:00:04 +0300

    vdpa/mlx5: add ConnectX-6 LX device ID
    This adds ConnectX-6 LX to the list of supported
    Mellanox devices that run the MLX5 vdpa PMD.
    
    Signed-off-by: Wisam Jaddo <wisamm@nvidia.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

--- drivers/vdpa/mlx5/mlx5_vdpa.c
+++ drivers/vdpa/mlx5/mlx5_vdpa.c
@@ -965,6 +965,10 @@ static const struct rte_pci_id mlx5_vdpa_pci_id_map[] = {
 	},
 	{
 		RTE_PCI_DEVICE(PCI_VENDOR_ID_MELLANOX,
+				PCI_DEVICE_ID_MELLANOX_CONNECTX6LX)
+	},
+	{
+		RTE_PCI_DEVICE(PCI_VENDOR_ID_MELLANOX,
 				PCI_DEVICE_ID_MELLANOX_CONNECTX7)
 	},
 	{
