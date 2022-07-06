879ebb7dcee5d4e8bc19c5e985cd005579ee604a
Long Li <longli@microsoft.com>
2022-07-06 10:48:53 -0700

    net/mlx5: fix Verbs FD leak in secondary process
    [ upstream commit bc5d8fdb7008210e2698fa1f91e51d7dfba00f77 ]
    
    FDs passed from rte_mp_msg are duplicated to the secondary process and
    need to be closed.
    
    Fixes: 9a8ab29b84 ("net/mlx5: replace IPC socket with EAL API")
    
    Signed-off-by: Long Li <longli@microsoft.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

--- drivers/net/mlx5/linux/mlx5_os.c
+++ drivers/net/mlx5/linux/mlx5_os.c
@@ -933,6 +933,7 @@ mlx5_dev_spawn(struct rte_device *dpdk_dev,
 	DRV_LOG(DEBUG, "naming Ethernet device \"%s\"", name);
 	if (rte_eal_process_type() == RTE_PROC_SECONDARY) {
 		struct mlx5_mp_id mp_id;
+		int fd;
 
 		eth_dev = rte_eth_dev_attach_secondary(name);
 		if (eth_dev == NULL) {
@@ -949,11 +950,12 @@ mlx5_dev_spawn(struct rte_device *dpdk_dev,
 			return NULL;
 		mlx5_mp_id_init(&mp_id, eth_dev->data->port_id);
 		/* Receive command fd from primary process */
-		err = mlx5_mp_req_verbs_cmd_fd(&mp_id);
-		if (err < 0)
+		fd = mlx5_mp_req_verbs_cmd_fd(&mp_id);
+		if (fd < 0)
 			goto err_secondary;
 		/* Remap UAR for Tx queues. */
-		err = mlx5_tx_uar_init_secondary(eth_dev, err);
+		err = mlx5_tx_uar_init_secondary(eth_dev, fd);
+		close(fd);
 		if (err)
 			goto err_secondary;
 		/*
