kailiangz1/dpdk-vhost-vfe

common/virtio_mi: Live migration commands
Signed-off-by: Gavin Li <gavinl@nvidia.com>

drivers/common/virtio_mi/virtio_lm.h
@@ -1,0 +1,60 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2022 NVIDIA Corporation & Affiliates
+ */
+
+#ifndef _VIRTIO_LM_H_
+#define _VIRTIO_LM_H_
+
+struct virtio_vdpa_pf_priv;
+
+__rte_internal int
+virtio_vdpa_cmd_identity(struct virtio_vdpa_pf_priv *priv,
+		struct virtio_admin_migration_identity_result *result);
+__rte_internal int
+virtio_vdpa_cmd_resume(struct virtio_vdpa_pf_priv *priv, uint16_t vdev_id,
+		enum virtio_internal_status status);
+__rte_internal int
+virtio_vdpa_cmd_suspend(struct virtio_vdpa_pf_priv *priv, uint16_t vdev_id,
+		enum virtio_internal_status status);
+__rte_internal int
+virtio_vdpa_cmd_save_state(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id, uint64_t offset, uint64_t length,
+		rte_iova_t out_data);
+__rte_internal int
+virtio_vdpa_cmd_restore_state(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id, uint64_t offset, uint64_t length,
+		rte_iova_t data);
+__rte_internal int
+virtio_vdpa_cmd_get_internal_pending_bytes(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		struct virtio_admin_migration_get_internal_state_pending_bytes_result *result);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_identity(struct virtio_vdpa_pf_priv *priv,
+		struct virtio_admin_dirty_page_identity_result *result);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_start_track(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		enum virtio_dirty_track_mode track_mode,
+		uint32_t vdev_host_page_size,
+		uint64_t vdev_host_range_addr,
+		uint64_t range_length,
+		int num_sges,
+		struct virtio_sge data[]);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_stop_track(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id, uint64_t vdev_host_range_addr);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_get_map_pending_bytes(
+		struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		uint64_t vdev_host_range_addr,
+		struct virtio_admin_dirty_page_get_map_pending_bytes_result *result);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_report_map(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		uint64_t offset,
+		uint64_t length,
+		uint64_t vdev_host_range_addr,
+		rte_iova_t data);
+
+#endif /* _VIRTIO_LM_H_ */

DPDK/dpdk

drivers/common/virtio_mi/virtio_lm.h
@@ -1,0 +1,60 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2022 NVIDIA Corporation & Affiliates
+ */
+
+#ifndef _VIRTIO_LM_H_
+#define _VIRTIO_LM_H_
+
+struct virtio_vdpa_pf_priv;
+
+__rte_internal int
+virtio_vdpa_cmd_identity(struct virtio_vdpa_pf_priv *priv,
+		struct virtio_admin_migration_identity_result *result);
+__rte_internal int
+virtio_vdpa_cmd_set_status(struct virtio_vdpa_pf_priv *priv, uint16_t vdev_id,
+		enum virtio_internal_status status);
+__rte_internal int
+virtio_vdpa_cmd_get_status(struct virtio_vdpa_pf_priv *priv, uint16_t vdev_id,
+		enum virtio_internal_status *status);
+__rte_internal int
+virtio_vdpa_cmd_save_state(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id, uint64_t offset, uint64_t length,
+		rte_iova_t out_data);
+__rte_internal int
+virtio_vdpa_cmd_restore_state(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id, uint64_t offset, uint64_t length,
+		rte_iova_t data);
+__rte_internal int
+virtio_vdpa_cmd_get_internal_pending_bytes(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		struct virtio_admin_migration_get_internal_state_pending_bytes_result *result);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_identity(struct virtio_vdpa_pf_priv *priv,
+		struct virtio_admin_dirty_page_identity_result *result);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_start_track(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		enum virtio_dirty_track_mode track_mode,
+		uint32_t vdev_host_page_size,
+		uint64_t vdev_host_range_addr,
+		uint64_t range_length,
+		int num_sges,
+		struct virtio_sge data[]);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_stop_track(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id, uint64_t vdev_host_range_addr);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_get_map_pending_bytes(
+		struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		uint64_t vdev_host_range_addr,
+		struct virtio_admin_dirty_page_get_map_pending_bytes_result *result);
+__rte_internal int
+virtio_vdpa_cmd_dirty_page_report_map(struct virtio_vdpa_pf_priv *priv,
+		uint16_t vdev_id,
+		uint64_t offset,
+		uint64_t length,
+		uint64_t vdev_host_range_addr,
+		rte_iova_t data);
+
+#endif /* _VIRTIO_LM_H_ */