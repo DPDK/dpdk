f57e36acb2b6bda02f54f71334fbe45c6485b683
Vamsi Attunuru <vattunuru@marvell.com>
2022-08-09 16:23:55 +0530

    crypto/cnxk: update flow label copy capability
    Update flow label copy capability in IPsec options.
    
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Acked-by: Akhil Goyal <gakhil@marvell.com>

--- drivers/crypto/cnxk/cnxk_cryptodev_capabilities.c
+++ drivers/crypto/cnxk/cnxk_cryptodev_capabilities.c
@@ -1294,6 +1294,7 @@ cn10k_sec_caps_update(struct rte_security_capability *sec_cap)
 	sec_cap->ipsec.options.l4_csum_enable = 1;
 	sec_cap->ipsec.options.stats = 1;
 	sec_cap->ipsec.options.esn = 1;
+	sec_cap->ipsec.options.copy_flabel = 1;
 	sec_cap->ipsec.replay_win_sz_max = ROC_AR_WIN_SIZE_MAX;
 }
 
