18895ee575c74388c6cd54dfa476f6209f252bc7
Bhagyada Modali <bhagyada.modali@amd.com>
2022-09-07 23:31:07 -0400

    net/axgbe: reset end of packet in scattered Rx
    [ upstream commit 2a761aec228caf66025cacf07c7c9662ae021e03 ]
    
    Reset the EOP in the failure scenario and also after the last segment.
    Remove updating the packet length explicitly as it is done in chaining.
    
    Fixes: 965b3127d425 ("net/axgbe: support scattered Rx")
    
    Signed-off-by: Bhagyada Modali <bhagyada.modali@amd.com>
    Acked-by: Chandubabu Namburu <chandu@amd.com>

--- drivers/net/axgbe/axgbe_rxtx.c
+++ drivers/net/axgbe/axgbe_rxtx.c
@@ -347,10 +347,11 @@ uint16_t eth_axgbe_recv_scattered_pkts(void *rx_queue,
 	uint32_t error_status = 0;
 	uint16_t idx, pidx, data_len = 0, pkt_len = 0;
 	uint64_t offloads;
+	bool eop = 0;
 
 	idx = AXGBE_GET_DESC_IDX(rxq, rxq->cur);
+
 	while (nb_rx < nb_pkts) {
-		bool eop = 0;
 next_desc:
 		idx = AXGBE_GET_DESC_IDX(rxq, rxq->cur);
 
@@ -417,9 +418,12 @@ next_desc:
 		mbuf->pkt_len = data_len;
 
 		if (first_seg != NULL) {
-			if (rte_pktmbuf_chain(first_seg, mbuf) != 0)
+			if (rte_pktmbuf_chain(first_seg, mbuf) != 0) {
 				rte_mempool_put(rxq->mb_pool,
 						first_seg);
+				eop = 0;
+				break;
+			}
 		} else {
 			first_seg = mbuf;
 		}
@@ -461,8 +465,8 @@ err_set:
 
 		if (!eop)
 			goto next_desc;
+		eop = 0;
 
-		first_seg->pkt_len = pkt_len;
 		rxq->bytes += pkt_len;
 
 		first_seg->port = rxq->port_id;
