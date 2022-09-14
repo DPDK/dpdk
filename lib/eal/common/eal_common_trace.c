b7926cae59353b445558c06af1dabc15db0175dd
David Marchand <david.marchand@redhat.com>
2022-09-14 13:45:42 +0200

    trace: fix race in debug dump
    [ upstream commit 782dbf1791911eece2557b1a4b6f7c0ea57ea690 ]
    
    trace->nb_trace_mem_list access must be under trace->lock to avoid
    races with threads allocating/freeing their trace buffers.
    
    Fixes: f6b2d65dcd5d ("trace: implement debug dump")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Jerin Jacob <jerinj@marvell.com>
    Acked-by: Sunil Kumar Kori <skori@marvell.com>

--- lib/eal/common/eal_common_trace.c
+++ lib/eal/common/eal_common_trace.c
@@ -255,10 +255,9 @@ trace_lcore_mem_dump(FILE *f)
 	struct __rte_trace_header *header;
 	uint32_t count;
 
-	if (trace->nb_trace_mem_list == 0)
-		return;
-
 	rte_spinlock_lock(&trace->lock);
+	if (trace->nb_trace_mem_list == 0)
+		goto out;
 	fprintf(f, "nb_trace_mem_list = %d\n", trace->nb_trace_mem_list);
 	fprintf(f, "\nTrace mem info\n--------------\n");
 	for (count = 0; count < trace->nb_trace_mem_list; count++) {
@@ -269,6 +268,7 @@ trace_lcore_mem_dump(FILE *f)
 		header->stream_header.lcore_id,
 		header->stream_header.thread_name);
 	}
+out:
 	rte_spinlock_unlock(&trace->lock);
 }
 
