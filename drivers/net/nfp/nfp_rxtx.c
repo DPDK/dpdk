8582b5b558d8081613e1bc5db01a5f40ae54e8f2
Long Wu <long.wu@corigine.com>
2022-09-22 15:09:44 +0200

    net/nfp: fix memory leak in Rx
    [ upstream commit bb340f56fcb7bac9ec04c1c1ca7a2a4f3012c848 ]
    
    nfp_net_recv_pkts() should not return a value that less than 0 and the
    inappropriate return value in receive loop also causes the memory leak.
    Modify code to avoid return a value less than 0. Furthermore, When
    nfp_net_recv_pkts() break out from the receive loop because of packet
    problems, a rte_mbuf will not be freed and it will cause memory leak.
    Free the rte_mbuf before break out.
    
    Fixes: b812daadad0d ("nfp: add Rx and Tx")
    
    Signed-off-by: Long Wu <long.wu@corigine.com>
    Signed-off-by: Niklas Söderlund <niklas.soderlund@corigine.com>
    Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>

--- drivers/net/nfp/nfp_rxtx.c
+++ drivers/net/nfp/nfp_rxtx.c
@@ -299,8 +299,9 @@ nfp_net_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts)
 	struct rte_mbuf *new_mb;
 	uint16_t nb_hold;
 	uint64_t dma_addr;
-	int avail;
+	uint16_t avail;
 
+	avail = 0;
 	rxq = rx_queue;
 	if (unlikely(rxq == NULL)) {
 		/*
@@ -308,11 +309,10 @@ nfp_net_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts)
 		 * enabled. But the queue needs to be configured
 		 */
 		RTE_LOG_DP(ERR, PMD, "RX Bad queue\n");
-		return -EINVAL;
+		return avail;
 	}
 
 	hw = rxq->hw;
-	avail = 0;
 	nb_hold = 0;
 
 	while (avail < nb_pkts) {
@@ -378,7 +378,8 @@ nfp_net_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts)
 				hw->rx_offset,
 				rxq->mbuf_size - hw->rx_offset,
 				mb->data_len);
-			return -EINVAL;
+			rte_pktmbuf_free(mb);
+			break;
 		}
 
 		/* Filling the received mbuf with packet info */
