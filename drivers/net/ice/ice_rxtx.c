34380524d2de06ed0a0e7592cd7cd295ab85cc09
Tomasz Jonak <tomasz@graphiant.com>
2022-10-12 14:27:57 +0200

    net/ice: fix null function pointer call
    [ upstream commit a8880adcd5f4ea92d0221ff41902d516e68e89ab ]
    
    In case rte_eth_dma_zone_reserve fails in ice_tx_queue_setup
    ice_tx_queue_release is called on 0 allocated but not initialized
    txq struct.
    This may happen on ENOMEM condition, size exhaustion of
    memconfig->memzones array as well as some others.
    
    Fixes: edec6dd83824 ("net/ice: remove redundant functions")
    
    Signed-off-by: Tomasz Jonak <tomasz@graphiant.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/ice/ice_rxtx.c
+++ drivers/net/ice/ice_rxtx.c
@@ -1158,7 +1158,8 @@ ice_rx_queue_release(void *rxq)
 		return;
 	}
 
-	q->rx_rel_mbufs(q);
+	if (q->rx_rel_mbufs != NULL)
+		q->rx_rel_mbufs(q);
 	rte_free(q->sw_ring);
 	rte_memzone_free(q->mz);
 	rte_free(q);
@@ -1356,7 +1357,8 @@ ice_tx_queue_release(void *txq)
 		return;
 	}
 
-	q->tx_rel_mbufs(q);
+	if (q->tx_rel_mbufs != NULL)
+		q->tx_rel_mbufs(q);
 	rte_free(q->sw_ring);
 	rte_memzone_free(q->mz);
 	rte_free(q);
