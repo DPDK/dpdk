a5b1ffd88042fd609bbd49c0de56d5ea140b4f92
Andrew Boyer <andrew.boyer@amd.com>
2022-10-18 12:41:31 -0700

    net/ionic: add watchdogs to protect each queue type
    Ring the doorbell again for the following scenarios:
     * No receives posted but Rx queue not empty after deadline
     * No transmits posted but Tx work still pending after deadline
     * Admin queue work still pending after deadline
    
    This will help the queues recover in the extremely rare case that
    a doorbell is missed by the FW.
    
    Signed-off-by: Andrew Boyer <andrew.boyer@amd.com>

--- drivers/net/ionic/ionic_dev.h
+++ drivers/net/ionic/ionic_dev.h
@@ -26,6 +26,10 @@
 #define IONIC_DEVCMD_CHECK_PERIOD_US	10	/* devcmd status chk period */
 #define IONIC_DEVCMD_RETRY_WAIT_US	20000
 
+#define IONIC_Q_WDOG_MS			10	/* 10ms */
+#define IONIC_Q_WDOG_MAX_MS		5000	/* 5s */
+#define IONIC_ADMINQ_WDOG_MS		500	/* 500ms */
+
 #define IONIC_ALIGN			4096
 
 struct ionic_adapter;
--- drivers/net/ionic/ionic_rxtx.c
+++ drivers/net/ionic/ionic_rxtx.c
@@ -551,6 +551,7 @@ ionic_dev_rx_queue_setup(struct rte_eth_dev *eth_dev,
 	}
 
 	rxq->mb_pool = mp;
+	rxq->wdog_ms = IONIC_Q_WDOG_MS;
 
 	/*
 	 * Note: the interface does not currently support
