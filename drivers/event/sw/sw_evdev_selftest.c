5f065a858c3bcd309a00927a697dc000ae240131
Olivier Matz <olivier.matz@6wind.com>
2022-10-19 19:06:46 +0530

    event/sw: fix log in self test
    [ upstream commit 05d22d4e061a3ac92cb8f076c5ecaed653d4f640 ]
    
    The log should display the value, not the ID.
    
    Fixes: e21df4b062b5 ("test/eventdev: add SW xstats tests")
    
    Signed-off-by: Olivier Matz <olivier.matz@6wind.com>

--- drivers/event/sw/sw_evdev_selftest.c
+++ drivers/event/sw/sw_evdev_selftest.c
@@ -1641,8 +1641,8 @@ xstats_id_reset_tests(struct test *t)
 		}
 		if (val != port_expected[i]) {
 			printf("%d: %s value incorrect, expected %"PRIu64
-				" got %d\n", __LINE__, port_names[i],
-				port_expected[i], id);
+				" got %" PRIu64 "\n", __LINE__, port_names[i],
+				port_expected[i], val);
 			failed = 1;
 		}
 		/* reset to zero */
