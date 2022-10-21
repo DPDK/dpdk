dee23e6c36d00b76c1bf74c46d069148487e996f
Chaoyong He <chaoyong.he@corigine.com>
2022-10-21 16:01:37 +0800

    net/nfp: fix CPP bridge service requirement
    The CPP (Command Pull Push) bridge service is needed for some debug
    tools, and should be optional, so remove the mandatory requirement of
    service lcore parameter.
    
    Fixes: b18804219537 ("net/nfp: add initial flower firmware support")
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>

--- drivers/net/nfp/nfp_ethdev.c
+++ drivers/net/nfp/nfp_ethdev.c
@@ -1072,11 +1072,8 @@ nfp_pf_init(struct rte_pci_device *pci_dev)
 
 	/* register the CPP bridge service here for primary use */
 	ret = nfp_enable_cpp_service(pf_dev->cpp);
-	if (ret != 0) {
-		PMD_INIT_LOG(ERR, "Enable cpp service failed.");
-		ret = -EINVAL;
-		goto hwqueues_cleanup;
-	}
+	if (ret != 0)
+		PMD_INIT_LOG(INFO, "Enable cpp service failed.");
 
 	return 0;
 
@@ -1212,13 +1209,6 @@ nfp_pf_secondary_init(struct rte_pci_device *pci_dev)
 		PMD_INIT_LOG(ERR, "Unsupported Firmware loaded");
 		ret = -EINVAL;
 		goto sym_tbl_cleanup;
-	}
-
-	/* Register the CPP bridge service for the secondary too */
-	ret = nfp_enable_cpp_service(cpp);
-	if (ret != 0) {
-		PMD_INIT_LOG(ERR, "Enable cpp service failed.");
-		ret = -EINVAL;
 	}
 
 sym_tbl_cleanup:
