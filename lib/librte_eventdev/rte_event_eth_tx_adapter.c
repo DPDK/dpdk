b6f4010798c294a8dc92869a994447990550bd25
Naga Harish K S V <s.v.naga.harish.k@intel.com>
2022-10-21 01:43:14 -0500

    eventdev/eth_tx: fix queue delete
    [ upstream commit 75c5bfc320fdc41226cbeeb864161268fb344c49 ]
    
    To delete all the queues of an ethdev device associated with
    adapter instance the queue_id can be passed as -1 to the queue
    delete API.
    
    When a subset of queues of a ethdev device are associated,
    the queue delete logic is exiting without deleting the queues
    in some cases (higher numbered associated queues) for above
    scenario as the queue delete logic is not checking all the
    queue association status.
    
    This patch fixes this issue by checking the queue association
    status of all the queues of the ethernet device.
    
    Fixes: 741b499e6421 ("eventdev/eth_tx: fix queue delete logic")
    
    Signed-off-by: Naga Harish K S V <s.v.naga.harish.k@intel.com>

--- lib/librte_eventdev/rte_event_eth_tx_adapter.c
+++ lib/librte_eventdev/rte_event_eth_tx_adapter.c
@@ -821,6 +821,8 @@ txa_service_queue_del(uint8_t id,
 		uint16_t i, q, nb_queues;
 		int ret = 0;
 
+		if (txa->txa_ethdev == NULL)
+			return 0;
 		nb_queues = txa->txa_ethdev[port_id].nb_queues;
 		if (nb_queues == 0)
 			return 0;
@@ -833,10 +835,10 @@ txa_service_queue_del(uint8_t id,
 
 			if (tqi[q].added) {
 				ret = txa_service_queue_del(id, dev, q);
+				i++;
 				if (ret != 0)
 					break;
 			}
-			i++;
 			q++;
 		}
 		return ret;
