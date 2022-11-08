c8c70e90664f5a9eb87737b2ad2ffafade607b29
Alexander Kozyrev <akozyrev@nvidia.com>
2022-11-08 15:45:00 +0200

    net/mlx5: fix first segment inline length
    [ upstream commit da4470cb178b87c9637cb575719e022182ea38da ]
    
    Packets can be split into several mbufs with various data sizes.
    There is no limitation on how small these segments can be.
    But there is a limitation on Tx side for inline configuration:
    send WQEs with inline headers less than the required are dropped.
    The very first segment must be more than minimal inline eth segment.
    Enforce this requirement by merging a few segments in this case.
    
    Fixes: ec837ad0fc7c ("net/mlx5: fix multi-segment inline for the first segments")
    
    Signed-off-by: Alexander Kozyrev <akozyrev@nvidia.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

--- drivers/net/mlx5/mlx5_rxtx.c
+++ drivers/net/mlx5/mlx5_rxtx.c
@@ -3490,6 +3490,8 @@ mlx5_tx_packet_multi_inline(struct mlx5_txq_data *__rte_restrict txq,
 		} else if (mbuf->ol_flags & PKT_TX_DYNF_NOINLINE ||
 			   nxlen > txq->inlen_send) {
 			return mlx5_tx_packet_multi_send(txq, loc, olx);
+		} else if (nxlen <= MLX5_ESEG_MIN_INLINE_SIZE) {
+			inlen = MLX5_ESEG_MIN_INLINE_SIZE;
 		} else {
 			goto do_first;
 		}
