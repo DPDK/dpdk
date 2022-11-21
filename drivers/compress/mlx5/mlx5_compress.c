559014f232b461d2dac217dee02c09c78019d357
Raslan Darawsheh <rasland@nvidia.com>
2022-11-21 16:12:46 +0200

    compress/mlx5: add Bluefield-3 device ID
    This adds the Bluefield-3 device ID to the list of
    supported NVIDIA devices that run the MLX5 compress PMD.
    The device is still in development stage.
    
    Signed-off-by: Raslan Darawsheh <rasland@nvidia.com>

--- drivers/compress/mlx5/mlx5_compress.c
+++ drivers/compress/mlx5/mlx5_compress.c
@@ -791,6 +791,10 @@ static const struct rte_pci_id mlx5_compress_pci_id_map[] = {
 				PCI_DEVICE_ID_MELLANOX_CONNECTX6DXBF)
 	},
 	{
+		RTE_PCI_DEVICE(PCI_VENDOR_ID_MELLANOX,
+				PCI_DEVICE_ID_MELLANOX_CONNECTX7BF)
+	},
+	{
 		.vendor_id = 0
 	}
 };
