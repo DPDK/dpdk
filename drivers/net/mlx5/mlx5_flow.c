d5be08262127a74d41e2db1f740dbdc099b1399d
Jiawei Wang <jiaweiw@nvidia.com>
2022-12-09 16:34:06 +0200

    net/mlx5: fix flow sample with ConnectX-5
    [ upstream commit 7aa6d7998a9e59dc510e4e5706f675493855ff27 ]
    
    The sample flow tried to get the reserved metadata register for
    the match implicitly, and if the reserved metadata register was
    invalid then back to use the application tag.
    
    The assertion failure was caused while getting registered due to
    the reserved metadata regC is invalid on CX-5 trusted device(VF/SF).
    
    This patch adds the checking for reserved metadata register before
    getting the register, to avoid assertion failure.
    
    Fixes: 9a726360dd30 ("net/mlx5: fix sample flow action on trusted device")
    
    Signed-off-by: Jiawei Wang <jiaweiw@nvidia.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

--- drivers/net/mlx5/mlx5_flow.c
+++ drivers/net/mlx5/mlx5_flow.c
@@ -6125,13 +6125,14 @@ flow_sample_split_prep(struct rte_eth_dev *dev,
 		/* Prepare the prefix tag action. */
 		append_index++;
 		set_tag = (void *)(actions_pre + actions_n + append_index);
-		ret = mlx5_flow_get_reg_id(dev, MLX5_SAMPLE_ID, 0, error);
 		/* Trust VF/SF on CX5 not supported meter so that the reserved
 		 * metadata regC is REG_NON, back to use application tag
 		 * index 0.
 		 */
-		if (unlikely(ret == REG_NON))
+		if (unlikely(priv->mtr_color_reg == REG_NON))
 			ret = mlx5_flow_get_reg_id(dev, MLX5_APP_TAG, 0, error);
+		else
+			ret = mlx5_flow_get_reg_id(dev, MLX5_SAMPLE_ID, 0, error);
 		if (ret < 0)
 			return ret;
 		mlx5_ipool_malloc(priv->sh->ipool
