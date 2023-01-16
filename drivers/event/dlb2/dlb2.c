c87e76f66b66a2d604c707e7f47c9f6f15b6bc2c
Morten Brørup <mb@smartsharesystems.com>
2023-01-16 14:07:22 +0100

    event/dlb2: remove superfluous memcpy
    [ upstream commit c1b086d2abdb7773700b4d216f323bd9278ace7a ]
    
    Copying with the same src and dst address has no effect; removed to
    avoid compiler warning with decorated rte_memcpy.
    
    Fixes: e7c9971a857a ("event/dlb2: add probe-time hardware init")
    
    Signed-off-by: Morten Brørup <mb@smartsharesystems.com>
    Acked-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Abdullah Sevincer <abdullah.sevincer@intel.com>

--- drivers/event/dlb2/dlb2.c
+++ drivers/event/dlb2/dlb2.c
@@ -112,7 +112,6 @@ static int
 dlb2_hw_query_resources(struct dlb2_eventdev *dlb2)
 {
 	struct dlb2_hw_dev *handle = &dlb2->qm_instance;
-	struct dlb2_hw_resource_info *dlb2_info = &handle->info;
 	int ret;
 
 	/* Query driver resources provisioned for this device */
@@ -169,8 +168,6 @@ dlb2_hw_query_resources(struct dlb2_eventdev *dlb2)
 
 	handle->info.hw_rsrc_max.reorder_window_size =
 		dlb2->hw_rsrc_query_results.num_hist_list_entries;
-
-	rte_memcpy(dlb2_info, &handle->info.hw_rsrc_max, sizeof(*dlb2_info));
 
 	return 0;
 }
