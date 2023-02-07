fefed7a297c774c11427b437e739033cace31865
Srikanth Yalavarthi <syalavarthi@marvell.com>
2023-02-07 08:00:05 -0800

    mldev: add IO type handling
    Implemented ML utility functions to convert IO data type to name,
    IO format to name and routine to get the size of an IO data type
    in bytes.
    
    Signed-off-by: Srikanth Yalavarthi <syalavarthi@marvell.com>
    Acked-by: Anup Prabhu <aprabhu@marvell.com>

---
+++ lib/mldev/mldev_utils.h
@@ -0,0 +1,76 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2022 Marvell.
+ */
+
+#ifndef RTE_MLDEV_UTILS_H
+#define RTE_MLDEV_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file
+ *
+ * ML Device PMD utility API
+ *
+ * These APIs for the use from ML drivers, user applications shouldn't use them.
+ *
+ */
+
+#include <rte_compat.h>
+#include <rte_mldev.h>
+
+/**
+ * @internal
+ *
+ * Get the size an ML IO type in bytes.
+ *
+ * @param[in] type
+ *	Enumeration of ML IO data type.
+ *
+ * @return
+ *	- > 0, Size of the data type in bytes.
+ *	- < 0, Error code on failure.
+ */
+__rte_internal
+int
+rte_ml_io_type_size_get(enum rte_ml_io_type type);
+
+/**
+ * @internal
+ *
+ * Get the name of an ML IO type.
+ *
+ * @param[in] type
+ *	Enumeration of ML IO data type.
+ * @param[in] str
+ *	Address of character array.
+ * @param[in] len
+ *	Length of character array.
+ */
+__rte_internal
+void
+rte_ml_io_type_to_str(enum rte_ml_io_type type, char *str, int len);
+
+/**
+ * @internal
+ *
+ * Get the name of an ML IO format.
+ *
+ * @param[in] type
+ *	Enumeration of ML IO format.
+ * @param[in] str
+ *	Address of character array.
+ * @param[in] len
+ *	Length of character array.
+ */
+__rte_internal
+void
+rte_ml_io_format_to_str(enum rte_ml_io_format format, char *str, int len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RTE_MLDEV_UTILS_H */
