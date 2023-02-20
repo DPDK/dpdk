a0c332995e20c3cb4c05570ddbffc73fef9a16f2
Raslan Darawsheh <rasland@nvidia.com>
2023-02-20 10:48:26 +0200

    crypto/mlx5: add Bluefield-3 and ConnectX-7 device IDs
    This adds the Bluefield-3 and ConnectX-7 device IDs to the
    list of supported NVIDIA devices that run the MLX5 compress PMD.
    The BlueField-3 device is still in development stage.
    
    Signed-off-by: Raslan Darawsheh <rasland@nvidia.com>

--- drivers/crypto/mlx5/mlx5_crypto.c
+++ drivers/crypto/mlx5/mlx5_crypto.c
@@ -995,6 +995,14 @@ static const struct rte_pci_id mlx5_crypto_pci_id_map[] = {
 					PCI_DEVICE_ID_MELLANOX_BLUEFIELD2)
 		},
 		{
+			RTE_PCI_DEVICE(PCI_VENDOR_ID_MELLANOX,
+					PCI_DEVICE_ID_MELLANOX_CONNECTX7)
+		},
+		{
+			RTE_PCI_DEVICE(PCI_VENDOR_ID_MELLANOX,
+					PCI_DEVICE_ID_MELLANOX_BLUEFIELD3)
+		},
+		{
 			.vendor_id = 0
 		}
 };
