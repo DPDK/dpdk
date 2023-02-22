946d771b293e49a35a9fdbc314ccc74af844ac7e
Hernan Vargas <hernan.vargas@intel.com>
2023-02-22 08:40:42 -0800

    baseband/acc: remove unused ops check
    Remove unnecessary debug check for ops.
    
    Signed-off-by: Hernan Vargas <hernan.vargas@intel.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

--- drivers/baseband/acc/rte_acc100_pmd.c
+++ drivers/baseband/acc/rte_acc100_pmd.c
@@ -4106,10 +4106,6 @@ acc100_dequeue_ldpc_enc(struct rte_bbdev_queue_data *q_data,
 	int ret, cbm;
 	struct rte_bbdev_enc_op *op;
 
-#ifdef RTE_LIBRTE_BBDEV_DEBUG
-	if (unlikely(ops == 0))
-		return 0;
-#endif
 	op = acc_op_tail(q, 0);
 	if (unlikely(ops == NULL || op == NULL))
 		return 0;
