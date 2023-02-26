DPDK/dpdk

commit 72185352955e180a23faeef857ff6570d2c2202f

net/mlx5: support ESP item in Verbs interface
ESP item is not currently supported in Verbs interface.

Validate/translate ESP item in Verbs interface.

Signed-off-by: Maayan Kashani <mkashani@nvidia.com>
Acked-by: Matan Azrad <matan@nvidia.com>

drivers/net/mlx5/mlx5_flow_verbs.c
@@ -24,6 +24,7 @@
 #include "mlx5.h"
 #include "mlx5_flow.h"
 #include "mlx5_rx.h"
+#include "mlx5_flow_os.h"
 
 #define VERBS_SPEC_INNER(item_flags) \
 	(!!((item_flags) & MLX5_FLOW_LAYER_TUNNEL) ? IBV_FLOW_SPEC_INNER : 0)
@@ -684,6 +685,42 @@ flow_verbs_translate_item_tcp(struct mlx5_flow *dev_flow,
  * @param[in] item_flags
  *   Parsed item flags.
  */
+#ifdef HAVE_IBV_FLOW_SPEC_ESP
+static void
+flow_verbs_translate_item_esp(struct mlx5_flow *dev_flow,
+			      const struct rte_flow_item *item,
+			      uint64_t item_flags __rte_unused)
+{
+	const struct rte_flow_item_esp *spec = item->spec;
+	const struct rte_flow_item_esp *mask = item->mask;
+	unsigned int size = sizeof(struct ibv_flow_spec_esp);
+	struct ibv_flow_spec_esp esp = {
+		.type = IBV_FLOW_SPEC_ESP | VERBS_SPEC_INNER(item_flags),
+		.size = size,
+	};
+
+	if (!mask)
+		mask = &rte_flow_item_esp_mask;
+	if (spec) {
+		esp.val.spi = spec->hdr.spi & mask->hdr.spi;
+		esp.mask.spi = mask->hdr.spi;
+	}
+	flow_verbs_spec_add(&dev_flow->verbs, &esp, size);
+}
+#endif
+
+/**
+ * Convert the @p item into a Verbs specification. This function assumes that
+ * the input is valid and that there is space to insert the requested item
+ * into the flow.
+ *
+ * @param[in, out] dev_flow
+ *   Pointer to dev_flow structure.
+ * @param[in] item
+ *   Item specification.
+ * @param[in] item_flags
+ *   Parsed item flags.
+ */
 static void
 flow_verbs_translate_item_udp(struct mlx5_flow *dev_flow,
 			      const struct rte_flow_item *item,
@@ -1293,6 +1330,14 @@ flow_verbs_validate(struct rte_eth_dev *dev,
 		int ret = 0;
 
 		switch (items->type) {
+		case RTE_FLOW_ITEM_TYPE_ESP:
+			ret = mlx5_flow_os_validate_item_esp(items, item_flags,
+							  next_protocol,
+							  error);
+			if (ret < 0)
+				return ret;
+			last_item = MLX5_FLOW_ITEM_ESP;
+			break;
 		case RTE_FLOW_ITEM_TYPE_VOID:
 			break;
 		case RTE_FLOW_ITEM_TYPE_ETH:
@@ -1879,6 +1924,18 @@ flow_verbs_translate(struct rte_eth_dev *dev,
 			item_flags |= tunnel ? MLX5_FLOW_LAYER_INNER_L4_UDP :
 					       MLX5_FLOW_LAYER_OUTER_L4_UDP;
 			break;
+#ifdef HAVE_IBV_FLOW_SPEC_ESP
+		case RTE_FLOW_ITEM_TYPE_ESP:
+			flow_verbs_translate_item_esp(dev_flow, items,
+						      item_flags);
+			dev_flow->hash_fields |=
+				mlx5_flow_hashfields_adjust
+				(rss_desc, tunnel,
+				RTE_ETH_RSS_ESP,
+				IBV_RX_HASH_IPSEC_SPI);
+			item_flags |= MLX5_FLOW_ITEM_ESP;
+			break;
+#endif
 		case RTE_FLOW_ITEM_TYPE_VXLAN:
 			flow_verbs_translate_item_vxlan(dev_flow, items,
 							item_flags);

drivers/net/mlx5/windows/mlx5_flow_os.c
@@ -419,8 +419,8 @@ mlx5_flow_os_set_specific_workspace(struct mlx5_flow_workspace *data)
 
 int
 mlx5_flow_os_validate_item_esp(const struct rte_flow_item *item,
-			    uint64_t item_flags,
-			    uint8_t target_protocol,
+			    uint64_t item_flags __rte_unused,
+			    uint8_t target_protocol __rte_unused,
 			    struct rte_flow_error *error)
 {
 	const struct rte_flow_item_esp *mask = item->mask;
@@ -432,6 +432,11 @@ mlx5_flow_os_validate_item_esp(const struct rte_flow_item *item,
 				      MLX5_FLOW_LAYER_OUTER_L4;
 	int ret;
 
+#ifndef HAVE_IBV_FLOW_SPEC_ESP
+	return rte_flow_error_set(error, ENOTSUP,
+					  RTE_FLOW_ERROR_TYPE_ITEM, item,
+					  "ESP item not supported");
+#endif
 	if (!(item_flags & l3m))
 		return rte_flow_error_set(error, EINVAL,
 					  RTE_FLOW_ERROR_TYPE_ITEM, item,

.mailmap
@@ -780,6 +780,7 @@ Luke Gorrie <luke@snabb.co>
 Lunyuan Cui <lunyuanx.cui@intel.com>
 Lu Qiuwen <luqiuwen@iie.ac.cn>
 Lyn M <netinal7@gmail.com>
+Maayan Kashani <mkashani@nvidia.com>
 Maciej Bielski <mba@semihalf.com>
 Maciej Czekaj <mczekaj@marvell.com> <maciej.czekaj@caviumnetworks.com>
 Maciej Fijalkowski <maciej.fijalkowski@intel.com>

drivers/common/mlx5/linux/meson.build
@@ -81,6 +81,8 @@ has_member_args = [
 # [ "MACRO to define if found", "header for the search",
 #   "symbol to search" ]
 has_sym_args = [
+        [ 'HAVE_IBV_FLOW_SPEC_ESP', 'infiniband/verbs.h',
+            'IBV_FLOW_SPEC_ESP' ],
         [ 'HAVE_IBV_RX_HASH_IPSEC_SPI', 'infiniband/verbs.h',
             'IBV_RX_HASH_IPSEC_SPI' ],
         [ 'HAVE_IBV_RELAXED_ORDERING', 'infiniband/verbs.h',

commit 90385c46bb623ee5d50e3d15ffe0f77a4f61d016

net/mlx5: fix ESP item validation in Verbs interface
While added ESP item support in Verbs interface,
mlx5_flow_os_validate_item_esp routine in windows/mlx5_flow_os.c was
changed.
All changes should be in mlx5_flow_verbs.c only.

If ESP is not supported in Verbs, return default "Item not supported
error" during item validation stage.

Also, updated mlx5.rst file regarding ESP SPI match support following
further debug.

Fixes: 72185352955e ("net/mlx5: support ESP item in Verbs interface")

Signed-off-by: Maayan Kashani <mkashani@nvidia.com>
Acked-by: Matan Azrad <matan@nvidia.com>

drivers/net/mlx5/mlx5_flow_verbs.c
@@ -1330,6 +1330,7 @@ flow_verbs_validate(struct rte_eth_dev *dev,
 		int ret = 0;
 
 		switch (items->type) {
+#ifdef HAVE_IBV_FLOW_SPEC_ESP
 		case RTE_FLOW_ITEM_TYPE_ESP:
 			ret = mlx5_flow_os_validate_item_esp(items, item_flags,
 							  next_protocol,
@@ -1338,6 +1339,7 @@ flow_verbs_validate(struct rte_eth_dev *dev,
 				return ret;
 			last_item = MLX5_FLOW_ITEM_ESP;
 			break;
+#endif
 		case RTE_FLOW_ITEM_TYPE_VOID:
 			break;
 		case RTE_FLOW_ITEM_TYPE_ETH:

drivers/net/mlx5/windows/mlx5_flow_os.c
@@ -419,8 +419,8 @@ mlx5_flow_os_set_specific_workspace(struct mlx5_flow_workspace *data)
 
 int
 mlx5_flow_os_validate_item_esp(const struct rte_flow_item *item,
-			    uint64_t item_flags __rte_unused,
-			    uint8_t target_protocol __rte_unused,
+			    uint64_t item_flags,
+			    uint8_t target_protocol,
 			    struct rte_flow_error *error)
 {
 	const struct rte_flow_item_esp *mask = item->mask;
@@ -432,11 +432,6 @@ mlx5_flow_os_validate_item_esp(const struct rte_flow_item *item,
 				      MLX5_FLOW_LAYER_OUTER_L4;
 	int ret;
 
-#ifndef HAVE_IBV_FLOW_SPEC_ESP
-	return rte_flow_error_set(error, ENOTSUP,
-					  RTE_FLOW_ERROR_TYPE_ITEM, item,
-					  "ESP item not supported");
-#endif
 	if (!(item_flags & l3m))
 		return rte_flow_error_set(error, EINVAL,
 					  RTE_FLOW_ERROR_TYPE_ITEM, item,