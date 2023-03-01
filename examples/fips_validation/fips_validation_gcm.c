DPDK/dpdk

commit 5b540bebac8e40971cdd0436f350797d02c7842e

examples/fips_validation: fix GMAC decryption output
AES GMAC decrypt test should output only
whether test passed or failed.

Fixes: ad42b228c6e2 ("examples/fips_validation: fix print for zero length payload")
Cc: stable@dpdk.org

Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
Reviewed-by: Brian Dooley <brian.dooley@intel.com>

examples/fips_validation/fips_validation_gcm.c
@@ -330,11 +330,16 @@ parse_test_gcm_json_writeback(struct fips_val *val)
 				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
 					json_string(info.one_line_text));
 			} else {
-				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
-					json_string(""));
+				json_object_set_new(json_info.json_write_case, "testPassed",
+					json_true());
 			}
 		} else {
-			json_object_set_new(json_info.json_write_case, "testPassed", json_false());
+			if (!info.interim_info.gcm_data.is_gmac)
+				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
+					json_string(""));
+			else
+				json_object_set_new(json_info.json_write_case, "testPassed",
+					json_false());
 		}
 	}
 

commit 02913adaed71fa8437fca727220303b4a0df0896

examples/fips_validation: fix AES-GCM tests
AES GCM validation tests fail in FIPS validation due to incorrect
fields populated in response file. This patch fixes them.

Fixes: 5b540bebac8e ("examples/fips_validation: fix GMAC decryption output")
Cc: stable@dpdk.org

Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
Acked-by: Brian Dooley <brian.dooley@intel.com>

examples/fips_validation/fips_validation_gcm.c
@@ -79,7 +79,7 @@ parser_read_gcm_pt_len(const char *key, char *src,
 	if (ret < 0)
 		return ret;
 
-	if (vec.pt.len == 0) {
+	if (info.algo == FIPS_TEST_ALGO_AES_GMAC && vec.pt.len == 0) {
 		info.interim_info.gcm_data.is_gmac = 1;
 		test_ops.prepare_sym_op = prepare_auth_op;
 		test_ops.prepare_sym_xform = prepare_gmac_xform;
@@ -296,6 +296,7 @@ parse_test_gcm_json_writeback(struct fips_val *val)
 			tmp_val.val = val->val;
 			tmp_val.len = vec.pt.len;
 
+			info.one_line_text[0] = '\0';
 			writeback_hex_str("", info.one_line_text, &tmp_val);
 			ct = json_string(info.one_line_text);
 			json_object_set_new(json_info.json_write_case, CT_JSON_STR, ct);
@@ -326,6 +327,7 @@ parse_test_gcm_json_writeback(struct fips_val *val)
 				tmp_val.val = val->val;
 				tmp_val.len = vec.pt.len;
 
+				info.one_line_text[0] = '\0';
 				writeback_hex_str("", info.one_line_text, &tmp_val);
 				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
 					json_string(info.one_line_text));
@@ -334,12 +336,8 @@ parse_test_gcm_json_writeback(struct fips_val *val)
 					json_true());
 			}
 		} else {
-			if (!info.interim_info.gcm_data.is_gmac)
-				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
-					json_string(""));
-			else
-				json_object_set_new(json_info.json_write_case, "testPassed",
-					json_false());
+			json_object_set_new(json_info.json_write_case, "testPassed",
+				json_false());
 		}
 	}
 