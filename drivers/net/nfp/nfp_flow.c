DPDK/dpdk

commit 596ae22172147931447c92bf39e8c2a7c1ff8bdf

net/nfp: support IPv6 source flow action
Add the corresponding data structure and logics, to support
the offload of set source IPv6 address action.

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

drivers/net/nfp/nfp_flow.c
@@ -631,6 +631,10 @@ nfp_flow_key_layers_calculate_actions(const struct rte_flow_action actions[],
 				ip_set_flag = true;
 			}
 			break;
+		case RTE_FLOW_ACTION_TYPE_SET_IPV6_SRC:
+			PMD_DRV_LOG(DEBUG, "RTE_FLOW_ACTION_TYPE_SET_IPV6_SRC detected");
+			key_ls->act_size += sizeof(struct nfp_fl_act_set_ipv6_addr);
+			break;
 		default:
 			PMD_DRV_LOG(ERR, "Action type %d not supported.", action->type);
 			return -ENOTSUP;
@@ -1322,6 +1326,32 @@ nfp_flow_action_set_ip(char *act_data,
 		set_ip->ipv4_dst = set_ipv4->ipv4_addr;
 }
 
+static void
+nfp_flow_action_set_ipv6(char *act_data,
+		const struct rte_flow_action *action,
+		bool ip_src_flag)
+{
+	int i;
+	size_t act_size;
+	struct nfp_fl_act_set_ipv6_addr *set_ip;
+	const struct rte_flow_action_set_ipv6 *set_ipv6;
+
+	set_ip = (struct nfp_fl_act_set_ipv6_addr *)act_data;
+	set_ipv6 = (const struct rte_flow_action_set_ipv6 *)action->conf;
+
+	if (ip_src_flag)
+		set_ip->head.jump_id = NFP_FL_ACTION_OPCODE_SET_IPV6_SRC;
+	else
+		set_ip->head.jump_id = NFP_FL_ACTION_OPCODE_SET_IPV6_DST;
+
+	act_size = sizeof(struct nfp_fl_act_set_ipv6_addr);
+	set_ip->head.len_lw = act_size >> NFP_FL_LW_SIZ;
+	set_ip->reserved = 0;
+
+	for (i = 0; i < 4; i++)
+		set_ip->ipv6[i].exact = set_ipv6->ipv6_addr[i];
+}
+
 static int
 nfp_flow_action_push_vlan(char *act_data,
 		const struct rte_flow_action *action)
@@ -1449,6 +1479,11 @@ nfp_flow_compile_action(__rte_unused struct nfp_flower_representor *representor,
 				position += sizeof(struct nfp_fl_act_set_ip4_addrs);
 				ip_set_flag = true;
 			}
+			break;
+		case RTE_FLOW_ACTION_TYPE_SET_IPV6_SRC:
+			PMD_DRV_LOG(DEBUG, "Process RTE_FLOW_ACTION_TYPE_SET_IPV6_SRC");
+			nfp_flow_action_set_ipv6(position, action, true);
+			position += sizeof(struct nfp_fl_act_set_ipv6_addr);
 			break;
 		default:
 			PMD_DRV_LOG(ERR, "Unsupported action type: %d", action->type);

doc/guides/nics/features/nfp.ini
@@ -46,5 +46,6 @@ of_set_vlan_vid      = Y
 port_id              = Y
 set_ipv4_dst         = Y
 set_ipv4_src         = Y
+set_ipv6_src         = Y
 set_mac_dst          = Y
 set_mac_src          = Y

drivers/net/nfp/flower/nfp_flower_cmsg.h
@@ -399,6 +399,39 @@ struct nfp_fl_act_set_ip4_addrs {
 	rte_be32_t ipv4_dst;
 };
 
+/*
+ * IPv6 addr
+ *    3                   2                   1
+ *  1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |   -   |opcode |       |jump_id|               -               |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_127_96_mask                     |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_127_96                          |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_95_64_mask                      |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_95_64                           |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_63_32_mask                      |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_63_32                           |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_31_0_mask                       |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv6_addr_31_0                            |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct nfp_fl_act_set_ipv6_addr {
+	struct nfp_fl_act_head head;
+	rte_be16_t reserved;
+	struct {
+		rte_be32_t mask;
+		rte_be32_t exact;
+	} ipv6[4];
+};
+
 int nfp_flower_cmsg_mac_repr(struct nfp_app_fw_flower *app_fw_flower);
 int nfp_flower_cmsg_repr_reify(struct nfp_app_fw_flower *app_fw_flower,
 		struct nfp_flower_representor *repr);

commit 784835e690e011754a67a240fdc6154ea349ec20

net/nfp: fix set IPv6 flow action
The former logic of set IPv6 source/destination address flow action
don't consider the mask filed of control message passed to the firmware.
This caused the firmware skip the set action logic, and the offloaded
packets don't have the right IPv6 address as expected.

Fixes: 596ae2217214 ("net/nfp: support IPv6 source flow action")
Fixes: 51384f79b264 ("net/nfp: support IPv6 destination flow action")
Cc: stable@dpdk.org

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

drivers/net/nfp/nfp_flow.c
@@ -2163,8 +2163,10 @@ nfp_flow_action_set_ipv6(char *act_data,
 	set_ip->head.len_lw = act_size >> NFP_FL_LW_SIZ;
 	set_ip->reserved = 0;
 
-	for (i = 0; i < 4; i++)
-		set_ip->ipv6[i].exact = set_ipv6->ipv6_addr[i];
+	for (i = 0; i < 4; i++) {
+		set_ip->ipv6[i].exact = set_ipv6->ipv6_addr[i * 4];
+		set_ip->ipv6[i].mask = RTE_BE32(0xffffffff);
+	}
 }
 
 static void