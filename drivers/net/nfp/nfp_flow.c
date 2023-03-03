DPDK/dpdk

commit ab80975b0a14496d9085d57364ccb5e340a400cc

net/nfp: support IPv4 source flow action
Add the corresponding data structure and logics, to support
the offload of set source IPv4 address action.

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

drivers/net/nfp/nfp_flow.c
@@ -562,6 +562,7 @@ nfp_flow_key_layers_calculate_actions(const struct rte_flow_action actions[],
 {
 	int ret = 0;
 	bool mac_set_flag = false;
+	bool ip_set_flag = false;
 	const struct rte_flow_action *action;
 
 	for (action = actions; action->type != RTE_FLOW_ACTION_TYPE_END; ++action) {
@@ -613,6 +614,14 @@ nfp_flow_key_layers_calculate_actions(const struct rte_flow_action actions[],
 			break;
 		case RTE_FLOW_ACTION_TYPE_OF_SET_VLAN_PCP:
 			PMD_DRV_LOG(DEBUG, "RTE_FLOW_ACTION_TYPE_OF_SET_VLAN_PCP detected");
+			break;
+		case RTE_FLOW_ACTION_TYPE_SET_IPV4_SRC:
+			PMD_DRV_LOG(DEBUG, "RTE_FLOW_ACTION_TYPE_SET_IPV4_SRC detected");
+			if (!ip_set_flag) {
+				key_ls->act_size +=
+					sizeof(struct nfp_fl_act_set_ip4_addrs);
+				ip_set_flag = true;
+			}
 			break;
 		default:
 			PMD_DRV_LOG(ERR, "Action type %d not supported.", action->type);
@@ -1278,6 +1287,33 @@ nfp_flow_action_pop_vlan(char *act_data,
 	nfp_flow_meta->shortcut = rte_cpu_to_be_32(NFP_FL_SC_ACT_POPV);
 }
 
+static void
+nfp_flow_action_set_ip(char *act_data,
+		const struct rte_flow_action *action,
+		bool ip_src_flag,
+		bool ip_set_flag)
+{
+	size_t act_size;
+	struct nfp_fl_act_set_ip4_addrs *set_ip;
+	const struct rte_flow_action_set_ipv4 *set_ipv4;
+
+	if (ip_set_flag)
+		set_ip = (struct nfp_fl_act_set_ip4_addrs *)act_data - 1;
+	else
+		set_ip = (struct nfp_fl_act_set_ip4_addrs *)act_data;
+
+	act_size = sizeof(struct nfp_fl_act_set_ip4_addrs);
+	set_ip->head.jump_id = NFP_FL_ACTION_OPCODE_SET_IPV4_ADDRS;
+	set_ip->head.len_lw  = act_size >> NFP_FL_LW_SIZ;
+	set_ip->reserved     = 0;
+
+	set_ipv4 = (const struct rte_flow_action_set_ipv4 *)action->conf;
+	if (ip_src_flag)
+		set_ip->ipv4_src = set_ipv4->ipv4_addr;
+	else
+		set_ip->ipv4_dst = set_ipv4->ipv4_addr;
+}
+
 static int
 nfp_flow_action_push_vlan(char *act_data,
 		const struct rte_flow_action *action)
@@ -1320,6 +1356,7 @@ nfp_flow_compile_action(__rte_unused struct nfp_flower_representor *representor,
 	char *position;
 	char *action_data;
 	bool drop_flag = false;
+	bool ip_set_flag = false;
 	bool mac_set_flag = false;
 	uint32_t total_actions = 0;
 	const struct rte_flow_action *action;
@@ -1388,6 +1425,14 @@ nfp_flow_compile_action(__rte_unused struct nfp_flower_representor *representor,
 			 */
 			action += 2;
 			position += sizeof(struct nfp_fl_act_push_vlan);
+			break;
+		case RTE_FLOW_ACTION_TYPE_SET_IPV4_SRC:
+			PMD_DRV_LOG(DEBUG, "Process RTE_FLOW_ACTION_TYPE_SET_IPV4_SRC");
+			nfp_flow_action_set_ip(position, action, true, ip_set_flag);
+			if (!ip_set_flag) {
+				position += sizeof(struct nfp_fl_act_set_ip4_addrs);
+				ip_set_flag = true;
+			}
 			break;
 		default:
 			PMD_DRV_LOG(ERR, "Unsupported action type: %d", action->type);

doc/guides/nics/features/nfp.ini
@@ -44,5 +44,6 @@ of_push_vlan         = Y
 of_set_vlan_pcp      = Y
 of_set_vlan_vid      = Y
 port_id              = Y
+set_ipv4_src         = Y
 set_mac_dst          = Y
 set_mac_src          = Y

drivers/net/nfp/flower/nfp_flower_cmsg.h
@@ -374,6 +374,31 @@ struct nfp_fl_act_push_vlan {
 	rte_be16_t vlan_tci;
 };
 
+/*
+ * IPv4 addrs
+ *    3                   2                   1
+ *  1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |   -   |opcode |       |jump_id|               -               |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv4_saddr_mask                           |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv4_saddr                                |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv4_daddr_mask                           |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |                     ipv4_daddr                                |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct nfp_fl_act_set_ip4_addrs {
+	struct nfp_fl_act_head head;
+	rte_be16_t reserved;
+	rte_be32_t ipv4_src_mask;
+	rte_be32_t ipv4_src;
+	rte_be32_t ipv4_dst_mask;
+	rte_be32_t ipv4_dst;
+};
+
 int nfp_flower_cmsg_mac_repr(struct nfp_app_fw_flower *app_fw_flower);
 int nfp_flower_cmsg_repr_reify(struct nfp_app_fw_flower *app_fw_flower,
 		struct nfp_flower_representor *repr);

commit d4b453c42989eb689392fccc7d4b0bc5e1499470

net/nfp: fix set IPv4 flow action
The former logic of set IPv4 source/destination address flow action
don't consider the mask filed of control message passed to the firmware.
This caused the firmware skip the set action logic, and the offloaded
packets don't have the right IPv4 address as expected.

Fixes: ab80975b0a14 ("net/nfp: support IPv4 source flow action")
Fixes: f85c1e05bff3 ("net/nfp: support IPv4 destination flow action")
Cc: stable@dpdk.org

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

drivers/net/nfp/nfp_flow.c
@@ -2132,10 +2132,13 @@ nfp_flow_action_set_ip(char *act_data,
 	set_ip->reserved     = 0;
 
 	set_ipv4 = (const struct rte_flow_action_set_ipv4 *)action->conf;
-	if (ip_src_flag)
+	if (ip_src_flag) {
 		set_ip->ipv4_src = set_ipv4->ipv4_addr;
-	else
+		set_ip->ipv4_src_mask = RTE_BE32(0xffffffff);
+	} else {
 		set_ip->ipv4_dst = set_ipv4->ipv4_addr;
+		set_ip->ipv4_dst_mask = RTE_BE32(0xffffffff);
+	}
 }
 
 static void