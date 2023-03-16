7faf9ad3c41bea806c9222110cdecd04d7db8bfe
Fengnan Chang <changfengnan@bytedance.com>
2023-03-16 19:48:18 +0800

    eal: cleanup alarm and hotplug before memory detach
    [ upstream commit cc75968228576250159918ec1531eecc606bc423 ]
    
    Alarm and multiprocess hotplug still need access hugepage memory,
    if alarm event processed after memory detach, it may cause SEGV.
    So cleanup alarm and multiprocess hotplug before memory detach.
    
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>

--- lib/eal/freebsd/eal.c
+++ lib/eal/freebsd/eal.c
@@ -988,9 +988,9 @@ rte_eal_cleanup(void)
 	rte_mp_channel_cleanup();
 	rte_trace_save();
 	eal_trace_fini();
+	rte_eal_alarm_cleanup();
 	/* after this point, any DPDK pointers will become dangling */
 	rte_eal_memory_detach();
-	rte_eal_alarm_cleanup();
 	eal_cleanup_config(internal_conf);
 	return 0;
 }
