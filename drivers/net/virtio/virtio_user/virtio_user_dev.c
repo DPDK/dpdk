2a45186cf44c51ad11e6a8f3d08a0a78a10da952
David Marchand <david.marchand@redhat.com>
2023-04-13 12:10:41 +0200

    net/virtio-user: fix leak when initialisation fails
    [ upstream commit c8254ef1a96c4897293b1d1db8c8a786dfb19e9d ]
    
    Caught with ASan.
    If initialising a virtio_user port fails, we may leak the ifname passed
    via a devargs.
    
    Fixes: 4214a1b493f2 ("net/virtio-user: support changing tap interface name")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Acked-by: Tyler Retzlaff <roretzla@linux.microsoft.com>

--- drivers/net/virtio/virtio_user/virtio_user_dev.c
+++ drivers/net/virtio/virtio_user/virtio_user_dev.c
@@ -586,11 +586,7 @@ virtio_user_dev_init(struct virtio_user_dev *dev, char *path, int queues,
 	dev->frontend_features = 0;
 	dev->unsupported_features = 0;
 	dev->backend_type = backend_type;
-
-	if (*ifname) {
-		dev->ifname = *ifname;
-		*ifname = NULL;
-	}
+	dev->ifname = *ifname;
 
 	if (virtio_user_dev_setup(dev) < 0) {
 		PMD_INIT_LOG(ERR, "(%s) backend set up fails", dev->path);
@@ -663,6 +659,7 @@ virtio_user_dev_init(struct virtio_user_dev *dev, char *path, int queues,
 		}
 	}
 
+	*ifname = NULL;
 	return 0;
 }
 
