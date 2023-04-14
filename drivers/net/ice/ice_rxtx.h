54b08280e728e709b654a31597aa2d119379dfd6
Wenjun Wu <wenjun1.wu@intel.com>
2023-04-14 13:47:41 +0800

    net/ice: fix Rx data buffer size
    [ upstream commit 9a5c9dc475428c7d2c74383c0903541ab80144ea ]
    
    This patch does two fixes.
    
    1. According to hardware spec, the data buffer size should not
    be greater than 16K - 128.
    
    2. Replace RTE_ALIGN with RTE_ALIGN_FLOOR according to [1].
    
    [1] Commit c9c45beb1b97 ("net/iavf: fix Rx queue buffer size alignment")
    
    Fixes: 50370662b727 ("net/ice: support device and queue ops")
    Fixes: 1b009275e2c8 ("net/ice: add Rx queue init in DCF")
    
    Signed-off-by: Wenjun Wu <wenjun1.wu@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/ice/ice_rxtx.h
+++ drivers/net/ice/ice_rxtx.h
@@ -45,6 +45,9 @@
 extern uint64_t ice_timestamp_dynflag;
 extern int ice_timestamp_dynfield_offset;
 
+/* Max data buffer size must be 16K - 128 bytes */
+#define ICE_RX_MAX_DATA_BUF_SIZE	(16 * 1024 - 128)
+
 typedef void (*ice_rx_release_mbufs_t)(struct ice_rx_queue *rxq);
 typedef void (*ice_tx_release_mbufs_t)(struct ice_tx_queue *txq);
 typedef void (*ice_rxd_to_pkt_fields_t)(struct ice_rx_queue *rxq,
