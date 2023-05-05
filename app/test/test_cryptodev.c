0428cfa1894ef3463cf16baf3e1b0b3b958250dc
Tejasree Kondoj <ktejasree@marvell.com>
2023-05-05 22:23:48 +0530

    test/crypto: fix session creation check
    [ upstream commit 142a8f4c578e420b916f84649873f99bad985412 ]
    
    Fixing return value if session failure occurs
    due to unsupported feature.
    
    Fixes: eec136f3c54f ("aesni_gcm: add driver for AES-GCM crypto operations")
    
    Signed-off-by: Tejasree Kondoj <ktejasree@marvell.com>
    Acked-by: Anoob Joseph <anoobj@marvell.com>
    Acked-by: Ciara Power <ciara.power@intel.com>

--- app/test/test_cryptodev.c
+++ app/test/test_cryptodev.c
@@ -8256,7 +8256,7 @@ test_authenticated_encryption(const struct aead_test_data *tdata)
 			tdata->key.data, tdata->key.len,
 			tdata->aad.len, tdata->auth_tag.len,
 			tdata->iv.len);
-	if (retval < 0)
+	if (retval != TEST_SUCCESS)
 		return retval;
 
 	if (tdata->aad.len > MBUF_SIZE) {
@@ -10904,7 +10904,7 @@ test_authenticated_decryption_sessionless(
 			key, tdata->key.len,
 			tdata->aad.len, tdata->auth_tag.len,
 			tdata->iv.len);
-	if (retval < 0)
+	if (retval != TEST_SUCCESS)
 		return retval;
 
 	ut_params->op->sym->m_src = ut_params->ibuf;
