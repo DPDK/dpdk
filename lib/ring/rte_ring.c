ce4bd6e14aa693c80c5218f7d896c98b3b85b54a
Yunjian Wang <wangyunjian@huawei.com>
2023-05-05 14:48:34 +0800

    ring: fix use after free
    After the memzone is freed, it is not removed from the 'rte_ring_tailq'.
    If rte_ring_lookup is called at this time, it will cause a use-after-free
    problem. This change prevents that from happening.
    
    Fixes: 4e32101f9b01 ("ring: support freeing")
    Cc: stable@dpdk.org
    
    Signed-off-by: Yunjian Wang <wangyunjian@huawei.com>
    Acked-by: Konstantin Ananyev <konstantin.v.ananyev@yandex.ru>
    Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>

--- lib/ring/rte_ring.c
+++ lib/ring/rte_ring.c
@@ -333,11 +333,6 @@ rte_ring_free(struct rte_ring *r)
 		return;
 	}
 
-	if (rte_memzone_free(r->memzone) != 0) {
-		RTE_LOG(ERR, RING, "Cannot free memory\n");
-		return;
-	}
-
 	ring_list = RTE_TAILQ_CAST(rte_ring_tailq.head, rte_ring_list);
 	rte_mcfg_tailq_write_lock();
 
@@ -355,6 +350,9 @@ rte_ring_free(struct rte_ring *r)
 	TAILQ_REMOVE(ring_list, te, next);
 
 	rte_mcfg_tailq_write_unlock();
+
+	if (rte_memzone_free(r->memzone) != 0)
+		RTE_LOG(ERR, RING, "Cannot free memory\n");
 
 	rte_free(te);
 }
