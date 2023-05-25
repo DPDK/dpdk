8f162d697ca5dfea1e35d15382a7148a89aca8ce
Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
2023-05-25 15:28:45 +0530

    common/cnxk: fix parameter in NIX dump
    Fix parameter passed to nix_dump to what expected in format
    specifier.
    
    Fixes: d2f168dfa5de ("common/cnxk: support 10K B0 for inline IPsec")
    Cc: stable@dpdk.org
    
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>

--- drivers/common/cnxk/roc_nix_debug.c
+++ drivers/common/cnxk/roc_nix_debug.c
@@ -664,8 +664,8 @@ nix_lf_cq_dump(__io struct nix_cq_ctx_s *ctx, FILE *file)
 	nix_dump(file,
 		 "W1: lbpid_high \t\t\t0x%03x\nW1: lbpid_med \t\t\t0x%03x\n"
 		 "W1: lbpid_low \t\t\t0x%03x\n(W1: lbpid) \t\t\t0x%03x\n",
-		 ctx->lbpid_high, ctx->lbpid_med, ctx->lbpid_low,
-		 ctx->lbpid_high << 6 | ctx->lbpid_med << 3 | ctx->lbpid_low);
+		 ctx->lbpid_high, ctx->lbpid_med, ctx->lbpid_low, (unsigned int)
+		 (ctx->lbpid_high << 6 | ctx->lbpid_med << 3 | ctx->lbpid_low));
 	nix_dump(file, "W1: lbp_ena \t\t\t\t%d\n", ctx->lbp_ena);
 
 	nix_dump(file, "W2: update_time \t\t%d\nW2: avg_level \t\t\t%d",
