d318e11099bb992c47d22c0c3d13f55dc868c3a2
Qin Ke <qin.ke@corigine.com>
2023-06-08 11:07:56 +0800

    net/nfp: fix disabling promiscuous mode
    The callback function of promiscuous_disable in
    nfp_flower_pf_repr_dev_ops and nfp_flower_repr_dev_ops should be
    nfp_net_promisc_disable, not nfp_net_promisc_enable, fix it.
    
    Fixes: 39d82d209805 ("net/nfp: fix promiscuous mode for representor port")
    Cc: stable@dpdk.org
    
    Signed-off-by: Qin Ke <qin.ke@corigine.com>
    Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

--- drivers/net/nfp/flower/nfp_flower_representor.c
+++ drivers/net/nfp/flower/nfp_flower_representor.c
@@ -518,7 +518,7 @@ static const struct eth_dev_ops nfp_flower_pf_repr_dev_ops = {
 	.stats_reset          = nfp_flower_repr_stats_reset,
 
 	.promiscuous_enable   = nfp_net_promisc_enable,
-	.promiscuous_disable  = nfp_net_promisc_enable,
+	.promiscuous_disable  = nfp_net_promisc_disable,
 
 	.mac_addr_set         = nfp_flower_repr_mac_addr_set,
 	.fw_version_get       = nfp_repr_firmware_version_get,
@@ -540,7 +540,7 @@ static const struct eth_dev_ops nfp_flower_repr_dev_ops = {
 	.stats_reset          = nfp_flower_repr_stats_reset,
 
 	.promiscuous_enable   = nfp_net_promisc_enable,
-	.promiscuous_disable  = nfp_net_promisc_enable,
+	.promiscuous_disable  = nfp_net_promisc_disable,
 
 	.mac_addr_set         = nfp_flower_repr_mac_addr_set,
 	.fw_version_get       = nfp_repr_firmware_version_get,
