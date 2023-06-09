16a82d0d1a7bcea2a3bd5d51e6a8700702571b8d
Jie Hai <haijie1@huawei.com>
2023-06-09 17:03:40 +0800

    app/testpmd: fix primary process not polling all queues
    [ upstream commit 141a520b35f789f37dc06557bc724bf7f0e0a52d ]
    
    Here's how the problem arises.
    step1: Start the app.
        dpdk-testpmd -a 0000:35:00.0 -l 0-3 -- -i --rxq=10 --txq=10
    
    step2: Perform the following steps and send traffic. As expected,
    queue 7 does not send or receive packets, and other queues do.
        port 0 rxq 7 stop
        port 0 txq 7 stop
        set fwd mac
        start
    
    step3: Perform the following steps and send traffic. All queues
    are expected to send and receive packets normally, but that's not
    the case for queue 7.
        stop
        port stop all
        port start all
        start
        show port xstats all
    
    In fact, only the value of rx_q7_packets for queue 7 is not zero,
    which means queue 7 is enabled for the driver but is not involved
    in packet receiving and forwarding by software. If we check queue
    state by command 'show rxq info 0 7' and 'show txq info 0 7',
    we see queue 7 is started as other queues are.
        Rx queue state: started
        Tx queue state: started
    The queue 7 is started but cannot forward. That's the problem.
    
    We know that each stream has a read-only "disabled" field that
    control if this stream should be used to forward. This field
    depends on testpmd local queue state, please see
    commit 3c4426db54fc ("app/testpmd: do not poll stopped queues").
    DPDK framework maintains ethdev queue state that drivers reported,
    which indicates the real state of queues.
    
    There are commands that update these two kind queue state such as
    'port X rxq|txq start|stop'. But these operations take effect only
    in one stop-start round. In the following stop-start round, the
    preceding operations do not take effect anymore. However, only
    the ethdev queue state is updated, causing the testpmd and ethdev
    state information to diverge and causing unexpected side effects
    as above problem.
    
    There was a similar problem for the secondary process, please see
    commit 5028f207a4fa ("app/testpmd: fix secondary process packet
    forwarding").
    
    This patch applies its workaround with some difference to the
    primary process. Not all PMDs implement rte_eth_rx_queue_info_get and
    rte_eth_tx_queue_info_get, however they may support deferred_start
    with primary process. To not break their behavior, retain the original
    testpmd local queue state for those PMDs.
    
    Fixes: 3c4426db54fc ("app/testpmd: do not poll stopped queues")
    
    Signed-off-by: Jie Hai <haijie1@huawei.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@amd.com>

--- app/test-pmd/testpmd.c
+++ app/test-pmd/testpmd.c
@@ -2356,6 +2356,13 @@ update_rx_queue_state(uint16_t port_id, uint16_t queue_id)
 			rx_qinfo.queue_state;
 	} else if (rc == -ENOTSUP) {
 		/*
+		 * Do not change the rxq state for primary process
+		 * to ensure that the PMDs do not implement
+		 * rte_eth_rx_queue_info_get can forward as before.
+		 */
+		if (rte_eal_process_type() == RTE_PROC_PRIMARY)
+			return;
+		/*
 		 * Set the rxq state to RTE_ETH_QUEUE_STATE_STARTED
 		 * to ensure that the PMDs do not implement
 		 * rte_eth_rx_queue_info_get can forward.
@@ -2380,6 +2387,13 @@ update_tx_queue_state(uint16_t port_id, uint16_t queue_id)
 		ports[port_id].txq[queue_id].state =
 			tx_qinfo.queue_state;
 	} else if (rc == -ENOTSUP) {
+		/*
+		 * Do not change the txq state for primary process
+		 * to ensure that the PMDs do not implement
+		 * rte_eth_tx_queue_info_get can forward as before.
+		 */
+		if (rte_eal_process_type() == RTE_PROC_PRIMARY)
+			return;
 		/*
 		 * Set the txq state to RTE_ETH_QUEUE_STATE_STARTED
 		 * to ensure that the PMDs do not implement
@@ -2447,8 +2461,7 @@ start_packet_forwarding(int with_tx_first)
 		return;
 
 	if (stream_init != NULL) {
-		if (rte_eal_process_type() == RTE_PROC_SECONDARY)
-			update_queue_state();
+		update_queue_state();
 		for (i = 0; i < cur_fwd_config.nb_fwd_streams; i++)
 			stream_init(fwd_streams[i]);
 	}
@@ -3211,8 +3224,7 @@ start_port(portid_t pid)
 		pl[cfg_pi++] = pi;
 	}
 
-	if (rte_eal_process_type() == RTE_PROC_SECONDARY)
-		update_queue_state();
+	update_queue_state();
 
 	if (at_least_one_port_successfully_started && !no_link_check)
 		check_all_ports_link_status(RTE_PORT_ALL);
