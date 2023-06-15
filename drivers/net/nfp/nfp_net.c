63b9491ae9dd749345dfafc85da0b2a89eb322db
Chaoyong He <chaoyong.he@corigine.com>
2023-06-15 14:21:34 +0800

    net/nfp: fix Tx descriptor free logic of NFD3
    [ upstream commit 36d0f4238b2f79850695f49b191ddd4540ff88a6 ]
    
    In the Tx descriptor free logic of nfd3, the former logic might force
    cast a negative number into a very big unsigned number, and which will
    cause potential problem in the xmit loop.
    
    The xmit loop will continue in the place where it should break, and will
    overwrite the Tx descriptor which is not free to use by the PMD.
    
    Fixes: 74a640dac864 ("net/nfp: avoid modulo operations for handling ring wrapping")
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

--- drivers/net/nfp/nfp_net.c
+++ drivers/net/nfp/nfp_net.c
@@ -2238,10 +2238,14 @@ nfp_net_tx_free_bufs(struct nfp_net_txq *txq)
 static inline
 uint32_t nfp_free_tx_desc(struct nfp_net_txq *txq)
 {
+	uint32_t free_desc;
+
 	if (txq->wr_p >= txq->rd_p)
-		return txq->tx_count - (txq->wr_p - txq->rd_p) - 8;
+		free_desc = txq->tx_count - (txq->wr_p - txq->rd_p);
 	else
-		return txq->rd_p - txq->wr_p - 8;
+		free_desc = txq->rd_p - txq->wr_p;
+
+	return (free_desc > 8) ? (free_desc - 8) : 0;
 }
 
 /*
