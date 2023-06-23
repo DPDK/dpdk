2e8f88824d9023004434c24258faf57496a76ed0
Ciara Power <ciara.power@intel.com>
2023-06-23 13:51:04 +0000

    crypto/qat: fix null algorithm digest placement
    [ upstream commit 8b4618a7b453dec02ca8ea40871fb124100f98f4 ]
    
    QAT HW generates bytes of 0x00 digest, even when a digest of len 0 is
    requested for NULL. This caused test failures when the test vector had
    digest len 0, as the buffer has unexpected changed bytes.
    
    By placing the digest into the cookie for NULL authentication,
    the buffer remains unchanged as expected, and the digest
    is placed to the side, as it won't be used anyway.
    
    Fixes: db0e952a5c01 ("crypto/qat: add NULL capability")
    
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Acked-by: Arkadiusz Kusztal <arkadiuszx.kusztal@intel.com>

--- drivers/crypto/qat/dev/qat_crypto_pmd_gen3.c
+++ drivers/crypto/qat/dev/qat_crypto_pmd_gen3.c
@@ -407,7 +407,7 @@ qat_sym_build_op_auth_gen3(void *in_op, struct qat_sym_session *ctx,
 	rte_mov128((uint8_t *)req, (const uint8_t *)&(ctx->fw_req));
 
 	ofs.raw = qat_sym_convert_op_to_vec_auth(op, ctx, &in_sgl, &out_sgl,
-			NULL, &auth_iv, &digest);
+			NULL, &auth_iv, &digest, op_cookie);
 	if (unlikely(ofs.raw == UINT64_MAX)) {
 		op->status = RTE_CRYPTO_OP_STATUS_INVALID_ARGS;
 		return -EINVAL;
