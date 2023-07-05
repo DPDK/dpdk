dd0132e908acb8976c976ad0c7f7cd14a0ada2f3
Ciara Power <ciara.power@intel.com>
2023-07-05 13:23:11 +0000

    crypto/qat: fix legacy SM4 ECB capability
    Following the deprecation of insecure algorithms in QAT,
    SM4-ECB should be included as legacy, to be disabled by default.
    
    Fixes: cffb726b7797 ("crypto/qat: enable insecure algorithms")
    
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Acked-by: Kai Ji <kai.ji@intel.com>
    Tested-by: Saoirse O'Donovan <saoirse.odonovan@intel.com>

--- drivers/crypto/qat/dev/qat_crypto_pmd_gen3.c
+++ drivers/crypto/qat/dev/qat_crypto_pmd_gen3.c
@@ -46,6 +46,9 @@ static struct rte_cryptodev_capabilities qat_sym_crypto_legacy_caps_gen3[] = {
 	QAT_SYM_PLAIN_AUTH_CAP(SHA3_224,
 		CAP_SET(block_size, 144),
 		CAP_RNG(digest_size, 28, 28, 0)),
+	QAT_SYM_CIPHER_CAP(SM4_ECB,
+		CAP_SET(block_size, 16),
+		CAP_RNG(key_size, 16, 16, 0), CAP_RNG(iv_size, 0, 0, 0))
 };
 
 static struct rte_cryptodev_capabilities qat_sym_crypto_caps_gen3[] = {
@@ -147,9 +150,6 @@ static struct rte_cryptodev_capabilities qat_sym_crypto_caps_gen3[] = {
 		CAP_RNG(key_size, 32, 32, 0),
 		CAP_RNG(digest_size, 16, 16, 0),
 		CAP_RNG(aad_size, 0, 240, 1), CAP_RNG(iv_size, 12, 12, 0)),
-	QAT_SYM_CIPHER_CAP(SM4_ECB,
-		CAP_SET(block_size, 16),
-		CAP_RNG(key_size, 16, 16, 0), CAP_RNG(iv_size, 0, 0, 0)),
 	QAT_SYM_CIPHER_CAP(SM4_CBC,
 		CAP_SET(block_size, 16),
 		CAP_RNG(key_size, 16, 16, 0), CAP_RNG(iv_size, 16, 16, 0)),
