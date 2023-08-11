60943c04f3bcd0ed32a9844993d1182c917b978b
Tyler Retzlaff <roretzla@linux.microsoft.com>
2023-08-11 12:20:47 -0700

    eal/x86: use intrinsics for power management
    Inline assembly is not supported for MSVC x64 instead use _umonitor,
    _umwait and _tpause intrinsics.
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Acked-by: Morten Brørup <mb@smartsharesystems.com>
    Acked-by: Konstantin Ananyev <konstantin.v.ananyev@yandex.ru>

--- lib/eal/x86/rte_power_intrinsics.c
+++ lib/eal/x86/rte_power_intrinsics.c
@@ -109,9 +109,13 @@ rte_power_monitor(const struct rte_power_monitor_cond *pmc,
 	 */
 
 	/* set address for UMONITOR */
+#if defined(RTE_TOOLCHAIN_MSVC) || defined(__WAITPKG__)
+	_umonitor(pmc->addr);
+#else
 	asm volatile(".byte 0xf3, 0x0f, 0xae, 0xf7;"
 			:
 			: "D"(pmc->addr));
+#endif
 
 	/* now that we've put this address into monitor, we can unlock */
 	rte_spinlock_unlock(&s->lock);
@@ -123,10 +127,14 @@ rte_power_monitor(const struct rte_power_monitor_cond *pmc,
 		goto end;
 
 	/* execute UMWAIT */
+#if defined(RTE_TOOLCHAIN_MSVC) || defined(__WAITPKG__)
+	_umwait(tsc_l, tsc_h);
+#else
 	asm volatile(".byte 0xf2, 0x0f, 0xae, 0xf7;"
 			: /* ignore rflags */
 			: "D"(0), /* enter C0.2 */
 			  "a"(tsc_l), "d"(tsc_h));
+#endif
 
 end:
 	/* erase sleep address */
@@ -153,10 +161,14 @@ rte_power_pause(const uint64_t tsc_timestamp)
 		return -ENOTSUP;
 
 	/* execute TPAUSE */
+#if defined(RTE_TOOLCHAIN_MSVC) || defined(__WAITPKG__)
+	_tpause(tsc_l, tsc_h);
+#else
 	asm volatile(".byte 0x66, 0x0f, 0xae, 0xf7;"
 			: /* ignore rflags */
 			: "D"(0), /* enter C0.2 */
 			"a"(tsc_l), "d"(tsc_h));
+#endif
 
 	return 0;
 }
