31f6839de6ff355df3bf30dee8efaa8b672e62f1
Sivaramakrishnan Venkat <venkatx.sivaramakrishnan@intel.com>
2023-08-18 09:52:47 +0000

    test/crypto: fix typo in asym tests
    [ upstream commit ea7b0e1a17717872a1d7d74b075a77fdb9414fa4 ]
    
    test_dh_keygenration() changed to test_dh_key_generation()
    
    Fixes: fc6c9066e40d ("test/crypto: add DH and DSA tests")
    Fixes: da74df7d38c0 ("test/crypto: move test suite parameters to header file")
    
    Signed-off-by: Sivaramakrishnan Venkat <venkatx.sivaramakrishnan@intel.com>
    Acked-by: Ciara Power <ciara.power@intel.com>

--- app/test/test_cryptodev_asym.c
+++ app/test/test_cryptodev_asym.c
@@ -1602,7 +1602,7 @@ error_exit:
 }
 
 static int
-test_dh_keygenration(void)
+test_dh_key_generation(void)
 {
 	int status;
 
@@ -2204,7 +2204,7 @@ static struct unit_test_suite cryptodev_openssl_asym_testsuite  = {
 		TEST_CASE_ST(ut_setup_asym, ut_teardown_asym, test_capability),
 		TEST_CASE_ST(ut_setup_asym, ut_teardown_asym, test_dsa),
 		TEST_CASE_ST(ut_setup_asym, ut_teardown_asym,
-				test_dh_keygenration),
+				test_dh_key_generation),
 		TEST_CASE_ST(ut_setup_asym, ut_teardown_asym, test_rsa_enc_dec),
 		TEST_CASE_ST(ut_setup_asym, ut_teardown_asym,
 				test_rsa_sign_verify),
