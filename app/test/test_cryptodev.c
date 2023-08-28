6660c7581990c2f1564a1b23a31d301f2135eb72
Hemant Agrawal <hemant.agrawal@nxp.com>
2023-08-28 12:21:07 +0530

    test/crypto: fix return value for GMAC case
    create_gmac_session is already converting the return value
    of ENOTSUP to TEST_SKIPPED.
    
    Fixes: bdce2564dbf7 ("cryptodev: rework session framework")
    Cc: stable@dpdk.org
    
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>
    Acked-by: Akhil Goyal <gakhil@marvell.com>

--- app/test/test_cryptodev.c
+++ app/test/test_cryptodev.c
@@ -13544,7 +13544,7 @@ test_AES_GMAC_authentication(const struct gmac_test_data *tdata)
 	retval = create_gmac_session(ts_params->valid_devs[0],
 			tdata, RTE_CRYPTO_AUTH_OP_GENERATE);
 
-	if (retval == -ENOTSUP)
+	if (retval == TEST_SKIPPED)
 		return TEST_SKIPPED;
 	if (retval < 0)
 		return retval;
@@ -13677,7 +13677,7 @@ test_AES_GMAC_authentication_verify(const struct gmac_test_data *tdata)
 	retval = create_gmac_session(ts_params->valid_devs[0],
 			tdata, RTE_CRYPTO_AUTH_OP_VERIFY);
 
-	if (retval == -ENOTSUP)
+	if (retval == TEST_SKIPPED)
 		return TEST_SKIPPED;
 	if (retval < 0)
 		return retval;
@@ -13808,7 +13808,7 @@ test_AES_GMAC_authentication_SGL(const struct gmac_test_data *tdata,
 	retval = create_gmac_session(ts_params->valid_devs[0],
 			tdata, RTE_CRYPTO_AUTH_OP_GENERATE);
 
-	if (retval == -ENOTSUP)
+	if (retval == TEST_SKIPPED)
 		return TEST_SKIPPED;
 	if (retval < 0)
 		return retval;
@@ -14425,7 +14425,7 @@ test_authentication_verify_fail_when_data_corruption(
 			reference,
 			RTE_CRYPTO_AUTH_OP_VERIFY);
 
-	if (retval == -ENOTSUP)
+	if (retval == TEST_SKIPPED)
 		return TEST_SKIPPED;
 	if (retval < 0)
 		return retval;
@@ -14514,6 +14514,8 @@ test_authentication_verify_GMAC_fail_when_corruption(
 			reference,
 			RTE_CRYPTO_AUTH_OP_VERIFY,
 			RTE_CRYPTO_CIPHER_OP_DECRYPT);
+	if (retval == TEST_SKIPPED)
+		return TEST_SKIPPED;
 	if (retval < 0)
 		return retval;
 
@@ -14606,8 +14608,7 @@ test_authenticated_decryption_fail_when_corruption(
 			reference,
 			RTE_CRYPTO_AUTH_OP_VERIFY,
 			RTE_CRYPTO_CIPHER_OP_DECRYPT);
-
-	if (retval == -ENOTSUP)
+	if (retval == TEST_SKIPPED)
 		return TEST_SKIPPED;
 	if (retval < 0)
 		return retval;
