28ddcaf4a6195f728cb5ead6a8e529f82dd525e6
Kaiwen Deng <kaiwenx.deng@intel.com>
2023-09-06 15:49:30 +0800

    net/ice: fix TM configuration clearing
    [ upstream commit c21a6a3ff6c49965d4373677255058d041dafe96 ]
    
    When the device is stopped, the PMD resets the commit flag so that
    we can update the hierarchy configuration. The commit flag is also
    used to determine if the hierarchy configuration needs to be cleared.
    When the PMD exits, it always stops the device first and also resets
    the commit flag result in the hierarchy configuration is not cleared.
    
    This commit changes the PMD to not reset the commit flag when the
    device is stopped. And we prevent additional commit when device is
    running by only checking the stop flag.
    
    Fixes: f5ec6a3a1987 ("net/ice: fix TM hierarchy commit flag reset")
    
    Signed-off-by: Kaiwen Deng <kaiwenx.deng@intel.com>
    Acked-by: Wenjun Wu <wenjun1.wu@intel.com>

--- drivers/net/ice/ice_dcf_ethdev.c
+++ drivers/net/ice/ice_dcf_ethdev.c
@@ -611,7 +611,6 @@ ice_dcf_dev_stop(struct rte_eth_dev *dev)
 	struct ice_dcf_adapter *dcf_ad = dev->data->dev_private;
 	struct rte_intr_handle *intr_handle = dev->intr_handle;
 	struct ice_adapter *ad = &dcf_ad->parent;
-	struct ice_dcf_hw *hw = &dcf_ad->real_hw;
 
 	if (ad->pf.adapter_stopped == 1) {
 		PMD_DRV_LOG(DEBUG, "Port is already stopped");
@@ -629,7 +628,6 @@ ice_dcf_dev_stop(struct rte_eth_dev *dev)
 	ice_dcf_add_del_all_mac_addr(&dcf_ad->real_hw, false);
 	dev->data->dev_link.link_status = RTE_ETH_LINK_DOWN;
 	ad->pf.adapter_stopped = 1;
-	hw->tm_conf.committed = false;
 
 	return 0;
 }
