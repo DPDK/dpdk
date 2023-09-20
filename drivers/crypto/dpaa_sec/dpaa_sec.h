01e378c4113bd91e38970e1e6d8e92bf0b7cfdff
Hemant Agrawal <hemant.agrawal@nxp.com>
2023-09-20 19:03:55 +0530

    crypto/dpaa_sec: support null cipher and auth
    Added NULL cipher and auth in capabilities.
    
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- drivers/crypto/dpaa_sec/dpaa_sec.h
+++ drivers/crypto/dpaa_sec/dpaa_sec.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  *
- *   Copyright 2016-2022 NXP
+ *   Copyright 2016-2023 NXP
  *
  */
 
@@ -780,6 +780,46 @@ static const struct rte_cryptodev_capabilities dpaa_sec_capabilities[] = {
 				.aad_size = { 0 },
 				.iv_size = { 0 }
 			}, }
+		}, }
+	},
+	{	/* NULL (AUTH) */
+		.op = RTE_CRYPTO_OP_TYPE_SYMMETRIC,
+		{.sym = {
+			.xform_type = RTE_CRYPTO_SYM_XFORM_AUTH,
+			{.auth = {
+				.algo = RTE_CRYPTO_AUTH_NULL,
+				.block_size = 1,
+				.key_size = {
+					.min = 0,
+					.max = 0,
+					.increment = 0
+				},
+				.digest_size = {
+					.min = 0,
+					.max = 0,
+					.increment = 0
+				},
+			}, },
+		}, },
+	},
+	{	/* NULL (CIPHER) */
+		.op = RTE_CRYPTO_OP_TYPE_SYMMETRIC,
+		{.sym = {
+			.xform_type = RTE_CRYPTO_SYM_XFORM_CIPHER,
+			{.cipher = {
+				.algo = RTE_CRYPTO_CIPHER_NULL,
+				.block_size = 1,
+				.key_size = {
+					.min = 0,
+					.max = 0,
+					.increment = 0
+				},
+				.iv_size = {
+					.min = 0,
+					.max = 0,
+					.increment = 0
+				}
+			}, },
 		}, }
 	},
 	RTE_CRYPTODEV_END_OF_CAPABILITIES_LIST()
