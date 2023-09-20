c9486280bc1ed6d4678c549de3f5603c3c3c01a7
Chaoyong He <chaoyong.he@corigine.com>
2023-09-20 19:34:49 +0800

    net/nfp: improve modularization of flower ctrl module
    Make the header file self-containing by adding the correct include
    statement.
    Also remove the unneeded header file include statement of source file.
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>

--- drivers/net/nfp/flower/nfp_flower_ctrl.h
+++ drivers/net/nfp/flower/nfp_flower_ctrl.h
@@ -6,6 +6,8 @@
 #ifndef _NFP_FLOWER_CTRL_H_
 #define _NFP_FLOWER_CTRL_H_
 
+#include "nfp_flower.h"
+
 void nfp_flower_ctrl_vnic_poll(struct nfp_app_fw_flower *app_fw_flower);
 uint16_t nfp_flower_ctrl_vnic_xmit(struct nfp_app_fw_flower *app_fw_flower,
 		struct rte_mbuf *mbuf);
