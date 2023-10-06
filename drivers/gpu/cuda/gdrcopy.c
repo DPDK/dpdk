DPDK/dpdk

commit ca12f5e8a7db9b08913f235ba8b8381248bf4970

gpu/cuda: mark unused GDRCopy functions parameters
Without enabling gdrcopy, DPDK build has warnings about
unused gdrcopy functions input parameters.

This patch set them to __rte_unused .

Fixes: 24c7759 ("gpu/cuda: map GPU memory with GDRCopy")

Signed-off-by: Elena Agostini <eagostini@nvidia.com>

drivers/gpu/cuda/gdrcopy.c
@@ -80,7 +80,9 @@ gdrcopy_open(gdr_t *g)
 #endif
 
 int
-gdrcopy_pin(gdr_t *gdrc_h, __rte_unused gdr_mh_t *mh, uint64_t d_addr, size_t size, void **h_addr)
+gdrcopy_pin(__rte_unused gdr_t *gdrc_h, __rte_unused gdr_mh_t *mh,
+		__rte_unused uint64_t d_addr, __rte_unused size_t size,
+		__rte_unused void **h_addr)
 {
 #ifdef DRIVERS_GPU_CUDA_GDRCOPY_H
 	if (*gdrc_h == NULL) {
@@ -116,7 +118,8 @@ gdrcopy_pin(gdr_t *gdrc_h, __rte_unused gdr_mh_t *mh, uint64_t d_addr, size_t si
 }
 
 int
-gdrcopy_unpin(gdr_t gdrc_h, __rte_unused gdr_mh_t mh, void *d_addr, size_t size)
+gdrcopy_unpin(gdr_t gdrc_h, __rte_unused gdr_mh_t mh,
+		__rte_unused void *d_addr, __rte_unused size_t size)
 {
 	if (gdrc_h == NULL)
 		return -EINVAL;

commit cda3d17609c2cb81af67d5babbd0c1702b503fca

gpu/cuda: fix build with external GDRCopy
If gdrapi.h is available, meson sets DRIVERS_GPU_CUDA_GDRCOPY_H as 1.
This causes gdrcopy.c build to give an error;
because compiler can not find signature of getenv.
stdlib.h is included for the definition of getenv function.

Bugzilla ID: 1133
Fixes: ca12f5e8a7db ("gpu/cuda: mark unused GDRCopy functions parameters")
Cc: stable@dpdk.org

Reported-by: John Romein <romein@astron.nl>
Signed-off-by: Levend Sayar <levendsayar@gmail.com>
Acked-by: Elena Agostini <eagostini@nvidia.com>

.mailmap
@@ -664,6 +664,7 @@ John McNamara <john.mcnamara@intel.com>
 John Miller <john.miller@atomicrules.com>
 John OLoughlin <john.oloughlin@intel.com>
 John Ousterhout <ouster@cs.stanford.edu>
+John Romein <romein@astron.nl>
 John W. Linville <linville@tuxdriver.com>
 Jonas Pfefferle <jpf@zurich.ibm.com> <pepperjo@japf.ch>
 Jonathan Erb <jonathan.erb@banduracyber.com>

drivers/gpu/cuda/gdrcopy.c
@@ -6,6 +6,8 @@
 
 #ifdef DRIVERS_GPU_CUDA_GDRCOPY_H
 
+#include <stdlib.h>
+
 static void *gdrclib;
 static gdr_t (*sym_gdr_open)(void);
 static int (*sym_gdr_pin_buffer)(gdr_t g, unsigned long addr, size_t size,