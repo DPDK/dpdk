615bd385b93e89e062b4e134e9cc09a79289a0f6
David Marchand <david.marchand@redhat.com>
2023-10-16 10:47:43 +0200

    net/bonding: fix link status callback stop
    If a bonding port gets released, a link status alarm callback still
    referenced the ethdev port that may be reused later.
    Cancel this callback when stopping the port.
    
    Bugzilla ID: 1301
    Fixes: a45b288ef21a ("bond: support link status polling")
    Cc: stable@dpdk.org
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Chengwen Feng <fengchengwen@huawei.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@amd.com>

--- drivers/net/bonding/rte_eth_bond_pmd.c
+++ drivers/net/bonding/rte_eth_bond_pmd.c
@@ -2164,6 +2164,10 @@ bond_ethdev_stop(struct rte_eth_dev *eth_dev)
 	eth_dev->data->dev_link.link_status = RTE_ETH_LINK_DOWN;
 	eth_dev->data->dev_started = 0;
 
+	if (internals->link_status_polling_enabled) {
+		rte_eal_alarm_cancel(bond_ethdev_member_link_status_change_monitor,
+			(void *)&rte_eth_devices[internals->port_id]);
+	}
 	internals->link_status_polling_enabled = 0;
 	for (i = 0; i < internals->member_count; i++) {
 		uint16_t member_id = internals->members[i].port_id;
