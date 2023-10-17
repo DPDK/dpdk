2cd2cf0cc53a508c4bd951a980ec534a646260de
David Marchand <david.marchand@redhat.com>
2023-10-17 13:31:25 +0200

    devtools: extend check on compiler builtin atomics
    rte_memory_order_* should be used when calling the new stdatomic API.
    Add a check on __ATOMIC_* tokens.
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Tyler Retzlaff <roretzla@linux.microsoft.com>

--- devtools/checkpatches.sh
+++ devtools/checkpatches.sh
@@ -113,9 +113,9 @@ check_forbidden_additions() { # <patch>
 
 	# refrain from using compiler __atomic_xxx builtins
 	awk -v FOLDERS="lib drivers app examples" \
-		-v EXPRESSIONS="__atomic_.*\\\(" \
+		-v EXPRESSIONS="__atomic_.*\\\( __ATOMIC_(RELAXED|CONSUME|ACQUIRE|RELEASE|ACQ_REL|SEQ_CST)" \
 		-v RET_ON_FAIL=1 \
-		-v MESSAGE='Using __atomic_xxx built-ins, prefer rte_atomic_xxx' \
+		-v MESSAGE='Using __atomic_xxx/__ATOMIC_XXX built-ins, prefer rte_atomic_xxx/rte_memory_order_xxx' \
 		-f $(dirname $(readlink -f $0))/check-forbidden-tokens.awk \
 		"$1" || res=1
 
