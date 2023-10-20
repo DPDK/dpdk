b4c4e5675c85a9b471c252305811d15f6a6d2aa6
Maxime Coquelin <maxime.coquelin@redhat.com>
2023-10-20 10:48:03 +0200

    vhost: fix missing lock protection in power monitor API
    The power monitor get API is missing both access lock
    protection and access status check.
    
    Fixes: 34fd4373ce76 ("vhost: add power monitor API")
    Cc: stable@dpdk.org
    
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Acked-by: David Marchand <david.marchand@redhat.com>

--- lib/vhost/vhost.c
+++ lib/vhost/vhost.c
@@ -2123,6 +2123,7 @@ rte_vhost_get_monitor_addr(int vid, uint16_t queue_id,
 {
 	struct virtio_net *dev = get_device(vid);
 	struct vhost_virtqueue *vq;
+	int ret = 0;
 
 	if (dev == NULL)
 		return -1;
@@ -2132,6 +2133,13 @@ rte_vhost_get_monitor_addr(int vid, uint16_t queue_id,
 	vq = dev->virtqueue[queue_id];
 	if (vq == NULL)
 		return -1;
+
+	rte_rwlock_read_lock(&vq->access_lock);
+
+	if (unlikely(!vq->access_ok)) {
+		ret = -1;
+		goto out_unlock;
+	}
 
 	if (vq_is_packed(dev)) {
 		struct vring_packed_desc *desc;
@@ -2152,7 +2160,10 @@ rte_vhost_get_monitor_addr(int vid, uint16_t queue_id,
 		pmc->match = 0;
 	}
 
-	return 0;
+out_unlock:
+	rte_rwlock_read_unlock(&vq->access_lock);
+
+	return ret;
 }
 
 
