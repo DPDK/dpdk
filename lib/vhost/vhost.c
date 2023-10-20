587aea0f91f7170f6812d248915f0c60aed3057d
Maxime Coquelin <maxime.coquelin@redhat.com>
2023-10-20 10:48:02 +0200

    vhost: fix check on virtqueue access in in-flight getter
    [ upstream commit 288cd1f8ca3ebaf20aeed92966da9020a5793852 ]
    
    Acquiring the access lock is not enough to ensure
    virtqueue's metadata such as vring pointers are valid.
    
    The access status must also be checked.
    
    Fixes: 0c0935c5f794 ("vhost: allow to check in-flight packets for async vhost")
    
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Acked-by: David Marchand <david.marchand@redhat.com>

--- lib/vhost/vhost.c
+++ lib/vhost/vhost.c
@@ -1929,9 +1929,15 @@ rte_vhost_async_get_inflight(int vid, uint16_t queue_id)
 		return ret;
 	}
 
+	if (unlikely(!vq->access_ok)) {
+		ret = -1;
+		goto out_unlock;
+	}
+
 	if (vq->async)
 		ret = vq->async->pkts_inflight_n;
 
+out_unlock:
 	rte_spinlock_unlock(&vq->access_lock);
 
 	return ret;
