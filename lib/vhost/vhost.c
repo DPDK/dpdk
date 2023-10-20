fc28b5474d976ef23920b82ead59c717f5496ff3
Maxime Coquelin <maxime.coquelin@redhat.com>
2023-10-20 10:47:58 +0200

    vhost: fix missing vring call check on virtqueue access
    [ upstream commit af7f683615244675fc4f472a2aa42880896476ad ]
    
    Acquiring the access lock is not enough to ensure
    virtqueue's metadata such as vring pointers are valid.
    
    The access status must also be checked.
    
    Fixes: 6c299bb7322f ("vhost: introduce vring call API")
    Fixes: c5736998305d ("vhost: fix missing virtqueue lock protection")
    Fixes: 830f7e790732 ("vhost: add non-blocking API for posting interrupt")
    
    Reported-by: Li Feng <fengli@smartx.com>
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Acked-by: David Marchand <david.marchand@redhat.com>

--- lib/vhost/vhost.c
+++ lib/vhost/vhost.c
@@ -1294,6 +1294,7 @@ rte_vhost_vring_call(int vid, uint16_t vring_idx)
 {
 	struct virtio_net *dev;
 	struct vhost_virtqueue *vq;
+	int ret = 0;
 
 	dev = get_device(vid);
 	if (!dev)
@@ -1308,14 +1309,20 @@ rte_vhost_vring_call(int vid, uint16_t vring_idx)
 
 	rte_spinlock_lock(&vq->access_lock);
 
+	if (unlikely(!vq->access_ok)) {
+		ret = -1;
+		goto out_unlock;
+	}
+
 	if (vq_is_packed(dev))
 		vhost_vring_call_packed(dev, vq);
 	else
 		vhost_vring_call_split(dev, vq);
 
+out_unlock:
 	rte_spinlock_unlock(&vq->access_lock);
 
-	return 0;
+	return ret;
 }
 
 uint16_t
