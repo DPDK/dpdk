9a8d7ab07ff5b41e2b307c5d5383b25d475c8c28
Huisong Li <lihuisong@huawei.com>
2023-11-23 16:05:26 +0800

    net/hns3: extract common function to obtain revision ID
    [ upstream commit f5ed7d99cf45d550a69c1430b7c4a5623a9c774a ]
    
    The code logic of obtaining the revision ID of PCI device is the same
    for PF and VF driver. This patch extracts a common interface to do it.
    
    Signed-off-by: Huisong Li <lihuisong@huawei.com>
    Acked-by: Min Hu (Connor) <humin29@huawei.com>

--- drivers/net/hns3/hns3_common.h
+++ drivers/net/hns3/hns3_common.h
@@ -58,6 +58,7 @@ int hns3_map_rx_interrupt(struct rte_eth_dev *dev);
 void hns3_unmap_rx_interrupt(struct rte_eth_dev *dev);
 int hns3_restore_rx_interrupt(struct hns3_hw *hw);
 
+int hns3_get_pci_revision_id(struct hns3_hw *hw, uint8_t *revision_id);
 void hns3_set_default_dev_specifications(struct hns3_hw *hw);
 int hns3_query_dev_specifications(struct hns3_hw *hw);
 
