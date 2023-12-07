dee031f9dad03af35b672fe4ea321bcc5adf98e3
Anoob Joseph <anoobj@marvell.com>
2023-12-07 18:32:06 +0530

    test/security: add SHA1-HMAC to auth list
    Add SHA1-HMAC with 12B digest to the auth list. This algorithm would be
    used in IPsec tests and combined tests would run on devices that support
    the capabilities.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Signed-off-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Acked-by: Akhil Goyal <gakhil@marvell.com>

--- app/test/test_security_proto.h
+++ app/test/test_security_proto.h
@@ -101,6 +101,12 @@ static const struct crypto_param auth_list[] = {
 	},
 	{
 		.type = RTE_CRYPTO_SYM_XFORM_AUTH,
+		.alg.auth =  RTE_CRYPTO_AUTH_SHA1_HMAC,
+		.key_length = 20,
+		.digest_length = 12,
+	},
+	{
+		.type = RTE_CRYPTO_SYM_XFORM_AUTH,
 		.alg.auth =  RTE_CRYPTO_AUTH_SHA256_HMAC,
 		.key_length = 32,
 		.digest_length = 16,
