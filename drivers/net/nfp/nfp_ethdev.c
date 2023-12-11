08ea9b09e6548f9fdbe1f0549d4c77e403233ad5
Zerun Fu <zerun.fu@corigine.com>
2023-12-11 11:08:56 +0800

    net/nfp: support getting FEC capability
    Add support for querying FEC capabilities from the device. This
    feature only shows the current port speed and the supported FEC
    modes at that speed.
    
    Signed-off-by: Zerun Fu <zerun.fu@corigine.com>
    Reviewed-by: Long Wu <long.wu@corigine.com>
    Reviewed-by: Peng Zhang <peng.zhang@corigine.com>
    Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>

--- drivers/net/nfp/nfp_ethdev.c
+++ drivers/net/nfp/nfp_ethdev.c
@@ -759,6 +759,7 @@ static const struct eth_dev_ops nfp_net_eth_dev_ops = {
 	.flow_ctrl_get          = nfp_net_flow_ctrl_get,
 	.flow_ctrl_set          = nfp_net_flow_ctrl_set,
 	.flow_ops_get           = nfp_net_flow_ops_get,
+	.fec_get_capability     = nfp_net_fec_get_capability,
 };
 
 static inline void
--- drivers/net/nfp/nfp_net_common.h
+++ drivers/net/nfp/nfp_net_common.h
@@ -288,6 +288,9 @@ int nfp_net_flow_ctrl_get(struct rte_eth_dev *dev,
 int nfp_net_flow_ctrl_set(struct rte_eth_dev *dev,
 		struct rte_eth_fc_conf *fc_conf);
 void nfp_pf_uninit(struct nfp_pf_dev *pf_dev);
+int nfp_net_fec_get_capability(struct rte_eth_dev *dev,
+		struct rte_eth_fec_capa *speed_fec_capa,
+		unsigned int num);
 
 #define NFP_PRIV_TO_APP_FW_NIC(app_fw_priv)\
 	((struct nfp_app_fw_nic *)app_fw_priv)
