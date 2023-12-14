48efa16873357fdac7264853cc6423ad1585a790
Qi Zhang <qi.z.zhang@intel.com>
2023-12-14 03:40:54 -0500

    net/ice: fix link update
    [ upstream commit 4e5dc111464e83e9a55fa466d8f682f0027b721e ]
    
    The ice_aq_get_link_info function is not thread-safe. However,
    it is possible to simultaneous invocations during both the dev_start
    and the LSC interrupt handler, potentially leading to unexpected adminq
    errors. This patch addresses the issue by introducing a thread-safe
    wrapper that utilizes a spinlock.
    
    Fixes: cf911d90e366 ("net/ice: support link update")
    
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>
    Acked-by: Qiming Yang <qiming.yang@intel.com>

--- drivers/net/ice/ice_ethdev.h
+++ drivers/net/ice/ice_ethdev.h
@@ -548,6 +548,10 @@ struct ice_pf {
 	uint64_t rss_hf;
 	struct ice_tm_conf tm_conf;
 	uint16_t outer_ethertype;
+	/* lock prevent race condition between lsc interrupt handler
+	 * and link status update during dev_start.
+	 */
+	rte_spinlock_t link_lock;
 };
 
 #define ICE_MAX_QUEUE_NUM  2048
