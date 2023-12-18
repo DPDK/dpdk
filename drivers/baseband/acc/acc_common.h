fc8e14108ff23a306f260726365dd39489134e07
David Marchand <david.marchand@redhat.com>
2023-12-18 16:43:07 +0100

    baseband/acc: fix common logs
    [ upstream commit 6bf0c1c2df217d8ad0c933b728aafa40aff58e1b ]
    
    Logs generated by helpers common to acc100 and vrb drivers were
    emitted with a RTE_LOG_NOTICE == 6 == RTE_LOGTYPE_HASH.
    Register a dedicated logtype for this.
    
    Fixes: 32e8b7ea35dd ("baseband/acc100: refactor to segregate common code")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Stephen Hemminger <stephen@networkplumber.org>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

--- drivers/baseband/acc/acc_common.h
+++ drivers/baseband/acc/acc_common.h
@@ -131,9 +131,11 @@
 #define ACC_LIM_31 20 /* 0.31 */
 #define ACC_MAX_E (128 * 1024 - 2)
 
+extern int acc_common_logtype;
+
 /* Helper macro for logging */
 #define rte_acc_log(level, fmt, ...) \
-	rte_log(RTE_LOG_ ## level, RTE_LOG_NOTICE, fmt "\n", \
+	rte_log(RTE_LOG_ ## level, acc_common_logtype, fmt "\n", \
 		##__VA_ARGS__)
 
 /* ACC100 DMA Descriptor triplet */
