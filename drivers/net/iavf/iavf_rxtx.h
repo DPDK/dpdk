a21df949f0a4f942dec13f9a2162bd6dbe6d6aad
Zhichao Zeng <zhichaox.zeng@intel.com>
2024-01-03 15:47:19 +0800

    net/iavf: support LLDP in scalar Tx
    This patch adds an mbuf dynfield IAVF_TX_LLDP_DYNFIELD to determine
    whether or not to fill the SWTCH_UPLINK bit in the Tx context descriptor
    to transmit LLDP packet.
    
    Signed-off-by: Zhichao Zeng <zhichaox.zeng@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/iavf/iavf_rxtx.h
+++ drivers/net/iavf/iavf_rxtx.h
@@ -107,8 +107,16 @@
 #define IAVF_MAX_DATA_PER_TXD \
 	(IAVF_TXD_QW1_TX_BUF_SZ_MASK >> IAVF_TXD_QW1_TX_BUF_SZ_SHIFT)
 
+#define IAVF_TX_LLDP_DYNFIELD "intel_pmd_dynfield_tx_lldp"
+#define IAVF_CHECK_TX_LLDP(m) \
+	((rte_pmd_iavf_tx_lldp_dynfield_offset > 0) && \
+	(*RTE_MBUF_DYNFIELD((m), \
+			rte_pmd_iavf_tx_lldp_dynfield_offset, \
+			uint8_t *)))
+
 extern uint64_t iavf_timestamp_dynflag;
 extern int iavf_timestamp_dynfield_offset;
+extern int rte_pmd_iavf_tx_lldp_dynfield_offset;
 
 /**
  * Rx Flex Descriptors
