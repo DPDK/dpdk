bd00ef3b097eae5625f96655cf04842ee8530c40
Konstantin Ananyev <konstantin.ananyev@huawei.com>
2024-03-01 16:39:31 +0000

    examples/l3fwd: fix Rx over not ready port
    [ upstream commit 495709d32be87cb962c55917b19ad7d4489cde3e ]
    
    Running l3fwd in event mode with SW eventdev, service cores
    can start RX before main thread is finished with PMD installation.
    to reproduce:
    ./dpdk-l3fwd --lcores=49,51 -n 6 -a ca:00.0 -s 0x8000000000000 \
    --vdev event_sw0 -- \
    -L -P -p 1  --mode eventdev --eventq-sched=ordered \
    --rule_ipv4=test/l3fwd_lpm_v4_u1.cfg --rule_ipv6=test/l3fwd_lpm_v6_u1.cfg \
    --no-numa
    
    At init stage user will most likely see the error message like that:
    ETHDEV: lcore 51 called rx_pkt_burst for not ready port 0
    0: ./dpdk-l3fwd (rte_dump_stack+0x1f) [15de723]
    ...
    9: ./dpdk-l3fwd (eal_thread_loop+0x5a2) [15c1324]
    ...
    
    And then all depends how luck/unlucky you are.
    If there are some actual packet in HW RX queue, then the app will most
    likely crash, otherwise it might survive.
    As error message suggests, the problem is that services are started
    before main thread finished with NIC setup and initialization.
    The suggested fix moves services startup after NIC setup phase.
    
    Bugzilla ID: 1390
    Fixes: 8bd537e9c6cf ("examples/l3fwd: add service core setup based on caps")
    
    Signed-off-by: Konstantin Ananyev <konstantin.v.ananyev@yandex.ru>
    Signed-off-by: Konstantin Ananyev <konstantin.ananyev@huawei.com>
    Acked-by: Pavan Nikhilesh <pbhagavatula@marvell.com>

--- examples/l3fwd/main.c
+++ examples/l3fwd/main.c
@@ -1512,7 +1512,6 @@ main(int argc, char **argv)
 			l3fwd_lkp.main_loop = evt_rsrc->ops.fib_event_loop;
 		else
 			l3fwd_lkp.main_loop = evt_rsrc->ops.lpm_event_loop;
-		l3fwd_event_service_setup();
 	} else
 		l3fwd_poll_resource_setup();
 
@@ -1542,6 +1541,11 @@ main(int argc, char **argv)
 					rte_strerror(-ret), portid);
 		}
 	}
+
+#ifdef RTE_LIB_EVENTDEV
+	if (evt_rsrc->enabled)
+		l3fwd_event_service_setup();
+#endif
 
 	printf("\n");
 
