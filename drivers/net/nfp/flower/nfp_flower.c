5b1b9f9c11bedb672cdf01c834d6c341d50a0f1e
Chaoyong He <chaoyong.he@corigine.com>
2024-03-14 15:40:17 +0800

    net/nfp: fix switch domain free check
    CI found calling 'rte_eth_switch_domain_free()' without checking return
    value.
    
    Coverity issue: 414936
    Fixes: 20eaa8e2ebae ("net/nfp: free switch domain ID on close")
    Cc: stable@dpdk.org
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
    Reviewed-by: Long Wu <long.wu@corigine.com>
    Reviewed-by: Peng Zhang <peng.zhang@corigine.com>

--- drivers/net/nfp/flower/nfp_flower.c
+++ drivers/net/nfp/flower/nfp_flower.c
@@ -793,7 +793,8 @@ nfp_uninit_app_fw_flower(struct nfp_pf_dev *pf_dev)
 	rte_free(app_fw_flower->pf_hw);
 	nfp_mtr_priv_uninit(pf_dev);
 	nfp_flow_priv_uninit(pf_dev);
-	rte_eth_switch_domain_free(app_fw_flower->switch_domain_id);
+	if (rte_eth_switch_domain_free(app_fw_flower->switch_domain_id) != 0)
+		PMD_DRV_LOG(WARNING, "Failed to free switch domain for device");
 	rte_free(app_fw_flower);
 }
 
