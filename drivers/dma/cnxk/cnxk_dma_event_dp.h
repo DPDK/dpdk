7eec5db1b21b7edc87a3f76bc9c08824c257429c
David Marchand <david.marchand@redhat.com>
2024-03-22 14:52:55 +0100

    dma/cnxk: fix event header file
    Add missing 'extern "C"' to file.
    Using __rte_internal requires including rte_compat.h.
    
    Fixes: 1693345b6a33 ("dma/cnxk: support DMA event enqueue/dequeue")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Tyler Retzlaff <roretzla@linux.microsoft.com>

--- drivers/dma/cnxk/cnxk_dma_event_dp.h
+++ drivers/dma/cnxk/cnxk_dma_event_dp.h
@@ -5,9 +5,14 @@
 #ifndef _CNXK_DMA_EVENT_DP_H_
 #define _CNXK_DMA_EVENT_DP_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
 #include <rte_common.h>
+#include <rte_compat.h>
 #include <rte_eventdev.h>
 
 __rte_internal
@@ -21,4 +26,9 @@ uint16_t cn9k_dma_adapter_dual_enqueue(void *ws, struct rte_event ev[], uint16_t
 
 __rte_internal
 uintptr_t cnxk_dma_adapter_dequeue(uintptr_t get_work1);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* _CNXK_DMA_EVENT_DP_H_ */
