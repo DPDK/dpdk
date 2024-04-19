DPDK/dpdk

commit 5eed6ad5a7b90471a730f38e9cab42dfc0e78ba2

net/nfp: use flag bits to control meta data parsing
Use flag bits to indicate whether meta data exists and determine
whether it needs to be parsed.

Signed-off-by: Long Wu <long.wu@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>

drivers/net/nfp/nfp_net_meta.c
@@ -27,13 +27,16 @@ nfp_net_meta_parse_chained(uint8_t *meta_base,
 
 	meta_info = rte_be_to_cpu_32(meta_header);
 	meta_offset = meta_base + 4;
+	meta->flags = 0;
 
 	for (; meta_info != 0; meta_info >>= NFP_NET_META_FIELD_SIZE, meta_offset += 4) {
 		switch (meta_info & NFP_NET_META_FIELD_MASK) {
 		case NFP_NET_META_PORTID:
+			meta->flags |= (1 << NFP_NET_META_PORTID);
 			meta->port_id = rte_be_to_cpu_32(*(rte_be32_t *)meta_offset);
 			break;
 		case NFP_NET_META_HASH:
+			meta->flags |= (1 << NFP_NET_META_HASH);
 			/* Next field type is about the hash type */
 			meta_info >>= NFP_NET_META_FIELD_SIZE;
 			/* Hash value is in the data field */
@@ -41,6 +44,7 @@ nfp_net_meta_parse_chained(uint8_t *meta_base,
 			meta->hash_type = meta_info & NFP_NET_META_FIELD_MASK;
 			break;
 		case NFP_NET_META_VLAN:
+			meta->flags |= (1 << NFP_NET_META_VLAN);
 			vlan_info = rte_be_to_cpu_32(*(rte_be32_t *)meta_offset);
 			meta->vlan[meta->vlan_layer].offload =
 					vlan_info >> NFP_NET_META_VLAN_OFFLOAD;
@@ -50,8 +54,8 @@ nfp_net_meta_parse_chained(uint8_t *meta_base,
 			meta->vlan_layer++;
 			break;
 		case NFP_NET_META_IPSEC:
+			meta->flags |= (1 << NFP_NET_META_IPSEC);
 			meta->sa_idx = rte_be_to_cpu_32(*(rte_be32_t *)meta_offset);
-			meta->ipsec_type = meta_info & NFP_NET_META_FIELD_MASK;
 			break;
 		case NFP_NET_META_MARK:
 			meta->flags |= (1 << NFP_NET_META_MARK);
@@ -77,6 +81,7 @@ nfp_net_meta_parse_single(uint8_t *meta_base,
 		rte_be32_t meta_header,
 		struct nfp_net_meta_parsed *meta)
 {
+	meta->flags |= (1 << NFP_NET_META_HASH);
 	meta->hash_type = rte_be_to_cpu_32(meta_header);
 	meta->hash = rte_be_to_cpu_32(*(rte_be32_t *)(meta_base + 4));
 }
@@ -90,6 +95,9 @@ nfp_net_meta_parse_hash(const struct nfp_net_meta_parsed *meta,
 	struct nfp_net_hw *hw = rxq->hw;
 
 	if ((hw->super.ctrl & NFP_NET_CFG_CTRL_RSS_ANY) == 0)
+		return;
+
+	if (((meta->flags >> NFP_NET_META_HASH) & 0x1) == 0)
 		return;
 
 	mbuf->hash.rss = meta->hash;
@@ -107,6 +115,9 @@ nfp_net_meta_parse_vlan(const struct nfp_net_meta_parsed *meta,
 
 	/* Skip if hardware don't support setting vlan. */
 	if ((ctrl & (NFP_NET_CFG_CTRL_RXVLAN | NFP_NET_CFG_CTRL_RXVLAN_V2)) == 0)
+		return;
+
+	if (((meta->flags >> NFP_NET_META_VLAN) & 0x1) == 0)
 		return;
 
 	/*
@@ -156,6 +167,9 @@ nfp_net_meta_parse_qinq(const struct nfp_net_meta_parsed *meta,
 			(hw->cap & NFP_NET_CFG_CTRL_RXQINQ) == 0)
 		return;
 
+	if (((meta->flags >> NFP_NET_META_VLAN) & 0x1) == 0)
+		return;
+
 	if (meta->vlan_layer < NFP_NET_META_MAX_VLANS)
 		return;
 
@@ -184,11 +198,11 @@ nfp_net_meta_parse_ipsec(struct nfp_net_meta_parsed *meta,
 	struct nfp_net_hw *hw;
 	struct nfp_tx_ipsec_desc_msg *desc_md;
 
+	if (((meta->flags >> NFP_NET_META_IPSEC) & 0x1) == 0)
+		return;
+
 	hw = rxq->hw;
 	sa_idx = meta->sa_idx;
-
-	if (meta->ipsec_type != NFP_NET_META_IPSEC)
-		return;
 
 	if (sa_idx >= NFP_NET_IPSEC_MAX_SA_CNT) {
 		mbuf->ol_flags |= RTE_MBUF_F_RX_SEC_OFFLOAD_FAILED;

drivers/net/nfp/nfp_net_meta.h
@@ -77,7 +77,6 @@ struct nfp_net_meta_parsed {
 	uint32_t mark_id;         /**< Mark id value */
 	uint16_t flags;           /**< Bitmap to indicate if meta exist */
 	uint8_t hash_type;        /**< RSS hash type */
-	uint8_t ipsec_type;       /**< IPsec type */
 	uint8_t vlan_layer;       /**< The valid number of value in @vlan[] */
 	/**
 	 * Holds information parses from NFP_NET_META_VLAN.

drivers/net/nfp/nfp_rxtx.c
@@ -497,7 +497,7 @@ nfp_net_recv_pkts(void *rx_queue,
 		mb->next = NULL;
 		mb->port = rxq->port_id;
 
-		struct nfp_net_meta_parsed meta = {};
+		struct nfp_net_meta_parsed meta;
 		nfp_net_meta_parse(rxds, rxq, hw, mb, &meta);
 
 		nfp_net_parse_ptype(rxq, rxds, mb);

commit 7223a7df3b808043452a1f432afb3aaec2378493

net/nfp: fix parsing with empty meta data
If the packet without meta data, the initialization logic of
'meta->flags' will be skipped and lead to the wrong logic
because of 'meta->flags' has random value.

Fix it by make sure 'meta->flags' can be initialized in any situation.

Fixes: 5eed6ad5a7b9 ("net/nfp: use flag bits to control meta data parsing")
Cc: stable@dpdk.org

Signed-off-by: Long Wu <long.wu@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Peng Zhang <peng.zhang@corigine.com>
Reviewed-by: Zerun Fu <zerun.fu@corigine.com>

drivers/net/nfp/nfp_net_meta.c
@@ -27,7 +27,6 @@ nfp_net_meta_parse_chained(uint8_t *meta_base,
 
 	meta_info = rte_be_to_cpu_32(meta_header);
 	meta_offset = meta_base + 4;
-	meta->flags = 0;
 
 	for (; meta_info != 0; meta_info >>= NFP_NET_META_FIELD_SIZE, meta_offset += 4) {
 		switch (meta_info & NFP_NET_META_FIELD_MASK) {
@@ -236,6 +235,8 @@ nfp_net_meta_parse(struct nfp_net_rx_desc *rxds,
 {
 	uint8_t *meta_base;
 	rte_be32_t meta_header;
+
+	meta->flags = 0;
 
 	if (unlikely(NFP_DESC_META_LEN(rxds) == 0))
 		return;