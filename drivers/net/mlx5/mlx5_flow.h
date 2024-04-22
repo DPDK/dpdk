41c5baeffcf3d47e863e3df7324fffacb4ad3239
Erez Shitrit <erezsh@nvidia.com>
2024-04-22 18:50:14 +0200

    net/mlx5/hws: fix port ID for root table
    [ upstream commit 572fe9ef2f461b7e7e195a3a4da5bf0c11f35949 ]
    
    In root tables matcher and rule need to have their port-id, otherwise
    the translate function that done in dpdk layer will not get the right
    attributes.
    For that whenever the matcher is matching the source-port we need to get
    the relevant port-id before calling the translate function.
    
    Fixes: 405242c52dd5 ("net/mlx5/hws: add rule object")
    Cc: stable@dpdk.org
    
    Signed-off-by: Erez Shitrit <erezsh@nvidia.com>
    Signed-off-by: Yevgeny Kliteynik <kliteyn@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>

--- drivers/net/mlx5/mlx5_flow.h
+++ drivers/net/mlx5/mlx5_flow.h
@@ -1759,6 +1759,28 @@ flow_hw_get_reg_id_from_ctx(void *dr_ctx,
 	return REG_NON;
 }
 
+static __rte_always_inline int
+flow_hw_get_port_id_from_ctx(void *dr_ctx, uint32_t *port_val)
+{
+#if defined(HAVE_IBV_FLOW_DV_SUPPORT) || !defined(HAVE_INFINIBAND_VERBS_H)
+	uint32_t port;
+
+	MLX5_ETH_FOREACH_DEV(port, NULL) {
+		struct mlx5_priv *priv;
+		priv = rte_eth_devices[port].data->dev_private;
+
+		if (priv->dr_ctx == dr_ctx) {
+			*port_val = port;
+			return 0;
+		}
+	}
+#else
+	RTE_SET_USED(dr_ctx);
+	RTE_SET_USED(port_val);
+#endif
+	return -EINVAL;
+}
+
 void flow_hw_set_port_info(struct rte_eth_dev *dev);
 void flow_hw_clear_port_info(struct rte_eth_dev *dev);
 int flow_hw_create_vport_action(struct rte_eth_dev *dev);
