DPDK/dpdk

commit 27595cd83053b2d39634a159d6709b3ce3cdf3b0

drivers: move alignment attribute on types for MSVC
Move location of __rte_aligned(a) to new conventional location. The new
placement between {struct,union} and the tag allows the desired
alignment to be imparted on the type regardless of the toolchain being
used for both C and C++. Additionally, it avoids confusion by Doxygen
when generating documentation.

Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Acked-by: Morten Brørup <mb@smartsharesystems.com>

drivers/crypto/mvsam/rte_mrvl_pmd.c
@@ -47,22 +47,20 @@ enum algo_supported {
 };
 
 /** Map elements for cipher mapping.*/
-struct cipher_params_mapping {
+/* We want to squeeze in multiple maps into the cache line. */
+struct __rte_aligned(32) cipher_params_mapping {
 	enum algo_supported  supported;   /**< On/Off switch */
 	enum sam_cipher_alg  cipher_alg;  /**< Cipher algorithm */
 	enum sam_cipher_mode cipher_mode; /**< Cipher mode */
 	unsigned int max_key_len;         /**< Maximum key length (in bytes)*/
-}
-/* We want to squeeze in multiple maps into the cache line. */
-__rte_aligned(32);
+};
 
 /** Map elements for auth mapping.*/
-struct auth_params_mapping {
+/* We want to squeeze in multiple maps into the cache line. */
+struct __rte_aligned(32) auth_params_mapping {
 	enum algo_supported supported;  /**< On/off switch */
 	enum sam_auth_alg   auth_alg;   /**< Auth algorithm */
 }
-/* We want to squeeze in multiple maps into the cache line. */
-__rte_aligned(32);
 
 /**
  * Map of supported cipher algorithms.

commit cd20679d67580c5893cd73b7b705164b6ea69d74

crypto/mvsam: fix build
When moving the alignment attribute, a semicolon was missed.

Fixes: 27595cd83053 ("drivers: move alignment attribute on types for MSVC")

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>

drivers/crypto/mvsam/rte_mrvl_pmd.c
@@ -60,7 +60,7 @@ struct __rte_aligned(32) cipher_params_mapping {
 struct __rte_aligned(32) auth_params_mapping {
 	enum algo_supported supported;  /**< On/off switch */
 	enum sam_auth_alg   auth_alg;   /**< Auth algorithm */
-}
+};
 
 /**
  * Map of supported cipher algorithms.