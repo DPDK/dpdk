ad99680c1dbef9bb02191465926b3c4df8fdf0f2
Venkat Kumar Ande <venkatkumar.ande@amd.com>
2024-06-04 17:41:34 +0530

    net/axgbe: reset link when link never comes back
    [ upstream commit edf463256dd35803aaa11b0da704a0542ad20071 ]
    
    Normally, auto negotiation and reconnect should be automatically done by
    the hardware. But there seems to be an issue where auto negotiation has
    to be restarted manually. This happens because of link training and so
    even though still connected to the partner the link never "comes back".
    This needs an auto-negotiation restart.
    
    Without the fix the user will not see the link up status
    
    Fixes: a5c7273771e8 ("net/axgbe: add phy programming APIs")
    
    Signed-off-by: Venkat Kumar Ande <venkatkumar.ande@amd.com>
    Acked-by: Selwin Sebastian <selwin.sebastian@amd.com>

--- drivers/net/axgbe/axgbe_mdio.c
+++ drivers/net/axgbe/axgbe_mdio.c
@@ -1051,7 +1051,7 @@ static void axgbe_phy_status(struct axgbe_port *pdata)
 							     &an_restart);
 	if (an_restart) {
 		axgbe_phy_config_aneg(pdata);
-		return;
+		goto adjust_link;
 	}
 
 	if (pdata->phy.link) {
