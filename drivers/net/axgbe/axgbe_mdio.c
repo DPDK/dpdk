c2baa4b8da3005ad18a1ec2e4c1267c5f399b111
Venkat Kumar Ande <venkatkumar.ande@amd.com>
2024-06-04 17:41:45 +0530

    net/axgbe: fix linkup in PHY status
    [ upstream commit 50362d0e1f386563a026b314bd0a7eedcd188910 ]
    
    In the event of a change in axgbe mode, the current auto-negotiation
    needs to be reset and the AN cycle needs to be re-triggered. However,
    the current code ignores the return value of axgbe_set_mode(), leading
    to false information as the link is declared without checking the status
    register.
    
    Fix this by propagating the mode switch status information to
    axgbe_phy_status().
    
    Fixes: 102b6ec3d5c3 ("net/axgbe: support auto-negotiation for 1Gbps")
    
    Signed-off-by: Venkat Kumar Ande <venkatkumar.ande@amd.com>
    Acked-by: Selwin Sebastian <selwin.sebastian@amd.com>

--- drivers/net/axgbe/axgbe_mdio.c
+++ drivers/net/axgbe/axgbe_mdio.c
@@ -235,13 +235,14 @@ static void axgbe_switch_mode(struct axgbe_port *pdata)
 	axgbe_change_mode(pdata, pdata->phy_if.phy_impl.switch_mode(pdata));
 }
 
-static void axgbe_set_mode(struct axgbe_port *pdata,
+static bool axgbe_set_mode(struct axgbe_port *pdata,
 			   enum axgbe_mode mode)
 {
 	if (mode == axgbe_cur_mode(pdata))
-		return;
+		return false;
 
 	axgbe_change_mode(pdata, mode);
+	return true;
 }
 
 static bool axgbe_use_mode(struct axgbe_port *pdata,
@@ -1039,7 +1040,7 @@ static enum axgbe_mode axgbe_phy_status_aneg(struct axgbe_port *pdata)
 	return pdata->phy_if.phy_impl.an_outcome(pdata);
 }
 
-static void axgbe_phy_status_result(struct axgbe_port *pdata)
+static bool axgbe_phy_status_result(struct axgbe_port *pdata)
 {
 	enum axgbe_mode mode;
 
@@ -1073,7 +1074,10 @@ static void axgbe_phy_status_result(struct axgbe_port *pdata)
 
 	pdata->phy.duplex = DUPLEX_FULL;
 
-	axgbe_set_mode(pdata, mode);
+	if (axgbe_set_mode(pdata, mode))
+		return true;
+	else
+		return false;
 }
 
 static int autoneg_time_out(unsigned long autoneg_start_time)
@@ -1140,7 +1144,10 @@ static void axgbe_phy_status(struct axgbe_port *pdata)
 				return;
 			}
 		}
-		axgbe_phy_status_result(pdata);
+
+		if (axgbe_phy_status_result(pdata))
+			return;
+
 		if (rte_bit_relaxed_get32(AXGBE_LINK_INIT, &pdata->dev_state))
 			rte_bit_relaxed_clear32(AXGBE_LINK_INIT,
 						&pdata->dev_state);
