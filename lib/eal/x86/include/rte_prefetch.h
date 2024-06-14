DPDK/dpdk

commit 28a5e0b9c7f066cec1b6dc6cbbad6ba0b333a280

eal/x86: implement prefetch for MSVC
Inline assembly is not supported for MSVC x64 instead use _mm_prefetch
and _mm_cldemote intrinsics.

Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>
Acked-by: Morten Brørup <mb@smartsharesystems.com>
Acked-by: Konstantin Ananyev <konstantin.v.ananyev@yandex.ru>
Signed-off-by: David Marchand <david.marchand@redhat.com>

lib/eal/x86/include/rte_prefetch.h
@@ -9,39 +9,63 @@
 extern "C" {
 #endif
 
+#ifdef RTE_TOOLCHAIN_MSVC
+#include <emmintrin.h>
+#endif
+
 #include <rte_compat.h>
 #include <rte_common.h>
 #include "generic/rte_prefetch.h"
 
 static inline void rte_prefetch0(const volatile void *p)
 {
+#ifdef RTE_TOOLCHAIN_MSVC
+	_mm_prefetch((const void *)p, _MM_HINT_T0);
+#else
 	asm volatile ("prefetcht0 %[p]" : : [p] "m" (*(const volatile char *)p));
+#endif
 }
 
 static inline void rte_prefetch1(const volatile void *p)
 {
+#ifdef RTE_TOOLCHAIN_MSVC
+	_mm_prefetch((const void *)p, _MM_HINT_T1);
+#else
 	asm volatile ("prefetcht1 %[p]" : : [p] "m" (*(const volatile char *)p));
+#endif
 }
 
 static inline void rte_prefetch2(const volatile void *p)
 {
+#ifdef RTE_TOOLCHAIN_MSVC
+	_mm_prefetch((const void *)p, _MM_HINT_T2);
+#else
 	asm volatile ("prefetcht2 %[p]" : : [p] "m" (*(const volatile char *)p));
+#endif
 }
 
 static inline void rte_prefetch_non_temporal(const volatile void *p)
 {
+#ifdef RTE_TOOLCHAIN_MSVC
+	_mm_prefetch((const void *)p, _MM_HINT_NTA);
+#else
 	asm volatile ("prefetchnta %[p]" : : [p] "m" (*(const volatile char *)p));
+#endif
 }
 
-/*
- * We use raw byte codes for now as only the newest compiler
- * versions support this instruction natively.
- */
 __rte_experimental
 static inline void
 rte_cldemote(const volatile void *p)
 {
+#ifdef RTE_TOOLCHAIN_MSVC
+	_mm_cldemote(p);
+#else
+	/*
+	 * We use raw byte codes for now as only the newest compiler
+	 * versions support this instruction natively.
+	 */
 	asm volatile(".byte 0x0f, 0x1c, 0x06" :: "S" (p));
+#endif
 }
 
 #ifdef __cplusplus

commit 10664ac6c4a52b715debb4cdc75d65a30015b026

eal/x86: fix prefetch with C++ for MSVC
_mm_prefetch does not take a volatile qualified pointer, cast it away.
Additionally the pointer type should be char * not void * so adjust the
cast to match.

_mm_cldemote does not take a volatile qualified pointer, cast it away.

Fixes: 28a5e0b9c7f0 ("eal/x86: implement prefetch for MSVC")

Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>

lib/eal/x86/include/rte_prefetch.h
@@ -20,7 +20,7 @@ extern "C" {
 static inline void rte_prefetch0(const volatile void *p)
 {
 #ifdef RTE_TOOLCHAIN_MSVC
-	_mm_prefetch((const void *)p, _MM_HINT_T0);
+	_mm_prefetch((const char *)(uintptr_t)p, _MM_HINT_T0);
 #else
 	asm volatile ("prefetcht0 %[p]" : : [p] "m" (*(const volatile char *)p));
 #endif
@@ -29,7 +29,7 @@ static inline void rte_prefetch0(const volatile void *p)
 static inline void rte_prefetch1(const volatile void *p)
 {
 #ifdef RTE_TOOLCHAIN_MSVC
-	_mm_prefetch((const void *)p, _MM_HINT_T1);
+	_mm_prefetch((const char *)(uintptr_t)p, _MM_HINT_T1);
 #else
 	asm volatile ("prefetcht1 %[p]" : : [p] "m" (*(const volatile char *)p));
 #endif
@@ -38,7 +38,7 @@ static inline void rte_prefetch1(const volatile void *p)
 static inline void rte_prefetch2(const volatile void *p)
 {
 #ifdef RTE_TOOLCHAIN_MSVC
-	_mm_prefetch((const void *)p, _MM_HINT_T2);
+	_mm_prefetch((const char *)(uintptr_t)p, _MM_HINT_T2);
 #else
 	asm volatile ("prefetcht2 %[p]" : : [p] "m" (*(const volatile char *)p));
 #endif
@@ -47,7 +47,7 @@ static inline void rte_prefetch2(const volatile void *p)
 static inline void rte_prefetch_non_temporal(const volatile void *p)
 {
 #ifdef RTE_TOOLCHAIN_MSVC
-	_mm_prefetch((const void *)p, _MM_HINT_NTA);
+	_mm_prefetch((const char *)(uintptr_t)p, _MM_HINT_NTA);
 #else
 	asm volatile ("prefetchnta %[p]" : : [p] "m" (*(const volatile char *)p));
 #endif
@@ -58,7 +58,7 @@ static inline void
 rte_cldemote(const volatile void *p)
 {
 #ifdef RTE_TOOLCHAIN_MSVC
-	_mm_cldemote(p);
+	_mm_cldemote((const void *)(uintptr_t)p);
 #else
 	/*
 	 * We use raw byte codes for now as only the newest compiler