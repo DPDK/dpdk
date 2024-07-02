fa4bb7025255036fcff9556c637efa2627ac4af4
Shai Brandes <shaibran@amazon.com>
2024-07-02 17:46:22 +0300

    net/ena: fix bad checksum handling
    Removed a workaround for a false L4 bad Rx csum
    indication from the device. The workaround was to set it
    as unknown so the application would check it instead.
    The issue was fixed in the device, thus the driver bad csum
    handling should be fixed in the PMD.
    
    Fixes: b2d2f1cf89a6 ("net/ena: fix checksum flag for L4")
    Cc: stable@dpdk.org
    
    Signed-off-by: Shai Brandes <shaibran@amazon.com>

--- drivers/net/ena/ena_ethdev.c
+++ drivers/net/ena/ena_ethdev.c
@@ -674,13 +674,7 @@ static inline void ena_rx_mbuf_prepare(struct ena_ring *rx_ring,
 	} else {
 		if (unlikely(ena_rx_ctx->l4_csum_err)) {
 			++rx_stats->l4_csum_bad;
-			/*
-			 * For the L4 Rx checksum offload the HW may indicate
-			 * bad checksum although it's valid. Because of that,
-			 * we're setting the UNKNOWN flag to let the app
-			 * re-verify the checksum.
-			 */
-			ol_flags |= RTE_MBUF_F_RX_L4_CKSUM_UNKNOWN;
+			ol_flags |= RTE_MBUF_F_RX_L4_CKSUM_BAD;
 		} else {
 			++rx_stats->l4_csum_good;
 			ol_flags |= RTE_MBUF_F_RX_L4_CKSUM_GOOD;
