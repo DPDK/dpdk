b3f493cc726272c77dd698076f4a4cf95d28cf7f
Gagandeep Singh <g.singh@nxp.com>
2024-07-03 15:56:39 +0530

    common/dpaax/caamflib: fix PDCP-SDAP watchdog error
    [ upstream commit 6671e82e5c6b4858573f4dae665e7f2442913d93 ]
    
    Adding a Jump instruction with CALM flag to ensure
    previous processing has been completed.
    
    Fixes: 3ba2e519eafa ("common/dpaax/caamflib: support PDCP-SDAP")
    
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/common/dpaax/caamflib/desc/sdap.h
+++ drivers/common/dpaax/caamflib/desc/sdap.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2020-2021 NXP
+ * Copyright 2020-2023 NXP
  */
 
 #ifndef __DESC_SDAP_H__
@@ -704,6 +704,10 @@ static inline int pdcp_sdap_insert_no_snoop_op(
 		/* Save the ICV generated */
 		MOVEB(p, CONTEXT1, 0, MATH3, 0, 4, WAITCOMP | IMMED);
 
+		/* conditional jump with calm added to ensure that the
+		 * previous processing has been completed
+		 */
+		JUMP(p, 1, LOCAL_JUMP, ALL_TRUE, CALM);
 		/* The CHA will be reused so we need to clear it */
 		LOAD(p, CLRW_RESET_CLS1_CHA |
 		     CLRW_CLR_C1KEY |
@@ -794,6 +798,10 @@ static inline int pdcp_sdap_insert_no_snoop_op(
 		/* Save the ICV which is stalling in output FIFO to MATH3 */
 		MOVEB(p, OFIFO, 0, MATH3, 0, 4, IMMED);
 
+		/* conditional jump with calm added to ensure that the
+		 * previous processing has been completed
+		 */
+		JUMP(p, 1, LOCAL_JUMP, ALL_TRUE, CALM);
 		/* Reset class 1 CHA */
 		LOAD(p, CLRW_RESET_CLS1_CHA |
 		     CLRW_CLR_C1KEY |
