2001ebe923fb02e2e3691eb5b550f405ea3a16f7
Gagandeep Singh <g.singh@nxp.com>
2024-07-03 15:56:48 +0530

    crypto/dpaa2_sec: initialize ESP sequence number
    Initialize the sequence number of ESP to 1.
    
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

--- drivers/crypto/dpaa2_sec/dpaa2_sec_dpseci.c
+++ drivers/crypto/dpaa2_sec/dpaa2_sec_dpseci.c
@@ -3143,6 +3143,11 @@ dpaa2_sec_set_ipsec_session(struct rte_cryptodev *dev,
 
 		if (ipsec_xform->options.iv_gen_disable == 0)
 			encap_pdb.options |= PDBOPTS_ESP_IVSRC;
+		/* Initializing the sequence number to 1, Security
+		 * engine will choose this sequence number for first packet
+		 * Refer: RFC4303 section: 3.3.3.Sequence Number Generation
+		 */
+		encap_pdb.seq_num = 1;
 		if (ipsec_xform->options.esn) {
 			encap_pdb.options |= PDBOPTS_ESP_ESN;
 			encap_pdb.seq_num_ext_hi = conf->ipsec.esn.hi;
