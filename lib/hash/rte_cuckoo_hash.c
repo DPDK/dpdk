61a734df4a701f1c94f4f66de49bc67259380a20
Yoan Picchi <yoan.picchi@arm.com>
2024-07-08 12:14:07 +0000

    hash: check hash bucket size
    If RTE_HASH_BUCKET_ENTRIES is set to a value superior to 8,
    a hash bucket would no longer fit in the vector (8*16b=128b),
    therefore failing to check some of the signatures.
    This patch adds a compile time check to fallback to scalar code.
    
    Signed-off-by: Yoan Picchi <yoan.picchi@arm.com>

--- lib/hash/rte_cuckoo_hash.c
+++ lib/hash/rte_cuckoo_hash.c
@@ -1891,7 +1891,7 @@ compare_signatures(uint32_t *prim_hash_matches, uint32_t *sec_hash_matches,
 
 	/* For match mask the first bit of every two bits indicates the match */
 	switch (sig_cmp_fn) {
-#if defined(__SSE2__)
+#if defined(__SSE2__) && RTE_HASH_BUCKET_ENTRIES <= 8
 	case RTE_HASH_COMPARE_SSE:
 		/* Compare all signatures in the bucket */
 		*prim_hash_matches = _mm_movemask_epi8(_mm_cmpeq_epi16(
@@ -1908,7 +1908,7 @@ compare_signatures(uint32_t *prim_hash_matches, uint32_t *sec_hash_matches,
 		/* Extract the even-index bits only */
 		*sec_hash_matches &= 0x5555;
 		break;
-#elif defined(__ARM_NEON)
+#elif defined(__ARM_NEON) && RTE_HASH_BUCKET_ENTRIES <= 8
 	case RTE_HASH_COMPARE_NEON: {
 		uint16x8_t vmat, vsig, x;
 		int16x8_t shift = {-15, -13, -11, -9, -7, -5, -3, -1};
