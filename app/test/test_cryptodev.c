5a257288f85d01c4c199ca66c7da1bd8937f39ce
Joel Kavanagh <joel.kavanagh@intel.com>
2024-07-17 15:49:06 +0000

    test/crypto: fix allocation comment
    [ upstream commit 8020502dde70cabc7f65219d8f8db762b03b0e41 ]
    
    This patch fixes a typo in the `test_cryptodev.c` file where "out-op" was
    incorrectly used instead of "out-of-place" on three separate occasions.
    
    Fixes: f3dbf94be60c ("app/test: check SGL on QAT")
    Fixes: 43220096d66a ("test/crypto: add PDCP cases for scatter gather")
    
    Signed-off-by: Joel Kavanagh <joel.kavanagh@intel.com>
    Acked-by: Akhil Goyal <gakhil@marvell.com>

--- app/test/test_cryptodev.c
+++ app/test/test_cryptodev.c
@@ -8616,7 +8616,7 @@ static int test_pdcp_proto(int i, int oop, enum rte_crypto_cipher_operation opc,
 	/* Out of place support */
 	if (oop) {
 		/*
-		 * For out-op-place we need to alloc another mbuf
+		 * For out-of-place we need to alloc another mbuf
 		 */
 		ut_params->obuf = rte_pktmbuf_alloc(ts_params->mbuf_pool);
 		rte_pktmbuf_append(ut_params->obuf, output_vec_len);
@@ -8825,7 +8825,7 @@ test_pdcp_proto_SGL(int i, int oop,
 	/* Out of place support */
 	if (oop) {
 		/*
-		 * For out-op-place we need to alloc another mbuf
+		 * For out-of-place we need to alloc another mbuf
 		 */
 		ut_params->obuf = rte_pktmbuf_alloc(ts_params->mbuf_pool);
 		rte_pktmbuf_append(ut_params->obuf, frag_size_oop);
@@ -14623,7 +14623,7 @@ test_authenticated_encryption_SGL(const struct aead_test_data *tdata,
 	}
 
 	/*
-	 * For out-op-place we need to alloc another mbuf
+	 * For out-of-place we need to alloc another mbuf
 	 */
 	if (oop) {
 		ut_params->obuf = rte_pktmbuf_alloc(ts_params->mbuf_pool);
