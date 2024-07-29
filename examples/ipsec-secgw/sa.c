ff2d7af17a475be45fc4eb309134f2b12b37c5b7
Vladimir Medvedkin <vladimir.medvedkin@intel.com>
2024-07-29 12:57:54 +0000

    examples/ipsec-secgw: revert SA salt endianness
    [ upstream commit 7f9b2c926a5192440eaecf0cb80cc45ac8d9a0f2 ]
    
    The previous commit swapped the bytes of SA salt which
    led to incorrect work of the ipsec-secgw application.
    This patch reverts the previous commit and changes
    the type of the salt field in the ipsec_sa structure.
    
    Fixes: e6bfd9676109 ("examples/ipsec-secgw: fix SA salt endianness")
    
    Signed-off-by: Vladimir Medvedkin <vladimir.medvedkin@intel.com>
    Acked-by: Radu Nicolau <radu.nicolau@intel.com>

--- examples/ipsec-secgw/sa.c
+++ examples/ipsec-secgw/sa.c
@@ -373,7 +373,6 @@ parse_sa_tokens(char **tokens, uint32_t n_tokens,
 	uint32_t ti; /*token index*/
 	uint32_t *ri /*rule index*/;
 	struct ipsec_sa_cnt *sa_cnt;
-	rte_be32_t salt; /*big-endian salt*/
 	uint32_t cipher_algo_p = 0;
 	uint32_t auth_algo_p = 0;
 	uint32_t aead_algo_p = 0;
@@ -508,8 +507,8 @@ parse_sa_tokens(char **tokens, uint32_t n_tokens,
 			if (algo->algo == RTE_CRYPTO_CIPHER_AES_CTR) {
 				key_len -= 4;
 				rule->cipher_key_len = key_len;
-				memcpy(&salt, &rule->cipher_key[key_len], 4);
-				rule->salt = rte_be_to_cpu_32(salt);
+				memcpy(&rule->salt,
+					&rule->cipher_key[key_len], 4);
 			}
 
 			cipher_algo_p = 1;
@@ -573,8 +572,8 @@ parse_sa_tokens(char **tokens, uint32_t n_tokens,
 				key_len -= 4;
 				rule->auth_key_len = key_len;
 				rule->iv_len = algo->iv_len;
-				memcpy(&salt, &rule->auth_key[key_len], 4);
-				rule->salt = rte_be_to_cpu_32(salt);
+				memcpy(&rule->salt,
+					&rule->auth_key[key_len], 4);
 			}
 
 			auth_algo_p = 1;
@@ -632,8 +631,8 @@ parse_sa_tokens(char **tokens, uint32_t n_tokens,
 
 			key_len -= 4;
 			rule->cipher_key_len = key_len;
-			memcpy(&salt, &rule->cipher_key[key_len], 4);
-			rule->salt = rte_be_to_cpu_32(salt);
+			memcpy(&rule->salt,
+				&rule->cipher_key[key_len], 4);
 
 			aead_algo_p = 1;
 			continue;
