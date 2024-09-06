9910db35962bb22af0b02aa28025720ebf31278a
Jeremy Spewock <jspewock@iol.unh.edu>
2024-09-06 14:00:36 -0400

    dts: add MTU config methods to testpmd shell
    There are methods within DTS currently that support updating the MTU of
    ports on a node, but the methods for doing this in a linux session rely
    on the ip command and the port being bound to the kernel driver. Since
    test suites are run while bound to the driver for DPDK, there needs to
    be a way to modify the value while bound to said driver as well. This is
    done by using testpmd to modify the MTU.
    
    Signed-off-by: Jeremy Spewock <jspewock@iol.unh.edu>
    Reviewed-by: Luca Vizzarro <luca.vizzarro@arm.com>
    Reviewed-by: Juraj Linkeš <juraj.linkes@pantheon.tech>

--- dts/framework/remote_session/testpmd_shell.py
+++ dts/framework/remote_session/testpmd_shell.py
@@ -937,6 +937,50 @@ class TestPmdShell(DPDKShell):
 
         return TestPmdPortStats.parse(output)
 
+    @requires_stopped_ports
+    def set_port_mtu(self, port_id: int, mtu: int, verify: bool = True) -> None:
+        """Change the MTU of a port using testpmd.
+
+        Some PMDs require that the port be stopped before changing the MTU, and it does no harm to
+        stop the port before configuring in cases where it isn't required, so ports are stopped
+        prior to changing their MTU.
+
+        Args:
+            port_id: ID of the port to adjust the MTU on.
+            mtu: Desired value for the MTU to be set to.
+            verify: If `verify` is :data:`True` then the output will be scanned in an attempt to
+                verify that the mtu was properly set on the port. Defaults to :data:`True`.
+
+        Raises:
+            InteractiveCommandExecutionError: If `verify` is :data:`True` and the MTU was not
+                properly updated on the port matching `port_id`.
+        """
+        set_mtu_output = self.send_command(f"port config mtu {port_id} {mtu}")
+        if verify and (f"MTU: {mtu}" not in self.send_command(f"show port info {port_id}")):
+            self._logger.debug(
+                f"Failed to set mtu to {mtu} on port {port_id}." f" Output was:\n{set_mtu_output}"
+            )
+            raise InteractiveCommandExecutionError(
+                f"Test pmd failed to update mtu of port {port_id} to {mtu}"
+            )
+
+    def set_port_mtu_all(self, mtu: int, verify: bool = True) -> None:
+        """Change the MTU of all ports using testpmd.
+
+        Runs :meth:`set_port_mtu` for every port that testpmd is aware of.
+
+        Args:
+            mtu: Desired value for the MTU to be set to.
+            verify: Whether to verify that setting the MTU on each port was successful or not.
+                Defaults to :data:`True`.
+
+        Raises:
+            InteractiveCommandExecutionError: If `verify` is :data:`True` and the MTU was not
+                properly updated on at least one port.
+        """
+        for port in self.ports:
+            self.set_port_mtu(port.id, mtu, verify)
+
     def _close(self) -> None:
         """Overrides :meth:`~.interactive_shell.close`."""
         self.stop()
