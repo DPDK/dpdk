6a41e6070ea6f251987f81f9d610190b9adfb978
David Marchand <david.marchand@redhat.com>
2024-09-10 16:47:40 +0200

    event/dlb2: fix base driver logs
    DLB2_(HW_)?(INFO|ERR|DEBUG) are defined in osdep.h for base/ driver code
    to log messages.
    The base/ driver should not bypass those macros, directly calling
    DLB2_LOG_(INFO|ERR|DEBUG).
    
    Besides, DLB2_LOG_* macros always append a \n, meaning that most of the
    logs coming from the base/ driver code have double \n.
    
    This patch fixes osdep.h macro definition, then remove a few direct calls
    to DLB2_LOG_* (and adjust \n accordingly).
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- drivers/event/dlb2/dlb2_log.h
+++ drivers/event/dlb2/dlb2_log.h
@@ -6,12 +6,11 @@
 #define _DLB2_EVDEV_LOG_H_
 
 extern int eventdev_dlb2_log_level;
-#define RTE_LOGTYPE_EVENTDEV_DLB2_LOG_LEVEL eventdev_dlb2_log_level
+#define RTE_LOGTYPE_EVENTDEV_DLB2 eventdev_dlb2_log_level
 
 /* Dynamic logging */
 #define DLB2_LOG_IMPL(level, fmt, args...) \
-	rte_log(RTE_LOG_ ## level, eventdev_dlb2_log_level, "%s" fmt "\n", \
-		__func__, ##args)
+	RTE_LOG(level, EVENTDEV_DLB2, "%s" fmt "\n", __func__, ##args)
 
 #define DLB2_LOG_INFO(fmt, args...) \
 	DLB2_LOG_IMPL(INFO, fmt, ## args)
@@ -21,6 +20,6 @@ extern int eventdev_dlb2_log_level;
 
 /* remove debug logs at compile time unless actually debugging */
 #define DLB2_LOG_DBG(fmt, args...) \
-	RTE_LOG_DP(DEBUG, EVENTDEV_DLB2_LOG_LEVEL, fmt, ## args)
+	RTE_LOG_DP(DEBUG, EVENTDEV_DLB2, fmt, ## args)
 
 #endif /* _DLB2_EVDEV_LOG_H_ */
