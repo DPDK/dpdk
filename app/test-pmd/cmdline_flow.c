2a03def86c6703daebc04376243b5ce7cd500fd7
Viacheslav Ovsiienko <viacheslavo@nvidia.com>
2024-09-18 16:46:20 +0300

    app/testpmd: remove flex item init command leftover
    [ upstream commit d5c50397a1cc06419970afbea9cd1c37e3c08a5b ]
    
    There was a leftover of "flow flex init" command used
    for debug purposes and had no useful functionality in
    the production code.
    
    Fixes: 59f3a8acbcdb ("app/testpmd: add flex item commands")
    
    Signed-off-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>
    Acked-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

--- app/test-pmd/cmdline_flow.c
+++ app/test-pmd/cmdline_flow.c
@@ -86,7 +86,6 @@ enum index {
 	FLEX,
 
 	/* Flex arguments */
-	FLEX_ITEM_INIT,
 	FLEX_ITEM_CREATE,
 	FLEX_ITEM_DESTROY,
 
@@ -921,7 +920,6 @@ struct parse_action_priv {
 	})
 
 static const enum index next_flex_item[] = {
-	FLEX_ITEM_INIT,
 	FLEX_ITEM_CREATE,
 	FLEX_ITEM_DESTROY,
 	ZERO,
@@ -2315,15 +2313,6 @@ static const struct token token_list[] = {
 		.help = "flex item API",
 		.next = NEXT(next_flex_item),
 		.call = parse_flex,
-	},
-	[FLEX_ITEM_INIT] = {
-		.name = "init",
-		.help = "flex item init",
-		.args = ARGS(ARGS_ENTRY(struct buffer, args.flex.token),
-			     ARGS_ENTRY(struct buffer, port)),
-		.next = NEXT(NEXT_ENTRY(COMMON_FLEX_TOKEN),
-			     NEXT_ENTRY(COMMON_PORT_ID)),
-		.call = parse_flex
 	},
 	[FLEX_ITEM_CREATE] = {
 		.name = "create",
@@ -7494,7 +7483,6 @@ parse_flex(struct context *ctx, const struct token *token,
 		switch (ctx->curr) {
 		default:
 			break;
-		case FLEX_ITEM_INIT:
 		case FLEX_ITEM_CREATE:
 		case FLEX_ITEM_DESTROY:
 			out->command = ctx->curr;
