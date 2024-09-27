2327fa724fb5b90f930f6936c72d86c1d877c15e
Nicolas Chautru <nicolas.chautru@intel.com>
2024-09-26 17:31:26 -0700

    bbdev: add new function to dump debug information
    This provides a new API to dump more debug information
    related to the status on a given bbdev queue.
    Some of this information is visible at bbdev level.
    This also provides a new option dev op, to print more
    information at the lower PMD level.
    This helps user to troubleshoot issues related to
    previous operations provided into a queue causing
    possible hard-to-debug negative scenarios.
    
    Signed-off-by: Nicolas Chautru <nicolas.chautru@intel.com>
    Acked-by: Hemant Agrawal <hemant.agrawal@nxp.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

--- lib/bbdev/rte_bbdev.h
+++ lib/bbdev/rte_bbdev.h
@@ -1062,6 +1062,53 @@ rte_bbdev_device_status_str(enum rte_bbdev_device_status status);
 const char*
 rte_bbdev_enqueue_status_str(enum rte_bbdev_enqueue_status status);
 
+/**
+ * Dump operations info from device to a file.
+ * This API is used for debugging provided input operations, not a dataplane API.
+ *
+ *  @param dev_id
+ *    The device identifier.
+ *
+ *  @param queue_index
+ *    Index of queue.
+ *
+ *  @param file
+ *    A pointer to a file for output.
+ *
+ * @returns
+ *   - 0 on success
+ *   - ENOTSUP if interrupts are not supported by the identified device
+ *   - negative value on failure - as returned from PMD
+ *
+ */
+__rte_experimental
+int
+rte_bbdev_queue_ops_dump(uint16_t dev_id, uint16_t queue_index, FILE *file);
+
+
+/**
+ * String of parameters related to the parameters of an operation of a given type.
+ *
+ *  @param op
+ *    Pointer to an operation.
+ *
+ *  @param op_type
+ *    Operation type enum.
+ *
+ *  @param str
+ *    String being describing the operations.
+ *
+ *  @param len
+ *    Size of the string buffer.
+ *
+ * @returns
+ *   String describing the provided operation.
+ *
+ */
+__rte_experimental
+char *
+rte_bbdev_ops_param_string(void *op, enum rte_bbdev_op_type op_type, char *str, uint32_t len);
+
 #ifdef __cplusplus
 }
 #endif
