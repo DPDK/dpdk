7ad2841fe8737c1c3e65ed4977bdb56a73f7f8c2
Sunil Kumar Kori <skori@marvell.com>
2024-10-01 11:30:45 +0530

    common/cnxk: fix MAC address change with active VF
    [ upstream commit 2d4505dc6d4b541710f1c178ee0b309fab4d2ee8 ]
    
    If device is in reconfigure state then it throws error while
    changing default MAC or adding new MAC in LMAC filter table
    if there are active VFs on a PF.
    
    Allowing MAC address set/add even active VFs are present on
    PF.
    
    Fixes: 313cc41830ec ("common/cnxk: support NIX MAC operations")
    
    Signed-off-by: Sunil Kumar Kori <skori@marvell.com>

--- drivers/common/cnxk/roc_nix_mac.c
+++ drivers/common/cnxk/roc_nix_mac.c
@@ -91,11 +91,6 @@ roc_nix_mac_addr_set(struct roc_nix *roc_nix, const uint8_t addr[])
 		goto exit;
 	}
 
-	if (dev_active_vfs(&nix->dev)) {
-		rc = NIX_ERR_OP_NOTSUP;
-		goto exit;
-	}
-
 	req = mbox_alloc_msg_cgx_mac_addr_set(mbox);
 	if (req == NULL)
 		goto exit;
@@ -148,11 +143,6 @@ roc_nix_mac_addr_add(struct roc_nix *roc_nix, uint8_t addr[])
 	int rc;
 
 	if (roc_nix_is_vf_or_sdp(roc_nix)) {
-		rc = NIX_ERR_OP_NOTSUP;
-		goto exit;
-	}
-
-	if (dev_active_vfs(&nix->dev)) {
 		rc = NIX_ERR_OP_NOTSUP;
 		goto exit;
 	}
