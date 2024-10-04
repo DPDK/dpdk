4f3159ae1667aa28115ab6df915985f3d4e74b2c
Ferruh Yigit <ferruh.yigit@amd.com>
2024-10-03 21:13:34 -0700

    ethdev: convert string initialization
    gcc 15 experimental [1], with -Wextra flag, gives warning in variable
    initialization as string [2].
    
    The warning has a point when initialized variable is intended to use as
    string, since assignment is missing the required null terminator for
    this case. But warning is useless for our usecase.
    
    In this patch only updated a few instance to show the issue, there are
    many instances to fix, if we prefer to go this way.
    Other option is to disable warning but it can be useful for actual
    string usecases, so I prefer to keep it.
    
    Converted string initialization to array initialization.
    
    [1]
    gcc (GCC) 15.0.0 20241003 (experimental)
    
    [2]
    ../lib/ethdev/rte_flow.h:906:36:
      error: initializer-string for array of ‘unsigned char’ is too long
            [-Werror=unterminated-string-initialization]
    906 |         .hdr.dst_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
        |                                    ^~~~~~~~~~~~~~~~~~~~~~~~~~
    
    ../lib/ethdev/rte_flow.h:907:36:
      error: initializer-string for array of ‘unsigned char’ is too long
             [-Werror=unterminated-string-initialization]
    907 |         .hdr.src_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
        |                                    ^~~~~~~~~~~~~~~~~~~~~~~~~~
    
    ../lib/ethdev/rte_flow.h:1009:25:
      error: initializer-string for array of ‘unsigned char’ is too long
             [-Werror=unterminated-string-initialization]
    1009 |                         "\xff\xff\xff\xff\xff\xff\xff\xff"
         |                         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    ../lib/ethdev/rte_flow.h:1012:25:
      error: initializer-string for array of ‘unsigned char’ is too long
             [-Werror=unterminated-string-initialization]
    1012 |                         "\xff\xff\xff\xff\xff\xff\xff\xff"
         |                         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    ../lib/ethdev/rte_flow.h:1135:20:
      error: initializer-string for array of ‘unsigned char’ is too long
             [-Werror=unterminated-string-initialization]
    1135 |         .hdr.vni = "\xff\xff\xff",
         |                    ^~~~~~~~~~~~~~
    
    Signed-off-by: Ferruh Yigit <ferruh.yigit@amd.com>
    Acked-by: Morten Brørup <mb@smartsharesystems.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Acked-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Acked-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

--- drivers/net/mlx5/mlx5_flow_dv.c
+++ drivers/net/mlx5/mlx5_flow_dv.c
@@ -7697,12 +7697,10 @@ const struct rte_flow_item_ipv4 nic_ipv4_mask = {
 
 const struct rte_flow_item_ipv6 nic_ipv6_mask = {
 	.hdr = {
-		.src_addr =
-		"\xff\xff\xff\xff\xff\xff\xff\xff"
-		"\xff\xff\xff\xff\xff\xff\xff\xff",
-		.dst_addr =
-		"\xff\xff\xff\xff\xff\xff\xff\xff"
-		"\xff\xff\xff\xff\xff\xff\xff\xff",
+		.src_addr = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+			      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+		.dst_addr = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+			      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 		.vtc_flow = RTE_BE32(0xffffffff),
 		.proto = 0xff,
 		.hop_limits = 0xff,
@@ -9291,8 +9289,8 @@ flow_dv_translate_item_eth(void *key, const struct rte_flow_item *item,
 	const struct rte_flow_item_eth *eth_m;
 	const struct rte_flow_item_eth *eth_v;
 	const struct rte_flow_item_eth nic_mask = {
-		.hdr.dst_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
-		.hdr.src_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
+		.hdr.dst_addr.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+		.hdr.src_addr.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 		.hdr.ether_type = RTE_BE16(0xffff),
 		.has_vlan = 0,
 	};
@@ -9549,12 +9547,10 @@ flow_dv_translate_item_ipv6(void *key, const struct rte_flow_item *item,
 	const struct rte_flow_item_ipv6 *ipv6_v;
 	const struct rte_flow_item_ipv6 nic_mask = {
 		.hdr = {
-			.src_addr =
-				"\xff\xff\xff\xff\xff\xff\xff\xff"
-				"\xff\xff\xff\xff\xff\xff\xff\xff",
-			.dst_addr =
-				"\xff\xff\xff\xff\xff\xff\xff\xff"
-				"\xff\xff\xff\xff\xff\xff\xff\xff",
+			.src_addr = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+				      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+			.dst_addr = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+				      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 			.vtc_flow = RTE_BE32(0xffffffff),
 			.proto = 0xff,
 			.hop_limits = 0xff,
@@ -10064,7 +10060,7 @@ flow_dv_translate_item_vxlan(struct rte_eth_dev *dev,
 	int i;
 	struct mlx5_priv *priv = dev->data->dev_private;
 	const struct rte_flow_item_vxlan nic_mask = {
-		.hdr.vni = "\xff\xff\xff",
+		.hdr.vni =  { 0xff, 0xff, 0xff },
 		.hdr.rsvd1 = 0xff,
 	};
 
--- drivers/net/tap/tap_flow.c
+++ drivers/net/tap/tap_flow.c
@@ -166,8 +166,8 @@ static const struct tap_flow_items tap_flow_items[] = {
 			RTE_FLOW_ITEM_TYPE_IPV4,
 			RTE_FLOW_ITEM_TYPE_IPV6),
 		.mask = &(const struct rte_flow_item_eth){
-			.hdr.dst_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
-			.hdr.src_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
+			.hdr.dst_addr.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+			.hdr.src_addr.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 			.hdr.ether_type = -1,
 		},
 		.mask_sz = sizeof(struct rte_flow_item_eth),
@@ -209,14 +209,10 @@ static const struct tap_flow_items tap_flow_items[] = {
 			       RTE_FLOW_ITEM_TYPE_TCP),
 		.mask = &(const struct rte_flow_item_ipv6){
 			.hdr = {
-				.src_addr = {
-					"\xff\xff\xff\xff\xff\xff\xff\xff"
-					"\xff\xff\xff\xff\xff\xff\xff\xff",
-				},
-				.dst_addr = {
-					"\xff\xff\xff\xff\xff\xff\xff\xff"
-					"\xff\xff\xff\xff\xff\xff\xff\xff",
-				},
+				.src_addr = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+				.dst_addr = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					      0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 				.proto = -1,
 			},
 		},
@@ -299,7 +295,7 @@ static struct remote_rule implicit_rte_flows[TAP_REMOTE_MAX_IDX] = {
 		.items[0] = {
 			.type = RTE_FLOW_ITEM_TYPE_ETH,
 			.mask =  &(const struct rte_flow_item_eth){
-				.hdr.dst_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
+				.hdr.dst_addr.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 			},
 		},
 		.items[1] = {
@@ -316,10 +312,10 @@ static struct remote_rule implicit_rte_flows[TAP_REMOTE_MAX_IDX] = {
 		.items[0] = {
 			.type = RTE_FLOW_ITEM_TYPE_ETH,
 			.mask =  &(const struct rte_flow_item_eth){
-				.hdr.dst_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
+				.hdr.dst_addr.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 			},
 			.spec = &(const struct rte_flow_item_eth){
-				.hdr.dst_addr.addr_bytes = "\xff\xff\xff\xff\xff\xff",
+				.hdr.dst_addr.addr_bytes = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
 			},
 		},
 		.items[1] = {
@@ -336,10 +332,10 @@ static struct remote_rule implicit_rte_flows[TAP_REMOTE_MAX_IDX] = {
 		.items[0] = {
 			.type = RTE_FLOW_ITEM_TYPE_ETH,
 			.mask =  &(const struct rte_flow_item_eth){
-				.hdr.dst_addr.addr_bytes = "\x33\x33\x00\x00\x00\x00",
+				.hdr.dst_addr.addr_bytes = { 0x33, 0x33, 0x00, 0x00, 0x00, 0x00 },
 			},
 			.spec = &(const struct rte_flow_item_eth){
-				.hdr.dst_addr.addr_bytes = "\x33\x33\x00\x00\x00\x00",
+				.hdr.dst_addr.addr_bytes = { 0x33, 0x33, 0x00, 0x00, 0x00, 0x00 },
 			},
 		},
 		.items[1] = {
@@ -370,10 +366,10 @@ static struct remote_rule implicit_rte_flows[TAP_REMOTE_MAX_IDX] = {
 		.items[0] = {
 			.type = RTE_FLOW_ITEM_TYPE_ETH,
 			.mask =  &(const struct rte_flow_item_eth){
-				.hdr.dst_addr.addr_bytes = "\x01\x00\x00\x00\x00\x00",
+				.hdr.dst_addr.addr_bytes = { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00 },
 			},
 			.spec = &(const struct rte_flow_item_eth){
-				.hdr.dst_addr.addr_bytes = "\x01\x00\x00\x00\x00\x00",
+				.hdr.dst_addr.addr_bytes = { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00 },
 			},
 		},
 		.items[1] = {
