a653fa9c505d214ef5dd0c639b793cd281d6d4a6
Oleksandr Kolomeiets <okl-plv@napatech.com>
2024-10-10 16:13:34 +0200

    net/ntnic: add header field update (HFU) flow module
    The Header Field Update module updates protocol fields
    if the packets have been changed,
    for example length fields and next protocol fields.
    
    Signed-off-by: Oleksandr Kolomeiets <okl-plv@napatech.com>
    Acked-by: Serhii Iliushyk <sil-plv@napatech.com>

--- drivers/net/ntnic/nthw/supported/nthw_fpga_reg_defs.h
+++ drivers/net/ntnic/nthw/supported/nthw_fpga_reg_defs.h
@@ -18,6 +18,7 @@
 #include "nthw_fpga_reg_defs_gfg.h"
 #include "nthw_fpga_reg_defs_gmf.h"
 #include "nthw_fpga_reg_defs_gpio_phy.h"
+#include "nthw_fpga_reg_defs_hfu.h"
 #include "nthw_fpga_reg_defs_hif.h"
 #include "nthw_fpga_reg_defs_hsh.h"
 #include "nthw_fpga_reg_defs_i2cm.h"
