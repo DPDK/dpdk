36bff3a1c0ef284b524850cde760d65a4f31521b
Chaoyong He <chaoyong.he@corigine.com>
2024-10-12 10:41:00 +0800

    net/nfp: simplify some function parameters
    Refactor to the logic of initialize process to simplify some function
    parameters, both for primary and secondary process.
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
    Reviewed-by: Long Wu <long.wu@corigine.com>
    Reviewed-by: Peng Zhang <peng.zhang@corigine.com>
    Acked-by: Stephen Hemminger <stephen@networkplumber.org>

--- drivers/net/nfp/nfp_net_common.c
+++ drivers/net/nfp/nfp_net_common.c
@@ -2724,13 +2724,12 @@ nfp_net_fec_set(struct rte_eth_dev *dev,
 }
 
 uint32_t
-nfp_net_get_port_num(struct nfp_pf_dev *pf_dev,
-		struct nfp_eth_table *nfp_eth_table)
+nfp_net_get_port_num(struct nfp_pf_dev *pf_dev)
 {
 	if (pf_dev->multi_pf.enabled)
 		return 1;
 	else
-		return nfp_eth_table->count;
+		return pf_dev->nfp_eth_table->count;
 }
 
 uint8_t
