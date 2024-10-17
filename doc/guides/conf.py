DPDK/dpdk

commit 7f9326423a045f7346a459280dc98fed4afd6811

dts: add API doc generation
The tool used to generate DTS API docs is Sphinx, which is already in
use in DPDK. The same configuration is used to preserve style with one
DTS-specific configuration (so that the DPDK docs are unchanged) that
modifies how the sidebar displays the content. There's other Sphinx
configuration related to Python docstrings which doesn't affect DPDK doc
build. All new configuration is in a conditional block, applied only
when DTS API docs are built to not interfere with DPDK doc build.

Sphinx generates the documentation from Python docstrings. The docstring
format is the Google format [0] which requires the sphinx.ext.napoleon
extension. The other extension, sphinx.ext.intersphinx, enables linking
to objects in external documentations, such as the Python documentation.

There is one requirement for building DTS docs - the same Python version
as DTS or higher, because Sphinx's autodoc extension imports the code.

The dependencies needed to import the code don't have to be satisfied,
as the autodoc extension allows us to mock the imports. The missing
packages are taken from the DTS pyproject.toml file.

And finally, the DTS API docs can be accessed from the DPDK API doxygen
page.

[0] https://google.github.io/styleguide/pyguide.html#38-comments-and-docstrings

Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
Acked-by: Thomas Monjalon <thomas@monjalon.net>
Reviewed-by: Jeremy Spewock <jspewock@iol.unh.edu>
Reviewed-by: Dean Marx <dmarx@iol.unh.edu>
Reviewed-by: Luca Vizzarro <luca.vizzarro@arm.com>

doc/guides/conf.py
@@ -10,7 +10,7 @@ from os import environ
 from os.path import basename
 from os.path import dirname
 from os.path import join as path_join
-from sys import argv, stderr
+from sys import argv, stderr, path
 
 import configparser
 
@@ -57,6 +57,51 @@ man_pages = [("testpmd_app_ug/run_app", "testpmd",
               "dump a PMDs hardware support info", "", 1),
              ("tools/devbind", "dpdk-devbind",
               "check device status and bind/unbind them from drivers", "", 8)]
+
+# DTS API docs additional configuration
+if environ.get('DTS_DOC_BUILD'):
+    extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+    # Napoleon enables the Google format of Python doscstrings.
+    napoleon_numpy_docstring = False
+    napoleon_attr_annotations = True
+    napoleon_preprocess_types = True
+
+    # Autodoc pulls documentation from code.
+    autodoc_default_options = {
+        'members': True,
+        'member-order': 'bysource',
+        'show-inheritance': True,
+    }
+    autodoc_class_signature = 'separated'
+    autodoc_typehints = 'both'
+    autodoc_typehints_format = 'short'
+    autodoc_typehints_description_target = 'documented'
+
+    # Intersphinx allows linking to external projects, such as Python docs.
+    intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+
+    # DTS docstring options.
+    add_module_names = False
+    toc_object_entries = True
+    toc_object_entries_show_parents = 'hide'
+    # DTS Sidebar config.
+    html_theme_options = {
+        'collapse_navigation': False,
+        'navigation_depth': -1,  # unlimited depth
+    }
+
+    # Add path to DTS sources so that Sphinx can find them.
+    dpdk_root = dirname(dirname(dirname(__file__)))
+    path.append(path_join(dpdk_root, 'dts'))
+
+    # Get missing DTS dependencies. Add path to buildtools to find the get_missing_imports function.
+    path.append(path_join(dpdk_root, 'buildtools'))
+    import importlib
+    # Ignore missing imports from DTS dependencies, allowing us to build the docs without them.
+    # There's almost no difference between docs built with and without dependencies.
+    # The qualified names of imported objects are fully expanded with dependencies, such as:
+    # fabric.Connection (without) vs. fabric.connection.Connection (with)
+    autodoc_mock_imports = importlib.import_module('check-dts-requirements').get_missing_imports()
 
 
 # ####### :numref: fallback ########

MAINTAINERS
@@ -1900,6 +1900,7 @@ M: Paul Szczepanek <paul.szczepanek@arm.com>
 M: Patrick Robb <probb@iol.unh.edu>
 T: git://dpdk.org/next/dpdk-next-dts
 F: dts/
+F: buildtools/check-dts-requirements.py
 F: devtools/dts-check-format.sh
 F: doc/guides/tools/dts.rst
 

buildtools/call-sphinx-build.py
@@ -15,6 +15,8 @@ from subprocess import run
 
 # set the version in environment for sphinx to pick up
 os.environ['DPDK_VERSION'] = version
+if 'dts' in src:
+    os.environ['DTS_DOC_BUILD'] = "y"
 
 sphinx_cmd = [sphinx] + extra_args
 

buildtools/check-dts-requirements.py
@@ -1,0 +1,97 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024 PANTHEON.tech s.r.o.
+#
+"""Utilities for DTS dependencies.
+
+The module can be used as an executable script,
+which verifies that the running Python version meets the version requirement of DTS.
+The script exits with the standard exit codes in this mode (0 is success, 1 is failure).
+
+The module also contains a function, get_missing_imports,
+which looks for runtime dependencies in the DTS pyproject.toml file
+and returns a list of module names used in an import statement (import packages) that are missing.
+This function is not used when the module is run as a script and is available to be imported.
+"""
+
+import configparser
+import importlib.metadata
+import importlib.util
+import os.path
+import platform
+
+from packaging.version import Version
+
+_VERSION_COMPARISON_CHARS = '^<>='
+# The names of packages used in import statements may be different from distribution package names.
+# We get distribution package names from pyproject.toml.
+# _EXTRA_DEPS adds those import names which don't match their distribution package name.
+_EXTRA_DEPS = {
+    'invoke': {'version': '>=1.3'},
+    'paramiko': {'version': '>=2.4'},
+    'PyYAML': {'version': '^6.0', 'import_package': 'yaml'}
+}
+_DPDK_ROOT = os.path.dirname(os.path.dirname(__file__))
+_DTS_DEP_FILE_PATH = os.path.join(_DPDK_ROOT, 'dts', 'pyproject.toml')
+
+
+def _get_dependencies(cfg_file_path):
+    cfg = configparser.ConfigParser()
+    with open(cfg_file_path) as f:
+        dts_deps_file_str = f.read()
+        dts_deps_file_str = dts_deps_file_str.replace("\n]", "]")
+        cfg.read_string(dts_deps_file_str)
+
+    deps_section = cfg['tool.poetry.dependencies']
+    return {dep: {'version': deps_section[dep].strip('"\'')} for dep in deps_section}
+
+
+def get_missing_imports():
+    """Get missing DTS import packages from third party libraries.
+
+    Scan the DTS pyproject.toml file for dependencies and find those that are not installed.
+    The dependencies in pyproject.toml are listed by their distribution package names,
+    but the function finds the associated import packages - those used in import statements.
+
+    The function is not used when the module is run as a script. It should be imported.
+
+    Returns:
+        A list of missing import packages.
+    """
+    missing_imports = []
+    req_deps = _get_dependencies(_DTS_DEP_FILE_PATH)
+    req_deps.pop('python')
+
+    for req_dep, dep_data in (req_deps | _EXTRA_DEPS).items():
+        req_ver = dep_data['version']
+        try:
+            import_package = dep_data['import_package']
+        except KeyError:
+            import_package = req_dep
+        import_package = import_package.lower().replace('-', '_')
+
+        try:
+            req_ver = Version(req_ver.strip(_VERSION_COMPARISON_CHARS))
+            found_dep_ver = Version(importlib.metadata.version(req_dep))
+            if found_dep_ver < req_ver:
+                print(
+                    f'The version "{found_dep_ver}" of package "{req_dep}" '
+                    f'is lower than required "{req_ver}".'
+                )
+        except importlib.metadata.PackageNotFoundError:
+            print(f'Package "{req_dep}" not found.')
+            missing_imports.append(import_package)
+
+    return missing_imports
+
+
+if __name__ == '__main__':
+    python_version = _get_dependencies(_DTS_DEP_FILE_PATH).pop('python')
+    if python_version:
+        sys_ver = Version(platform.python_version())
+        req_ver = Version(python_version['version'].strip(_VERSION_COMPARISON_CHARS))
+        if sys_ver < req_ver:
+            print(
+                f'The available Python version "{sys_ver}" is lower than required "{req_ver}".'
+            )
+            exit(1)

buildtools/meson.build
@@ -24,6 +24,7 @@ get_numa_count_cmd = py3 + files('get-numa-count.py')
 get_test_suites_cmd = py3 + files('get-test-suites.py')
 has_hugepages_cmd = py3 + files('has-hugepages.py')
 cmdline_gen_cmd = py3 + files('dpdk-cmdline-gen.py')
+check_dts_requirements = py3 + files('check-dts-requirements.py')
 
 # install any build tools that end-users might want also
 install_data([

doc/api/doxy-api.conf.in
@@ -124,6 +124,8 @@ SEARCHENGINE            = YES
 SORT_MEMBER_DOCS        = NO
 SOURCE_BROWSER          = YES
 
+ALIASES                 = "dts_api_main_page=@DTS_API_MAIN_PAGE@"
+
 EXAMPLE_PATH            = @TOPDIR@/examples
 EXAMPLE_PATTERNS        = *.c
 EXAMPLE_RECURSIVE       = YES

doc/api/dts/custom.css
@@ -1,0 +1,1 @@
+../../guides/custom.css

doc/api/dts/meson.build
@@ -1,0 +1,31 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2023 PANTHEON.tech s.r.o.
+
+sphinx = find_program('sphinx-build', required: get_option('enable_docs'))
+if not sphinx.found()
+    subdir_done()
+endif
+
+python_ver_satisfied = run_command(check_dts_requirements, check: false).returncode()
+if python_ver_satisfied != 0
+    subdir_done()
+endif
+
+cdata.set('DTS_API_MAIN_PAGE', join_paths('..', 'dts', 'html', 'index.html'))
+
+extra_sphinx_args = ['-E', '-c', join_paths(doc_source_dir, 'guides')]
+if get_option('werror')
+    extra_sphinx_args += '-W'
+endif
+
+htmldir = join_paths(get_option('datadir'), 'doc', 'dpdk', 'dts')
+dts_api_html = custom_target('dts_api_html',
+        output: 'html',
+        command: [sphinx_wrapper, sphinx, meson.project_version(),
+            meson.current_source_dir(), meson.current_build_dir(), extra_sphinx_args],
+        build_by_default: get_option('enable_docs'),
+        install: get_option('enable_docs'),
+        install_dir: htmldir)
+
+doc_targets += dts_api_html
+doc_target_names += 'DTS_API_HTML'

commit c523e8643b60f39eda3864af022a84203d962a94

doc: fix Sphinx build without RTD theme
DTS is using a specific configuration of Sphinx
which is supported only with the theme Read The Docs.
If not available, this error may be seen:
	unsupported theme option 'collapse_navigation' given

In order to allow doc generation without this theme,
a check is added to make the specific configuration optional.

Fixes: 7f9326423a04 ("dts: add API doc generation")

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
Signed-off-by: Robin Jarry <rjarry@redhat.com>

doc/guides/conf.py
@@ -16,13 +16,12 @@ import configparser
 
 try:
     import sphinx_rtd_theme
-
     html_theme = "sphinx_rtd_theme"
-except:
+except ImportError:
     print('Install the sphinx ReadTheDocs theme for improved html documentation '
           'layout: https://sphinx-rtd-theme.readthedocs.io/',
           file=stderr)
-    pass
+    html_theme = "default"
 
 stop_on_error = ('-W' in argv)
 
@@ -85,10 +84,11 @@ if environ.get('DTS_DOC_BUILD'):
     toc_object_entries = True
     toc_object_entries_show_parents = 'hide'
     # DTS Sidebar config.
-    html_theme_options = {
-        'collapse_navigation': False,
-        'navigation_depth': -1,  # unlimited depth
-    }
+    if html_theme == "sphinx_rtd_theme":
+        html_theme_options = {
+            'collapse_navigation': False,
+            'navigation_depth': -1,  # unlimited depth
+        }
 
     # Add path to DTS sources so that Sphinx can find them.
     dpdk_root = dirname(dirname(dirname(__file__)))