DPDK/dpdk

commit 0012111a3d879b0b0e27d14a841e2a729545727d

net/ixgbe: fix link status synchronization on BSD
DPDK does not implement interrupt mechanism on BSD,
so force NIC status synchronization.

Fixes: dc66e5fd01b9 ("net/ixgbe: improve link state check on VF")
Cc: stable@dpdk.org

Signed-off-by: Zhihong Peng <zhihongx.peng@intel.com>
Tested-by: Zhimin Huang <zhiminx.huang@intel.com>
Acked-by: Xiaolong Ye <xiaolong.ye@intel.com>

drivers/net/ixgbe/ixgbe_ethdev.c
@@ -4257,6 +4257,11 @@ ixgbe_dev_link_update_share(struct rte_eth_dev *dev,
 	if (wait_to_complete == 0 || dev->data->dev_conf.intr_conf.lsc != 0)
 		wait = 0;
 
+/* BSD has no interrupt mechanism, so force NIC status synchronization. */
+#ifdef RTE_EXEC_ENV_FREEBSD
+	wait = 1;
+#endif
+
 	if (vf)
 		diag = ixgbevf_check_link(hw, &link_speed, &link_up, wait);
 	else

commit f775386d92d68e534600fcff3fc4bcaa30d3e68c

net/ixgbe: fix link status delay on FreeBSD
Forcing wait true prevents checking link status without delay, because
the function will wait more than 10 seconds for link status to be true,
even if the API caller requested no waiting.

Fixes: 0012111a3d87 ("net/ixgbe: fix link status synchronization on BSD")
Cc: stable@dpdk.org

Signed-off-by: Lewis Donzis <lew@perftech.com>
Acked-by: Stephen Hemminger <stephen@networkplumber.org>

drivers/net/ixgbe/ixgbe_ethdev.c
@@ -4314,11 +4314,6 @@ ixgbe_dev_link_update_share(struct rte_eth_dev *dev,
 	if (wait_to_complete == 0 || dev->data->dev_conf.intr_conf.lsc != 0)
 		wait = 0;
 
-/* BSD has no interrupt mechanism, so force NIC status synchronization. */
-#ifdef RTE_EXEC_ENV_FREEBSD
-	wait = 1;
-#endif
-
 	if (vf)
 		diag = ixgbevf_check_link(hw, &link_speed, &link_up, wait);
 	else