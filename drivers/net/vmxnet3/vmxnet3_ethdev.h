d19359e0173ef570c32415ead2fcd0a82f7deb68
Morten Brørup <mb@smartsharesystems.com>
2024-11-04 10:52:19 +0000

    net/vmxnet3: fix potential out of bounds stats access
    [ upstream commit d3a229dd493abcb29d5717c5ce37e0a0bc1777c4 ]
    
    With virtual hardware version 6, the max number of RX queues was
    increased to VMXNET3_EXT_MAX_RX_QUEUES (32) from VMXNET3_MAX_RX_QUEUES
    (16), similarly, the max number of TX queues was increased to
    VMXNET3_EXT_MAX_TX_QUEUES (32) from VMXNET3_MAX_TX_QUEUES (8).  These
    increases were not fully considered in the PMD...
    
    The configured number of queues to provide statistics for
    (RTE_ETHDEV_QUEUE_STAT_CNTRS) can be smaller than driver's max number of
    supported transmit queues for virtual hardware version 6
    (VMXNET3_EXT_MAX_RX_QUEUES) (32), which will cause accessing the queue
    stats array out of boundary if the application uses more than
    RTE_ETHDEV_QUEUE_STAT_CNTRS queues.
    
    This patch fixes this by two modifications
    - Increased stats array size to support hardware version 6.
    - Respect RTE_ETHDEV_QUEUE_STAT_CNTRS when getting the per-queue
      counters.
    
    The build time check
    RTE_BUILD_BUG_ON(RTE_ETHDEV_QUEUE_STAT_CNTRS < VMXNET3_MAX_TX_QUEUES)
    has become irrelevant, so it is removed.
    With this removal, per-queue stats for fewer queues is supported.
    
    Fixes: b1584dd0affe ("net/vmxnet3: support version 6")
    
    Signed-off-by: Morten Brørup <mb@smartsharesystems.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@amd.com>

--- drivers/net/vmxnet3/vmxnet3_ethdev.h
+++ drivers/net/vmxnet3/vmxnet3_ethdev.h
@@ -120,8 +120,8 @@ struct vmxnet3_hw {
 #define VMXNET3_VFT_TABLE_SIZE     (VMXNET3_VFT_SIZE * sizeof(uint32_t))
 	UPT1_TxStats	      saved_tx_stats[VMXNET3_EXT_MAX_TX_QUEUES];
 	UPT1_RxStats	      saved_rx_stats[VMXNET3_EXT_MAX_RX_QUEUES];
-	UPT1_TxStats          snapshot_tx_stats[VMXNET3_MAX_TX_QUEUES];
-	UPT1_RxStats          snapshot_rx_stats[VMXNET3_MAX_RX_QUEUES];
+	UPT1_TxStats          snapshot_tx_stats[VMXNET3_EXT_MAX_TX_QUEUES];
+	UPT1_RxStats          snapshot_rx_stats[VMXNET3_EXT_MAX_RX_QUEUES];
 };
 
 #define VMXNET3_REV_6		5		/* Vmxnet3 Rev. 6 */
