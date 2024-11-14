e5180d0546c8904986a4848e7cce5ba61a331903
Stephen Hemminger <stephen@networkplumber.org>
2024-11-14 09:11:55 -0800

    net/e1000/base: fix fallthrough in switch
    [ upstream commit 11a5adba21237d5905bcb3f5f695aa5a5cfecd9f ]
    
    There is an incorrect fallthrough identified by PVS studio.
    Even though this is in base code it should be fixed, and
    the warning should be re-enabled to prevent future bugs.
    
    Link: https://pvs-studio.com/en/blog/posts/cpp/1183/
    Fixes: f2553cb9eba6 ("net/e1000/base: add new I219 devices")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- drivers/net/e1000/base/e1000_api.c
+++ drivers/net/e1000/base/e1000_api.c
@@ -295,6 +295,7 @@ s32 e1000_set_mac_type(struct e1000_hw *hw)
 	case E1000_DEV_ID_PCH_RPL_I219_LM23:
 	case E1000_DEV_ID_PCH_RPL_I219_V23:
 		mac->type = e1000_pch_tgp;
+		break;
 	case E1000_DEV_ID_PCH_ADL_I219_LM17:
 	case E1000_DEV_ID_PCH_ADL_I219_V17:
 	case E1000_DEV_ID_PCH_RPL_I219_LM22:
