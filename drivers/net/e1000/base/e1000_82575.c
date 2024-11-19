DPDK/dpdk

commit f2553cb9eba6cebbca93387ac8699750aed0b468

net/e1000/base: add new I219 devices
Added new device ids for I219 NIC.

Signed-off-by: Nir Efrati <nir.efrati@intel.com>
Signed-off-by: Qiming Yang <qiming.yang@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/e1000/base/e1000_api.c
@@ -292,8 +292,13 @@ s32 e1000_set_mac_type(struct e1000_hw *hw)
 		break;
 	case E1000_DEV_ID_PCH_ADL_I219_LM16:
 	case E1000_DEV_ID_PCH_ADL_I219_V16:
+	case E1000_DEV_ID_PCH_RPL_I219_LM23:
+	case E1000_DEV_ID_PCH_RPL_I219_V23:
+		mac->type = e1000_pch_tgp;
 	case E1000_DEV_ID_PCH_ADL_I219_LM17:
 	case E1000_DEV_ID_PCH_ADL_I219_V17:
+	case E1000_DEV_ID_PCH_RPL_I219_LM22:
+	case E1000_DEV_ID_PCH_RPL_I219_V22:
 		mac->type = e1000_pch_adp;
 		break;
 	case E1000_DEV_ID_82575EB_COPPER:
@@ -450,6 +455,7 @@ s32 e1000_setup_init_funcs(struct e1000_hw *hw, bool init_device)
 	case e1000_pch_spt:
 	case e1000_pch_cnp:
 	case e1000_pch_adp:
+	case e1000_pch_tgp:
 		e1000_init_function_pointers_ich8lan(hw);
 		break;
 	case e1000_82575:

drivers/net/e1000/base/e1000_hw.h
@@ -128,6 +128,10 @@ struct e1000_hw;
 #define E1000_DEV_ID_PCH_ADL_I219_V16		0x1A1F
 #define E1000_DEV_ID_PCH_ADL_I219_LM17		0x1A1C
 #define E1000_DEV_ID_PCH_ADL_I219_V17		0x1A1D
+#define E1000_DEV_ID_PCH_RPL_I219_LM23          0x0DC5
+#define E1000_DEV_ID_PCH_RPL_I219_V23           0x0DC6
+#define E1000_DEV_ID_PCH_RPL_I219_LM22          0x0DC7
+#define E1000_DEV_ID_PCH_RPL_I219_V22           0x0DC8
 #define E1000_DEV_ID_82576			0x10C9
 #define E1000_DEV_ID_82576_FIBER		0x10E6
 #define E1000_DEV_ID_82576_SERDES		0x10E7
@@ -217,6 +221,7 @@ enum e1000_mac_type {
 	e1000_pch_spt,
 	e1000_pch_cnp,
 	e1000_pch_adp,
+	e1000_pch_tgp,
 	e1000_82575,
 	e1000_82576,
 	e1000_82580,

commit 11a5adba21237d5905bcb3f5f695aa5a5cfecd9f

net/e1000/base: fix fallthrough in switch
There is an incorrect fallthrough identified by PVS studio.
Even though this is in base code it should be fixed, and
the warning should be re-enabled to prevent future bugs.

Link: https://pvs-studio.com/en/blog/posts/cpp/1183/
Fixes: f2553cb9eba6 ("net/e1000/base: add new I219 devices")
Cc: stable@dpdk.org

Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

drivers/net/e1000/base/e1000_82575.c
@@ -1722,6 +1722,7 @@ STATIC s32 e1000_get_media_type_82575(struct e1000_hw *hw)
 			break;
 		}
 		/* Fall through for I2C based SGMII */
+		/* Fall through */
 	case E1000_CTRL_EXT_LINK_MODE_PCIE_SERDES:
 		/* read media type from SFP EEPROM */
 		ret_val = e1000_set_sfp_media_type_82575(hw);

drivers/net/e1000/base/e1000_api.c
@@ -295,6 +295,7 @@ s32 e1000_set_mac_type(struct e1000_hw *hw)
 	case E1000_DEV_ID_PCH_RPL_I219_LM23:
 	case E1000_DEV_ID_PCH_RPL_I219_V23:
 		mac->type = e1000_pch_tgp;
+		break;
 	case E1000_DEV_ID_PCH_ADL_I219_LM17:
 	case E1000_DEV_ID_PCH_ADL_I219_V17:
 	case E1000_DEV_ID_PCH_RPL_I219_LM22:

drivers/net/e1000/base/meson.build
@@ -24,7 +24,6 @@ sources = [
 
 error_cflags = [
         '-Wno-unused-parameter',
-        '-Wno-implicit-fallthrough',
 ]
 c_args = cflags
 foreach flag: error_cflags