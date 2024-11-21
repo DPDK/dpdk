65491527fa667ee05abf48043978ace2e6cbb154
Alexander Kozyrev <akozyrev@nvidia.com>
2024-11-21 02:23:06 +0200

    net/mlx5: fix miniCQEs number calculation
    Use the information from the CQE, not from the title packet,
    for getting the number of miniCQEs in the compressed CQEs array.
    This way we can avoid segfaults in the rxq_cq_decompress_v()
    in case of mbuf corruption (due to double mbuf free, for example).
    
    Fixes: 6cb559d67b ("net/mlx5: add vectorized Rx/Tx burst for x86")
    
    Signed-off-by: Alexander Kozyrev <akozyrev@nvidia.com>

--- drivers/net/mlx5/mlx5_rxtx_vec_altivec.h
+++ drivers/net/mlx5/mlx5_rxtx_vec_altivec.h
@@ -93,8 +93,7 @@ rxq_cq_decompress_v(struct mlx5_rxq_data *rxq, volatile struct mlx5_cqe *cq,
 			-1, -1,           /* skip vlan_tci */
 			11, 10,  9,  8};  /* bswap32, rss */
 	/* Restore the compressed count. Must be 16 bits. */
-	const uint16_t mcqe_n = t_pkt->data_len +
-		(rxq->crc_present * RTE_ETHER_CRC_LEN);
+	const uint16_t mcqe_n = rte_be_to_cpu_32(cq->byte_cnt);
 	const __vector unsigned char rearm =
 		(__vector unsigned char)vec_vsx_ld(0,
 		(signed int const *)&t_pkt->rearm_data);
