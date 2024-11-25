DPDK/dpdk

commit 11ea97805ba167864f318bd32dcd309bf33a4d49

net/ntnic: add minimal flow inline profile
The flow profile implements all flow-related operations

The Napatech adapters support more different functionality than can fit
into a single FPGA.
This functionality is grouped into a number of profiles called inline,
capture, vswitch, and basic.

Inline profile: Uses a scatter gather packet system, which is quite fast
and lightweight.
The FPGA contains functionality for hardware offload use-cases, such as
stateful flow matching, encap/decap, packet steering etc.

Signed-off-by: Serhii Iliushyk <sil-plv@napatech.com>

drivers/net/ntnic/include/flow_api.h
@@ -74,6 +74,21 @@ struct flow_nic_dev {
 	struct flow_nic_dev *next;
 };
 
+enum flow_nic_err_msg_e {
+	ERR_SUCCESS = 0,
+	ERR_FAILED = 1,
+	ERR_OUTPUT_TOO_MANY = 3,
+	ERR_MATCH_INVALID_OR_UNSUPPORTED_ELEM = 12,
+	ERR_MATCH_RESOURCE_EXHAUSTION = 14,
+	ERR_ACTION_UNSUPPORTED = 28,
+	ERR_REMOVE_FLOW_FAILED = 29,
+	ERR_OUTPUT_INVALID = 33,
+	ERR_ACTION_MULTIPLE_PORT_ID_UNSUPPORTED = 40,
+	ERR_MSG_NO_MSG
+};
+
+void flow_nic_set_error(enum flow_nic_err_msg_e msg, struct rte_flow_error *error);
+
 /*
  * Resources
  */

drivers/net/ntnic/nthw/flow_api/flow_api.c
@@ -37,6 +37,29 @@ static struct flow_nic_dev *dev_base;
 static pthread_mutex_t base_mtx = PTHREAD_MUTEX_INITIALIZER;
 
 /*
+ * Error handling
+ */
+
+static const struct {
+	const char *message;
+} err_msg[] = {
+	/* 00 */ { "Operation successfully completed" },
+	/* 01 */ { "Operation failed" },
+	/* 29 */ { "Removing flow failed" },
+};
+
+void flow_nic_set_error(enum flow_nic_err_msg_e msg, struct rte_flow_error *error)
+{
+	assert(msg < ERR_MSG_NO_MSG);
+
+	if (error) {
+		error->message = err_msg[msg].message;
+		error->type = (msg == ERR_SUCCESS) ? RTE_FLOW_ERROR_TYPE_NONE :
+			RTE_FLOW_ERROR_TYPE_UNSPECIFIED;
+	}
+}
+
+/*
  * Resources
  */
 
@@ -136,7 +159,8 @@ static struct flow_handle *flow_create(struct flow_eth_dev *dev __rte_unused,
 		return NULL;
 	}
 
-	return NULL;
+	return profile_inline_ops->flow_create_profile_inline(dev, attr,
+		forced_vlan_vid, caller_id,  item, action, error);
 }
 
 static int flow_destroy(struct flow_eth_dev *dev __rte_unused,
@@ -149,7 +173,7 @@ static int flow_destroy(struct flow_eth_dev *dev __rte_unused,
 		return -1;
 	}
 
-	return -1;
+	return profile_inline_ops->flow_destroy_profile_inline(dev, flow, error);
 }
 
 /*

drivers/net/ntnic/meson.build
@@ -47,6 +47,7 @@ sources = files(
         'nthw/core/nthw_sdc.c',
         'nthw/core/nthw_si5340.c',
         'nthw/flow_api/flow_api.c',
+        'nthw/flow_api/profile_inline/flow_api_profile_inline.c',
         'nthw/flow_api/flow_backend/flow_backend.c',
         'nthw/flow_api/flow_filter.c',
         'nthw/flow_api/flow_kcc.c',

drivers/net/ntnic/nthw/flow_api/profile_inline/flow_api_profile_inline.c
@@ -1,0 +1,65 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2023 Napatech A/S
+ */
+
+#include "ntlog.h"
+
+#include "flow_api_profile_inline.h"
+#include "ntnic_mod_reg.h"
+
+struct flow_handle *flow_create_profile_inline(struct flow_eth_dev *dev __rte_unused,
+	const struct rte_flow_attr *attr __rte_unused,
+	uint16_t forced_vlan_vid __rte_unused,
+	uint16_t caller_id __rte_unused,
+	const struct rte_flow_item elem[] __rte_unused,
+	const struct rte_flow_action action[] __rte_unused,
+	struct rte_flow_error *error __rte_unused)
+{
+	return NULL;
+}
+
+int flow_destroy_locked_profile_inline(struct flow_eth_dev *dev,
+	struct flow_handle *fh,
+	struct rte_flow_error *error)
+{
+	assert(dev);
+	assert(fh);
+
+	int err = 0;
+
+	flow_nic_set_error(ERR_SUCCESS, error);
+
+	return err;
+}
+
+int flow_destroy_profile_inline(struct flow_eth_dev *dev, struct flow_handle *flow,
+	struct rte_flow_error *error)
+{
+	int err = 0;
+
+	flow_nic_set_error(ERR_SUCCESS, error);
+
+	if (flow) {
+		/* Delete this flow */
+		pthread_mutex_lock(&dev->ndev->mtx);
+		err = flow_destroy_locked_profile_inline(dev, flow, error);
+		pthread_mutex_unlock(&dev->ndev->mtx);
+	}
+
+	return err;
+}
+
+static const struct profile_inline_ops ops = {
+	/*
+	 * Flow functionality
+	 */
+	.flow_destroy_locked_profile_inline = flow_destroy_locked_profile_inline,
+	.flow_create_profile_inline = flow_create_profile_inline,
+	.flow_destroy_profile_inline = flow_destroy_profile_inline,
+};
+
+void profile_inline_init(void)
+{
+	register_profile_inline_ops(&ops);
+}

drivers/net/ntnic/nthw/flow_api/profile_inline/flow_api_profile_inline.h
@@ -1,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2023 Napatech A/S
+ */
+
+#ifndef _FLOW_API_PROFILE_INLINE_H_
+#define _FLOW_API_PROFILE_INLINE_H_
+
+#include <stdint.h>
+
+#include "flow_api.h"
+#include "stream_binary_flow_api.h"
+
+/*
+ * Flow functionality
+ */
+int flow_destroy_locked_profile_inline(struct flow_eth_dev *dev,
+	struct flow_handle *fh,
+	struct rte_flow_error *error);
+
+struct flow_handle *flow_create_profile_inline(struct flow_eth_dev *dev,
+	const struct rte_flow_attr *attr,
+	uint16_t forced_vlan_vid,
+	uint16_t caller_id,
+	const struct rte_flow_item elem[],
+	const struct rte_flow_action action[],
+	struct rte_flow_error *error);
+
+int flow_destroy_profile_inline(struct flow_eth_dev *dev,
+	struct flow_handle *flow,
+	struct rte_flow_error *error);
+
+#endif	/* _FLOW_API_PROFILE_INLINE_H_ */

drivers/net/ntnic/ntnic_mod_reg.c
@@ -118,9 +118,19 @@ const struct flow_backend_ops *get_flow_backend_ops(void)
 	return flow_backend_ops;
 }
 
+static const struct profile_inline_ops *profile_inline_ops;
+
+void register_profile_inline_ops(const struct profile_inline_ops *ops)
+{
+	profile_inline_ops = ops;
+}
+
 const struct profile_inline_ops *get_profile_inline_ops(void)
 {
-	return NULL;
+	if (profile_inline_ops == NULL)
+		profile_inline_init();
+
+	return profile_inline_ops;
 }
 
 static const struct flow_filter_ops *flow_filter_ops;

drivers/net/ntnic/ntnic_mod_reg.h
@@ -225,7 +225,30 @@ void register_flow_backend_ops(const struct flow_backend_ops *ops);
 const struct flow_backend_ops *get_flow_backend_ops(void);
 void flow_backend_init(void);
 
+struct profile_inline_ops {
+	/*
+	 * Flow functionality
+	 */
+	int (*flow_destroy_locked_profile_inline)(struct flow_eth_dev *dev,
+		struct flow_handle *fh,
+		struct rte_flow_error *error);
+
+	struct flow_handle *(*flow_create_profile_inline)(struct flow_eth_dev *dev,
+		const struct rte_flow_attr *attr,
+		uint16_t forced_vlan_vid,
+		uint16_t caller_id,
+		const struct rte_flow_item elem[],
+		const struct rte_flow_action action[],
+		struct rte_flow_error *error);
+
+	int (*flow_destroy_profile_inline)(struct flow_eth_dev *dev,
+		struct flow_handle *flow,
+		struct rte_flow_error *error);
+};
+
+void register_profile_inline_ops(const struct profile_inline_ops *ops);
 const struct profile_inline_ops *get_profile_inline_ops(void);
+void profile_inline_init(void);
 
 struct flow_filter_ops {
 	int (*flow_filter_init)(nthw_fpga_t *p_fpga, struct flow_nic_dev **p_flow_device,

commit c4e84cd7f77ae44598576d88357138a08734a494

net/ntnic: fix log messages
Add missed error messages to the list to avoid incorrect logs.
Change order base initializers to index base initializers.
Add static assertions to control the completeness of the list of messages.

Fixes: 11ea97805ba1 ("net/ntnic: add minimal flow inline profile")

Signed-off-by: Serhii Iliushyk <sil-plv@napatech.com>

drivers/net/ntnic/include/flow_api.h
@@ -168,7 +168,8 @@ enum flow_nic_err_msg_e {
 	ERR_ACTION_MULTIPLE_PORT_ID_UNSUPPORTED = 40,
 	ERR_RSS_TOO_LONG_KEY = 41,
 	ERR_ACTION_AGE_UNSUPPORTED_GROUP_0 = 42,
-	ERR_MSG_NO_MSG
+	ERR_MSG_NO_MSG = 43,
+	ERR_MSG_END
 };
 
 void flow_nic_set_error(enum flow_nic_err_msg_e msg, struct rte_flow_error *error);

drivers/net/ntnic/include/flow_api_engine.h
@@ -44,7 +44,8 @@ enum res_type_e {
 	RES_TPE_RPL,
 	RES_SCRUB_RCP,
 	RES_COUNT,
-	RES_INVALID
+	RES_INVALID,
+	RES_END
 };
 
 /*

drivers/net/ntnic/nthw/flow_api/flow_api.c
@@ -19,27 +19,30 @@
 	}
 
 const char *dbg_res_descr[] = {
-	/* RES_QUEUE */ "RES_QUEUE",
-	/* RES_CAT_CFN */ "RES_CAT_CFN",
-	/* RES_CAT_COT */ "RES_CAT_COT",
-	/* RES_CAT_EXO */ "RES_CAT_EXO",
-	/* RES_CAT_LEN */ "RES_CAT_LEN",
-	/* RES_KM_FLOW_TYPE */ "RES_KM_FLOW_TYPE",
-	/* RES_KM_CATEGORY */ "RES_KM_CATEGORY",
-	/* RES_HSH_RCP */ "RES_HSH_RCP",
-	/* RES_PDB_RCP */ "RES_PDB_RCP",
-	/* RES_QSL_RCP */ "RES_QSL_RCP",
-	/* RES_QSL_LTX */ "RES_QSL_LTX",
-	/* RES_QSL_QST */ "RES_QSL_QST",
-	/* RES_SLC_LR_RCP */ "RES_SLC_LR_RCP",
-	/* RES_FLM_FLOW_TYPE */ "RES_FLM_FLOW_TYPE",
-	/* RES_FLM_RCP */ "RES_FLM_RCP",
-	/* RES_TPE_RCP */ "RES_TPE_RCP",
-	/* RES_TPE_EXT */ "RES_TPE_EXT",
-	/* RES_TPE_RPL */ "RES_TPE_RPL",
-	/* RES_COUNT */ "RES_COUNT",
-	/* RES_INVALID */ "RES_INVALID"
+	[RES_QUEUE] = "RES_QUEUE",
+	[RES_CAT_CFN] = "RES_CAT_CFN",
+	[RES_CAT_COT] = "RES_CAT_COT",
+	[RES_CAT_EXO] = "RES_CAT_EXO",
+	[RES_CAT_LEN] = "RES_CAT_LEN",
+	[RES_KM_FLOW_TYPE] = "RES_KM_FLOW_TYPE",
+	[RES_KM_CATEGORY] = "RES_KM_CATEGORY",
+	[RES_HSH_RCP] = "RES_HSH_RCP",
+	[RES_PDB_RCP] = "RES_PDB_RCP",
+	[RES_QSL_RCP] = "RES_QSL_RCP",
+	[RES_QSL_QST] = "RES_QSL_QST",
+	[RES_SLC_LR_RCP] = "RES_SLC_LR_RCP",
+	[RES_FLM_FLOW_TYPE] = "RES_FLM_FLOW_TYPE",
+	[RES_FLM_RCP] = "RES_FLM_RCP",
+	[RES_TPE_RCP] = "RES_TPE_RCP",
+	[RES_TPE_EXT] = "RES_TPE_EXT",
+	[RES_TPE_RPL] = "RES_TPE_RPL",
+	[RES_SCRUB_RCP] = "RES_SCRUB_RCP",
+	[RES_COUNT] = "RES_COUNT",
+	[RES_INVALID] = "RES_INVALID",
 };
+
+static_assert(RTE_DIM(dbg_res_descr) == RES_END,
+	"The list of debug descriptions is not fully completed");
 
 static struct flow_nic_dev *dev_base;
 static rte_spinlock_t base_mtx = RTE_SPINLOCK_INITIALIZER;
@@ -51,37 +54,98 @@ static rte_spinlock_t base_mtx = RTE_SPINLOCK_INITIALIZER;
 static const struct {
 	const char *message;
 } err_msg[] = {
-	/* 00 */ { "Operation successfully completed" },
-	/* 01 */ { "Operation failed" },
-	/* 02 */ { "Memory allocation failed" },
-	/* 03 */ { "Too many output destinations" },
-	/* 04 */ { "Too many output queues for RSS" },
-	/* 05 */ { "The VLAN TPID specified is not supported" },
-	/* 06 */ { "The VxLan Push header specified is not accepted" },
-	/* 07 */ { "While interpreting VxLan Pop action, could not find a destination port" },
-	/* 08 */ { "Failed in creating a HW-internal VTEP port" },
-	/* 09 */ { "Too many VLAN tag matches" },
-	/* 10 */ { "IPv6 invalid header specified" },
-	/* 11 */ { "Too many tunnel ports. HW limit reached" },
-	/* 12 */ { "Unknown or unsupported flow match element received" },
-	/* 13 */ { "Match failed because of HW limitations" },
-	/* 14 */ { "Match failed because of HW resource limitations" },
-	/* 15 */ { "Match failed because of too complex element definitions" },
-	/* 16 */ { "Action failed. To too many output destinations" },
-	/* 17 */ { "Action Output failed, due to HW resource exhaustion" },
-	/* 18 */ { "Push Tunnel Header action cannot output to multiple destination queues" },
-	/* 19 */ { "Inline action HW resource exhaustion" },
-	/* 20 */ { "Action retransmit/recirculate HW resource exhaustion" },
-	/* 21 */ { "Flow counter HW resource exhaustion" },
-	/* 22 */ { "Internal HW resource exhaustion to handle Actions" },
-	/* 23 */ { "Internal HW QSL compare failed" },
-	/* 24 */ { "Internal CAT CFN reuse failed" },
-	/* 25 */ { "Match variations too complex" },
-	/* 26 */ { "Match failed because of CAM/TCAM full" },
-	/* 27 */ { "Internal creation of a tunnel end point port failed" },
-	/* 28 */ { "Unknown or unsupported flow action received" },
-	/* 29 */ { "Removing flow failed" },
+	[ERR_SUCCESS] = {
+		"Operation successfully completed" },
+	[ERR_FAILED] = {
+		"Operation failed" },
+	[ERR_MEMORY] = {
+		"Memory allocation failed" },
+	[ERR_OUTPUT_TOO_MANY] = {
+		"Too many output destinations" },
+	[ERR_RSS_TOO_MANY_QUEUES] = {
+		"Too many output queues for RSS" },
+	[ERR_VLAN_TYPE_NOT_SUPPORTED] = {
+		"The VLAN TPID specified is not supported" },
+	[ERR_VXLAN_HEADER_NOT_ACCEPTED] = {
+		"The VxLan Push header specified is not accepted" },
+	[ERR_VXLAN_POP_INVALID_RECIRC_PORT] = {
+		"While interpreting VxLan Pop action, could not find a destination port" },
+	[ERR_VXLAN_POP_FAILED_CREATING_VTEP] = {
+		"Failed in creating a HW-internal VTEP port" },
+	[ERR_MATCH_VLAN_TOO_MANY] = {
+		"Too many VLAN tag matches" },
+	[ERR_MATCH_INVALID_IPV6_HDR] = {
+		"IPv6 invalid header specified" },
+	[ERR_MATCH_TOO_MANY_TUNNEL_PORTS] = {
+		"Too many tunnel ports. HW limit reached" },
+	[ERR_MATCH_INVALID_OR_UNSUPPORTED_ELEM] = {
+		"Unknown or unsupported flow match element received" },
+	[ERR_MATCH_FAILED_BY_HW_LIMITS] = {
+		"Match failed because of HW limitations" },
+	[ERR_MATCH_RESOURCE_EXHAUSTION] = {
+		"Match failed because of HW resource limitations" },
+	[ERR_MATCH_FAILED_TOO_COMPLEX] = {
+		"Match failed because of too complex element definitions" },
+	[ERR_ACTION_REPLICATION_FAILED] = {
+		"Action failed. To too many output destinations" },
+	[ERR_ACTION_OUTPUT_RESOURCE_EXHAUSTION] = {
+		"Action Output failed, due to HW resource exhaustion" },
+	[ERR_ACTION_TUNNEL_HEADER_PUSH_OUTPUT_LIMIT] = {
+		"Push Tunnel Header action cannot output to multiple destination queues" },
+	[ERR_ACTION_INLINE_MOD_RESOURCE_EXHAUSTION] = {
+		"Inline action HW resource exhaustion" },
+	[ERR_ACTION_RETRANSMIT_RESOURCE_EXHAUSTION] = {
+		"Action retransmit/recirculate HW resource exhaustion" },
+	[ERR_ACTION_FLOW_COUNTER_EXHAUSTION] = {
+		"Flow counter HW resource exhaustion" },
+	[ERR_ACTION_INTERNAL_RESOURCE_EXHAUSTION] = {
+		"Internal HW resource exhaustion to handle Actions" },
+	[ERR_INTERNAL_QSL_COMPARE_FAILED] = {
+		"Internal HW QSL compare failed" },
+	[ERR_INTERNAL_CAT_FUNC_REUSE_FAILED] = {
+		"Internal CAT CFN reuse failed" },
+	[ERR_MATCH_ENTROPHY_FAILED] = {
+		"Match variations too complex" },
+	[ERR_MATCH_CAM_EXHAUSTED] = {
+		"Match failed because of CAM/TCAM full" },
+	[ERR_INTERNAL_VIRTUAL_PORT_CREATION_FAILED] = {
+		"Internal creation of a tunnel end point port failed" },
+	[ERR_ACTION_UNSUPPORTED] = {
+		"Unknown or unsupported flow action received" },
+	[ERR_REMOVE_FLOW_FAILED] = {
+		"Removing flow failed" },
+	[ERR_ACTION_NO_OUTPUT_DEFINED_USE_DEFAULT] = {
+		"No output queue specified. Ignore this flow offload and uses default queue"},
+	[ERR_ACTION_NO_OUTPUT_QUEUE_FOUND] = {
+		"No output queue found"},
+	[ERR_MATCH_UNSUPPORTED_ETHER_TYPE] = {
+		"Unsupported EtherType or rejected caused by offload policy"},
+	[ERR_OUTPUT_INVALID] = {
+		"Destination port specified is invalid or not reachable from this NIC"},
+	[ERR_MATCH_PARTIAL_OFFLOAD_NOT_SUPPORTED] = {
+		"Partial offload is not supported in this configuration"},
+	[ERR_MATCH_CAT_CAM_EXHAUSTED] = {
+		"Match failed because of CAT CAM exhausted"},
+	[ERR_MATCH_KCC_KEY_CLASH] = {
+		"Match failed because of CAT CAM Key clashed with an existing KCC Key"},
+	[ERR_MATCH_CAT_CAM_FAILED] = {
+		"Match failed because of CAT CAM write failed"},
+	[ERR_PARTIAL_FLOW_MARK_TOO_BIG] = {
+		"Partial flow mark too big for device"},
+	[ERR_FLOW_PRIORITY_VALUE_INVALID] = {
+		"Invalid priority value"},
+	[ERR_ACTION_MULTIPLE_PORT_ID_UNSUPPORTED] = {
+		"Multiple port_id actions for one flow is not supported"},
+	[ERR_RSS_TOO_LONG_KEY] = {
+		"Too long hash key for RSS"},
+	[ERR_ACTION_AGE_UNSUPPORTED_GROUP_0] = {
+		"Action AGE is not supported for flow in group 0"},
+	[ERR_MSG_NO_MSG] = {
+		"Unknown error"},
 };
+
+static_assert(RTE_DIM(err_msg) == ERR_MSG_END,
+	"The list of error messages is not fully completed.");
 
 void flow_nic_set_error(enum flow_nic_err_msg_e msg, struct rte_flow_error *error)
 {