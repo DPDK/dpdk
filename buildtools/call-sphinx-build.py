497cf54829c28859482998957d75477ae2b1bc1c
Paul Szczepanek <paul.szczepanek@arm.com>
2024-11-28 23:05:15 +0000

    dts: remove nested html directory for API doc
    To facilitate deploying docs to the website
    and make paths more consistent remove the html
    directory from docs nested in API html directory.
    
    Signed-off-by: Paul Szczepanek <paul.szczepanek@arm.com>
    Reviewed-by: Luca Vizzarro <luca.vizzarro@arm.com>
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>

--- buildtools/call-sphinx-build.py
+++ buildtools/call-sphinx-build.py
@@ -30,8 +30,10 @@ os.makedirs(dst, exist_ok=True)
 
 # run sphinx, putting the html output in a "html" directory
 with open(join(dst, 'sphinx_html.out'), 'w') as out:
-    process = run(sphinx_cmd + ['-b', 'html', src, join(dst, 'html')],
-                  stdout=out)
+    # don't append html dir if dst is already nested in a html dir
+    last_two_dirs = os.path.join(*os.path.normpath(dst).split(os.path.sep)[-2:])
+    html_dst = dst if 'html' in last_two_dirs else join(dst, 'html')
+    process = run(sphinx_cmd + ['-b', 'html', src, html_dst], stdout=out)
 
 # create a gcc format .d file giving all the dependencies of this doc build
 with open(join(dst, '.html.d'), 'w') as d:
@@ -40,7 +42,7 @@ with open(join(dst, '.html.d'), 'w') as d:
 # copy custom CSS file
 css = 'custom.css'
 src_css = join(src, css)
-dst_css = join(dst, 'html', '_static', 'css', css)
+dst_css = join(html_dst, '_static', 'css', css)
 if not os.path.exists(dst_css) or not filecmp.cmp(src_css, dst_css):
     os.makedirs(os.path.dirname(dst_css), exist_ok=True)
     shutil.copyfile(src_css, dst_css)
