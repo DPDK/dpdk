70005ea51c0dc800c75f26ed4bcd09f48c76213f
Stephen Hemminger <stephen@networkplumber.org>
2025-01-25 10:36:44 -0800

    ring: annotate allocation function
    Use function attributes to catch cases where ring is allocated
    but not freed correctly.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@huawei.com>

--- lib/ring/rte_ring.h
+++ lib/ring/rte_ring.h
@@ -34,6 +34,7 @@
  * for more information.
  */
 
+#include <rte_common.h>
 #include <rte_ring_core.h>
 #include <rte_ring_elem.h>
 
@@ -120,6 +121,15 @@ int rte_ring_init(struct rte_ring *r, const char *name, unsigned int count,
 	unsigned int flags);
 
 /**
+ * De-allocate all memory used by the ring.
+ *
+ * @param r
+ *   Ring to free.
+ *   If NULL then, the function does nothing.
+ */
+void rte_ring_free(struct rte_ring *r);
+
+/**
  * Create a new ring named *name* in memory.
  *
  * This function uses ``memzone_reserve()`` to allocate memory. Then it
@@ -183,16 +193,8 @@ int rte_ring_init(struct rte_ring *r, const char *name, unsigned int count,
  *    - ENOMEM - no appropriate memory area found in which to create memzone
  */
 struct rte_ring *rte_ring_create(const char *name, unsigned int count,
-				 int socket_id, unsigned int flags);
-
-/**
- * De-allocate all memory used by the ring.
- *
- * @param r
- *   Ring to free.
- *   If NULL then, the function does nothing.
- */
-void rte_ring_free(struct rte_ring *r);
+				 int socket_id, unsigned int flags)
+	__rte_malloc __rte_dealloc(rte_ring_free, 1);
 
 /**
  * Dump the status of the ring to a file.
