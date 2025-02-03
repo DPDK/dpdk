615e91b70678cc9dda294426250c793acd136ba1
David Marchand <david.marchand@redhat.com>
2025-02-03 13:33:19 +0100

    cfgfile: fix section count with no name
    [ upstream commit 02bce2f1e938b409bb6f85391510a2a33ecc1443 ]
    
    Passing a NULL to strncmp is incorrect.
    
     + ------------------------------------------------------- +
     + Test Suite : Test Cfgfile Unit Test Suite
     + ------------------------------------------------------- +
    ../lib/cfgfile/rte_cfgfile.c:475:7: runtime error: null pointer passed as
    	argument 2, which is declared to never be null
    
    On the other hand, it seems the intent was to count all sections, so
    skip the whole loop and section name comparisons.
    
    Fixes: c54e7234bc9e ("test/cfgfile: add basic unit tests")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Acked-by: Cristian Dumitrescu <cristian.dumitrescu@intel.com>
    Acked-by: Chengwen Feng <fengchengwen@huawei.com>

--- lib/cfgfile/rte_cfgfile.c
+++ lib/cfgfile/rte_cfgfile.c
@@ -467,10 +467,14 @@ int rte_cfgfile_close(struct rte_cfgfile *cfg)
 
 int
 rte_cfgfile_num_sections(struct rte_cfgfile *cfg, const char *sectionname,
-size_t length)
+	size_t length)
 {
-	int i;
 	int num_sections = 0;
+	int i;
+
+	if (sectionname == NULL)
+		return cfg->num_sections;
+
 	for (i = 0; i < cfg->num_sections; i++) {
 		if (strncmp(cfg->sections[i].name, sectionname, length) == 0)
 			num_sections++;
