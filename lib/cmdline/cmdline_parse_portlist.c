f3a07a33d7b3a0b153f7f5b60c13bebede9a9104
David Marchand <david.marchand@redhat.com>
2025-02-03 13:50:48 +0100

    cmdline: fix highest bit port list parsing
    pl->map is a uint32_t.
    
    Caught by UBSan:
    
    ../lib/cmdline/cmdline_parse_portlist.c:27:17: runtime error:
    	left shift of 1 by 31 places cannot be represented in type 'int'
    SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior
    	../lib/cmdline/cmdline_parse_portlist.c:27:17 in
    
    Fixes: af75078fece3 ("first public release")
    Cc: stable@dpdk.org
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Reviewed-by: Marat Khalili <marat.khalili@huawei.com>
    Acked-by: Chengwen Feng <fengchengwen@huawei.com>

--- lib/cmdline/cmdline_parse_portlist.c
+++ lib/cmdline/cmdline_parse_portlist.c
@@ -10,7 +10,9 @@
 #include <errno.h>
 
 #include <eal_export.h>
+#include <rte_bitops.h>
 #include <rte_string_fns.h>
+
 #include "cmdline_parse.h"
 #include "cmdline_parse_portlist.h"
 
@@ -26,7 +28,8 @@ static void
 parse_set_list(cmdline_portlist_t *pl, size_t low, size_t high)
 {
 	do {
-		pl->map |= (1 << low++);
+		pl->map |= RTE_BIT32(low);
+		low++;
 	} while (low <= high);
 }
 
