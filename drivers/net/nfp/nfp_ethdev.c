DPDK/dpdk

commit 95f978efab421de51e55d2c3027f1fe2a0823589

net/nfp: enable multiple PF in application firmware
For backward compatibility concern, the new application firmware
is designed to support both single PF scenario and multiple PF scenario.
Thus driver should inform application firmware which setup current
is. This should be done as early as possible since the setup may
affect some configurations exposed by firmware.

Signed-off-by: Peng Zhang <peng.zhang@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Long Wu <long.wu@corigine.com>

drivers/net/nfp/nfp_ethdev.c
@@ -938,6 +938,50 @@ nfp_check_multi_pf_from_nsp(struct rte_pci_device *pci_dev,
 }
 
 static int
+nfp_enable_multi_pf(struct nfp_pf_dev *pf_dev)
+{
+	int err = 0;
+	uint64_t tx_base;
+	uint8_t *ctrl_bar;
+	struct nfp_hw *hw;
+	uint32_t cap_extend;
+	struct nfp_net_hw net_hw;
+	struct nfp_cpp_area *area;
+	char name[RTE_ETH_NAME_MAX_LEN];
+
+	memset(&net_hw, 0, sizeof(struct nfp_net_hw));
+
+	/* Map the symbol table */
+	snprintf(name, sizeof(name), "_pf%u_net_bar0",
+			pf_dev->multi_pf.function_id);
+	ctrl_bar = nfp_rtsym_map(pf_dev->sym_tbl, name, NFP_NET_CFG_BAR_SZ,
+			&area);
+	if (ctrl_bar == NULL) {
+		PMD_INIT_LOG(ERR, "Failed to find data vNIC memory symbol");
+		return -ENODEV;
+	}
+
+	hw = &net_hw.super;
+	hw->ctrl_bar = ctrl_bar;
+
+	cap_extend = nn_cfg_readl(hw, NFP_NET_CFG_CAP_WORD1);
+	if ((cap_extend & NFP_NET_CFG_CTRL_MULTI_PF) == 0) {
+		PMD_INIT_LOG(ERR, "Loaded firmware doesn't support multiple PF");
+		err = -EINVAL;
+		goto end;
+	}
+
+	tx_base = nn_cfg_readl(hw, NFP_NET_CFG_START_TXQ);
+	net_hw.tx_bar = pf_dev->qc_bar + tx_base * NFP_QCP_QUEUE_ADDR_SZ;
+	nfp_net_cfg_queue_setup(&net_hw);
+	rte_spinlock_init(&hw->reconfig_lock);
+	nfp_ext_reconfig(&net_hw.super, NFP_NET_CFG_CTRL_MULTI_PF, NFP_NET_CFG_UPDATE_GEN);
+end:
+	nfp_cpp_area_release_free(area);
+	return err;
+}
+
+static int
 nfp_init_app_fw_nic(struct nfp_pf_dev *pf_dev,
 		const struct nfp_dev_info *dev_info)
 {
@@ -1224,6 +1268,12 @@ nfp_pf_init(struct rte_pci_device *pci_dev)
 	 */
 	switch (pf_dev->app_fw_id) {
 	case NFP_APP_FW_CORE_NIC:
+		if (pf_dev->multi_pf.enabled) {
+			ret = nfp_enable_multi_pf(pf_dev);
+			if (ret != 0)
+				goto hwqueues_cleanup;
+		}
+
 		PMD_INIT_LOG(INFO, "Initializing coreNIC");
 		ret = nfp_init_app_fw_nic(pf_dev, dev_info);
 		if (ret != 0) {

drivers/common/nfp/nfp_common_ctrl.h
@@ -222,6 +222,7 @@ struct nfp_net_fw_ver {
 #define NFP_NET_CFG_CTRL_MCAST_FILTER     (0x1 << 2) /**< Multicast Filter */
 #define NFP_NET_CFG_CTRL_IPSEC_SM_LOOKUP  (0x1 << 3) /**< SA short match lookup */
 #define NFP_NET_CFG_CTRL_IPSEC_LM_LOOKUP  (0x1 << 4) /**< SA long match lookup */
+#define NFP_NET_CFG_CTRL_MULTI_PF         (0x1 << 5)
 #define NFP_NET_CFG_CTRL_IN_ORDER         (0x1 << 11) /**< Virtio in-order flag */
 
 #define NFP_NET_CFG_CAP_WORD1           0x00a4

commit 02c056aaac1ba1bd16658eef19cb78457d556041

net/nfp: fix multi-PF control flag
In the original logic of 'nfp_net_start()', we don't set the
'NFP_NET_CFG_CTRL_MULTI_PF' flag when using multi-pf firmware.

Fix it by adding the missing logic.

Fixes: 95f978efab42 ("net/nfp: enable multiple PF in application firmware")
Cc: stable@dpdk.org

Signed-off-by: Peng Zhang <peng.zhang@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>

drivers/net/nfp/nfp_ethdev.c
@@ -472,6 +472,9 @@ nfp_net_start(struct rte_eth_dev *dev)
 	if ((cap_extend & NFP_NET_CFG_CTRL_FLOW_STEER) != 0)
 		ctrl_extend |= NFP_NET_CFG_CTRL_FLOW_STEER;
 
+	if ((cap_extend & NFP_NET_CFG_CTRL_MULTI_PF) != 0 && pf_dev->multi_pf.enabled)
+		ctrl_extend |= NFP_NET_CFG_CTRL_MULTI_PF;
+
 	update = NFP_NET_CFG_UPDATE_GEN;
 	if (nfp_ext_reconfig(hw, ctrl_extend, update) != 0)
 		return -EIO;