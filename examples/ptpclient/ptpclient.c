3eed0aca9057c3d9d5b2e122093be7a789dbc23e
Stephen Hemminger <stephen@networkplumber.org>
2025-02-05 08:23:10 -0800

    examples/ptpclient: fix message parsing
    [ upstream commit 19630bd0d735badb06143086d4f1c50d726b7bad ]
    
    Calling memcmp on same structure will always be true.
    Replace with same conditional used elsewhere.
    
    Link: https://pvs-studio.com/en/blog/posts/cpp/1183/
    
    Fixes: ab129e9065a5 ("examples/ptpclient: add minimal PTP client")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- examples/ptpclient/ptpclient.c
+++ examples/ptpclient/ptpclient.c
@@ -369,7 +369,7 @@ parse_sync(struct ptpv2_time_receiver_ordinary *ptp_data, uint16_t rx_tstamp_idx
 		ptp_data->ptpset = 1;
 	}
 
-	if (memcmp(&ptp_hdr->source_port_id.clock_id,
+	if (memcmp(&ptp_data->transmitter_clock_id,
 			&ptp_hdr->source_port_id.clock_id,
 			sizeof(struct clock_id)) == 0) {
 
