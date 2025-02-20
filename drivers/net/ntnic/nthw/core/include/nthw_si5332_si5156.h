b02df6792d547e3e5b6c6bf6276e7095e24f4b41
Danylo Vodopianov <dvo-plv@napatech.com>
2025-02-20 23:03:30 +0100

    net/ntnic: init line loopback
    Implement PHY host loopback configuration for Agilex FPGA
    
    Signed-off-by: Danylo Vodopianov <dvo-plv@napatech.com>

--- drivers/net/ntnic/nthw/core/include/nthw_si5332_si5156.h
+++ drivers/net/ntnic/nthw/core/include/nthw_si5332_si5156.h
@@ -6,13 +6,19 @@
 #ifndef __NTHW_SI5332_SI5156_H__
 #define __NTHW_SI5332_SI5156_H__
 
+#include "nthw_i2cm.h"
 /*
  * PCA9849 I2c mux class
  */
 
 struct nthw_pca9849 {
+	nthw_i2cm_t *mp_nt_i2cm;
+	uint8_t m_current_channel;
+	uint8_t m_dev_address;
 };
 
 typedef struct nthw_pca9849 nthw_pca9849_t;
+
+int nthw_pca9849_set_channel(nthw_pca9849_t *p, uint8_t channel);
 
 #endif	/* __NTHW_SI5332_SI5156_H__ */
