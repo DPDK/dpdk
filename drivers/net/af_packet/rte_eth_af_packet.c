ea3f9bfe146034a2e2a6564d17fcfe2cc4683ffb
Shaokai Zhang <felix.zhang@jaguarmicro.com>
2025-03-21 11:05:51 +0800

    net/af_packet: register supported param fanout mode
    Although af_packet PMD supports setting the fanout algorithm, it is not
    explicitly declared during registration. Add fanout parameter registration
    to facilitate users in quickly obtaining supported fanout algorithms.
    
    Additionally, add configuration parameter printing during initialization.
    
    Adjust unnecessary printing to debug level.
    
    Fixes: d3bc77ab05a4 ("net/af_packet: allow changing fanout mode")
    Cc: stable@dpdk.org
    
    Signed-off-by: Shaokai Zhang <felix.zhang@jaguarmicro.com>
    Reviewed-by: Joey Xing <joey.xing@jaguarmicro.com>

--- drivers/net/af_packet/rte_eth_af_packet.c
+++ drivers/net/af_packet/rte_eth_af_packet.c
@@ -1167,11 +1167,16 @@ rte_eth_from_packet(struct rte_vdev_device *dev,
 		return -1;
 	}
 
-	PMD_LOG(INFO, "%s: AF_PACKET MMAP parameters:", name);
-	PMD_LOG(INFO, "%s:\tblock size %d", name, blocksize);
-	PMD_LOG(INFO, "%s:\tblock count %d", name, blockcount);
-	PMD_LOG(INFO, "%s:\tframe size %d", name, framesize);
-	PMD_LOG(INFO, "%s:\tframe count %d", name, framecount);
+	PMD_LOG(DEBUG, "%s: AF_PACKET MMAP parameters:", name);
+	PMD_LOG(DEBUG, "%s:\tblock size %d", name, blocksize);
+	PMD_LOG(DEBUG, "%s:\tblock count %d", name, blockcount);
+	PMD_LOG(DEBUG, "%s:\tframe size %d", name, framesize);
+	PMD_LOG(DEBUG, "%s:\tframe count %d", name, framecount);
+	PMD_LOG(DEBUG, "%s:\tqdisc bypass %d", name, qdisc_bypass);
+	if (fanout_mode)
+		PMD_LOG(DEBUG, "%s:\tfanout mode %s", name, fanout_mode);
+	else
+		PMD_LOG(DEBUG, "%s:\tfanout mode %s", name, "default PACKET_FANOUT_HASH");
 
 	if (rte_pmd_init_internals(dev, *sockfd, qpairs,
 				   blocksize, blockcount,
@@ -1274,4 +1279,5 @@ RTE_PMD_REGISTER_PARAM_STRING(net_af_packet,
 	"blocksz=<int> "
 	"framesz=<int> "
 	"framecnt=<int> "
-	"qdisc_bypass=<0|1>");
+	"qdisc_bypass=<0|1> "
+	"fanout_mode=<hash|lb|cpu|rollover|rnd|qm>");
