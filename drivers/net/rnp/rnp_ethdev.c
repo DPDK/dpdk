7e805a027f5d226024b19974ca0fd06956e91254
Wenbo Cao <caowenbo@mucse.com>
2025-03-28 05:14:24 +0000

    net/rnp: support queue start and stop
    support rx/tx queue stop/start,for rx queue stop
    need to reset a queue,must stop all rx queue
    during reset this queue.
    
    Signed-off-by: Wenbo Cao <caowenbo@mucse.com>
    Reviewed-by: Stephen Hemminger <stephen@networkplumber.org>

--- drivers/net/rnp/rnp_ethdev.c
+++ drivers/net/rnp/rnp_ethdev.c
@@ -249,8 +249,12 @@ static const struct eth_dev_ops rnp_eth_dev_ops = {
 
 	.rx_queue_setup               = rnp_rx_queue_setup,
 	.rx_queue_release             = rnp_dev_rx_queue_release,
+	.rx_queue_stop                = rnp_rx_queue_stop,
+	.rx_queue_start               = rnp_rx_queue_start,
 	.tx_queue_setup               = rnp_tx_queue_setup,
 	.tx_queue_release             = rnp_dev_tx_queue_release,
+	.tx_queue_stop                = rnp_tx_queue_stop,
+	.tx_queue_start               = rnp_tx_queue_start,
 };
 
 static void
