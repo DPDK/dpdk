DPDK/dpdk

commit 9c3a123f9e08ec135cbe7120be9988b5e32bb561

net/zxdh: optimize msg processing ops and modify some issues
In order to accommodate previous firmware versions,
this patch has optimized the message processing ops,
and modify some issues.

Signed-off-by: Bingbin Chen <chen.bingbin@zte.com.cn>

drivers/net/zxdh/zxdh_common.h
@@ -9,6 +9,7 @@
 #include <rte_ethdev.h>
 
 #include "zxdh_ethdev.h"
+#include "zxdh_logs.h"
 
 #define ZXDH_VF_LOCK_REG               0x90
 #define ZXDH_VF_LOCK_ENABLE_MASK       0x1
@@ -19,6 +20,105 @@ struct zxdh_res_para {
 	uint16_t pcie_id;
 	uint16_t src_type; /* refer to BAR_DRIVER_TYPE */
 };
+
+static inline size_t
+zxdh_get_value(uint32_t fld_sz, uint8_t *addr) {
+	size_t result = 0;
+	switch (fld_sz) {
+	case 1:
+		result = *((uint8_t *)addr);
+		break;
+	case 2:
+		result = *((uint16_t *)addr);
+		break;
+	case 4:
+		result = *((uint32_t *)addr);
+		break;
+	case 8:
+		result = *((uint64_t *)addr);
+		break;
+	default:
+		PMD_MSG_LOG(ERR, "unreachable field size %u", fld_sz);
+		break;
+	}
+	return result;
+}
+
+static inline void
+zxdh_set_value(uint32_t fld_sz, uint8_t *addr, size_t value) {
+	switch (fld_sz) {
+	case 1:
+		*(uint8_t *)addr = (uint8_t)value;
+		break;
+	case 2:
+		*(uint16_t *)addr = (uint16_t)value;
+		break;
+	case 4:
+		*(uint32_t *)addr = (uint32_t)value;
+		break;
+	case 8:
+		*(uint64_t *)addr = (uint64_t)value;
+		break;
+	default:
+		PMD_MSG_LOG(ERR, "unreachable field size %u", fld_sz);
+		break;
+	}
+}
+
+#define __zxdh_nullp(typ) ((struct zxdh_ifc_##typ##_bits *)0)
+#define __zxdh_bit_sz(typ, fld) sizeof(__zxdh_nullp(typ)->fld)
+#define __zxdh_bit_off(typ, fld) ((unsigned int)(uintptr_t) \
+				  (&(__zxdh_nullp(typ)->fld)))
+#define __zxdh_dw_bit_off(typ, fld) (32 - __zxdh_bit_sz(typ, fld) - \
+				    (__zxdh_bit_off(typ, fld) & 0x1f))
+#define __zxdh_dw_off(typ, fld) (__zxdh_bit_off(typ, fld) / 32)
+#define __zxdh_64_off(typ, fld) (__zxdh_bit_off(typ, fld) / 64)
+#define __zxdh_dw_mask(typ, fld) (__zxdh_mask(typ, fld) << \
+				  __zxdh_dw_bit_off(typ, fld))
+#define __zxdh_mask(typ, fld) ((uint32_t)((1ull << __zxdh_bit_sz(typ, fld)) - 1))
+#define __zxdh_16_off(typ, fld) (__zxdh_bit_off(typ, fld) / 16)
+#define __zxdh_16_bit_off(typ, fld) (16 - __zxdh_bit_sz(typ, fld) - \
+				    (__zxdh_bit_off(typ, fld) & 0xf))
+#define __zxdh_mask16(typ, fld) ((uint16_t)((1ull << __zxdh_bit_sz(typ, fld)) - 1))
+#define __zxdh_16_mask(typ, fld) (__zxdh_mask16(typ, fld) << \
+				  __zxdh_16_bit_off(typ, fld))
+#define ZXDH_ST_SZ_BYTES(typ) (sizeof(struct zxdh_ifc_##typ##_bits) / 8)
+#define ZXDH_ST_SZ_DW(typ) (sizeof(struct zxdh_ifc_##typ##_bits) / 32)
+#define ZXDH_BYTE_OFF(typ, fld) (__zxdh_bit_off(typ, fld) / 8)
+#define ZXDH_ADDR_OF(typ, p, fld) ((uint8_t *)(p) + ZXDH_BYTE_OFF(typ, fld))
+
+#define BUILD_BUG_ON(condition) do { \
+	if (condition) \
+		__builtin_unreachable(); \
+	} while (0)
+
+#define ZXDH_SET(typ, p, fld, v) do { \
+	BUILD_BUG_ON(__zxdh_bit_sz(typ, fld) % 8); \
+	uint32_t fld_sz = __zxdh_bit_sz(typ, fld) / 8; \
+	uint8_t *addr = ZXDH_ADDR_OF(typ, p, fld); \
+	zxdh_set_value(fld_sz, addr, v); \
+} while (0)
+
+#define ZXDH_GET(typ, p, fld) ({ \
+	BUILD_BUG_ON(__zxdh_bit_sz(typ, fld) % 8); \
+	uint32_t fld_sz = __zxdh_bit_sz(typ, fld) / 8; \
+	uint8_t *addr = ZXDH_ADDR_OF(typ, p, fld); \
+	zxdh_get_value(fld_sz, addr); \
+})
+
+#define ZXDH_SET_ARRAY(typ, p, fld, index, value, type) \
+	do { \
+		type *addr = (type *)((uint8_t *)ZXDH_ADDR_OF(typ, p, fld) + \
+		(index) * sizeof(type)); \
+		*addr = (type)(value); \
+	} while (0)
+
+#define ZXDH_GET_ARRAY(typ, p, fld, index, type) \
+	({ \
+		type *addr = (type *)((uint8_t *)ZXDH_ADDR_OF(typ, p, fld) + \
+		(index) * sizeof(type)); \
+		*addr; \
+	})
 
 int32_t zxdh_phyport_get(struct rte_eth_dev *dev, uint8_t *phyport);
 int32_t zxdh_panelid_get(struct rte_eth_dev *dev, uint8_t *pannelid);

drivers/net/zxdh/zxdh_ethdev.c
@@ -1250,10 +1250,6 @@ zxdh_set_rxtx_funcs(struct rte_eth_dev *eth_dev)
 {
 	struct zxdh_hw *hw = eth_dev->data->dev_private;
 
-	if (!zxdh_pci_packed_queue(hw)) {
-		PMD_DRV_LOG(ERR, "port %u not support packed queue", eth_dev->data->port_id);
-		return -1;
-	}
 	if (!zxdh_pci_with_feature(hw, ZXDH_NET_F_MRG_RXBUF)) {
 		PMD_DRV_LOG(ERR, "port %u not support rx mergeable", eth_dev->data->port_id);
 		return -1;
@@ -1498,6 +1494,8 @@ zxdh_dtb_dump_res_init(struct zxdh_hw *hw, ZXDH_DEV_INIT_CTRL_T *dpp_ctrl)
 
 	struct zxdh_dtb_bulk_dump_info dtb_dump_baseres[] = {
 		{"sdt_vport_att_table", 4 * 1024 * 1024, ZXDH_SDT_VPORT_ATT_TABLE, NULL},
+		{"sdt_vlan_att_table", 4 * 1024 * 1024, ZXDH_SDT_VLAN_ATT_TABLE, NULL},
+		{"sdt_rss_table", 4 * 1024 * 1024, ZXDH_SDT_RSS_ATT_TABLE, NULL},
 		{"sdt_l2_entry_table0", 5 * 1024 * 1024, ZXDH_SDT_L2_ENTRY_TABLE0, NULL},
 		{"sdt_l2_entry_table1", 5 * 1024 * 1024, ZXDH_SDT_L2_ENTRY_TABLE1, NULL},
 		{"sdt_l2_entry_table2", 5 * 1024 * 1024, ZXDH_SDT_L2_ENTRY_TABLE2, NULL},
@@ -1514,7 +1512,7 @@ zxdh_dtb_dump_res_init(struct zxdh_hw *hw, ZXDH_DEV_INIT_CTRL_T *dpp_ctrl)
 	for (i = 0; i < (int)RTE_DIM(dtb_dump_baseres); i++) {
 		struct zxdh_dtb_bulk_dump_info *p = dtb_dump_baseres + i;
 		char buf[ZXDH_MAX_NAME_LEN] = {0};
-
+		snprintf(buf, sizeof(buf), "%s_%x", p->mz_name, hw->dev_id);
 		p->mz_name = buf;
 
 		const struct rte_memzone *generic_dump_mz =
@@ -1544,6 +1542,7 @@ zxdh_np_dtb_res_init(struct rte_eth_dev *dev)
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_bar_offset_params param = {0};
 	struct zxdh_bar_offset_res res = {0};
+	char buf[ZXDH_MAX_NAME_LEN] = {0};
 	struct zxdh_dtb_shared_data *dtb_data = &hw->dev_sd->dtb_sd;
 	int ret = 0;
 
@@ -1569,7 +1568,7 @@ zxdh_np_dtb_res_init(struct rte_eth_dev *dev)
 	dpp_ctrl->vport = hw->vport.vport;
 	dpp_ctrl->vector = ZXDH_MSIX_INTR_DTB_VEC;
 	strlcpy(dpp_ctrl->port_name, dev->device->name, sizeof(dpp_ctrl->port_name));
-	dpp_ctrl->pcie_vir_addr = (uint32_t)hw->bar_addr[0];
+	dpp_ctrl->pcie_vir_addr = hw->bar_addr[0];
 
 	param.pcie_id = hw->pcie_id;
 	param.virt_addr = hw->bar_addr[0] + ZXDH_CTRLCH_OFFSET;
@@ -1584,7 +1583,8 @@ zxdh_np_dtb_res_init(struct rte_eth_dev *dev)
 	dpp_ctrl->np_bar_offset = res.bar_offset;
 
 	if (!dtb_data->dtb_table_conf_mz) {
-		const struct rte_memzone *conf_mz = rte_memzone_reserve_aligned("zxdh_dtb_table_conf_mz",
+		snprintf(buf, sizeof(buf), "%s_%x", "zxdh_dtb_table_conf_mz", hw->dev_id);
+		const struct rte_memzone *conf_mz = rte_memzone_reserve_aligned(buf,
 				ZXDH_DTB_TABLE_CONF_SIZE, SOCKET_ID_ANY, 0, RTE_CACHE_LINE_SIZE);
 
 		if (conf_mz == NULL) {
@@ -1600,7 +1600,9 @@ zxdh_np_dtb_res_init(struct rte_eth_dev *dev)
 	}
 
 	if (!dtb_data->dtb_table_dump_mz) {
-		const struct rte_memzone *dump_mz = rte_memzone_reserve_aligned("zxdh_dtb_table_dump_mz",
+		memset(buf, '\0', sizeof(buf));
+		snprintf(buf, sizeof(buf), "%s_%x", "zxdh_dtb_table_dump_mz", hw->dev_id);
+		const struct rte_memzone *dump_mz = rte_memzone_reserve_aligned(buf,
 				ZXDH_DTB_TABLE_DUMP_SIZE, SOCKET_ID_ANY, 0, RTE_CACHE_LINE_SIZE);
 
 		if (dump_mz == NULL) {

drivers/net/zxdh/zxdh_ethdev_ops.c
@@ -15,6 +15,7 @@
 #include "zxdh_np.h"
 #include "zxdh_queue.h"
 #include "zxdh_mtr.h"
+#include "zxdh_common.h"
 
 #define ZXDH_VLAN_FILTER_GROUPS       64
 #define ZXDH_INVALID_LOGIC_QID        0xFFFFU
@@ -278,9 +279,11 @@ zxdh_link_info_get(struct rte_eth_dev *dev, struct rte_eth_link *link)
 {
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
-	struct zxdh_msg_reply_info reply_info = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
 	uint16_t status = 0;
 	int32_t ret = 0;
+	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
+	void *link_msg_addr = ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, link_msg);
 
 	if (zxdh_pci_with_feature(hw, ZXDH_NET_F_STATUS))
 		zxdh_pci_read_dev_config(hw, offsetof(struct zxdh_net_config, status),
@@ -295,17 +298,18 @@ zxdh_link_info_get(struct rte_eth_dev *dev, struct rte_eth_link *link)
 		zxdh_agent_msg_build(hw, ZXDH_MAC_LINK_GET, &msg_info);
 
 		ret = zxdh_send_msg_to_riscv(dev, &msg_info, sizeof(struct zxdh_msg_info),
-				&reply_info, sizeof(struct zxdh_msg_reply_info),
+				zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info),
 				ZXDH_BAR_MODULE_MAC);
 		if (ret) {
 			PMD_DRV_LOG(ERR, "Failed to send msg: port 0x%x msg type %d",
 					hw->vport.vport, ZXDH_MAC_LINK_GET);
 			return -1;
 		}
-		link->link_speed = reply_info.reply_body.link_msg.speed;
-		link->link_autoneg = reply_info.reply_body.link_msg.autoneg;
-		hw->speed_mode = reply_info.reply_body.link_msg.speed_modes;
-		if ((reply_info.reply_body.link_msg.duplex & RTE_ETH_LINK_FULL_DUPLEX) ==
+
+		link->link_speed = ZXDH_GET(link_info_msg, link_msg_addr, speed);
+		link->link_autoneg = ZXDH_GET(link_info_msg, link_msg_addr, autoneg);
+		hw->speed_mode = ZXDH_GET(link_info_msg, link_msg_addr, speed_modes);
+		if ((ZXDH_GET(link_info_msg, link_msg_addr, duplex) & RTE_ETH_LINK_FULL_DUPLEX) ==
 				RTE_ETH_LINK_FULL_DUPLEX)
 			link->link_duplex = RTE_ETH_LINK_FULL_DUPLEX;
 		else
@@ -433,7 +437,7 @@ zxdh_dev_mac_addr_set(struct rte_eth_dev *dev, struct rte_ether_addr *addr)
 		ret = zxdh_del_mac_table(hw, hw->vport.vport, old_addr,
 			hw->hash_search_index, 0, 0);
 		if (ret) {
-			PMD_DRV_LOG(ERR, "mac_addr_add failed, code:%d", ret);
+			PMD_DRV_LOG(ERR, "mac_addr_del failed, code:%d", ret);
 			return ret;
 		}
 		hw->uc_num--;
@@ -467,6 +471,8 @@ zxdh_dev_mac_addr_set(struct rte_eth_dev *dev, struct rte_ether_addr *addr)
 		hw->uc_num--;
 	}
 	rte_ether_addr_copy(addr, (struct rte_ether_addr *)hw->mac_addr);
+	zxdh_pci_write_dev_config(hw, offsetof(struct zxdh_net_config, mac),
+								&hw->mac_addr, RTE_ETHER_ADDR_LEN);
 	return ret;
 }
 
@@ -566,7 +572,7 @@ zxdh_dev_mac_addr_add(struct rte_eth_dev *dev, struct rte_ether_addr *mac_addr,
 
 void zxdh_dev_mac_addr_remove(struct rte_eth_dev *dev, uint32_t index)
 {
-	struct zxdh_hw *hw	= dev->data->dev_private;
+	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
 	struct rte_ether_addr *mac_addr = &dev->data->mac_addrs[index];
 	uint16_t ret = 0;
@@ -1072,7 +1078,9 @@ zxdh_dev_rss_reta_query(struct rte_eth_dev *dev,
 {
 	struct zxdh_hw *hw = (struct zxdh_hw *)dev->data->dev_private;
 	struct zxdh_msg_info msg = {0};
-	struct zxdh_msg_reply_info reply_msg = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
+	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
+	void *rss_reta_msg_addr = ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, rss_reta_msg);
 	uint16_t idx;
 	uint16_t i;
 	int ret = 0;
@@ -1094,21 +1102,21 @@ zxdh_dev_rss_reta_query(struct rte_eth_dev *dev,
 	zxdh_msg_head_build(hw, ZXDH_RSS_RETA_GET, &msg);
 
 	if (hw->is_pf) {
-		ret = zxdh_rss_table_get(hw, hw->vport.vport, &reply_msg.reply_body.rss_reta);
+		ret = zxdh_rss_table_get(hw, hw->vport.vport, rss_reta_msg_addr);
 		if (ret) {
 			PMD_DRV_LOG(ERR, "rss reta table set failed");
 			return -EINVAL;
 		}
 	} else {
 		ret = zxdh_vf_send_msg_to_pf(dev, &msg, sizeof(struct zxdh_msg_info),
-					&reply_msg, sizeof(struct zxdh_msg_reply_info));
+					zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info));
 		if (ret) {
 			PMD_DRV_LOG(ERR, "vf rss reta table get failed");
 			return -EINVAL;
 		}
 	}
 
-	struct zxdh_rss_reta *reta_table = &reply_msg.reply_body.rss_reta;
+	struct zxdh_rss_reta *reta_table = rss_reta_msg_addr;
 
 	for (idx = 0, i = 0; i < reta_size; ++i) {
 		idx = i / RTE_ETH_RETA_GROUP_SIZE;
@@ -1232,10 +1240,13 @@ zxdh_rss_hash_conf_get(struct rte_eth_dev *dev, struct rte_eth_rss_conf *rss_con
 	struct zxdh_hw *hw = (struct zxdh_hw *)dev->data->dev_private;
 	struct rte_eth_rss_conf *old_rss_conf = &dev->data->dev_conf.rx_adv_conf.rss_conf;
 	struct zxdh_msg_info msg = {0};
-	struct zxdh_msg_reply_info reply_msg = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
+	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
+	void *rss_hf_msg_addr = ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, rss_hf_msg);
 	struct zxdh_port_attr_table port_attr = {0};
-	int ret;
+	uint32_t rss_hf;
 	uint32_t hw_hf;
+	int ret;
 
 	if (rss_conf == NULL) {
 		PMD_DRV_LOG(ERR, "rss conf is NULL");
@@ -1252,16 +1263,17 @@ zxdh_rss_hash_conf_get(struct rte_eth_dev *dev, struct rte_eth_rss_conf *rss_con
 			PMD_DRV_LOG(ERR, "rss hash factor set failed");
 			return -EINVAL;
 		}
-		reply_msg.reply_body.rss_hf.rss_hf = port_attr.rss_hash_factor;
+		ZXDH_SET(rss_hf, rss_hf_msg_addr, rss_hf, port_attr.rss_hash_factor);
 	} else {
 		ret = zxdh_vf_send_msg_to_pf(dev, &msg, sizeof(struct zxdh_msg_info),
-				&reply_msg, sizeof(struct zxdh_msg_reply_info));
+				zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info));
 		if (ret) {
 			PMD_DRV_LOG(ERR, "rss hash factor set failed");
 			return -EINVAL;
 		}
 	}
-	rss_conf->rss_hf = zxdh_rss_hf_to_eth(reply_msg.reply_body.rss_hf.rss_hf);
+	rss_hf = ZXDH_GET(rss_hf, rss_hf_msg_addr, rss_hf);
+	rss_conf->rss_hf = zxdh_rss_hf_to_eth(rss_hf);
 
 	return 0;
 }
@@ -1382,7 +1394,8 @@ zxdh_hw_vqm_stats_get(struct rte_eth_dev *dev, enum zxdh_agent_msg_type opcode,
 {
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
-	struct zxdh_msg_reply_info reply_info = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
+	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
 	enum ZXDH_BAR_MODULE_ID module_id;
 	int ret = 0;
 
@@ -1404,14 +1417,15 @@ zxdh_hw_vqm_stats_get(struct rte_eth_dev *dev, enum zxdh_agent_msg_type opcode,
 	zxdh_agent_msg_build(hw, opcode, &msg_info);
 
 	ret = zxdh_send_msg_to_riscv(dev, &msg_info, sizeof(struct zxdh_msg_info),
-				&reply_info, sizeof(struct zxdh_msg_reply_info), module_id);
+				zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info), module_id);
 	if (ret) {
 		PMD_DRV_LOG(ERR, "Failed to get hw stats");
 		return -1;
 	}
-	struct zxdh_msg_reply_body *reply_body = &reply_info.reply_body;
 
-	memcpy(hw_stats, &reply_body->vqm_stats, sizeof(struct zxdh_hw_vqm_stats));
+	void *hw_vqm_stats = ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, vqm_stats);
+	memcpy(hw_stats, hw_vqm_stats, sizeof(struct zxdh_hw_vqm_stats));
+
 	return 0;
 }
 
@@ -1578,7 +1592,9 @@ zxdh_hw_np_stats_get(struct rte_eth_dev *dev, struct zxdh_hw_np_stats *np_stats)
 {
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
-	struct zxdh_msg_reply_info reply_info = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
+	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
+	void *hw_stas_addr = ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, hw_stats);
 	int ret = 0;
 
 	if (hw->is_pf) {
@@ -1590,13 +1606,13 @@ zxdh_hw_np_stats_get(struct rte_eth_dev *dev, struct zxdh_hw_np_stats *np_stats)
 	} else {
 		zxdh_msg_head_build(hw, ZXDH_GET_NP_STATS, &msg_info);
 		ret = zxdh_vf_send_msg_to_pf(dev, &msg_info, sizeof(struct zxdh_msg_info),
-					&reply_info, sizeof(struct zxdh_msg_reply_info));
+					zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info));
 		if (ret) {
 			PMD_DRV_LOG(ERR,
 				"Failed to send msg: port 0x%x msg type", hw->vport.vport);
 			return -1;
 		}
-		memcpy(np_stats, &reply_info.reply_body.np_stats, sizeof(struct zxdh_hw_np_stats));
+		memcpy(np_stats, hw_stas_addr, sizeof(struct zxdh_hw_np_stats));
 	}
 	return ret;
 }
@@ -1666,7 +1682,7 @@ zxdh_hw_stats_reset(struct rte_eth_dev *dev, enum zxdh_agent_msg_type opcode)
 {
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
-	struct zxdh_msg_reply_info reply_info = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
 	enum ZXDH_BAR_MODULE_ID module_id;
 	int ret = 0;
 
@@ -1685,7 +1701,7 @@ zxdh_hw_stats_reset(struct rte_eth_dev *dev, enum zxdh_agent_msg_type opcode)
 	zxdh_agent_msg_build(hw, opcode, &msg_info);
 
 	ret = zxdh_send_msg_to_riscv(dev, &msg_info, sizeof(struct zxdh_msg_info),
-				&reply_info, sizeof(struct zxdh_msg_reply_info), module_id);
+				zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info), module_id);
 	if (ret) {
 		PMD_DRV_LOG(ERR, "Failed to reset hw stats");
 		return -1;
@@ -1767,13 +1783,13 @@ zxdh_hw_np_stats_vf_reset(struct rte_eth_dev *dev)
 {
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
-	struct zxdh_msg_reply_info reply_info = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
 	int ret = 0;
 
 	msg_info.data.np_stats_query.clear_mode = 1;
 	zxdh_msg_head_build(hw, ZXDH_GET_NP_STATS, &msg_info);
 	ret = zxdh_vf_send_msg_to_pf(dev, &msg_info, sizeof(struct zxdh_msg_info),
-			&reply_info, sizeof(reply_info));
+			zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info));
 	if (ret)
 		PMD_DRV_LOG(ERR, "Failed to send ZXDH_PORT_METER_STAT_GET msg. code:%d", ret);
 
@@ -2034,29 +2050,24 @@ zxdh_dev_fw_version_get(struct rte_eth_dev *dev,
 {
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
-	struct zxdh_msg_reply_info reply_info = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
+	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
+	void *flash_msg_addr = ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, flash_msg);
 	char fw_ver[ZXDH_FWVERS_LEN] = {0};
 	uint32_t ret = 0;
 
 	zxdh_agent_msg_build(hw, ZXDH_FLASH_FIR_VERSION_GET, &msg_info);
 
-	struct zxdh_msg_recviver_mem rsp_data = {
-			.recv_buffer = (void *)&reply_info,
-			.buffer_len = sizeof(struct zxdh_msg_reply_info),
-	};
-
 	ret = zxdh_send_msg_to_riscv(dev, &msg_info, sizeof(struct zxdh_msg_info),
-				&reply_info, sizeof(struct zxdh_msg_reply_info),
+				zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info),
 				ZXDH_MODULE_FLASH);
 	if (ret) {
 		PMD_DRV_LOG(ERR, "Failed to send msg: port 0x%x msg type %d",
 				hw->vport.vport, ZXDH_FLASH_FIR_VERSION_GET);
 		return -1;
 	}
-	struct zxdh_msg_reply_body *ack_msg =
-			 &(((struct zxdh_msg_reply_info *)rsp_data.recv_buffer)->reply_body);
 
-	memcpy(fw_ver, ack_msg->flash_msg.firmware_version, ZXDH_FWVERS_LEN);
+	memcpy(fw_ver, flash_msg_addr, ZXDH_FWVERS_LEN);
 	snprintf(fw_version, ZXDH_FWVERS_LEN - 1, "%s", fw_ver);
 
 	return 0;
@@ -2068,7 +2079,7 @@ zxdh_en_module_eeprom_read(struct rte_eth_dev *dev,
 {
 	struct zxdh_hw *hw = dev->data->dev_private;
 	struct zxdh_msg_info msg_info = {0};
-	struct zxdh_msg_reply_info reply_info = {0};
+	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
 	uint8_t ret = 0;
 
 	zxdh_agent_msg_build(hw, ZXDH_MAC_MODULE_EEPROM_READ, &msg_info);
@@ -2079,26 +2090,24 @@ zxdh_en_module_eeprom_read(struct rte_eth_dev *dev,
 	msg_info.data.module_eeprom_msg.offset = query->offset;
 	msg_info.data.module_eeprom_msg.length = query->length;
 
-	struct zxdh_msg_recviver_mem rsp_data = {
-			.recv_buffer = (void *)&reply_info,
-			.buffer_len = sizeof(struct zxdh_msg_reply_info),
-	};
-
 	ret = zxdh_send_msg_to_riscv(dev, &msg_info, sizeof(struct zxdh_msg_info),
-				&reply_info, sizeof(struct zxdh_msg_reply_info),
+				zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info),
 				ZXDH_BAR_MODULE_MAC);
 	if (ret) {
 		PMD_DRV_LOG(ERR, "Failed to send msg: port 0x%x msg type %d",
 				hw->vport.vport, ZXDH_MAC_MODULE_EEPROM_READ);
 		return -1;
 	}
-	struct zxdh_msg_reply_body *ack_msg =
-			 &(((struct zxdh_msg_reply_info *)rsp_data.recv_buffer)->reply_body);
-
+	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
+	void *module_eeprom_msg_addr =
+			ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, module_eeprom_msg);
+	void *agent_mac_module_eeprom_msg_data_addr =
+			ZXDH_ADDR_OF(agent_mac_module_eeprom_msg, module_eeprom_msg_addr, data);
+	uint8_t length = ZXDH_GET(agent_mac_module_eeprom_msg, module_eeprom_msg_addr, length);
 	if (data)
-		memcpy(data, ack_msg->module_eeprom_msg.data, ack_msg->module_eeprom_msg.length);
+		memcpy(data, agent_mac_module_eeprom_msg_data_addr, length);
 
-	return ack_msg->module_eeprom_msg.length;
+	return length;
 }
 
 int

drivers/net/zxdh/zxdh_ethdev_ops.h
@@ -62,6 +62,30 @@ struct zxdh_hw_np_stats {
 	uint64_t tx_ssvpc_pkts;
 };
 
+struct zxdh_ifc_hw_np_stats_bits {
+	uint8_t rx_unicast_pkts[0x40];
+	uint8_t tx_unicast_pkts[0x40];
+	uint8_t rx_unicast_bytes[0x40];
+	uint8_t tx_unicast_bytes[0x40];
+	uint8_t rx_multicast_pkts[0x40];
+	uint8_t tx_multicast_pkts[0x40];
+	uint8_t rx_multicast_bytes[0x40];
+	uint8_t tx_multicast_bytes[0x40];
+	uint8_t rx_broadcast_pkts[0x40];
+	uint8_t tx_broadcast_pkts[0x40];
+	uint8_t rx_broadcast_bytes[0x40];
+	uint8_t tx_broadcast_bytes[0x40];
+	uint8_t rx_mtu_drop_pkts[0x40];
+	uint8_t tx_mtu_drop_pkts[0x40];
+	uint8_t rx_mtu_drop_bytes[0x40];
+	uint8_t tx_mtu_drop_bytes[0x40];
+	uint8_t rx_mtr_drop_pkts[0x40];
+	uint8_t tx_mtr_drop_pkts[0x40];
+	uint8_t rx_mtr_drop_bytes[0x40];
+	uint8_t tx_mtr_drop_bytes[0x40];
+	uint8_t tx_ssvpc_pkts[0x40];
+};
+
 struct zxdh_hw_vqm_stats {
 	uint64_t rx_total;
 	uint64_t tx_total;
@@ -71,6 +95,17 @@ struct zxdh_hw_vqm_stats {
 	uint64_t tx_error;
 	uint64_t rx_drop;
 };
+
+struct zxdh_ifc_hw_vqm_stats_bits {
+	uint8_t rx_total[0x40];
+	uint8_t tx_total[0x40];
+	uint8_t rx_bytes[0x40];
+	uint8_t tx_bytes[0x40];
+	uint8_t rx_error[0x40];
+	uint8_t tx_error[0x40];
+	uint8_t rx_drop[0x40];
+};
+
 
 int zxdh_dev_set_link_up(struct rte_eth_dev *dev);
 int zxdh_dev_set_link_down(struct rte_eth_dev *dev);

drivers/net/zxdh/zxdh_msg.h
@@ -11,6 +11,7 @@
 
 #include "zxdh_ethdev_ops.h"
 #include "zxdh_mtr.h"
+#include "zxdh_common.h"
 
 #define ZXDH_BAR0_INDEX                 0
 #define ZXDH_CTRLCH_OFFSET              (0x2000)
@@ -46,8 +47,10 @@
 
 #define ZXDH_MSG_REPLYBODY_HEAD    sizeof(enum zxdh_reps_flag)
 #define ZXDH_MSG_HEADER_SIZE       4
-#define ZXDH_MSG_REPLY_BODY_MAX_LEN  \
-		(ZXDH_MSG_PAYLOAD_MAX_LEN - sizeof(struct zxdh_msg_reply_head))
+#define ZXDH_MSG_REPLY_BODY_MAX_LEN \
+		(ZXDH_MSG_PAYLOAD_MAX_LEN - ZXDH_MSG_HEADER_SIZE)
+#define ZXDH_MSG_REPLY_DATA \
+		(ZXDH_MSG_REPLY_BODY_MAX_LEN - ZXDH_MSG_REPLYBODY_HEAD)
 
 #define ZXDH_MSG_HEAD_LEN            8
 #define ZXDH_MSG_REQ_BODY_MAX_LEN  \
@@ -329,12 +332,6 @@ struct zxdh_offset_get_msg {
 	uint16_t type;
 };
 
-struct zxdh_msg_reply_head {
-	uint8_t flag;
-	uint16_t reps_len;
-	uint8_t resvd;
-};
-
 enum zxdh_reps_flag {
 	ZXDH_REPS_FAIL,
 	ZXDH_REPS_SUCC = 0xaa,
@@ -354,16 +351,37 @@ struct zxdh_link_info_msg {
 	uint32_t speed;
 };
 
+struct zxdh_ifc_link_info_msg_bits {
+	uint8_t autoneg[0x8];
+	uint8_t link_state[0x8];
+	uint8_t blink_enable[0x8];
+	uint8_t duplex[0x8];
+	uint8_t speed_modes[0x20];
+	uint8_t speed[0x20];
+};
+
 struct zxdh_rss_reta {
 	uint32_t reta[RTE_ETH_RSS_RETA_SIZE_256];
+};
+
+struct zxdh_ifc_rss_reta_bits {
+	uint32_t reta[RTE_ETH_RSS_RETA_SIZE_256 * 8];
 };
 
 struct zxdh_rss_hf {
 	uint32_t rss_hf;
 };
 
+struct zxdh_ifc_rss_hf_bits {
+	uint8_t rss_hf[0x20];
+};
+
 struct zxdh_mac_reply_msg {
 	uint8_t mac_flag;
+};
+
+struct zxdh_ifc_mac_reply_msg_bits {
+	uint8_t mac_flag[0x8];
 };
 
 struct zxdh_mac_module_eeprom_msg {
@@ -375,34 +393,57 @@ struct zxdh_mac_module_eeprom_msg {
 	uint8_t data[ZXDH_MODULE_EEPROM_DATA_LEN];
 };
 
+struct zxdh_ifc_agent_mac_module_eeprom_msg_bits {
+	uint8_t i2c_addr[0x8];
+	uint8_t bank[0x8];
+	uint8_t page[0x8];
+	uint8_t offset[0x8];
+	uint8_t length[0x8];
+	uint8_t data[ZXDH_MODULE_EEPROM_DATA_LEN * 8];
+};
+
 struct zxdh_flash_msg {
 	uint8_t firmware_version[ZXDH_FWVERS_LEN];
+};
+
+struct zxdh_ifc_agent_flash_msg_bits {
+	uint8_t firmware_version[0x100];
 };
 
 struct zxdh_mtr_profile_info {
 	uint64_t profile_id;
 };
 
-struct zxdh_msg_reply_body {
-	enum zxdh_reps_flag flag;
+struct zxdh_ifc_mtr_profile_info_bits {
+	uint8_t profile_id[0x40];
+};
+
+struct zxdh_ifc_msg_reply_body_bits {
+	uint8_t flag[0x8];
 	union {
-		uint8_t reply_data[ZXDH_MSG_REPLY_BODY_MAX_LEN - sizeof(enum zxdh_reps_flag)];
-		struct zxdh_hw_np_stats np_stats;
-		struct zxdh_link_info_msg link_msg;
-		struct zxdh_rss_reta rss_reta;
-		struct zxdh_rss_hf rss_hf;
-		struct zxdh_hw_vqm_stats vqm_stats;
-		struct zxdh_mac_reply_msg mac_reply_msg;
-		struct zxdh_flash_msg flash_msg;
-		struct zxdh_mac_module_eeprom_msg module_eeprom_msg;
-		struct zxdh_mtr_profile_info mtr_profile_info;
-		struct zxdh_mtr_stats hw_mtr_stats;
+		uint8_t reply_data[ZXDH_MSG_REPLY_DATA * 8];
+		struct zxdh_ifc_hw_np_stats_bits hw_stats;
+		struct zxdh_ifc_link_info_msg_bits link_msg;
+		struct zxdh_ifc_rss_reta_bits rss_reta_msg;
+		struct zxdh_ifc_rss_hf_bits rss_hf_msg;
+		struct zxdh_ifc_hw_vqm_stats_bits vqm_stats;
+		struct zxdh_ifc_mac_reply_msg_bits mac_reply_msg;
+		struct zxdh_ifc_agent_flash_msg_bits flash_msg;
+		struct zxdh_ifc_agent_mac_module_eeprom_msg_bits module_eeprom_msg;
+		struct zxdh_ifc_mtr_profile_info_bits  mtr_profile_info;
+		struct zxdh_ifc_mtr_stats_bits hw_mtr_stats;
 	};
 };
 
-struct zxdh_msg_reply_info {
-	struct zxdh_msg_reply_head reply_head;
-	struct zxdh_msg_reply_body reply_body;
+struct zxdh_ifc_msg_reply_head_bits {
+	uint8_t flag[0x8];
+	uint8_t reps_len[0x10];
+	uint8_t resvd[0x8];
+};
+
+struct zxdh_ifc_msg_reply_info_bits {
+	struct zxdh_ifc_msg_reply_head_bits reply_head;
+	struct zxdh_ifc_msg_reply_body_bits reply_body;
 };
 
 struct zxdh_vf_init_msg {
@@ -412,12 +453,12 @@ struct zxdh_vf_init_msg {
 	uint8_t rss_enable;
 };
 
-struct zxdh_msg_head {
-	enum zxdh_msg_type msg_type;
+struct __rte_packed_begin zxdh_msg_head {
+	uint8_t msg_type;
 	uint16_t  vport;
 	uint16_t  vf_id;
 	uint16_t pcieid;
-};
+} __rte_packed_end;
 
 struct zxdh_port_attr_set_msg {
 	uint32_t mode;
@@ -455,7 +496,7 @@ struct zxdh_rss_enable {
 };
 
 struct zxdh_agent_msg_head {
-	enum zxdh_agent_msg_type msg_type;
+	uint8_t msg_type;
 	uint8_t panel_id;
 	uint8_t phyport;
 	uint8_t rsv;
@@ -526,7 +567,7 @@ struct zxdh_msg_info {
 typedef int (*zxdh_bar_chan_msg_recv_callback)(void *pay_load, uint16_t len,
 		void *reps_buffer, uint16_t *reps_len, void *dev);
 typedef int (*zxdh_msg_process_callback)(struct zxdh_hw *hw, uint16_t vport, void *cfg_data,
-	struct zxdh_msg_reply_body *res_info, uint16_t *res_len);
+	void *res_info, uint16_t *res_len);
 
 typedef int (*zxdh_bar_chan_msg_recv_callback)(void *pay_load, uint16_t len,
 			void *reps_buffer, uint16_t *reps_len, void *dev);

commit b69fb7abb6d211cb74284029c6cdb2066b9ac0e9

net/zxdh: fix using static assert in macro definition
using static_assert(RTE_BUILD_BUG_ON) replaced BUILD_BUG_ON.

Fixes: 9c3a123f9e08 ("net/zxdh: optimize msg processing ops and modify some issues")

Signed-off-by: Junlong Wang <wang.junlong1@zte.com.cn>

drivers/net/zxdh/zxdh_common.h
@@ -87,20 +87,15 @@ zxdh_set_value(uint32_t fld_sz, uint8_t *addr, size_t value) {
 #define ZXDH_BYTE_OFF(typ, fld) (__zxdh_bit_off(typ, fld) / 8)
 #define ZXDH_ADDR_OF(typ, p, fld) ((uint8_t *)(p) + ZXDH_BYTE_OFF(typ, fld))
 
-#define BUILD_BUG_ON(condition) do { \
-	if (condition) \
-		__builtin_unreachable(); \
-	} while (0)
-
 #define ZXDH_SET(typ, p, fld, v) do { \
-	BUILD_BUG_ON(__zxdh_bit_sz(typ, fld) % 8); \
+	RTE_BUILD_BUG_ON(__zxdh_bit_sz(typ, fld) % 8); \
 	uint32_t fld_sz = __zxdh_bit_sz(typ, fld) / 8; \
 	uint8_t *addr = ZXDH_ADDR_OF(typ, p, fld); \
 	zxdh_set_value(fld_sz, addr, v); \
 } while (0)
 
 #define ZXDH_GET(typ, p, fld) ({ \
-	BUILD_BUG_ON(__zxdh_bit_sz(typ, fld) % 8); \
+	RTE_BUILD_BUG_ON(__zxdh_bit_sz(typ, fld) % 8); \
 	uint32_t fld_sz = __zxdh_bit_sz(typ, fld) / 8; \
 	uint8_t *addr = ZXDH_ADDR_OF(typ, p, fld); \
 	zxdh_get_value(fld_sz, addr); \