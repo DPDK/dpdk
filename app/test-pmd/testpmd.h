fab6ca0bf769033df85fda43b3b0fcbedf2b0dc8
Chengwen Feng <fengchengwen@huawei.com>
2025-04-24 14:17:48 +0800

    app/testpmd: support don't adjust queue num in DCB command
    In some test scenarios, users want to test DCB by specifying the number
    of Rx/Tx queues. But the "port config 0 dcb ..." command will auto
    adjust Rx/Tx queue number.
    
    This patch introduces an optional parameter "keep-qnum" which make sure
    the "port config 0 dcb ..." command don't adjust Rx/Tx queue number.
    The new command:
    
      port config 0 dcb vt off 4 pfc off keep-qnum
    
    If this parameter is not specified, the Rx/Tx queue number was adjusted
    by default.
    
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>

--- app/test-pmd/testpmd.h
+++ app/test-pmd/testpmd.h
@@ -1153,7 +1153,8 @@ int init_port_dcb_config(portid_t pid, enum dcb_mode_enable dcb_mode,
 		     enum rte_eth_nb_tcs num_tcs,
 		     uint8_t pfc_en,
 		     uint8_t prio_tc[RTE_ETH_DCB_NUM_USER_PRIORITIES],
-		     uint8_t prio_tc_en);
+		     uint8_t prio_tc_en,
+		     uint8_t keep_qnum);
 int start_port(portid_t pid);
 void stop_port(portid_t pid);
 void close_port(portid_t pid);
