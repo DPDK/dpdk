e9e52ecc9f1cf0c27eca9c190609da46098a53bf
Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
2025-05-15 13:35:25 +0530

    crypto/cnxk: fix out-of-bounds access in SM2
    [ upstream commit cfefc94a8c155aceb2da519b6b34a01b5caa65e3 ]
    
    Fix coverity issue on out-of-bounds access.
    
    Coverity issue: 403166, 403171, 403172
    Fixes: 5686b573e4bb ("crypto/cnxk: support SM2")
    
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
    Acked-by: Akhil Goyal <gakhil@marvell.com>

--- drivers/crypto/cnxk/cnxk_ae.h
+++ drivers/crypto/cnxk/cnxk_ae.h
@@ -1090,6 +1090,9 @@ cnxk_ae_sm2_sign_prep(struct rte_crypto_sm2_op_param *sm2,
 	if (order_len > ROC_AE_EC_DATA_MAX)
 		order_len = ROC_AE_EC_DATA_MAX;
 
+	if (pkey_len > ROC_AE_EC_DATA_MAX)
+		pkey_len = ROC_AE_EC_DATA_MAX;
+
 	/* Truncate input length to curve prime length */
 	if (message_len > prime_len)
 		message_len = prime_len;
@@ -1180,6 +1183,12 @@ cnxk_ae_sm2_verify_prep(struct rte_crypto_sm2_op_param *sm2,
 	order_len = ec_grp->order.length;
 	if (order_len > ROC_AE_EC_DATA_MAX)
 		order_len = ROC_AE_EC_DATA_MAX;
+
+	if (qx_len > ROC_AE_EC_DATA_MAX)
+		qx_len = ROC_AE_EC_DATA_MAX;
+
+	if (qy_len > ROC_AE_EC_DATA_MAX)
+		qy_len = ROC_AE_EC_DATA_MAX;
 
 	/* Truncate input length to curve prime length */
 	if (message_len > prime_len)
