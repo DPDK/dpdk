00add16978762a8bcd98cd9465503194dc4c1e9a
Sunyang Wu <sunyang.wu@jaguarmicro.com>
2025-05-22 15:42:42 +0800

    ethdev: keep promiscuous/allmulti value before disabling
    The values were reset to 0 before calling the driver function
    for disabling promiscuous or allmulticast,
    which could lead the driver to skip disabling.
    
    The values of the promiscuous and allmulticast variables
    are set after calling the driver, according to the return value.
    
    Fixes: af75078fece3 ("first public release")
    Cc: stable@dpdk.org
    
    Signed-off-by: Sunyang Wu <sunyang.wu@jaguarmicro.com>
    Signed-off-by: Morten Brørup <mb@smartsharesystems.com>

--- lib/ethdev/rte_ethdev.c
+++ lib/ethdev/rte_ethdev.c
@@ -3044,10 +3044,9 @@ rte_eth_promiscuous_disable(uint16_t port_id)
 	if (dev->dev_ops->promiscuous_disable == NULL)
 		return -ENOTSUP;
 
-	dev->data->promiscuous = 0;
 	diag = dev->dev_ops->promiscuous_disable(dev);
-	if (diag != 0)
-		dev->data->promiscuous = 1;
+	if (diag == 0)
+		dev->data->promiscuous = 0;
 
 	diag = eth_err(port_id, diag);
 
@@ -3112,10 +3111,10 @@ rte_eth_allmulticast_disable(uint16_t port_id)
 
 	if (dev->dev_ops->allmulticast_disable == NULL)
 		return -ENOTSUP;
-	dev->data->all_multicast = 0;
+
 	diag = dev->dev_ops->allmulticast_disable(dev);
-	if (diag != 0)
-		dev->data->all_multicast = 1;
+	if (diag == 0)
+		dev->data->all_multicast = 0;
 
 	diag = eth_err(port_id, diag);
 
