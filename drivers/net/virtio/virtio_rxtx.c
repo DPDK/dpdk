DPDK/dpdk

commit 72514b5d55431c4798ad25bcf6c67bc62b0a5800

ethdev: fix check of threshold for Tx freeing
The parameter tx_free_thresh is not consistent between the drivers:
some use it as rte_eth_tx_burst() requires, some release buffers when
the number of free descriptors drop below this value.
Let's use it as most fast-path code does, which is the latter, and update
comments throughout the code to reflect that.

Signed-off-by: Zoltan Kiss <zoltan.kiss@linaro.org>
Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>

drivers/net/virtio/virtio_rxtx.c
@@ -744,7 +744,7 @@ virtio_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts, uint16_t nb_pkts)
 	nb_used = VIRTQUEUE_NUSED(txvq);
 
 	virtio_rmb();
-	if (likely(nb_used > txvq->vq_free_thresh))
+	if (likely(nb_used > txvq->vq_nentries - txvq->vq_free_thresh))
 		virtio_xmit_cleanup(txvq, nb_used);
 
 	nb_tx = 0;

drivers/net/e1000/em_rxtx.c
@@ -182,7 +182,9 @@ struct em_tx_queue {
 	volatile uint32_t      *tdt_reg_addr; /**< Address of TDT register. */
 	uint16_t               nb_tx_desc;    /**< number of TX descriptors. */
 	uint16_t               tx_tail;  /**< Current value of TDT register. */
-	uint16_t               tx_free_thresh;/**< minimum TX before freeing. */
+	/**< Start freeing TX buffers if there are less free descriptors than
+	     this value. */
+	uint16_t               tx_free_thresh;
 	/**< Number of TX descriptors to use before RS bit is set. */
 	uint16_t               tx_rs_thresh;
 	/** Number of TX descriptors used since RS bit was set. */
@@ -418,9 +420,8 @@ eth_em_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
 	txe = &sw_ring[tx_id];
 
 	/* Determine if the descriptor ring needs to be cleaned. */
-	if ((txq->nb_tx_desc - txq->nb_tx_free) > txq->tx_free_thresh) {
+	 if (txq->nb_tx_free < txq->tx_free_thresh)
 		em_xmit_cleanup(txq);
-	}
 
 	/* TX loop */
 	for (nb_tx = 0; nb_tx < nb_pkts; nb_tx++) {

drivers/net/fm10k/fm10k.h
@@ -200,7 +200,6 @@ struct fm10k_tx_queue {
 	uint16_t next_free;
 	uint16_t nb_free;
 	uint16_t nb_used;
-	uint16_t free_trigger;
 	uint16_t free_thresh;
 	uint16_t rs_thresh;
 	volatile uint32_t *tail_ptr;

drivers/net/fm10k/fm10k_ethdev.c
@@ -203,7 +203,6 @@ tx_queue_reset(struct fm10k_tx_queue *q)
 	q->next_free = 0;
 	q->nb_used = 0;
 	q->nb_free = q->nb_desc - 1;
-	q->free_trigger = q->nb_free - q->free_thresh;
 	fifo_reset(&q->rs_tracker, (q->nb_desc + 1) / q->rs_thresh);
 	FM10K_PCI_REG_WRITE(q->tail_ptr, 0);
 }

drivers/net/fm10k/fm10k_rxtx.c
@@ -450,7 +450,7 @@ fm10k_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
 		mb = tx_pkts[count];
 
 		/* running low on descriptors? try to free some... */
-		if (q->nb_free < q->free_trigger)
+		if (q->nb_free < q->free_thresh)
 			tx_free_descriptors(q);
 
 		/* make sure there are enough free descriptors to transmit the

drivers/net/i40e/i40e_rxtx.c
@@ -1236,7 +1236,7 @@ i40e_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts, uint16_t nb_pkts)
 	txe = &sw_ring[tx_id];
 
 	/* Check if the descriptor ring needs to be cleaned. */
-	if ((txq->nb_tx_desc - txq->nb_tx_free) > txq->tx_free_thresh)
+	if (txq->nb_tx_free < txq->tx_free_thresh)
 		i40e_xmit_cleanup(txq);
 
 	for (nb_tx = 0; nb_tx < nb_pkts; nb_tx++) {

drivers/net/i40e/i40e_rxtx.h
@@ -136,8 +136,9 @@ struct i40e_tx_queue {
 	uint16_t last_desc_cleaned;
 	/**< Total number of TX descriptors ready to be allocated. */
 	uint16_t nb_tx_free;
-	/**< Number of TX descriptors to use before RS bit is set. */
-	uint16_t tx_free_thresh; /**< minimum TX before freeing. */
+	/**< Start freeing TX buffers if there are less free descriptors than
+	     this value. */
+	uint16_t tx_free_thresh;
 	/** Number of TX descriptors to use before RS bit is set. */
 	uint16_t tx_rs_thresh;
 	uint8_t pthresh; /**< Prefetch threshold register. */

drivers/net/ixgbe/ixgbe_rxtx.c
@@ -593,9 +593,8 @@ ixgbe_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
 	txe = &sw_ring[tx_id];
 
 	/* Determine if the descriptor ring needs to be cleaned. */
-	if ((txq->nb_tx_desc - txq->nb_tx_free) > txq->tx_free_thresh) {
+	if (txq->nb_tx_free < txq->tx_free_thresh)
 		ixgbe_xmit_cleanup(txq);
-	}
 
 	rte_prefetch0(&txe->mbuf->pool);
 

commit 3e3c7f3fa5ac3f2748a4463d87e73eb28024b401

net/virtio: fix check of threshold for Tx freeing
Like most drivers, make the fast path of virtio_xmit_cleanup() behave as
described by the comments of rte_eth_txconf::tx_free_thresh ([0]):
    Start freeing Tx buffers if there are
    less free descriptors than this value.

The rationale behind this change is that:
  * vq->vq_nentries is set during device probe
    with the queue size specified by vhost backend,
    this value does not reflect the real nb_tx_desc
  * the real available tx desc is set to vq->vq_free_cnt
    via the nb_tx_desc param of rte_eth_tx_queue_setup() API
  * so `nb_used > vq->vq_nentries - vq->vq_free_thresh` could never be true
    if say nb_tx_desc=2048, vq->vq_nentries=4096, vq->vq_free_thresh=32,
    see bug report 1716 ([1]) for details.

[0]: https://github.com/DPDK/dpdk/commit/72514b5d5543
[1]: https://bugs.dpdk.org/show_bug.cgi?id=1716

Bugzilla ID: 1716
Fixes: 72514b5d5543 ("ethdev: fix check of threshold for Tx freeing")
Cc: stable@dpdk.org

Signed-off-by: Baoyuan Li <updoing@sina.com>
Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>
Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>

.mailmap
@@ -167,6 +167,7 @@ Balakrishna Bhamidipati <bbhamidipati@marvell.com>
 Balasubramanian Manoharan <balasubramanian.manoharan@caviumnetworks.com>
 Balazs Nemeth <bnemeth@redhat.com> <balazs.nemeth@intel.com>
 Bao-Long Tran <longtb5@viettel.com.vn>
+Baoyuan Li <updoing@sina.com>
 Bar Neuman <bneuman@nvidia.com>
 Barak Enat <barak@saguna.net>
 Barbara Skobiej <barbara.skobiej@intel.com>
@@ -552,6 +553,7 @@ Hemant Agrawal <hemant.agrawal@nxp.com> <hemant@freescale.com>
 Heng Ding <hengx.ding@intel.com>
 Hengjian Zhang <hengjianx.zhang@intel.com>
 Heng Jiang <jiangheng14@huawei.com>
+Hengqi Chen <hengqi.chen@gmail.com>
 Heng Wang <heng.wang@ericsson.com>
 Henning Schild <henning.schild@siemens.com>
 Henry Cai <caihe@huawei.com>

drivers/net/virtio/virtio_rxtx.c
@@ -1873,7 +1873,7 @@ virtio_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts, uint16_t nb_pkts)
 
 	nb_used = virtqueue_nused(vq);
 
-	if (likely(nb_used > vq->vq_nentries - vq->vq_free_thresh))
+	if (likely(vq->vq_free_cnt < vq->vq_free_thresh))
 		virtio_xmit_cleanup(vq, nb_used);
 
 	for (nb_tx = 0; nb_tx < nb_pkts; nb_tx++) {