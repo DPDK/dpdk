e31d4679575571890974730c989aa286cb516398
Pravin Pathak <pravin.pathak@intel.com>
2025-06-19 17:31:50 -0500

    event/dlb2: fix public symbol namespace
    Added RTE_PMD_DLB2_ prefix to dlb2 token pop mode enums
    to avoid namespace conflict. These enums are passed to public
    API rte_pmd_dlb2_set_token_pop_mode().
    
    Fixes: c667583d82f4 ("event/dlb2: add token pop API")
    Cc: stable@dpdk.org
    
    Signed-off-by: Pravin Pathak <pravin.pathak@intel.com>

--- drivers/event/dlb2/rte_pmd_dlb2.h
+++ drivers/event/dlb2/rte_pmd_dlb2.h
@@ -49,18 +49,18 @@ extern "C" {
  *
  * Selects the token pop mode for a DLB2 port.
  */
-enum dlb2_token_pop_mode {
+enum rte_pmd_dlb2_token_pop_mode {
 	/* Pop the CQ tokens immediately after dequeuing. */
-	AUTO_POP,
+	RTE_PMD_DLB2_AUTO_POP,
 	/* Pop CQ tokens after (dequeue_depth - 1) events are released.
 	 * Supported on load-balanced ports only.
 	 */
-	DELAYED_POP,
+	RTE_PMD_DLB2_DELAYED_POP,
 	/* Pop the CQ tokens during next dequeue operation. */
-	DEFERRED_POP,
+	RTE_PMD_DLB2_DEFERRED_POP,
 
 	/* NUM_TOKEN_POP_MODES must be last */
-	NUM_TOKEN_POP_MODES
+	RTE_PMD_DLB2_NUM_TOKEN_POP_MODES
 };
 
 /**
@@ -68,8 +68,9 @@ enum dlb2_token_pop_mode {
  * @b EXPERIMENTAL: this API may change, or be removed, without prior notice
  *
  * Configure the token pop mode for a DLB2 port. By default, all ports use
- * AUTO_POP. This function must be called before calling rte_event_port_setup()
- * for the port, but after calling rte_event_dev_configure().
+ * RTE_PMD_DLB2_AUTO_POP. This function must be called before calling
+ * rte_event_port_setup() for the port, but after calling
+ * rte_event_dev_configure().
  *
  * @param dev_id
  *    The identifier of the event device.
@@ -80,16 +81,16 @@ enum dlb2_token_pop_mode {
  *
  * @return
  * - 0: Success
- * - EINVAL: Invalid dev_id, port_id, or mode
- * - EINVAL: The DLB2 is not configured, is already running, or the port is
- *   already setup
+ * - EINVAL: Invalid parameter dev_id, port_id, or mode
+ * - EINVAL: The DLB2 device is not configured or is already running,
+ *           or the port is already setup
  */
 
 __rte_experimental
 int
 rte_pmd_dlb2_set_token_pop_mode(uint8_t dev_id,
 				uint8_t port_id,
-				enum dlb2_token_pop_mode mode);
+				enum rte_pmd_dlb2_token_pop_mode mode);
 
 /** Set inflight threshold for flow migration */
 #define DLB2_SET_PORT_FLOW_MIGRATION_THRESHOLD RTE_BIT64(0)
