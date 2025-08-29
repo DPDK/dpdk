DPDK/dpdk

commit 01acc23bab131de376a0cbd81b4a3f3e844aea60

net/zxdh: optimize RSS operations
optimize RSS hash config/update,RETA update/get.

Signed-off-by: Junlong Wang <wang.junlong1@zte.com.cn>

drivers/net/zxdh/zxdh_ethdev_ops.c
@@ -1144,7 +1144,6 @@ zxdh_rss_hash_conf_get(struct rte_eth_dev *dev, struct rte_eth_rss_conf *rss_con
 		}
 		reply_msg.reply_body.rss_hf.rss_hf = port_attr.rss_hash_factor;
 	} else {
-		zxdh_msg_head_build(hw, ZXDH_RSS_HF_SET, &msg);
 		ret = zxdh_vf_send_msg_to_pf(dev, &msg, sizeof(struct zxdh_msg_info),
 				&reply_msg, sizeof(struct zxdh_msg_reply_info));
 		if (ret) {

drivers/net/zxdh/zxdh_msg.c
@@ -1544,6 +1544,130 @@ zxdh_vf_set_vlan_offload(struct zxdh_hw *hw, uint16_t vport, void *cfg_data,
 	return ret;
 }
 
+static int
+zxdh_vf_rss_hf_get(struct zxdh_hw *hw, uint16_t vport, void *cfg_data __rte_unused,
+			struct zxdh_msg_reply_body *reply, uint16_t *res_len)
+{
+	char str[ZXDH_MSG_REPLY_BODY_MAX_LEN] = "rss_hf";
+	struct zxdh_port_attr_table vport_att = {0};
+	int ret = 0;
+
+	ret = zxdh_get_port_attr(hw, vport, &vport_att);
+	if (ret) {
+		sprintf(str, "get rss hash factor failed, ret:%d\n", ret);
+		PMD_DRV_LOG(ERR, "get rss hash factor failed.");
+		goto proc_end;
+	}
+
+	reply->rss_hf.rss_hf = vport_att.rss_hash_factor;
+
+proc_end:
+	*res_len = strlen(str) + sizeof(enum zxdh_reps_flag);
+	reply->flag = (ret == 0) ? ZXDH_REPS_SUCC : ZXDH_REPS_FAIL;
+	memcpy(&reply->reply_data, str, strlen(str) + 1);
+	return ret;
+}
+
+static int
+zxdh_vf_rss_hf_set(struct zxdh_hw *hw, uint16_t vport, void *cfg_data,
+			struct zxdh_msg_reply_body *reply, uint16_t *res_len)
+{
+	char str[ZXDH_MSG_REPLY_BODY_MAX_LEN] = "rss_hf";
+	struct zxdh_rss_hf *rss_hf = cfg_data;
+	struct zxdh_port_attr_table vport_att = {0};
+	int ret = 0;
+
+	ret = zxdh_get_port_attr(hw, vport, &vport_att);
+	if (ret) {
+		sprintf(str, "set rss hash factor (set vport tbl failed, hf is %d). ret:%d\n",
+				rss_hf->rss_hf, ret);
+		PMD_DRV_LOG(ERR, "rss enable hash factor set failed");
+		goto proc_end;
+	}
+
+	vport_att.rss_hash_factor = rss_hf->rss_hf;
+	ret = zxdh_set_port_attr(hw, vport, &vport_att);
+	if (ret) {
+		sprintf(str, "set rss hash factor (set vport tbl failed, hf is %d). ret:%d\n",
+				rss_hf->rss_hf, ret);
+		PMD_DRV_LOG(ERR, "rss config hf failed");
+	}
+
+proc_end:
+	*res_len = strlen(str) + sizeof(enum zxdh_reps_flag);
+	reply->flag = (ret == 0) ? ZXDH_REPS_SUCC : ZXDH_REPS_FAIL;
+	memcpy(&reply->reply_data, str, strlen(str) + 1);
+	return ret;
+}
+
+static int
+zxdh_vf_rss_enable(struct zxdh_hw *hw, uint16_t vport, void *cfg_data,
+			struct zxdh_msg_reply_body *reply, uint16_t *res_len)
+{
+	char str[ZXDH_MSG_REPLY_BODY_MAX_LEN] = "rss_enable";
+	struct zxdh_rss_enable *rss_enable = cfg_data;
+	struct zxdh_port_attr_table vport_att = {0};
+	int ret = 0;
+
+	ret = zxdh_get_port_attr(hw, vport, &vport_att);
+	if (ret) {
+		sprintf(str, "set rss enable (get vport tbl failed, rss_enable is %d). ret:%d\n",
+				rss_enable->enable, ret);
+		PMD_DRV_LOG(ERR, "rss enable set failed");
+		goto proc_end;
+	}
+
+	vport_att.rss_enable = rss_enable->enable;
+	ret = zxdh_set_port_attr(hw, vport, &vport_att);
+	if (ret) {
+		sprintf(str, "set rss enable (set vport tbl failed, rss_enable is %d). ret:%d\n",
+				rss_enable->enable, ret);
+		PMD_DRV_LOG(ERR, "rss enable set failed");
+	}
+
+proc_end:
+	*res_len = strlen(str) + sizeof(enum zxdh_reps_flag);
+	reply->flag = (ret == 0) ? ZXDH_REPS_SUCC : ZXDH_REPS_FAIL;
+	memcpy(&reply->reply_data, str, strlen(str) + 1);
+	return ret;
+}
+
+static int
+zxdh_vf_rss_table_set(struct zxdh_hw *hw, uint16_t vport, void *cfg_data,
+		struct zxdh_msg_reply_body *reply, uint16_t *res_len)
+{
+	char str[ZXDH_MSG_REPLY_BODY_MAX_LEN] = "rss_table";
+	struct zxdh_rss_reta *rss_reta = cfg_data;
+	int32_t ret = 0;
+
+	ret = zxdh_rss_table_set(hw, vport, rss_reta);
+	if (ret)
+		sprintf(str, "set rss reta tbl failed, code:%d", ret);
+
+	*res_len = strlen(str) + sizeof(enum zxdh_reps_flag);
+	reply->flag = (ret == 0) ? ZXDH_REPS_SUCC : ZXDH_REPS_FAIL;
+	memcpy(&reply->reply_data, str, strlen(str) + 1);
+	return ret;
+}
+
+static int
+zxdh_vf_rss_table_get(struct zxdh_hw *hw, uint16_t vport, void *cfg_data __rte_unused,
+		struct zxdh_msg_reply_body *reply, uint16_t *res_len)
+{
+	char str[ZXDH_MSG_REPLY_BODY_MAX_LEN] = "rss_table";
+	struct zxdh_rss_reta *rss_reta = &reply->rss_reta;
+	int ret = 0;
+
+	ret = zxdh_rss_table_get(hw, vport, rss_reta);
+	if (ret)
+		sprintf(str, "set rss reta tbl failed, code:%d", ret);
+
+	*res_len = strlen(str) + sizeof(enum zxdh_reps_flag);
+	reply->flag = (ret == 0) ? ZXDH_REPS_SUCC : ZXDH_REPS_FAIL;
+	memcpy(&reply->reply_data, str, strlen(str) + 1);
+	return ret;
+}
+
 static const zxdh_msg_process_callback zxdh_proc_cb[] = {
 	[ZXDH_NULL] = NULL,
 	[ZXDH_VF_PORT_INIT] = zxdh_vf_port_init,
@@ -1555,6 +1679,11 @@ static const zxdh_msg_process_callback zxdh_proc_cb[] = {
 	[ZXDH_VLAN_FILTER_ADD] = zxdh_vf_vlan_filter_table_add,
 	[ZXDH_VLAN_FILTER_DEL] = zxdh_vf_vlan_filter_table_del,
 	[ZXDH_VLAN_OFFLOAD] = zxdh_vf_set_vlan_offload,
+	[ZXDH_RSS_ENABLE] = zxdh_vf_rss_enable,
+	[ZXDH_RSS_RETA_GET] = zxdh_vf_rss_table_get,
+	[ZXDH_RSS_RETA_SET] = zxdh_vf_rss_table_set,
+	[ZXDH_RSS_HF_SET] = zxdh_vf_rss_hf_set,
+	[ZXDH_RSS_HF_GET] = zxdh_vf_rss_hf_get,
 };
 
 static inline int

commit a337a7a118a51b952b004a56e1c2e82322e8f1a9

net/zxdh: fix RSS
modify rss related issues in testing process.

Fixes: 01acc23bab13 ("net/zxdh: optimize RSS operations")
Cc: stable@dpdk.org

Signed-off-by: Junlong Wang <wang.junlong1@zte.com.cn>

drivers/net/zxdh/zxdh_ethdev.h
@@ -54,6 +54,7 @@
 #define ZXDH_SLOT_MAX             256
 #define ZXDH_MAX_VF               256
 #define ZXDH_HASHIDX_MAX          6
+#define ZXDH_RSS_HASH_KEY_LEN     40U
 
 union zxdh_virport_num {
 	uint16_t vport;

drivers/net/zxdh/zxdh_ethdev_ops.c
@@ -1170,7 +1170,7 @@ zxdh_rss_hf_to_hw(uint64_t hf)
 }
 
 static uint64_t
-zxdh_rss_hf_to_eth(uint32_t hw_hf)
+zxdh_rss_hf_to_eth(uint64_t hw_hf)
 {
 	uint64_t hf = 0;
 
@@ -1208,7 +1208,7 @@ zxdh_rss_hash_update(struct rte_eth_dev *dev,
 	hw_hf_new = zxdh_rss_hf_to_hw(rss_conf->rss_hf);
 	hw_hf_old = zxdh_rss_hf_to_hw(old_rss_conf->rss_hf);
 
-	if ((hw_hf_new != hw_hf_old || !!rss_conf->rss_hf))
+	if (hw_hf_new != hw_hf_old || hw->rss_enable != !!rss_conf->rss_hf)
 		need_update_hf = 1;
 
 	if (need_update_hf) {
@@ -1231,6 +1231,9 @@ zxdh_rss_hash_update(struct rte_eth_dev *dev,
 			}
 		}
 		if (hw->is_pf) {
+			hw->rss_enable = !!rss_conf->rss_hf;
+			if (rss_conf->rss_hf == 0)
+				return 0;
 			ret = zxdh_get_port_attr(hw, hw->vport.vport, &port_attr);
 			port_attr.rss_hash_factor = hw_hf_new;
 			ret = zxdh_set_port_attr(hw, hw->vport.vport, &port_attr);
@@ -1257,15 +1260,16 @@ zxdh_rss_hash_update(struct rte_eth_dev *dev,
 int
 zxdh_rss_hash_conf_get(struct rte_eth_dev *dev, struct rte_eth_rss_conf *rss_conf)
 {
-	struct zxdh_hw *hw = (struct zxdh_hw *)dev->data->dev_private;
+	struct zxdh_hw *hw = dev->data->dev_private;
 	struct rte_eth_rss_conf *old_rss_conf = &dev->data->dev_conf.rx_adv_conf.rss_conf;
 	struct zxdh_msg_info msg = {0};
 	uint8_t zxdh_msg_reply_info[ZXDH_ST_SZ_BYTES(msg_reply_info)] = {0};
 	void *reply_body_addr = ZXDH_ADDR_OF(msg_reply_info, zxdh_msg_reply_info, reply_body);
 	void *rss_hf_msg_addr = ZXDH_ADDR_OF(msg_reply_body, reply_body_addr, rss_hf_msg);
 	struct zxdh_port_attr_table port_attr = {0};
-	uint32_t rss_hf;
-	uint32_t hw_hf;
+	uint64_t rss_hf = 0;
+	uint64_t hw_hf = 0;
+	uint8_t need_update_hf = 0;
 	int ret;
 
 	if (rss_conf == NULL) {
@@ -1273,27 +1277,40 @@ zxdh_rss_hash_conf_get(struct rte_eth_dev *dev, struct rte_eth_rss_conf *rss_con
 		return -ENOMEM;
 	}
 
-	hw_hf = zxdh_rss_hf_to_hw(old_rss_conf->rss_hf);
-	rss_conf->rss_hf = zxdh_rss_hf_to_eth(hw_hf);
-
-	zxdh_msg_head_build(hw, ZXDH_RSS_HF_GET, &msg);
-	if (hw->is_pf) {
-		ret = zxdh_get_port_attr(hw, hw->vport.vport, &port_attr);
-		if (ret) {
-			PMD_DRV_LOG(ERR, "rss hash factor set failed");
-			return -EINVAL;
-		}
-		ZXDH_SET(rss_hf, rss_hf_msg_addr, rss_hf, port_attr.rss_hash_factor);
-	} else {
-		ret = zxdh_vf_send_msg_to_pf(dev, &msg, sizeof(struct zxdh_msg_info),
-				zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info));
-		if (ret) {
-			PMD_DRV_LOG(ERR, "rss hash factor set failed");
-			return -EINVAL;
-		}
+	if (hw->rss_enable == 0) {
+		rss_conf->rss_hf = 0;
+		return 0;
 	}
-	rss_hf = ZXDH_GET(rss_hf, rss_hf_msg_addr, rss_hf);
-	rss_conf->rss_hf = zxdh_rss_hf_to_eth(rss_hf);
+
+	if (old_rss_conf->rss_hf == 0)
+		need_update_hf = 1;
+
+	if (!need_update_hf) {
+		hw_hf = zxdh_rss_hf_to_hw(old_rss_conf->rss_hf);
+		rss_conf->rss_hf = zxdh_rss_hf_to_eth(hw_hf);
+	}
+
+	if (need_update_hf) {
+		zxdh_msg_head_build(hw, ZXDH_RSS_HF_GET, &msg);
+		if (hw->is_pf) {
+			ret = zxdh_get_port_attr(hw, hw->vport.vport, &port_attr);
+			if (ret) {
+				PMD_DRV_LOG(ERR, "rss hash factor set failed");
+				return -EINVAL;
+			}
+			ZXDH_SET(rss_hf, rss_hf_msg_addr, rss_hf, port_attr.rss_hash_factor);
+		} else {
+			ret = zxdh_vf_send_msg_to_pf(dev, &msg, sizeof(struct zxdh_msg_info),
+					zxdh_msg_reply_info, ZXDH_ST_SZ_BYTES(msg_reply_info));
+			if (ret) {
+				PMD_DRV_LOG(ERR, "rss hash factor set failed");
+				return -EINVAL;
+			}
+		}
+		rss_hf = ZXDH_GET(rss_hf, rss_hf_msg_addr, rss_hf);
+		rss_conf->rss_hf = zxdh_rss_hf_to_eth(rss_hf);
+		old_rss_conf->rss_hf = zxdh_rss_hf_to_eth(hw_hf);
+	}
 
 	return 0;
 }
@@ -1352,7 +1369,6 @@ zxdh_rss_configure(struct rte_eth_dev *dev)
 
 	if (curr_rss_enable && hw->rss_init == 0) {
 		/* config hash factor */
-		dev->data->dev_conf.rx_adv_conf.rss_conf.rss_hf = ZXDH_HF_F5_ETH;
 		hw_hf = zxdh_rss_hf_to_hw(dev->data->dev_conf.rx_adv_conf.rss_conf.rss_hf);
 		memset(&msg, 0, sizeof(msg));
 		if (hw->is_pf) {

drivers/net/zxdh/zxdh_msg.c
@@ -1713,12 +1713,15 @@ zxdh_vf_rss_table_get(struct zxdh_hw *hw, uint16_t vport, void *cfg_data __rte_u
 	if (ret)
 		sprintf(str, "set rss reta tbl failed, code:%d", ret);
 
-	*res_len = strlen(str) + sizeof(uint8_t);
-	if (ret == 0)
+	if (ret == 0) {
+		*res_len = ZXDH_ST_SZ_BYTES(rss_reta) + sizeof(uint8_t);
 		ZXDH_SET(msg_reply_body, reply, flag, ZXDH_REPS_SUCC);
-	else
+	} else {
+		*res_len = strlen(str) + sizeof(uint8_t);
 		ZXDH_SET(msg_reply_body, reply, flag, ZXDH_REPS_FAIL);
-	memcpy(reply_data_addr, str, strlen(str) + 1);
+		memcpy(reply_data_addr, str, strlen(str) + 1);
+	}
+
 	return ret;
 }
 