1abf7e31f268f3ec07df0ef96f612cc9f6a9d332
Kerem Aksu <kerem.aksu@i2i-systems.com>
2025-09-12 14:35:25 +0300

    net/af_packet: fix crash in secondary process
    [ upstream commit d57124f60ef60b24cd39e895cf6d211b93b897ae ]
    
    dumpcap crashes when trying to capture from af_packet devices. This is
    caused by allocating interface name with
    strdup (i.e. malloc). Interface name is not accessible from secondary
    process and causes segmentation fault. Use rte_malloc instead of
    strdup to fix the issue.
    
    Bugzilla ID: 1786
    Fixes: 1b93c2aa81b4 ("net/af_packet: add interface name to internals")
    
    Signed-off-by: Kerem Aksu <kerem.aksu@i2i-systems.com>

--- drivers/net/af_packet/rte_eth_af_packet.c
+++ drivers/net/af_packet/rte_eth_af_packet.c
@@ -462,7 +462,7 @@ eth_dev_close(struct rte_eth_dev *dev)
 		rte_free(internals->rx_queue[q].rd);
 		rte_free(internals->tx_queue[q].rd);
 	}
-	free(internals->if_name);
+	rte_free(internals->if_name);
 	rte_free(internals->rx_queue);
 	rte_free(internals->tx_queue);
 
@@ -752,9 +752,10 @@ rte_pmd_init_internals(struct rte_vdev_device *dev,
 		PMD_LOG_ERRNO(ERR, "%s: ioctl failed (SIOCGIFINDEX)", name);
 		goto free_internals;
 	}
-	(*internals)->if_name = strdup(pair->value);
+	(*internals)->if_name = rte_malloc_socket(name, ifnamelen + 1, 0, numa_node);
 	if ((*internals)->if_name == NULL)
 		goto free_internals;
+	strlcpy((*internals)->if_name, pair->value, ifnamelen + 1);
 	(*internals)->if_index = ifr.ifr_ifindex;
 
 	if (ioctl(sockfd, SIOCGIFHWADDR, &ifr) == -1) {
@@ -941,7 +942,7 @@ error:
 free_internals:
 	rte_free((*internals)->rx_queue);
 	rte_free((*internals)->tx_queue);
-	free((*internals)->if_name);
+	rte_free((*internals)->if_name);
 	rte_free(*internals);
 	return -1;
 }
