534942279e1b955f2321d84f5102be9642ab33e6
Sucharitha Sarananaga <ssarananaga@marvell.com>
2025-09-29 15:13:49 +0530

    crypto/cnxk: refactor RSA verification
    [ upstream commit dfd038b97ec3d173ded0f985df39301b7c7662f2 ]
    
    This patch avoid copying the decrypted message into
    the signature buffer, which is actually an input to the
    verify operation. This prevents overwriting the input
    buffer unnecessarily.
    
    Fixes: 6661bedf1605 ("crypto/cnxk: add asymmetric datapath")
    
    Signed-off-by: Sucharitha Sarananaga <ssarananaga@marvell.com>

--- drivers/crypto/cnxk/cnxk_ae.h
+++ drivers/crypto/cnxk/cnxk_ae.h
@@ -1592,20 +1592,17 @@ cnxk_ae_dequeue_rsa_op(struct rte_crypto_op *cop, uint8_t *rptr,
 	case RTE_CRYPTO_ASYM_OP_VERIFY:
 		if (rsa_ctx->padding.type == RTE_CRYPTO_RSA_PADDING_NONE) {
 			rsa->sign.length = rsa_ctx->n.length;
-			memcpy(rsa->sign.data, rptr, rsa->sign.length);
+			if (memcmp(rptr, rsa->message.data, rsa->message.length))
+				cop->status = RTE_CRYPTO_OP_STATUS_ERROR;
 		} else {
 			/* Get length of signed output */
-			rsa->sign.length =
-				rte_cpu_to_be_16(*((uint16_t *)rptr));
+			rsa->sign.length = rte_cpu_to_be_16(*((uint16_t *)rptr));
 			/*
 			 * Offset output data pointer by length field
-			 * (2 bytes) and copy signed data.
+			 * (2 bytes) and compare signed data.
 			 */
-			memcpy(rsa->sign.data, rptr + 2, rsa->sign.length);
-		}
-		if (memcmp(rsa->sign.data, rsa->message.data,
-			   rsa->message.length)) {
-			cop->status = RTE_CRYPTO_OP_STATUS_ERROR;
+			if (memcmp(rptr + 2, rsa->message.data, rsa->message.length))
+				cop->status = RTE_CRYPTO_OP_STATUS_ERROR;
 		}
 		break;
 	default:
