059f9f130cb425d9186563ce7e5142ed0554ec56
Vanshika Shukla <vanshika.shukla@nxp.com>
2025-10-06 13:34:01 +0530

    net/enetfec: fix out-of-bounds access in UIO mapping
    [ upstream commit 22b0837bd93a777b8ca7fcf234985175e456a4f5 ]
    
    NXP internal Coverity flagged a potential out-of-bounds
    access due to invalid mapping size. This patch adds a check to
    ensure the mapping size is within valid bounds before proceeding
    with memory mapping.
    
    Fixes: b84fdd39638b ("net/enetfec: support UIO")
    
    Signed-off-by: Vanshika Shukla <vanshika.shukla@nxp.com>
    Acked-by: Sachin Saxena <sachin.saxena@nxp.com>

--- drivers/net/enetfec/enet_uio.c
+++ drivers/net/enetfec/enet_uio.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2021,2024 NXP
+ * Copyright 2021,2024-2025 NXP
  */
 
 #include <stdbool.h>
@@ -142,6 +142,10 @@ uio_map_mem(int uio_device_fd, int uio_device_id,
 	}
 	/* Read mapping size and physical address expressed in hexa(base 16) */
 	uio_map_size = strtol(uio_map_size_str, NULL, 16);
+	if (uio_map_size <= 0 || uio_map_size > INT_MAX) {
+		ENETFEC_PMD_ERR("Invalid mapping size: %u.", uio_map_size);
+		return NULL;
+	}
 	uio_map_p_addr = strtol(uio_map_p_addr_str, NULL, 16);
 
 	if (uio_map_id == 0) {
