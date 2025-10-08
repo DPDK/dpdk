DPDK/dpdk

commit 12b1c29017615baf6353782623a2e857a898458e

net/idpf: use common Rx path selection infrastructure
Update the common Rx path selection infrastructure to include the
feature "single queue" which is relevant for the idpf and cpfl drivers.
Replace the existing complicated logic in the idpf driver with the use
of the common function.

Signed-off-by: Ciara Loftus <ciara.loftus@intel.com>
Tested-by: Shaiq Wani <shaiq.wani@intel.com>

drivers/net/intel/idpf/idpf_common_device.h
@@ -44,6 +44,16 @@
 	(sizeof(struct virtchnl2_ptype) +				\
 	 (((p)->proto_id_count ? ((p)->proto_id_count - 1) : 0) * sizeof((p)->proto_id[0])))
 
+enum idpf_rx_func_type {
+	IDPF_RX_DEFAULT,
+	IDPF_RX_SINGLEQ,
+	IDPF_RX_SINGLEQ_SCATTERED,
+	IDPF_RX_SINGLEQ_AVX2,
+	IDPF_RX_AVX512,
+	IDPF_RX_SINGLQ_AVX512,
+	IDPF_RX_MAX
+};
+
 struct idpf_adapter {
 	struct idpf_hw hw;
 	struct virtchnl2_version_info virtchnl_version;
@@ -59,6 +69,8 @@ struct idpf_adapter {
 
 	/* For timestamp */
 	uint64_t time_hw;
+
+	enum idpf_rx_func_type rx_func_type;
 };
 
 struct idpf_chunks_info {

drivers/net/intel/idpf/idpf_common_rxtx.c
@@ -7,6 +7,8 @@
 #include <rte_errno.h>
 
 #include "idpf_common_rxtx.h"
+#include "idpf_common_device.h"
+#include "../common/rx.h"
 
 int idpf_timestamp_dynfield_offset = -1;
 uint64_t idpf_timestamp_dynflag;
@@ -1622,3 +1624,52 @@ idpf_qc_splitq_rx_vec_setup(struct idpf_rx_queue *rxq)
 	rxq->bufq2->idpf_ops = &def_rx_ops_vec;
 	return idpf_rxq_vec_setup_default(rxq->bufq2);
 }
+
+RTE_EXPORT_INTERNAL_SYMBOL(idpf_rx_path_infos)
+const struct ci_rx_path_info idpf_rx_path_infos[] = {
+	[IDPF_RX_DEFAULT] = {
+		.pkt_burst = idpf_dp_splitq_recv_pkts,
+		.info = "Split Scalar",
+		.features = {
+			.rx_offloads = IDPF_RX_SCALAR_OFFLOADS,
+			.simd_width = RTE_VECT_SIMD_DISABLED}},
+	[IDPF_RX_SINGLEQ] = {
+		.pkt_burst = idpf_dp_singleq_recv_pkts,
+		.info = "Single Scalar",
+		.features = {
+			.rx_offloads = IDPF_RX_SCALAR_OFFLOADS,
+			.simd_width = RTE_VECT_SIMD_DISABLED,
+			.extra.single_queue = true}},
+	[IDPF_RX_SINGLEQ_SCATTERED] = {
+		.pkt_burst = idpf_dp_singleq_recv_scatter_pkts,
+		.info = "Single Scalar Scattered",
+		.features = {
+			.rx_offloads = IDPF_RX_SCALAR_OFFLOADS,
+			.simd_width = RTE_VECT_SIMD_DISABLED,
+			.extra.scattered = true,
+			.extra.single_queue = true}},
+#ifdef RTE_ARCH_X86
+	[IDPF_RX_SINGLEQ_AVX2] = {
+		.pkt_burst = idpf_dp_singleq_recv_pkts_avx2,
+		.info = "Single AVX2 Vector",
+		.features = {
+			.rx_offloads = IDPF_RX_VECTOR_OFFLOADS,
+			.simd_width = RTE_VECT_SIMD_256,
+			.extra.single_queue = true}},
+#ifdef CC_AVX512_SUPPORT
+	[IDPF_RX_AVX512] = {
+		.pkt_burst = idpf_dp_splitq_recv_pkts_avx512,
+		.info = "Split AVX512 Vector",
+		.features = {
+			.rx_offloads = IDPF_RX_VECTOR_OFFLOADS,
+			.simd_width = RTE_VECT_SIMD_512}},
+	[IDPF_RX_SINGLQ_AVX512] = {
+		.pkt_burst = idpf_dp_singleq_recv_pkts_avx512,
+		.info = "Single AVX512 Vector",
+		.features = {
+			.rx_offloads = IDPF_RX_VECTOR_OFFLOADS,
+			.simd_width = RTE_VECT_SIMD_512,
+			.extra.single_queue = true}},
+#endif /* CC_AVX512_SUPPORT */
+#endif /* RTE_ARCH_X86 */
+};

drivers/net/intel/common/rx.h
@@ -131,6 +131,7 @@ struct ci_rx_path_features_extra {
 	bool flex_desc;
 	bool bulk_alloc;
 	bool disabled;
+	bool single_queue;
 };
 
 struct ci_rx_path_features {
@@ -276,6 +277,10 @@ ci_rx_path_select(struct ci_rx_path_features req_features,
 
 		/* If requested, ensure the path uses the flexible descriptor. */
 		if (path_features->extra.flex_desc != req_features.extra.flex_desc)
+			continue;
+
+		/* If requested, ensure the path supports single queue RX. */
+		if (path_features->extra.single_queue != req_features.extra.single_queue)
 			continue;
 
 		/* If requested, ensure the path supports scattered RX. */

drivers/net/intel/idpf/idpf_common_rxtx.h
@@ -11,6 +11,7 @@
 
 #include "idpf_common_device.h"
 #include "../common/tx.h"
+#include "../common/rx.h"
 
 #define IDPF_RX_MAX_BURST		32
 
@@ -95,6 +96,15 @@
 
 #define IDPF_RX_SPLIT_BUFQ1_ID	1
 #define IDPF_RX_SPLIT_BUFQ2_ID	2
+
+#define IDPF_RX_SCALAR_OFFLOADS (			\
+		RTE_ETH_RX_OFFLOAD_IPV4_CKSUM |	\
+		RTE_ETH_RX_OFFLOAD_UDP_CKSUM |	\
+		RTE_ETH_RX_OFFLOAD_TCP_CKSUM |	\
+		RTE_ETH_RX_OFFLOAD_OUTER_IPV4_CKSUM |	\
+		RTE_ETH_RX_OFFLOAD_TIMESTAMP | \
+		RTE_ETH_RX_OFFLOAD_SCATTER)
+#define IDPF_RX_VECTOR_OFFLOADS 0
 
 struct idpf_rx_stats {
 	RTE_ATOMIC(uint64_t) mbuf_alloc_failed;
@@ -252,5 +262,7 @@ __rte_internal
 uint16_t idpf_dp_singleq_xmit_pkts_avx2(void *tx_queue,
 					struct rte_mbuf **tx_pkts,
 					uint16_t nb_pkts);
+
+extern const struct ci_rx_path_info idpf_rx_path_infos[IDPF_RX_MAX];
 
 #endif /* _IDPF_COMMON_RXTX_H_ */

drivers/net/intel/idpf/idpf_ethdev.c
@@ -694,6 +694,8 @@ idpf_dev_configure(struct rte_eth_dev *dev)
 		(dev->data->mtu == 0) ? IDPF_DEFAULT_MTU : dev->data->mtu +
 		IDPF_ETH_OVERHEAD;
 
+	vport->adapter->rx_func_type = IDPF_RX_DEFAULT;
+
 	return 0;
 }
 

drivers/net/intel/idpf/idpf_rxtx.c
@@ -760,97 +760,50 @@ void
 idpf_set_rx_function(struct rte_eth_dev *dev)
 {
 	struct idpf_vport *vport = dev->data->dev_private;
+	struct idpf_adapter *ad = vport->adapter;
+	struct ci_rx_path_features req_features = {
+		.rx_offloads = dev->data->dev_conf.rxmode.offloads,
+		.simd_width = RTE_VECT_SIMD_DISABLED,
+	};
 #ifdef RTE_ARCH_X86
 	struct idpf_rx_queue *rxq;
-	enum rte_vect_max_simd rx_simd_width = RTE_VECT_SIMD_DISABLED;
 	int i;
 
 	if (idpf_rx_vec_dev_check_default(dev) == IDPF_VECTOR_PATH &&
-	    rte_vect_get_max_simd_bitwidth() >= RTE_VECT_SIMD_128) {
-		vport->rx_vec_allowed = true;
-		rx_simd_width = idpf_get_max_simd_bitwidth();
-	} else {
-		vport->rx_vec_allowed = false;
-	}
+	    rte_vect_get_max_simd_bitwidth() >= RTE_VECT_SIMD_256)
+		req_features.simd_width = idpf_get_max_simd_bitwidth();
 #endif /* RTE_ARCH_X86 */
 
+	req_features.extra.single_queue = (vport->rxq_model == VIRTCHNL2_QUEUE_MODEL_SINGLE);
+	req_features.extra.scattered = dev->data->scattered_rx;
+
+	ad->rx_func_type = ci_rx_path_select(req_features,
+						&idpf_rx_path_infos[0],
+						IDPF_RX_MAX,
+						IDPF_RX_DEFAULT);
+
 #ifdef RTE_ARCH_X86
-	if (vport->rxq_model == VIRTCHNL2_QUEUE_MODEL_SPLIT) {
-		if (vport->rx_vec_allowed) {
+	if (idpf_rx_path_infos[ad->rx_func_type].features.simd_width >= RTE_VECT_SIMD_256) {
+		/* Vector function selected. Prepare the rxq accordingly. */
+		if (idpf_rx_path_infos[ad->rx_func_type].features.extra.single_queue) {
+			for (i = 0; i < dev->data->nb_rx_queues; i++) {
+				rxq = dev->data->rx_queues[i];
+				(void)idpf_qc_singleq_rx_vec_setup(rxq);
+			}
+		} else {
 			for (i = 0; i < dev->data->nb_rx_queues; i++) {
 				rxq = dev->data->rx_queues[i];
 				(void)idpf_qc_splitq_rx_vec_setup(rxq);
 			}
-#ifdef CC_AVX512_SUPPORT
-			if (rx_simd_width == RTE_VECT_SIMD_512) {
-				PMD_DRV_LOG(NOTICE,
-					    "Using Split AVX512 Vector Rx (port %d).",
-					    dev->data->port_id);
-				dev->rx_pkt_burst = idpf_dp_splitq_recv_pkts_avx512;
-				return;
-			}
-#endif /* CC_AVX512_SUPPORT */
-		}
-		PMD_DRV_LOG(NOTICE,
-			    "Using Split Scalar Rx (port %d).",
-			    dev->data->port_id);
-		dev->rx_pkt_burst = idpf_dp_splitq_recv_pkts;
-	} else {
-		if (vport->rx_vec_allowed) {
-			for (i = 0; i < dev->data->nb_tx_queues; i++) {
-				rxq = dev->data->rx_queues[i];
-				(void)idpf_qc_singleq_rx_vec_setup(rxq);
-			}
-#ifdef CC_AVX512_SUPPORT
-			if (rx_simd_width == RTE_VECT_SIMD_512) {
-				PMD_DRV_LOG(NOTICE,
-					    "Using Single AVX512 Vector Rx (port %d).",
-					    dev->data->port_id);
-				dev->rx_pkt_burst = idpf_dp_singleq_recv_pkts_avx512;
-				return;
-			}
-#endif /* CC_AVX512_SUPPORT */
-			if (rx_simd_width == RTE_VECT_SIMD_256) {
-				PMD_DRV_LOG(NOTICE,
-					    "Using Single AVX2 Vector Rx (port %d).",
-					    dev->data->port_id);
-				dev->rx_pkt_burst = idpf_dp_singleq_recv_pkts_avx2;
-				return;
-			}
 		}
 
-		if (dev->data->scattered_rx) {
-			PMD_DRV_LOG(NOTICE,
-				    "Using Single Scalar Scatterd Rx (port %d).",
-				    dev->data->port_id);
-			dev->rx_pkt_burst = idpf_dp_singleq_recv_scatter_pkts;
-			return;
-		}
-		PMD_DRV_LOG(NOTICE,
-			    "Using Single Scalar Rx (port %d).",
-			    dev->data->port_id);
-		dev->rx_pkt_burst = idpf_dp_singleq_recv_pkts;
 	}
-#else
-	if (vport->rxq_model == VIRTCHNL2_QUEUE_MODEL_SPLIT) {
-		PMD_DRV_LOG(NOTICE,
-			    "Using Split Scalar Rx (port %d).",
-			    dev->data->port_id);
-		dev->rx_pkt_burst = idpf_dp_splitq_recv_pkts;
-	} else {
-		if (dev->data->scattered_rx) {
-			PMD_DRV_LOG(NOTICE,
-				    "Using Single Scalar Scatterd Rx (port %d).",
-				    dev->data->port_id);
-			dev->rx_pkt_burst = idpf_dp_singleq_recv_scatter_pkts;
-			return;
-		}
-		PMD_DRV_LOG(NOTICE,
-			    "Using Single Scalar Rx (port %d).",
-			    dev->data->port_id);
-		dev->rx_pkt_burst = idpf_dp_singleq_recv_pkts;
-	}
-#endif /* RTE_ARCH_X86 */
+#endif
+
+	dev->rx_pkt_burst = idpf_rx_path_infos[ad->rx_func_type].pkt_burst;
+	PMD_DRV_LOG(NOTICE, "Using %s Rx (port %d).",
+			idpf_rx_path_infos[ad->rx_func_type].info, dev->data->port_id);
+
 }
 
 void

commit 21c1fe280a2480dcd7edbb3e51cc2f9a37c1c6e0

net/idpf: fix enum constant name
Correct the typo in the idpf_rx_func_type enum by renaming
IDPF_RX_SINGLQ_AVX512 to IDPF_RX_SINGLEQ_AVX512 for consistency
and improved readability.

Fixes: 12b1c2901761 ("net/idpf: use common Rx path selection infrastructure")
Cc: stable@dpdk.org

Signed-off-by: Shaiq Wani <shaiq.wani@intel.com>
Acked-by: Anatoly Burakov <anatoly.burakov@intel.com>

drivers/net/intel/idpf/idpf_common_device.h
@@ -53,7 +53,7 @@ enum idpf_rx_func_type {
 	IDPF_RX_SINGLEQ_SCATTERED,
 	IDPF_RX_SINGLEQ_AVX2,
 	IDPF_RX_AVX512,
-	IDPF_RX_SINGLQ_AVX512,
+	IDPF_RX_SINGLEQ_AVX512,
 	IDPF_RX_MAX
 };
 

drivers/net/intel/idpf/idpf_common_rxtx.c
@@ -1691,7 +1691,7 @@ const struct ci_rx_path_info idpf_rx_path_infos[] = {
 		.features = {
 			.rx_offloads = IDPF_RX_VECTOR_OFFLOADS,
 			.simd_width = RTE_VECT_SIMD_512}},
-	[IDPF_RX_SINGLQ_AVX512] = {
+	[IDPF_RX_SINGLEQ_AVX512] = {
 		.pkt_burst = idpf_dp_singleq_recv_pkts_avx512,
 		.info = "Single AVX512 Vector",
 		.features = {