4d8e2937e2fed7dd8d27a22e822e606202eba2cc
Chengwen Feng <fengchengwen@huawei.com>
2025-10-13 17:22:11 +0800

    dma/hisilicon: fix stop with pending transfers
    [ upstream commit 8aa458f1c44b9f6e73f75047aca77191dc79746f ]
    
    Stop dmadev may fail if there are pending DMA transfers, we need make
    sure there are no pending DMA transfers when stop.
    
    This commit uses following scheme:
    1. flag stop proc so that new request will not process.
    2. setting drop flag for all descriptor to quick complete.
    3. waiting dmadev to complete.
    
    Fixes: 3c5f5f03a047 ("dma/hisilicon: add control path")
    
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>

--- drivers/dma/hisilicon/hisi_dmadev.c
+++ drivers/dma/hisilicon/hisi_dmadev.c
@@ -378,6 +378,7 @@ hisi_dma_start(struct rte_dma_dev *dev)
 	hw->cq_head = 0;
 	hw->cqs_completed = 0;
 	hw->cqe_vld = 1;
+	hw->stop_proc = 0;
 	hw->submitted = 0;
 	hw->completed = 0;
 	hw->errors = 0;
@@ -387,12 +388,6 @@ hisi_dma_start(struct rte_dma_dev *dev)
 				  HISI_DMA_QUEUE_CTRL0_EN_B, true);
 
 	return 0;
-}
-
-static int
-hisi_dma_stop(struct rte_dma_dev *dev)
-{
-	return hisi_dma_reset_hw(dev->data->dev_private);
 }
 
 static int
@@ -454,6 +449,37 @@ hisi_dma_vchan_status(const struct rte_dma_dev *dev, uint16_t vchan,
 		*status = RTE_DMA_VCHAN_HALTED_ERROR;
 
 	return 0;
+}
+
+static int
+hisi_dma_stop(struct rte_dma_dev *dev)
+{
+#define MAX_WAIT_MSEC	10
+	struct hisi_dma_dev *hw = dev->data->dev_private;
+	enum rte_dma_vchan_status status;
+	uint32_t i;
+
+	/* Flag stop processing new requests. */
+	hw->stop_proc = 1;
+	rte_delay_ms(1);
+
+	/* Force set drop flag so that the hardware can quickly complete. */
+	for (i = 0; i <= hw->sq_depth_mask; i++)
+		hw->sqe[i].dw0 |= SQE_DROP_FLAG;
+
+	i = 0;
+	do {
+		hisi_dma_vchan_status(dev, 0, &status);
+		if (status != RTE_DMA_VCHAN_ACTIVE)
+			break;
+		rte_delay_ms(1);
+	} while (i++ < MAX_WAIT_MSEC);
+	if (status == RTE_DMA_VCHAN_ACTIVE) {
+		HISI_DMA_ERR(hw, "dev is still active!");
+		return -EBUSY;
+	}
+
+	return hisi_dma_reset_hw(dev->data->dev_private);
 }
 
 static void
@@ -550,14 +576,14 @@ hisi_dma_dump(const struct rte_dma_dev *dev, FILE *f)
 		"    revision: 0x%x queue_id: %u ring_size: %u\n"
 		"    ridx: %u cridx: %u\n"
 		"    sq_head: %u sq_tail: %u cq_sq_head: %u\n"
-		"    cq_head: %u cqs_completed: %u cqe_vld: %u\n"
+		"    cq_head: %u cqs_completed: %u cqe_vld: %u stop_proc: %u\n"
 		"    submitted: %" PRIu64 " completed: %" PRIu64 " errors: %"
 		PRIu64 " qfulls: %" PRIu64 "\n",
 		hw->revision, hw->queue_id,
 		hw->sq_depth_mask > 0 ? hw->sq_depth_mask + 1 : 0,
 		hw->ridx, hw->cridx,
 		hw->sq_head, hw->sq_tail, hw->cq_sq_head,
-		hw->cq_head, hw->cqs_completed, hw->cqe_vld,
+		hw->cq_head, hw->cqs_completed, hw->cqe_vld, hw->stop_proc,
 		hw->submitted, hw->completed, hw->errors, hw->qfulls);
 	hisi_dma_dump_queue(hw, f);
 	hisi_dma_dump_common(hw, f);
@@ -574,6 +600,9 @@ hisi_dma_copy(void *dev_private, uint16_t vchan,
 	struct hisi_dma_sqe *sqe = &hw->sqe[hw->sq_tail];
 
 	RTE_SET_USED(vchan);
+
+	if (unlikely(hw->stop_proc > 0))
+		return -EPERM;
 
 	if (((hw->sq_tail + 1) & hw->sq_depth_mask) == hw->sq_head) {
 		hw->qfulls++;
