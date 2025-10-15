e816819ba8dee9f421c4a45b0ef71cebcdf24f12
Stephen Hemminger <stephen@networkplumber.org>
2025-10-15 11:59:46 -0700

    dmadev: replace zero-length array
    Use of zero length arrays is GCC extension.
    Found by zero_length_array.cocci.
    
    Fixes: 9674119fbf27 ("dmadev: add enqueue/dequeue operations")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Acked-by: Morten Brørup <mb@smartsharesystems.com>
    Acked-by: Pavan Nikhilesh <pbhagavatula@marvell.com>

--- lib/dmadev/rte_dmadev.h
+++ lib/dmadev/rte_dmadev.h
@@ -1100,7 +1100,7 @@ struct rte_dma_op {
 	/** Number of destination segments. */
 	uint16_t nb_dst;
 	/** Source and destination segments. */
-	struct rte_dma_sge src_dst_seg[0];
+	struct rte_dma_sge src_dst_seg[];
 };
 
 #ifdef __cplusplus
