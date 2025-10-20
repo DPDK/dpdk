DPDK/dpdk

commit 11c0663c94454a103ea74d50c9436af4ef5aadd8

net/netvsc: add function to get vmbus device
Introduce a function get the vmbus device from hn_data. For secondary
process, the vmbus device is in eth_dev's private region.

Signed-off-by: Long Li <longli@microsoft.com>

drivers/net/netvsc/hn_nvs.c
@@ -44,6 +44,21 @@ static const uint32_t hn_nvs_version[] = {
 	NVS_VERSION_1
 };
 
+struct rte_vmbus_device *get_vmbus_device(struct hn_data *hv)
+{
+	struct rte_vmbus_device *vmbus = hv->vmbus;
+
+	/* For secondary process, vmbus is in the eth_dev private */
+	if (rte_eal_process_type() == RTE_PROC_SECONDARY) {
+		struct rte_eth_dev *dev = &rte_eth_devices[hv->port_id];
+		struct hn_nvs_process_priv *process_priv = dev->process_private;
+
+		vmbus = process_priv->vmbus_dev;
+	}
+
+	return vmbus;
+}
+
 static int hn_nvs_req_send(struct hn_data *hv,
 			   void *req, uint32_t reqlen)
 {

drivers/net/netvsc/hn_nvs.h
@@ -221,6 +221,8 @@ void	hn_nvs_handle_vfassoc(struct rte_eth_dev *dev,
 			      const struct vmbus_chanpkt_hdr *hdr,
 			      const void *data);
 
+struct rte_vmbus_device *get_vmbus_device(struct hn_data *hv);
+
 static inline int
 hn_nvs_send(struct vmbus_channel *chan, uint16_t flags,
 	    void *nvs_msg, int nvs_msglen, uintptr_t sndc,

commit e1e27de3fa79a9665dda721a6ceae3f96179be5f

net/netvsc: rename internal function with prefix
All global functions should use a common prefix. For netvsc all
globals start with hn_. Rename get_vmbus_device to
hn_nvs_get_vmbus_device to follow pattern used in this code.

Fixes: 11c0663c9445 ("net/netvsc: add function to get vmbus device")

Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
Reviewed-by: Long Li <longli@microsoft.com>

drivers/net/netvsc/hn_nvs.c
@@ -44,7 +44,7 @@ static const uint32_t hn_nvs_version[] = {
 	NVS_VERSION_1
 };
 
-struct rte_vmbus_device *get_vmbus_device(struct hn_data *hv)
+struct rte_vmbus_device *hn_nvs_get_vmbus_device(struct hn_data *hv)
 {
 	struct rte_vmbus_device *vmbus = hv->vmbus;
 
@@ -62,7 +62,7 @@ struct rte_vmbus_device *get_vmbus_device(struct hn_data *hv)
 static int hn_nvs_req_send(struct hn_data *hv,
 			   void *req, uint32_t reqlen)
 {
-	return rte_vmbus_chan_send(get_vmbus_device(hv), hn_primary_chan(hv),
+	return rte_vmbus_chan_send(hn_nvs_get_vmbus_device(hv), hn_primary_chan(hv),
 				   VMBUS_CHANPKT_TYPE_INBAND,
 				   req, reqlen, 0,
 				   VMBUS_CHANPKT_FLAG_NONE, NULL);
@@ -82,7 +82,7 @@ __hn_nvs_execute(struct hn_data *hv,
 	int ret;
 
 	/* Send request to ring buffer */
-	ret = rte_vmbus_chan_send(get_vmbus_device(hv), chan,
+	ret = rte_vmbus_chan_send(hn_nvs_get_vmbus_device(hv), chan,
 				  VMBUS_CHANPKT_TYPE_INBAND, req, reqlen, 0,
 				  VMBUS_CHANPKT_FLAG_RC, NULL);
 
@@ -93,7 +93,7 @@ __hn_nvs_execute(struct hn_data *hv,
 
  retry:
 	len = sizeof(buffer);
-	ret = rte_vmbus_chan_recv(get_vmbus_device(hv), chan, buffer, &len, &xactid);
+	ret = rte_vmbus_chan_recv(hn_nvs_get_vmbus_device(hv), chan, buffer, &len, &xactid);
 	if (ret == -EAGAIN) {
 		rte_delay_us(HN_CHAN_INTERVAL_US);
 		goto retry;
@@ -530,7 +530,7 @@ hn_nvs_ack_rxbuf(struct hn_data *hv, struct vmbus_channel *chan, uint64_t tid)
 	PMD_RX_LOG(DEBUG, "ack RX id %" PRIu64, tid);
 
  again:
-	error = rte_vmbus_chan_send(get_vmbus_device(hv), chan,
+	error = rte_vmbus_chan_send(hn_nvs_get_vmbus_device(hv), chan,
 				    VMBUS_CHANPKT_TYPE_COMP, &ack, sizeof(ack),
 				    tid, VMBUS_CHANPKT_FLAG_NONE, NULL);
 

drivers/net/netvsc/hn_nvs.h
@@ -222,14 +222,14 @@ void	hn_nvs_handle_vfassoc(struct rte_eth_dev *dev,
 			      const struct vmbus_chanpkt_hdr *hdr,
 			      const void *data);
 
-struct rte_vmbus_device *get_vmbus_device(struct hn_data *hv);
+struct rte_vmbus_device *hn_nvs_get_vmbus_device(struct hn_data *hv);
 
 static inline int
 hn_nvs_send(struct hn_data *hv, struct vmbus_channel *chan,
 	    uint16_t flags, void *nvs_msg, int nvs_msglen, uintptr_t sndc,
 	    bool *need_sig)
 {
-	return rte_vmbus_chan_send(get_vmbus_device(hv), chan, VMBUS_CHANPKT_TYPE_INBAND,
+	return rte_vmbus_chan_send(hn_nvs_get_vmbus_device(hv), chan, VMBUS_CHANPKT_TYPE_INBAND,
 				   nvs_msg, nvs_msglen, (uint64_t)sndc,
 				   flags, need_sig);
 }
@@ -240,6 +240,6 @@ hn_nvs_send_sglist(struct hn_data *hv, struct vmbus_channel *chan,
 		   void *nvs_msg, int nvs_msglen,
 		   uintptr_t sndc, bool *need_sig)
 {
-	return rte_vmbus_chan_send_sglist(get_vmbus_device(hv), chan, sg, sglen, nvs_msg,
+	return rte_vmbus_chan_send_sglist(hn_nvs_get_vmbus_device(hv), chan, sg, sglen, nvs_msg,
 					  nvs_msglen, (uint64_t)sndc, need_sig);
 }

drivers/net/netvsc/hn_rxtx.c
@@ -1176,7 +1176,7 @@ retry:
 	}
 
 	if (bytes_read > 0)
-		rte_vmbus_chan_signal_read(get_vmbus_device(hv), rxq->chan, bytes_read);
+		rte_vmbus_chan_signal_read(hn_nvs_get_vmbus_device(hv), rxq->chan, bytes_read);
 
 	rte_spinlock_unlock(&rxq->ring_lock);
 
@@ -1641,7 +1641,7 @@ hn_xmit_pkts(void *ptxq, struct rte_mbuf **tx_pkts, uint16_t nb_pkts)
 
 fail:
 	if (need_sig)
-		rte_vmbus_chan_signal_tx(get_vmbus_device(hv), txq->chan);
+		rte_vmbus_chan_signal_tx(hn_nvs_get_vmbus_device(hv), txq->chan);
 
 	return nb_tx;
 }