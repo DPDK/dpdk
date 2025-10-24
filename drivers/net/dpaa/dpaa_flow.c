d546ee6c680304731a7b5cfcf85ef7888d640ada
Vanshika Shukla <vanshika.shukla@nxp.com>
2025-10-24 11:49:06 +0530

    net/dpaa: fix resource leak coverity issue
    This patch correct the cleanup order of PCD and
    FMan handles to prevent resource leaks reported by
    coverity tool.
    
    Coverity issue: 362787
    Fixes: 4defbc8cbb6d ("net/dpaa: support FMCless mode")
    Cc: sachin.saxena@nxp.com
    Cc: stable@dpdk.org
    
    Signed-off-by: Vanshika Shukla <vanshika.shukla@nxp.com>

--- drivers/net/dpaa/dpaa_flow.c
+++ drivers/net/dpaa/dpaa_flow.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2017-2019,2021-2024 NXP
+ * Copyright 2017-2019,2021-2025 NXP
  */
 
 /* System headers */
@@ -889,9 +889,9 @@ int dpaa_fm_init(void)
 	/* FM PCD Enable */
 	ret = fm_pcd_enable(pcd_handle);
 	if (ret) {
-		fm_close(fman_handle);
-		fm_pcd_close(pcd_handle);
 		DPAA_PMD_ERR("fm_pcd_enable: Failed");
+		fm_pcd_close(pcd_handle);
+		fm_close(fman_handle);
 		return -1;
 	}
 
