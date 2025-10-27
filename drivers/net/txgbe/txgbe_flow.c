af96a6bc869e4c86b579f37c6da8007cf5eda8df
Jiawen Wu <jiawenwu@trustnetic.com>
2025-10-27 11:15:35 +0800

    net/txgbe: fix FDIR filter for SCTP tunnel
    [ upstream commit c9a341034a29bc5245dd9fc21678be0b30313394 ]
    
    This commit is the same as commit
    0db38d54b57a ("net/txgbe: fix to create FDIR filter for SCTP packet").
    
    The check for the mask of SCTP item is repeated and wrong, fix it to
    make it work.
    
    Fixes: a1851465f825 ("net/txgbe: fix to create FDIR filter for tunnel packet")
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>

--- drivers/net/txgbe/txgbe_flow.c
+++ drivers/net/txgbe/txgbe_flow.c
@@ -2783,19 +2783,6 @@ txgbe_parse_fdir_filter_tunnel(const struct rte_flow_attr *attr,
 			rule->input.dst_port =
 				sctp_spec->hdr.dst_port;
 		}
-		/* others even sctp port is not supported */
-		sctp_mask = item->mask;
-		if (sctp_mask &&
-		    (sctp_mask->hdr.src_port ||
-		     sctp_mask->hdr.dst_port ||
-		     sctp_mask->hdr.tag ||
-		     sctp_mask->hdr.cksum)) {
-			memset(rule, 0, sizeof(struct txgbe_fdir_rule));
-			rte_flow_error_set(error, EINVAL,
-					   RTE_FLOW_ERROR_TYPE_ITEM,
-					   item, "Not supported by fdir filter");
-			return -rte_errno;
-		}
 	}
 
 	if (item->type != RTE_FLOW_ITEM_TYPE_END) {
