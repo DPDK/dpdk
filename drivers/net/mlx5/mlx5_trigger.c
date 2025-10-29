09948bea9851fd1d599cb2d4eb14bdd695160257
Michael Baum <michaelba@nvidia.com>
2025-10-29 17:57:08 +0200

    net/mlx5: fix multi-process Tx default rules
    [ upstream commit 2f1bb792ad51aeb2da00198a63422fc478131bd5 ]
    
    When representor matching is disabled, an egress default rule is
    inserted which matches all and copies REG_A to REG_C_1 (when dv_xmeta_en
    == 4) and jump to group 1. All user rules started from group 1.
    
    When 2 processes are working together, the first one creates this flow
    rule and the second one is failed with errno EEXIST. This renders all
    user egress rules in 2nd process to be invalid.
    
    This patch changes this default rule match on SQs.
    
    Fixes: 483181f7b6dd ("net/mlx5: support device control of representor matching")
    
    Signed-off-by: Michael Baum <michaelba@nvidia.com>
    Acked-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

--- drivers/net/mlx5/mlx5_trigger.c
+++ drivers/net/mlx5/mlx5_trigger.c
@@ -1479,18 +1479,6 @@ mlx5_traffic_enable_hws(struct rte_eth_dev *dev)
 	unsigned int i;
 	int ret;
 
-	/*
-	 * With extended metadata enabled, the Tx metadata copy is handled by default
-	 * Tx tagging flow rules, so default Tx flow rule is not needed. It is only
-	 * required when representor matching is disabled.
-	 */
-	if (config->dv_esw_en &&
-	    !config->repr_matching &&
-	    config->dv_xmeta_en == MLX5_XMETA_MODE_META32_HWS &&
-	    priv->master) {
-		if (mlx5_flow_hw_create_tx_default_mreg_copy_flow(dev))
-			goto error;
-	}
 	for (i = 0; i < priv->txqs_n; ++i) {
 		struct mlx5_txq_ctrl *txq = mlx5_txq_get(dev, i);
 		uint32_t queue;
@@ -1508,6 +1496,19 @@ mlx5_traffic_enable_hws(struct rte_eth_dev *dev)
 		}
 		if (config->dv_esw_en && config->repr_matching) {
 			if (mlx5_flow_hw_tx_repr_matching_flow(dev, queue, false)) {
+				mlx5_txq_release(dev, i);
+				goto error;
+			}
+		}
+		/*
+		 * With extended metadata enabled, the Tx metadata copy is handled by default
+		 * Tx tagging flow rules, so default Tx flow rule is not needed. It is only
+		 * required when representor matching is disabled.
+		 */
+		if (config->dv_esw_en && !config->repr_matching &&
+		    config->dv_xmeta_en == MLX5_XMETA_MODE_META32_HWS &&
+		    (priv->master || priv->representor)) {
+			if (mlx5_flow_hw_create_tx_default_mreg_copy_flow(dev, queue, false)) {
 				mlx5_txq_release(dev, i);
 				goto error;
 			}
