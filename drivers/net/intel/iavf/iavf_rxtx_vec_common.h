1aaa183a9fa499bd28ee7738c0893b3033f620e0
Ciara Loftus <ciara.loftus@intel.com>
2025-11-06 11:50:11 +0000

    net/iavf: fix compilation with QinQ offload disabled
    If IAVF_TX_VLAN_QINQ_OFFLOAD is undefined there will be an unused
    variable warning. Fix this.
    
    Fixes: 3aa4efa36438 ("net/iavf: support VLAN insertion in AVX512 Tx")
    
    Signed-off-by: Ciara Loftus <ciara.loftus@intel.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- drivers/net/intel/iavf/iavf_rxtx_vec_common.h
+++ drivers/net/intel/iavf/iavf_rxtx_vec_common.h
@@ -174,6 +174,8 @@ iavf_txd_enable_offload(__rte_unused struct rte_mbuf *tx_pkt,
 	uint32_t td_offset = 0;
 #endif
 
+	RTE_SET_USED(vlan_flag);
+
 #ifdef IAVF_TX_CSUM_OFFLOAD
 	/* Set MACLEN */
 	if (ol_flags & RTE_MBUF_F_TX_TUNNEL_MASK)
