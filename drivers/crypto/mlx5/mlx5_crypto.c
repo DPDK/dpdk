54fe33798a76d764cef628bc3b7c192e8c55ac98
Raslan Darawsheh <rasland@nvidia.com>
2025-11-11 14:16:28 +0200

    common/mlx5: add BlueField-4 DPU
    This adds the BlueField-4 device ID to the list of
    NVIDIA devices that run the mlx5 drivers.
    The device is still in development stage.
    
    Signed-off-by: Raslan Darawsheh <rasland@nvidia.com>

--- drivers/crypto/mlx5/mlx5_crypto.c
+++ drivers/crypto/mlx5/mlx5_crypto.c
@@ -492,6 +492,10 @@ static const struct rte_pci_id mlx5_crypto_pci_id_map[] = {
 					PCI_DEVICE_ID_MELLANOX_CONNECTX9)
 		},
 		{
+			RTE_PCI_DEVICE(PCI_VENDOR_ID_MELLANOX,
+					PCI_DEVICE_ID_MELLANOX_BLUEFIELD4)
+		},
+		{
 			.vendor_id = 0
 		}
 };
