f3ede4f9c5bb71153557a7b0fc3687f0843e3ed6
Gregory Etelson <getelson@nvidia.com>
2025-11-12 18:24:40 +0200

    net/mlx5: fix send to kernel action resources release
    [ upstream commit 472b0994319198090e44a7c2de1e43f0a0e0a270 ]
    
    In the MLX5 PMD hierarchy a flow table is bound to a domain object.
    A domain object can be released if it does not reference any flow
    tables.
    
    When the PMD creates a send to kernel flow action, it also creates
    a dedicated flow table for that action.
    
    The PMD called for Rx, Tx and FDB domain destruction before it
    destroyed send to kernel resources - flow action and flow table.
    As a result, domain destruction could not be completed.
    
    The patch moves send to kernel actions and tables destruction before
    domain destruction.
    
    Fixes: f31a141e6478 ("net/mlx5: add send to kernel action resource holder")
    
    Signed-off-by: Gregory Etelson <getelson@nvidia.com>
    Acked-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

--- drivers/net/mlx5/linux/mlx5_os.c
+++ drivers/net/mlx5/linux/mlx5_os.c
@@ -705,6 +705,26 @@ error:
 	return err;
 }
 
+#ifdef HAVE_MLX5DV_DR
+static void
+mlx5_destroy_send_to_kernel_action(struct mlx5_dev_ctx_shared *sh)
+{
+	if (sh->send_to_kernel_action.action) {
+		void *action = sh->send_to_kernel_action.action;
+
+		mlx5_glue->destroy_flow_action(action);
+		sh->send_to_kernel_action.action = NULL;
+	}
+	if (sh->send_to_kernel_action.tbl) {
+		struct mlx5_flow_tbl_resource *tbl =
+				sh->send_to_kernel_action.tbl;
+
+		flow_dv_tbl_resource_release(sh, tbl);
+		sh->send_to_kernel_action.tbl = NULL;
+	}
+}
+#endif /* HAVE_MLX5DV_DR */
+
 /**
  * Destroy DR related data within private structure.
  *
@@ -721,6 +741,7 @@ mlx5_os_free_shared_dr(struct mlx5_priv *priv)
 		return;
 	MLX5_ASSERT(LIST_EMPTY(&sh->shared_rxqs));
 #ifdef HAVE_MLX5DV_DR
+	mlx5_destroy_send_to_kernel_action(sh);
 	if (sh->rx_domain) {
 		mlx5_glue->dr_destroy_domain(sh->rx_domain);
 		sh->rx_domain = NULL;
@@ -742,19 +763,6 @@ mlx5_os_free_shared_dr(struct mlx5_priv *priv)
 	if (sh->pop_vlan_action) {
 		mlx5_glue->destroy_flow_action(sh->pop_vlan_action);
 		sh->pop_vlan_action = NULL;
-	}
-	if (sh->send_to_kernel_action.action) {
-		void *action = sh->send_to_kernel_action.action;
-
-		mlx5_glue->destroy_flow_action(action);
-		sh->send_to_kernel_action.action = NULL;
-	}
-	if (sh->send_to_kernel_action.tbl) {
-		struct mlx5_flow_tbl_resource *tbl =
-				sh->send_to_kernel_action.tbl;
-
-		flow_dv_tbl_resource_release(sh, tbl);
-		sh->send_to_kernel_action.tbl = NULL;
 	}
 #endif /* HAVE_MLX5DV_DR */
 	if (sh->default_miss_action)
