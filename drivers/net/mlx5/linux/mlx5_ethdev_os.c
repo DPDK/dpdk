abf4c84c1520b88f8527a69fe1f6ee1a3dd3292c
Stephen Hemminger <stephen@networkplumber.org>
2025-11-14 09:49:12 -0800

    net/mlx5: fix uninitialized variable
    [ upstream commit 7429374afba9827a43cf2efabce14e27ccc4bdef ]
    
    Gcc-16 detects use of uninitialized variable.
    If the retry loop exits the code would do memcmp against uninitialized
    stack value. Resolve by initializing to zero.
    
    Bugzilla ID: 1823
    Fixes: 1256805dd54d ("net/mlx5: move Linux-specific functions")
    Fixes: cfee94752b8f ("net/mlx5: fix link status to use wait to complete")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Raslan Darawsheh <rasland@nvidia.com>

--- drivers/net/mlx5/linux/mlx5_ethdev_os.c
+++ drivers/net/mlx5/linux/mlx5_ethdev_os.c
@@ -511,7 +511,7 @@ int
 mlx5_link_update(struct rte_eth_dev *dev, int wait_to_complete)
 {
 	int ret;
-	struct rte_eth_link dev_link;
+	struct rte_eth_link dev_link = { 0 };
 	time_t start_time = time(NULL);
 	int retry = MLX5_GET_LINK_STATUS_RETRY_COUNT;
 
@@ -1965,4 +1965,3 @@ int mlx5_txpp_map_hca_bar(struct rte_eth_dev *dev)
 		rte_mem_unmap(base, MLX5_ST_SZ_BYTES(initial_seg));
 	return 0;
 }
-
