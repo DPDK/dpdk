21ff8f42be45ee8bdc08890898f20983a627c54b
Stephen Hemminger <stephen@networkplumber.org>
2026-01-21 13:06:25 -0800

    test/cfgfile: avoid temp filename truncation
    [ upstream commit 356956154480d2906f11ed959b3c62cc0c8649ad ]
    
    Although, it is not possible on Linux (which always uses /tmp)
    the compiler complains about possible snprintf() truncation.
    
    Use existing code to make empty tmp file which puts the
    OS specific code in one spot and avoids any races if
    two tests are run at once.
    
    Fixes: be22019a58c4 ("test: restore cfgfile tests")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Anatoly Burakov <anatoly.burakov@intel.com>
    Acked-by: Marat Khalili <marat.khalili@huawei.com>

--- app/test/test_cfgfile.c
+++ app/test/test_cfgfile.c
@@ -178,12 +178,8 @@ test_cfgfile_realloc_sections(void)
 	ret = remove(filename);
 	TEST_ASSERT_SUCCESS(ret, "Failed to remove file");
 
-	char tmp[PATH_MAX] = "/tmp/";
-#ifdef RTE_EXEC_ENV_WINDOWS
-	ret = GetTempPathA(sizeof(tmp), tmp);
-	TEST_ASSERT(ret > 0, "Failed to get tmp directory");
-#endif
-	snprintf(filename, sizeof(filename), "%s%s", tmp, "cfg_save.ini");
+	ret = make_tmp_file(filename, "save", "");
+	TEST_ASSERT(ret == 0, "Failed to make empty tmp filename for save");
 
 	ret = rte_cfgfile_save(cfgfile, filename);
 	TEST_ASSERT_SUCCESS(ret, "Failed to save to %s", filename);
