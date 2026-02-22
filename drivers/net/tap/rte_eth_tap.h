a817893cde772b47a69358efaa34a25ab744dac0
Stephen Hemminger <stephen@networkplumber.org>
2026-02-22 09:30:38 -0800

    net/tap: use correct length for interface names
    [ upstream commit 0e5bbe2a4b25aab4c83cf4234212a8064de18f52 ]
    
    The interface names in the driver internals are Linux
    kernel network interface names which have a maximum size of 16;
    not DPDK device names which can take up to 64 characters.
    
    Fixes: 02f96a0a82d1 ("net/tap: add TUN/TAP device PMD")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- drivers/net/tap/rte_eth_tap.h
+++ drivers/net/tap/rte_eth_tap.h
@@ -67,8 +67,8 @@ struct tx_queue {
 
 struct pmd_internals {
 	struct rte_eth_dev *dev;          /* Ethernet device. */
-	char remote_iface[RTE_ETH_NAME_MAX_LEN]; /* Remote netdevice name */
-	char name[RTE_ETH_NAME_MAX_LEN];  /* Internal Tap device name */
+	char remote_iface[IFNAMSIZ];	  /* Remote netdevice name */
+	char name[IFNAMSIZ];		  /* Internal Tap device name */
 	int type;                         /* Type field - TUN|TAP */
 	struct rte_ether_addr eth_addr;   /* Mac address of the device port */
 	struct ifreq remote_initial_flags;/* Remote netdevice flags on init */
