5b216d0427d77dee1ba0c1ca8a9c80d54890245e
Stephen Hemminger <stephen@networkplumber.org>
2026-03-05 09:50:55 -0800

    test: add pause to synchronization spinloops
    [ upstream commit 0df23c6d0714d5aa676c06b8c6037a0810222607 ]
    
    The atomic and thread tests use tight spinloops to synchronize.
    These spinloops lack rte_pause() which causes problems on high core
    count systems, particularly AMD Zen architectures where:
    
    - Tight spinloops without pause can starve SMT sibling threads
    - Memory ordering and store-buffer forwarding behave differently
    - Higher core counts amplify timing windows for race conditions
    
    This manifests as sporadic test failures on systems with 32+ cores
    that don't reproduce on smaller core count systems.
    
    Add rte_pause() to all seven synchronization spinloops to allow
    proper CPU resource sharing and improve memory ordering behavior.
    
    Fixes: af75078fece3 ("first public release")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- app/test/test_atomic.c
+++ app/test/test_atomic.c
@@ -15,6 +15,7 @@
 #include <rte_atomic.h>
 #include <rte_eal.h>
 #include <rte_lcore.h>
+#include <rte_pause.h>
 #include <rte_random.h>
 #include <rte_hash_crc.h>
 
@@ -113,7 +114,7 @@ test_atomic_usual(__rte_unused void *arg)
 	unsigned i;
 
 	while (rte_atomic32_read(&synchro) == 0)
-		;
+		rte_pause();
 
 	for (i = 0; i < N; i++)
 		rte_atomic16_inc(&a16);
@@ -149,7 +150,7 @@ static int
 test_atomic_tas(__rte_unused void *arg)
 {
 	while (rte_atomic32_read(&synchro) == 0)
-		;
+		rte_pause();
 
 	if (rte_atomic16_test_and_set(&a16))
 		rte_atomic64_inc(&count);
@@ -170,7 +171,7 @@ test_atomic_addsub_and_return(__rte_unused void *arg)
 	unsigned i;
 
 	while (rte_atomic32_read(&synchro) == 0)
-		;
+		rte_pause();
 
 	for (i = 0; i < N; i++) {
 		tmp16 = rte_atomic16_add_return(&a16, 1);
@@ -209,7 +210,7 @@ static int
 test_atomic_inc_and_test(__rte_unused void *arg)
 {
 	while (rte_atomic32_read(&synchro) == 0)
-		;
+		rte_pause();
 
 	if (rte_atomic16_inc_and_test(&a16)) {
 		rte_atomic64_inc(&count);
@@ -236,7 +237,7 @@ static int
 test_atomic_dec_and_test(__rte_unused void *arg)
 {
 	while (rte_atomic32_read(&synchro) == 0)
-		;
+		rte_pause();
 
 	if (rte_atomic16_dec_and_test(&a16))
 		rte_atomic64_inc(&count);
@@ -268,7 +269,7 @@ test_atomic128_cmp_exchange(__rte_unused void *arg)
 	unsigned int i;
 
 	while (rte_atomic32_read(&synchro) == 0)
-		;
+		rte_pause();
 
 	expected = count128;
 
@@ -406,7 +407,7 @@ test_atomic_exchange(__rte_unused void *arg)
 
 	/* Wait until all of the other threads have been dispatched */
 	while (rte_atomic32_read(&synchro) == 0)
-		;
+		rte_pause();
 
 	/*
 	 * Let the battle begin! Every thread attempts to steal the current
