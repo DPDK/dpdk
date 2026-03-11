db0e5d7aaa312a42fea6d4054f76e7ef28b933ef
Andrew Bailey <abailey@iol.unh.edu>
2026-03-11 12:03:06 -0400

    dts: make one link the default topology
    Currently the default topology for test suites when not specified
    is two link. However, the majority of the DPDK functions tested
    in DTS can be tested with a single link. It is more intuitive for
    a testsuite or testcase lacking a topology decorator to default
    to the more basic requirement of 1 link, and force tests which
    really do require two link to include decorators explicitly
    saying so.
    
    Signed-off-by: Andrew Bailey <abailey@iol.unh.edu>
    Signed-off-by: Dean Marx <dmarx@iol.unh.edu>
    Reviewed-by: Patrick Robb <probb@iol.unh.edu>
    Reviewed-by: Luca Vizzarro <luca.vizzarro@arm.com>

--- dts/api/capabilities.py
+++ dts/api/capabilities.py
@@ -63,7 +63,7 @@ class LinkTopology(IntEnum):
     @classmethod
     def default(cls) -> "LinkTopology":
         """The default topology required by test cases if not specified otherwise."""
-        return cls.TWO_LINKS
+        return cls.ONE_LINK
 
 
 class NicCapability(IntEnum):
