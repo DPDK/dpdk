DPDK/dpdk

commit 3dd316f03ec631cb3d1cc79347a69222840e7151

net/intel: fix comma operator warnings
Fix comma operator misuse warnings with clang compiler for intel PMDs
when -Wcomma enabled.

Bugzilla ID: 1809
Fixes: b3fa893d84d6 ("i40e: fix using memory after free")
Fixes: aed68d5b0e81 ("net/i40e: add AVX2 Tx function")
Fixes: e6a6a138919f ("net/i40e: add AVX512 vector path")
Fixes: af0c246a3800 ("net/iavf: enable AVX2 for iavf")
Fixes: 9ab9514c150e ("net/iavf: enable AVX512 for Tx")
Fixes: f9cf4f864150 ("net/ice: support device initialization")
Fixes: 2d5f6953d56d ("net/ice: support vector AVX2 in Tx")
Fixes: 7f85d5ebcfe1 ("net/ice: add AVX512 vector path")
Fixes: afac6261461f ("common/idpf: enable AVX2 for single queue Tx")
Fixes: 5bf87b45b2c8 ("net/idpf: add AVX512 data path for single queue model")
Fixes: e528d7c74819 ("common/idpf: add AVX512 data path for split queue model")
Cc: stable@dpdk.org

Signed-off-by: Venkatesh Vemula <venkatesh.vemula@intel.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

drivers/net/intel/idpf/idpf_common_rxtx_avx2.c
@@ -504,7 +504,7 @@ idpf_singleq_vtx(volatile struct ci_tx_desc *txdp,
 	/* if unaligned on 32-bit boundary, do one to align */
 	if (((uintptr_t)txdp & 0x1F) != 0 && nb_pkts != 0) {
 		idpf_singleq_vtx1(txdp, *pkt, flags);
-		nb_pkts--, txdp++, pkt++;
+		nb_pkts--; txdp++; pkt++;
 	}
 
 	/* do two at a time while possible, in bursts */
@@ -537,7 +537,7 @@ idpf_singleq_vtx(volatile struct ci_tx_desc *txdp,
 	/* do any last ones */
 	while (nb_pkts) {
 		idpf_singleq_vtx1(txdp, *pkt, flags);
-		txdp++, pkt++, nb_pkts--;
+		txdp++; pkt++; nb_pkts--;
 	}
 }
 

drivers/net/intel/i40e/i40e_ethdev.c
@@ -5108,16 +5108,14 @@ i40e_res_pool_destroy(struct i40e_res_pool_info *pool)
 	if (pool == NULL)
 		return;
 
-	for (entry = LIST_FIRST(&pool->alloc_list);
-			entry && (next_entry = LIST_NEXT(entry, next), 1);
-			entry = next_entry) {
+	for (entry = LIST_FIRST(&pool->alloc_list); entry; entry = next_entry) {
+		next_entry = LIST_NEXT(entry, next);
 		LIST_REMOVE(entry, next);
 		rte_free(entry);
 	}
 
-	for (entry = LIST_FIRST(&pool->free_list);
-			entry && (next_entry = LIST_NEXT(entry, next), 1);
-			entry = next_entry) {
+	for (entry = LIST_FIRST(&pool->free_list); entry; entry = next_entry) {
+		next_entry = LIST_NEXT(entry, next);
 		LIST_REMOVE(entry, next);
 		rte_free(entry);
 	}

drivers/net/intel/i40e/i40e_rxtx_vec_avx2.c
@@ -702,7 +702,7 @@ vtx(volatile struct ci_tx_desc *txdp,
 	/* if unaligned on 32-bit boundary, do one to align */
 	if (((uintptr_t)txdp & 0x1F) != 0 && nb_pkts != 0) {
 		vtx1(txdp, *pkt, flags);
-		nb_pkts--, txdp++, pkt++;
+		nb_pkts--; txdp++; pkt++;
 	}
 
 	/* do two at a time while possible, in bursts */
@@ -729,7 +729,7 @@ vtx(volatile struct ci_tx_desc *txdp,
 	/* do any last ones */
 	while (nb_pkts) {
 		vtx1(txdp, *pkt, flags);
-		txdp++, pkt++, nb_pkts--;
+		txdp++; pkt++; nb_pkts--;
 	}
 }
 

drivers/net/intel/i40e/i40e_rxtx_vec_avx512.c
@@ -789,7 +789,7 @@ vtx(volatile struct ci_tx_desc *txdp,
 	/* do any last ones */
 	while (nb_pkts) {
 		vtx1(txdp, *pkt, flags);
-		txdp++, pkt++, nb_pkts--;
+		txdp++; pkt++; nb_pkts--;
 	}
 }
 

drivers/net/intel/iavf/iavf_rxtx_vec_avx2.c
@@ -1653,7 +1653,7 @@ iavf_vtx(volatile struct ci_tx_desc *txdp,
 	/* if unaligned on 32-bit boundary, do one to align */
 	if (((uintptr_t)txdp & 0x1F) != 0 && nb_pkts != 0) {
 		iavf_vtx1(txdp, *pkt, flags, offload, vlan_flag);
-		nb_pkts--, txdp++, pkt++;
+		nb_pkts--; txdp++; pkt++;
 	}
 
 	/* do two at a time while possible, in bursts */
@@ -1694,7 +1694,7 @@ iavf_vtx(volatile struct ci_tx_desc *txdp,
 	/* do any last ones */
 	while (nb_pkts) {
 		iavf_vtx1(txdp, *pkt, flags, offload, vlan_flag);
-		txdp++, pkt++, nb_pkts--;
+		txdp++; pkt++; nb_pkts--;
 	}
 }
 

drivers/net/intel/iavf/iavf_rxtx_vec_avx512.c
@@ -1867,7 +1867,7 @@ iavf_vtx(volatile struct ci_tx_desc *txdp,
 	/* if unaligned on 32-bit boundary, do one to align */
 	if (((uintptr_t)txdp & 0x1F) != 0 && nb_pkts != 0) {
 		iavf_vtx1(txdp, *pkt, flags, offload, vlan_flag);
-		nb_pkts--, txdp++, pkt++;
+		nb_pkts--; txdp++; pkt++;
 	}
 
 	/* do 4 at a time while possible, in bursts */
@@ -1903,7 +1903,7 @@ iavf_vtx(volatile struct ci_tx_desc *txdp,
 	/* do any last ones */
 	while (nb_pkts) {
 		iavf_vtx1(txdp, *pkt, flags, offload, vlan_flag);
-		txdp++, pkt++, nb_pkts--;
+		txdp++; pkt++; nb_pkts--;
 	}
 }
 
@@ -2105,7 +2105,7 @@ ctx_vtx(volatile struct ci_tx_desc *txdp,
 	/* if unaligned on 32-bit boundary, do one to align */
 	if (((uintptr_t)txdp & 0x1F) != 0 && nb_pkts != 0) {
 		ctx_vtx1(txdp, *pkt, flags, offload, vlan_flag);
-		nb_pkts--, txdp++, pkt++;
+		nb_pkts--; txdp++; pkt++;
 	}
 
 	for (; nb_pkts > 1; txdp += 4, pkt += 2, nb_pkts -= 2) {

drivers/net/intel/ice/ice_ethdev.c
@@ -945,16 +945,14 @@ ice_res_pool_destroy(struct ice_res_pool_info *pool)
 	if (!pool)
 		return;
 
-	for (entry = LIST_FIRST(&pool->alloc_list);
-	     entry && (next_entry = LIST_NEXT(entry, next), 1);
-	     entry = next_entry) {
+	for (entry = LIST_FIRST(&pool->alloc_list); entry; entry = next_entry) {
+		next_entry = LIST_NEXT(entry, next);
 		LIST_REMOVE(entry, next);
 		rte_free(entry);
 	}
 
-	for (entry = LIST_FIRST(&pool->free_list);
-	     entry && (next_entry = LIST_NEXT(entry, next), 1);
-	     entry = next_entry) {
+	for (entry = LIST_FIRST(&pool->free_list); entry; entry = next_entry) {
+		next_entry = LIST_NEXT(entry, next);
 		LIST_REMOVE(entry, next);
 		rte_free(entry);
 	}

drivers/net/intel/ice/ice_rxtx_vec_avx2.c
@@ -796,7 +796,7 @@ ice_vtx(volatile struct ci_tx_desc *txdp,
 	/* if unaligned on 32-bit boundary, do one to align */
 	if (((uintptr_t)txdp & 0x1F) != 0 && nb_pkts != 0) {
 		ice_vtx1(txdp, *pkt, flags, offload);
-		nb_pkts--, txdp++, pkt++;
+		nb_pkts--; txdp++; pkt++;
 	}
 
 	/* do four at a time while possible, in bursts */
@@ -833,7 +833,7 @@ ice_vtx(volatile struct ci_tx_desc *txdp,
 	/* do any last ones */
 	while (nb_pkts) {
 		ice_vtx1(txdp, *pkt, flags, offload);
-		txdp++, pkt++, nb_pkts--;
+		txdp++; pkt++; nb_pkts--;
 	}
 }
 

commit b3b8301dee7f1fd3e6bc8c2d79fd6f6cc04a7d58

net/idpf: fix commas instead of semicolons
Fix comma operator misuse warnings when -Wcomma enabled.

Bugzilla ID: 1809
Fixes: 3dd316f03ec6 ("net/intel: fix comma operator warnings")
Fixes: 57560a92167a ("net/idpf: add AVX2 Tx path for split queue config")
Cc: stable@dpdk.org

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>

drivers/net/intel/idpf/idpf_common_rxtx_avx2.c
@@ -842,7 +842,7 @@ idpf_splitq_vtx_avx2(struct idpf_flex_tx_sched_desc *txdp,
 	/* align if needed */
 	if (((uintptr_t)txdp & 0x1F) != 0 && nb_pkts != 0) {
 		idpf_splitq_vtx1_avx2(txdp, *pkt, flags);
-		txdp++, pkt++, nb_pkts--;
+		txdp++; pkt++; nb_pkts--;
 	}
 
 	for (; nb_pkts >= IDPF_VPMD_DESCS_PER_LOOP; txdp += IDPF_VPMD_DESCS_PER_LOOP,