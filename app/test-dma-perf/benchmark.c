DPDK/dpdk

commit 2f2f7af667916409943793894eae45b028457f66

app/dma-perf: fix crash with IOVA as physical address
do_cpu_mem_copy uses DPDK API rte_mbuf_data_iova to return
the start of the virtual address for both src and dst.
But in case of iova mode set as PA, this results in seg-fault.
This is because rte_memcpy uses VA address and not PA.

This fix invokes `rte_pktmbuf_mtod` for both src and dst.

Bugzilla ID: 1269
Fixes: 623dc9364dc6 ("app/dma-perf: introduce DMA performance test")
Cc: stable@dpdk.org

Signed-off-by: Vipin Varghese <vipin.varghese@amd.com>
Acked-by: Anoob Joseph <anoobj@marvell.com>
Reviewed-by: Cheng Jiang <cheng1.jiang@intel.com>
Acked-by: Chengwen Feng <fengchengwen@huawei.com>
Acked-by: Huisong Li <lihuisong@huawei.com>

app/test-dma-perf/benchmark.c
@@ -288,10 +288,11 @@ do_cpu_mem_copy(void *p)
 
 	while (1) {
 		for (i = 0; i < nr_buf; i++) {
+			const void *src = rte_pktmbuf_mtod(dsts[i], void *);
+			void *dst = rte_pktmbuf_mtod(srcs[i], void *);
+
 			/* copy buffer form src to dst */
-			rte_memcpy((void *)(uintptr_t)rte_mbuf_data_iova(dsts[i]),
-				(void *)(uintptr_t)rte_mbuf_data_iova(srcs[i]),
-				(size_t)buf_size);
+			rte_memcpy(dst, src, (size_t)buf_size);
 			worker_info->total_cpl++;
 		}
 		if (worker_info->stop_flag)

commit e76312d0792e3bb8958cd4c16ed014d2890ae55d

app/dma-perf: fix reversed CPU copy
In do_cpu_mem_copy(), src and dst were reversed.

Fixes: 2f2f7af66791 ("app/dma-perf: fix crash with IOVA as physical address")
Cc: stable@dpdk.org

Signed-off-by: Liangxing Wang <wangliangxing@hygon.cn>
Acked-by: Chengwen Feng <fengchengwen@huawei.com>

.mailmap
@@ -912,7 +912,7 @@ Liang Ma <liangma@bytedance.com> <liang.j.ma@intel.com>
 Liang Xu <liang.xu@cinfotech.cn>
 Liang Zhang <zhangliang@bigo.sg>
 Liang-Min Larry Wang <liang-min.wang@intel.com>
-Liangxing Wang <liangxing.wang@arm.com>
+Liangxing Wang <wangliangxing@hygon.cn> <liangxing.wang@arm.com>
 Lihong Ma <lihongx.ma@intel.com>
 Lijian Zhang <lijian.zhang@arm.com>
 Lijie Shan <shan.lijie@zte.com.cn>

app/test-dma-perf/benchmark.c
@@ -520,10 +520,10 @@ do_cpu_mem_copy(void *p)
 
 	while (1) {
 		for (i = 0; i < nr_buf; i++) {
-			const void *src = rte_pktmbuf_mtod(dsts[i], void *);
-			void *dst = rte_pktmbuf_mtod(srcs[i], void *);
+			const void *src = rte_pktmbuf_mtod(srcs[i], void *);
+			void *dst = rte_pktmbuf_mtod(dsts[i], void *);
 
-			/* copy buffer form src to dst */
+			/* copy buffer from src to dst */
 			rte_memcpy(dst, src, (size_t)buf_size);
 			worker_info->total_cpl++;
 		}