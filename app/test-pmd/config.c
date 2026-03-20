f5f3f0d012122b3e23e31d2390b667c7f9e4b087
Talluri Chaitanyababu <chaitanyababux.talluri@intel.com>
2026-03-20 06:29:54 +0000

    app/testpmd: fix DCB forwarding TC mismatch handling
    [ upstream commit 388bb5d87a4e88182fc625f6d23e68fe790f2f25 ]
    
    Fix DCB forwarding failed when the number of TCs on ports is inconsistent.
    
    When ports have asymmetric TC configurations (e.g. 2 ports, port0 has
    4 TCs and port1 has 8 TCs), the forwarding logic iterates based only
    on the Rx port TC count.
    This can lead to accessing invalid Tx TC entries and incorrect queue
    mapping, which will result in a SIGFPE exception.
    
    Additionally, the existing VMDq pool guard in dcb_fwd_config_setup()
    only checks RX queue counts and does not consider the case where the TX
    port has no queues for a given pool/TC combination.
    
    Fix this by:
    1. Introducing an effective TC count using RTE_MIN() of Rx and Tx TC
       values, ensuring forwarding only operates on valid TCs supported by
       both ports.
    2. Updating the loop condition to use the effective TC count instead of
       only the Rx TC count.
    3. Extending the queue validation in dcb_fwd_config_setup() to ensure
       both Rx and Tx queues are valid for a given TC.
    
    Testpmd command to reproduce:
    
    x86_64-native-linuxapp-gcc/app/dpdk-testpmd -l 1-20 -n 4 \
    -a 0000:31:00.0 -a 0000:4b:00.0 \
    --file-prefix=testpmd1 -- -i --rxq=256 --txq=256 \
    --nb-cores=16 --total-num-mbufs=600000
    
    port stop all
    port config 0 dcb vt off 8 pfc on
    port config 1 dcb vt off 8 pfc on
    port start all
    port stop all
    port config 0 dcb vt off 4 pfc on
    
    This ensures correct queue mapping and avoids issues when switching
    between different DCB configurations across ports.
    
    Fixes: 1a572499beb6 ("app/testpmd: setup DCB forwarding based on traffic class")
    
    Signed-off-by: Talluri Chaitanyababu <chaitanyababux.talluri@intel.com>
    Signed-off-by: Shaiq Wani <shaiq.wani@intel.com>
    Acked-by: Chengwen Feng <fengchengwen@huawei.com>

--- app/test-pmd/config.c
+++ app/test-pmd/config.c
@@ -5286,6 +5286,7 @@ dcb_fwd_config_setup(void)
 	lcoreid_t  lc_id;
 	uint16_t nb_rx_queue, nb_tx_queue;
 	uint16_t i, j, k, sm_id = 0;
+	uint8_t effective_nb_tcs;
 	uint16_t total_tc_num;
 	struct rte_port *port;
 	uint8_t tc = 0;
@@ -5331,6 +5332,7 @@ dcb_fwd_config_setup(void)
 	/* get the dcb info on the first RX and TX ports */
 	(void)rte_eth_dev_get_dcb_info(fwd_ports_ids[rxp], &rxp_dcb_info);
 	(void)rte_eth_dev_get_dcb_info(fwd_ports_ids[txp], &txp_dcb_info);
+	effective_nb_tcs = RTE_MIN(rxp_dcb_info.nb_tcs, txp_dcb_info.nb_tcs);
 
 	for (lc_id = 0; lc_id < cur_fwd_config.nb_fwd_lcores; lc_id++) {
 		fwd_lcores[lc_id]->stream_nb = 0;
@@ -5339,7 +5341,8 @@ dcb_fwd_config_setup(void)
 			/* if the nb_queue is zero, means this tc is
 			 * not enabled on the POOL
 			 */
-			if (rxp_dcb_info.tc_queue.tc_rxq[i][tc].nb_queue == 0)
+			if (rxp_dcb_info.tc_queue.tc_rxq[i][tc].nb_queue == 0 ||
+			    txp_dcb_info.tc_queue.tc_txq[i][tc].nb_queue == 0)
 				break;
 			k = fwd_lcores[lc_id]->stream_nb +
 				fwd_lcores[lc_id]->stream_idx;
@@ -5364,7 +5367,7 @@ dcb_fwd_config_setup(void)
 		sm_id = (streamid_t) (sm_id + fwd_lcores[lc_id]->stream_nb);
 
 		tc++;
-		if (tc < rxp_dcb_info.nb_tcs)
+		if (tc < effective_nb_tcs)
 			continue;
 		/* Restart from TC 0 on next RX port */
 		tc = 0;
@@ -5379,6 +5382,8 @@ dcb_fwd_config_setup(void)
 		/* get the dcb information on next RX and TX ports */
 		rte_eth_dev_get_dcb_info(fwd_ports_ids[rxp], &rxp_dcb_info);
 		rte_eth_dev_get_dcb_info(fwd_ports_ids[txp], &txp_dcb_info);
+
+		effective_nb_tcs = RTE_MIN(rxp_dcb_info.nb_tcs, txp_dcb_info.nb_tcs);
 	}
 }
 
