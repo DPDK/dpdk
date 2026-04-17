d78eb9dded5357922e96f1e27be15d1ab529a32f
David Marchand <david.marchand@redhat.com>
2026-04-17 17:10:02 +0200

    bus/ifpga: remove unused AFU lookup helper
    This helper got left behind after another cleanup.
    
    Fixes: 8418c92811b4 ("net/ipn3ke: remove configuration for i40e port bonding")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>

--- drivers/bus/ifpga/ifpga_bus.c
+++ drivers/bus/ifpga/ifpga_bus.c
@@ -74,19 +74,6 @@ ifpga_find_afu_dev(const struct rte_rawdev *rdev,
 	return NULL;
 }
 
-RTE_EXPORT_INTERNAL_SYMBOL(rte_ifpga_find_afu_by_name)
-struct rte_afu_device *
-rte_ifpga_find_afu_by_name(const char *name)
-{
-	struct rte_afu_device *afu_dev = NULL;
-
-	TAILQ_FOREACH(afu_dev, &ifpga_afu_dev_list, next) {
-		if (!strcmp(afu_dev->device.name, name))
-			return afu_dev;
-	}
-	return NULL;
-}
-
 static const char * const valid_args[] = {
 #define IFPGA_ARG_NAME         "ifpga"
 	IFPGA_ARG_NAME,
