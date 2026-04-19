648d0ed7f364b4a0448d77c61e865d036922e4db
Stephen Hemminger <stephen@networkplumber.org>
2026-04-19 09:09:44 -0700

    net/pcap: add datapath debug logging
    Add datapath debug logging macros (PMD_RX_LOG, PMD_TX_LOG) gated
    on RTE_ETHDEV_DEBUG_RX/TX. Use PMD_TX_LOG in transmit error paths
    to aid debugging without impacting normal datapath performance.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- drivers/net/pcap/pcap_osdep.h
+++ drivers/net/pcap/pcap_osdep.h
@@ -13,6 +13,20 @@
 extern int eth_pcap_logtype;
 #define RTE_LOGTYPE_ETH_PCAP eth_pcap_logtype
 
+#ifdef RTE_ETHDEV_DEBUG_RX
+#define PMD_RX_LOG(level, ...) \
+	RTE_LOG_LINE_PREFIX(level, ETH_PCAP, "%s() rx: ", __func__, __VA_ARGS__)
+#else
+#define PMD_RX_LOG(...) do { } while (0)
+#endif
+
+#ifdef RTE_ETHDEV_DEBUG_TX
+#define PMD_TX_LOG(level, ...) \
+	RTE_LOG_LINE_PREFIX(level, ETH_PCAP, "%s() tx: ", __func__, __VA_ARGS__)
+#else
+#define PMD_TX_LOG(...) do { } while (0)
+#endif
+
 int osdep_iface_index_get(const char *name);
 int osdep_iface_mac_get(const char *name, struct rte_ether_addr *mac);
 
