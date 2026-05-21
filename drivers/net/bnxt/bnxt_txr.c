41f3faf0ce4a062b2bcd08bff68f4b5d2fa7400f
Mohammad Shuab Siddique <mohammad-shuab.siddique@broadcom.com>
2026-05-20 23:45:20 -0600

    net/bnxt: fix IOVA range check for external mbuf
    For extmem, the mbuf head node may be allocated from a pool
    different from the data buffer pool. In that case the IOVA
    address check using rte_mempool_virt2iova() will fail. To fix
    this, skip the IOVA range check for external mbufs.
    Also add unlikely() in checking invalid mbuf size.
    
    Fixes: d01de33f98e2 ("net/bnxt: skip IOVA range check for external mbuf")
    Cc: stable@dpdk.org
    
    Signed-off-by: Damodharam Ammepalli <damodharam.ammepalli@broadcom.com>
    Signed-off-by: Mohammad Shuab Siddique <mohammad-shuab.siddique@broadcom.com>

--- drivers/net/bnxt/bnxt_txr.c
+++ drivers/net/bnxt/bnxt_txr.c
@@ -220,8 +220,10 @@ static int bnxt_invalid_mbuf(struct rte_mbuf *mbuf)
 	if (unlikely(rte_mbuf_check(mbuf, 1, &reason)))
 		return -EINVAL;
 
-	if (unlikely(!(mbuf->ol_flags & RTE_MBUF_F_EXTERNAL) &&
-		     (mbuf->buf_iova < mbuf_size ||
+	if (unlikely(mbuf->ol_flags & RTE_MBUF_F_EXTERNAL))
+		return 0;
+
+	if (unlikely((mbuf->buf_iova < mbuf_size ||
 		      (mbuf->buf_iova != rte_mempool_virt2iova(mbuf) + mbuf_size))))
 		return -EINVAL;
 
