d83627e578c115bb80a85cd817667ee93d95af9d
David Marchand <david.marchand@redhat.com>
2026-06-08 13:56:47 +0200

    bus/pci: fix mapping leak in bus cleanup
    [ upstream commit 858273503bf8643c4befa276b1c4262802b8953b ]
    
    When calling this bus cleanup, PCI resources were not unmapped.
    
    Fixes: 1cab1a40ea9b ("bus: cleanup devices on shutdown")
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>

--- drivers/bus/pci/pci_common.c
+++ drivers/bus/pci/pci_common.c
@@ -441,6 +441,10 @@ pci_cleanup(void)
 			rte_errno = errno;
 			error = -1;
 		}
+
+		if (drv->drv_flags & RTE_PCI_DRV_NEED_MAPPING)
+			rte_pci_unmap_device(dev);
+
 		dev->driver = NULL;
 		dev->device.driver = NULL;
 
