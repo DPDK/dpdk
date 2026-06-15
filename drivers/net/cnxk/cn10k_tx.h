660cb16261e2a47249ba8dc579153af67f066365
Aarnav JP <ajp@marvell.com>
2026-06-15 21:54:38 +0530

    net/cnxk: fix underflow in LSO calculation
    [ upstream commit 9de65cf09f2c9bfdb232fed6166c4cf225674478 ]
    
    Replace branchless mask-based selection with a ternary operator
    to resolve Coverity integer underflow warning. The expression
    -(!w1.il3type) assigned -1 to a uint64_t variable, which is
    well-defined but flagged as an unsigned integer underflow.
    
    Coverity issue: 502004
    
    Fixes: 19f3af2371a7 ("net/cnxk: add Tx burst for CN10K")
    Fixes: 39dc567c1955 ("net/cnxk: add Tx burst for CN9K")
    Fixes: 006c1daa89b9 ("net/cnxk: support Tx burst scalar for CN20K")
    
    Signed-off-by: Aarnav JP <ajp@marvell.com>

--- drivers/net/cnxk/cn10k_tx.h
+++ drivers/net/cnxk/cn10k_tx.h
@@ -1138,10 +1138,8 @@ cn10k_nix_xmit_prepare(struct cn10k_eth_txq *txq,
 	if (flags & NIX_TX_NEED_EXT_HDR && flags & NIX_TX_OFFLOAD_TSO_F &&
 	    (ol_flags & RTE_MBUF_F_TX_TCP_SEG)) {
 		uint16_t lso_sb;
-		uint64_t mask;
 
-		mask = -(!w1.il3type);
-		lso_sb = (mask & w1.ol4ptr) + (~mask & w1.il4ptr) + m->l4_len;
+		lso_sb = (w1.il3type ? w1.il4ptr : w1.ol4ptr) + m->l4_len;
 
 		send_hdr_ext->w0.lso_sb = lso_sb;
 		send_hdr_ext->w0.lso = 1;
@@ -1766,13 +1764,11 @@ cn10k_nix_prepare_tso(struct rte_mbuf *m, union nix_send_hdr_w1_u *w1,
 		      const uint64_t flags, const uint64_t lso_tun_fmt)
 {
 	uint16_t lso_sb;
-	uint64_t mask;
 
 	if (!(ol_flags & RTE_MBUF_F_TX_TCP_SEG))
 		return;
 
-	mask = -(!w1->il3type);
-	lso_sb = (mask & w1->ol4ptr) + (~mask & w1->il4ptr) + m->l4_len;
+	lso_sb = (w1->il3type ? w1->il4ptr : w1->ol4ptr) + m->l4_len;
 
 	w0->u |= BIT(14);
 	w0->lso_sb = lso_sb;
