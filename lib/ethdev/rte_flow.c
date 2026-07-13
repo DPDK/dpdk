2aae80844fa0e3d1245eb5ee77d2d60a9d58c0cf
Dariusz Sosnowski <dsosnowski@nvidia.com>
2026-07-13 12:59:35 +0200

    ethdev: remove flow metadata symbol aliases
    Version aliases for rte_flow_dynf_metadata_register
    and EXPERIMENTAL version on rte_flow_dynf_metadata_offs
    and rte_flow_dynf_metadata_mask symbols added in
    commit 56045d198968 ("ethdev: add aliases for flow metadata symbols")
    are not needed after ABI version bump.
    This patch removes them.
    
    Signed-off-by: Dariusz Sosnowski <dsosnowski@nvidia.com>
    Acked-by: David Marchand <david.marchand@redhat.com>

--- lib/ethdev/rte_flow.c
+++ lib/ethdev/rte_flow.c
@@ -23,11 +23,11 @@
 #define FLOW_LOG RTE_ETHDEV_LOG_LINE
 
 /* Mbuf dynamic field name for metadata. */
-RTE_EXPORT_EXPERIMENTAL_SYMBOL(rte_flow_dynf_metadata_offs, 19.11)
+RTE_EXPORT_SYMBOL(rte_flow_dynf_metadata_offs)
 int32_t rte_flow_dynf_metadata_offs = -1;
 
 /* Mbuf dynamic field flag bit number for metadata. */
-RTE_EXPORT_EXPERIMENTAL_SYMBOL(rte_flow_dynf_metadata_mask, 19.11)
+RTE_EXPORT_SYMBOL(rte_flow_dynf_metadata_mask)
 uint64_t rte_flow_dynf_metadata_mask;
 
 /**
@@ -282,7 +282,9 @@ static const struct rte_flow_desc_data rte_flow_desc_action[] = {
 	MK_FLOW_ACTION(JUMP_TO_TABLE_INDEX, sizeof(struct rte_flow_action_jump_to_table_index)),
 };
 
-RTE_DEFAULT_SYMBOL(27, int, rte_flow_dynf_metadata_register, (void))
+RTE_EXPORT_SYMBOL(rte_flow_dynf_metadata_register)
+int
+rte_flow_dynf_metadata_register(void)
 {
 	int offset;
 	int flag;
@@ -313,11 +315,6 @@ error:
 	rte_flow_dynf_metadata_offs = -1;
 	rte_flow_dynf_metadata_mask = UINT64_C(0);
 	return -rte_errno;
-}
-
-RTE_VERSION_EXPERIMENTAL_SYMBOL(int, rte_flow_dynf_metadata_register, (void))
-{
-	return rte_flow_dynf_metadata_register();
 }
 
 static inline void
