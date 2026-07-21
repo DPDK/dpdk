f9b45075fb8deda3fbaab4243749e78e50b40e29
Bruce Richardson <bruce.richardson@intel.com>
2026-07-21 14:23:49 +0100

    test/event_eth_tx_adapter: skip tests if no eventdev
    If there is no eventdev present, and the SW eventdev driver is not
    available, skip the eth tx adapter tests, rather than failing on event
    creation.
    
    Note: We keep the failure case where we do have the event/sw driver
    built, but fail to create an instance of it.
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>

--- app/test/test_event_eth_tx_adapter.c
+++ app/test/test_event_eth_tx_adapter.c
@@ -202,18 +202,23 @@ deinit_ports(void)
 static int
 testsuite_setup(void)
 {
-	const char *vdev_name = "event_sw0";
-
 	int err = init_ports();
 	TEST_ASSERT(err == 0, "Port initialization failed err %d\n", err);
 
 	if (rte_event_dev_count() == 0) {
+#ifdef RTE_EVENT_SW
+		const char *vdev_name = "event_sw0";
+
 		printf("Failed to find a valid event device,"
 			" testing with event_sw0 device\n");
 		err = rte_vdev_init(vdev_name, NULL);
 		TEST_ASSERT(err == 0, "vdev %s creation failed  %d\n",
 			vdev_name, err);
 		event_dev_delete = 1;
+#else
+		printf("Failed to find a valid event device, skipping test\n");
+		return TEST_SKIPPED;
+#endif
 	}
 	return err;
 }
