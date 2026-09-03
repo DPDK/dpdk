a7c8e8c3d4f91323bd96c6323449c73ee45e12e8
Jie Liu <liujie5@linkdatatechnology.com>
2026-09-03 09:55:02 +0800

    net/sxe2: clean up duplicate function declarations
    Remove duplicate function declarations from the sxe2 driver headers:
    - sxe2_drv_promisc_config, sxe2_drv_vsi_info_get and
      sxe2_drv_rxq_mapping_set in sxe2_cmd_chnl.h
    - sxe2_dev_pci_seg_unmap in sxe2_ethdev.h
    - sxe2_rx_pkts_scattered in sxe2_txrx_poll.h
    
    Cc: stephen@networkplumber.org
    Signed-off-by: Jie Liu <liujie5@linkdatatechnology.com>

--- drivers/net/sxe2/sxe2_txrx_poll.h
+++ drivers/net/sxe2/sxe2_txrx_poll.h
@@ -13,8 +13,6 @@ uint16_t sxe2_tx_pkts_simple(void *tx_queue, struct rte_mbuf **tx_pkts, uint16_t
 
 uint16_t sxe2_rx_pkts_scattered(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts);
 
-uint16_t sxe2_rx_pkts_scattered(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts);
-
 uint16_t sxe2_rx_pkts_scattered_split(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts);
 
 #endif /* SXE2_TXRX_POLL_H */
