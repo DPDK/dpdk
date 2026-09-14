94ddc048ba84402f7509cfe9704896eba6666388
Bruce Richardson <bruce.richardson@intel.com>
2026-09-14 17:22:33 +0100

    net/intel: fix overflow on store of single-queue settings
    The Rx and Tx single queue settings in idpf and cpfl drivers are both
    boolean values, but the parse_bool function writes to them as int. This
    causes an extra 3 bytes to be written beyond the variable itself.  Fix
    by changing store type to bool.
    
    Bugzilla ID: 2042
    Fixes: 549343c25db8 ("net/idpf: support device initialization")
    Fixes: 2d823ecd671c ("net/cpfl: support device initialization")
    Cc: stable@dpdk.org
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
    Acked-by: Praveen Shetty <praveen.shetty@intel.com>

--- drivers/net/intel/cpfl/cpfl_ethdev.c
+++ drivers/net/intel/cpfl/cpfl_ethdev.c
@@ -1561,7 +1561,7 @@ parse_vport(const char *key, const char *value, void *args)
 static int
 parse_bool(const char *key, const char *value, void *args)
 {
-	int *i = args;
+	bool *i = args;
 	char *end;
 	int num;
 
@@ -1575,7 +1575,7 @@ parse_bool(const char *key, const char *value, void *args)
 		return -EINVAL;
 	}
 
-	*i = num;
+	*i = (num == 1);
 	return 0;
 }
 
